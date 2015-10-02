028b112301ccb9d68f7a41a6a8cdff5b89c12811
lindexi_gd <lindexi_gd@163.com>
2015-10-02 17:09:14 +0800

    2015年10月2日17:08:13

---
+++ cstorage_file/cstorage_file/ViewModel/notify_property.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace ViewModel
+{
+    /// <summary>
+    /// 提供继承通知UI改变值
+    /// </summary>
+    public class notify_property : INotifyPropertyChanged
+    {
+        public notify_property()
+        {
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this , new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
