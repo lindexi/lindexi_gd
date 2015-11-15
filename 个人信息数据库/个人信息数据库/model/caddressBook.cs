5d46e99bbde64118a721cfe768fe4fd4e2b4fb8c
lindexi_gd <lindexi_gd@163.com>
2015-11-15 21:23:53 +0800

    通讯

---
+++ 个人信息数据库/个人信息数据库/model/caddressBook.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 个人信息数据库.model
+{
+    public class caddressBook
+    {
+        public caddressBook()
+        {
+
+        }
+        /// <summary>
+        /// 通讯人姓名
+        /// </summary>
+        public string name
+        {
+            set;
+            get;
+        }
+        /// <summary>
+        /// 联系方式
+        /// </summary>
+        public string contact
+        {
+            set;
+            get;
+        }
+        /// <summary>
+        /// 工作地点
+        /// </summary>
+        public string address
+        {
+            set;
+            get;
+        }
+        /// <summary>
+         /// 城市
+         /// </summary>
+        public string city
+        {
+            set;
+            get;
+        }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string comment
+        {
+            set;
+            get;
+        }
+    }
+}
--- 个人信息数据库/个人信息数据库/viewModel.cs
+++ 个人信息数据库/个人信息数据库/viewModel.cs
@@ -5,16 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Sql;
 using System.Data.SqlClient;
-
+using 个人信息数据库.model;
 namespace 个人信息数据库
 {
-    public partial class viewModel: notify_property
+    public partial class viewModel : notify_property
     {
         public viewModel()
         {
 
-            SqlConnection sql = new SqlConnection();
+            //SqlConnection sql = new SqlConnection();
+
         }
-        
+        public caddressBook[] addressBook
+        {
+            set;
+            get;
+        } =
+            {
+                new caddressBook()
+                {
+                    name ="通讯人姓名",
+                    contact ="联系方式",
+                    address ="工作地点",
+                    city ="城市",
+                    comment ="备注"
+                },
+                new caddressBook()
+                {
+                    name ="张三",
+                    contact ="1",
+                    address ="中国",
+                    city =" ",
+                    comment =" "
+                } ,
+                new caddressBook()
+                {
+                    name ="张三",
+                    contact ="1",
+                    address ="中国",
+                    city =" ",
+                    comment =" "
+                }
+            };
     }
 }
