d99668450f0cfc00968ac0770f301edddb619874
lindexi_gd <lindexi_gd@163.com>
2015-11-15 21:45:25 +0800

    随机名

---
+++ 个人信息数据库/个人信息数据库/model/model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 个人信息数据库.model
+{
+    public class model
+    {
+        public model()
+        {
+            ran = new Random();
+
+
+
+            string name;
+            int n = 100;
+            int[] 中文 = new int[2] { 19968 , 40895 };
+            name = "";
+            for (int i = 0; i < n; i++)
+            {
+                name = "";
+                for (int j = ran.Next(1 , 3) + 1; j > 0; j--)
+                {
+                    name += Convert.ToChar(ran.Next(中文[0] , 中文[1]));
+                }
+            }
+        }
+        Random ran
+        {
+            set;
+            get;
+        }
+    }
+}
