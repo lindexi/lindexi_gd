084d2c2462b1c0dc03275d45dadfee8f096b9879
lindexi_gd <lindexi_gd@163.com>
2015-11-15 21:53:27 +0800

    通讯人姓名

--- 个人信息数据库/个人信息数据库/model/model.cs
+++ 个人信息数据库/个人信息数据库/model/model.cs
@@ -32,5 +32,15 @@ namespace 个人信息数据库.model
             set;
             get;
         }
+        private string ranstr(int n)
+        {
+            StringBuilder str = new StringBuilder();
+            int[] 中文 = new int[2] { 19968 , 40895 };
+            for (int i = 0; i < n; i++)
+            {
+                str.Append(Convert.ToChar(ran.Next(中文[0] , 中文[1])));
+            }
+            return str.ToString();
+        }
     }
 }
