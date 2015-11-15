917a5b873c8da62f12d1856fb2bc32c80d98f055
lindexi_gd <lindexi_gd@163.com>
2015-11-15 20:40:56 +0800

    数据库

---
+++ 个人信息数据库/个人信息数据库/viewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace 个人信息数据库
+{
+    public partial class viewModel: notify_property
+    {
+        public viewModel()
+        {
+
+            SqlConnection sql = new SqlConnection();
+        }
+        
+    }
+}
