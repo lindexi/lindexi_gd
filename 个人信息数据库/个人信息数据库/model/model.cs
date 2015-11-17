6f87b7868b789698143804a8dc8b05f4a46076c9
lindexi_gd <lindexi_gd@163.com>
2015-11-17 20:10:55 +0800

    建立表 插入数据

--- 个人信息数据库/个人信息数据库/model/model.cs
+++ 个人信息数据库/个人信息数据库/model/model.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 
 namespace 个人信息数据库.model
@@ -17,6 +18,27 @@ namespace 个人信息数据库.model
             set;
             get;
         }
+
+        //public void ce()
+        {
+            string inputJsonString = @"
+                [
+                    {StudentID:'100',Name:'aaa',Hometown:'china'},
+                    {StudentID:'101',Name:'bbb',Hometown:'us'},
+                    {StudentID:'102',Name:'ccc',Hometown:'england'}
+                ]";
+            JArray jsonObj = JArray.Parse(inputJsonString);
+            string message = @"<table border='1'>
+                    <tr><td width='80'>StudentID</td><td width='100'>Name</td><td width='100'>Hometown</td></tr>";
+            string tpl = "<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>";
+            foreach (JObject jObject in jsonObj)
+            {
+                message += String.Format(tpl , jObject["StudentID"] , jObject["Name"] , jObject["Hometown"]);
+            }
+            message += "</table>";
+            //lbMsg.InnerHtml = message;
+        }
+
         private string ranstr(int n)
         {
             StringBuilder str = new StringBuilder();
