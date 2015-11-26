aa9a4ee788c4c72164cf94b1fabe9c411935a46e
lindexi_gd <lindexi_gd@163.com>
2015-11-26 21:00:37 +0800

    visibility

---
+++ 个人信息数据库/个人信息数据库/ViewModel/viewproperty.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 个人信息数据库.model;
+
+namespace 个人信息数据库.ViewModel
+{
+    public class viewproperty : notify_property
+    {
+        public viewproperty(viewModel _viewModel,model.model _model)
+        {
+            this._model = _model;
+            this._viewModel = _viewModel;
+        }
+
+        public cproperty Property
+        {
+            set
+            {
+                _property = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _property;
+            }
+        }
+        public System.Collections.ObjectModel.ObservableCollection<cproperty> lproperty
+        {
+            set;
+            get;
+        } = new System.Collections.ObjectModel.ObservableCollection<cproperty>();
+        public System.Windows.Visibility visibility
+        {
+            set
+            {
+                _visibility = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _visibility;
+            }
+        }
+        private System.Windows.Visibility _visibility= System.Windows.Visibility.Hidden;
+
+        private viewModel _viewModel;
+        private cproperty _property = new cproperty();
+        private model.model _model;
+
+    }
+}
