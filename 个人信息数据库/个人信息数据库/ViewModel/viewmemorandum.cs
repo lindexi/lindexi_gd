43fc44918e45f62dffabdd78d44d4891ba68627e
lindexi_gd <lindexi_gd@163.com>
2015-11-26 20:17:56 +0800

    viewmemorandum

---
+++ 个人信息数据库/个人信息数据库/ViewModel/viewmemorandum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 个人信息数据库.model;
+
+namespace 个人信息数据库.ViewModel
+{
+    public class viewmemorandum : notify_property
+    {
+        public viewmemorandum(model.model _model)
+        {
+            this._model = _model;
+        }
+
+        public cmemorandum memorandum
+        {
+            set
+            {
+                _memorandum = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _memorandum;
+            }
+        }
+
+        public System.Collections.ObjectModel.ObservableCollection<cmemorandum> lmemorandum
+        {
+            set;
+            get;
+        } = new System.Collections.ObjectModel.ObservableCollection<cmemorandum>();
+
+
+        private cmemorandum _memorandum = new cmemorandum();
+        private model.model _model;
+    }
+}
