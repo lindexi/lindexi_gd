e12e3c191d0c526fd50b8429b7fe93e8888dbbcc
lindexi_gd <lindexi_gd@163.com>
2015-12-03 20:09:47 +0800

    芒果

--- classifyapp/classifyapp/view/movie.xaml.cs
+++ classifyapp/classifyapp/view/movie.xaml.cs
@@ -41,5 +41,25 @@ namespace classifyapp.view
                 return model.cmodel();
             }
         }
+
+        private void blibli_Click(object sender , RoutedEventArgs e)
+        {
+            //https://www.microsoft.com/zh-cn/store/apps/%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9%E5%8A%A8%E7%94%BB/9wzdncrfj28k
+            string ProductId = "9wzdncrfj28k";
+            _model.windowsapp(ProductId);
+        }
+
+        private void manguo_Click(object sender , RoutedEventArgs e)
+        {
+            string ProductId = "9nblggh62k52";
+            _model.windowsapp(ProductId);
+        }
+
+        private void youku_Click(object sender , RoutedEventArgs e)
+        {
+            string ProductId = "9wzdncrdv0bk";
+            _model.windowsapp(ProductId);
+        }
+
     }
 }
