95e6754f7f0fc6cc2f9f3f56e31749ae0aa4dc3a
lindexi_gd <lindexi_gd@163.com>
2015-12-07 10:58:17 +0800

    同学录notify

---
+++ impressAlumniBook/impressAlumniBook/viewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impressAlumniBook
+{
+    public class viewModel:notify_property
+    {
+        public viewModel()
+        {
+
+        }
+    }
+}
