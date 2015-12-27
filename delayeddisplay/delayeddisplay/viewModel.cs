765f76a9e47ad6c736a6142560d1e5c6c07fdc89
lindexi_gd <lindexi_gd@163.com>
2015-12-27 11:15:35 +0800

    延迟显示图片

---
+++ delayeddisplay/delayeddisplay/viewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delayeddisplay
+{
+    public class viewModel : notify_property
+    {
+        public viewModel()
+        {
+            ce();
+        }
+
+        public void ce()
+        {
+            System.Net.Http.HttpClient http = new System.Net.Http.HttpClient();
+            string str = "http://appserver.m.bing.net/BackgroundImageService/TodayImageService.svc/GetTodayImage?dateOffset=0&urlEncodeHeaders=true&osName=windowsPhone&osVersion=8.10&orientation=480x800&deviceName=WP8&mkt=en-US";
+
+            Uri uri;
+            uri = new Uri(str);
+            student.img = new Windows.UI.Xaml.Media.Imaging.BitmapImage(uri);
+
+            //System.Net.HttpWebRequest httpweb= (System.Net.HttpWebRequest)System.Net.WebRequest.Create(str);
+            //httpweb.Method = "get";
+            //httpweb.BeginGetRequestStream((result) =>
+            //{
+            //    System.Net.HttpWebResponse response;
+
+            //    try
+            //    {
+            //        response = (System.Net.HttpWebResponse)httpweb.EndGetResponse(result);
+            //    }
+            //    catch 
+            //    {
+
+                    
+            //    }
+
+            //    if (httpweb.HaveResponse)
+            //    {
+            //        Uri uri;
+            //        uri = new Uri(str);
+            //        student.img = new Windows.UI.Xaml.Media.Imaging.BitmapImage(uri);
+            //    }
+            //}
+            //, null);
+        }
+
+        public modelbusiness.student student
+        {
+            set
+            {
+                _student = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _student;
+            }
+        }
+        private modelbusiness.student _student=new modelbusiness.student()
+        {
+            name="学生",
+            city="上海",
+            age="100",
+            //img=new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:///Assets/gamersky_05small_10_20155131557102.jpg"))
+        };
+        private modelbusiness.model _model=new modelbusiness.model();
+    }
+}
---
+++ delayeddisplay/modelbusiness/model.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Sockets;
+
+namespace modelbusiness
+{
+    public class model
+    {
+        public model()
+        {
+
+        }
+
+        public void ce()
+        {
+            
+        }
+
+
+        private int _post;
+
+        private string _ip;
+
+        /// <summary>
+        /// 端口
+        /// </summary>
+        public int post
+        {
+            set
+            {
+                _post = value;
+            }
+            get
+            {
+                return _post;
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ip
+        {
+            set
+            {
+                _ip = value;
+            }
+            get
+            {
+                return _ip;
+            }
+
+        }
+    }    
+
+    public class student:notify_property
+    {
+        public string name
+        {
+            set
+            {
+                _name = value;
+                OnPropertyChanged("name");
+            }
+            get
+            {
+                return _name;
+            }
+        }
+        public string city
+        {
+            set
+            {
+                _city = value;
+                OnPropertyChanged("city");
+            }
+            get
+            {
+                return _city;
+            }
+        }
+        public string age
+        {
+            set
+            {
+                _age = value;
+                OnPropertyChanged("age");
+            }
+            get
+            {
+                return _age;
+            }
+        }
+        public Windows.UI.Xaml.Media.ImageSource img
+        {
+            set
+            {
+                _img = value;
+                OnPropertyChanged("img");
+            }
+            get
+            {
+                return _img;
+            }
+        }
+        private string _name;
+        private string _city;
+        private string _age;
+        private Windows.UI.Xaml.Media.ImageSource _img;
+
+    }
+}
