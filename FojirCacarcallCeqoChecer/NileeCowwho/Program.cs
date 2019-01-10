c427370ec39d1d2c3bffd2e3098416c75f59434d
lindexi <lindexi_gd@163.com>
2019-01-10 20:51:33 +0800

    CizallCearno

--- FojirCacarcallCeqoChecer/NileeCowwho/Program.cs
+++ FojirCacarcallCeqoChecer/NileeCowwho/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text;
 using System.Xml.Serialization;
 
 namespace NileeCowwho
@@ -20,28 +18,6 @@ namespace NileeCowwho
             var trayteeTrecabawKayloo = dosoogooBidrorlimurTrearfama.Serialize(muyorkearTisdusilu);
             Console.WriteLine(trayteeTrecabawKayloo);
             dosoogooBidrorlimurTrearfama.Deserialize(trayteeTrecabawKayloo);
-        }
-    }
-
-    public class DosoogooBidrorlimurTrearfama
-    {
-        public string Serialize(MuyorkearTisdusilu jirrowWouniraltere)
-        {
-            var reserXelpiRoorairlurmer = new XmlSerializer(typeof(MuyorkearTisdusilu));
-
-            var str = new StringBuilder();
-            TextWriter toucoheaSairoubeejasGoraxallza = new StringWriter(str);
-
-            reserXelpiRoorairlurmer.Serialize(toucoheaSairoubeejasGoraxallza, jirrowWouniraltere);
-
-            return str.ToString();
-        }
-
-        public MuyorkearTisdusilu Deserialize(string qayneNoceeNemnuka)
-        {
-            var reserXelpiRoorairlurmer = new XmlSerializer(typeof(MuyorkearTisdusilu));
-
-            return (MuyorkearTisdusilu) reserXelpiRoorairlurmer.Deserialize(new StringReader(qayneNoceeNemnuka));
         }
     }
 }
