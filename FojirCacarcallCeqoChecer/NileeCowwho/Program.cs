581aa52c69ad13be86f7cf0ec5fcdcca97fba446
lindexi <lindexi_gd@163.com>
2019-01-10 20:51:01 +0800

    ZousevicaiSeeyudaMorhedeHehou

--- FojirCacarcallCeqoChecer/NileeCowwho/Program.cs
+++ FojirCacarcallCeqoChecer/NileeCowwho/Program.cs
@@ -10,13 +10,38 @@ namespace NileeCowwho
         static void Main(string[] args)
         {
             var reserXelpiRoorairlurmer = new XmlSerializer(typeof(MuyorkearTisdusilu));
-            var muyorkearTisdusilu = new MuyorkearTisdusilu();
+
+            var muyorkearTisdusilu = new MuyorkearTisdusilu()
+            {
+               
+            };
+
+            var dosoogooBidrorlimurTrearfama = new DosoogooBidrorlimurTrearfama();
+            var trayteeTrecabawKayloo = dosoogooBidrorlimurTrearfama.Serialize(muyorkearTisdusilu);
+            Console.WriteLine(trayteeTrecabawKayloo);
+            dosoogooBidrorlimurTrearfama.Deserialize(trayteeTrecabawKayloo);
+        }
+    }
+
+    public class DosoogooBidrorlimurTrearfama
+    {
+        public string Serialize(MuyorkearTisdusilu jirrowWouniraltere)
+        {
+            var reserXelpiRoorairlurmer = new XmlSerializer(typeof(MuyorkearTisdusilu));
 
             var str = new StringBuilder();
             TextWriter toucoheaSairoubeejasGoraxallza = new StringWriter(str);
-            reserXelpiRoorairlurmer.Serialize(toucoheaSairoubeejasGoraxallza, muyorkearTisdusilu);
 
-            Console.WriteLine(str.ToString());
+            reserXelpiRoorairlurmer.Serialize(toucoheaSairoubeejasGoraxallza, jirrowWouniraltere);
+
+            return str.ToString();
+        }
+
+        public MuyorkearTisdusilu Deserialize(string qayneNoceeNemnuka)
+        {
+            var reserXelpiRoorairlurmer = new XmlSerializer(typeof(MuyorkearTisdusilu));
+
+            return (MuyorkearTisdusilu) reserXelpiRoorairlurmer.Deserialize(new StringReader(qayneNoceeNemnuka));
         }
     }
 }
