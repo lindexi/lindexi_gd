a083adc3a3a390abbda6c6ee084c9c8aafbb0304
lindexi <lindexi_gd@163.com>
2019-01-10 20:02:52 +0800

    KeezokowgeJewhalsu

--- FojirCacarcallCeqoChecer/FojirCacarcallCeqoChecer/Startup.cs
+++ FojirCacarcallCeqoChecer/FojirCacarcallCeqoChecer/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using FojirCacarcallCeqoChecer.Models;
 
 namespace FojirCacarcallCeqoChecer
 {
@@ -25,6 +27,9 @@ namespace FojirCacarcallCeqoChecer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<RasasCootrorvouContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RasasCootrorvouContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
