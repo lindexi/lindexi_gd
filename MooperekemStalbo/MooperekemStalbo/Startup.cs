eca874d218186f26a8f47c116ed0d530e0107352
lindexi <lindexi_gd@163.com>
2019-02-18 18:58:08 +0800

    用户可以使用文件

--- MooperekemStalbo/MooperekemStalbo/Startup.cs
+++ MooperekemStalbo/MooperekemStalbo/Startup.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using MooperekemStalbo.Models;
 
 namespace MooperekemStalbo
@@ -39,6 +44,18 @@ namespace MooperekemStalbo
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Debug.WriteLine(folder);
+
+            folder = Path.Combine(folder, "Package");
+            Directory.CreateDirectory(folder);
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(folder),
+                RequestPath = "/storage/package"
+            });
 
             app.UseMvc();
         }
