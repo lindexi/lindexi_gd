58978bb59a74a552e89b9c79bdb7b0846796a895
lindexi <lindexi_gd@163.com>
2019-02-22 12:50:08 +0800

    不使用其他代码拿到进程参数

--- GetProcessCommandLine/HurwhelayNagapeserjeCagai/Program.cs
+++ GetProcessCommandLine/HurwhelayNagapeserjeCagai/Program.cs
@@ -5,37 +5,152 @@ using System.Text;
 
 namespace HurwhelayNagapeserjeCagai
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static string GetCommandLineOfProcess(int processId)
         {
-            foreach (var process in Process.GetProcesses())
+            var pid = processId;
+
+            var pbi = new NativeMethods.PROCESS_BASIC_INFORMATION();
+
+            IntPtr proc = NativeMethods.OpenProcess
+            (
+                PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid
+            );
+
+            if (proc == IntPtr.Zero)
             {
-                Console.WriteLine($"{process.ProcessName} {GetCommandLineOfProcess(process)}");
+                return "";
             }
+
+            if (NativeMethods.NtQueryInformationProcess(proc, 0, ref pbi, pbi.Size, IntPtr.Zero) == 0)
+            {
+                var buff = new byte[IntPtr.Size];
+                if (NativeMethods.ReadProcessMemory
+                (
+                    proc,
+                    (IntPtr) (pbi.PebBaseAddress.ToInt32() + 0x10),
+                    buff,
+                    IntPtr.Size, out _
+                ))
+                {
+                    var buffPtr = BitConverter.ToInt32(buff, 0);
+                    var commandLine = new byte[Marshal.SizeOf(typeof(NativeMethods.UNICODE_STRING))];
+
+                    if
+                    (
+                        NativeMethods.ReadProcessMemory
+                        (
+                            proc, (IntPtr) (buffPtr + 0x40),
+                            commandLine,
+                            Marshal.SizeOf(typeof(NativeMethods.UNICODE_STRING)), out _
+                        )
+                    )
+                    {
+                        var ucsData = ByteArrayToStructure<NativeMethods.UNICODE_STRING>(commandLine);
+                        var parms = new byte[ucsData.Length];
+                        if
+                        (
+                            NativeMethods.ReadProcessMemory
+                            (
+                                proc, ucsData.buffer, parms,
+                                ucsData.Length, out _
+                            )
+                        )
+                        {
+                            return Encoding.Unicode.GetString(parms);
+                        }
+                    }
+                }
+            }
+
+            NativeMethods.CloseHandle(proc);
+
+            return "";
         }
 
-        public static string GetCommandLineOfProcess(Process process)
+        [STAThread]
+        private static void Main(string[] args)
         {
-            // max size of a command line is USHORT/sizeof(WCHAR), so we are going
-            // just allocate max USHORT for sanity sake.
-            var stringBuilder = new StringBuilder(0xFFFF);
             if (Environment.Is64BitProcess)
             {
-                GetProcCmdLine64((uint) process.Id, stringBuilder, (uint) stringBuilder.Capacity);
-            }
-            else
-            {
-                GetProcCmdLine32((uint) process.Id, stringBuilder, (uint) stringBuilder.Capacity);
+                throw new InvalidOperationException("暂时只支持x86程序");
             }
 
-            return stringBuilder.ToString();
+            foreach (var process in Process.GetProcesses())
+            {
+                Console.WriteLine($"{process.ProcessName} {GetCommandLineOfProcess(process.Id)}");
+            }
         }
 
-        [DllImport("ProcCmdLine32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetProcCmdLine")]
-        private static extern bool GetProcCmdLine32(uint nProcId, StringBuilder stringBuilder, uint dwSizeBuf);
 
-        [DllImport("ProcCmdLine64.dll", CharSet = CharSet.Unicode, EntryPoint = "GetProcCmdLine")]
-        private static extern bool GetProcCmdLine64(uint nProcId, StringBuilder stringBuilder, uint dwSizeBuf);
+        private const uint PROCESS_QUERY_INFORMATION = 0x400;
+        private const uint PROCESS_VM_READ = 0x010;
+
+        private static T ByteArrayToStructure<T>(byte[] bytes) where T : struct
+        {
+            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            var stuff = (T) Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+            handle.Free();
+            return stuff;
+        }
+
+        private static class NativeMethods
+        {
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool CloseHandle(IntPtr hObject);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            internal static extern IntPtr OpenProcess
+            (
+                uint dwDesiredAccess,
+                [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle,
+                int dwProcessId
+            );
+
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool ReadProcessMemory
+            (
+                IntPtr hProcess,
+                IntPtr lpBaseAddress,
+                byte[] lpBuffer,
+                int nSize,
+                out IntPtr lpNumberOfBytesRead
+            );
+
+            [DllImport("ntdll.dll")]
+            internal static extern int NtQueryInformationProcess
+            (
+                IntPtr ProcessHandle,
+                uint ProcessInformationClass,
+                ref PROCESS_BASIC_INFORMATION ProcessInformation,
+                uint ProcessInformationLength,
+                IntPtr ReturnLength
+            );
+
+            [StructLayout(LayoutKind.Sequential, Pack = 1)]
+            internal struct PROCESS_BASIC_INFORMATION
+            {
+                internal int ExitProcess;
+                internal IntPtr PebBaseAddress;
+                internal IntPtr AffinityMask;
+                internal int BasePriority;
+                internal IntPtr UniqueProcessId;
+                internal IntPtr InheritedFromUniqueProcessId;
+
+                internal uint Size => (uint) Marshal.SizeOf(typeof(PROCESS_BASIC_INFORMATION));
+            }
+
+            [StructLayout(LayoutKind.Sequential, Pack = 1)]
+            internal struct UNICODE_STRING
+            {
+                internal ushort Length;
+                internal ushort MaximumLength;
+                internal IntPtr buffer;
+            }
+        }
     }
 }
