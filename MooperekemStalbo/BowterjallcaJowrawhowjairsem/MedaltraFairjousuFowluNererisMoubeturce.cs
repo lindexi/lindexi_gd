0c8cc9253d6eafbd92f0c692f36ef02831803a1f
lindexi <lindexi_gd@163.com>
2019-03-04 21:03:16 +0800

    判断上传文件

--- MooperekemStalbo/BowterjallcaJowrawhowjairsem/MedaltraFairjousuFowluNererisMoubeturce.cs
+++ MooperekemStalbo/BowterjallcaJowrawhowjairsem/MedaltraFairjousuFowluNererisMoubeturce.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace MooperekemStalbo.Controllers
 {
     public class MedaltraFairjousuFowluNererisMoubeturce
     {
-        public void CheckFile(FileInfo file)
+        public Package CheckFile(FileInfo file)
         {
             // 如果文件夹不为空
             if (!Directory.Exists(Folder))
@@ -14,7 +19,59 @@ namespace MooperekemStalbo.Controllers
                 Folder = Path.GetTempPath();
             }
 
-            ZipFile.ExtractToDirectory(file.FullName, Path.Combine(Folder, "temp", Guid.NewGuid().ToString()));
+            var folder = Path.Combine(Folder, "temp", Guid.NewGuid().ToString());
+            ZipFile.ExtractToDirectory(file.FullName, folder);
+
+            CheckPackage(folder);
+
+            return _package;
+        }
+
+        internal void CheckPackage(string folder)
+        {
+            var file = Path.Combine(folder, "Package.xml");
+            if (!File.Exists(file))
+            {
+                throw new ArgumentException("找不到 Package.xml 文件");
+            }
+
+            var package = ParseFile(file);
+
+            if (string.IsNullOrEmpty(package.Name) || !_regex.IsMatch(package.Name) ||
+                (package.Name[0] >= '0' && package.Name[0] <= '9'))
+            {
+                throw new ArgumentException($"上传 package 的 Name 不符合规范");
+            }
+
+            // 判断文件存在
+            if (!File.Exists(Path.Combine(folder, "File", package.File)))
+            {
+                throw new ArgumentException($"找不到文件 package file {package.File}");
+            }
+
+            // 判断 Name 是否对的
+
+            if (_regex == null)
+            {
+                _regex = new Regex(@"^[a-zA-Z0-9]+$", RegexOptions.Compiled);
+            }
+
+            _package = package;
+        }
+
+        private Package _package;
+
+        private static Regex _regex;
+
+        private Package ParseFile(string file)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Package));
+            using (var stream = new FileStream(file, FileMode.Open))
+            {
+                var read = new XmlTextReader(new StreamReader(stream, Encoding.UTF8));
+
+                return (Package) xmlSerializer.Deserialize(read);
+            }
         }
 
         public string Folder { get; set; }
--- MooperekemStalbo/BowterjallcaJowrawhowjairsem/Program.cs
+++ MooperekemStalbo/BowterjallcaJowrawhowjairsem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using MooperekemStalbo.Controllers;
 
 namespace BowterjallcaJowrawhowjairsem
@@ -8,9 +9,20 @@ namespace BowterjallcaJowrawhowjairsem
         static void Main(string[] args)
         {
             var folder = AppDomain.CurrentDomain.BaseDirectory;
+            var file = Path.Combine(folder, "WeserefairsouWorlokeqa.zip");
+            folder = Path.Combine(folder, "temp");
+            if (Directory.Exists(folder))
+            {
+                Directory.Delete(folder, true);
+            }
+
+            Directory.CreateDirectory(folder);
+
             // 判断上传的文件是否可以
             var medaltraFairjousuFowluNererisMoubeturce = new MedaltraFairjousuFowluNererisMoubeturce();
             medaltraFairjousuFowluNererisMoubeturce.Folder = folder;
+
+            medaltraFairjousuFowluNererisMoubeturce.CheckFile(new FileInfo(file));
         }
     }
 }
