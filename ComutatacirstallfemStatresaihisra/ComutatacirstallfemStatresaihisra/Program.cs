e4b6ef5b433b7aebc2a7f09865fa684e1474f2e9
lindexi <lindexi_gd@163.com>
2019-03-12 10:33:46 +0800

    修改命名

--- ComutatacirstallfemStatresaihisra/ComutatacirstallfemStatresaihisra/Program.cs
+++ ComutatacirstallfemStatresaihisra/ComutatacirstallfemStatresaihisra/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ComutatacirstallfemStatresaihisra
@@ -36,7 +33,7 @@ namespace ComutatacirstallfemStatresaihisra
                     var n = random.Next(100, 1000);
                     var backgroundTask = new BackgroundTask($"等待{n}毫秒", () => { }, TimeSpan.FromMilliseconds(n));
                     backgroundTaskList.Add(backgroundTask);
-                    threadManagerService.AddBackgroundTask(backgroundTask);
+                    threadManagerService.RunBackgroundTask(backgroundTask);
                 }
                 else
                 {
@@ -53,180 +50,5 @@ namespace ComutatacirstallfemStatresaihisra
 
             Console.Read();
         }
-    }
-
-    /// <summary>
-    ///  进程服务
-    /// </summary>
-    class ThreadManagerService
-    {
-        public void AddBackgroundTask(BackgroundTask task)
-        {
-            lock (_obj)
-            {
-                BackgroundTaskList.Add(task);
-                Log("添加任务 " + task.Name);
-            }
-        }
-
-        public void RemoveBackgroundTask(BackgroundTask task)
-        {
-            lock (_obj)
-            {
-                BackgroundTaskList.Remove(task);
-                Log("删除");
-            }
-        }
-
-        [Conditional("DEBUG")]
-        private void Log(string str)
-        {
-            Debug.WriteLine(str);
-        }
-
-        public void Start()
-        {
-            if (_thread == null)
-            {
-                _thread = new Thread(Run);
-                _thread.Start();
-            }
-
-            _isStop = false;
-        }
-
-        public void Stop()
-        {
-            _isStop = true;
-        }
-
-        private readonly object _obj = new object();
-
-        private TimeSpan MaxDelayTime { get; } = TimeSpan.FromSeconds(1);
-        private TimeSpan MinDelayTime { get; } = TimeSpan.FromMilliseconds(5);
-
-        private TimeSpan[] WaitDelayTimeList { get; } = new TimeSpan[]
-        {
-            TimeSpan.FromMilliseconds(5),
-            TimeSpan.FromMilliseconds(10),
-            TimeSpan.FromMilliseconds(20),
-            TimeSpan.FromMilliseconds(50),
-            TimeSpan.FromMilliseconds(100),
-            TimeSpan.FromMilliseconds(200),
-            TimeSpan.FromMilliseconds(200),
-            TimeSpan.FromMilliseconds(200),
-            TimeSpan.FromMilliseconds(200),
-            TimeSpan.FromMilliseconds(200),
-        };
-
-        private void Run()
-        {
-            try
-            {
-                int waitCount = 0;
-                while (!_isStop)
-                {
-                    List<BackgroundTask> backgroundTaskList;
-
-                    lock (_obj)
-                    {
-                        backgroundTaskList = BackgroundTaskList.ToList();
-                    }
-
-                    if (backgroundTaskList.Count == 0)
-                    {
-                        Console.WriteLine("没有任务自己暂停一下" + waitCount);
-                        if (waitCount < WaitDelayTimeList.Length)
-                        {
-                            Thread.Sleep(WaitDelayTimeList[waitCount]);
-                            waitCount++;
-                        }
-                        else
-                        {
-                            Thread.Sleep(MaxDelayTime);
-                            waitCount++;
-                        }
-
-                        continue;
-                    }
-
-                    waitCount = 0;
-
-                    var minDelayTime = TimeSpan.MaxValue;
-                    foreach (var backgroundTask in backgroundTaskList)
-                    {
-                        var lastTime = (DateTime.Now - backgroundTask.LastStartTime);
-
-                        if (lastTime >= backgroundTask.DelayTime)
-                        {
-                            backgroundTask.Action();
-
-                            Console.WriteLine(
-                                $"{DateTime.Now} {DateTime.Now.Millisecond} 运行 {backgroundTask.Name} 距离上次运行 {(lastTime).Milliseconds} 误差{lastTime.Milliseconds - backgroundTask.DelayTime.Milliseconds}");
-
-                            backgroundTask.LastStartTime = DateTime.Now;
-
-                            var delayTime = backgroundTask.DelayTime;
-                            if (delayTime < minDelayTime)
-                            {
-                                minDelayTime = delayTime;
-                            }
-                        }
-                        else
-                        {
-                            var delayTime = backgroundTask.DelayTime - (DateTime.Now - backgroundTask.LastStartTime);
-                            if (delayTime < minDelayTime)
-                            {
-                                minDelayTime = delayTime;
-                            }
-                        }
-                    }
-
-                    if (minDelayTime > MaxDelayTime)
-                    {
-                        minDelayTime = MaxDelayTime;
-                    }
-                    else if (minDelayTime < MinDelayTime)
-                    {
-                        minDelayTime = MinDelayTime;
-                    }
-
-                    Thread.Sleep(minDelayTime);
-                }
-            }
-            finally
-            {
-                _thread = null;
-            }
-        }
-
-        private bool _isStop;
-
-        private Thread _thread;
-
-        private List<BackgroundTask> BackgroundTaskList { get; } = new List<BackgroundTask>();
-    }
-
-    /// <summary>
-    /// 后台代码
-    /// </summary>
-    public class BackgroundTask
-    {
-        /// <inheritdoc />
-        public BackgroundTask(string name, Action action, TimeSpan delayTime)
-        {
-            Name = name;
-            Action = action;
-            DelayTime = delayTime;
-            LastStartTime = DateTime.Now;
-        }
-
-        public string Name { get; }
-
-        public Action Action { get; }
-
-        public TimeSpan DelayTime { get; }
-
-        public DateTime LastStartTime { set; get; }
     }
 }
