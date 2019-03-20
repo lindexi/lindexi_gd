7d553d290d68b567f91daed16c814c023e90c1a8
lindexi <lindexi_gd@163.com>
2019-03-20 09:13:43 +0800

    修复json字符串

---
+++ BepirquwiKedoucawji/BepirquwiKedoucawji/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace BepirquwiKedoucawji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> errors = new List<string>();
+
+            List<DateTime> list = JsonConvert.DeserializeObject<List<DateTime>>(@"[
+      '2009-09-09T00:00:00Z',
+      '这是歪楼的',
+      [
+        1
+      ],
+      '1977-02-20T00:00:00Z',
+      null,
+      '2000-12-01T00:00:00Z'
+    ]",
+                new JsonSerializerSettings
+                {
+                    Error = (sender, e) =>
+                    {
+                        errors.Add(e.ErrorContext.Error.Message);
+                        e.ErrorContext.Handled = true;
+                    },
+                    Converters = { new IsoDateTimeConverter() }
+                });
+        }
+    }
+}
