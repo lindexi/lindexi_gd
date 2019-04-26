ebcd65c41d21c8e4379e8924a7235f22025cbbb7
lindexi <lindexi_gd@163.com>
2019-04-26 10:42:54 +0800

    修改文件

---
+++ HiaiemhiuBobmnawa/HiaiemhiuBobmnawa/CodeGeneratorRegistrationAttribute.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+
+namespace HiaiemhiuBobmnawa
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public sealed class CodeGeneratorRegistrationAttribute : RegistrationAttribute
+    {
+        public CodeGeneratorRegistrationAttribute(Type generatorType, string generatorName, string contextGuid)
+        {
+            if (generatorType == null)
+                throw new ArgumentNullException(nameof(generatorType));
+            if (generatorName == null)
+                throw new ArgumentNullException(nameof(generatorName));
+            if (contextGuid == null)
+                throw new ArgumentNullException(nameof(contextGuid));
+
+            ContextGuid = contextGuid;
+            GeneratorType = generatorType;
+            GeneratorName = generatorName;
+            GeneratorRegKeyName = generatorType.Name;
+            GeneratorGuid = generatorType.GUID;
+        }
+
+        /// <summary> 
+        /// Get the generator Type 
+        /// </summary> 
+        public Type GeneratorType { get; }
+
+        /// <summary> 
+        /// Get the Guid representing the project type 
+        /// </summary> 
+        public string ContextGuid { get; }
+
+        /// <summary> 
+        /// Get the Guid representing the generator type 
+        /// </summary> 
+        public Guid GeneratorGuid { get; }
+
+        /// <summary> 
+        /// Get or Set the GeneratesDesignTimeSource value 
+        /// </summary> 
+        public bool GeneratesDesignTimeSource { get; set; }
+
+        /// <summary> 
+        /// Get or Set the GeneratesSharedDesignTimeSource value 
+        /// </summary> 
+        public bool GeneratesSharedDesignTimeSource { get; set; }
+
+
+        /// <summary> 
+        /// Gets the Generator name  
+        /// </summary> 
+        public string GeneratorName { get; }
+
+        /// <summary> 
+        /// Gets the Generator reg key name under  
+        /// </summary> 
+        public string GeneratorRegKeyName { get; set; }
+
+        /// <summary> 
+        /// Property that gets the generator base key name 
+        /// </summary> 
+        private string GeneratorRegKey => string.Format(CultureInfo.InvariantCulture, @"Generators\{0}\{1}", ContextGuid, GeneratorRegKeyName);
+
+        /// <summary> 
+        ///     Called to register this attribute with the given context.  The context 
+        ///     contains the location where the registration information should be placed. 
+        ///     It also contains other information such as the type being registered and path information. 
+        /// </summary> 
+        public override void Register(RegistrationContext context)
+        {
+            using (var childKey = context.CreateKey(GeneratorRegKey))
+            {
+                childKey.SetValue(string.Empty, GeneratorName);
+                childKey.SetValue("CLSID", GeneratorGuid.ToString("B"));
+
+                if (GeneratesDesignTimeSource)
+                    childKey.SetValue("GeneratesDesignTimeSource", 1);
+
+                if (GeneratesSharedDesignTimeSource)
+                    childKey.SetValue("GeneratesSharedDesignTimeSource", 1);
+
+            }
+        }
+
+        /// <summary> 
+        /// Unregister this file extension. 
+        /// </summary> 
+        /// <param name="context"></param> 
+        public override void Unregister(RegistrationContext context)
+        {
+            context.RemoveKey(GeneratorRegKey);
+        }
+    }
+}
---
+++ HiaiemhiuBobmnawa/HiaiemhiuBobmnawa/FilePluralizationGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace HiaiemhiuBobmnawa
+{
+    [ComVisible(true)]
+    [Guid(FilePluralizationGeneratorId)]
+    [CodeGeneratorRegistrationAttribute(typeof(FilePluralizationGenerator), nameof(FilePluralizationGenerator), VSConstants.UICONTEXT.CSharpProject_string, GeneratesDesignTimeSource = true)]
+    [ProvideObject(typeof(FilePluralizationGenerator))]
+    public sealed class FilePluralizationGenerator : IVsSingleFileGenerator
+    {
+        public int DefaultExtension(out string defaultExtension)
+        {
+            defaultExtension = ".generated.cs";
+            return defaultExtension.Length;
+        }
+
+        public const string FilePluralizationGeneratorId = "2F6B300C-6A90-4C08-8F11-D4E0C75391C4";
+
+        public int Generate(string wszInputFilePath, string bstrInputFileContents, string wszDefaultNamespace, IntPtr[] rgbOutputFileContents, out uint pcbOutput, IVsGeneratorProgress pGenerateProgress)
+        {
+            var bytes = Encoding.UTF8.GetBytes("林德熙");
+            var length = bytes.Length;
+
+            rgbOutputFileContents[0] = Marshal.AllocCoTaskMem(length);
+            Marshal.Copy(bytes, 0, rgbOutputFileContents[0], length);
+            pcbOutput = (uint) length;
+
+            return VSConstants.S_OK;
+        }
+    }
+}
