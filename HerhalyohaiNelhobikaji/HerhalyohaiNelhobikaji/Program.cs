a65aff5f2984d6c390d61679765598f388e3a89d
lindexi <lindexi_gd@163.com>
2019-05-15 14:50:12 +0800

    异步方法

---
+++ HerhalyohaiNelhobikaji/HerhalyohaiNelhobikaji/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HerhalyohaiNelhobikaji
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var fileStream = new FileStream("File.txt", FileMode.Open))
+            {
+                var buffer = new byte[1024];
+                var task = Task<int>.Factory.FromAsync(fileStream.BeginRead, fileStream.EndRead, buffer, 0, 1024, null);
+                var n = await task;
+            }
+        }
+    }
+}
