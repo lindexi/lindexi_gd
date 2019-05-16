8e346e750fe2075e1366a2a098d63af3b50db177
lindexi <lindexi_gd@163.com>
2019-05-16 18:47:37 +0800

    光标忽略

---
+++ FawlalnejajerelaWhallgemcurkear/FawlalnejajerelaWhallgemcurkear/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FawlalnejajerelaWhallgemcurkear
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var newTempFolder = @"D:\lindexi\无法访问文件夹";
+            Environment.SetEnvironmentVariable("TEMP", newTempFolder);
+            Environment.SetEnvironmentVariable("TMP", newTempFolder);
+
+            var uri = new Uri("pack://application:,,,/Text.cur");
+            var resource = Application.GetResourceStream(uri);
+            Cursor = new Cursor(resource.Stream);
+        }
+    }
+}
