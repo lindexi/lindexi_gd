67965115ea7d4401a65396284e725e1db6fd4cdc
lindexi <lindexi_gd@163.com>
2019-05-19 13:12:00 +0800

    启动程序

---
+++ KogawjeyeChawkeyojayju/KogawjeyeChawkeyojayju/RunFile.cs
@@ -0,0 +1,25 @@
+﻿namespace KogawjeyeChawkeyojayju
+{
+    public class RunFile
+    {
+        /// <summary>
+        /// 开启的程序名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 启动的文件
+        /// </summary>
+        public string File { get; set; }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public string Argument { set; get; }
+
+        /// <summary>
+        /// 如果写了进程名，那么这个进程只会启动一次
+        /// </summary>
+        public string ProcessName { set; get; }
+    }
+}
