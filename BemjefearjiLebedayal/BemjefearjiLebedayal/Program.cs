423f7de145212d75ada919bbef7e0ff50fac0777
lindexi <lindexi_gd@163.com>
2019-05-29 19:11:27 +0800

    急速调试库

---
+++ BemjefearjiLebedayal/BemjefearjiLebedayal/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FibiwhearweJukelerekalu;
+
+namespace BemjefearjiLebedayal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            Console.WriteLine(foo.GetFoo());
+
+            Console.Read();
+        }
+    }
+}
