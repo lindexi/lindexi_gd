b332e647a9ac55cb3134f5a2b25934e877d76ad5
lindexi <lindexi_gd@163.com>
2019-06-26 21:26:35 +0800

    画线

---
+++ WaveLineDemo/WaveLineDemo/ViewModel.cs
@@ -0,0 +1,137 @@
+﻿using System.Windows;
+using lindexi.MVVM.Framework.ViewModel;
+
+namespace WaveLineDemo
+{
+    public class ViewModel : ViewModelBase
+    {
+        public string WaveLength
+        {
+            get => _waveLength;
+            set
+            {
+                if (value == _waveLength) return;
+                _waveLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string WaveHeight
+        {
+            get => _waveHeight;
+            set
+            {
+                if (value == _waveHeight) return;
+                _waveHeight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string CurveSquaring
+        {
+            get => _curveSquaring;
+            set
+            {
+                if (value == _curveSquaring) return;
+                _curveSquaring = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string BorderThickness
+        {
+            get => _borderThickness;
+            set
+            {
+                if (value == _borderThickness) return;
+                _borderThickness = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstPointY
+        {
+            get => _firstPointY;
+            set
+            {
+                if (value == _firstPointY) return;
+                _firstPointY = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SecondPointX
+        {
+            get => _secondPointX;
+            set
+            {
+                if (value == _secondPointX) return;
+                _secondPointX = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SecondPointY
+        {
+            set
+            {
+                if (value == _secondPointY) return;
+                _secondPointY = value;
+                OnPropertyChanged();
+            }
+            get => _secondPointY;
+        }
+
+        public string FirstPointX
+        {
+            set
+            {
+                if (value == _firstPointX) return;
+                _firstPointX = value;
+                OnPropertyChanged();
+            }
+            get => _firstPointX;
+        }
+
+        public WaveLine Draw()
+        {
+            double.TryParse(FirstPointX, out var firstPointX);
+            double.TryParse(FirstPointY, out var firstPointY);
+            double.TryParse(SecondPointX, out var secondPointX);
+            double.TryParse(SecondPointY, out var secondPointY);
+            double.TryParse(BorderThickness, out var borderThickness);
+            double.TryParse(CurveSquaring, out var curveSquaring);
+            double.TryParse(WaveHeight, out var waveHeight);
+            double.TryParse(WaveLength, out var waveLength);
+
+            var waveLine = new WaveLine
+            {
+                BorderThickness = borderThickness,
+                CurveSquaring = curveSquaring,
+                WaveHeight = waveHeight,
+                WaveLength = waveLength
+            };
+            waveLine.DrawWaveLine(new Point(firstPointX, firstPointY), new Point(secondPointX, secondPointY));
+            return waveLine;
+        }
+
+        /// <inheritdoc />
+        public override void OnNavigatedFrom(object sender, object obj)
+        {
+        }
+
+        /// <inheritdoc />
+        public override void OnNavigatedTo(object sender, object obj)
+        {
+        }
+
+        private string _borderThickness = 1.ToString();
+        private string _curveSquaring = 0.57.ToString();
+        private string _firstPointX = 50.ToString();
+        private string _firstPointY = 290.ToString();
+        private string _secondPointX = 760.ToString();
+        private string _secondPointY = 226.ToString();
+        private string _waveHeight = 90.ToString();
+        private string _waveLength = 100.ToString();
+    }
+}
---
+++ WaveLineDemo/WaveLineDemo/WaveLine.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WaveLineDemo
+{
+    public class WaveLine : UIElement
+    {
+        /// <inheritdoc />
+        public WaveLine()
+        {
+            _waveLineList = new VisualCollection(this);
+        }
+
+        public double BorderThickness { get; set; } = 1.0;
+        public double WaveLength { get; set; } = 4;
+        public double WaveHeight { get; set; } = 5;
+
+        public double CurveSquaring { get; set; } = 0.57;
+
+        /// <inheritdoc />
+        protected override int VisualChildrenCount => _waveLineList.Count;
+
+        public void DrawWaveLine(Point startPoint, Point endPoint)
+        {
+            var p1 = startPoint;
+            var p2 = endPoint;
+
+            var distance = (p1 - p2).Length;
+
+            var angle = CalculateAngle(p1, p2);
+            var waveLength = WaveLength;
+            var waveHeight = WaveHeight;
+            var howManyWaves = distance / waveLength;
+            var waveInterval = distance / howManyWaves;
+            var maxBcpLength =
+                Math.Sqrt(waveInterval / 4.0 * (waveInterval / 4.0) + waveHeight / 2.0 * (waveHeight / 2.0));
+
+            var curveSquaring = CurveSquaring;
+            var bcpLength = maxBcpLength * curveSquaring;
+            var bcpInclination = CalculateAngle(new Point(0, 0), new Point(waveInterval / 4.0, waveHeight / 2.0));
+
+            var wigglePoints = new List<(Point bcpOut, Point bcpIn, Point anchor)>();
+            var prevFlexPt = p1;
+            var polarity = 1;
+
+            for (var waveIndex = 0; waveIndex < howManyWaves * 2; waveIndex++)
+            {
+                var bcpOutAngle = angle + bcpInclination * polarity;
+                var bcpOut = new Point(prevFlexPt.X + Math.Cos(bcpOutAngle) * bcpLength,
+                    prevFlexPt.Y + Math.Sin(bcpOutAngle) * bcpLength);
+                var flexPt = new Point(prevFlexPt.X + Math.Cos(angle) * waveInterval / 2.0,
+                    prevFlexPt.Y + Math.Sin(angle) * waveInterval / 2.0);
+                var bcpInAngle = angle + (Math.PI - bcpInclination) * polarity;
+                var bcpIn = new Point(flexPt.X + Math.Cos(bcpInAngle) * bcpLength,
+                    flexPt.Y + Math.Sin(bcpInAngle) * bcpLength);
+
+                wigglePoints.Add((bcpOut, bcpIn, flexPt));
+
+                polarity *= -1;
+                prevFlexPt = flexPt;
+            }
+
+            var streamGeometry = new StreamGeometry();
+            using (var streamGeometryContext = streamGeometry.Open())
+            {
+                streamGeometryContext.BeginFigure(wigglePoints[0].anchor, true, false);
+
+                for (var i = 1; i < wigglePoints.Count; i += 1)
+                {
+                    var (bcpOut, bcpIn, anchor) = wigglePoints[i];
+
+                    streamGeometryContext.BezierTo(bcpOut, bcpIn, anchor, true, false);
+                }
+            }
+
+            var visual = new DrawingVisual();
+            var fillBrush = Brushes.Transparent;
+            var lineBrush = Brushes.DarkSeaGreen;
+            var borderThickness = BorderThickness;
+            var strokePen = new Pen(lineBrush, borderThickness);
+            using (var context = visual.RenderOpen())
+            {
+                context.DrawGeometry(fillBrush, strokePen, streamGeometry);
+
+                //for (var i = 0; i < wigglePoints.Count; i += 1)
+                //{
+                //    var (bcpOut, bcpIn, anchor) = wigglePoints[i];
+
+                //    context.DrawEllipse(Brushes.Black, null, anchor, 5, 5);
+                //}
+            }
+
+            _waveLineList.Add(visual);
+        }
+
+        private readonly VisualCollection _waveLineList;
+
+        private static double CalculateAngle(Point p1, Point p2)
+        {
+            return Math.Atan2(p2.Y - p1.Y, p2.X - p1.X);
+        }
+
+        /// <inheritdoc />
+        protected override Visual GetVisualChild(int index)
+        {
+            return _waveLineList[index];
+        }
+    }
+}
