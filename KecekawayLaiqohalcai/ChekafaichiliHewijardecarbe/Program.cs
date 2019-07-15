8374af196c238c5d247c7921fbb1926ab4913336
lindexi <lindexi_gd@163.com>
2019-07-15 12:08:39 +0800

    写出一个不能被继承的抽象类

---
+++ KecekawayLaiqohalcai/ChekafaichiliHewijardecarbe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChekafaichiliHewijardecarbe
+{
+    public abstract class A
+    {
+        internal abstract IB B();
+    }
+
+    internal interface IB
+    {
+
+    }
+}
