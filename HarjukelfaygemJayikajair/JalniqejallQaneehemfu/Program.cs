05837d667a304827cc62de2e3d8c1a35bfdfed42
lindexi <lindexi_gd@163.com>
2019-07-19 08:35:54 +0800

    自动编译

---
+++ HarjukelfaygemJayikajair/JalniqejallQaneehemfu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace JalniqejallQaneehemfu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 请自己创建项目，然后替换下面两个值
+            var repo = @"d:\lindexi\NolekekanuYemrobemjaywo\";
+            var commit = "b81c6cdd0514a2bc643e1ae94398ec91fd0ab11d";
+
+            AutoBuild(repo, commit);
+        }
+
+        private static void AutoBuild(string repo, string commit)
+        {
+            
+        }
+    }
+}
