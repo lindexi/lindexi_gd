6eb385e9896a7c7aa1e3599001f7479a55b2ecac
lindexi <lindexi_gd@163.com>
2019-08-01 19:49:45 +0800

    最多更新

--- RegeejairhemFurwhurrahuki/RegeejairhemFurwhurrahuki/Program.cs
+++ RegeejairhemFurwhurrahuki/RegeejairhemFurwhurrahuki/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RegeejairhemFurwhurrahuki
 {
@@ -199,6 +200,25 @@ namespace RegeejairhemFurwhurrahuki
             //向标准输入写入要执行的命令。这里使用&是批处理命令的符号，表示前面一个命令不管是否执行成功都执行后面(exit)命令，如果不执行exit命令，后面调用ReadToEnd()方法会假死
             //同类的符号还有&&和||前者表示必须前一个命令执行成功才会执行后面的命令，后者表示必须前一个命令执行失败才会执行后面的命令
 
+            // 超时
+            Task.Run(() =>
+            {
+                Task.Delay(TimeSpan.FromMinutes(1)).ContinueWith(_ =>
+                {
+                    Console.WriteLine("超时");
+                    try
+                    {
+                        if (!p.HasExited)
+                        {
+                            p.Kill();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                });
+            });
 
             //获取cmd窗口的输出信息
             string output = p.StandardOutput.ReadToEnd();
