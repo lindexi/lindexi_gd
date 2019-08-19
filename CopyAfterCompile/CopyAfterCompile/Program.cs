5c6ff4e5d0f6a28d0745b39cb0930fea962528b8
lindexi <lindexi_gd@163.com>
2019-08-19 20:02:49 +0800

    测试编译

---
+++ CopyAfterCompile/CopyAfterCompile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using dotnetCampus.GitCommand;
+
+namespace CopyAfterCompile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "1.txt";
+            if (File.Exists(file))
+            {
+                var str = File.ReadAllText("1.txt");
+            }
+            var directory = new DirectoryInfo(KonairlerecufeeCealemwowa.Directory);
+
+            var git = new Git(directory);
+            var logCommit = git.GetLogCommit();
+        }
+    }
+
+    class KonairlerecufeeCealemwowa
+    {
+        public const string Directory = "../../../../../";
+    }
+}
