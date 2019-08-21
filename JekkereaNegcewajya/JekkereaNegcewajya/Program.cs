1470bcb9ee77fcc3beea28df22ee9aaa7dd09d6c
lindexi <lindexi_gd@163.com>
2019-08-21 10:55:55 +0800

    测试代码

---
+++ JekkereaNegcewajya/JekkereaNegcewajya/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JekkereaNegcewajyaML.Model.DataModels;
+using Microsoft.ML;
+
+namespace JekkereaNegcewajya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            // input.
+            input.SentimentText = "pika is a doubi"; // 这句话是我加的
+
+            // Try model on sample data
+            ModelOutput result = predEngine.Predict(input);
+
+            Console.WriteLine(result.Prediction);
+        }
+    }
+}
