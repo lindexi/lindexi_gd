de3c493051f15be07c4327e797d081c6869c6f93
lindexi <lindexi_gd@163.com>
2019-09-22 20:23:53 +0800

    格式代码

---
+++ BearqalkeawaiKaleenemcemfo/BearqalkeawaiKaleenemcemfo/Foo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BearqalkeawaiKaleenemcemfo
+{
+    [ComVisible(true)]
+    [Guid("5742D257-CCCC-4F7A-8191-6362609C458D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IFoo
+    {
+        /// <summary>
+        /// 有趣方法
+        /// </summary>
+        /// <returns></returns>
+        string Foo();
+    }
+
+    [ComVisible(true)]
+    [Guid("5742D257-CCCC-4F7A-8191-6362609C458D")]
+    public class Foo : IFoo
+    {
+        /// <inheritdoc />
+        string IFoo.Foo()
+        {
+            return "林德熙是逗比";
+        }
+    }
+}
