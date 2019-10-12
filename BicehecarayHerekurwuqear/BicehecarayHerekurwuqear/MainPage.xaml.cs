7c8c7c680edba9209f697bad0f284eb66462d0fb
lindexi <lindexi_gd@163.com>
2019-10-12 19:28:07 +0800

    录界面

---
+++ BicehecarayHerekurwuqear/BicehecarayHerekurwuqear/MainPage.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace BicehecarayHerekurwuqear
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Setup();
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await StartCaptureAsync();
+        }
+
+        private void Setup()
+        {
+            _canvasDevice = new CanvasDevice();
+
+            _compositionGraphicsDevice = CanvasComposition.CreateCompositionGraphicsDevice(
+                Window.Current.Compositor,
+                _canvasDevice);
+
+            _compositor = Window.Current.Compositor;
+
+            _surface = _compositionGraphicsDevice.CreateDrawingSurface(
+                new Size(400, 400),
+                DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                DirectXAlphaMode.Premultiplied); // This is the only value that currently works with
+            // the composition APIs.
+
+            var visual = _compositor.CreateSpriteVisual();
+            visual.RelativeSizeAdjustment = Vector2.One;
+            var brush = _compositor.CreateSurfaceBrush(_surface);
+            brush.HorizontalAlignmentRatio = 0.5f;
+            brush.VerticalAlignmentRatio = 0.5f;
+            brush.Stretch = CompositionStretch.Uniform;
+            visual.Brush = brush;
+            ElementCompositionPreview.SetElementChildVisual(this, visual);
+        }
+
+        public async Task StartCaptureAsync()
+        {
+            // The GraphicsCapturePicker follows the same pattern the
+            // file pickers do.
+            var picker = new GraphicsCapturePicker();
+            GraphicsCaptureItem item = await picker.PickSingleItemAsync();
+
+            // The item may be null if the user dismissed the
+            // control without making a selection or hit Cancel.
+            if (item != null)
+            {
+                StartCaptureInternal(item);
+            }
+        }
+
+        private void StartCaptureInternal(GraphicsCaptureItem item)
+        {
+            _item = item;
+            _lastSize = _item.Size;
+
+            _framePool = Direct3D11CaptureFramePool.Create(
+                _canvasDevice, // D3D device
+                DirectXPixelFormat.B8G8R8A8UIntNormalized, // Pixel format
+                2, // Number of frames
+                _item.Size); // Size of the buffers
+
+            _framePool.FrameArrived += (s, a) =>
+            {
+                // The FrameArrived event is raised for every frame on the thread
+                // that created the Direct3D11CaptureFramePool. This means we
+                // don't have to do a null-check here, as we know we're the only
+                // one dequeueing frames in our application.  
+
+                // NOTE: Disposing the frame retires it and returns  
+                // the buffer to the pool.
+
+                using (var frame = _framePool.TryGetNextFrame())
+                {
+                    ProcessFrame(frame);
+                }
+            };
+
+            _session = _framePool.CreateCaptureSession(_item);
+            _session.StartCapture();
+        }
+
+        private void ProcessFrame(Direct3D11CaptureFrame frame)
+        {
+            // Resize and device-lost leverage the same function on the
+            // Direct3D11CaptureFramePool. Refactoring it this way avoids
+            // throwing in the catch block below (device creation could always
+            // fail) along with ensuring that resize completes successfully and
+            // isn’t vulnerable to device-lost.
+            bool recreateDevice = false;
+
+            if ((frame.ContentSize.Width != _lastSize.Width) ||
+                (frame.ContentSize.Height != _lastSize.Height))
+            {
+                _lastSize = frame.ContentSize;
+            }
+
+            try
+            {
+                // Take the D3D11 surface and draw it into a  
+                // Composition surface.
+
+                // Convert our D3D11 surface into a Win2D object.
+                CanvasBitmap canvasBitmap = CanvasBitmap.CreateFromDirect3D11Surface(
+                    _canvasDevice,
+                    frame.Surface);
+
+                _currentFrame = canvasBitmap;
+
+                // Helper that handles the drawing for us.
+                FillSurfaceWithBitmap(canvasBitmap);
+            }
+
+            // This is the device-lost convention for Win2D.
+            catch (Exception e) when (_canvasDevice.IsDeviceLost(e.HResult))
+            {
+                // We lost our graphics device. Recreate it and reset
+                // our Direct3D11CaptureFramePool.  
+                recreateDevice = true;
+            }
+        }
+
+        private void FillSurfaceWithBitmap(CanvasBitmap canvasBitmap)
+        {
+            CanvasComposition.Resize(_surface, canvasBitmap.Size);
+
+            using (var session = CanvasComposition.CreateDrawingSession(_surface))
+            {
+                session.Clear(Colors.Transparent);
+                session.DrawImage(canvasBitmap);
+            }
+        }
+
+        private SizeInt32 _lastSize;
+        private GraphicsCaptureItem _item;
+        private Direct3D11CaptureFramePool _framePool;
+        private GraphicsCaptureSession _session;
+
+        private CanvasDevice _canvasDevice;
+        private CompositionGraphicsDevice _compositionGraphicsDevice;
+        private Compositor _compositor;
+        private CompositionDrawingSurface _surface;
+        private CanvasBitmap _currentFrame;
+    }
+}
