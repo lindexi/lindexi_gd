cfae1924c231508cef524ccaea0aa876122fed57
lindexi <lindexi_gd@163.com>
2019-12-01 20:29:56 +0800

    没有代码

---
+++ JedeeyacearfarWhinelheefi/KeahawkelchunaceJaykalralllea/KeahawkelchunaceJaykalralllea/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using KeahawkelchunaceJaykalralllea.Models;
+
+namespace KeahawkelchunaceJaykalralllea.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
