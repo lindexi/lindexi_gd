7dd7baf3d876bed9760c3c0a9eca5d5c1bd637c0
lindexi <lindexi_gd@163.com>
2020-01-01 19:17:09 +0800

    格式代码

---
+++ FealeneredeeJiwinurdejeenel/FealeneredeeJiwinurdejeenel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FealeneredeeJiwinurdejeenel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class FileFormatChecker
+    {
+        public FileFormatChecker(FileInfo file)
+        {
+            File = file;
+        }
+
+        public FileInfo File { get; }
+    }
+}
