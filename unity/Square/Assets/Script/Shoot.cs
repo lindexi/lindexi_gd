438da19e2ac0425660fd8efb11d57b37863fa7dc
lindexi <lindexi_gd@163.com>
2020-01-30 18:08:22 +0800

    打砖块

---
+++ unity/Square/Assets/Script/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject Sphere;
+
+    public float Speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var sphere = GameObject.Instantiate(Sphere);
+
+            sphere.transform.Translate(base.transform.position);
+            sphere.transform.Translate(new Vector3(0, 0, 1));
+
+            var rigidbody = sphere.GetComponent<Rigidbody>();
+            rigidbody.velocity = new Vector3(0, 0, 1) * Speed;
+           
+        }
+    }
+}
