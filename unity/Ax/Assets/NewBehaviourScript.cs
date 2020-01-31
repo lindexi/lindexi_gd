699e6c7dc8ccadd55a92181a18e6901df4ade9db
lindexi <lindexi_gd@163.com>
2020-01-31 17:29:09 +0800

    点击鼠标

--- unity/Ax/Assets/NewBehaviourScript.cs
+++ unity/Ax/Assets/NewBehaviourScript.cs
@@ -13,32 +13,17 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var horizontalAsixName = "Horizontal";
-        var horizontal = Input.GetAxis(horizontalAsixName);
-
-        var verticalAsixName = "Vertical";
-        var vertical = Input.GetAxis(verticalAsixName);
-
         var z = 0f;
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetButtonDown("Fire1"))
         {
             z = 1f;
         }
-        else if(Input.GetMouseButtonDown(1))
+        else if (Input.GetButton("Fire2"))
         {
-            z = -1f;
+            z = -0.1f;
         }
 
-        transform.Translate(new Vector3(horizontal, vertical, z) * 0.5f);
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            transform.Rotate(new Vector3(0, -10));
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
-        {
-            transform.Rotate(new Vector3(0, 10));
-        }
-
+        transform.Translate(new Vector3(0, 0, z) * 0.5f);
     }
 }
---
+++ unity/PinebargarcunearQaigechoho/Assets/GameControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var shoot = GameObject.Find("Pin").GetComponent<Shoot>();
+            shoot.StartFly = true;
+        }
+    }
+}
