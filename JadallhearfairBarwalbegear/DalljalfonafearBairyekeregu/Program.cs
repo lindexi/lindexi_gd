3e907b2ee99fbe61f43e07bd8fb43e9c056befc6
lindexi <lindexi_gd@163.com>
2020-02-10 19:30:38 +0800

    防火墙

---
+++ JadallhearfairBarwalbegear/DalljalfonafearBairyekeregu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace DalljalfonafearBairyekeregu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = Path.Combine(Assembly.GetExecutingAssembly().Location, @"..\NarhedeachawhearWeargijawgowe.exe");
+            Process.Start(file);
+        }
+    }
+}
