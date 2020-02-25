6ffd44b95d97f1956a2afb68a41b7443ff9eb6eb
lindexi <lindexi_gd@163.com>
2020-02-25 21:38:44 +0800

    用 Xamarin 写人工智能

---
+++ JardalllojoHayeajemjuli/JardalllojoHayeajemjuli/JardalllojoHayeajemjuli/DoubleRange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace JardalllojoHayeajemjuli
+{
+    public class DoubleRange
+    {
+        public DoubleRange(double a, double b)
+        {
+            Max = Math.Max(a, b);
+            Min = Math.Min(a, b);
+
+            Length = Max - Min;
+        }
+
+        public double Length { get; }
+        public double Max { get; }
+        public double Min { get; }
+
+        /// <summary>
+        /// 返回在 Min 和 Max 的随机数
+        /// </summary>
+        /// <returns></returns>
+        public double GetRan()
+        {
+            return Rand.NextDouble() * Length + Min;
+        }
+
+        /// <summary>
+        /// 随机数生成器
+        /// </summary>
+        private static Random Rand { get; set; } = new Random();
+    }
+}
--- JardalllojoHayeajemjuli/JardalllojoHayeajemjuli/JardalllojoHayeajemjuli/MainPage.xaml.cs
+++ JardalllojoHayeajemjuli/JardalllojoHayeajemjuli/JardalllojoHayeajemjuli/MainPage.xaml.cs
@@ -16,6 +16,54 @@ namespace JardalllojoHayeajemjuli
         public MainPage()
         {
             InitializeComponent();
+
+            NeuronTask = Task.Run(() =>
+            {
+                var data = new[]
+                {
+                    new Data()
+                    {
+                        Input = new double[] {0, 0},
+                        Output = 0
+                    },
+                    new Data()
+                    {
+                        Input = new double[] {0, 1},
+                        Output = 1
+                    },
+                    new Data()
+                    {
+                        Input = new double[] {1, 0},
+                        Output = 1
+                    },
+                    new Data()
+                    {
+                        Input = new double[] {1, 1},
+                        Output = 1
+                    },
+                };
+
+                var learning = new Learning();
+                return learning.Learn(data);
+            });
         }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var neuron = await NeuronTask;
+
+            var n1Text = N1Text.Text;
+            var n2Text = N2Text.Text;
+
+            if (int.TryParse(n1Text, out var n1))
+            {
+                if (int.TryParse(n2Text, out var n2))
+                {
+                    Text.Text = neuron.Compute(new double[] {n1, n2}).ToString();
+                }
+            }
+        }
+
+        private Task<Neuron> NeuronTask { get; }
     }
 }
