0495ca07ac65af548810035628a4d565b26f1c91
lindexi <lindexi_gd@163.com>
2020-03-23 16:02:17 +0800

    分割换行

--- BepirquwiKedoucawji/BepirquwiKedoucawji/Program.cs
+++ BepirquwiKedoucawji/BepirquwiKedoucawji/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
@@ -10,23 +12,54 @@ namespace BepirquwiKedoucawji
     {
         static void Main(string[] args)
         {
-            if (Enum.TryParse(typeof(Di),null,out var value))
-            {
-                
-            }
+            var str = "123123\r123123\n123123\r\n123";
+            var newLineList = str.Split('\n', '\r').Select(text => text = text.Replace("\r", ""))
+                .ToList();
+
+            newLineList = SplitMultiLines(str);
         }
-    }
 
-    public enum Di
-    {
-        /// <summary>
-        /// 轨道
-        /// </summary>
-        Railway,
+        private static List<string> SplitMultiLines(string str)
+        {
+            var lineList = new List<string>();
+            var text = new StringBuilder(str.Length);
+            for (var i = 0; i < str.Length; i++)
+            {
+                var c = str[i];
+                if (c == '\r')
+                {
+                    lineList.Add(text.ToString());
+                    text.Clear();
 
-        /// <summary>
-        /// 河流
-        /// </summary>
-        River,
+                    if (i < str.Length - 1)
+                    {
+                        if (str[i + 1] == '\n')
+                        {
+                            i++;
+                        }
+                    }
+                }
+                else if (c == '\n')
+                {
+                    lineList.Add(text.ToString());
+                    text.Clear();
+
+                    if (i < str.Length - 1)
+                    {
+                        if (str[i + 1] == '\r')
+                        {
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    text.Append(c);
+                }
+            }
+
+            lineList.Add(text.ToString());
+            return lineList;
+        }
     }
 }
