0bfd184dbabc3cd206dd395c6a5a42a170f64699
lindexi <lindexi_gd@163.com>
2020-04-05 15:08:58 +0800

    使用 GTK 写应用

---
+++ XamarinGTK/Cla/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace GameOfLife.GTK
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Game of Life");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
