cd40375e63911a052d33a4d5160b4fbd4e103b85
lindexi <lindexi_gd@163.com>
2020-04-10 08:31:08 +0800

    ChibarbalkeliLerferone

--- BallkowhejallColalljaygairwair/BallkowhejallColalljaygairwair/MainWindow.xaml.cs
+++ BallkowhejallColalljaygairwair/BallkowhejallColalljaygairwair/MainWindow.xaml.cs
@@ -26,27 +26,5 @@ namespace BallkowhejallColalljaygairwair
         {
             InitializeComponent();
         }
-
-        private void ScrollViewer_OnScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            UIElement control = TextBlock;
-
-            var top = control.TranslatePoint(new Point(), StackPanel);
-            // 控件的宽度和高度
-            var controlBounds = new Rect(top, control.DesiredSize);
-
-            // 用户可以看到的大小
-            var viewBounds = new Rect(new Point(e.HorizontalOffset, e.VerticalOffset),
-                new Size(e.ViewportWidth, e.ViewportHeight));
-
-            if (viewBounds.IntersectsWith(controlBounds))
-            {
-                Debug.WriteLine("歪楼");
-            }
-            else
-            {
-                Debug.WriteLine("不歪楼");
-            }
-        }
     }
 }
