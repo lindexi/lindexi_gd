cf8c1add01a571bafeb0548b6aa43da8670227c9
lindexi <lindexi_gd@163.com>
2020-04-14 22:57:39 +0800

    队列

---
+++ CallnernawbawceKairwemwhejeene/CallnernawbawceKairwemwhejeene/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace CallnernawbawceKairwemwhejeene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var semaphoreSlim = new SemaphoreSlim(10, 10);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var n = i;
+                _autoResetEvent.WaitOne();
+                new Thread(() => { GeregelkunoNeawhikarcee(semaphoreSlim, n); }).Start();
+            }
+
+            Console.Read();
+        }
+
+        private static readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(true);
+
+        private static void GeregelkunoNeawhikarcee(SemaphoreSlim semaphoreSlim, int n)
+        {
+            Console.WriteLine($"{n} 进入");
+            _autoResetEvent.Set();
+
+            semaphoreSlim.Wait();
+            Console.WriteLine(n);
+
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            semaphoreSlim.Release();
+        }
+    }
+}
