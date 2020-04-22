e3568146947ee63011c3826dbfd1c1e38073cb22
lindexi <lindexi_gd@163.com>
2020-04-22 19:58:15 +0800

    ChehairgerekuLahenehar

--- Babukeelleneeoai/Babukeelleneeoai/MainWindow.xaml.cs
+++ Babukeelleneeoai/Babukeelleneeoai/MainWindow.xaml.cs
@@ -9,14 +9,15 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.StylusInput;
+using Microsoft.StylusInput.PluginData;
 
 namespace Babukeelleneeoai
 {
@@ -29,66 +30,29 @@ namespace Babukeelleneeoai
         {
             InitializeComponent();
 
-            var realTimeStylus = new RealTimeStylus();
+            SourceInitialized += MainWindow_SourceInitialized;
+
         }
 
-        private void Button_OnClick(object sender, RoutedEventArgs e)
+        private void MainWindow_SourceInitialized(object sender, EventArgs e)
         {
-            // 修改这个路径
-            string fileName = @"c:\path\to\my\file.xlsx";
+            var windowInteropHelper = new WindowInteropHelper(this);
+            var handle = windowInteropHelper.Handle;
 
-            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            var realTimeStylus = new RealTimeStylus(handle)
             {
-                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
-                {
-                    WorkbookPart workbookPart = doc.WorkbookPart;
-                    SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
-                    SharedStringTable sst = sstpart.SharedStringTable;
+                MultiTouchEnabled = true,
+                AllTouchEnabled = true,
+            };
+            RealTimeStylus = realTimeStylus;
 
-                    WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
-                    Worksheet sheet = worksheetPart.Worksheet;
-
-                    var cells = sheet.Descendants<Cell>();
-                    var rows = sheet.Descendants<Row>();
-
-                    Debug.WriteLine("Row count = {0}", rows.LongCount());
-                    Debug.WriteLine("Cell count = {0}", cells.LongCount());
-
-                    // One way: go through each cell in the sheet
-                    foreach (Cell cell in cells)
-                    {
-                        if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
-                        {
-                            int ssid = int.Parse(cell.CellValue.Text);
-                            string str = sst.ChildElements[ssid].InnerText;
-                            Debug.WriteLine("Shared string {0}: {1}", ssid, str);
-                        }
-                        else if (cell.CellValue != null)
-                        {
-                            Debug.WriteLine("Cell contents: {0}", cell.CellValue.Text);
-                        }
-                    }
-
-                    // Or... via each row
-                    foreach (Row row in rows)
-                    {
-                        foreach (Cell c in row.Elements<Cell>())
-                        {
-                            if ((c.DataType != null) && (c.DataType == CellValues.SharedString))
-                            {
-                                int ssid = int.Parse(c.CellValue.Text);
-                                string str = sst.ChildElements[ssid].InnerText;
-                                Debug.WriteLine("Shared string {0}: {1}", ssid, str);
-                            }
-                            else if (c.CellValue != null)
-                            {
-                                Debug.WriteLine("Cell contents: {0}", c.CellValue.Text);
-                            }
-                        }
-                    }
-                }
-            }
+            var styluePlugIn = new StyluePlugIn();
+            realTimeStylus.SyncPluginCollection.Add(styluePlugIn);
+            // 不能提前，否则提示没有添加任何的 PlugIn 对象
+            realTimeStylus.Enabled = true;
         }
+
+        private RealTimeStylus RealTimeStylus { set; get; }
 
         private void Border_TouchDown(object sender, TouchEventArgs e)
         {
@@ -103,5 +67,107 @@ namespace Babukeelleneeoai
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
         }
+    }
+
+    public class StyluePlugIn : UIElement, IStylusSyncPlugin
+    {
+        /// <inheritdoc />
+        public void RealTimeStylusEnabled(RealTimeStylus sender, RealTimeStylusEnabledData data)
+        {
+
+        }
+
+        /// <inheritdoc />
+        public void RealTimeStylusDisabled(RealTimeStylus sender, RealTimeStylusDisabledData data)
+        {
+        }
+
+        /// <inheritdoc />
+        void IStylusSyncPlugin.StylusInRange(RealTimeStylus sender, StylusInRangeData data)
+        {
+        }
+
+        /// <inheritdoc />
+        void IStylusSyncPlugin.StylusOutOfRange(RealTimeStylus sender, StylusOutOfRangeData data)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+
+        private StrokeCollection StrokeCollection { get; } = new StrokeCollection();
+
+        private Dictionary<int, StylusPointCollection> StrokeList { get; } = new Dictionary<int, StylusPointCollection>();
+
+        /// <inheritdoc />
+        void IStylusSyncPlugin.StylusDown(RealTimeStylus sender, StylusDownData data)
+        {
+            var stylusPointCollection = new StylusPointCollection();
+            StrokeList[data.Stylus.Id] = stylusPointCollection;
+
+            stylusPointCollection.Add(new StylusPoint());
+
+            StrokeCollection.Add(new Stroke(stylusPointCollection));
+        }
+
+        /// <inheritdoc />
+        void IStylusSyncPlugin.StylusUp(RealTimeStylus sender, StylusUpData data)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        void IStylusSyncPlugin.StylusButtonDown(RealTimeStylus sender, StylusButtonDownData data)
+        {
+        }
+
+        /// <inheritdoc />
+        void IStylusSyncPlugin.StylusButtonUp(RealTimeStylus sender, StylusButtonUpData data)
+        {
+        }
+
+        /// <inheritdoc />
+        public void InAirPackets(RealTimeStylus sender, InAirPacketsData data)
+        {
+        }
+
+        /// <inheritdoc />
+        public void Packets(RealTimeStylus sender, PacketsData data)
+        {
+        }
+
+        /// <inheritdoc />
+        public void SystemGesture(RealTimeStylus sender, SystemGestureData data)
+        {
+        }
+
+        /// <inheritdoc />
+        public void TabletAdded(RealTimeStylus sender, TabletAddedData data)
+        {
+        }
+
+        /// <inheritdoc />
+        public void TabletRemoved(RealTimeStylus sender, TabletRemovedData data)
+        {
+        }
+
+        /// <inheritdoc />
+        public void CustomStylusDataAdded(RealTimeStylus sender, CustomStylusData data)
+        {
+        }
+
+        /// <inheritdoc />
+        public void Error(RealTimeStylus sender, ErrorData data)
+        {
+        }
+
+        /// <inheritdoc />
+        // Defines which handlers are called by the framework. We set the flags for pen-down, pen-up and pen-move.
+        public DataInterestMask DataInterest =>DataInterestMask.StylusDown |
+                                               DataInterestMask.Packets |
+                                               DataInterestMask.StylusUp;
     }
 }
--- BojakallhoLajacenu/BojakallhoLajacenu/MainWindow.xaml.cs
+++ BojakallhoLajacenu/BojakallhoLajacenu/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,34 @@ namespace BojakallhoLajacenu
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AddPopup(sender);
+        }
+
+        private void Border_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            AddPopup(sender);
+        }
+
+        private void AddPopup(object sender)
+        {
+            var popup = new Popup
+            {
+                Child = new Border()
+                {
+                    Width = 100,
+                    Height = 100,
+                    Background = Brushes.Gray
+                },
+
+                IsOpen = true,
+                StaysOpen = false,
+                PlacementTarget = (UIElement) sender,
+                Placement = PlacementMode.Center
+            };
         }
     }
 }
