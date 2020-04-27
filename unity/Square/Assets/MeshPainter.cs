fcb9a5296e2e5b96c27a7003c86905f9ee265351
lindexi <lindexi_gd@163.com>
2020-04-27 08:49:46 +0800

    刷新

--- unity/Square/Assets/MeshPainter.cs
+++ unity/Square/Assets/MeshPainter.cs
@@ -10,35 +10,48 @@ public class MeshPainter : MonoBehaviour
     void Start()
     {
         var imageList = Directory.GetFiles(@"D:\lindexi\image");
-        foreach (var temp in imageList)
-        {
-            Debug.Log(temp);
-        }
 
         _imageList = imageList;
+
+        var renderTexture = new RenderTexture(1920, 1080, 8);
+        RenderTexture = renderTexture;
+
+        Texture2D texture = new Texture2D(Width, Height);
+        _texture2D = texture;
+
+        var meshRender = gameObject.GetComponent<MeshRenderer>();
+        meshRender.material.mainTexture = _texture2D;
     }
 
+    private const int Width = 1920;
+    private const int Height = 1080;
+
+    private Texture2D _texture2D;
+
+    public RenderTexture RenderTexture;
+
+
     private string[] _imageList;
+
+
 
     // Update is called once per frame
     void Update()
     {
         // 鼠标左键
-        if (Input.GetButtonDown("Fire1"))
+        //if (Input.GetButtonDown("Fire1"))
         {
-            var meshRender = gameObject.GetComponent<MeshRenderer>();
-            var path = _imageList[_count];
-            _count++;
-
-            if (_count == _imageList.Length)
+            for (int i = 0; i < Width; i++)
             {
-                _count = 0;
+                var c = new Color(Random.value, Random.value, Random.value);
+                for (int j = 0; j < Height; j++)
+                {
+                    Color color = ((i & j) != 0 ? Color.white : Color.gray);
+                    _texture2D.SetPixel(i, j, c);
+                }
             }
 
-            Texture2D texture = new Texture2D(1920, 1080);
-            texture.LoadImage(File.ReadAllBytes(path));
-            
-            meshRender.material.mainTexture = texture;
+            _texture2D.Apply();
         }
     }
 
