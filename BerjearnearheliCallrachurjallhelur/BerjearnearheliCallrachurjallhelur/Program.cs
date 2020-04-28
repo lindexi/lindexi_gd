c4dd7a59cd2c45b5ca0d53438964ac9af0d439d1
lindexi <lindexi_gd@163.com>
2020-04-28 08:11:04 +0800

    文件夹

--- BerjearnearheliCallrachurjallhelur/BerjearnearheliCallrachurjallhelur/Program.cs
+++ BerjearnearheliCallrachurjallhelur/BerjearnearheliCallrachurjallhelur/Program.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Diagnostics.Runtime;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace BerjearnearheliCallrachurjallhelur
 {
@@ -8,29 +8,13 @@ namespace BerjearnearheliCallrachurjallhelur
     {
         static void Main(string[] args)
         {
-            using (DataTarget dataTarget = DataTarget.LoadCrashDump(@"f:\lindexi\test\dotnet.exe.52348.dmp"))
+            var folder = @"e:\上传\坚果云\回收站\网络课程\Enbx\";
+            foreach (var file in Directory.GetFiles(folder, "*.enbx", SearchOption.AllDirectories))
             {
-                foreach (ClrInfo version in dataTarget.ClrVersions)
-                {
-                    Console.WriteLine("Found CLR Version: " + version.Version);
-
-                    // This is the data needed to request the dac from the symbol server:
-                    DacInfo dacInfo = version.DacInfo;
-
-                    Console.WriteLine("Filesize:  {0:X}", dacInfo.IndexFileSize);
-                    Console.WriteLine("Timestamp: {0:X}", dacInfo.IndexTimeStamp);
-                    Console.WriteLine("Dac File:  {0}", dacInfo.PlatformSpecificFileName);
-
-                    // If we just happen to have the correct dac file installed on the machine,
-                    // the "LocalMatchingDac" property will return its location on disk:
-                    string dacLocation = version.LocalMatchingDac;
-                    if (!string.IsNullOrEmpty(dacLocation))
-                        Console.WriteLine("Local dac location: " + dacLocation);
-
-                    // You may also download the dac from the symbol server, which is covered
-                    // in a later section of this tutorial.
-                }
+                Console.WriteLine(file);
             }
+
+            Console.Read();
         }
     }
 }
