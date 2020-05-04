bdead1c7f6021d0e89e6f828868090c5b1f2a7ed
lindexi <lindexi_gd@163.com>
2020-05-04 20:45:31 +0800

    LallrelicuHowhelawere

--- Babukeelleneeoai/Babukeelleneeoai/MainWindow.xaml.cs
+++ Babukeelleneeoai/Babukeelleneeoai/MainWindow.xaml.cs
@@ -30,144 +30,21 @@ namespace Babukeelleneeoai
         {
             InitializeComponent();
 
-            SourceInitialized += MainWindow_SourceInitialized;
-
-        }
-
-        private void MainWindow_SourceInitialized(object sender, EventArgs e)
-        {
-            var windowInteropHelper = new WindowInteropHelper(this);
-            var handle = windowInteropHelper.Handle;
-
-            var realTimeStylus = new RealTimeStylus(handle)
+            var challcharyiqerelalCaykihelnere = new List<ChallcharyiqerelalCaykihelnere>();
+            for (int i = 0; i < 10; i++)
             {
-                MultiTouchEnabled = true,
-                AllTouchEnabled = true,
-            };
-            RealTimeStylus = realTimeStylus;
+                challcharyiqerelalCaykihelnere.Add(new ChallcharyiqerelalCaykihelnere()
+                {
+                    Name = (i + 1).ToString()
+                });
+            }
 
-            var styluePlugIn = new StyluePlugIn();
-            realTimeStylus.SyncPluginCollection.Add(styluePlugIn);
-            // 不能提前，否则提示没有添加任何的 PlugIn 对象
-            realTimeStylus.Enabled = true;
-        }
-
-        private RealTimeStylus RealTimeStylus { set; get; }
-
-        private void Border_TouchDown(object sender, TouchEventArgs e)
-        {
-
-        }
-
-        private void Border_StylusDown(object sender, StylusDownEventArgs e)
-        {
-
-        }
-
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-        {
+            ListBox.ItemsSource = challcharyiqerelalCaykihelnere;
         }
     }
 
-    public class StyluePlugIn : UIElement, IStylusSyncPlugin
+    public class ChallcharyiqerelalCaykihelnere
     {
-        /// <inheritdoc />
-        public void RealTimeStylusEnabled(RealTimeStylus sender, RealTimeStylusEnabledData data)
-        {
-
-        }
-
-        /// <inheritdoc />
-        public void RealTimeStylusDisabled(RealTimeStylus sender, RealTimeStylusDisabledData data)
-        {
-        }
-
-        /// <inheritdoc />
-        void IStylusSyncPlugin.StylusInRange(RealTimeStylus sender, StylusInRangeData data)
-        {
-        }
-
-        /// <inheritdoc />
-        void IStylusSyncPlugin.StylusOutOfRange(RealTimeStylus sender, StylusOutOfRangeData data)
-        {
-        }
-
-        /// <inheritdoc />
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            base.OnRender(drawingContext);
-        }
-
-        private StrokeCollection StrokeCollection { get; } = new StrokeCollection();
-
-        private Dictionary<int, StylusPointCollection> StrokeList { get; } = new Dictionary<int, StylusPointCollection>();
-
-        /// <inheritdoc />
-        void IStylusSyncPlugin.StylusDown(RealTimeStylus sender, StylusDownData data)
-        {
-            var stylusPointCollection = new StylusPointCollection();
-            StrokeList[data.Stylus.Id] = stylusPointCollection;
-
-            stylusPointCollection.Add(new StylusPoint());
-
-            StrokeCollection.Add(new Stroke(stylusPointCollection));
-        }
-
-        /// <inheritdoc />
-        void IStylusSyncPlugin.StylusUp(RealTimeStylus sender, StylusUpData data)
-        {
-            
-        }
-
-        /// <inheritdoc />
-        void IStylusSyncPlugin.StylusButtonDown(RealTimeStylus sender, StylusButtonDownData data)
-        {
-        }
-
-        /// <inheritdoc />
-        void IStylusSyncPlugin.StylusButtonUp(RealTimeStylus sender, StylusButtonUpData data)
-        {
-        }
-
-        /// <inheritdoc />
-        public void InAirPackets(RealTimeStylus sender, InAirPacketsData data)
-        {
-        }
-
-        /// <inheritdoc />
-        public void Packets(RealTimeStylus sender, PacketsData data)
-        {
-        }
-
-        /// <inheritdoc />
-        public void SystemGesture(RealTimeStylus sender, SystemGestureData data)
-        {
-        }
-
-        /// <inheritdoc />
-        public void TabletAdded(RealTimeStylus sender, TabletAddedData data)
-        {
-        }
-
-        /// <inheritdoc />
-        public void TabletRemoved(RealTimeStylus sender, TabletRemovedData data)
-        {
-        }
-
-        /// <inheritdoc />
-        public void CustomStylusDataAdded(RealTimeStylus sender, CustomStylusData data)
-        {
-        }
-
-        /// <inheritdoc />
-        public void Error(RealTimeStylus sender, ErrorData data)
-        {
-        }
-
-        /// <inheritdoc />
-        // Defines which handlers are called by the framework. We set the flags for pen-down, pen-up and pen-move.
-        public DataInterestMask DataInterest =>DataInterestMask.StylusDown |
-                                               DataInterestMask.Packets |
-                                               DataInterestMask.StylusUp;
+        public string Name { set; get; }
     }
 }
