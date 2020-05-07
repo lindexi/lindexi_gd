a0c74d13a44883e1f1f982ca4d972ccf4dfb9cc4
lindexi <lindexi_gd@163.com>
2020-05-07 19:23:07 +0800

    LerecallelairKefaikeaner

--- CallnernawbawceKairwemwhejeene/CallnernawbawceKairwemwhejeene/Program.cs
+++ CallnernawbawceKairwemwhejeene/CallnernawbawceKairwemwhejeene/Program.cs
@@ -1,36 +1,104 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace CallnernawbawceKairwemwhejeene
 {
+    class Context : IContext
+    {
+        public async Task WaitForContinue()
+        {
+            await (CurrentTask?.Task ?? Task.CompletedTask);
+        }
+
+        private TaskCompletionSource<bool> CurrentTask { set; get; }
+
+        public void SetState(State state)
+        {
+            switch (state)
+            {
+                case State.Continue:
+                    CurrentTask?.TrySetResult(false);
+                    CurrentTask = null;
+                    break;
+                case State.Pause:
+                    CurrentTask ??= new TaskCompletionSource<bool>();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    enum State
+    {
+        Continue,
+        Pause,
+    }
+
+    interface IContext
+    {
+        Task WaitForContinue();
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var semaphoreSlim = new SemaphoreSlim(10, 10);
-
-            for (int i = 0; i < 1000; i++)
+            var context = new Context();
+            Task.Run(() =>
             {
-                var n = i;
-                _autoResetEvent.WaitOne();
-                new Thread(() => { GeregelkunoNeawhikarcee(semaphoreSlim, n); }).Start();
-            }
-
-            Console.Read();
+                var random = new Random();
+                while (true)
+                {
+                    Task.Run(() => context.SetState(State.Pause)).Wait();
+                    Task.Delay(random.Next(1000, 3000)).Wait();
+                    Task.Run(() =>
+                    {
+                        context.SetState(State.Continue);
+                    });
+                }
+            });
+            Task.Delay(100).Wait();
+            Foo(context).Wait();
         }
 
-        private static readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(true);
-
-        private static void GeregelkunoNeawhikarcee(SemaphoreSlim semaphoreSlim, int n)
+        static async Task Foo(IContext context)
         {
-            Console.WriteLine($"{n} 进入");
-            _autoResetEvent.Set();
+            var n = 0;
+            var n1 = 1;
+            var n2 = 1;
+            while (n1 > 0)
+            {
+                F1();
+                await context.WaitForContinue();
+                n1 = F2(n, n1);
+                await context.WaitForContinue();
+                n = F3(n1, n2);
+                await context.WaitForContinue();
+                await Task.Delay(10);
+            }
+        }
 
-            semaphoreSlim.Wait();
-            Console.WriteLine(n);
+        private static int F3(int n1, in int n2)
+        {
+            Task.Delay(10).Wait();
+            Console.WriteLine($"{DateTime.Now:hh:mm:ss} F3");
+            return 10;
+        }
 
-            Thread.Sleep(TimeSpan.FromSeconds(1));
-            semaphoreSlim.Release();
+        private static int F2(int n, int n1)
+        {
+            Task.Delay(10).Wait();
+            Console.WriteLine($"{DateTime.Now:hh:mm:ss} F2");
+            return 10;
+        }
+
+        private static void F1()
+        {
+            Task.Delay(10).Wait();
+
+            Console.WriteLine($"{DateTime.Now:hh:mm:ss} F1");
         }
     }
 }
