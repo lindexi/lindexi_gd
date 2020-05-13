9738cad89cf8afb92105d6c9ed76d50324028500
lindexi <lindexi_gd@163.com>
2020-05-13 10:57:29 +0800

    测试 .NET 5 控制台

---
+++ Foo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Foo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
