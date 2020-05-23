1cf397895a7c16b834846379dbda7947e724f193
lindexi <lindexi_gd@163.com>
2020-05-23 19:12:07 +0800

    字节

---
+++ DairqeldejuDawyewheawelbehe/DairqeldejuDawyewheawelbehe/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.Recognizers.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Recognizers.Text.Number;
+using Microsoft.Recognizers.Text.Choice;
+using Microsoft.Recognizers.Text.Sequence;
+using Microsoft.Recognizers.Text.DateTime;
+using Microsoft.Recognizers.Text.NumberWithUnit;
+
+namespace DairqeldejuDawyewheawelbehe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static string ModelResultToString(List<ModelResult> list)
+        {
+            var pre = "";
+            var breakLine = "\r\n";
+            var str = new StringBuilder();
+            foreach (var modelResult in list)
+            {
+                str.Append(pre)
+                    .Append("关键词： ")
+                    .Append(modelResult.Text)
+                    .Append(breakLine)
+                    .Append(pre)
+                    .Append($"起点 {modelResult.Start} 终点 {modelResult.End}")
+                    .Append(breakLine);
+                if (modelResult.Resolution.TryGetValue("value", out var value))
+                {
+                    str.Append(pre)
+                        .Append("值：")
+                        .Append(value)
+                        .Append(breakLine);
+                }
+
+                str.Append(breakLine);
+            }
+
+            return str.ToString();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = Text.Text;
+            var modelInfoList = new List<ModelInfo>();
+
+            RecognizeNumber(text, modelInfoList);
+            RecognizeOrdinal(text, modelInfoList);
+
+            RecognizeAge(text, modelInfoList);
+            RecognizeCurrency(text, modelInfoList);
+            RecognizeDimension(text, modelInfoList);
+            RecognizeTemperature(text, modelInfoList);
+
+            RecognizeDateTime(text, modelInfoList);
+
+            RecognizePhoneNumber(text, modelInfoList);
+            RecognizeIpAddress(text, modelInfoList);
+
+            RecognizeBoolean(text, modelInfoList);
+
+            ListView.ItemsSource = modelInfoList;
+        }
+
+        private void RecognizeBoolean(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeBoolean = ChoiceRecognizer.RecognizeBoolean(text, Culture.Chinese);
+
+            if (recognizeBoolean.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("布尔", ModelResultToString(recognizeBoolean)));
+            }
+        }
+
+        private void RecognizeIpAddress(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeIpAddress = SequenceRecognizer.RecognizeIpAddress(text, Culture.Chinese);
+
+            if (recognizeIpAddress.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("IP", ModelResultToString(recognizeIpAddress)));
+            }
+        }
+
+        private void RecognizePhoneNumber(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizePhoneNumber = SequenceRecognizer.RecognizePhoneNumber(text, Culture.Chinese);
+
+            if (recognizePhoneNumber.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("电话号", ModelResultToString(recognizePhoneNumber)));
+            }
+        }
+
+        private void RecognizeDateTime(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeDateTime = DateTimeRecognizer.RecognizeDateTime(text, Culture.Chinese);
+
+            if (recognizeDateTime.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("时间", ModelResultToString(recognizeDateTime)));
+            }
+        }
+
+        private void RecognizeTemperature(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeTemperature = NumberWithUnitRecognizer.RecognizeTemperature(text, Culture.Chinese);
+
+            if (recognizeTemperature.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("温度", ModelResultToString(recognizeTemperature)));
+            }
+        }
+
+        private void RecognizeDimension(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeDimension = NumberWithUnitRecognizer.RecognizeDimension(text, Culture.Chinese);
+
+            if (recognizeDimension.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("大小", ModelResultToString(recognizeDimension)));
+            }
+        }
+
+        private void RecognizeCurrency(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeCurrency = NumberWithUnitRecognizer.RecognizeCurrency(text, Culture.Chinese);
+
+            if (recognizeCurrency.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("货币", ModelResultToString(recognizeCurrency)));
+            }
+        }
+
+        private void RecognizeAge(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeAge = NumberWithUnitRecognizer.RecognizeAge(text, Culture.Chinese);
+
+            if (recognizeAge.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("年龄", ModelResultToString(recognizeAge)));
+            }
+        }
+
+        private void RecognizeOrdinal(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeOrdinal = NumberRecognizer.RecognizeOrdinal(text, Culture.Chinese);
+            if (recognizeOrdinal.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("序号", ModelResultToString(recognizeOrdinal)));
+            }
+        }
+
+        private void RecognizeNumber(string text, List<ModelInfo> modelInfoList)
+        {
+            var recognizeNumber = NumberRecognizer.RecognizeNumber(text, Culture.Chinese);
+
+            if (recognizeNumber.Count > 0)
+            {
+                modelInfoList.Add(new ModelInfo("数值", ModelResultToString(recognizeNumber)));
+            }
+        }
+    }
+
+    public class ModelInfo
+    {
+        public ModelInfo(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+
+        public string Title { get; }
+
+        public string Content { get; }
+    }
+}
---
+++ HaifeljiweajeYeelarkerjairere/HaifeljiweajeYeelarkerjairere/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.Number;
+
+namespace HaifeljiweajeYeelarkerjairere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestRecognizeNumber("我挖了两个坑");
+
+            TestRecognizeNumber("我挖了两个坑，埋了两千个小伙伴");
+
+            TestRecognizeNumber("在2020的时候，我挖了两个坑，埋了两千个小伙伴，其中一百三十个小伙伴是逗比");
+        }
+
+        private static void TestRecognizeNumber(string text)
+        {
+            var recognizeChineseNumber = NumberRecognizer.RecognizeNumber(text, Culture.Chinese);
+            Console.WriteLine(text);
+            Console.WriteLine(ModelResultToString(recognizeChineseNumber));
+        }
+
+        private static string ModelResultToString(List<ModelResult> list)
+        {
+            var pre = "\t";
+            var breakLine = "\r\n";
+            var str = new StringBuilder();
+            foreach (var modelResult in list)
+            {
+                str.Append(pre)
+                    .Append("关键词： ")
+                    .Append(modelResult.Text)
+                    .Append(breakLine)
+                    .Append(pre)
+                    .Append($"起点 {modelResult.Start} 终点 {modelResult.End}")
+                    .Append(breakLine);
+                if (modelResult.Resolution.TryGetValue("value", out var value))
+                {
+                    str.Append(pre)
+                        .Append("值：")
+                        .Append(value)
+                        .Append(breakLine);
+                }
+
+                str.Append(breakLine);
+            }
+
+            return str.ToString();
+        }
+    }
+}
