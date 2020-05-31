6d20b1a3b60ca8cc1da351f19b0f1d55342af5b2
lindexi <lindexi_gd@163.com>
2020-05-31 18:38:50 +0800

    跟随相机

---
+++ unity/SolidGeometry/Assets/03.Script/ControlPointDrag.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPointDrag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public GameObject controlArrow;
+
+    public GameObject solidCubeWithControlArrow;
+
+    public float xSpeed;
+
+    public float ySpeed;
+
+    public float zSpeed;
+
+    IEnumerator OnMouseDown()
+    {
+        var controlArrowTransform = controlArrow.transform;
+
+        //将物体由世界坐标系转化为屏幕坐标系，由vector3 结构体变量ScreenSpace存储，以用来明确屏幕坐标系Z轴的位置
+        Vector3 ScreenSpace = Camera.main.WorldToScreenPoint(controlArrowTransform.position);
+        //完成了两个步骤，1由于鼠标的坐标系是2维的，需要转化成3维的世界坐标系，2只有三维的情况下才能来计算鼠标位置与物体的距离，offset即是距离
+        Vector3 offset = controlArrowTransform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, ScreenSpace.z));
+
+        Debug.Log("down");
+        //当鼠标左键按下时
+        while (Input.GetMouseButton(0))
+        {
+          
+
+            //得到现在鼠标的2维坐标系位置
+            Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, ScreenSpace.z);
+            //将当前鼠标的2维位置转化成三维的位置，再加上鼠标的移动量
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+            //CurPosition就是物体应该的移动向量赋给transform的position属性
+
+            var currentOffset = curPosition - controlArrowTransform.position;
+
+            currentOffset = new Vector3(currentOffset.x * xSpeed, currentOffset.y * ySpeed, currentOffset.z * zSpeed);
+            var newPosition = controlArrowTransform.position + currentOffset;
+
+            controlArrowTransform.position = newPosition;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ unity/SolidGeometry/Assets/03.Script/FollowCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform targetTr;
+    public float dist = 10;
+    public float height = 3.0f;
+    public float dampTrace = 20.0f;
+
+    private Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.position = Vector3.Lerp(tr.position, targetTr.position - (targetTr.forward * dist) + (Vector3.up * height), Time.deltaTime * dampTrace);
+        tr.LookAt(targetTr.position);
+    }
+}
