2a14aeef59ddd6ea778a929fd31a4f4d14c2ab34
lindexi <lindexi_gd@163.com>
2020-06-12 08:59:20 +0800

    BochularkelnallaycaikallhayjoWhurrehelyar

---
+++ ChaihacikemjercekoLanembocerehe/ChaihacikemjercekoLanembocerehe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ChaihacikemjercekoLanembocerehe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            double db = double.NaN;
+            writer.Write(db);
+
+            Console.WriteLine(string.Join(",", stream.ToArray().Select(b => b.ToString("X2"))));
+
+            stream.Seek(0, SeekOrigin.Begin);
+            db = double.PositiveInfinity;
+            writer.Write(db);
+            Console.WriteLine(string.Join(",", stream.ToArray().Select(b => b.ToString("X2"))));
+
+            stream.Seek(0, SeekOrigin.Begin);
+            db = double.NegativeInfinity;
+            writer.Write(db);
+            Console.WriteLine(string.Join(",", stream.ToArray().Select(b => b.ToString("X2"))));
+        }
+    }
+}
