9c2c783c018658fe0f69616c46cfc1567626fb73
lindexi <lindexi_gd@163.com>
2020-06-20 22:00:33 +0800

    QejurbadiDeeqearjakelwhaki

--- CakifallwurnallDallbereluchar/CakifallwurnallDallbereluchar/Program.cs
+++ CakifallwurnallDallbereluchar/CakifallwurnallDallbereluchar/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Reflection.Metadata;
+using System.Threading.Tasks;
 
 namespace CakifallwurnallDallbereluchar
 {
@@ -9,6 +11,7 @@ namespace CakifallwurnallDallbereluchar
         static void Main(string[] args)
         {
             // 从零开始写一个依赖注入容器
+            // 尝试解决线程并发问题
             _container = new Container();
 
             Init();
@@ -18,9 +21,29 @@ namespace CakifallwurnallDallbereluchar
 
         private static void Init()
         {
-            Foo foo = new Foo();
+            Foo foo0 = new Foo();
+            Foo foo1 = new Foo();
+            Foo foo2 = new Foo();
             //_container.Set(typeof(Foo), foo);
-            _container.Set(foo);
+            _container.Set(foo0);
+
+            int n = 100;
+            var taskList = new List<Task>();
+            for (int i = 0; i < n; i++)
+            {
+                var foo = i < n / 2 ? foo1 : foo2;
+
+                var task = Task.Run(() => _container.Set(foo));
+                taskList.Add(task);
+            }
+
+            Task.WaitAll(taskList.ToArray());
+
+            var fx = _container.Get<Foo>();
+            if (ReferenceEquals(foo0, fx))
+            {
+
+            }
         }
 
         private static void Use()
@@ -60,6 +83,6 @@ namespace CakifallwurnallDallbereluchar
             return InnerDictionary[type];
         }
 
-        private Dictionary<Type, object> InnerDictionary { get; } = new Dictionary<Type, object>();
+        private ConcurrentDictionary<Type, object> InnerDictionary { get; } = new ConcurrentDictionary<Type, object>();
     }
 }
---
+++ RekelaljoqiKolijejee/RekelaljoqiKolijejee/Program.qs
@@ -0,0 +1,25 @@
+﻿namespace Quantum.RekelaljoqiKolijejee 
+{
+	open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Measurement;
+    open Microsoft.Quantum.Math;
+    open Microsoft.Quantum.Convert;
+
+    @EntryPoint()
+    operation GenerateRandom() : Unit
+    {
+	    mutable r = GenerateQuantumRandom();
+        mutable b = ResultAsBool(r);
+        Message(BoolAsString(b));
+	}
+
+    operation GenerateQuantumRandom() : Result
+    {
+        using(q = Qubit())
+        {
+			H(q);
+            return MResetZ(q);
+		}
+	}
+}               
