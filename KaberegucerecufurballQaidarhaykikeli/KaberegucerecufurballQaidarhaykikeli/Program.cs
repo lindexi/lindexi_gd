19bcf40f6d7659cfd813ce93d2f4d60e4a6de078
lindexi <lindexi_gd@163.com>
2020-06-21 16:14:11 +0800

    HeargewearnemcheraiNeejenenemkel

---
+++ KaberegucerecufurballQaidarhaykikeli/KaberegucerecufurballQaidarhaykikeli/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using dotnetCampus.ClrAttachedProperty;
+
+namespace KaberegucerecufurballQaidarhaykikeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bank = new Bank();
+            var person = new Person();
+            bank.IdProperty.SetValue(person, "123123");
+            var idCard = bank.IdProperty.GetValue(person);
+            Console.WriteLine(idCard);
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Bank
+    {
+        public AttachedProperty<string> IdProperty { get; } = new AttachedProperty<string>();
+    }
+}
