7d3e0a2ef4c536fea17674270ff2671c2f7eb371
lindexi <lindexi_gd@163.com>
2020-06-27 20:06:54 +0800

    可以发布博客

--- Blog/BlogTool/Program.cs
+++ Blog/BlogTool/Program.cs
@@ -8,6 +8,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.Json.Serialization;
 using System.Text.Json;
+using System.Text.RegularExpressions;
+using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace BlogTool
 {
@@ -40,6 +43,9 @@ namespace BlogTool
                 blogList.Add(blog);
             }
 
+            blogList.Sort(new BlogExcerptModelComparer());
+            blogList.Reverse();
+
             var json = JsonSerializer.Serialize(blogList, new JsonSerializerOptions()
             {
                 IgnoreNullValues = true,
@@ -50,9 +56,29 @@ namespace BlogTool
 
             File.WriteAllText(Path.Combine(folder, "FileList.txt"), string.Join("\n", blogList.Select(temp => temp.Title)));
 
+            var pageCount = blogList.Count * 1.0 / MaxBlogPrePage;
+
+            for (int i = 0; i < pageCount; i++)
+            {
+                BlogPageModel blogPage = new BlogPageModel
+                {
+                    PageCount = (int)pageCount,
+                    BlogList = blogList.Skip(i * MaxBlogPrePage).Take(MaxBlogPrePage).ToList()
+                };
+
+                json = JsonSerializer.Serialize(blogPage, new JsonSerializerOptions()
+                {
+                    IgnoreNullValues = true,
+                    WriteIndented = false,
+                });
+
+                File.WriteAllText(Path.Combine(folder, $"Summary-{i}.json"), json);
+            }
+
             return 0;
         }
 
+        private const int MaxBlogPrePage = 25;
 
         private static BlogExcerptModel ParseBlog(string file)
         {
@@ -98,10 +124,26 @@ namespace BlogTool
                 excerpt.AppendLine(line);
             }
 
+            DateTime? createTime = null;
+            var match = Regex.Match(text, @"<!-- CreateTime:([\S ]+) -->");
+            if (match.Success)
+            {
+                if (DateTime.TryParse(match.Groups[1].Value, out var time))
+                {
+                    createTime = time;
+                }
+            }
+
+            if (createTime is null)
+            {
+                createTime = File.GetCreationTime(file);
+            }
+
             return new BlogExcerptModel
             {
                 Title = title,
-                Excerpt = excerpt.ToString().Trim()
+                Excerpt = excerpt.ToString().Trim(),
+                Time = createTime,
             };
         }
 
@@ -109,6 +151,18 @@ namespace BlogTool
         private static int ConvertFileList(FileListConverter fileListConverter)
         {
             return 0;
+        }
+    }
+
+    class BlogExcerptModelComparer : IComparer<BlogExcerptModel>
+    {
+        public int Compare([AllowNull] BlogExcerptModel x, [AllowNull] BlogExcerptModel y)
+        {
+            if (x?.Time is null && y?.Time is null) return 0;
+            if (x?.Time is null) return -1;
+            if (y?.Time is null) return 1;
+
+            return x.Time.Value.CompareTo(y.Time.Value);
         }
     }
 
