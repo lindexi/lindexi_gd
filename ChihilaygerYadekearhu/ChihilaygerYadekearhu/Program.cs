4f01ab6fc4f994a8eb58cf0f4593aaf8bfd6e693
lindexi <lindexi_gd@163.com>
2020-07-04 11:07:02 +0800

    打开文件

---
+++ ChihilaygerYadekearhu/ChihilaygerYadekearhu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace ChihilaygerYadekearhu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var presentationDocument =
+                PresentationDocument.Open("test.pptx", false))
+            {
+                var presentationPart = presentationDocument.PresentationPart;
+                foreach (var slidePart in presentationPart.SlideParts)
+                {
+                    var slide = slidePart.Slide;
+                    var slideLayoutPart = slidePart.SlideLayoutPart;
+                    var slideMasterPart = slideLayoutPart.SlideMasterPart;
+
+                    var textSlideLayout = slideLayoutPart.SlideLayout.Descendants<DocumentFormat.OpenXml.Drawing.Text>().First();
+                    Console.WriteLine(textSlideLayout);
+
+                    var textSlideMaster = slideMasterPart.SlideMaster.Descendants<DocumentFormat.OpenXml.Drawing.Text>().First();
+                    Console.WriteLine(textSlideMaster);
+                }
+            }
+        }
+    }
+}
