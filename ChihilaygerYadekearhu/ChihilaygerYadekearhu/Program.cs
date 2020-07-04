b552920d1e89d2d876145a5588dbe898e2e80441
lindexi <lindexi_gd@163.com>
2020-07-04 11:44:35 +0800

    页面的元素

--- ChihilaygerYadekearhu/ChihilaygerYadekearhu/Program.cs
+++ ChihilaygerYadekearhu/ChihilaygerYadekearhu/Program.cs
@@ -15,14 +15,17 @@ namespace ChihilaygerYadekearhu
                 foreach (var slidePart in presentationPart.SlideParts)
                 {
                     var slide = slidePart.Slide;
-                    var slideLayoutPart = slidePart.SlideLayoutPart;
-                    var slideMasterPart = slideLayoutPart.SlideMasterPart;
 
-                    var textSlideLayout = slideLayoutPart.SlideLayout.Descendants<DocumentFormat.OpenXml.Drawing.Text>().First();
-                    Console.WriteLine(textSlideLayout);
+                    var slideData = slide.CommonSlideData;
+                    var shapeTree = slideData.ShapeTree;
 
-                    var textSlideMaster = slideMasterPart.SlideMaster.Descendants<DocumentFormat.OpenXml.Drawing.Text>().First();
-                    Console.WriteLine(textSlideMaster);
+                    foreach (var openXmlElement in shapeTree)
+                    {
+                        if (openXmlElement is DocumentFormat.OpenXml.Presentation.Shape shape)
+                        {
+
+                        }
+                    }
                 }
             }
         }
