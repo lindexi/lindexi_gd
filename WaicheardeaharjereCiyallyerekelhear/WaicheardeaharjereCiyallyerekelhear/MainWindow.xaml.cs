b7541e73a9da54ba4b83078d2afe86803c1570a6
lindexi <lindexi_gd@163.com>
2020-07-11 13:17:25 +0800

    修改界面

---
+++ WaicheardeaharjereCiyallyerekelhear/WaicheardeaharjereCiyallyerekelhear/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WaicheardeaharjereCiyallyerekelhear
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+            {
+                DataList.Add(new Foo()
+                {
+                    Index = i,
+                    Name = "lindexi",
+                    Remark = "doubi"
+                });
+            }
+
+            DataContext = this;
+        }
+
+        public ObservableCollection<Foo> DataList { get; } = new ObservableCollection<Foo>();
+    }
+
+    public class Foo
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
