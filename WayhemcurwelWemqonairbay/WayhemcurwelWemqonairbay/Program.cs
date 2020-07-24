223499d8946356a0e06b48acc458b7443630d923
lindexi <lindexi_gd@163.com>
2020-07-24 11:11:55 +0800

    优化内存

---
+++ WayhemcurwelWemqonairbay/WayhemcurwelWemqonairbay/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace WayhemcurwelWemqonairbay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _semaphoreSlim = new SemaphoreSlim(0);
+
+            Foo();
+
+            Thread.Sleep(1000);
+            _semaphoreSlim.Dispose();
+            Thread.Sleep(1000);
+            Console.Read();
+        }
+
+        private static async void Foo()
+        {
+            await _semaphoreSlim.WaitAsync();
+            Console.WriteLine("F");
+        }
+
+        private static SemaphoreSlim _semaphoreSlim;
+
+    }
+}
