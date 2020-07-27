916a11d68c0fba14c75b5e438bdecddb1ff421be
lindexi <lindexi_gd@163.com>
2020-07-27 19:53:21 +0800

    多线程

--- CallbadojuBaheanurjair/CallbadojuBaheanurjair/Program.cs
+++ CallbadojuBaheanurjair/CallbadojuBaheanurjair/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
@@ -10,88 +11,33 @@ namespace CallbadojuBaheanurjair
     {
         static void Main(string[] args)
         {
-            //var listRemoveTest = new ListRemoveTest();
-            //var list = listRemoveTest.GetArgumentList().First();
-            //listRemoveTest.RemoveFromEnd(list);
+            _taskCompletionSource = new TaskCompletionSource<bool>();
 
-            //if (list.All(temp => temp.N <= ListRemoveTest.MaxCount / 2))
-            //{
+            Foo();
 
-            //}
+            var taskList = new List<Task>();
 
-            BenchmarkRunner.Run<ListRemoveTest>();
-        }
-    }
-
-    public class ListRemoveTest
-    {
-        [Benchmark]
-        [ArgumentsSource(nameof(GetArgumentList))]
-        public void RemoveAll(List<Foo> list)
-        {
-            list.RemoveAll(temp => temp.N > MaxCount / 2);
-        }
-
-        [Benchmark]
-        [ArgumentsSource(nameof(GetArgumentList))]
-        public void RemoveFromStart(List<Foo> list)
-        {
-            for (var i = 0; i < list.Count; i++)
+            for (int i = 0; i < 100; i++)
             {
-                if (list[i].N > MaxCount / 2)
+                var task = Task.Run(() =>
                 {
-                    list.RemoveAt(i);
-                    i--;
-                }
-            }
-        }
-
-        [Benchmark]
-        [ArgumentsSource(nameof(GetArgumentList))]
-        public void RemoveFromEnd(List<Foo> list)
-        {
-            for (var i = list.Count - 1; i >= 0; i--)
-            {
-                if (list[i].N > MaxCount / 2)
-                {
-                    var lastIndex = list.Count - 1;
-                    if (i != lastIndex)
-                    {
-                        // 假设列表有值是 1 10 5 而当前 i = 1 而 lastIndex = 2 要移除元素 10 可以先将最后一个值赋值给当前的元素
-                        // list[1] = list[lastIndex=2] = 5
-                        // 赋值之后的列表是 1 5 5 也就是实际上干掉了 10 这个元素了
-                        // 最后再删除多余的最后一个元素就可以了
-                        list[i] = list[lastIndex];
-                    }
-
-                    list.RemoveAt(lastIndex);
-                }
-            }
-        }
-
-        public const int MaxCount = 2000000;
-
-        public IEnumerable<List<Foo>> GetArgumentList()
-        {
-            var list = new List<Foo>(MaxCount);
-            for (int i = 0; i < MaxCount / 2; i++)
-            {
-                list.Add(new Foo()
-                {
-                    N = i
+                    _taskCompletionSource.TrySetResult(true);
                 });
-                list.Add(new Foo()
-                {
-                    N = MaxCount - i
-                });
+
+                taskList.Add(task);
             }
 
-            yield return list;
-        }
-    }
+            Task.WaitAll(taskList.ToArray());
 
-    public class Foo
-    {
-        public int N { set; get; }
+            Console.Read();
+        }
+
+        private static async void Foo()
+        {
+            await _taskCompletionSource.Task;
+            Console.WriteLine("F");
+        }
+
+        private static TaskCompletionSource<bool> _taskCompletionSource;
     }
 }
