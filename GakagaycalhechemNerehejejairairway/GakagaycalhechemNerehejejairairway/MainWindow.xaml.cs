11ee2825f2b1eee1e5a68efe172e7346f866ad41
lindexi <lindexi_gd@163.com>
2020-07-28 17:03:34 +0800

    文本边距

---
+++ GakagaycalhechemNerehejejairairway/GakagaycalhechemNerehejejairairway/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using DocumentFormat.OpenXml.Packaging;
+using dotnetCampus.OpenXMLUnitConverter;
+
+namespace GakagaycalhechemNerehejejairairway
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            var file = GetFile("文本框边距.pptx");
+            ParseFile(file);
+        }
+
+        private void ParseFile(FileInfo file)
+        {
+            using (var presentationDocument =
+                PresentationDocument.Open(file.FullName, false))
+            {
+                var slide = presentationDocument.PresentationPart.SlideParts.First().Slide;
+                var textBody = slide.Descendants<DocumentFormat.OpenXml.Presentation.TextBody>().First();
+
+                var textBodyProperties = textBody.BodyProperties;
+
+                var marginLeft = textBodyProperties.LeftInset?.ToEmu().ToPixel()
+                                 
+                                 ?? DefaultMargin.Left;
+
+                var marginTop = textBodyProperties.TopInset?.ToEmu().ToPixel()
+                                ?? DefaultMargin.Top;
+
+                var marginRight = textBodyProperties.RightInset?.ToEmu().ToPixel()
+                                  ?? DefaultMargin.Right;
+
+                var marginBottom = textBodyProperties.BottomInset
+                                       ?.ToEmu().ToPixel()
+                                   ?? DefaultMargin.Bottom;
+
+                TextBlock.Text = $@"左边距 {marginLeft.Value:0.00} 像素
+上边距 {marginTop.Value:0.00} 像素
+右边距 {marginRight.Value:0.00} 像素
+下边距 {marginBottom.Value:0.00} 像素";
+            }
+        }
+
+        private FileInfo GetFile(string name)
+        {
+            string folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new FileInfo(Path.Combine(folder!, name));
+        }
+
+        private static MarginThickness DefaultMargin { get; } = new MarginThickness
+        (
+            new Inch(0.1).ToPixel(),
+            new Inch(0.05).ToPixel(),
+            new Inch(0.1).ToPixel(),
+            new Inch(0.05).ToPixel()
+        );
+    }
+}
---
+++ GakagaycalhechemNerehejejairairway/GakagaycalhechemNerehejejairairway/OpenXMLUnitConverter.cs
@@ -0,0 +1,51 @@
+﻿using DocumentFormat.OpenXml;
+using dotnetCampus.OpenXMLUnitConverter;
+
+namespace GakagaycalhechemNerehejejairairway
+{
+    /// <summary>
+    /// 单位转换
+    /// </summary>
+    public static class OpenXMLUnitConverter
+    {
+        /// <summary>
+        /// 转换为 EMU 格式
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static Emu ToEmu(this Int64Value value, in Emu defaultValue)
+        {
+            if (value == null)
+            {
+                return defaultValue;
+            }
+
+            return new Emu(value.Value);
+        }
+
+        /// <summary>
+        /// 转换为 EMU 格式
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Emu ToEmu(this Int32Value value)
+            => ToEmu(value, Emu.Zero);
+
+        /// <summary>
+        /// 转换为 EMU 格式
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static Emu ToEmu(this Int32Value value, in Emu defaultValue)
+        {
+            if (value == null)
+            {
+                return defaultValue;
+            }
+
+            return new Emu(value.Value);
+        }
+    }
+}
