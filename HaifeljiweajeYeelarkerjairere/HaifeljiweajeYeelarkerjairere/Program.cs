1e65084e8a8f56695ab6580f20dee74e6c10353b
lindexi <lindexi_gd@163.com>
2020-08-01 20:30:11 +0800

    优化方法

--- HaifeljiweajeYeelarkerjairere/HaifeljiweajeYeelarkerjairere/Program.cs
+++ HaifeljiweajeYeelarkerjairere/HaifeljiweajeYeelarkerjairere/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Recognizers.Text;
 using Microsoft.Recognizers.Text.Number;
 
@@ -11,46 +13,32 @@ namespace HaifeljiweajeYeelarkerjairere
     {
         static void Main(string[] args)
         {
-            TestRecognizeNumber("我挖了两个坑");
+            var autoResetEvent = new AutoResetEvent(false);
 
-            TestRecognizeNumber("我挖了两个坑，埋了两千个小伙伴");
+            Foo(autoResetEvent);
 
-            TestRecognizeNumber("在2020的时候，我挖了两个坑，埋了两千个小伙伴，其中一百三十个小伙伴是逗比");
-        }
+            autoResetEvent.Set();
 
-        private static void TestRecognizeNumber(string text)
-        {
-            var recognizeChineseNumber = NumberRecognizer.RecognizeNumber(text, Culture.Chinese);
-            Console.WriteLine(text);
-            Console.WriteLine(ModelResultToString(recognizeChineseNumber));
-        }
-
-        private static string ModelResultToString(List<ModelResult> list)
-        {
-            var pre = "\t";
-            var breakLine = "\r\n";
-            var str = new StringBuilder();
-            foreach (var modelResult in list)
+            for (int i = 0; i < 10; i++)
             {
-                str.Append(pre)
-                    .Append("关键词： ")
-                    .Append(modelResult.Text)
-                    .Append(breakLine)
-                    .Append(pre)
-                    .Append($"起点 {modelResult.Start} 终点 {modelResult.End}")
-                    .Append(breakLine);
-                if (modelResult.Resolution.TryGetValue("value", out var value))
-                {
-                    str.Append(pre)
-                        .Append("值：")
-                        .Append(value)
-                        .Append(breakLine);
-                }
-
-                str.Append(breakLine);
+                autoResetEvent.Set();
+                Thread.Sleep(100);
             }
 
-            return str.ToString();
+            Console.Read();
+        }
+
+        private static void Foo(AutoResetEvent autoResetEvent)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    autoResetEvent.WaitOne();
+
+                    Console.WriteLine("Foo");
+                }
+            });
         }
     }
 }
