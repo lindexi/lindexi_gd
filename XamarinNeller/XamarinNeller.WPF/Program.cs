96c9063fdba9fe318eb099da67422de5cc9ae5af
lindexi <lindexi_gd@163.com>
2020-08-08 09:28:18 +0800

    简单应用

---
+++ XamarinNeller/XamarinNeller.WPF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+using Application = System.Windows.Application;
+
+namespace XamarinNeller.WPF
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var application = new Application();
+            Forms.Init();
+            var formsApplicationPage = new FormsApplicationPage();
+            formsApplicationPage.LoadApplication(new XamarinNeller.App());
+            application.Run(formsApplicationPage);
+        }
+    }
+}
