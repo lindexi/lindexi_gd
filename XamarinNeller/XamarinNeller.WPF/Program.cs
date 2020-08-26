80ca761db43189eecb03fb7cee93c374001e344d
lindexi <lindexi_gd@163.com>
2020-08-26 09:28:16 +0800

    FormsWindow.SynchronizeAppBar will throw NullReferenceException

--- XamarinNeller/XamarinNeller.WPF/Program.cs
+++ XamarinNeller/XamarinNeller.WPF/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.WPF;
 using Application = System.Windows.Application;
@@ -18,7 +20,29 @@ namespace XamarinNeller.WPF
             Forms.Init();
             var formsApplicationPage = new FormsApplicationPage();
             formsApplicationPage.LoadApplication(new XamarinNeller.App());
-            application.Run(formsApplicationPage);
+
+            Task.Run(() =>
+            {
+                Task.Delay(1000).Wait();
+
+                application.Dispatcher.InvokeAsync(() =>
+                {
+                    var window = new Window()
+                    {
+                        Height = 600,
+                        Width = 600
+                    };
+
+                    var mainPage = new MainPage();
+                    var formsContentLoader = new FormsContentLoader();
+                    var content = formsContentLoader.LoadContentAsync(window,null, mainPage,new CancellationToken()).Result;
+
+                    window.Content = content;
+                    window.Show();
+                });
+            });
+
+            application.Run();
         }
     }
 }
