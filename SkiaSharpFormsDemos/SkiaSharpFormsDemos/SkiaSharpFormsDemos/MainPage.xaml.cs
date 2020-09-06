e0006bec1bd65cc1ebca686ad5bde39de5eeeb5a
lindexi <lindexi_gd@163.com>
2020-09-06 14:06:24 +0800

    测试skia方法

---
+++ SkiaSharpFormsDemos/SkiaSharpFormsDemos/SkiaSharpFormsDemos/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaSharpFormsDemos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SKCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+
+            // 圆心是 info.Width / 2.0f, info.Height / 2.0f
+            canvas.DrawCircle(info.Width / 2.0f, info.Height / 2.0f, radius: 100, paint);
+        }
+    }
+}
