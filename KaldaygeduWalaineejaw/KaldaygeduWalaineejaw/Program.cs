36ed4000d4ef13c2edb62f4f012a4be9b2792758
lindexi <lindexi_gd@163.com>
2020-09-17 08:38:09 +0800

    尝试读取ole文件

---
+++ KaldaygeduWalaineejaw/KaldaygeduWalaineejaw/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace KaldaygeduWalaineejaw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"f:\temp\带密码的课件PPTX.pptx";
+            OleDbConnection connection = new OleDbConnection($"Provider=SQLOLEDB;Data Source={file}");
+            connection.Open();
+
+
+        }
+    }
+}
