fb39d6fdbf93f473f6983b38a42f3f2bca370e20
lindexi <lindexi_gd@163.com>
2020-09-19 08:35:00 +0800

    压缩代码

--- KaldaygeduWalaineejaw/KaldaygeduWalaineejaw/Program.cs
+++ KaldaygeduWalaineejaw/KaldaygeduWalaineejaw/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Data.OleDb;
 using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using OpenMcdf;
 
 namespace KaldaygeduWalaineejaw
@@ -10,6 +14,31 @@ namespace KaldaygeduWalaineejaw
     {
         static void Main(string[] args)
         {
+            //var fromBase64String = Convert.FromBase64String("dehYIJ5fJ/8ykdnFaPPn7g==");
+            //var text = Encoding.UTF8.GetString(fromBase64String);
+
+            // SHA512
+            var sha512 = SHA512.Create();
+
+            // saltValue="0CxQlkIZI3Z5dQWea+KLMw=="
+            var saltBase64 = "0CxQlkIZI3Z5dQWea+KLMw==";
+            var saltByteList = Convert.FromBase64String(saltBase64);
+            var key = "123";
+            var keyByteList = Encoding.Unicode.GetBytes(key);
+
+            var h0 = sha512.ComputeHash(saltByteList.Concat(keyByteList).ToArray());
+
+            var hn = h0;
+            var iterator = 0x00000000;
+            var spinCount = 100000;
+            for (; iterator < spinCount; iterator++)
+            {
+                var iteratorByteList = BitConverter.GetBytes(iterator);
+                hn = sha512.ComputeHash(iteratorByteList.Concat(hn).ToArray());
+            }
+
+            var hf = sha512.ComputeHash(hn.Concat(new byte[]{0x5f, 0xb2, 0xad, 0x01, 0x0c, 0xb9, 0xe1, 0xf6 }).ToArray());
+
             var file = @"带密码的课件PPTX.pptx";
 
             if (CheckOfficeDocumentWithPassword(new FileInfo(file)))
@@ -24,6 +53,7 @@ namespace KaldaygeduWalaineejaw
 
             var dataSpaces = DataSpaces.Load(cf);
             var encryptionInfo = EncryptionInfo.Load(cf);
+
 
             var numDirectories = cf.GetNumDirectories();
             for (int i = 0; i < numDirectories; i++)
