47853678ec84ff888bac1289881bca6e6975531b
lindexi <lindexi_gd@163.com>
2020-09-26 20:03:01 +0800

    不等待创建文件就可写入的日志

---
+++ dotnetCampus.FastFileLogger/dotnetCampus.FastFileLogger/FileLogger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using dotnetCampus.Threading;
+
+namespace dotnetCampus.FastFileLogger
+{
+    public class FileLogger
+    {
+        public FileLogger()
+        {
+            _bufferTaskDoUtilInitialized = new DoubleBufferTaskDoUtilInitialized<string>(WriteLinesToFile);
+        }
+
+        public void Initialize(FileInfo logFile)
+        {
+            var directory = logFile.Directory;
+            directory!.Create();
+
+            _logFilePath = logFile.FullName;
+
+            _bufferTaskDoUtilInitialized.OnInitialized();
+        }
+
+        public void QueueWriteLineToFile(string text)
+        {
+            _bufferTaskDoUtilInitialized.AddTask(text);
+        }
+
+        private Task WriteLinesToFile(List<string> textList)
+        {
+            return File.AppendAllLinesAsync(_logFilePath, textList);
+        }
+
+        private string _logFilePath;
+
+        private readonly DoubleBufferTaskDoUtilInitialized<string> _bufferTaskDoUtilInitialized;
+    }
+}
