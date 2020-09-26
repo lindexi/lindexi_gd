09db75db7fe01f766df83a4ffcbb760e7085af62
lindexi <lindexi_gd@163.com>
2020-09-26 19:10:21 +0800

    创建日志

---
+++ dotnetCampus.FastFileLogger/dotnetCampus.FastFileLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetCampus.FastFileLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
