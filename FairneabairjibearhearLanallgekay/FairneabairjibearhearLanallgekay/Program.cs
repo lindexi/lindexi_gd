63c5c752af863a916bdb0855341b84412684134e
lindexi <lindexi_gd@163.com>
2020-10-02 15:42:11 +0800

    修复多线程

--- FairneabairjibearhearLanallgekay/FairneabairjibearhearLanallgekay/Program.cs
+++ FairneabairjibearhearLanallgekay/FairneabairjibearhearLanallgekay/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace FairneabairjibearhearLanallgekay
 {
@@ -8,58 +10,114 @@ namespace FairneabairjibearhearLanallgekay
     {
         static void Main(string[] args)
         {
-            List<int> l=new List<int>();
-            l.Capacity = 10;
-            Console.WriteLine(l[5]);
+
+            KurlinelchuFalberekebi();
+        }
+
+        private static void KurlinelchuFalberekebi()
+        {
+            // 设置和读取值
+
+            var storageValue = new StorageValue<int>();
+            storageValue.DefaultValue = 100;
+            AssertEqual(100, storageValue.CurrentValue);
+
+            storageValue.CurrentValue = 10;
+            AssertEqual(100, storageValue.DefaultValue);
+            AssertEqual(10, storageValue.CurrentValue);
+        }
+
+        private static void AssertEqual(object a, object b)
+        {
+            if (!a.Equals(b))
+            {
+            }
         }
     }
 
     public class StorageValue<T>
     {
+        public void Increment()
+        {
+            lock (_locker)
+            {
+                CurrentValueIndex++;
+            }
+        }
+
+        public void Decrement()
+        {
+            lock (_locker)
+            {
+                CurrentValueIndex--;
+                if (CurrentValueIndex < 0)
+                {
+                    CurrentValueIndex = 0;
+                }
+            }
+        }
+
+        private readonly object _locker = new object();
+
         public T this[int valueIndex]
         {
             set
             {
-                ValueList[valueIndex] = value;
+                lock (_locker)
+                {
+                    ValueList[valueIndex] = value;
+                }
             }
-            get => ValueList[valueIndex];
+            get
+            {
+                lock (_locker)
+                {
+                    return ValueList[valueIndex];
+                }
+            }
         }
 
         public T CurrentValue
         {
             set
             {
-                ValueList ??= new List<T>();
+                lock (_locker)
+                {
+                    ValueList ??= new List<T>();
 
-                if (CurrentValueIndex == ValueList.Count)
-                {
-                    ValueList.Add(value);
-                }
-                else if (CurrentValueIndex > ValueList.Count)
-                {
-                    var addCount = CurrentValueIndex - ValueList.Count;
-                    ValueList.AddRange(Enumerable.Repeat<T>(default, addCount));
+                    if (CurrentValueIndex == ValueList.Count)
+                    {
+                        ValueList.Add(value);
+                    }
+                    else if (CurrentValueIndex > ValueList.Count)
+                    {
+                        var addCount = CurrentValueIndex - ValueList.Count;
+                        ValueList.AddRange(Enumerable.Repeat<T>(default, addCount));
 
-                    ValueList.Add(value);
-                }
-                else
-                {
-                    ValueList[CurrentValueIndex] = value;
+                        ValueList.Add(value);
+                    }
+                    else
+                    {
+                        ValueList[CurrentValueIndex] = value;
+                    }
                 }
             }
             get
             {
-                if (ValueList == null || ValueList.Count == 0)
+                lock (_locker)
                 {
-                    return DefaultValue;
-                }
+                    if (ValueList == null || ValueList.Count == 0)
+                    {
+                        return DefaultValue;
+                    }
 
-                if (CurrentValueIndex >= ValueList.Count)
-                {
-                    return ValueList[^1];
-                }
+                    if (CurrentValueIndex >= ValueList.Count)
+                    {
+                        return ValueList[^1];
+                    }
 
-                return ValueList[CurrentValueIndex];
+                    return ValueList[CurrentValueIndex];
+                }
             }
         }
 
@@ -67,6 +125,6 @@ namespace FairneabairjibearhearLanallgekay
 
         public List<T> ValueList { set; get; }
 
-        public int CurrentValueIndex { set; get; }
+        public int CurrentValueIndex { private set; get; }
     }
 }
