c7129b126224d215f4504e7bc9886b75690c5401
lindexi <lindexi_gd@163.com>
2020-10-03 14:06:18 +0800

    多进程管道

---
+++ Ipc/Ipc/ByteListExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace Ipc
+{
+    static class ByteListExtension
+    {
+        public static bool Equals(byte[] a, byte[] b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Ipc/Ipc/ByteListMessageStream.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Ipc
+{
+    class ByteListMessageStream : MemoryStream
+    {
+        private readonly ISharedArrayPool _sharedArrayPool;
+        private byte[] Buffer { get; }
+
+        public ByteListMessageStream(byte[] buffer, int count, ISharedArrayPool sharedArrayPool) : base(buffer, 0, count, false)
+        {
+            _sharedArrayPool = sharedArrayPool;
+            Buffer = buffer;
+        }
+
+        ~ByteListMessageStream()
+        {
+            _sharedArrayPool.Return(Buffer);
+        }
+    }
+}
