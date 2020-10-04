2d26dc27112fd1955ffba8ca8d2cb4faffda8eb3
lindexi <lindexi_gd@163.com>
2020-10-04 15:34:20 +0800

    提供读取方法

---
+++ Ipc/IpcMessageContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Ipc
+{
+    internal readonly struct IpcMessageContext
+    {
+        public IpcMessageContext(in ulong ack, byte[] messageBuffer, in uint messageLength,
+            ISharedArrayPool sharedArrayPool)
+        {
+            Ack = ack;
+            MessageBuffer = messageBuffer;
+            MessageLength = messageLength;
+            SharedArrayPool = sharedArrayPool;
+        }
+
+        public ulong Ack { get; }
+        public byte[] MessageBuffer { get; }
+        public uint MessageLength { get; }
+        public ISharedArrayPool SharedArrayPool { get; }
+    }
+}
---
+++ Ipc/IpcMessageConverterTest.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Ipc
+{
+    class IpcMessageConverterTest
+    {
+        public async void Run()
+        {
+            await ChawewhuhiLafihoweredal();
+        }
+
+        public async Task ChawewhuhiLafihoweredal()
+        {
+            // 写入的数据和读取的相同
+            using var memoryStream = new MemoryStream();
+
+            var ipcConfiguration = new IpcConfiguration();
+            ulong ack = 10;
+            var buffer = new byte[] { 0x12, 0x12, 0x00 };
+            await IpcMessageConverter.WriteAsync(memoryStream, ipcConfiguration.MessageHeader, ack, buffer, 0, buffer.Length);
+
+            memoryStream.Position = 0;
+            var (success, ipcMessageContext) = await IpcMessageConverter.ReadAsync(memoryStream, ipcConfiguration.MessageHeader, new SharedArrayPool());
+            if (success)
+            {
+
+            }
+
+            if (ipcMessageContext.Ack == ack)
+            {
+
+            }
+        }
+    }
+
+
+}
