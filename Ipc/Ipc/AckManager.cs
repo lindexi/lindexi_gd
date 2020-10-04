384b38387bca313d4f2a85735a991e90186a62a8
lindexi <lindexi_gd@163.com>
2020-10-04 10:34:40 +0800

    修改回复信息

---
+++ Ipc/Ipc/AckManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+namespace Ipc
+{
+    class AckManager
+    {
+        public ulong CurrentAck { set; get; }
+
+        public bool IsAckMessage(Stream stream, out ulong ack)
+        {
+            var position = stream.Position;
+            if (IsAckMessageInner(stream, out ack))
+            {
+                return true;
+            }
+
+            stream.Position = position;
+
+            return false;
+        }
+
+        public byte[] BuildAckMessage(ulong receivedAck)
+        {
+            const int ackLength = sizeof(ulong) + sizeof(ulong);
+            byte[] buffer = new byte[AckHeader.Length + ackLength];
+
+            Array.Copy(AckHeader,buffer,AckHeader.Length);
+
+            var memoryStream = new MemoryStream(buffer)
+            {
+                Position = AckHeader.Length
+            };
+            var binaryWriter = new BinaryWriter(memoryStream);
+            binaryWriter.Write(receivedAck);
+            CurrentAck++;
+            binaryWriter.Write(CurrentAck);
+
+            return buffer;
+        }
+
+        private bool IsAckMessageInner(Stream stream, out ulong ack)
+        {
+            /*
+             * AckHeader
+             * 回复的 ACK ulong
+             * 推荐的下一次使用的Ack ulong
+             */
+            ack = 0;
+            const int ackLength = sizeof(ulong) + sizeof(ulong);
+            if (stream.Length - stream.Position != AckHeader.Length + ackLength)
+            {
+                return false;
+            }
+
+            if (!IsAckHeader(stream)) return false;
+
+            var binaryReader = new BinaryReader(stream);
+            ack = binaryReader.ReadUInt64();
+            var nextAck = binaryReader.ReadUInt64();
+            CurrentAck = Math.Max(CurrentAck, nextAck);
+
+            return false;
+        }
+
+        private bool IsAckHeader(Stream stream)
+        {
+            foreach (var ack in AckHeader)
+            {
+                if (stream.ReadByte() != ack)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // ACK 0x41, 0x43, 0x4B
+        public byte[] AckHeader { get; } = new byte[] { 0x41, 0x43, 0x4B };
+    }
+}
