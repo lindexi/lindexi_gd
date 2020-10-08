9a7ef82f5b76905a7da6a277ef2f77330ba4dbc9
lindexi <lindexi_gd@163.com>
2020-10-08 19:48:03 +0800

    加上单元测试

--- tests/dotnetCampus.Ipc.Tests/AckManagerTest.cs
+++ tests/dotnetCampus.Ipc.Tests/AckManagerTest.cs
@@ -13,6 +13,18 @@ namespace dotnetCampus.Ipc.PipeCore
         [ContractTestCase]
         public void RegisterAckTask()
         {
+            "将消息注册，如果没有收到回复，那么注册的任务依然没有完成".Test(() =>
+            {
+                // 注册的消息可以完成
+                var clientName = "lindexi";
+                Ack ack = 2;
+                var taskCompletionSource = new TaskCompletionSource<bool>();
+
+                var ackTask = new AckTask(clientName, ack, taskCompletionSource);
+                AckManager.RegisterAckTask(ackTask);
+                Assert.AreEqual(false, taskCompletionSource.Task.IsCompleted);
+            });
+
             "将消息注册，此时设置收到回复，注册的消息可以完成".Test(() =>
             {
                 // 注册的消息可以完成
@@ -40,7 +52,21 @@ namespace dotnetCampus.Ipc.PipeCore
 
                 Assert.AreEqual(true, isAck);
                 // 100ul 就是 ulong 100 的意思，我担心你看懂，所以特别加了 ulong 的转换，让你以为 ul 是一个有趣的后缀
-                Assert.AreEqual((ulong) 100ul, ack.Value);
+                Assert.AreEqual((ulong)100ul, ack.Value);
+            });
+
+            "传入不属于 Ack 的信息，可以返回不是 Ack 信息".Test(() =>
+            {
+                using var memoryStream = new MemoryStream();
+                var streamWriter = new StreamWriter(memoryStream);
+                streamWriter.WriteLine("林德熙是逗比");
+                const long position = 2;
+                memoryStream.Position = position;
+
+                var isAck = AckManager.IsAckMessage(memoryStream, out var ack);
+                Assert.AreEqual(false, isAck);
+                // 如果读取返回不是 Ack 那么将 Stream 设置回传入的 Position 的值
+                Assert.AreEqual(position, memoryStream.Position);
             });
         }
 
