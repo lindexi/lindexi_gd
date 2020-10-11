353b7be7546f4fb2e6bebd073da4245b63091d36
lindexi <lindexi_gd@163.com>
2020-10-11 22:43:18 +0800

    加上测试

--- tests/dotnetCampus.Ipc.Tests/IpcMessageConverterTest.cs
+++ tests/dotnetCampus.Ipc.Tests/IpcMessageConverterTest.cs
@@ -13,6 +13,27 @@ namespace dotnetCampus.Ipc.PipeCore
         [ContractTestCase]
         public void IpcMessageConverterWriteAsync()
         {
+            "读取消息头不对的数据，可以返回读取失败".Test(async () =>
+            {
+                using var memoryStream = new MemoryStream();
+
+                var ipcConfiguration = new IpcConfiguration();
+                ulong ack = 10;
+                var buffer = new byte[] { 0x12, 0x12, 0x00 };
+                var messageHeader = new byte[] {0x00, 0x00};
+                await IpcMessageConverter.WriteAsync(memoryStream, messageHeader, ack, IpcMessageCommandType.Unknown, buffer, 0,
+                    buffer.Length, "test", null!);
+
+                memoryStream.Position = 0;
+                var ipcMessageResult = await IpcMessageConverter.ReadAsync(memoryStream,
+                    ipcConfiguration.MessageHeader, new SharedArrayPool());
+                var success = ipcMessageResult.Success;
+                var ipcMessageCommandType = ipcMessageResult.IpcMessageCommandType;
+
+                Assert.AreEqual(false, success);
+                Assert.AreEqual(IpcMessageCommandType.Unknown, ipcMessageCommandType);
+            });
+
             "写入的数据和读取的相同，可以读取到写入的数据".Test(async () =>
             {
                 // 写入的数据和读取的相同
