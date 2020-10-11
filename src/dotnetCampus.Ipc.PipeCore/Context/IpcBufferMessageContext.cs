85e3240547794811982e21f945f1d86df0493c4e
lindexi <lindexi_gd@163.com>
2020-10-11 19:43:38 +0800

    支持将多条消息合作为一条

--- src/dotnetCampus.Ipc.PipeCore/Context/IpcBufferMessageContext.cs
+++ src/dotnetCampus.Ipc.PipeCore/Context/IpcBufferMessageContext.cs
@@ -1,4 +1,7 @@
-﻿namespace dotnetCampus.Ipc.PipeCore.Context
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnetCampus.Ipc.PipeCore.Context
 {
     /// <summary>
     /// 包含多条信息的上下文
@@ -36,6 +39,39 @@
 
                 return length;
             }
+        }
+
+        /// <summary>
+        /// 和其他的合并然后创建新的
+        /// </summary>
+        /// <param name="mergeBefore">将加入的内容合并到新的消息前面，为 true 合并到前面，否则合并到后面</param>
+        /// <param name="ipcBufferMessageList"></param>
+        /// <returns></returns>
+        public IpcBufferMessageContext BuildWithCombine(bool mergeBefore, params IpcBufferMessage[] ipcBufferMessageList)
+            => BuildWithCombine(Summary, mergeBefore, ipcBufferMessageList);
+
+        /// <summary>
+        /// 和其他的合并然后创建新的
+        /// </summary>
+        /// <param name="summary">表示写入的是什么内容，用于调试</param>
+        /// <param name="mergeBefore">将加入的内容合并到新的消息前面，为 true 合并到前面，否则合并到后面</param>
+        /// <param name="ipcBufferMessageList"></param>
+        /// <returns></returns>
+        public IpcBufferMessageContext BuildWithCombine(string summary, bool mergeBefore, params IpcBufferMessage[] ipcBufferMessageList)
+        {
+            var newIpcBufferMessageList = new List<IpcBufferMessage>(ipcBufferMessageList.Length + IpcBufferMessageList.Length);
+            if (mergeBefore)
+            {
+                newIpcBufferMessageList.AddRange(ipcBufferMessageList);
+                newIpcBufferMessageList.AddRange(IpcBufferMessageList);
+            }
+            else
+            {
+                newIpcBufferMessageList.AddRange(IpcBufferMessageList);
+                newIpcBufferMessageList.AddRange(ipcBufferMessageList);
+            }
+
+            return new IpcBufferMessageContext(summary, newIpcBufferMessageList.ToArray());
         }
     }
 }
