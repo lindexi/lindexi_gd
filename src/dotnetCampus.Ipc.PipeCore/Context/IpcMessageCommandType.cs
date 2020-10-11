dca03f4c2e61314178f4120838d2d22ce4cbf404
lindexi <lindexi_gd@163.com>
2020-10-11 22:20:39 +0800

    加上命令枚举

---
+++ src/dotnetCampus.Ipc.PipeCore/Context/IpcMessageCommandType.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnetCampus.Ipc.PipeCore.Context
+{
+    /// <summary>
+    /// 用于作为命令类型，用于框架的命令和业务的命令
+    /// </summary>
+    internal enum IpcMessageCommandType : ushort
+    {
+        Business = 0,
+
+        Unknown = ushort.MaxValue,
+    }
+}
