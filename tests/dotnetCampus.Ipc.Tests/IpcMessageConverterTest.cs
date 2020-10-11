82502c89af6dff71b4f3c8d638d9cd19e3249ba8
lindexi <lindexi_gd@163.com>
2020-10-11 22:46:04 +0800

    加上测试

--- tests/dotnetCampus.Ipc.Tests/IpcMessageConverterTest.cs
+++ tests/dotnetCampus.Ipc.Tests/IpcMessageConverterTest.cs
@@ -13,6 +13,28 @@ namespace dotnetCampus.Ipc.PipeCore
         [ContractTestCase]
         public void IpcMessageConverterWriteAsync()
         {
+            "读取消息头不对的数据，可以返回读取失败".Test(async () =>
+            {
+                using var memoryStream = new MemoryStream();
+
+                ulong ack = 10;
+                var buffer = new byte[] { 0x12, 0x12, 0x00 };
+                var messageHeader = new byte[] { 0x00, 0x00 };
+                var breakMessageHeader = new byte[] { 0x00, 0x01 };
+
+                await IpcMessageConverter.WriteAsync(memoryStream, messageHeader, ack, IpcMessageCommandType.Unknown, buffer, 0,
+                    buffer.Length, "test", null!);
+
+                memoryStream.Position = 0;
+                var ipcMessageResult = await IpcMessageConverter.ReadAsync(memoryStream,
+                    breakMessageHeader, new SharedArrayPool());
+                var success = ipcMessageResult.Success;
+                var ipcMessageCommandType = ipcMessageResult.IpcMessageCommandType;
+
+                Assert.AreEqual(false, success);
+                Assert.AreEqual(IpcMessageCommandType.Unknown, ipcMessageCommandType);
+            });
+
             "读取消息头长度不对的数据，可以返回读取失败".Test(async () =>
             {
                 using var memoryStream = new MemoryStream();
