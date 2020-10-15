467ee32441a829d504aafaf507990f7c6ed54d2d
lindexi <lindexi_gd@163.com>
2020-10-15 20:36:44 +0800

    对称控件

---
+++ JecolairbuChejemdaina/JecolairbuChejemdaina/VisualCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace JecolairbuChejemdaina
+{
+    public class VisualCanvas : FrameworkElement
+    {
+        protected override Visual GetVisualChild(int index)
+        {
+            return Visual;
+        }
+
+        protected override int VisualChildrenCount => 1;
+
+        public VisualCanvas(DrawingVisual visual)
+        {
+            Visual = visual;
+            AddVisualChild(visual);
+        }
+
+        public DrawingVisual Visual { get; }
+    }
+}
