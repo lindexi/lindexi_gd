cceca825b8ccbc19dbd94aea7e14c7bcc3986b79
lindexi <lindexi_gd@163.com>
2020-10-18 19:36:41 +0800

    加入测试不通过的代码

---
+++ src/dotnetCampus.Ipc.PipeCore/Core_/Peer_/PeerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace dotnetCampus.Ipc.PipeCore
+{
+    class PeerManager
+    {
+        public ConcurrentDictionary<string, PeerProxy> ConnectedServerManagerList { get; } =
+            new ConcurrentDictionary<string, PeerProxy>();
+
+        public bool TryAdd(PeerProxy peerProxy) => ConnectedServerManagerList.TryAdd(peerProxy.PeerName, peerProxy);
+
+        public async Task WaitForPeerConnectFinishedAsync(PeerProxy peerProxy)
+        {
+            await peerProxy.WaitForFinishedTaskCompletionSource.Task;
+
+            // 更新或注册，用于解决之前注册的实际上是断开的连接
+            ConnectedServerManagerList.AddOrUpdate(peerProxy.PeerName, peerProxy, (s, proxy) => proxy);
+        }
+    }
+}
