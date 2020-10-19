423235ec3bef471af9e7fd732c285d8f27251d91
lindexi <lindexi_gd@163.com>
2020-10-19 10:19:51 +0800

    WPF Binding StackOverflowException

---
+++ Walterlv.WpfIssues.StackOverflowException/Walterlv.Issues.Binding/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Walterlv.Issues
+{
+    public class MainViewModel
+    {
+        public List<object> Items { get; } = new List<object>
+        {
+            "123",
+        };
+    }
+}
