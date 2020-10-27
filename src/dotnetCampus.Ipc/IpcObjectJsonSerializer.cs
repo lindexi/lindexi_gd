b079f45256663d67bba33ba8c4c7b4608fb83e1f
lindexi <lindexi_gd@163.com>
2020-10-27 08:58:52 +0800

    加上发送给服务器的信息和从服务器收到
    Copy from: https://github.com/jacqueskang/IpcServiceFramework
    
    https://github.com/jacqueskang/IpcServiceFramework/issues/171

--- src/dotnetCampus.Ipc/IpcObjectJsonSerializer.cs
+++ src/dotnetCampus.Ipc/IpcObjectJsonSerializer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -19,6 +21,96 @@ namespace dotnetCampus.Ipc
             var json = Encoding.UTF8.GetString(byteList);
             return JsonConvert.DeserializeObject<T>(json);
         }
+    }
+
+    /// <summary>
+    /// 发送给服务器的请求数据
+    /// </summary>
+    /// Copy From: https://github.com/jacqueskang/IpcServiceFramework.git
+    public class IpcRequest
+    {
+        public Type ObjectType { set; get; }
+
+        public string MethodName { get; set; }
+
+        public List<IpcRequestParameter> ParameterList { set; get; }
+
+        public List<Type> GenericArgumentList { set; get; }
+
+        public Type ReturnType { set; get; }
+    }
+
+    /// <summary>
+    /// 从服务器返回的信息
+    /// </summary>
+    /// Copy From: https://github.com/jacqueskang/IpcServiceFramework.git
+    public class IpcResponse
+    {
+        public static IpcResponse Success(object data)
+            => new IpcResponse(IpcStatus.Ok, data, null, null);
+
+        public static IpcResponse BadRequest()
+            => new IpcResponse(IpcStatus.BadRequest, null, null, null);
+
+        public static IpcResponse BadRequest(string errorDetails)
+            => new IpcResponse(IpcStatus.BadRequest, null, errorDetails, null);
+
+        public static IpcResponse BadRequest(string errorDetails, Exception innerException)
+            => new IpcResponse(IpcStatus.BadRequest, null, errorDetails, innerException);
+
+        public static IpcResponse InternalServerError()
+            => new IpcResponse(IpcStatus.InternalServerError, null, null, null);
+
+        public static IpcResponse InternalServerError(string errorDetails)
+            => new IpcResponse(IpcStatus.InternalServerError, null, errorDetails, null);
+
+        public static IpcResponse InternalServerError(string errorDetails, Exception innerException)
+            => new IpcResponse(IpcStatus.InternalServerError, null, errorDetails, innerException);
+
+        public IpcResponse(
+            IpcStatus status,
+            object data,
+            string errorMessage,
+            Exception innerException)
+        {
+            Status = status;
+            Data = data;
+            ErrorMessage = errorMessage;
+            InnerException = innerException;
+        }
+
+        [DataMember]
+        public IpcStatus Status { get; }
+
+        [DataMember]
+        public object Data { get; }
+
+        [DataMember]
+        public string ErrorMessage { get; set; }
+
+        [DataMember]
+        public Exception InnerException { get; }
+
+        public bool Succeed() => Status == IpcStatus.Ok;
+    }
+
+    /// <summary>
+    /// 从服务器返回的值
+    /// </summary>
+    /// Copy From: https://github.com/jacqueskang/IpcServiceFramework.git
+    public enum IpcStatus : int
+    {
+        Unknown = 0,
+        Ok = 200,
+        BadRequest = 400,
+        InternalServerError = 500,
+    }
+
+    public class IpcRequestParameter
+    {
+        public Type ParameterType { set; get; }
+
+        public object Value { set; get; }
     }
 
     public class IpcProxy<T> : DispatchProxy
