93dbcbf6f9e0e1e0828af36111164cf0401413d0
lindexi <lindexi_gd@163.com>
2020-10-27 17:36:28 +0800

    加上空白的项目

---
+++ incorporates/incorporates/incorporates/instrument.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incorporates
+{
+   public static class instrument
+    {
+        public static Random ran
+        {
+            set
+            {
+                _ran = value;
+            }
+            get
+            {
+                return _ran;
+            }
+        }        
+        public static bool stochastic(double per)
+        {
+            return ran.Next() % 10000 < per * 10000;
+        }
+        public static double region(double a , double b , double value)
+        {
+            if (value > a)
+            {
+                return a;
+            }
+            else if (value < b)
+            {
+                return b;
+            }
+            else
+            {
+                return value;
+            }
+        }
+        private static Random _ran = new Random();        
+    }
+}
