b5bc99a1c6e51ff179c865c690e660115abe88f8
lindexi <lindexi_gd@163.com>
2020-10-28 10:36:28 +0800

    英语读音

---
+++ LinemlallledurKaicawkeedaykerewho/LinemlallledurKaicawkeedaykerewho/WordFileInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace LinemlallledurKaicawkeedaykerewho
+{
+    public class WordFileInfo
+    {
+        public WordFileInfo(string word, string fileName)
+        {
+            Word = word;
+            FileName = fileName;
+        }
+
+        public string Word { get; }
+
+        public string FileName { get; }
+    }
+}
