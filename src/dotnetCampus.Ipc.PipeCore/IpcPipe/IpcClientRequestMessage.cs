0eb6f6d337a37e67634cf26478beab8cc3fccf0d
lindexi <lindexi_gd@163.com>
2020-11-01 22:17:22 +0800

    加上请求定义

---
+++ src/dotnetCampus.Ipc.PipeCore/IpcPipe/IpcClientRequestMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using dotnetCampus.Ipc.PipeCore.Context;
+
+namespace dotnetCampus.Ipc.PipeCore
+{
+    class IpcClientRequestMessage
+    {
+        public IpcClientRequestMessage(IpcBufferMessageContext ipcBufferMessageContext, Task<IpcBufferMessage> task, ulong messageId)
+        {
+            IpcBufferMessageContext = ipcBufferMessageContext;
+            Task = task;
+            MessageId = messageId;
+        }
+
+        public IpcBufferMessageContext IpcBufferMessageContext { get; }
+
+        public Task<IpcBufferMessage> Task { get; }
+
+        public ulong MessageId { get; }
+    }
+}
