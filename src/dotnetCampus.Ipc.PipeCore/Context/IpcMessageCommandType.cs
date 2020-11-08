239dec85be64ced714698471d54a5a2edbb213ab
lindexi <lindexi_gd@163.com>
2020-11-08 16:19:23 +0800

    开放命令枚举

--- src/dotnetCampus.Ipc.PipeCore/Context/IpcMessageCommandType.cs
+++ src/dotnetCampus.Ipc.PipeCore/Context/IpcMessageCommandType.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// 用于作为命令类型，用于框架的命令和业务的命令
     /// </summary>
-    internal enum IpcMessageCommandType : ushort
+    public enum IpcMessageCommandType : ushort
     {
         /// <summary>
         /// 向对方服务器注册
