d20149b7ace4d0b6e8ebb0a00aaede29a8de5118
lindexi <lindexi_gd@163.com>
2020-11-09 09:27:42 +0800

    特性标记成员过时保持库和框架的兼容性

---
+++ BegibaberGawhilofigurwhal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BegibaberGawhilofigurwhal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Foo
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [Obsolete("请使用 F2 代替")]
+        public int F1
+        {
+            set => F2 = value;
+            get => F2;
+        }
+
+        public int F2 { set; get; }
+    }
+}
