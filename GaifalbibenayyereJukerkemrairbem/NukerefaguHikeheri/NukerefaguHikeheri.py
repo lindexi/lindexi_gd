9b4f948b2a3f070c14f4eb281a5590b19288feab
lindexi <lindexi_gd@163.com>
2020-11-20 17:15:10 +0800

    定义颜色

---
+++ GaifalbibenayyereJukerkemrairbem/NukerefaguHikeheri/NukerefaguHikeheri.py
@@ -0,0 +1,10 @@
+import wpf
+
+from System.Windows import Application, Window
+
+class MyWindow(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'NukerefaguHikeheri.xaml')  
+
+if __name__ == '__main__':
+    Application().Run(MyWindow())
