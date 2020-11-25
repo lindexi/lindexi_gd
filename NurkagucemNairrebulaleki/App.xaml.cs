1f465f6370cb6581642752d344eaf97b25740bf6
lindexi <lindexi_gd@163.com>
2020-11-25 11:31:19 +0800

    读写代码

--- NurkagucemNairrebulaleki/App.xaml.cs
+++ NurkagucemNairrebulaleki/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace KemkicemdurFemceloja
+namespace NurkagucemNairrebulaleki
 {
     /// <summary>
     /// Interaction logic for App.xaml
