3f6a26721185e3e75cb7a9eaf83fead2b8ed0fe9
lindexi <lindexi_gd@163.com>
2020-11-26 15:34:22 +0800

    控件资源

---
+++ LojafeajahaykaWiweyarcerhelralya/LojafeajahaykaWiweyarcerhelralya/F1.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LojafeajahaykaWiweyarcerhelralya
+{
+    public class F1 : UserControl
+    {
+        public F1()
+        {
+            Loaded += F1_Loaded;
+        }
+
+        private void F1_Loaded(object sender, RoutedEventArgs e)
+        {
+            var foo = Resources["Foo"];
+        }
+    }
+}
--- LojafeajahaykaWiweyarcerhelralya/LojafeajahaykaWiweyarcerhelralya/MainWindow.xaml.cs
+++ LojafeajahaykaWiweyarcerhelralya/LojafeajahaykaWiweyarcerhelralya/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,38 @@ namespace LojafeajahaykaWiweyarcerhelralya
         public MainWindow()
         {
             InitializeComponent();
+        }
+    }
+
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Collection.Add(i.ToString());
+            }
+        }
+
+        public ObservableCollection<string> Collection { get; }  = new ObservableCollection<string>();
+    }
+
+    public class FooTextBlock : TextBlock
+    {
+        public FooTextBlock()
+        {
+            Loaded += FooTextBlock_Loaded;
+            Unloaded += FooTextBlock_Unloaded;
+        }
+
+        private void FooTextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"{Text} FooTextBlock_Loaded");
+        }
+
+        private void FooTextBlock_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"{Text} FooTextBlock_Unloaded");
         }
     }
 }
