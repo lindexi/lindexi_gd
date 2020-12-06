897d431d19e4fcedc8f8d6e28c95bf6cc10d0e07
lindexi <lindexi_gd@163.com>
2020-12-06 17:50:31 +0800

    访问文件

---
+++ KawbacayerelaKejeldemwearlai/KawbacayerelaKejeldemwearlai/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace KawbacayerelaKejeldemwearlai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe";
+
+            try
+            {
+                var fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None, bufferSize: 1024, FileOptions.None);
+                File.Open(filePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                File.Open(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                File.Open("1.txt", FileMode.Append, FileAccess.Read, FileShare.None);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ KebejeeballNeejayurwe/KebejeeballNeejayurwe/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace KebejeeballNeejayurwe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logFile = @"f:\temp\build\build.txt";
+            PrintLog(logFile);
+        }
+
+        private static void PrintLog(string logFile)
+        {
+            DateTimeOffset? lastTime = null;
+
+            var allLogTextList = File.ReadAllLines(logFile);
+            foreach (var logText in allLogTextList)
+            {
+                // 读取第一个空格
+                var text = logText;
+                var spaceIndex = logText.IndexOf(' ');
+                if (spaceIndex > 0)
+                {
+                    var span = logText.AsSpan();
+                    var timeText = span.Slice(0, spaceIndex);
+                    if (DateTimeOffset.TryParse(timeText, out var currentTime))
+                    {
+                        if (lastTime != null)
+                        {
+                            var offset = currentTime - lastTime.Value;
+
+                            if (offset > TimeSpan.FromMilliseconds(10) && offset < TimeSpan.FromMinutes(1))
+                            {
+                                Task.Delay(offset).Wait();
+                            }
+                        }
+
+                        lastTime = currentTime;
+
+                        text = logText.Substring(spaceIndex);
+                    }
+                }
+
+                Console.WriteLine(DateTimeOffset.Now.ToString("O") + " " + text);
+            }
+        }
+    }
+}
