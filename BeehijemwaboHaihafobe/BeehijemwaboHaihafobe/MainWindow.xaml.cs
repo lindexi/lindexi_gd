df12af70d3ca6cb496e67a29ffee3ec52443d354
lindexi <lindexi_gd@163.com>
2020-12-17 19:38:22 +0800

    点击界面

--- BeehijemwaboHaihafobe/BeehijemwaboHaihafobe/MainWindow.xaml.cs
+++ BeehijemwaboHaihafobe/BeehijemwaboHaihafobe/MainWindow.xaml.cs
@@ -22,28 +22,6 @@ namespace BeehijemwaboHaihafobe
         public MainWindow()
         {
             InitializeComponent();
-
-            TouchDown += MainWindow_TouchDown;
-            TouchUp += MainWindow_TouchUp;
         }
-
-        private void MainWindow_TouchUp(object sender, TouchEventArgs e)
-        {
-            _currentTouchCount--;
-        }
-
-        private void MainWindow_TouchDown(object sender, TouchEventArgs e)
-        {
-            CaptureTouch(e.TouchDevice);
-
-            if (_currentTouchCount == 0)
-            {
-                TouchDragMoveWindowHelper.DragMove(this);
-            }
-
-            _currentTouchCount++;
-        }
-
-        private uint _currentTouchCount;
     }
 }
