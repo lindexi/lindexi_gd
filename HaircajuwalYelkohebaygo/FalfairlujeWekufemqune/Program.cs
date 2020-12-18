60f693570de8b862b25e5417f284067f2eab1204
lindexi <lindexi_gd@163.com>
2020-12-18 20:50:26 +0800

    序列化

---
+++ HaircajuwalYelkohebaygo/FalfairlujeWekufemqune/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+namespace FalfairlujeWekufemqune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new HaircajuwalYelkohebaygo.Program();
+        }
+    }
+}
