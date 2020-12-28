609b6e45982b3037de5c89f5efc923c270edeb2f
lindexi <lindexi_gd@163.com>
2020-12-28 09:11:08 +0800

    转换代码

--- MacListConvert/MacListConvert/Program.cs
+++ MacListConvert/MacListConvert/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using CsvHelper;
 
 namespace MacListConvert
 {
@@ -8,9 +13,55 @@ namespace MacListConvert
     {
         static void Main(string[] args)
         {
-            var text = "2:123:45:67;123:123:123:123:123";
+            var csvFile = "1.csv";
 
+            if (args.Length > 0)
+            {
+                csvFile = args[0];
+            }
+
+            csvFile = Path.GetFullPath(csvFile);
+            Console.WriteLine($"进行转换的 CSV 文件:{csvFile}");
+
+            var output = "2.csv";
+            var stringBuilder = new StringBuilder();
+
+            var csvFileTextLine = File.ReadLines(csvFile).ToList();
+            var regex = new Regex(@"""([\d:]*)"",""(\d)""");
+            for (var i = 1; i < csvFileTextLine.Count; i++)
+            {
+                var textLine = csvFileTextLine[i];
+                var match = regex.Match(textLine);
+                if (match.Success)
+                {
+                    var macListText = match.Groups[1].Value;
+                    var macList = ConvertMacList(macListText);
+                    foreach (var mac in macList)
+                    {
+                        //var macInfo = new MacInfo()
+                        //{
+                        //    Mac = mac,
+                        //    Number= match.Groups[2].Value
+                        //};
+
+                        //csvWriter.WriteRecord(macInfo);
+                        stringBuilder.Append($"\"{mac}\",\"{match.Groups[2].Value}\"\r\n");
+                    }
+                }
+            }
+
+            //csvWriter.Dispose();
+            File.WriteAllText(output, stringBuilder.ToString());
+        }
+
+        private static List<string> ConvertMacList(string text)
+        {
             List<string> macList = new List<string>();
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return macList;
+            }
 
             // 先使用;分号分割
 
@@ -29,10 +80,7 @@ namespace MacListConvert
                 macList.Add(string.Join(':', decimalList.Select(temp => temp.ToString("X2"))));
             }
 
-            foreach (var mac in macList)
-            {
-                Console.WriteLine(mac);
-            }
+            return macList;
         }
     }
 }
