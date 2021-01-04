3c9d405df0f87521127de9bd2bdba05e3491f4bb
lindexi <lindexi_gd@163.com>
2021-01-04 16:04:20 +0800

    修复链接

---
+++ BelehereluJewelemkiho/BelehereluJewelemkiho/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Packaging;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace BelehereluJewelemkiho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "Excel.xlsx";
+
+            var openSettings = new OpenSettings()
+            {
+                RelationshipErrorHandlerFactory = RelationshipErrorHandler.CreateRewriterFactory(Rewriter)
+            };
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, isEditable: true, openSettings))
+                {
+
+                }
+            }
+        }
+
+        static string Rewriter(Uri partUri, string id, string uri)
+            => $"http://unknown?id={id}";
+    }
+}
