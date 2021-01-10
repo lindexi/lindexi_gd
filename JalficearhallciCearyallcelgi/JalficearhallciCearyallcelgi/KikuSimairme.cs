519ed308e98d8c8726af596e5a5edc17fd378f16
lindexi <lindexi_gd@163.com>
2021-01-10 10:44:35 +0800

    创建窗口

---
+++ JalficearhallciCearyallcelgi/JalficearhallciCearyallcelgi/KikuSimairme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using SharpDX.Windows;
+
+namespace JalficearhallciCearyallcelgi
+{
+    class KikuSimairme : IDisposable
+    {
+        public KikuSimairme()
+        {
+            _renderForm = new RenderForm
+            {
+                ClientSize = new Size(Width, Height)
+            };
+        }
+
+        private const int Width = 1280;
+
+        private const int Height = 720;
+
+        public void Run()
+        {
+            RenderLoop.Run(_renderForm, RenderCallback);
+        }
+
+        private readonly RenderForm _renderForm;
+
+        private void RenderCallback()
+        {
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _renderForm?.Dispose();
+        }
+    }
+}
