472672749e5f6357ecccc064ab7d99d8e103d2c1
lindexi <lindexi_gd@163.com>
2021-01-12 16:37:36 +0800

    优化接口

---
+++ HaricejelheeJukolaleehe/HaricejelheeJukolaleehe/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HaricejelheeJukolaleehe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return Assembly.LoadFile(@"f:\temp\WayaijocuFijulularqea.dll");
+        }
+    }
+}
---
+++ HaricejelheeJukolaleehe/WayaijocuFijulularqea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WayaijocuFijulularqea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Foo
+    {
+
+    }
+}
