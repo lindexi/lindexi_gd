0d8829cc7664bc2d60c6da3cbf08098aa6ea3da6
lindexi <lindexi_gd@163.com>
2021-01-17 10:28:18 +0800

    模拟输入

---
+++ DeademchemhuwereweeCiceegereli/DeademchemhuwereweeCiceegereli/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Input.Manipulations;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeademchemhuwereweeCiceegereli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var manipulationProcessor2D = new ManipulationProcessor2D(Manipulations2D.All);
+            manipulationProcessor2D.SetParameters(new ManipulationPivot2D()
+            {
+
+            });
+            manipulationProcessor2D.ProcessManipulators(0, new Manipulator2D[]
+            {
+                new Manipulator2D(1,0,0),
+            });
+            InitializeComponent();
+        }
+    }
+}
