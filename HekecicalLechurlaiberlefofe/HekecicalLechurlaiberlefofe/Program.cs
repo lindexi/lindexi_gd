5ef7085f36db5d242d0e085dcaa261e4bae3d674
lindexi <lindexi_gd@163.com>
2021-01-21 18:59:17 +0800

    格式化代码

--- HekecicalLechurlaiberlefofe/HekecicalLechurlaiberlefofe/Program.cs
+++ HekecicalLechurlaiberlefofe/HekecicalLechurlaiberlefofe/Program.cs
@@ -52,12 +52,6 @@ namespace HekecicalLechurlaiberlefofe
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
-
-
 }
