5ffa409e5631c2cc2dc27110ae9c335bc16ddb72
lindexi <lindexi_gd@163.com>
2021-02-22 17:44:02 +0800

    加上修改链接
    https://github.com/OfficeDev/Open-XML-SDK/pull/835

---
+++ LichearheecalJalewhujulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace LichearheecalJalewhujulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var fileStream =
+                new FileStream("1.pptx", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+
+            var openSettings = new OpenSettings()
+            {
+                RelationshipErrorHandlerFactory = RelationshipErrorHandler.CreateRewriterFactory(Rewriter)
+            };
+
+            using (var presentationDocument =
+                DocumentFormat.OpenXml.Packaging.PresentationDocument.Open(fileStream, true, openSettings))
+            {
+            }
+        }
+
+        static string Rewriter(Uri partUri, string id, string uri)
+            => $"http://unknown?id={id}";
+    }
+}
