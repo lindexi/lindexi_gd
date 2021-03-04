2927eb91e8e9a8c077062be6c411eede1b778d84
lindexi <lindexi_gd@163.com>
2021-03-04 19:29:04 +0800

    核平

--- server/newClassProject/WebRoot/js/register.js
+++
@@ -1,4 +0,0 @@
-function refresh() {
-	var vcode = document.getElementById('randImage');
-	vcode.src = "./getCheckCode.do?nocache=" + new Date().getTime();
-}
--- server/newClassProject/src/com/shusheng/util/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.shusheng.util;
-
-public class Constant {
-
-	public static final int studentStatus = 0;//表示学生
-	public static final int teacherStatus = 1;//表示教师
-}
