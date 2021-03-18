9c68faa6fefa7b34555e0c9a6d0807800ec4b4df
lindexi <lindexi_gd@163.com>
2021-03-18 16:32:33 +0800

    任务栏

--- BekairlilearDujalgereno/BekairlilearDujalgereno/App.xaml.cs
+++ BekairlilearDujalgereno/BekairlilearDujalgereno/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace LeekailawnahelNarjailearyaydi
+namespace BekairlilearDujalgereno
 {
     /// <summary>
     /// Interaction logic for App.xaml
