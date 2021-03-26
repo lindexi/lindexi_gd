0129f0e782de9ea4e0e16e3d85a31bdfc1297a0f
lindexi <lindexi_gd@163.com>
2021-03-26 19:09:25 +0800

    读取图片

---
+++ HairleakaibaniWawfeahewur/HairleakaibaniWawfeahewur/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using stakx.WIC;
+
+namespace HairleakaibaniWawfeahewur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WICImagingFactory factory = new WICImagingFactory();
+
+            foreach (var wicBitmapEncoderInfo in EnumEncoders(factory))
+            {
+                Console.WriteLine(wicBitmapEncoderInfo.GetFriendlyName());
+            }
+        }
+
+        static IEnumerable<IWICBitmapEncoderInfo> EnumEncoders(IWICImagingFactory wic)
+        {
+            return wic.CreateComponentEnumerator(WICComponentType.WICEncoder, WICComponentEnumerateOptions.WICComponentEnumerateDefault)
+                .AsEnumerable()
+                .OfType<IWICBitmapEncoderInfo>();
+        }
+    }
+}
