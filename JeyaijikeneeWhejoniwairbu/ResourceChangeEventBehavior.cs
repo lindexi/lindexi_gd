fe0c808b10336222aa7f427479c57accba28fa29
lindexi <lindexi_gd@163.com>
2021-04-12 19:34:37 +0800

    修改资源

---
+++ JeyaijikeneeWhejoniwairbu/ResourceChangeEventBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace JeyaijikeneeWhejoniwairbu
+{
+    public class ResourceChangeEventBehavior : Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty ResourceProperty = DependencyProperty.Register(
+            "Resource", typeof(object), typeof(ResourceChangeEventBehavior), new PropertyMetadata(default(object), ResourceChangedCallback));
+
+        public event EventHandler<ResourceChangedEventArgs> ResourceChanged;
+
+        public object Resource
+        {
+            get { return GetValue(ResourceProperty); }
+            set { SetValue(ResourceProperty, value); }
+        }
+
+        private static void ResourceChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            if (dependencyObject is ResourceChangeEventBehavior resourceChangeNotifier)
+            {
+                resourceChangeNotifier.OnResourceChanged(new ResourceChangedEventArgs(args.OldValue, args.NewValue));
+            }
+        }
+
+        private void OnResourceChanged(ResourceChangedEventArgs args)
+        {
+            ResourceChanged?.Invoke(this, args);
+        }
+    }
+}
