702fb9efb72abd7928810e20d814d70f51172c59
lindexi <lindexi_gd@163.com>
2021-04-24 11:45:15 +0800

    读取系统

---
+++ QerefadibairLehihekurhea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace QerefadibairLehihekurhea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"SystemManufacturer={Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS").GetValue("SystemManufacturer")}");
+            Console.WriteLine($"SystemProductName={Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS").GetValue("SystemProductName")}");
+            Console.Read();
+        }
+    }
+}
