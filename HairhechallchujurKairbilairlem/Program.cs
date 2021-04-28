6d3f9acd625d7005227ef4223b3b911b1cdef55a
lindexi <lindexi_gd@163.com>
2021-04-28 20:32:39 +0800

    获取启动项目

--- HairhechallchujurKairbilairlem/Program.cs
+++ HairhechallchujurKairbilairlem/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using OpenMcdf;
 using SetStartupProjects;
 
@@ -15,14 +17,40 @@ namespace HairhechallchujurKairbilairlem
             var sln = new FileInfo(@"..\..\..\HairhechallchujurKairbilairlem.sln");
 
             var projectList = SetStartupProjects.SolutionProjectExtractor.GetAllProjectFiles(sln.FullName).ToList();
-            var startProjectFinder = new StartProjectFinder();
-            var startProjectList = startProjectFinder.GetStartProjects(sln.FullName).ToList();
 
             using (var fileStream = suo.Open(FileMode.Open))
             {
-                using CompoundFile compoundFile = new CompoundFile(fileStream, CFSUpdateMode.ReadOnly, CFSConfiguration.SectorRecycle | CFSConfiguration.EraseFreeSectors);
+                CompoundFile compoundFile = new CompoundFile(fileStream, CFSUpdateMode.ReadOnly, CFSConfiguration.SectorRecycle | CFSConfiguration.EraseFreeSectors);
+                var cfStream = compoundFile.RootStorage.GetStream("SolutionConfiguration");
+                var byteList = cfStream.GetData();
+                var encoding = Encoding.GetEncodings()
+                    .Single(x => string.Equals(x.Name, "utf-16", StringComparison.OrdinalIgnoreCase));
+                var text = encoding.GetEncoding().GetString(byteList);
+
+                const char nul = '\u0000';
+                const char dc1 = '\u0011';
+                const char etx = '\u0003';
+                const char soh = '\u0001';
+
+                var regex = new Regex(@$"{dc1}{nul}MultiStartupProj{nul}={etx}[{nul}{soh}]{nul};4{nul}(.{{38}}).dwStartupOpt");
+                var match = regex.Match(text);
+
+                if (match.Success)
+                {
+                    var guid = Guid.Parse(match.Groups[1].Value);
+                    var project = projectList.FirstOrDefault(temp => new Guid(temp.Guid) == guid);
+
+                }
+
+                var startupProjectRegex = new Regex(@$"StartupProject{nul}={'\b'}&{nul}(.{{38}});A");
+                var startupProjectMatch = startupProjectRegex.Match(text);
+                if (startupProjectMatch.Success)
+                {
+                    var guid = Guid.Parse(startupProjectMatch.Groups[1].Value);
+                    var project = projectList.FirstOrDefault(temp => new Guid(temp.Guid) == guid);
+                }
             }
-           
+
         }
     }
 }
