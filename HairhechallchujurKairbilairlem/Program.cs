92385a8ea284dd3b1d30ba74af34bf1368c68223
lindexi <lindexi_gd@163.com>
2021-04-28 19:50:46 +0800

    读写启动文件

---
+++ HairhechallchujurKairbilairlem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using OpenMcdf;
+using SetStartupProjects;
+
+namespace HairhechallchujurKairbilairlem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var suo = new FileInfo(@"..\..\..\.vs\HairhechallchujurKairbilairlem\v16\.suo");
+
+            var sln = new FileInfo(@"..\..\..\HairhechallchujurKairbilairlem.sln");
+
+            var projectList = SetStartupProjects.SolutionProjectExtractor.GetAllProjectFiles(sln.FullName).ToList();
+            var startProjectFinder = new StartProjectFinder();
+            var startProjectList = startProjectFinder.GetStartProjects(sln.FullName).ToList();
+
+            using (var fileStream = suo.Open(FileMode.Open))
+            {
+                using CompoundFile compoundFile = new CompoundFile(fileStream, CFSUpdateMode.ReadOnly, CFSConfiguration.SectorRecycle | CFSConfiguration.EraseFreeSectors);
+            }
+           
+        }
+    }
+}
---
+++ KeqirawaKebuwheke/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeqirawaKebuwheke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
