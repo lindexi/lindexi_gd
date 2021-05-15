78c73fe25229f0b50992102e59c01cd535e60c31
lindexi <lindexi_gd@163.com>
2021-05-15 08:50:31 +0800

    The IWICImagingFactory_CreateDecoderFromFileHandle_Proxy do not support Asynchronous
    [BitmapDecoder.Create does not handle FileStream with FileOptions.Asynchronous · Issue #4355 · dotnet/wpf](https://github.com/dotnet/wpf/issues/4355 )

---
+++ JemlemlacuLemjakarbabo/DllImport.cs
@@ -0,0 +1,8 @@
+﻿namespace JemlemlacuLemjakarbabo
+{
+    internal static class DllImport
+    {
+        internal const string WindowsCodecs = "WindowsCodecs.dll";
+        internal const string WindowsCodecsExt = "WindowsCodecsExt.dll";
+    }
+}
