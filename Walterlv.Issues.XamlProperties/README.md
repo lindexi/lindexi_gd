1f303fa1a7ddd696f56145c1a3b93f765609fb5b
lindexi <lindexi_gd@163.com>
2021-06-04 20:39:53 +0800

    加上测试代码

---
+++ Walterlv.Issues.XamlProperties/README.md
@@ -0,0 +1,5 @@
+# Walterlv.Issues.XamlProperties
+
+This issue is reported here:
+
+* https://github.com/dotnet/wpf/issues/4544
---
+++ Walterlv.Issues.XamlProperties/Walterlv.Demo.XamlProperties/DependencyTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walterlv.Demo.XamlProperties
+{
+    public static class DependencyTest
+    {
+        public static readonly System.Windows.DependencyProperty FooProperty = System.Windows.DependencyProperty.RegisterAttached(
+            "Foo", typeof(bool), typeof(DependencyTest),
+            new System.Windows.PropertyMetadata(default(bool)));
+
+        public static bool GetFoo(System.Windows.DependencyObject element) => (bool)element.GetValue(FooProperty);
+
+        public static void SetFoo(System.Windows.DependencyObject element, bool value) => element.SetValue(FooProperty, value);
+    }
+}
