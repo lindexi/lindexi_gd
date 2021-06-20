116d727103d5ecfa6547bd44ae2cb860b963fc54
lindexi <lindexi_gd@163.com>
2021-06-20 15:56:06 +0800

    获取端口

---
+++ YagabaigekeaLuliluje/YagabaigekeaLuliluje/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace YagabaigekeaLuliluje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var availablePort = GetAvailablePort(IPAddress.Any);
+            Console.WriteLine(availablePort);
+            TcpListener l = new TcpListener(IPAddress.Any, availablePort);
+            l.Start();
+        }
+
+        public static int GetAvailablePort(IPAddress ip)
+        {
+            TcpListener l = new TcpListener(ip, 0);
+            l.Start();
+            int port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }
+    }
+}
