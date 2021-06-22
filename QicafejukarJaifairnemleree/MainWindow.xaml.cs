54b57574b4e99362c651a77251c7bc1d7ad6106f
lindexi <lindexi_gd@163.com>
2021-06-22 19:29:11 +0800

    命中点击

--- QicafejukarJaifairnemleree/MainWindow.xaml.cs
+++ QicafejukarJaifairnemleree/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QicafejukarJaifairnemleree
 {
@@ -23,6 +14,17 @@ namespace QicafejukarJaifairnemleree
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ComboBox_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.OriginalSource is FrameworkElement frameworkElement)
+            {
+                if (frameworkElement.DataContext is Brush brush)
+                {
+                    TextBlock.Foreground = brush;
+                }
+            }
         }
     }
 }
