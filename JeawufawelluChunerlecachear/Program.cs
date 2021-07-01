3d8d6a8391a3f37f2da00c881b6a428f04e3ff2e
lindexi <lindexi_gd@163.com>
2021-07-01 08:37:42 +0800

    不写文件内容

---
+++ JeawufawelluChunerlecachear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace JeawufawelluChunerlecachear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var wordprocessingDocument = WordprocessingDocument.Open("Test.docx", isEditable: true, new OpenSettings()
+            {
+                AutoSave = false
+            });
+
+            var rootPart = (MainDocumentPart) wordprocessingDocument.RootPart;
+            var document = rootPart!.Document;
+            var paragraph = document.Body!.GetFirstChild<Paragraph>();
+            var run = paragraph!.GetFirstChild<Run>();
+            var text = run!.GetFirstChild<Text>();
+            text!.Text = "逗比";
+        }
+    }
+}
