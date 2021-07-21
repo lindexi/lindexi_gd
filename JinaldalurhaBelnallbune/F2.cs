2f00793486fcb1962de7e368ec527cf1169db135
lindexi <lindexi_gd@163.com>
2021-07-21 19:27:29 +0800

    创建空对象

---
+++ JinaldalurhaBelnallbune/F2.cs
@@ -0,0 +1,7 @@
+﻿namespace JinaldalurhaBelnallbune
+{
+    class F2
+    {
+        public F3 F3 { get; } = new F3();
+    }
+}
---
+++ JinaldalurhaBelnallbune/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace JinaldalurhaBelnallbune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f1 = FormatterServices.GetUninitializedObject(typeof(F1));
+        }
+    }
+}
