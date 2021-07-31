0c3cd838fa60d519838989d3de74db67bb8a75da
lindexi <lindexi_gd@163.com>
2021-07-31 09:50:43 +0800

    修改命名空间

--- LightTextEditorPlus/TextEditorPlus/Api/TextEditor.cs
+++ LightTextEditorPlus/TextEditorPlus/Api/TextEditor.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using System.Windows.Media;
 
-namespace LightTextEditorPlus.TextEditorPlus.Api
+namespace LightTextEditorPlus.TextEditorPlus
 {
     /// <summary>
     /// 文本编辑控件
@@ -12,5 +13,19 @@ namespace LightTextEditorPlus.TextEditorPlus.Api
     /// <remarks> 这个项目的核心和入口就是这个类</remarks>
     public partial class TextEditor : FrameworkElement
     {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return base.MeasureOverride(availableSize);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            return base.ArrangeOverride(finalSize);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
     }
 }
