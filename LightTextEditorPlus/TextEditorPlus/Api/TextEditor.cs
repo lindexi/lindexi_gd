e3951684ddeef8f0313ab05a1bbf6f549ea427b1
lindexi <lindexi_gd@163.com>
2021-07-31 09:40:32 +0800

    搭建文本基础框架

---
+++ LightTextEditorPlus/TextEditorPlus/Api/TextEditor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace LightTextEditorPlus.TextEditorPlus.Api
+{
+    /// <summary>
+    /// 文本编辑控件
+    /// 支持复杂文本编辑，支持添加扩展字符包括图片等到文本
+    /// </summary>
+    /// <remarks> 这个项目的核心和入口就是这个类</remarks>
+    public partial class TextEditor : FrameworkElement
+    {
+    }
+}
