ea756f01b279201a4bb351df1a231faf347c53c7
lindexi <lindexi_gd@163.com>
2021-07-31 11:08:30 +0800

    尝试构建

---
+++ LightTextEditorPlus/TextEditorPlus/Render/TextGeometryRender_/TextGeometry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LightTextEditorPlus.TextEditorPlus.Document;
+
+namespace LightTextEditorPlus.TextEditorPlus.Render
+{
+    class TextGeometry
+    {
+        static TextGeometryRenderResult BuildGeometry(RunProperty runProperty,
+            string text)
+        {
+            return default;
+        }
+    }
+}
