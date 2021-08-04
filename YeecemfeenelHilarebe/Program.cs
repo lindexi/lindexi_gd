dde889e83952fcdf7013dac2f04fd4b7615fd85f
lindexi <lindexi_gd@163.com>
2021-08-04 09:20:33 +0800

    尝试加上单元测试框架

---
+++ YeecemfeenelHilarebe/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+using MSTest.Extensions.Core;
+using MSTest.Extensions.Utils;
+
+namespace YeecemfeenelHilarebe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var methodInfo = typeof(FooTest).GetMethod("Foo");
+            var fooTestMethod = new FooTestMethod(methodInfo)
+            {
+            };
+
+            var testMethodProxy = new TestMethodProxy(fooTestMethod);
+            var testResult = testMethodProxy.Invoke(new object[0]);
+        }
+    }
+
+    [TestClass]
+    public class FooTest
+    {
+        [ContractTestCase]
+        public void Foo()
+        {
+            "123".Test(() =>
+            {
+
+            });
+        }
+    }
+
+    class FooTestMethod : ITestMethod
+    {
+        public FooTestMethod(MethodInfo methodInfo)
+        {
+            MethodInfo = methodInfo;
+        }
+
+        public TestResult Invoke(object[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Attribute[] GetAllAttributes(bool inherit)
+        {
+            return MethodInfo.GetCustomAttributes().ToArray();
+        }
+
+        public TAttributeType[] GetAttributes<TAttributeType>(bool inherit) where TAttributeType : Attribute
+        {
+            return MethodInfo.GetCustomAttributes().OfType<TAttributeType>().ToArray();
+        }
+
+        public string TestMethodName => MethodInfo.Name;
+        public string TestClassName => MethodInfo.DeclaringType?.Name ?? string.Empty;
+        public Type ReturnType => MethodInfo.ReturnType;
+        public object[] Arguments { set; get; }
+        public ParameterInfo[] ParameterTypes => MethodInfo.GetParameters();
+        public MethodInfo MethodInfo { set; get; }
+
+        public object Parent { get; } = new object();
+    }
+}
