93c34069823139fd7fb847a15a306a32313c7d7a
lindexi <lindexi_gd@163.com>
2021-08-09 21:04:16 +0800

    创建空白应用

---
+++ LarwearceceyokuDokealuwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xwt;
+
+namespace LarwearceceyokuDokealuwo
+{
+    
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.Initialize(ToolkitType.Gtk);
+
+            var mainWindow = new Window()
+            {
+                Title = "Xwt Demo Application",
+                Width = 500,
+                Height = 400
+            };
+            mainWindow.Show();
+            Application.Run();
+        }
+    }
+}
