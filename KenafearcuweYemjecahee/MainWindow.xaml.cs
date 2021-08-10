fd138376d3f8fb35600933917be973ec9b707a58
lindexi <lindexi_gd@163.com>
2021-08-10 20:35:03 +0800

    加上全屏

---
+++ KenafearcuweYemjecahee/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KenafearcuweYemjecahee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = (ToggleButton)sender;
+
+            if (toggleButton.IsChecked is true)
+            {
+                FullScreenHelper.StartFullScreen(this);
+            }
+            else
+            {
+                FullScreenHelper.EndFullScreen(this);
+            }
+        }
+    }
+}
