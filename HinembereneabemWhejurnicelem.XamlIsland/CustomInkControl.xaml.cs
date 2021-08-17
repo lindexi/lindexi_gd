a16b2a90bee0eda34623e2ed8155a5960ae501bd
lindexi <lindexi_gd@163.com>
2021-08-17 20:28:33 +0800

    给笔迹控件加上背景

---
+++ HinembereneabemWhejurnicelem.XamlIsland/CustomInkControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace HinembereneabemWhejurnicelem.XamlIsland
+{
+    public sealed partial class CustomInkControl : UserControl
+    {
+        public CustomInkControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InkCanvas_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            InkCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse| CoreInputDeviceTypes.Touch;
+        }
+    }
+}
