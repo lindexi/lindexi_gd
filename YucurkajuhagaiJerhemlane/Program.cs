5bec1e5c5392f3b2dd99956d9757397f7ed71785
lindexi <lindexi_gd@163.com>
2021-09-08 20:13:19 +0800

    判断枚举的值是否相等

---
+++ YucurkajuhagaiJerhemlane/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace YucurkajuhagaiJerhemlane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = Foo.F2;
+            NukerohoheQawhallnerwalni(args, foo);
+        }
+
+        private static void NukerohoheQawhallnerwalni(string[] args, Foo foo)
+        {
+            while (args.Length == 0)
+            {
+                if (foo == Foo.F1)
+                {
+                }
+            }
+        }
+    }
+
+    enum Foo
+    {
+        F1,
+        F2
+    }
+}
