241a02f9f89cde5848d43f71c755c3bce84ec2b6
lindexi <lindexi_gd@163.com>
2021-09-10 14:57:43 +0800

    读取文件内容

---
+++ WibifelqeQolawwaljecili/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace WibifelqeQolawwaljecili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new FileInfo("slide1.xml");
+            using var fileStream = file.OpenRead();
+            var xmlReader = XmlReader.Create(fileStream);
+            var readElementContentAsString = xmlReader.ReadElementContentAsString();
+        }
+    }
+}
