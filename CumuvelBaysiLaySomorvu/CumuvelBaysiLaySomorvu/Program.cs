70a1e059a61ca293526640e5baa6b621bac5e745
lindexi <lindexi_gd@163.com>
2021-09-10 09:34:28 +0800

    测试QPush输出文本

---
+++ CumuvelBaysiLaySomorvu/CumuvelBaysiLaySomorvu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lindexi.src;
+
+namespace CumuvelBaysiLaySomorvu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var qpush = new Qpush("lindexi","23");
+            qpush.PushMessageAsync("测试");
+        }
+    }
+}
