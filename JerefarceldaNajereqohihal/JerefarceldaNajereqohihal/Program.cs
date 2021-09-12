e4188e64e215d9e1d4ec96d285e98a30b02b0bf5
lindexi <lindexi_gd@163.com>
2021-09-12 19:05:01 +0800

    读取委托

---
+++ JerefarceldaNajereqohihal/JerefarceldaNajereqohihal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JerefarceldaNajereqohihal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<object> foo = Foo;
+            object o = foo;
+            var f1 = o as F1;
+
+        }
+
+        private static void Foo(object obj)
+        {
+            
+        }
+    }
+
+    delegate void F1(object obj);
+    delegate void F2(object obj);
+}
