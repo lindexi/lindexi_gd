a416285d2d4df00837092d546d5a4bddbdd0e1cc
lindexi <lindexi_gd@163.com>
2021-09-25 16:09:54 +0800

    提升拷贝性能

---
+++ WemjallnacaleayayLerelaywhekebelnaw/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WemjallnacaleayayLerelaywhekebelnaw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = 3;
+
+            string subString = _pathString.Substring(start, _curIndex - start);
+            var num1 = System.Convert.ToDouble(subString, _formatProvider);
+
+            try
+            {
+                start = 2;
+
+                var span = _pathString.AsSpan(start, _curIndex - start);
+                var num2 = double.Parse(span, provider: _formatProvider);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static string _pathString = "abc123ab";
+        private static int _curIndex = 6;
+        private static IFormatProvider _formatProvider = System.Globalization.CultureInfo.InvariantCulture;
+    }
+}
