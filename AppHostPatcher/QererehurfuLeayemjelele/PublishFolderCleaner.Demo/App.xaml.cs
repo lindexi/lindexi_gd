a61d0dd8770bd751252e88c82a1692451c26b672
lindexi <lindexi_gd@163.com>
2021-10-08 19:41:46 +0800

    修改命名

--- AppHostPatcher/QererehurfuLeayemjelele/PublishFolderCleaner.Demo/App.xaml.cs
+++ AppHostPatcher/QererehurfuLeayemjelele/PublishFolderCleaner.Demo/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace HalrerbelfehiQaneebihegal
+namespace PublishFolderCleaner.Demo
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- AppHostPatcher/QererehurfuLeayemjelele/PublishFolderCleaner.Demo/MainWindow.xaml.cs
+++ AppHostPatcher/QererehurfuLeayemjelele/PublishFolderCleaner.Demo/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HalrerbelfehiQaneebihegal
+namespace PublishFolderCleaner.Demo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
