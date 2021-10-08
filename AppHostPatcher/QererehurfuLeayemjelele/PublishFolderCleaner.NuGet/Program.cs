9c7049f0fdd9acd057fdddb98f7f67508a540aa4
lindexi <lindexi_gd@163.com>
2021-10-08 19:34:09 +0800

    加上打包

---
+++ AppHostPatcher/QererehurfuLeayemjelele/PublishFolderCleaner.NuGet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PublishFolderCleaner.NuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
