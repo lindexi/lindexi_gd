b732e9acb790db13418010351818157ba30c8968
lindexi <lindexi_gd@163.com>
2021-10-08 19:28:33 +0800

    加上路径应用

--- AppHostPatcher/QererehurfuLeayemjelele/HallfidefelFallhairheefeefu/Program.cs
+++ AppHostPatcher/QererehurfuLeayemjelele/HallfidefelFallhairheefeefu/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using dotnetCampus.Cli;
 
-namespace HallfidefelFallhairheefeefu
+namespace PublishFolderCleaner
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"PublishFolderCleaner {Environment.CommandLine}");
         }
+    }
+
+    public class Options
+    {
+        [Option('p', "PublishFolder")]
+        public string PublishFolder { set; get; } = null!;
+
+        [Option('a', "ApplicationName")]
+        public string ApplicationName { set; get; } = null!;
     }
 }
