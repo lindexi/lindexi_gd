01f8608b636b102e479a0726634518e063edb9ed
lindexi <lindexi_gd@163.com>
2021-10-11 11:10:46 +0800

    更改测试逻辑

--- Pptx/MainWindow.xaml.cs
+++ Pptx/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 using System;
+using System.Buffers;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
@@ -52,20 +53,32 @@ namespace Pptx
             Debug.Assert(oleObject.GetFirstChild<OleObjectEmbed>() != null);
             var id = oleObject.Id!;
             var part = slide.SlidePart!.GetPartById(id!);
-            Debug.Assert(part.ContentType== "application/vnd.openxmlformats-officedocument.oleObject");
+            Debug.Assert(part.ContentType == "application/vnd.openxmlformats-officedocument.oleObject");
 
             var allocatedBytesForCurrentThread = GC.GetAllocatedBytesForCurrentThread();
             var s = part.GetStream();
-            var cf = new CompoundFile(s);
 
-            var lastAllocatedBytesForCurrentThread = GC.GetAllocatedBytesForCurrentThread();
-            Debug.WriteLine(lastAllocatedBytesForCurrentThread - allocatedBytesForCurrentThread);
-
+            var byteArrayPool = new ByteArrayPool();
+            var fakeStream = new ForwardSeekStream(s,byteArrayPool);
+            var cf = new CompoundFile(fakeStream);
+            var packageStream = cf.RootStorage.GetStream("Package"); 
             var tempFolder = @"F:\temp";
             if (!Directory.Exists(tempFolder))
             {
                 tempFolder = System.IO.Path.GetTempPath();
             }
+            var xlsxFile = System.IO.Path.Combine(tempFolder, System.IO.Path.GetRandomFileName() + ".xlsx");
+            using (var fileStream = File.OpenWrite(xlsxFile))
+            {
+                fileStream.Write(packageStream.GetData().AsSpan());
+            }
+            using var spreadsheetDocument = SpreadsheetDocument.Open(xlsxFile, false);
+            var sheets = spreadsheetDocument.WorkbookPart!.Workbook.Sheets;
+
+            var lastAllocatedBytesForCurrentThread = GC.GetAllocatedBytesForCurrentThread();
+            Debug.WriteLine(lastAllocatedBytesForCurrentThread - allocatedBytesForCurrentThread);
+
+      
 
             //// OpenMcdf.CFException:“Cannot load a non-seekable Stream”
             //var compoundFile = new CompoundFile(part.GetStream(FileMode.Open));
@@ -77,18 +90,104 @@ namespace Pptx
                 stream.CopyTo(fileStream);
             }
 
-            var compoundFile = new CompoundFile(oleFile);
-            var packageStream = compoundFile.RootStorage.GetStream("Package");
-            var xlsxFile = System.IO.Path.Combine(tempFolder, System.IO.Path.GetRandomFileName()+".xlsx");
-            using (var fileStream = File.OpenWrite(xlsxFile))
-            {
-                fileStream.Write(packageStream.GetData().AsSpan());
-            }
+            //var compoundFile = new CompoundFile(oleFile);
+            //var packageStream = compoundFile.RootStorage.GetStream("Package");
+            //var xlsxFile = System.IO.Path.Combine(tempFolder, System.IO.Path.GetRandomFileName() + ".xlsx");
+            //using (var fileStream = File.OpenWrite(xlsxFile))
+            //{
+            //    fileStream.Write(packageStream.GetData().AsSpan());
+            //}
 
-            using var spreadsheetDocument = SpreadsheetDocument.Open(xlsxFile,false);
-            var sheets = spreadsheetDocument.WorkbookPart!.Workbook.Sheets;
+            //using var spreadsheetDocument = SpreadsheetDocument.Open(xlsxFile, false);
+            //var sheets = spreadsheetDocument.WorkbookPart!.Workbook.Sheets;
 
 
         }
+    }
+
+    public interface IByteArrayPool
+    {
+        byte[] Rent(int minimumLength);
+        void Return(byte[] byteList);
+    }
+
+    public class ByteArrayPool : IByteArrayPool
+    {
+        public byte[] Rent(int minimumLength)
+        {
+            return ArrayPool<byte>.Shared.Rent(minimumLength);
+        }
+
+        public void Return(byte[] byteList)
+        {
+            ArrayPool<byte>.Shared.Return(byteList);
+        }
+    }
+
+    public class ForwardSeekStream : Stream
+    {
+        public ForwardSeekStream(Stream sourceStream, IByteArrayPool byteArrayPool)
+        {
+            _sourceStream = sourceStream;
+            _byteArrayPool = byteArrayPool;
+        }
+
+        public override void Flush()
+        {
+            _sourceStream.Flush();
+        }
+
+        public long CurrentPosition { private set; get; }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var n = _sourceStream.Read(buffer, offset, count);
+            CurrentPosition += n;
+            return n;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            if (offset == CurrentPosition) return CurrentPosition;
+            if (offset > CurrentPosition)
+            {
+                int length = (int) (offset - CurrentPosition);
+                var byteList = _byteArrayPool.Rent(length);
+                Read(byteList, 0, length);
+                _byteArrayPool.Return(byteList);
+
+                Debug.Assert(offset == CurrentPosition);
+                return CurrentPosition;
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            _sourceStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _sourceStream.Write(buffer, offset, count);
+        }
+
+        public override bool CanRead => _sourceStream.CanRead;
+
+        public override bool CanSeek => _sourceStream.CanSeek;
+
+        public override bool CanWrite => _sourceStream.CanWrite;
+
+        public override long Length => _sourceStream.Length;
+
+        public override long Position
+        {
+            get => _sourceStream.Position;
+            set => _sourceStream.Position = value;
+        }
+
+        private readonly Stream _sourceStream;
+        private readonly IByteArrayPool _byteArrayPool;
     }
 }
