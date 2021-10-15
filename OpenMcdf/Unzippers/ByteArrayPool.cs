364e0c53ea8ec9f313ef6aa03c03a053b4bc38f6
lindexi <lindexi_gd@163.com>
2021-10-15 15:17:30 +0800

    优化内存池

--- OpenMcdf/Unzippers/ByteArrayPool.cs
+++ OpenMcdf/Unzippers/ByteArrayPool.cs
@@ -4,12 +4,13 @@
     {
         public byte[] Rent(int minimumLength)
         {
-            if (_buffer != null)
+            for (var i = 0; i < _bufferList.Length; i++)
             {
-                if (_buffer.Length >= minimumLength)
+                var buffer = _bufferList[i];
+
+                if (buffer != null && buffer.Length >= minimumLength)
                 {
-                    var buffer = _buffer;
-                    _buffer = null;
+                    _bufferList[i] = null;
                     return buffer;
                 }
             }
@@ -24,13 +25,18 @@
                 return;
             }
 
-            if (_buffer is null || byteList.Length > _buffer.Length)
+            for (var i = 0; i < _bufferList.Length; i++)
             {
-                _buffer = byteList;
+                var buffer = _bufferList[i];
+                if (buffer is null || byteList.Length > buffer.Length)
+                {
+                    buffer = byteList;
+                    _bufferList[i] = buffer;
+                }
             }
         }
 
-        private byte[] _buffer;
+        private readonly byte[][] _bufferList = new byte[4][];
         private const int MaxBufferLength = 8 * 1024;
     }
 }
