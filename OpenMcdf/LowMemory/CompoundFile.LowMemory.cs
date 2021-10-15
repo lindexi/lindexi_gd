d53eb008662631332f4a67fc47318de051c5b3f1
lindexi <lindexi_gd@163.com>
2021-10-15 16:10:30 +0800

    拆分逻辑

--- OpenMcdf/LowMemory/CompoundFile.LowMemory.cs
+++ OpenMcdf/LowMemory/CompoundFile.LowMemory.cs
@@ -313,44 +313,4 @@ namespace OpenMcdf
         private readonly Stream _sourceStream;
         private readonly IByteArrayPool _byteArrayPool;
     }
-
-    class SectorList
-    {
-        public SectorList(List<int> idIndexList, Stream sourceStream, int sectorSize, SectorType type)
-        {
-            IdIndexList = idIndexList;
-            SourceStream = sourceStream;
-            SectorSize = sectorSize;
-            Type = type;
-        }
-
-        public SectorList(Stream sourceStream, int sectorSize, SectorType type)
-        {
-            SourceStream = sourceStream;
-            SectorSize = sectorSize;
-            Type = type;
-
-            IdIndexList = new List<int>();
-        }
-
-        public List<int> IdIndexList { set; get; }
-
-        public int Count => IdIndexList.Count;
-
-        public Stream SourceStream { get; }
-
-        public int SectorSize { get; }
-
-        public SectorType Type { get; }
-
-        public int Read(int sectorIndex, byte[] buffer, int position, int offset, int count)
-        {
-            var idIndex = IdIndexList[sectorIndex];
-            var sectorPosition = SectorSize + idIndex * SectorSize;
-
-            var streamPosition = sectorPosition + position;
-            SourceStream.Seek(streamPosition, SeekOrigin.Begin);
-            return SourceStream.Read(buffer, offset, count);
-        }
-    }
 }
