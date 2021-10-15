409080b03c501d8df642d8c1da3bbf1a1c9f819a
lindexi <lindexi_gd@163.com>
2021-10-15 16:05:36 +0800

    拆分逻辑

--- OpenMcdf/Unzippers/CompoundFileUnzipper.cs
+++ OpenMcdf/Unzippers/CompoundFileUnzipper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 
@@ -56,34 +55,6 @@ namespace OpenMcdf
                     compoundFile.CopyTo(stream, fileStream, byteArrayPool);
                 }
             }, false);
-        }
-    }
-
-    static class StreamHelper
-    {
-        public static void CopyTo(this Stream sourceStream, Stream destinationStream, IByteArrayPool byteArrayPool,
-            long position, long size)
-        {
-            sourceStream.Seek(position, SeekOrigin.Begin);
-            const int defaultBufferLength = 4096;
-            var bufferLength = (int) Math.Min(defaultBufferLength, size);
-            var buffer = byteArrayPool.Rent(bufferLength);
-
-            var readCount = 0;
-            while (readCount < size)
-            {
-                var count = (int) Math.Min(size - readCount, bufferLength);
-                var n = sourceStream.Read(buffer, 0, count);
-                if (n == 0)
-                {
-                    break;
-                }
-                readCount += n;
-
-                destinationStream.Write(buffer, 0, n);
-            }
-
-            byteArrayPool.Return(buffer);
         }
     }
 }
---
+++ OpenMcdf/Unzippers/StreamHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace OpenMcdf
+{
+    static class StreamHelper
+    {
+        public static void CopyTo(this Stream sourceStream, Stream destinationStream, IByteArrayPool byteArrayPool,
+            long position, long size)
+        {
+            sourceStream.Seek(position, SeekOrigin.Begin);
+            const int defaultBufferLength = 4096;
+            var bufferLength = (int) Math.Min(defaultBufferLength, size);
+            var buffer = byteArrayPool.Rent(bufferLength);
+
+            var readCount = 0;
+            while (readCount < size)
+            {
+                var count = (int) Math.Min(size - readCount, bufferLength);
+                var n = sourceStream.Read(buffer, 0, count);
+                if (n == 0)
+                {
+                    break;
+                }
+                readCount += n;
+
+                destinationStream.Write(buffer, 0, n);
+            }
+
+            byteArrayPool.Return(buffer);
+        }
+    }
+}
