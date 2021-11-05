0d632e7c1b7e9245744c9816d2c91b3ba94e1838
lindexi <lindexi_gd@163.com>
2021-11-05 20:17:20 +0800

    创建符号链接

---
+++ BemfejulereLarcayjika/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace BemfejulereLarcayjika
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = Directory.CreateDirectory("aa");
+
+            var result = Directory.CreateSymbolicLink("bbb", a.FullName) as DirectoryInfo;
+
+            // 输出 bbb 文件夹
+            Console.WriteLine(result.FullName);
+        }
+    }
+}
+
