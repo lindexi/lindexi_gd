407ef940c08f3666d13565df18c9f9448cf819e0
lindexi <lindexi_gd@163.com>
2021-11-07 17:14:03 +0800

    尝试自己绘制

--- WpfRepacker/dotnetCampus.WPF/dotnetCampus.WPF/Program.cs
+++ WpfRepacker/dotnetCampus.WPF/dotnetCampus.WPF/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Composition;
 using System.Windows.Navigation;
 using MS.Internal;
 
@@ -39,10 +42,13 @@ namespace dotnetCampus.WPF
         public Foo()
         {
             DrawingVisual drawingVisual = Visual = new DrawingVisual();
-            using (var drawingContext = drawingVisual.RenderOpen())
-            {
-                drawingContext.DrawRectangle(Brushes.CadetBlue, null, new Rect(10, 10, 100, 100));
-            }
+            //using (var drawingContext = drawingVisual.RenderOpen())
+            //{
+            //    drawingContext.DrawRectangle(Brushes.CadetBlue, null, new Rect(10, 10, 100, 100));
+            //}
+            using var directDrawingContext = new DirectDrawingContext(drawingVisual);
+            directDrawingContext.DrawLine(new Pen(Brushes.CadetBlue, 2), new Point(10, 10), null, new Point(100, 100),
+                null);
         }
 
         protected override int VisualChildrenCount => 1;
@@ -52,5 +58,78 @@ namespace dotnetCampus.WPF
         }
 
         private DrawingVisual Visual { get; }
+    }
+
+    class DirectDrawingContext : IDisposable
+    {
+        public DirectDrawingContext(DrawingVisual drawingVisual)
+        {
+            _ownerVisual = drawingVisual;
+            _renderData = new RenderData();
+        }
+
+        public void DrawLine(
+            Pen pen,
+            Point point0,
+            AnimationClock point0Animations,
+            Point point1,
+            AnimationClock point1Animations)
+        {
+            unsafe
+            {
+                // Always assume visual and drawing brushes need realization updates
+
+                UInt32 hPoint0Animations = CompositionResourceManager.InvalidResourceHandle;
+                UInt32 hPoint1Animations = CompositionResourceManager.InvalidResourceHandle;
+                hPoint0Animations = UseAnimations(point0, point0Animations);
+                hPoint1Animations = UseAnimations(point1, point1Animations);
+
+                MILCMD_DRAW_LINE_ANIMATE record =
+                    new MILCMD_DRAW_LINE_ANIMATE(
+                        _renderData.AddDependentResource(pen),
+                        point0,
+                        hPoint0Animations,
+                        point1,
+                        hPoint1Animations
+                    );
+
+                // Assert that the calculated packet size is the same as the size returned by sizeof().
+                Debug.Assert(sizeof(MILCMD_DRAW_LINE_ANIMATE) == 48);
+
+                _renderData.WriteDataRecord(MILCMD.MilDrawLineAnimate,
+                    (byte*)&record,
+                    48 /* sizeof(MILCMD_DRAW_LINE_ANIMATE) */);
+            }
+        }
+
+        private UInt32 UseAnimations(
+            Point baseValue,
+            AnimationClock animations)
+        {
+            if (animations == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return _renderData.AddDependentResource(
+                    new PointAnimationClockResource(
+                        baseValue,
+                        animations));
+            }
+        }
+
+        public void Close()
+        {
+            _ownerVisual.RenderClose(_renderData);
+        }
+
+        private Visual _ownerVisual;
+        private readonly RenderData _renderData;
+
+        public void Dispose()
+        {
+            Close();
+        }
     }
 }
