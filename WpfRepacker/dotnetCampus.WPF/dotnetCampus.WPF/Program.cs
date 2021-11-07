e362a7a0540bafc1f0bf52084ee808b8d865c29b
lindexi <lindexi_gd@163.com>
2021-11-07 12:03:50 +0800

    加上使用的方法

--- WpfRepacker/dotnetCampus.WPF/dotnetCampus.WPF/Program.cs
+++ WpfRepacker/dotnetCampus.WPF/dotnetCampus.WPF/Program.cs
@@ -13,11 +13,17 @@ namespace dotnetCampus.WPF
         [STAThread]
         static void Main(string[] args)
         {
+            // 开始之前
+            // 从 [CSDN 下载](https://download.csdn.net/download/lindexi_gd/13769715) 基础框架逻辑
+            // 获取发一封邮件给我，让我发链接给你
+
+            // 解压缩内容，放在 Lib 文件夹下，参考 dotnetCampus.WPF.csproj 的代码，确保文件都能找到
+
             var application = new Application();
             var window = new Window()
             {
-                Title = "林德熙是逗比"
-
+                Title = "林德熙是逗比",
+                Content = new Foo(),
             };
             window.Loaded += (sender, eventArgs) =>
             {
@@ -26,5 +32,25 @@ namespace dotnetCampus.WPF
             };
             application.Run(window);
         }
+    }
+
+    class Foo : UIElement
+    {
+        public Foo()
+        {
+            DrawingVisual drawingVisual = Visual = new DrawingVisual();
+            using (var drawingContext = drawingVisual.RenderOpen())
+            {
+                drawingContext.DrawRectangle(Brushes.CadetBlue, null, new Rect(10, 10, 100, 100));
+            }
+        }
+
+        protected override int VisualChildrenCount => 1;
+        protected override Visual GetVisualChild(int index)
+        {
+            return Visual;
+        }
+
+        private DrawingVisual Visual { get; }
     }
 }
