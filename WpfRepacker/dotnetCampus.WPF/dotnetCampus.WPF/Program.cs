8b6a9d1d0e06f7a105547fab49b38520f8af24bd
lindexi <lindexi_gd@163.com>
2021-11-07 11:58:15 +0800

    加上自定义的 WPF 项目

---
+++ WpfRepacker/dotnetCampus.WPF/dotnetCampus.WPF/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using MS.Internal;
+
+
+namespace dotnetCampus.WPF
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var application = new Application();
+            var window = new Window()
+            {
+                Title = "林德熙是逗比"
+
+            };
+            window.Loaded += (sender, eventArgs) =>
+            {
+                // 这里的 GetAppWindow 是 internal 的方法，但是在这个程序集可以访问
+                var navigationWindow = application.GetAppWindow();
+            };
+            application.Run(window);
+        }
+    }
+}
