0976c7091b07ddf2dc52370cf6561fe2d9ecd4e8
lindexi <lindexi_gd@163.com>
2021-11-15 16:25:47 +0800

    调试点击暂停失败

---
+++ VisualStudioDebuggerBug/App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Lib;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.Foo);
+            Console.Read();
+        }
+    }
+}
---
+++ VisualStudioDebuggerBug/Lib/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Class1
+    {
+        public static int Foo { get; } = 1;
+    }
+}
