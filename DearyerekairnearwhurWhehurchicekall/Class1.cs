796638432d996d604330607c35ea0ebc63ba3544
lindexi <lindexi_gd@163.com>
2021-11-15 19:17:29 +0800

    加上调试

---
+++ DearyerekairnearwhurWhehurchicekall/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DearyerekairnearwhurWhehurchicekall
+{
+    public class Class1
+    {
+        public static string Foo = "6";
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ QarwiheejulayRejegallefurkear/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DearyerekairnearwhurWhehurchicekall;
+
+namespace QarwiheejulayRejegallefurkear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.Foo);
+            Console.Read();
+        }
+    }
+}
