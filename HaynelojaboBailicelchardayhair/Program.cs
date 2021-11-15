d10ce1c12cb2dd136faaa4125d66d3d7f928600b
lindexi <lindexi_gd@163.com>
2021-11-15 21:19:31 +0800

    自动格式化

---
+++ HaynelojaboBailicelchardayhair/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using dotnetCampus.Cli;
+using dotnetCampus.DotNETBuild.Utils;
+using dotnetCampus.GitCommand;
+
+namespace HaynelojaboBailicelchardayhair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directoryInfo = new DirectoryInfo(@"f:\temp\WalhallchogehaiKirerlibarlerho\");
+            var git = new Git(directoryInfo);
+            git.ExecuteCommand("add .");
+            var (success, output) = git.ExecuteCommand("commit -m \"Format Code\"");
+            if (success)
+            {
+                git.CheckoutNewBranch($"t/bot/FormatCode_{DateTime.Now:yyMMddhhmmssfff}");
+                git.ExecuteCommand("push");
+            }
+        }
+    }
+
+    class Options
+    {
+        [Option("Gitlab")]
+        public string GitlabUrl { set; get; }
+
+        [Option("Token")]
+        public string GitlabToken { set; get; }
+    }
+
+    static class GitHelper
+    {
+        public static (bool success, string output) ExecuteCommand(this Git git, string args)
+        {
+            return ProcessCommand.ExecuteCommand("git", args, git.Repo.FullName);
+        }
+    }
+}
