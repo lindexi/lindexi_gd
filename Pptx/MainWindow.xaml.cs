cd98a7a6b29e9297864aad9d7326a635b6b68e5b
lindexi <lindexi_gd@163.com>
2021-11-30 16:54:56 +0800

    对齐方法

--- Pptx/MainWindow.xaml.cs
+++ Pptx/MainWindow.xaml.cs
@@ -8,12 +8,18 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
+
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Presentation;
+
 using dotnetCampus.OpenXmlUnitConverter;
+
+using GeneratedCode;
+
 using OpenMcdf;
+
 using ColorMap = DocumentFormat.OpenXml.Presentation.ColorMap;
 using GraphicFrame = DocumentFormat.OpenXml.Presentation.GraphicFrame;
 using Path = DocumentFormat.OpenXml.Drawing.Path;
@@ -38,50 +44,18 @@ namespace Pptx
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            var file = new FileInfo("Test.pptx");
-
-            using var presentationDocument = PresentationDocument.Open(file.FullName, false);
-            var slide = presentationDocument.PresentationPart!.SlideParts.First().Slide;
-
-            var graphicFrame = slide.CommonSlideData!.ShapeTree!.GetFirstChild<GraphicFrame>()!;
-            var graphic = graphicFrame.Graphic!;
-            var graphicData = graphic.GraphicData!;
-            var alternateContent = graphicData.GetFirstChild<AlternateContent>()!;
-            var choice = alternateContent.GetFirstChild<AlternateContentChoice>()!;
-            var oleObject = choice.GetFirstChild<OleObject>()!;
-            Debug.Assert(oleObject.GetFirstChild<OleObjectEmbed>() != null);
-            var id = oleObject.Id!;
-            var part = slide.SlidePart!.GetPartById(id!);
-            Debug.Assert(part.ContentType== "application/vnd.openxmlformats-officedocument.oleObject");
-
-            var tempFolder = @"F:\temp";
-            if (!Directory.Exists(tempFolder))
+            foreach (var textAlignmentTypeValue in Enum.GetValues<TextAlignmentTypeValues>())
             {
-                tempFolder = System.IO.Path.GetTempPath();
+                var generatedClass = new GeneratedClass()
+                {
+                    TextAlignment = textAlignmentTypeValue
+                };
+
+                var file = $"{textAlignmentTypeValue}.pptx";
+                generatedClass.CreatePackage(file);
+
+                Process.Start("explorer.exe", file);
             }
-
-            //// OpenMcdf.CFException:“Cannot load a non-seekable Stream”
-            //var compoundFile = new CompoundFile(part.GetStream(FileMode.Open));
-
-            var oleFile = System.IO.Path.Combine(tempFolder, System.IO.Path.GetRandomFileName());
-            using (var fileStream = File.OpenWrite(oleFile))
-            {
-                using var stream = part.GetStream(FileMode.Open);
-                stream.CopyTo(fileStream);
-            }
-
-            var compoundFile = new CompoundFile(oleFile);
-            var packageStream = compoundFile.RootStorage.GetStream("Package");
-            var xlsxFile = System.IO.Path.Combine(tempFolder, System.IO.Path.GetRandomFileName()+".xlsx");
-            using (var fileStream = File.OpenWrite(xlsxFile))
-            {
-                fileStream.Write(packageStream.GetData().AsSpan());
-            }
-
-            using var spreadsheetDocument = SpreadsheetDocument.Open(xlsxFile,false);
-            var sheets = spreadsheetDocument.WorkbookPart!.Workbook.Sheets;
-
-
         }
     }
 }
