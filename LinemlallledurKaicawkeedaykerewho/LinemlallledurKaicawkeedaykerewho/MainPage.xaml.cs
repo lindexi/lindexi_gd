bb7b63b26d352f6ead931e00b19ed6d32e57e735
lindexi <lindexi_gd@163.com>
2022-02-05 19:59:33 +0800

    文字转语音的音色

--- LinemlallledurKaicawkeedaykerewho/LinemlallledurKaicawkeedaykerewho/MainPage.xaml.cs
+++ LinemlallledurKaicawkeedaykerewho/LinemlallledurKaicawkeedaykerewho/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.SpeechSynthesis;
@@ -33,57 +34,37 @@ namespace LinemlallledurKaicawkeedaykerewho
         {
             InitializeComponent();
         }
-
-        private static async Task ConvertWordListToFile(IList<string> wordList)
-        {
-            var wordFolder =
-                await ApplicationData.Current.TemporaryFolder.CreateFolderAsync(
-                    DateTime.Now.ToString("yyyy MM dd hhmmss"));
-            var wordFileInfoList = new List<WordFileInfo>();
-
-            using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
-            {
-                foreach (var word in wordList)
-                {
-                    try
-                    {
-                        SpeechSynthesisStream stream = await synthesizer.SynthesizeTextToStreamAsync(word);
-                        using (stream)
-                        {
-                            var wordFile =
-                                await wordFolder.CreateFileAsync($"{word}.wav",
-                                    CreationCollisionOption.GenerateUniqueName);
-
-                            using (var wordFileStream = await wordFile.OpenStreamForWriteAsync())
-                            {
-                                await stream.AsStreamForRead().CopyToAsync(wordFileStream);
-                            }
-
-                            wordFileInfoList.Add(new WordFileInfo(word, wordFile.Name));
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.WriteLine(e);
-                    }
-                }
-            }
-
-            var wordListFile = await wordFolder.CreateFileAsync("word list.txt");
-
-            await FileIO.WriteLinesAsync(wordListFile, wordFileInfoList.Select(temp => $"{temp.Word}|{temp.FileName}"));
-
-            await Launcher.LaunchFolderAsync(ApplicationData.Current.TemporaryFolder);
-        }
-
         private async void Button_OnClick(object sender, RoutedEventArgs e)
         {
-            // 词典通过 ChobigarqerKuhawnakayewe 创建
-            var fileOpenPicker = new FileOpenPicker();
-            fileOpenPicker.FileTypeFilter.Add(".txt");
-            var wordFile = await fileOpenPicker.PickSingleFileAsync();
-            var wordList = await FileIO.ReadLinesAsync(wordFile);
-            await ConvertWordListToFile(wordList);
+            using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
+            {
+                var voice = SpeechSynthesizer.AllVoices.FirstOrDefault(v => v.Id.Contains("zhCN_KangkangM"));
+                synthesizer.Voice = voice;
+
+                var text = InputTextBox.Text;
+
+                try
+                {
+                    SpeechSynthesisStream stream = await synthesizer.SynthesizeTextToStreamAsync(text);
+                    using (stream)
+                    {
+                        FileSavePicker savePicker = new FileSavePicker();
+                        savePicker.FileTypeChoices.Add("音频", new[] { ".wav" });
+
+                        var result = await savePicker.PickSaveFileAsync();
+                        var wordFile = result;
+
+                        using (var wordFileStream = await wordFile.OpenStreamForWriteAsync())
+                        {
+                            await stream.AsStreamForRead().CopyToAsync(wordFileStream);
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+            }
         }
     }
 }
