0026d09d740a55a9e390754ab19ab579f1bfa9ca
lindexi <lindexi_gd@163.com>
2022-02-09 19:19:13 +0800

    测试拔掉 U 盘

--- HebarlawkuKekebuwagay/HebarlawkuKekebuwagay/Program.cs
+++ HebarlawkuKekebuwagay/HebarlawkuKekebuwagay/Program.cs
@@ -1,25 +1,39 @@
 ﻿
+using System;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
-Console.WriteLine($"Fx {Environment.CurrentDirectory}");
-
-if (args.Length > 0)
+namespace HebarlawkuKekebuwagay
 {
-    return;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Fx {Environment.CurrentDirectory}");
+
+            if (args.Length > 0)
+            {
+                Console.Read();
+                return;
+            }
+
+            var location = Assembly.GetExecutingAssembly().Location;
+            var fileName = Path.GetFileNameWithoutExtension(location);
+            var directory = Path.GetDirectoryName(location);
+
+            //var testFolder = new DirectoryInfo("Test2");
+            //Directory.CreateDirectory(testFolder.FullName);
+            //Environment.CurrentDirectory = testFolder.FullName;
+            Environment.CurrentDirectory = @"I:\";
+
+            var exe = Path.Combine(directory, fileName + ".exe");
+            var processStartInfo = new ProcessStartInfo(exe, "fx");
+            var process = Process.Start(processStartInfo);
+            process.WaitForExit();
+            // System.ComponentModel.Win32Exception: 'An error occurred trying to start process' 目录名称无效
+            // The directory name is invalid
+            // [c# - Win32Exception: The directory name is invalid - Stack Overflow](https://stackoverflow.com/questions/990562/win32exception-the-directory-name-is-invalid )
+        }
+    }
 }
-
-var location = Assembly.GetExecutingAssembly().Location;
-var fileName = Path.GetFileNameWithoutExtension(location);
-var directory = Path.GetDirectoryName(location);
-
-var exe = Path.Combine(directory, fileName + ".exe");
-var processStartInfo = new ProcessStartInfo(exe,"fx")
-{
-    WorkingDirectory = "Z:\\Windows"
-};
-var process = Process.Start(processStartInfo);
-process.WaitForExit();
-// System.ComponentModel.Win32Exception: 'An error occurred trying to start process' 目录名称无效
-// The directory name is invalid
-// [c# - Win32Exception: The directory name is invalid - Stack Overflow](https://stackoverflow.com/questions/990562/win32exception-the-directory-name-is-invalid )
