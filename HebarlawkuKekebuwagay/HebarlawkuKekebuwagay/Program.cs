f7696a3e9f33dfcbfdd8ab92afaa77ab668dfeb9
lindexi <lindexi_gd@163.com>
2022-02-09 19:10:21 +0800

    设置工作路径为不存在的文件

---
+++ HebarlawkuKekebuwagay/HebarlawkuKekebuwagay/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Diagnostics;
+using System.Reflection;
+
+Console.WriteLine($"Fx {Environment.CurrentDirectory}");
+
+if (args.Length > 0)
+{
+    return;
+}
+
+var location = Assembly.GetExecutingAssembly().Location;
+var fileName = Path.GetFileNameWithoutExtension(location);
+var directory = Path.GetDirectoryName(location);
+
+var exe = Path.Combine(directory, fileName + ".exe");
+var processStartInfo = new ProcessStartInfo(exe,"fx")
+{
+    WorkingDirectory = "Z:\\Windows"
+};
+var process = Process.Start(processStartInfo);
+process.WaitForExit();
+// System.ComponentModel.Win32Exception: 'An error occurred trying to start process' 目录名称无效
+// The directory name is invalid
+// [c# - Win32Exception: The directory name is invalid - Stack Overflow](https://stackoverflow.com/questions/990562/win32exception-the-directory-name-is-invalid )
