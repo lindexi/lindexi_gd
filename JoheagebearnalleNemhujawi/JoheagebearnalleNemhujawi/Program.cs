c4daab49c8b3f1c33ab124ac98f68f04427ad73f
lindexi <lindexi_gd@163.com>
2022-02-19 09:42:46 +0800

    定义逻辑格式化代码

---
+++ JoheagebearnalleNemhujawi/JoheagebearnalleNemhujawi/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+A a = new A();
+object o = a;
+B b = a;
+b = (B) o;
+
+class A
+{
+    public static implicit operator B(A a)
+    {
+        return new B();
+    }
+}
+
+class B
+{
+
+}
