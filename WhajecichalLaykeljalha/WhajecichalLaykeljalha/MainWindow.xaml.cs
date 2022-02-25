b3c6a4299c3d517a357cc05f045dd3a16ec261e2
lindexi <lindexi_gd@163.com>
2022-02-25 19:48:57 +0800

    修改触摸功能

---
+++ WhajecichalLaykeljalha/WhajecichalLaykeljalha/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WhajecichalLaykeljalha
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SourceInitialized += OnSourceInitialized;
+        }
+
+        private void OnSourceInitialized(object? sender, EventArgs e)
+        {
+            var windowInteropHelper = new WindowInteropHelper(this);
+            var hwnd = windowInteropHelper.Handle;
+
+            HwndSource source = HwndSource.FromHwnd(hwnd)!;
+            source.AddHook(Hook);
+        }
+
+        private IntPtr Hook(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            const int WM_POINTERDOWN = 0x0246;
+
+            if (msg == WM_POINTERDOWN)
+            {
+                // 开启了 Pointer 消息
+                Debugger.Break();
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
