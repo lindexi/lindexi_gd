62e90836cd137e1f8885fbc54d848ad33446d61b
lindexi <lindexi_gd@163.com>
2022-03-26 08:58:51 +0800

    拼接代码

---
+++ BajehenuLofelhoji/BajehenuLofelhoji/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var nasSetupSubRepositoryRelativeFolderPath=@"Foo 1\Foo 2";
+
+var fileName="Foo 3应用.exe";
+
+var relative = Path.Combine(nasSetupSubRepositoryRelativeFolderPath,fileName);
+
+var url = "http://download.lindexi.com";
+
+var uri = new Uri(url);
+
+Uri.TryCreate(uri, relative, out var result);
+
+var t = result.AbsoluteUri;
+
+Console.Read();
