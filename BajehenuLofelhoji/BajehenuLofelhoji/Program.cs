5d8f86d5e211f8f4e91dbf2ade7be898c15b4aba
lindexi <lindexi_gd@163.com>
2022-03-26 10:23:45 +0800

    判断字符

--- BajehenuLofelhoji/BajehenuLofelhoji/Program.cs
+++ BajehenuLofelhoji/BajehenuLofelhoji/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var nasSetupSubRepositoryRelativeFolderPath=@"Foo 1\Foo 2";
+var nasSetupSubRepositoryRelativeFolderPath = @"Foo 1\Foo 2";
 
-var fileName="Foo 3应用.exe";
+var fileName = "Foo 3应用.exe";
 
-var relative = Path.Combine(nasSetupSubRepositoryRelativeFolderPath,fileName);
+var relative = Path.Combine(nasSetupSubRepositoryRelativeFolderPath, fileName);
 
 var url = "http://download.lindexi.com";
 
@@ -14,4 +14,44 @@ Uri.TryCreate(uri, relative, out var result);
 
 var t = result.AbsoluteUri;
 
+
+var c = (char) ('9' + 2);
+var b1 = IsAsciiLetterOrDigit(c);
+var b2 = IsAsciiLetterOrDigit2(c);
+
+c = (char) ('0' - 2);
+b1 = IsAsciiLetterOrDigit(c);
+b2 = IsAsciiLetterOrDigit2(c);
+var t2 = Convert.ToString('a', 2);
+
 Console.Read();
+
+static bool IsAsciiLetterOrDigit(char character) =>
+    ((((uint) (character - 'A')) & ~0x20) < 26) ||
+    (((uint) (character - '0')) < 10);
+
+static bool IsAsciiLetterOrDigit2(char character)
+{
+    if (IsAsciiLetter(character))
+    {
+        return true;
+    }
+    if (character >= '0')
+    {
+        return (character <= '9');
+    }
+    return false;
+}
+
+static bool IsAsciiLetter(char character)
+{
+    if ((character >= 'a') && (character <= 'z'))
+    {
+        return true;
+    }
+    if (character >= 'A')
+    {
+        return (character <= 'Z');
+    }
+    return false;
+}
