b47c6416fc7731d327a1512dcf9e8e1222a121a1
lindexi <lindexi_gd@163.com>
2022-06-12 18:06:32 +0800

    导入逻辑

---
+++ RijoqicainejoHifolurqall/RijoqicainejoHifolurqall/Fonts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace Microsoft.Maui.Graphics.Xaml
+{
+    static class Fonts
+    {
+        public readonly static FontService CurrentService = new FontService();
+    }
+
+    class FontService
+    {
+        internal FakeFontStyle? GetFontStyleById(string fontName)
+        {
+            return null;
+        }
+    }
+
+    class FakeFontStyle
+    {
+        public FontStyleType StyleType { get; internal set; }
+
+        public Font FontFamily { get; internal set; }
+        public int Weight { get; internal set; }
+    }
+}
