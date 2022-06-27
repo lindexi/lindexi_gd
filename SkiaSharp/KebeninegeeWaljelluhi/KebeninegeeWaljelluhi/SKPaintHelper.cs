c5a088753423c8488380236bb2e2a2a6c9e67f05
lindexi <lindexi_gd@163.com>
2022-06-27 16:55:28 +0800

    拆分代码

---
+++ SkiaSharp/KebeninegeeWaljelluhi/KebeninegeeWaljelluhi/SKPaintHelper.cs
@@ -0,0 +1,89 @@
+﻿using SkiaSharp;
+
+namespace KebeninegeeWaljelluhi;
+
+static class SKPaintHelper
+{
+    public static SKPaint[] GetSKPaintList()
+    {
+        return new[]
+        {
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+            },
+            //new SKPaint
+            //{
+            //    IsAntialias = false,
+            //    Style = SKPaintStyle.Stroke,
+            //    Color = SKColors.Blue,
+            //    StrokeWidth = 5,
+            //    BlendMode = SKBlendMode.ColorBurn,
+            //},
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Butt,
+            },
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Round,
+            },
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Round,
+                StrokeJoin = SKStrokeJoin.Bevel,
+            },
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 6,
+                StrokeCap = SKStrokeCap.Round,
+                StrokeJoin = SKStrokeJoin.Bevel,
+                StrokeMiter = 3,
+            },
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Square,
+            },
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Square,
+                StrokeMiter = 10,
+            },
+            new SKPaint
+            {
+                IsAntialias = false,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Square,
+                StrokeMiter = 20,
+            },
+        };
+    }
+}
