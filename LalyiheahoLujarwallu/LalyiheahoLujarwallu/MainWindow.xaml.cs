e284ff96734a84e9d4f49d76d5de06aa21e3423b
lindexi <lindexi_gd@163.com>
2022-06-29 14:15:42 +0800

    加上事件用来调试某个属性的变更的逻辑

---
+++ LalyiheahoLujarwallu/LalyiheahoLujarwallu/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LalyiheahoLujarwallu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DependencyPropertyDescriptor.FromProperty(FrameworkElement.DataContextProperty, typeof(FrameworkElement)).AddValueChanged(this,
+                (sender, args) =>
+                {
+                });
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is null)
+            {
+                DataContext = this;
+            }
+            else
+            {
+                DataContext = null;
+            }
+        }
+    }
+}
