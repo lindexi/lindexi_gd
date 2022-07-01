32b43df62a184ee4bfa1b866c32ebf72bef7f53e
lindexi <lindexi_gd@163.com>
2022-07-01 14:11:29 +0800

    修复背景透明

--- SkiaSharp/BihuwelcairkiDelalurnere/BihuwelcairkiDelalurnere/Program.cs
+++ SkiaSharp/BihuwelcairkiDelalurnere/BihuwelcairkiDelalurnere/Program.cs
@@ -1,27 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using BihuwelcairkiDelalurnere;
+
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Graphics.Skia;
 
 using SkiaSharp;
 
-var skImageInfo = new SKImageInfo(1920, 1080, SKColorType.Bgra8888, SKAlphaType.Opaque, SKColorSpace.CreateSrgb());
+var skImageInfo = new SKImageInfo(1920, 1080, SKColorType.Bgra8888, SKAlphaType.Unpremul, SKColorSpace.CreateSrgb());
 
-var fileName = $"xx.svg";
+var fileName = $"xx.png";
 
-using var stream = File.OpenWrite(fileName);
-using var skCanvas = SKSvgCanvas.Create(new SKRect(0, 0, 100, 100), stream);
+using (var skImage = SKImage.Create(skImageInfo))
+{
+    using (var skBitmap = SKBitmap.FromImage(skImage))
+    {
+        using (var skCanvas = new SKCanvas(skBitmap))
+        {
+            skCanvas.Clear(SKColors.Transparent);
 
-var skiaCanvas = new SkiaCanvas();
-skiaCanvas.Canvas = skCanvas;
+            var skiaCanvas = new SkiaCanvas();
+            skiaCanvas.Canvas = skCanvas;
 
-ICanvas canvas = skiaCanvas;
+            ICanvas canvas = skiaCanvas;
+            var areaDraw = new AreaDraw();
+            areaDraw.OnRender(canvas);
+            skCanvas.Flush();
 
-var areaDraw = new AreaDraw();
-areaDraw.OnRender(canvas);
-
-canvas.DrawLine(10, 10, 100, 10);
-
-skCanvas.Flush();
-
+            using (var skData = skBitmap.Encode(SKEncodedImageFormat.Png, 100))
+            {
+                var file = new FileInfo(fileName);
+                using (var fileStream = file.OpenWrite())
+                {
+                    fileStream.SetLength(0);
+                    skData.SaveTo(fileStream);
+                }
+            }
+        }
+    }
+}
