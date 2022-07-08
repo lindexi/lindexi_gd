8fd64c8d1a2492db535c0d57ef0d84529717d339
lindexi <lindexi_gd@163.com>
2022-07-08 08:48:09 +0800

    WindowFromPoint malfunctions for x64 WPF application

---
+++ CemlochiharwhalYukekemcu/Controls/Cross.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1.Controls
+{
+	public class CrossReleasedEventArgs : EventArgs
+	{
+		internal Point Point { get; set; }
+	}
+	public class Cross : Control
+	{
+		private bool Dragginng = false;
+		public event EventHandler<CrossReleasedEventArgs> CrossReleased;
+		static Cross()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(Cross), new FrameworkPropertyMetadata(typeof(Cross)));
+		}
+		public Cross()
+		{
+		}
+
+		protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+		{
+			base.OnMouseLeftButtonDown(e);
+			CaptureMouse();
+			Dragginng = true;
+			Opacity = 0;
+		}
+		protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+			base.OnPreviewMouseLeftButtonUp(e);
+			if (Dragginng)
+			{
+				ReleaseMouseCapture();
+				Dragginng = false;
+				CrossReleased?.Invoke(this, new CrossReleasedEventArgs() { Point = e.GetPosition(this) });
+				Opacity = 1;
+			}
+		}
+		protected override void OnMouseEnter(MouseEventArgs e)
+		{
+			base.OnMouseEnter(e);
+			Cursor = Cursors.Cross;
+		}
+		protected override void OnMouseLeave(MouseEventArgs e)
+		{
+			base.OnMouseLeave(e);
+			if (!Dragginng)
+				Cursor = Cursors.Arrow;
+		}
+
+	}
+}
---
+++ CemlochiharwhalYukekemcu/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		[DllImport("User32.dll")]
+		private static extern nuint WindowFromPoint(int x, int y);
+		[DllImport("User32.dll")]
+		private static extern void GetWindowThreadProcessId(nuint hwnd, out int ID);
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Cross_CrossReleased(object sender, Controls.CrossReleasedEventArgs e)
+		{
+			Point p = (sender as Control).PointToScreen(e.Point);
+			nuint hwnd = WindowFromPoint((int)p.X, (int)p.Y);
+			GetWindowThreadProcessId(hwnd, out int pid);
+			var process = Process.GetProcessById(pid);
+			MessageBox.Show($"locaition = {p}, processId = {pid}, name = {process.ProcessName}");
+		}
+	}
+}
