d2959ab2d217d57e9357684d7bf39510e4d9cae8
lindexi <lindexi_gd@163.com>
2022-07-23 20:01:15 +0800

    图片转文本

---
+++ JagerekukibeHelcewhalay/JagerekukibeHelcewhalay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace JagerekukibeHelcewhalay;
+
+public class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        Application application = new Application();
+        application.Startup += (s, e) =>
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+        };
+        application.Run();
+    }
+}
+
