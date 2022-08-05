562f5b1ce0cd22ee476bfe140d5d12a351f24fa3
lindexi <lindexi_gd@163.com>
2022-08-05 14:37:10 +0800

    尝试读取字体文件

---
+++ TtfReader/TtfReader/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Buffers;
+using System.Runtime.InteropServices;
+
+var file = @"C:\windows\fonts\simhei.ttf";
+
+using var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+TtfInfo.Read(fileStream);
+
+Console.WriteLine("Hello, World!");
+
+
+class TtfInfo
+{
+    public static unsafe void Read(Stream stream)
+    {
+        var originBuffer = ArrayPool<byte>.Shared.Rent(1024);
+        var i = sizeof(OffsetTable);
+        Span<byte> buffer = originBuffer.AsSpan(0,i);
+        stream.Read(originBuffer,0,i);
+
+        fixed (byte* ptr = originBuffer)
+        {
+            var offsetTable = Marshal.PtrToStructure<OffsetTable>(new IntPtr(ptr));
+        }
+        //stream.Read()
+    }
+}
+
+public readonly struct Version
+{
+    public ushort Major { get; }
+    public ushort Minor { get; }
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct OffsetTable
+{
+    public Version SfntVersion { get; }
+    public ushort NumTables { get; }
+    public ushort SearchRange { get; }
+    public ushort EntrySelector { get; }
+    public ushort RangeShift { get; }
+}
