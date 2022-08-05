62c5e78042c7506734959a50cfc7ac1440182690
lindexi <lindexi_gd@163.com>
2022-08-05 17:47:29 +0800

    解析字体文件

---
+++ TtfReader/TtfReader/PlatformIdentifier.cs
@@ -0,0 +1,9 @@
+﻿namespace TtfReader;
+
+public enum PlatformIdentifier : ushort
+{
+    Unicode = 0,
+    Macintosh = 1,
+    Reserved = 2,
+    Microsoft = 3
+}
---
+++ TtfReader/TtfReader/TtfInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace TtfReader;
+
+record TtfInfo(TableDirectoryEntry[] TableDirectoryEntryArray, NameTable NameTable)
+{
+    public static TtfInfo Read(Stream stream)
+    {
+        using var bigEndianBinaryReader = new BigEndianBinaryReader(stream);
+        var offsetTable = OffsetTable.Read(bigEndianBinaryReader);
+        var tableDirectoryEntryArray = TableDirectoryEntryArrayReader.Read(bigEndianBinaryReader, offsetTable);
+
+        // [Glyph Properties Table - TrueType Reference Manual - Apple Developer](https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6prop.html )
+        var nameDirTableEntry = tableDirectoryEntryArray.First(t => t.Tag == "name");
+        stream.Position = nameDirTableEntry.Offset;
+
+        var nameTable = NameTable.Read(bigEndianBinaryReader);
+
+        return new TtfInfo(tableDirectoryEntryArray, nameTable);
+    }
+}
