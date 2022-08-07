1041006b7751d0d887adc86de151051374e5b4cd
lindexi <lindexi_gd@163.com>
2022-08-07 21:53:38 +0800

    测试控制台的逻辑是否可以在系统打出界面

---
+++ KallkadallwercedeWoqurneelurjawa/KallkadallwercedeWoqurneelurjawa/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Gtk;
+namespace KallkadallwercedeWoqurneelurjawa;
+
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        Application.Init();
+
+        var app = new Application("com.companyname.skiasharpsample", GLib.ApplicationFlags.None);
+        app.Register(GLib.Cancellable.Current);
+
+        var win = new MainWindow();
+        app.AddWindow(win);
+
+        win.Show();
+        Application.Run();
+    }
+}
