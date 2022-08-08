23259e0ffda16851834d757c0b1619dee299c7c7
lindexi <lindexi_gd@163.com>
2022-08-08 16:20:38 +0800

    导入图片解析可以在界面上将图片转换为像素的方式进行渲染

--- GafawwaybalachaCemleardearha/GafawwaybalachaCemleardearha/MainWindow.xaml.cs
+++ GafawwaybalachaCemleardearha/GafawwaybalachaCemleardearha/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using SkiaSharp;
 using Svg.Skia;
 using Path = System.IO.Path;
@@ -29,8 +18,6 @@ namespace GafawwaybalachaCemleardearha
             InitializeComponent();
 
             Loaded += MainWindow_Loaded;
-
-            
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -83,41 +70,5 @@ namespace GafawwaybalachaCemleardearha
 
             Root.Children.Add(image);
         }
-    }
-
-    public class SkiaCanvas : Image
-    {
-        public SkiaCanvas()
-        {
-            Loaded += SkiaCanvas_Loaded;
-        }
-
-        private void SkiaCanvas_Loaded(object sender, RoutedEventArgs e)
-        {
-            var writeableBitmap = new WriteableBitmap(PixelWidth, PixelHeight, 96, 96, PixelFormats.Bgra32,
-                BitmapPalettes.Halftone256Transparent);
-
-            _writeableBitmap = writeableBitmap;
-
-            var skImageInfo = new SKImageInfo()
-            {
-                Width = PixelWidth,
-                Height = PixelHeight,
-                ColorType = SKColorType.Bgra8888,
-                AlphaType = SKAlphaType.Premul,
-                ColorSpace = SKColorSpace.CreateSrgb()
-            };
-
-            SKSurface surface = SKSurface.Create(skImageInfo, writeableBitmap.BackBuffer);
-            _skSurface = surface;
-
-            Source = writeableBitmap;
-        }
-
-        private WriteableBitmap _writeableBitmap = null!; // 这里的 null! 是 C# 的新语法，是给智能分析用的，表示这个字段在使用的时候不会为空
-        private SKSurface _skSurface = null!; // 实际上 null! 的含义是我明确给他一个空值，也就是说如果是空也是预期的
-
-        public int PixelWidth => (int) Width;
-        public int PixelHeight => (int) Height;
     }
 }
