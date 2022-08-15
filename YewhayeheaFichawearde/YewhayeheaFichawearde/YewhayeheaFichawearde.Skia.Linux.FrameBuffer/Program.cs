84109e3163ba694622c92a77bbac7f1d96daa1ac
lindexi <lindexi_gd@163.com>
2022-08-15 14:16:08 +0800

    测试 UNO 不支持做多个控件的传入支持

---
+++ YewhayeheaFichawearde/YewhayeheaFichawearde/YewhayeheaFichawearde.Skia.Linux.FrameBuffer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Uno.UI.Runtime.Skia;
+
+namespace YewhayeheaFichawearde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.CursorVisible = false;
+
+                var host = new FrameBufferHost(() => new App());
+                host.Run();
+            }
+            finally
+            {
+                Console.CursorVisible = true;
+            }
+        }
+    }
+}
---
+++ YewhayeheaFichawearde/YewhayeheaFichawearde/YewhayeheaFichawearde.Skia.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YewhayeheaFichawearde.WPF.Host
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Root1.Content = new global::Uno.UI.Skia.Platform.WpfHost(Dispatcher, () => new YewhayeheaFichawearde.App()
+            {
+                Text = "WpfHost 1"
+            });
+
+            Root2.Content = new global::Uno.UI.Skia.Platform.WpfHost(Dispatcher, () => new YewhayeheaFichawearde.App());
+        }
+    }
+}
