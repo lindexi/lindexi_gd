582817073bb001820d362823d0fc813278f67c38
lindexi <lindexi_gd@163.com>
2022-08-27 16:40:58 +0800

    测试代码生成

---
+++ WhacadenaKewarfellaja/WhacadenaKewarfellaja/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace WhacadenaKewarfellaja
+{
+    public static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            HelloFrom("Fxx");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
