7808825af4346d97e6d5571199104fe505fa3bb3
lindexi <lindexi_gd@163.com>
2022-09-27 10:47:45 +0800

    定义入口类型

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/TextEditorCore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightTextEditorPlus.Core;
+
+/// <summary>
+/// 文本编辑控件
+/// <para></para>
+/// 支持复杂文本编辑，支持添加扩展字符包括图片等到文本
+/// <para></para>
+/// 这是一个底层类型，提供很多定制逻辑，设计上属于功能多但是不可简单使用，上层应该对此进行再次封装
+/// </summary>
+/// <remarks> 这个项目的核心和入口就是这个类</remarks>
+public class TextEditorCore
+{
+}
