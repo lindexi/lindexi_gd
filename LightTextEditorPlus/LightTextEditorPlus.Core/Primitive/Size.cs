af5aefe31b8634564233c5a6a7f046de2e710d90
lindexi <lindexi_gd@163.com>
2022-09-27 11:14:05 +0800

    定义基础类型

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Primitive/Size.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightTextEditorPlus.Core.Primitive;
+
+/// <summary>
+/// 文本库使用的尺寸
+/// </summary>
+public readonly struct Size
+{
+    public Size(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double Width { get; }
+    public double Height { get; }
+
+    public static Size Zero => new Size(0, 0);
+}
