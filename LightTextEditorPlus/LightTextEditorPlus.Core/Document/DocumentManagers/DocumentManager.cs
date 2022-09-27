9f6a57b78fc1a8d12b2b700d3cb96bdb6bbf18db
lindexi <lindexi_gd@163.com>
2022-09-27 11:14:57 +0800

    加上数据层和布局

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers/DocumentManager.cs
@@ -0,0 +1,26 @@
+﻿
+using TextEditor = LightTextEditorPlus.Core.TextEditorCore;
+
+namespace LightTextEditorPlus.Core.Document.DocumentManagers
+{
+    /// <summary>
+    /// 提供文档管理，只提供数据管理
+    /// </summary>
+    public class DocumentManager
+    {
+        public DocumentManager(TextEditor textEditor)
+        {
+
+        }
+
+        /// <summary>
+        /// 文档的宽度
+        /// </summary>
+        internal double DocumentWidth { set; get; }
+
+        /// <summary>
+        /// 文档的高度
+        /// </summary>
+        internal double DocumentHeight { set; get; }
+    }
+}
---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Layout/LayoutManager.cs
@@ -0,0 +1,11 @@
+﻿using TextEditor = LightTextEditorPlus.Core.TextEditorCore;
+
+namespace LightTextEditorPlus.Core.Layout;
+
+class LayoutManager
+{
+    public LayoutManager(TextEditor textEditor)
+    {
+    }
+
+}
