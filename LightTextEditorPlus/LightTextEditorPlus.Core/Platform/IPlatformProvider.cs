4fe0e863528076863e5bd61ac1e526638f605b4f
lindexi <lindexi_gd@163.com>
2022-09-27 15:36:09 +0800

    加上调度方法

--- LightTextEditorPlus/LightTextEditorPlus.Core/Platform/IPlatformProvider.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Platform/IPlatformProvider.cs
@@ -9,4 +9,13 @@ namespace LightTextEditorPlus.Core.Platform;
 public interface IPlatformProvider
 {
     // 获取默认字体
+
+    /// <summary>
+    /// 加入调度更新布局请求
+    /// </summary>
+    /// 推荐处理：快速多次触发时，只触发一次，以及调度到合适的时机去执行
+    /// <param name="textLayout"></param>
+    void RequireDispatchUpdateLayout(Action textLayout);
 }
+
+ 
