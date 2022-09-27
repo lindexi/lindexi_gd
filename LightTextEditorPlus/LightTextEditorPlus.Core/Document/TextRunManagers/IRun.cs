ac9c37006a056ffd2accc194d2d341abcc8ea8b2
lindexi <lindexi_gd@163.com>
2022-09-27 12:08:33 +0800

    定义纯文本

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/IRun.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/IRun.cs
@@ -5,5 +5,20 @@
 /// </summary>
 public interface IRun
 {
+    /// <summary>
+    /// 包含的字符数量
+    /// </summary>
+    int Count { get; }
+
+    /// <summary>
+    /// 获取某个字符
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    ICharObject GetChar(int index);
+
+    /// <summary>
+    /// 定义的样式，可为空。空代表着没有定义特别的样式
+    /// </summary>
     IReadOnlyRunProperty? RunProperty { get; }
 }
---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/ITextRun.cs
@@ -0,0 +1,9 @@
+﻿namespace LightTextEditorPlus.Core.Document;
+
+/// <summary>
+/// 纯文本段
+/// </summary>
+public interface ITextRun : IRun
+{
+
+}
