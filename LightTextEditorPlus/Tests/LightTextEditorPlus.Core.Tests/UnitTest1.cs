de5e588f8597986d9c11f137f29d612390ffd136
lindexi <lindexi_gd@163.com>
2022-09-27 10:07:04 +0800

    拆分项目搭建框架

---
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LightTextEditorPlus.Core.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LightTextEditorPlus.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
