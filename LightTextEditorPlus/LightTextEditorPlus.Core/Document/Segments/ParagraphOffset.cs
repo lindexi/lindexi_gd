fbcad5fbf9f8be32d35f3cf062bdaf838a63c746
lindexi <lindexi_gd@163.com>
2022-10-13 17:20:53 +0800

    优化调试

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/Segments/ParagraphOffset.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/Segments/ParagraphOffset.cs
@@ -1,4 +1,6 @@
-﻿namespace LightTextEditorPlus.Core.Document.Segments;
+﻿using System.Diagnostics;
+
+namespace LightTextEditorPlus.Core.Document.Segments;
 
 /// <summary>
 /// 表示段落的偏移量
@@ -8,6 +10,7 @@ readonly struct ParagraphOffset
     /// <summary>
     /// 创建段落的偏移量
     /// </summary>
+    [DebuggerStepThrough]
     public ParagraphOffset(int offset)
     {
         Offset = offset;
@@ -17,4 +20,6 @@ readonly struct ParagraphOffset
     /// 段落偏移量
     /// </summary>
     public int Offset { get; }
+
+    public override string ToString() => Offset.ToString();
 }
