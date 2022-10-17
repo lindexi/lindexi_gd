dfcb04a6bd6ba18b3d14fb4847d186498b412195
lindexi <lindexi_gd@163.com>
2022-10-17 11:33:32 +0800

    拆分文件

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/HitParagraphDataResult.cs
@@ -0,0 +1,16 @@
+﻿using LightTextEditorPlus.Core.Carets;
+using LightTextEditorPlus.Core.Document.Segments;
+
+namespace LightTextEditorPlus.Core.Document;
+
+/// <summary>
+/// 命中段落数据的结果
+/// </summary>
+/// <param name="Offset">参与命中的光标偏移量</param>
+/// <param name="ParagraphData">命中到的段落</param>
+/// <param name="HitOffset">命中到的段落偏移量</param>
+/// <param name="ParagraphManager"></param>
+/// 此类型用来减少重复计算
+readonly record struct HitParagraphDataResult(CaretOffset Offset, ParagraphData ParagraphData, ParagraphOffset HitOffset, ParagraphManager ParagraphManager)
+{
+}
--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/TextRunManager.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/TextRunManager.cs
@@ -97,18 +97,6 @@ internal class TextRunManager
 }
 
 /// <summary>
-/// 命中段落数据的结果
-/// </summary>
-/// <param name="Offset">参与命中的光标偏移量</param>
-/// <param name="ParagraphData">命中到的段落</param>
-/// <param name="HitOffset">命中到的段落偏移量</param>
-/// <param name="ParagraphManager"></param>
-/// 此类型用来减少重复计算
-readonly record struct HitParagraphDataResult(CaretOffset Offset, ParagraphData ParagraphData, ParagraphOffset HitOffset, ParagraphManager ParagraphManager)
-{
-}
-
-/// <summary>
 /// 段落管理
 /// </summary>
 class ParagraphManager
