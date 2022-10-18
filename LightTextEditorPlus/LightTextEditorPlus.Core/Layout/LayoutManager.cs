c77fdffe75c8dbd10afaacf0f683b5e36cd9d170
lindexi <lindexi_gd@163.com>
2022-10-18 15:30:39 +0800

    完善从 RunList 获取信息

--- LightTextEditorPlus/LightTextEditorPlus.Core/Layout/LayoutManager.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Layout/LayoutManager.cs
@@ -173,6 +173,12 @@ class HorizontalArrangingLayoutProvider : ArrangingLayoutProvider
 
 public class RunVisitor
 {
+    public RunVisitor(IReadOnlyRunProperty defaultRunProperty, IReadOnlyList<IRun> runList)
+    {
+        DefaultRunProperty = defaultRunProperty;
+        RunList = runList;
+    }
+
     private IReadOnlyRunProperty DefaultRunProperty { get; }
     private IReadOnlyList<IRun> RunList { get; }
 
@@ -188,11 +194,52 @@ public class RunVisitor
     /// </summary>
     private int RunIndex { get; set; }
 
-    public (ICharObject charObject, IReadOnlyRunProperty RunProperty) GetCurrentChar()
+    public (ICharObject charObject, IReadOnlyRunProperty RunProperty) GetCurrentCharInfo()
     {
         var run = RunList[RunIndex];
         var charObject = run.GetChar(CurrentCharIndex- CharStartIndexOfCurrentRun);
         return (charObject, run.RunProperty ?? DefaultRunProperty);
+    }
+
+    public (ICharObject charObject, IReadOnlyRunProperty RunProperty) GetCharInfo(int charIndex)
+    {
+        var (charObject, runProperty) = RunList.GetCharInfo(charIndex);
+        return (charObject, runProperty ?? DefaultRunProperty);
+    }
+}
+
+static class ReadOnlyRunListExtension
+{
+    public static (ICharObject charObject, IReadOnlyRunProperty? RunProperty) GetCharInfo(
+        this IReadOnlyList<IRun> runList, int charIndex)
+    {
+        var (run, _, hitIndex) = runList.GetRunByCharIndex(charIndex);
+        return (run.GetChar(hitIndex), run.RunProperty);
+    }
+
+    public static (IRun run, int runIndex, int hitIndex) GetRunByCharIndex(this IReadOnlyList<IRun> runList, int charIndex)
+    {
+        var currentCharCount = 0;
+        for (var i = 0; i < runList.Count; i++)
+        {
+            var run = runList[i];
+            var length = run.Count;
+            var behindOffset = currentCharCount + length;
+
+            // 判断是否落在当前的里面
+            if (behindOffset >= charIndex)
+            {
+                var hitIndex = charIndex - currentCharCount;
+                var runIndex = i;
+                return (run, runIndex, hitIndex);
+            }
+            else
+            {
+                currentCharCount = behindOffset;
+            }
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(charIndex),$"最大的字符数量是: {currentCharCount}；传入的字符索引是: {charIndex}");
     }
 }
 
