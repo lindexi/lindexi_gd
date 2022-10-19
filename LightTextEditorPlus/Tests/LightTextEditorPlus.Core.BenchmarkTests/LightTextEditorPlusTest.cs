272620dbf0e1d7ea93184947e3e615cb8e5fb7b3
lindexi <lindexi_gd@163.com>
2022-10-19 20:07:27 +0800

    加上性能测试

---
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.BenchmarkTests/LightTextEditorPlusTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+using LightTextEditorPlus.Core.Tests;
+
+namespace LightTextEditorPlus.Core.BenchmarkTests;
+
+[MemoryDiagnoser]
+public class LightTextEditorPlusTest
+{
+    [Benchmark()]
+    [Arguments(TestHelper.PlainNumberText)]
+    [Arguments(TestHelper.PlainLongNumberText)]
+    public void AppendText(string text)
+    {
+        var textEditor = TestHelper.GetTextEditorCore();
+        textEditor.AppendText(text);
+    }
+
+    [Benchmark()]
+    [ArgumentsSource(nameof(GetLongTextArgument))]
+    public void LongText(string text)
+    {
+        var textEditor = TestHelper.GetTextEditorCore();
+        textEditor.AppendText(text);
+    }
+
+    public IEnumerable<string> GetLongTextArgument()
+    {
+        var stringBuilder = new StringBuilder();
+        for (int i = 0; i < 100000; i++)
+        {
+            stringBuilder.Append('T');
+        }
+
+        yield return stringBuilder.ToString();
+    }
+}
