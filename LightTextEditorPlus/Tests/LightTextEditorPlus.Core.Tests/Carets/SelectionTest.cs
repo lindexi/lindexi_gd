47f1fdb40d78078ab7670d56c562be719dd1e8f7
lindexi <lindexi_gd@163.com>
2022-10-20 18:27:03 +0800

    加上更多单元测试

--- LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/Carets/SelectionTest.cs
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/Carets/SelectionTest.cs
@@ -29,4 +29,63 @@ public class SelectionTest
             Assert.AreEqual(selection.FrontOffset, selection.BehindOffset);
         });
     }
+
+    [ContractTestCase]
+    public void GetLength()
+    {
+        "给定长度和开始点，可以拿到和传入长度相同的选择长度".Test(() =>
+        {
+            // Arrange
+            var startOffset = new CaretOffset();
+            var length = 100;
+
+            // Action
+            var selection = new Selection(startOffset, length);
+
+            // Assert
+            Assert.AreEqual(length, selection.Length);
+        });
+
+        //"修改结束光标，可以更新长度".Test(() =>
+        //{
+        //    // Arrange
+        //    var startOffset = new CaretOffset();
+        //    var endOffset = new CaretOffset()
+        //    {
+        //        Offset = 100
+        //    };
+
+        //    var selection = new Selection(startOffset, endOffset);
+
+        //    selection.EndOffset = new CaretOffset()
+        //    {
+        //        Offset = 9
+        //    };
+
+        //    // Action
+
+        //    var length = selection.Length;
+
+        //    // Assert
+        //    Assert.AreEqual(9, length);
+        //});
+
+        "传入两个光标，可以拿到正确长度".Test(() =>
+        {
+            // Arrange
+            var startOffset = new CaretOffset();
+            var endOffset = new CaretOffset(100)
+            {
+                //Offset = 100
+            };
+
+            var selection = new Selection(startOffset, endOffset);
+
+            // Action
+            var length = selection.Length;
+
+            // Assert
+            Assert.AreEqual(100, length);
+        });
+    }
 }
