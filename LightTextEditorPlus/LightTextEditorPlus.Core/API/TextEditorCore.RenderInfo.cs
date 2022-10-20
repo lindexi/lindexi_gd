e92267beabb38ecf83029565e765844f81133442
lindexi <lindexi_gd@163.com>
2022-10-20 18:09:25 +0800

    支持获取渲染信息

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/API/TextEditorCore.RenderInfo.cs
@@ -0,0 +1,17 @@
+﻿using LightTextEditorPlus.Core.Exceptions;
+using LightTextEditorPlus.Core.Primitive;
+
+namespace LightTextEditorPlus.Core;
+
+public partial class TextEditorCore
+{
+    public Rect GetDocumentBounds()
+    {
+        if (_layoutManager.DocumentRenderData.IsDirty)
+        {
+            throw new TextEditorDirtyException();
+        }
+
+        return _layoutManager.DocumentRenderData.DocumentBounds;
+    }
+}
