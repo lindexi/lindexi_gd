b385148f1c2ad55758446a58bd181993c4b8cb51
lindexi <lindexi_gd@163.com>
2022-10-21 15:34:51 +0800

    删除注释

--- LightTextEditorPlus/LightTextEditorPlus.Core/Layout/Context_/Platform_/WholeLineLayoutResult.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Layout/Context_/Platform_/WholeLineLayoutResult.cs
@@ -1,6 +1,4 @@
-﻿//using System.Collections.Generic;
-//using LightTextEditorPlus.Core.Document;
-using LightTextEditorPlus.Core.Primitive;
+﻿using LightTextEditorPlus.Core.Primitive;
 
 namespace LightTextEditorPlus.Core.Layout;
 
@@ -11,9 +9,4 @@ namespace LightTextEditorPlus.Core.Layout;
 /// <param name="CharCount">这一行使用的 字符 的数量</param>
 public readonly record struct WholeLineLayoutResult(Size Size, int CharCount)
 {
-    // 现在使用字符布局了，不再需要对 Run 进行分割
-    ///// <summary>
-    ///// 是否最后一个 Run 需要被分割。也就是最后一个 Run 将会跨多行
-    ///// </summary>
-    //public bool NeedSplitLastRun => LastRunHitIndex > 0;
 }
