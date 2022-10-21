a75e54dd3824c441e6abaa7baf97abc2aa6e9f3c
lindexi <lindexi_gd@163.com>
2022-10-21 11:57:51 +0800

    提供行的绘制更新支持

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/TextRunManager.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/TextRunManager.cs
@@ -715,7 +715,41 @@ class LineVisualData : IParagraphCache
     /// <summary>
     /// 这一行的左上角的点，相对于文本框
     /// </summary>
-    public Point LeftTop { set; get; }
+    public Point LeftTop
+    {
+        set
+        {
+            _leftTop = value;
+            IsLineStartPointUpdated = true;
+        }
+        get => _leftTop;
+    }
+    private Point _leftTop;
+
+    /// <summary>
+    /// 是否需要绘制
+    /// </summary>
+    /// 如果没有画过，或者是行的起始点变更，需要绘制
+    public bool NeedDraw => !IsDrawn || IsLineStartPointUpdated;
+
+    /// <summary>
+    /// 是否已经画过
+    /// </summary>
+    public bool IsDrawn { get; private set; }
+
+    /// <summary>
+    /// 是否行的起始点变更
+    /// </summary>
+    public bool IsLineStartPointUpdated { get; private set; } = false;
+
+    /// <summary>
+    /// 设置已经画完了
+    /// </summary>
+    public void SetDrawn()
+    {
+        IsDrawn = true;
+        IsLineStartPointUpdated = false;
+    }
 
     /// <summary>
     /// 这一行的尺寸
