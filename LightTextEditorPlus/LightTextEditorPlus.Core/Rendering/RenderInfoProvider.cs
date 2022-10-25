311618571ca9eeb650eb17ef71cf1edce2866aac
lindexi <lindexi_gd@163.com>
2022-10-25 17:11:39 +0800

    提供输出段落渲染信息

--- LightTextEditorPlus/LightTextEditorPlus.Core/Rendering/RenderInfoProvider.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Rendering/RenderInfoProvider.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
-
+using LightTextEditorPlus.Core.Document;
 using TextEditor = LightTextEditorPlus.Core.TextEditorCore;
 
 namespace LightTextEditorPlus.Core.Rendering;
@@ -17,5 +18,24 @@ public class RenderInfoProvider
 
     public TextEditor TextEditor { get; }
 
+    public IEnumerable<ParagraphRenderInfo> GetParagraphRenderInfoList()
+    {
+        var paragraphManager = TextEditor.DocumentManager.TextRunManager.ParagraphManager;
+        var list = paragraphManager.GetParagraphList();
+        for (var index = 0; index < list.Count; index++)
+        {
+            var paragraphData = list[index];
+            yield return new ParagraphRenderInfo(index, paragraphData);
+        }
+    }
+}
+
+public class ParagraphRenderInfo
+{
+    internal ParagraphRenderInfo(int index, ParagraphData paragraphData)
+    {
+        
+    }
+    // todo 考虑作为结构体
 
 }
