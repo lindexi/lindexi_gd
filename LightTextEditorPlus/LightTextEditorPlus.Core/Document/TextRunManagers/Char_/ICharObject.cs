90ddf059c2e60b6ba134bed0a3be42a39ce3f3f7
lindexi <lindexi_gd@163.com>
2022-10-27 15:04:10 +0800

    拆分文件

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/Char_/ICharObject.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/TextRunManagers/Char_/ICharObject.cs
@@ -13,12 +13,3 @@ public interface ICharObject : IEquatable<string>, IDeepCloneable<ICharObject>
     /// <returns></returns>
     string ToText();
 }
-
-/// <summary>
-/// 表示一个由单个字符构成的字符
-/// </summary>
-/// 由于文本里面存在一些语言是不能使用一个 char 表示一个字符的。所以大部分情况下都是需要使用 string 表示
-public interface ISingleCharObject : ICharObject
-{
-    char GetChar();
-}
