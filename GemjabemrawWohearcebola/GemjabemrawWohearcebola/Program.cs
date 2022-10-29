4b303875f66f959e82875b93f09f4c666fbd5453
lindexi <lindexi_gd@163.com>
2022-10-29 16:03:39 +0800

    生成源代码

---
+++ GemjabemrawWohearcebola/GemjabemrawWohearcebola/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+namespace GemjabemrawWohearcebola;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonString =
+            @"{
+  ""Date"": ""2019-09-01T00:00:00"",
+  ""TemperatureCelsius"": 25,
+  ""Summary"": ""Hot""
+}
+";
+        WeatherForecast? weatherForecast;
+
+        weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(
+            jsonString, MyJsonContext.Default.WeatherForecast);
+        Console.WriteLine($"Date={weatherForecast?.Date}");
+
+        jsonString = JsonSerializer.Serialize(
+            weatherForecast!, MyJsonContext.Default.WeatherForecast);
+        Console.WriteLine(jsonString);
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string? Summary { get; set; }
+    }
+
+    [JsonSerializableAttribute(typeof(WeatherForecast))]
+    internal partial class MyJsonContext : JsonSerializerContext
+    {
+
+    }
+}
