46f5ad6ba404a4a48f0581598f4351e817e104a8
lindexi <lindexi_gd@163.com>
2022-10-30 17:13:35 +0800

    加上更多单元测试

--- PackageManager/Test/PackageManager.Test/PackageTest.cs
+++ PackageManager/Test/PackageManager.Test/PackageTest.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MSTest.Extensions.Contracts;
+using PackageManager.Server.Controllers;
 
 namespace PackageManager.Test;
 
 [TestClass]
 public class PackageTest
 {
+    [ContractTestCase]
+    public void Put()
+    {
+        "调用 Put 推送包时，在 Header 带上错误的 Token 将会返回 NotFound 错误".Test(async () =>
+        {
+            var httpClient = TestFramework.GetTestClient();
+
+            var putPackageRequest = new PutPackageRequest();
+            var jsonContent = JsonContent.Create(putPackageRequest);
+            httpClient.DefaultRequestHeaders.Add("Token","Error");
+            var result = await httpClient.PutAsync("/Package", jsonContent);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, result.StatusCode);
+        });
+
+        "调用 Put 推送包时，没有在 Header 带上 Token 将会返回 NotFound 错误".Test(async () =>
+        {
+            var httpClient = TestFramework.GetTestClient();
+
+            var putPackageRequest = new PutPackageRequest();
+            var result = await httpClient.PutAsync("/Package", JsonContent.Create(putPackageRequest));
+
+            Assert.AreEqual(HttpStatusCode.NotFound,result.StatusCode);
+        });
+    }
+
     [ContractTestCase]
     public void Get()
     {
