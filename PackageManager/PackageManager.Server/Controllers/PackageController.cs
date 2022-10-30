58c8d7bdf4e79b7d3400d36abda97f0bef0f4d35
lindexi <lindexi_gd@163.com>
2022-10-30 20:21:13 +0800

    拆分文件

--- PackageManager/PackageManager.Server/Controllers/PackageController.cs
+++ PackageManager/PackageManager.Server/Controllers/PackageController.cs
@@ -72,9 +72,4 @@ public class PackageController : ControllerBase
     }
 }
 
-public static class TokenConfiguration
-{
-    public const string Token = "B44A0A9E-D6C8-434F-9215-894A30BC5674";
-}
-
 public record PutPackageRequest(PackageInfo PackageInfo);
---
+++ PackageManager/PackageManager.Server/Controllers/TokenConfiguration.cs
@@ -0,0 +1,6 @@
+namespace PackageManager.Server.Controllers;
+
+public static class TokenConfiguration
+{
+    public const string Token = "B44A0A9E-D6C8-434F-9215-894A30BC5674";
+}
