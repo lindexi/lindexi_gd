5707ee58324b4198e2ba73b3ccdb2fedabcf3c9a
lindexi <lindexi_gd@163.com>
2022-10-30 17:32:36 +0800

    搭建数据存储

---
+++ PackageManager/PackageManager.Server/Context/PackageManagerContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PackageManager.Server.Model;
+
+namespace PackageManager.Server.Context;
+
+public class PackageManagerContext:DbContext
+{
+    public PackageManagerContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    /// <summary>
+    /// 只包含最新版本的信息
+    /// </summary>
+    public DbSet<PackageInfo> LatestPackageDbSet { set; get; } 
+    // 框架注入
+        = null!;
+
+    /// <summary>
+    /// 包含历史版本的
+    /// </summary>
+    public DbSet<PackageInfo> PackageStorageDbSet { set; get; }
+    // 框架注入
+        = null!;
+}
