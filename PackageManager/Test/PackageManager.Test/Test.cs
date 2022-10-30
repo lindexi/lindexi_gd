2cdae7fc3c2618e08d5c9910e6019138bba31e40
lindexi <lindexi_gd@163.com>
2022-10-30 15:23:23 +0800

    搭建项目基础

---
+++ PackageManager/Test/PackageManager.Test/Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PackageManager.Server;
+
+namespace PackageManager.Test;
+[TestClass]
+public static class TestHostBuild
+{
+    public static HttpClient GetTestClient() => _host.GetTestClient();
+
+    [AssemblyInitialize]
+    public static async Task GlobalInitialize(TestContext testContext)
+    {
+        IHost host = await CreateAndRun();
+        _host = host;
+    }
+
+    private static IHost _host;
+
+    [AssemblyCleanup]
+    public static void GlobalCleanup()
+    {
+        _host.Dispose();
+    }
+
+    private static Task<IHost> CreateAndRun() => CreateHostBuilder().StartAsync();
+
+    public static IHostBuilder CreateHostBuilder() =>
+        Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseTestServer(); //关键是多了这一行建立TestServer
+            })
+            //.ConfigureAppConfiguration((hostingContext, config) =>
+            //{
+            //    // 进行测试的配置
+            //    var appConfigurator = config.ToAppConfigurator();
+            //    // 这里使用了 https://github.com/dotnet-campus/dotnetCampus.Configurations 做配置
+            //    var apmConfiguration = appConfigurator.Of<ApmConfiguration>();
+            //    apmConfiguration.DisableApm = true;
+            //})
+           ;
+
+}
