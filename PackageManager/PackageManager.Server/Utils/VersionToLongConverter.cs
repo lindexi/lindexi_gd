d0ce193f12a5a360a8138ba7fe1903cd6fb5c6d2
lindexi <lindexi_gd@163.com>
2022-11-06 19:50:04 +0800

    加上新的辅助类

---
+++ PackageManager/PackageManager.Server/Utils/VersionToLongConverter.cs
@@ -0,0 +1,13 @@
+﻿namespace PackageManager.Server.Utils;
+
+public static class VersionToLongConverter
+{
+    public static long VersionToLong(this Version version)
+    {
+        long value = ((long)version.Major) << (16 * 3);
+        value += ((long)version.Minor) << (16 * 2);
+        value += version.Build << 16;
+        value += version.Revision;
+        return value;
+    }
+}
