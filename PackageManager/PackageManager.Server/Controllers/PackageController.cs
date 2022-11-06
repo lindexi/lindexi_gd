dd7542d068751dba8418c0178726dc8b7f460890
lindexi <lindexi_gd@163.com>
2022-11-06 16:18:06 +0800

    实现比较版本

--- PackageManager/PackageManager.Server/Controllers/PackageController.cs
+++ PackageManager/PackageManager.Server/Controllers/PackageController.cs
@@ -82,3 +82,33 @@ public class PackageController : ControllerBase
 }
 
 public record PutPackageRequest(PackageInfo PackageInfo);
+
+public class StringVersionComparer : IComparer<string>
+{
+    public int Compare(string? x, string? y)
+    {
+        // 大于 返回1
+        // 等于 返回0
+        // 小于 返回负数
+        if (x is null && y is null) return 0;
+        if (x is null && y is not null) return -1;
+        if (x is not null && y is null) return 1;
+
+        var xSuccess = Version.TryParse(x, out var versionX);
+        var ySuccess = Version.TryParse(y, out var versionY);
+
+        if (!xSuccess && !ySuccess) return 0;
+        if (!xSuccess && ySuccess) return -1;
+        if (xSuccess && !ySuccess) return 1;
+
+        if (versionX is not null && versionY is not null)
+        {
+            return versionX.CompareTo(versionY);
+        }
+        else
+        {
+            // 理论上不会进入这个逻辑
+            return 0;
+        }
+    }
+}
