4fb152f1383953e1baef71a1c05a35ca89077095
lindexi <lindexi_gd@163.com>
2022-11-11 14:59:17 +0800

    整理代码

--- LightTextEditorPlus/LightTextEditorPlus.Core/API/Extensions_/TextEditorCoreEditExtension.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/API/Extensions_/TextEditorCoreEditExtension.cs
@@ -1,33 +1,7 @@
-﻿using LightTextEditorPlus.Core.Carets;
-using LightTextEditorPlus.Core.Document;
+﻿using LightTextEditorPlus.Core.Document;
 using LightTextEditorPlus.Core.Document.DocumentManagers;
 
 namespace LightTextEditorPlus.Core;
-
-public static class TextEditorCoreSelectionExtension
-{
-    /// <summary>
-    /// 全选文本
-    /// </summary>
-    public static void SelectAll(this TextEditorCore textEditor)
-    {
-        DocumentManager documentManager = textEditor.DocumentManager;
-        var allDocumentSelection = documentManager.GetAllDocumentSelection();
-        documentManager.SetSelection(allDocumentSelection);
-    }
-
-    /// <summary>
-    /// 清空选择
-    /// </summary>
-    public static void ClearSelection(this TextEditorCore textEditor)
-    {
-        // todo 确认清空选择的时候，光标应该在哪
-        DocumentManager documentManager = textEditor.DocumentManager;
-
-        var selection = new Selection(documentManager.CurrentCaretOffset, 0);
-        documentManager.SetSelection(selection);
-    }
-}
 
 /// <summary>
 /// 文档的编辑扩展
