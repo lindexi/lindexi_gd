b3cac28225171ffc20efc70aca0bfacef529f2ec
lindexi <lindexi_gd@163.com>
2022-11-13 16:51:55 +0800

    加上注释

--- PackageManager/PackageManager.Server/Context/ResponseErrorCode.cs
+++ PackageManager/PackageManager.Server/Context/ResponseErrorCode.cs
@@ -3,5 +3,9 @@
 public static class ResponseErrorCode
 {
     public static ErrorCode Ok => new ErrorCode(0, "OK");
+
+    /// <summary>
+    /// 不支持此客户端版本
+    /// </summary>
     public static ErrorCode DoNotSupportClientVersion => new ErrorCode(1000, "不支持此客户端版本");
 }
