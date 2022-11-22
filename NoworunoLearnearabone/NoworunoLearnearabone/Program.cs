d8c89fe085da7d40601b01ffeab65e34791768e4
lindexi <lindexi_gd@163.com>
2022-11-22 08:44:37 +0800

    测试分词

---
+++ NoworunoLearnearabone/NoworunoLearnearabone/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Add using statements
+using Microsoft.ML;
+using Microsoft.ML.Tokenizers;
+
+// Initialize MLContext
+//var mlContext = new MLContext();
+
+// Define vocabulary file paths
+var vocabFilePath = @"F:\lindexi\Code\Tokenizer\vocab.json";
+var mergeFilePath = @"F:\lindexi\Code\Tokenizer\merges.txt";
+
+// Initialize Tokenizer
+var tokenizer = new Tokenizer(new Bpe(vocabFilePath, mergeFilePath), RobertaPreTokenizer.Instance);
+
+// Define input for tokenization
+var input = "the brown fox jumped over the lazy dog!";
+
+// Encode input
+var tokenizerEncodedResult = tokenizer.Encode(input);
+
+// Decode results
+var result = tokenizer.Decode(tokenizerEncodedResult.Ids);
+
+Console.WriteLine();
