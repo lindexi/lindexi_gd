ba911a0e85426b29205c3fb63f69d89ada5d72b1
lindexi <lindexi_gd@163.com>
2022-11-28 15:56:12 +0800

    加上画刷定义

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/Property_/RunProperty_/IImmutableRunPropertyValue.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/Property_/RunProperty_/IImmutableRunPropertyValue.cs
@@ -6,3 +6,17 @@
 public interface IImmutableRunPropertyValue
 {
 }
+
+/// <summary>
+/// 表示一个不可变的对象值
+/// </summary>
+/// 要是还有人去拿属性去改，那我也救不了了
+public class ImmutableRunPropertyValue<T> : IImmutableRunPropertyValue
+{
+    public ImmutableRunPropertyValue(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; }
+}
--- LightTextEditorPlus/LightTextEditorPlus/WpfFramework_/Document/Property_/RunProperty_/RunProperty.cs
+++ LightTextEditorPlus/LightTextEditorPlus/WpfFramework_/Document/Property_/RunProperty_/RunProperty.cs
@@ -1,4 +1,5 @@
-﻿using LightTextEditorPlus.Core.Document;
+﻿using System.Windows.Media;
+using LightTextEditorPlus.Core.Document;
 
 namespace LightTextEditorPlus.Document;
 
@@ -13,4 +14,17 @@ public class RunProperty : LayoutOnlyRunProperty
     /// 继承样式里的属性
     /// </summary>
     private LayoutOnlyRunProperty? StyleRunProperty { get; }
+
+
+}
+
+/// <summary>
+/// 表示一个不可变的画刷
+/// </summary>
+/// 要是还有人去拿属性去改，那我也救不了了
+public class ImmutableBrush : ImmutableRunPropertyValue<Brush>
+{
+    public ImmutableBrush(Brush value) : base(value)
+    {
+    }
 }
