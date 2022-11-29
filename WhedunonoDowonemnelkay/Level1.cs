49d40420d1a99b09a129c31c240466032fb69e46
lindexi <lindexi_gd@163.com>
2022-11-29 09:13:49 +0800

    TreeView Virtualization seems to be broken
    https://github.com/dotnet/wpf/issues/7321

---
+++ WhedunonoDowonemnelkay/Level1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2;
+
+public class Level1
+{
+    public string DisplayText { get; set; } = "";
+
+    public ObservableCollection<Level2> Children { get; } = new();
+}
