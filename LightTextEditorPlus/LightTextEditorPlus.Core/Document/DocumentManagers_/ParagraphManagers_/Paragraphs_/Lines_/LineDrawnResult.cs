07ab8fbd2efe760761c1e71b99fb0291580265dc
lindexi <lindexi_gd@163.com>
2022-11-30 16:50:12 +0800

    加上注释

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/Lines_/LineDrawnResult.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/Lines_/LineDrawnResult.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// 行的渲染结果
 /// </summary>
-/// <param name="LineAssociatedRenderData">行的关联渲染信息。下次更新渲染，将会自动带上</param>
+/// <param name="LineAssociatedRenderData">行的关联渲染信息。下次更新渲染，将会自动带上，传入到 <see cref="LineDrawnArgument.LineAssociatedRenderData"/> 参数</param>
 public readonly record struct LineDrawnResult(object? LineAssociatedRenderData)
 {
 }
