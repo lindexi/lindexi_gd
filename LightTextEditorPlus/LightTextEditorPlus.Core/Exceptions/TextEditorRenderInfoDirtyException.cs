9c76427a02bf9c636ecf83e0e55155b4089f4d76
lindexi <lindexi_gd@163.com>
2022-12-02 17:35:17 +0800

    优化异常信息

--- LightTextEditorPlus/LightTextEditorPlus.Core/Exceptions/TextEditorRenderInfoDirtyException.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Exceptions/TextEditorRenderInfoDirtyException.cs
@@ -4,5 +4,5 @@ namespace LightTextEditorPlus.Core.Exceptions;
 
 public class TextEditorRenderInfoDirtyException : Exception
 {
-    public override string Message => "文本布局已更新，此渲染信息是脏的";
+    public override string Message => "文本布局已更新，此渲染信息是脏的。请不要缓存 RenderInfoProvider 对象";
 }
