d643361233720bb93f63cab7b7fd3f07e5f72ae6
lindexi <lindexi_gd@163.com>
2022-12-09 15:30:02 +0800

    Revert "测量内存占用"
    This reverts commit 9df5012dcba59a32c2a9b636c4257d8a40584f08.

--- LightTextEditorPlus/Tests/LightTextEditorPlus.Core.BenchmarkTests/Program.cs
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.BenchmarkTests/Program.cs
@@ -1,21 +1,11 @@
 ﻿using BenchmarkDotNet.Running;
 
-using LightTextEditorPlus.Core.TestsFramework;
-
 namespace LightTextEditorPlus.Core.BenchmarkTests;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        var textEditor = TestHelper.GetTextEditorCore();
-
-        for (int i = 0; i < 100000; i++)
-        {
-            textEditor.AppendText("123");
-        }
-
-        Console.Read();
-        //BenchmarkRunner.Run<LightTextEditorPlusTest>();
+        BenchmarkRunner.Run<LightTextEditorPlusTest>();
     }
 }
