f758059f3f9f9bbfe2e7205a137d2e5b3da31f7a
lindexi <lindexi_gd@163.com>
2022-12-21 08:45:40 +0800

    在等待任务执行完成之后干某个活的事情上有多个可选方法需要警惕使用等待方法无论是成功还是失败都能进入

---
+++ HojohoyahobaWayfahurhalqeje/HojohoyahobaWayfahurhalqeje/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace HojohoyahobaWayfahurhalqeje;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var task = Task.Run(Foo).ContinueWith(t =>
+        {
+
+        }, TaskContinuationOptions.OnlyOnFaulted);
+
+        try
+        {
+            await task;
+        }
+        catch (TaskCanceledException e)
+        {
+
+        }
+
+        var task1 = Task.Run(FooWithException).ContinueWith(t =>
+        {
+
+        }, TaskContinuationOptions.OnlyOnFaulted);
+
+        await task1;
+    }
+
+    static void Foo()
+    {
+
+    }
+
+    static void FooWithException()
+    {
+        throw new Exception("lindexi is doubi");
+    }
+}
