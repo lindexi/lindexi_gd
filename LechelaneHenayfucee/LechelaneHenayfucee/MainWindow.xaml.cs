c7556d7b92605000011425f82793f9e4063e5a00
lindexi <lindexi_gd@163.com>
2022-12-27 19:27:59 +0800

    The SortDescriptionCollection may throw NullReferenceException when remove the item
    https://github.com/dotnet/wpf/issues/7389

---
+++ LechelaneHenayfucee/LechelaneHenayfucee/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LechelaneHenayfucee;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var collectionViewSource = new CollectionViewSource()
+        {
+            Source = new List<Foo>(),
+            IsLiveSortingRequested = true,
+        };
+
+        var collectionView = collectionViewSource.View;
+        _collectionView = collectionView;
+
+        collectionView.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Descending));
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private readonly ICollectionView _collectionView;
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        GC.Collect();
+        GC.WaitForFullGCComplete();
+        GC.Collect();
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        _collectionView.SortDescriptions.Clear();
+    }
+}
+
+public class Foo
+{
+    public string? Name { set; get; }
+}
