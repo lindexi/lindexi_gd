a57b7c9a440be2aa549f4284184c84927fbd23b0
lindexi <lindexi_gd@163.com>
2023-01-13 15:09:20 +0800

    拆分代码

--- LightTextEditorPlus/LightTextEditorPlus/WpfFramework_/Document/Property_/RunProperty_/RunPropertyCreator.cs
+++ LightTextEditorPlus/LightTextEditorPlus/WpfFramework_/Document/Property_/RunProperty_/RunPropertyCreator.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
 using LightTextEditorPlus.Core.Document;
@@ -28,39 +26,4 @@ class RunPropertyCreator : PlatformRunPropertyCreatorBase<RunProperty>
     }
 
     private readonly RunPropertyPlatformManager _runPropertyPlatformManager = new RunPropertyPlatformManager();
-}
-
-/// <summary>
-/// 用来管理本地资源，包括画刷或者是字体等
-/// </summary>
-class RunPropertyPlatformManager
-{
-    public GlyphTypeface GetGlyphTypeface(RunProperty runProperty)
-    {
-        // todo 字体回滚，字体缓存
-        FontFamily fontFamily;
-        if (runProperty.FontName.IsNotDefineFontName)
-        {
-            fontFamily = new FontFamily("微软雅黑");
-        }
-        else
-        {
-            fontFamily = new FontFamily(runProperty.FontName.UserFontName);
-        }
-
-        var collection = fontFamily.GetTypefaces();
-        Typeface typeface = collection.First();
-
-        foreach (var t in collection)
-        {
-            if (t.Stretch == runProperty.Stretch && t.Weight == runProperty.Weight)
-            {
-                typeface = t;
-                break;
-            }
-        }
-
-        bool success = typeface.TryGetGlyphTypeface(out var glyphTypeface);
-        return glyphTypeface;
-    }
 }
---
+++ LightTextEditorPlus/LightTextEditorPlus/WpfFramework_/Document/Property_/RunProperty_/RunPropertyPlatformManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Windows.Media;
+
+namespace LightTextEditorPlus.Document;
+
+/// <summary>
+/// 用来管理本地资源，包括画刷或者是字体等
+/// </summary>
+class RunPropertyPlatformManager
+{
+    public GlyphTypeface GetGlyphTypeface(RunProperty runProperty)
+    {
+        // todo 字体回滚，字体缓存
+        FontFamily fontFamily;
+        if (runProperty.FontName.IsNotDefineFontName)
+        {
+            fontFamily = new FontFamily("微软雅黑");
+        }
+        else
+        {
+            fontFamily = new FontFamily(runProperty.FontName.UserFontName);
+        }
+
+        var collection = fontFamily.GetTypefaces();
+        Typeface typeface = collection.First();
+
+        foreach (var t in collection)
+        {
+            if (t.Stretch == runProperty.Stretch && t.Weight == runProperty.Weight)
+            {
+                typeface = t;
+                break;
+            }
+        }
+
+        bool success = typeface.TryGetGlyphTypeface(out var glyphTypeface);
+        return glyphTypeface;
+    }
+}
