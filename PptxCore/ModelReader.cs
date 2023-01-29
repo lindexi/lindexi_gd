8b1df5a31fc1853b2d3c50393e05c158d1b9d628
lindexi <lindexi_gd@163.com>
2023-01-29 17:06:46 +0800

    加上测试 Ipc 代码

--- PptxCore/ModelReader.cs
+++ PptxCore/ModelReader.cs
@@ -15,6 +15,59 @@ using ShapeProperties = DocumentFormat.OpenXml.Presentation.ShapeProperties;
 
 namespace PptxCore;
 
+class B
+{
+    public void F1()
+    {
+        var ipcShellGexxx = new IpcShellGexxx(GetXx);
+    }
+}
+
+interface IXxxxCrea<T>
+{
+    Task<T> GetTAsync();
+}
+
+interface IBusiIpc
+{
+    Task Fxx();
+}
+
+class IpcShellGexxx : XxBase<IBusiIpc>, IBusiIpc
+{
+    public IpcShellGexxx(IXxxxCrea<IBusiIpc> creator) : base(creator)
+    {
+    }
+
+    public async Task Fxx()
+    {
+       var a =  await base.GetTAsync();
+       await a.Fxx();
+    }
+}
+
+class XxBase<T>
+{
+    public IXxxxCrea<T> Creator { get; }
+
+    public XxBase(IXxxxCrea<T> creator)
+    {
+        Init(creator);
+    }
+
+    public void Init(IXxxxCrea<T> creator)
+    {
+
+    }
+
+    protected async Task<T> GetTAsync()
+    {
+        return await Creator.GetTAsync();
+    }
+
+    private T? A { set; get; }
+}
+
 /// <summary>
 /// 评估结果
 /// </summary>
@@ -76,12 +129,65 @@ readonly record struct OpenXmlElementEvaluationScore(double Score, double Weight
 
 class OpenXmlConverterEvaluator
 {
-    public void Evaluate(OpenXmlElement element)
+    public void Evaluate(OpenXmlElement inputElement)
     {
+        OpenXmlElement? element = inputElement;
+        var unsupportedAttributeList = new List<OpenXmlAttribute>();
+
+        {
+            bool isFirst = true;
+            var isLeafElement = false;
+            var finishChild = false;
+
+            while (element is not null)
+            {
+                // 处理元素本身
+                var evaluationArgument = new EvaluationArgument(element, OpenXmlAttribute: null);
+
+                var evaluationResult = Handle(evaluationArgument);
+
+                // 如果元素本身不能处理，那就继续处理下一层的元素，直到元素属于叶子。依然没有任何处理，记录没有处理
+
+                var firstChild = element.FirstChild;
+                isLeafElement = firstChild is not null;
+
+                if (isLeafElement && !finishChild)
+                {
+                    element = firstChild;
+
+                    continue;
+                }
+                else
+                {
+                    finishChild = false;
+
+                    // 这是叶子了
+
+                    // 处理相同的一个元素下的其他元素
+                    var elementAfter = element.ElementsAfter().FirstOrDefault();
+
+                    if (elementAfter is not null)
+                    {
+                        element = elementAfter;
+
+                        continue;
+                    }
+                    else
+                    {
+                        // 这个元素完成了
+                        element = element.Parent;
+
+                        finishChild = true;
+
+                        continue;
+                    }
+                }
+            }
+        }
+
         var stack = new Stack<OpenXmlElement>();
         stack.Push(element);
 
-        var unsupportedAttributeList = new List<OpenXmlAttribute>();
 
         while (stack.TryPop(out var currentElement))
         {
