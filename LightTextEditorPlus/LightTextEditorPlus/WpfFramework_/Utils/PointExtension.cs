8d3a558b69a1a8402235be8dd1aab4308e45034b
lindexi <lindexi_gd@163.com>
2023-01-31 17:03:03 +0800

    加上辅助方法

---
+++ LightTextEditorPlus/LightTextEditorPlus/WpfFramework_/Utils/PointExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace LightTextEditorPlus.Utils;
+
+public static class PointExtension
+{
+    public static Point ToWpfPoint(this LightTextEditorPlus.Core.Primitive.Point point) => new Point(point.X, point.Y);
+}
---
+++ LightTextEditorPlus/LightTextEditorPlus/WpfFramework_/Utils/SizeExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace LightTextEditorPlus.Utils;
+
+public static class SizeExtension
+{
+    public static Size ToWpfSize(this LightTextEditorPlus.Core.Primitive.Size size) => new Size(size.Width, size.Height);
+}
