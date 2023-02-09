973d39cdc0300ce17075545ff0b578afc93c7a08
lindexi <lindexi_gd@163.com>
2023-02-09 17:16:55 +0800

    加上注释

--- LightTextEditorPlus/LightTextEditorPlus.Core/Layout/Context_/Platform_/WholeLineLayoutArgument.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Layout/Context_/Platform_/WholeLineLayoutArgument.cs
@@ -4,6 +4,13 @@ using LightTextEditorPlus.Core.Primitive.Collections;
 
 namespace LightTextEditorPlus.Core.Layout;
 
+/// <summary>
+/// 段内行测量布局参数
+/// </summary>
+/// <param name="ParagraphProperty"></param>
+/// <param name="CharDataList"></param>
+/// <param name="LineMaxWidth">这一行能布局的最大宽度</param>
+/// <param name="CurrentStartPoint">当前行的起始点，相对于文本框的坐标</param>
 public readonly record struct WholeLineLayoutArgument(ParagraphProperty ParagraphProperty, in ReadOnlyListSpan<CharData> CharDataList, double LineMaxWidth, Point CurrentStartPoint)
 {
     
