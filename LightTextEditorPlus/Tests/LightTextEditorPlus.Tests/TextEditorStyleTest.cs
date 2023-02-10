c66de81a97ba9702ad016a43bb86a68ec4d25c22
lindexi <lindexi_gd@163.com>
2023-02-10 12:03:29 +0800

    加上单元测试

--- LightTextEditorPlus/Tests/LightTextEditorPlus.Tests/TextEditorStyleTest.cs
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Tests/TextEditorStyleTest.cs
@@ -8,6 +8,53 @@ namespace LightTextEditorPlus.Tests;
 public class TextEditorStyleTest
 {
     [UIContractTestCase]
+    public void ChangeStyle()
+    {
+        "未选择时，修改当前光标字符属性样式，只触发 StyleChanging 和 StyleChanged 事件，不触发布局变更".Test(() =>
+        {
+            // Arrange
+            using var context = TestFramework.CreateTextEditorInNewWindow();
+            var textEditor = context.TextEditor;
+            // 只触发 StyleChanging 和 StyleChanged 事件。不用测试了，交给其他单元测试
+            textEditor.TextEditorCore.LayoutCompleted += (sender, args) =>
+            {
+                // Assert
+                // 未选择时，修改当前光标字符属性样式，只触发 StyleChanging 和 StyleChanged 事件，不触发布局变更
+                Assert.Fail();
+            };
+
+            // Action
+            textEditor.ToggleBold();
+        });
+
+        "修改样式时，先触发 StyleChanging 事件，再触发 StyleChanged 事件，且只触发一次".Test(() =>
+        {
+            // Arrange
+            using var context = TestFramework.CreateTextEditorInNewWindow();
+            var textEditor = context.TextEditor;
+
+            int count = 0;
+            textEditor.StyleChanging += (sender, args) =>
+            {
+                Assert.AreEqual(0, count);
+                count++;
+            };
+            textEditor.StyleChanged += (sender, args) =>
+            {
+                Assert.AreEqual(1, count);
+                count++;
+            };
+
+            // Action
+            textEditor.ToggleBold();
+
+            // Assert
+            // 只触发一次，一共两个事件
+            Assert.AreEqual(2, count);
+        });
+    }
+
+    [UIContractTestCase]
     public void ToggleBold()
     {
         "未选择时，调用 ToggleBold 给文本当前光标设置加粗，追加文本之后，当前光标的字符属性是加粗".Test(async () =>
