4a6de4218ee4a9d1722b8b4bbd800fc473a3927a
lindexi <lindexi_gd@163.com>
2023-02-14 14:18:19 +0800

    拆分代码

--- LightTextEditorPlus/LightTextEditorPlus.Wpf/Layout/TextView.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/Layout/TextView.cs
@@ -10,8 +10,6 @@ using LightTextEditorPlus.Core.Rendering;
 using LightTextEditorPlus.Document;
 using LightTextEditorPlus.Rendering;
 
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
-
 using Math = System.Math;
 using Rect = System.Windows.Rect;
 
@@ -283,40 +281,4 @@ class SelectionAndCaretLayer : DrawingVisual, ICaretManager, ILayer
     }
 
     #endregion
-}
-
-interface ICaretManager
-{
-    void OnTick();
-}
-
-/// <summary>
-/// 用来控制光标的 <see cref="DispatcherTimer"/> 类型，同时解决业务端忘记调用 Stop 关闭光标，从而被 <see cref="DispatcherTimer"/> 内存泄露
-/// </summary>
-class CaretBlinkDispatcherTimer : DispatcherTimer
-{
-    public CaretBlinkDispatcherTimer(ICaretManager caretManager)
-    {
-        _caretManagerWeakReference = new WeakReference<ICaretManager>(caretManager);
-
-        Tick += CaretBlinkDispatcherTimer_Tick;
-    }
-
-    private void CaretBlinkDispatcherTimer_Tick(object? sender, EventArgs e)
-    {
-        if (_caretManagerWeakReference.TryGetTarget(out var caretManager))
-        {
-            caretManager.OnTick();
-        }
-        else
-        {
-            // 被回收，忘记关闭 Timer 那就自己关闭了
-            Stop();
-        }
-    }
-
-    /// <summary>
-    /// 用来解决被 <see cref="DispatcherTimer"/> 类型引用 CaretManager 类，从而内存泄露
-    /// </summary>
-    private readonly WeakReference<ICaretManager> _caretManagerWeakReference;
 }
