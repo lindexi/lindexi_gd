cd77a7391bc4576066f7e81df941210f722ecfc0
lindexi <lindexi_gd@163.com>
2023-02-17 17:34:42 +0800

    加上获取状态

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/API/TextEditorCore.Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightTextEditorPlus.Core;
+
+// 此文件存放状态获取相关的方法
+public partial class TextEditorCore
+{
+
+}
