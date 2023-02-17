cc9763e9b2c57613d4ebd1fbb0f8e8ee2b46acf3
lindexi <lindexi_gd@163.com>
2023-02-17 15:36:35 +0800

    删掉仅布局代码

--- LightTextEditorPlus/LightTextEditorPlus.Wpf/Document/Property_/RunProperty_/Fonts_/FallBackFontFamily.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/Document/Property_/RunProperty_/Fonts_/FallBackFontFamily.cs
@@ -12,12 +12,6 @@ namespace LightTextEditorPlus.Document
     /// </summary>
     class FallBackFontFamily
     {
-#if LayoutOnly
-        private FallBackFontFamily(CultureInfo culture)
-        {
-            // 不需要做什么，理论上仅布局不会用到这个类，因为此时不会存在找不到字体
-        }
-#else
         private const string FallBackFontFamilyName = "#GLOBAL USER INTERFACE";
         public FontFamily FallBack { get; } = new FontFamily(FallBackFontFamilyName);
 
@@ -29,7 +23,6 @@ namespace LightTextEditorPlus.Document
         }
 
         private IReadOnlyList<FontFamilyMapItem> FontFamilyItems { get; }
-#endif
 
         /// <summary>
         /// 获取<see cref="FallBackFontFamily"/>对象的单例
@@ -48,10 +41,6 @@ namespace LightTextEditorPlus.Document
       
         public bool TryGetFallBackFontFamily(char unicodeChar,[NotNullWhen(true)] out string? familyName)
         {
-#if LayoutOnly
-            familyName = TextContext.DefaultFontFamily.Source;
-            return true;
-#else
             var mapItem = GetFontFamilyMapItem(unicodeChar);
             familyName = null;
 
@@ -62,7 +51,6 @@ namespace LightTextEditorPlus.Document
             }
 
             return false;
-#endif
         }
 
         public FontFamilyMapItem? GetFontFamilyMapItem(char unicodeChar)
