dba4a8846b5258c647d1185cc9aba3bf020998c8
lindexi <lindexi_gd@163.com>
2023-02-23 17:17:09 +0800

    加上单元测试

--- LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/Rendering/CaretRenderInfoTest.cs
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/Rendering/CaretRenderInfoTest.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LightTextEditorPlus.Core.Primitive;
 using LightTextEditorPlus.Core.TestsFramework;
 
 namespace LightTextEditorPlus.Core.Tests.Rendering;
@@ -12,6 +13,31 @@ namespace LightTextEditorPlus.Core.Tests.Rendering;
 [TestClass]
 public class CaretRenderInfoTest
 {
+    [ContractTestCase]
+    public void GetCaretRenderInfo()
+    {
+        "获取第二段的文本光标信息，在文本第二段是空段时，可以获取到正确的渲染信息".Test(() =>
+        {
+            // Arrange
+            // 这是空文本，没有任何的布局等信息
+            // 采用 FixCharSizePlatformProvider 固定数值
+            var textEditorCore = new TextEditorCore(new FixCharSizePlatformProvider());
+
+            // Action
+            // 追加两段文本，其中第二段是空白，这样就可以获取在文本第二段是空段时的光标
+            textEditorCore.AppendText("123\r\n");
+
+            // Assert
+            // 由于追加两段文本，刚好当前光标就是测试所需的光标
+            var renderInfoProvider = textEditorCore.GetRenderInfo();
+            Assert.IsNotNull(renderInfoProvider);
+            var caretRenderInfo = renderInfoProvider.GetCaretRenderInfo(textEditorCore.CurrentCaretOffset);
+            Assert.AreEqual(true, caretRenderInfo.IsEmptyParagraph);
+            // 一行的高度是 15 也就是第二行就是 (0,15) 的
+            Assert.AreEqual(new Rect(0, 15, 0, 15), caretRenderInfo.LineBounds);
+        });
+    }
+
     [ContractTestCase]
     public void GetCaretRenderInfoEmptyTextEditor()
     {
