a15480742cc62dc7b74aeed382c8b27d6669ab5b
lindexi <lindexi_gd@163.com>
2023-02-23 09:40:16 +0800

    加上单元测试

---
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/Rendering/CaretRenderInfoTest.cs
@@ -0,0 +1,34 @@
+﻿using MSTest.Extensions.Contracts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightTextEditorPlus.Core.TestsFramework;
+
+namespace LightTextEditorPlus.Core.Tests.Rendering;
+
+[TestClass]
+public class CaretRenderInfoTest
+{
+    [ContractTestCase]
+    public void GetCaretRenderInfoEmptyTextEditor()
+    {
+        "对空文本获取光标信息，可以获取到渲染信息".Test(() =>
+        {
+            // Arrange
+            var testPlatformProvider = new TestPlatformProvider();
+            // 这是空文本，没有任何的布局等信息
+            var textEditorCore = new TextEditorCore(testPlatformProvider);
+
+            // Action
+            var renderInfoProvider = textEditorCore.GetRenderInfo();
+            Assert.IsNotNull(renderInfoProvider);
+
+            // Assert
+            var caretRenderInfo = renderInfoProvider.GetCaretRenderInfo(textEditorCore.CurrentCaretOffset);
+            Assert.AreEqual(textEditorCore.CurrentCaretOffset, caretRenderInfo.CaretOffset);
+        });
+    }
+}
