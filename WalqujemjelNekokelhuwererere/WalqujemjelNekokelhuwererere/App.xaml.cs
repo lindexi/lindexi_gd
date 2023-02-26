f006732c9f97f370f5c063de024125b201313bd3
lindexi <lindexi_gd@163.com>
2023-02-26 10:20:48 +0800

    测试触摸的性能

---
+++ WalqujemjelNekokelhuwererere/WalqujemjelNekokelhuwererere/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WalqujemjelNekokelhuwererere;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        AppContext.SetSwitch("Switch.System.Windows.Input.Stylus.DisableStylusAndTouchSupport", true);
+    }
+}
