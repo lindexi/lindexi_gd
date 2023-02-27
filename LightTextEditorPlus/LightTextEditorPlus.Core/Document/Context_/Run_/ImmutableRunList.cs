c06f2160ea7f9e797988a70780acaff02f50492f
lindexi <lindexi_gd@163.com>
2023-02-27 15:30:50 +0800

    加上列表

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/Context_/Run_/ImmutableRunList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LightTextEditorPlus.Core.Document;
+
+class ImmutableRunList : List<IImmutableRun>, IImmutableRunList
+{
+    public ImmutableRunList()
+    {
+    }
+
+    public ImmutableRunList(IEnumerable<IImmutableRun> collection) : base(collection)
+    {
+    }
+
+    public ImmutableRunList(int capacity) : base(capacity)
+    {
+    }
+
+    public int RunCount => Count;
+    public IImmutableRun GetRun(int index) => this[index];
+}
