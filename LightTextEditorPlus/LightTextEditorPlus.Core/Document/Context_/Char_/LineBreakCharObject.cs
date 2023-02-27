42221886d29974370089e9f2ec51d3587754b803
lindexi <lindexi_gd@163.com>
2023-02-27 17:36:48 +0800

    加上换行字符

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/Context_/Char_/LineBreakCharObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using LightTextEditorPlus.Core.Utils;
+
+namespace LightTextEditorPlus.Core.Document;
+
+/// <summary>
+/// 表示一个换行的字符
+/// </summary>
+public class LineBreakCharObject : ICharObject
+{
+    bool IEquatable<string>.Equals(string? other)
+    {
+        if (string.Equals(other, "\r\n"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    ICharObject IDeepCloneable<ICharObject>.DeepClone()
+    {
+        return this;
+    }
+
+    string ICharObject.ToText() => TextContext.NewLine;
+}
