e24633ab99ebc5a1def7204d4d4595bc582c7d1d
lindexi <lindexi_gd@163.com>
2023-03-03 19:47:45 +0800

    没有触发 MeasureOverride 和 Loaded 事件
    F1 MeasureOverride
    F1_Loaded

---
+++ KearkemnerwhayneqiChaywibelfo/KearkemnerwhayneqiChaywibelfo/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KearkemnerwhayneqiChaywibelfo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+
+class F1 : FrameworkElement
+{
+    public F1()
+    {
+        Width = 500;
+        Height = 500;
+
+        F2 = new F2();
+
+        Loaded += F1_Loaded;
+    }
+
+    private F2 F2 { get; }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        Debug.WriteLine("F1 MeasureOverride");
+        return base.MeasureOverride(availableSize);
+    }
+
+    private void F1_Loaded(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine(nameof(F1_Loaded));
+    }
+}
+
+class F2 : FrameworkElement
+{
+    public F2()
+    {
+        Width = 500;
+        Height = 500;
+
+        Loaded += F2_Loaded;
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        Debug.WriteLine("F2 MeasureOverride");
+        return base.MeasureOverride(availableSize);
+    }
+
+    private void F2_Loaded(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine(nameof(F2_Loaded));
+    }
+}
