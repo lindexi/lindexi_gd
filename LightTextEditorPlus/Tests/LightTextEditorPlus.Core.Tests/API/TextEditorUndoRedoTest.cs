9f3da20c30b86fed18f27107560cd668cc422123
lindexi <lindexi_gd@163.com>
2023-03-20 10:58:15 +0800

    加上更多测试

--- LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/API/TextEditorUndoRedoTest.cs
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/API/TextEditorUndoRedoTest.cs
@@ -1,4 +1,5 @@
-﻿using LightTextEditorPlus.Core.Document;
+﻿using System.Reflection;
+using LightTextEditorPlus.Core.Document;
 using LightTextEditorPlus.Core.Primitive;
 using LightTextEditorPlus.Core.TestsFramework;
 
@@ -9,6 +10,55 @@ namespace LightTextEditorPlus.Core.Tests;
 [TestClass]
 public class TextEditorUndoRedoTest
 {
+    [ContractTestCase]
+    public void SetUndoRedoEnable()
+    {
+        "设置文本禁用撤销重做，后又开启撤销重做，开启后文本追加可以加入撤销恢复".Test(() =>
+        {
+            // Arrange
+            var testTextEditorUndoRedoProvider = new TestTextEditorUndoRedoProvider();
+            var textEditorCore = TestHelper.GetTextEditorCore(new TestPlatformProvider()
+            {
+                UndoRedoProvider = testTextEditorUndoRedoProvider
+            });
+
+            // Action
+            // 设置文本禁用撤销重做
+            textEditorCore.SetUndoRedoEnable(false, "Test");
+            // 文本追加等将不会加入撤销恢复
+            textEditorCore.AppendText(TestHelper.PlainNumberText);
+
+            // 重新开启撤销重做
+            textEditorCore.SetUndoRedoEnable(true, "Test");
+            // 再次追加内容，预期追加的内容加入到撤销重做
+            textEditorCore.AppendText(TestHelper.PlainNumberText);
+
+            // Assert
+            // 只加入一条到撤销恢复
+            Assert.AreEqual(1, testTextEditorUndoRedoProvider.UndoOperationList.Count);
+        });
+
+        "设置文本禁用撤销重做，文本追加等将不会加入撤销恢复".Test(() =>
+        {
+            // Arrange
+            var testTextEditorUndoRedoProvider = new TestTextEditorUndoRedoProvider();
+            var textEditorCore = TestHelper.GetTextEditorCore(new TestPlatformProvider()
+            {
+                UndoRedoProvider = testTextEditorUndoRedoProvider
+            });
+
+            // Action
+            // 设置文本禁用撤销重做
+            textEditorCore.SetUndoRedoEnable(false, "Test");
+            // 文本追加等将不会加入撤销恢复
+            textEditorCore.AppendText(TestHelper.PlainNumberText);
+
+            // Assert
+            // 不会加入撤销恢复，也就是撤销恢复空列表
+            Assert.AreEqual(0, testTextEditorUndoRedoProvider.UndoOperationList.Count);
+        });
+    }
+
     [ContractTestCase]
     public void UndoRedo()
     {
@@ -21,14 +71,16 @@ public class TextEditorUndoRedoTest
                 UndoRedoProvider = testTextEditorUndoRedoProvider
             });
 
+            // Action
             // 追加带样式的文本
             const string fontName = "测试用的字体";
             var platformRunPropertyCreator = textEditorCore.PlatformProvider.GetPlatformRunPropertyCreator();
-            var runProperty = platformRunPropertyCreator.BuildNewProperty(t => ((LayoutOnlyRunProperty)t).FontName = new FontName(fontName),
+            var runProperty = platformRunPropertyCreator.BuildNewProperty(t => ((LayoutOnlyRunProperty) t).FontName = new FontName(fontName),
                 platformRunPropertyCreator.GetDefaultRunProperty());
 
             textEditorCore.AppendRun(new TextRun(TestHelper.PlainNumberText, runProperty));
 
+            // Assert
             // 可以通过撤销撤回更改
             Assert.AreEqual(1, testTextEditorUndoRedoProvider.UndoOperationList.Count);
 
@@ -38,9 +90,11 @@ public class TextEditorUndoRedoTest
             // 撤销完成，那就是空文本了
             Assert.AreEqual(0, textEditorCore.DocumentManager.CharCount);
 
+            // Action
             // 通过恢复加上原本样式的文本
             testTextEditorUndoRedoProvider.Redo();
 
+            // Assert
             var runPropertyList = textEditorCore.DocumentManager.GetRunPropertyRange(textEditorCore.GetAllDocumentSelection()).ToList();
             Assert.AreEqual(3, runPropertyList.Count);
             Assert.AreEqual(fontName, runPropertyList[0].FontName.UserFontName);
@@ -73,7 +127,7 @@ public class TextEditorUndoRedoTest
 
             // 继续重做，那就是存在文本
             testTextEditorUndoRedoProvider.Redo();
-            Assert.AreEqual(TestHelper.PlainNumberText,textEditorCore.GetText());
+            Assert.AreEqual(TestHelper.PlainNumberText, textEditorCore.GetText());
         });
     }
 }
