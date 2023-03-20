72158ae1dbe4cd913de3133de8c4e077fa43c18a
lindexi <lindexi_gd@163.com>
2023-03-20 15:41:18 +0800

    拆分文件

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/Context_/Property_/RunProperty_/LayoutOnlyRunProperty.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/Context_/Property_/RunProperty_/LayoutOnlyRunProperty.cs
@@ -1,59 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using LightTextEditorPlus.Core.Primitive;
 using LightTextEditorPlus.Core.Utils.Maths;
 
 namespace LightTextEditorPlus.Core.Document
 {
-    /// <summary>
-    /// 只读的文本字符属性
-    /// </summary>
-    public interface IReadOnlyRunProperty : IEquatable<IReadOnlyRunProperty>
-    {
-        /// <summary>
-        /// 字体大小
-        /// </summary>
-        /// 没有明确的属性，交给文本业务层。有些使用像素、有些使用磅
-        double FontSize { get; }
-
-        /// <summary>
-        /// 用户设置的字体名
-        /// </summary>
-        /// 非底层找不到字体而进行回滚的字体
-        /// 
-        /// 在 Word 里面，可以同时设置一个文本 Run 的中文使用一个字体，西文使用一个字体
-        /// 虽然 Word 这么做看起来不错，但是也存在设计无解的问题，例如西文字体的行高比中文字体的高
-        /// 此时用户在输入中文，输入法先输入的是拼音，使用西文字体，此时行高变更，接着用户完成打字
-        /// 输入法修改输入为中文，使用中文字体，于是行高再次变更，可以看到行高就在跳动
-        /// 大部分的中文字体都有带英文字符，那不如就依然是单个字体
-        FontName FontName { get; }
-
-        /// <summary>
-        /// 尝试获取属性
-        /// </summary>
-        /// <param name="propertyName"></param>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        bool TryGetProperty(string propertyName, [NotNullWhen(true)] out IImmutableRunPropertyValue? value);
-    }
-
-    /// <summary>
-    /// 平台相关的字符属性，用于给各个平台继承，实现其特定属性。要求此属性是不可变的
-    /// </summary>
-    public class PlatformImmutableRunProperty<T> : LayoutOnlyRunProperty
-        where T : PlatformImmutableRunProperty<T>
-    {
-    }
-
-    /// <summary>
-    /// 用来限制某个类型不能被其他程序集继承
-    /// </summary>
-    /// [C# 如何写出一个不能被其他程序集继承的抽象类](https://blog.lindexi.com/post/C-%E5%A6%82%E4%BD%95%E5%86%99%E5%87%BA%E4%B8%80%E4%B8%AA%E4%B8%8D%E8%83%BD%E8%A2%AB%E5%85%B6%E4%BB%96%E7%A8%8B%E5%BA%8F%E9%9B%86%E7%BB%A7%E6%89%BF%E7%9A%84%E6%8A%BD%E8%B1%A1%E7%B1%BB.html )
-    internal interface ICanNotInheritance
-    {
-    }
-
     /// <summary>
     /// 仅布局支持的文本字符属性
     /// </summary>
