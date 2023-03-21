a8f3ea9982a1cd374940a13f89480424a00669d3
lindexi <lindexi_gd@163.com>
2023-03-21 09:00:17 +0800

    测试热重载

---
+++ NeedeanarkawFudarkalwi/NeedeanarkawFudarkalwi/Foo.cs
@@ -0,0 +1,25 @@
+﻿using NeedeanarkawFudarkalwi;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: MetadataUpdateHandler(typeof(HotReloadMetadataUpdateHandler))]
+
+namespace NeedeanarkawFudarkalwi;
+public class HotReloadMetadataUpdateHandler
+{
+    public static void ClearCache(Type[]? updatedTypes)
+    { 
+        HotReload++;
+    }
+
+    public static void UpdateApplication(Type[]? updatedTypes)
+    {
+        Console.WriteLine("HotReloadManager.UpdateApplication");
+    }
+
+    public static int HotReload { set; get; }
+}
---
+++ NeedeanarkawFudarkalwi/NeedeanarkawFudarkalwi/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NeedeanarkawFudarkalwi;
+
+var f2 = new F2();
+
+int n = f2.Foo();
+
+n = HotReloadMetadataUpdateHandler.HotReload;
+
+Console.WriteLine(n);
+
+Console.ReadLine();
+
+class F2
+{
+    public int Foo()
+    {
+        var n = 2;
+        return n;
+    }
+}
