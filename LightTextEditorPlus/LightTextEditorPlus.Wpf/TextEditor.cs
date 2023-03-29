b5b8d7330b8ca86d2a9ffbcd7c9e2fda36179d82
lindexi <lindexi_gd@163.com>
2023-03-29 14:58:02 +0800

    拆分文件

--- LightTextEditorPlus/LightTextEditorPlus.Wpf/TextEditor.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/TextEditor.cs
@@ -9,7 +9,6 @@ using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Markup;
@@ -376,22 +375,6 @@ public partial class TextEditor : FrameworkElement, IRenderManager, IIMETextEdit
     // ReSharper disable once IdentifierTypo
     private string _emojiCache = string.Empty;
     #endregion
-}
-
-/// <summary>
-/// 命中测试方式
-/// </summary>
-public enum TextEditorHitTestMode
-{
-    /// <summary>
-    /// 只有在文本框范围内，都视为命中。行为如 <see cref="TextBox"/> 这些
-    /// </summary>
-    All,
-
-    /// <summary>
-    /// 只有命中到有文字的地方才算命中到。也许没有什么业务会用到
-    /// </summary>
-    HitCharOnly,
 }
 
 internal class TextEditorPlatformProvider : PlatformProvider
