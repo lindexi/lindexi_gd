55aa84a041b5f9e3446a646662fc079695783e81
lindexi <lindexi_gd@163.com>
2023-04-17 20:34:26 +0800

    形状识别功能

---
+++ WekihairfawKudurnearka/WekihairfawKudurnearka/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Windows.Foundation;
+using Windows.UI.Input.Inking;
+using Windows.UI.Input.Inking.Analysis;
+
+using Point = Windows.Foundation.Point;
+
+namespace WekihairfawKudurnearka;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void InkCanvas_OnStrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+    {
+        var inkStrokeBuilder = new InkStrokeBuilder();
+        var inkStroke = inkStrokeBuilder.CreateStroke(e.Stroke.StylusPoints.Select(t => new Point(t.X, t.Y)));
+        var inkAnalyzer = new InkAnalyzer();
+        inkAnalyzer.AddDataForStroke(inkStroke);
+        var result = await inkAnalyzer.AnalyzeAsync();
+        foreach (IInkAnalysisNode inkAnalysisNode in inkAnalyzer.AnalysisRoot.FindNodes(InkAnalysisNodeKind.InkDrawing))
+        {
+            var inkAnalysisInkDrawing = inkAnalysisNode as InkAnalysisInkDrawing;
+            var value = inkAnalysisInkDrawing?.DrawingKind;
+            if (value == InkAnalysisDrawingKind.Triangle)
+            {
+
+            }
+        }
+    }
+}
