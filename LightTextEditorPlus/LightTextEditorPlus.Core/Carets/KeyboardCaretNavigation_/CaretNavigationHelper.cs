2817c444c03c4b7c8465b00ba4e5360aaa443160
lindexi <lindexi_gd@163.com>
2023-04-24 15:58:17 +0800

    加上光标导航辅助类

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Carets/KeyboardCaretNavigation_/CaretNavigationHelper.cs
@@ -0,0 +1,20 @@
+﻿using TextArea = LightTextEditorPlus.Core.TextEditorCore; // 这个只是保持和旧代码相同的命名而已 
+
+namespace LightTextEditorPlus.Core.Carets;
+
+/// <summary>
+/// 光标导航辅助类
+/// </summary>
+public static class CaretNavigationHelper
+{
+    /// <summary>
+    /// 根据键盘操作获取光标导航
+    /// </summary>
+    /// <param name="textArea"></param>
+    /// <param name="caretMoveType"></param>
+    /// <returns></returns>
+    public static CaretOffset GetNewCaretOffset(TextArea textArea, CaretMoveType caretMoveType)
+    {
+        throw null;
+    }
+}
