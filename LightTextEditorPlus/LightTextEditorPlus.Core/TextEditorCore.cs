3773183118eda501c4cfb329e4a97cffa0cedc87
lindexi <lindexi_gd@163.com>
2023-04-25 17:15:40 +0800

    减少文件数量

--- LightTextEditorPlus/LightTextEditorPlus.Core/TextEditorCore.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/TextEditorCore.cs
@@ -386,6 +386,34 @@ public partial class TextEditorCore
         get => CaretManager.CurrentSelection;
     }
 
+    /// <summary>
+    /// 移动光标。如已知 <see cref="CaretOffset"/> 可直接给 <see cref="CurrentCaretOffset"/> 属性赋值
+    /// </summary>
+    /// <param name="type"></param>
+    [TextEditorPublicAPI]
+    public void MoveCaret(CaretMoveType type)
+    {
+        var caretOffset = GetNewCaretOffset(type);
+        CaretManager.CurrentCaretOffset = caretOffset;
+    }
+
+    /// <summary>
+    /// 移动光标
+    /// </summary>
+    /// <param name="caretOffset"></param>
+    [Obsolete("如已知 CaretOffset 可直接给 CurrentCaretOffset 属性赋值。此方法仅仅只是用来告诉你正确的方法应该是给 CurrentCaretOffset 属性赋值")]
+    public void MoveCaret(CaretOffset caretOffset) => CaretManager.CurrentCaretOffset = caretOffset;
+
+    /// <summary>
+    /// 根据键盘操作获取光标导航
+    /// </summary>
+    /// <param name="caretMoveType"></param>
+    /// <returns></returns>
+    public CaretOffset GetNewCaretOffset(CaretMoveType caretMoveType)
+    {
+        return KeyboardCaretNavigationHelper.GetNewCaretOffset(this, caretMoveType);
+    }
+
     #endregion
 
     #region 状态属性
