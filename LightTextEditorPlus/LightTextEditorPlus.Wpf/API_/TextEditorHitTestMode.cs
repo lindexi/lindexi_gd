b3607ed1e10e04c1535ac1b35d3af18970f80e51
lindexi <lindexi_gd@163.com>
2023-04-28 16:29:33 +0800

    废弃代码

--- LightTextEditorPlus/LightTextEditorPlus.Wpf/API_/TextEditorHitTestMode.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/API_/TextEditorHitTestMode.cs
@@ -1,20 +1,19 @@
-﻿using System.Windows.Controls;
+﻿//using System.Windows.Controls;
 
-namespace LightTextEditorPlus;
+//namespace LightTextEditorPlus;
 
-/// <summary>
-/// 命中测试方式
-/// </summary>
-/// todo 考虑是否需要此类型
-enum TextEditorHitTestMode
-{
-    /// <summary>
-    /// 只有在文本框范围内，都视为命中。行为如 <see cref="TextBox"/> 这些
-    /// </summary>
-    All,
+///// <summary>
+///// 命中测试方式
+///// </summary>
+//enum TextEditorHitTestMode
+//{
+//    /// <summary>
+//    /// 只有在文本框范围内，都视为命中。行为如 <see cref="TextBox"/> 这些
+//    /// </summary>
+//    All,
 
-    /// <summary>
-    /// 只有命中到有文字的地方才算命中到。也许没有什么业务会用到
-    /// </summary>
-    HitCharOnly,
-}
+//    /// <summary>
+//    /// 只有命中到有文字的地方才算命中到。也许没有什么业务会用到
+//    /// </summary>
+//    HitCharOnly,
+//}
