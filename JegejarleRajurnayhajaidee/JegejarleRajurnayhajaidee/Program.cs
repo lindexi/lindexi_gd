4cb2d3c557122601447d5b4a1e58587650535bb7
lindexi <lindexi_gd@163.com>
2023-05-12 10:27:02 +0800

    获取解码器

---
+++ JegejarleRajurnayhajaidee/JegejarleRajurnayhajaidee/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+
+using System.Runtime.CompilerServices;
+using System.Security;
+using stakx.WIC;
+
+
+namespace JegejarleRajurnayhajaidee;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        WICImagingFactory factory = new WICImagingFactory();
+        var componentEnumerator = factory.CreateComponentEnumerator(WICComponentType.WICDecoder,WICComponentEnumerateOptions.WICComponentEnumerateDefault);
+
+        foreach (var o in componentEnumerator.AsEnumerable())
+        {
+            IWICBitmapCodecInfo codecInfo = o as IWICBitmapCodecInfo;
+            if (codecInfo != null)
+            {
+                Console.WriteLine("----------");
+                Console.WriteLine($"CLSID: {codecInfo.GetCLSID()}");
+                Console.WriteLine(codecInfo.GetFriendlyName());
+                Console.WriteLine($"FileExtensions: {string.Join(";", codecInfo.GetFileExtensions())}");
+                Console.WriteLine($"MimeType: {string.Join(";", codecInfo.GetMimeTypes())}");
+                Console.WriteLine($"Version: {codecInfo.GetVersion()}");
+                Console.WriteLine("----------");
+            }
+        }
+    }
+}
