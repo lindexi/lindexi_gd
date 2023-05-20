615c235ce34b8c38abe1e99e65a5e34ddc9addb0
lindexi <lindexi_gd@163.com>
2023-05-20 22:49:58 +0800

    设置有趣的效果

--- VorticeD2DEffect1/VorticeD2DEffect1/Program.cs
+++ VorticeD2DEffect1/VorticeD2DEffect1/Program.cs
@@ -272,6 +272,8 @@ class Program
                 using ID2D1Effect d2dEffect = new ID2D1Effect(gaussianBlurEffect);
 
                 d2dEffect.SetInput(0, intputBitmap,new RawBool(true));
+                const int D2D1_GAUSSIANBLUR_PROP_STANDARD_DEVIATION = 0;
+                d2dEffect.SetValue(D2D1_GAUSSIANBLUR_PROP_STANDARD_DEVIATION, count / 60f * 3f);
 
                 //renderTarget.DrawBitmap(intputBitmap);
                 renderTarget.DrawImage(d2dEffect);
