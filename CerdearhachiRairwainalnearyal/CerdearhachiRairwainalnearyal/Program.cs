d11899ac336175e43ca3c686405bca9fb1b5eb43
lindexi <lindexi_gd@163.com>
2023-06-12 19:45:07 +0800

    简化测试的代码

--- CerdearhachiRairwainalnearyal/CerdearhachiRairwainalnearyal/Program.cs
+++ CerdearhachiRairwainalnearyal/CerdearhachiRairwainalnearyal/Program.cs
@@ -12,30 +12,10 @@ for (int i = 0; i < int.MaxValue; i++)
     var dictionary = new ConcurrentDictionary<Foo, int>();
     dictionary.TryAdd(new Foo(), i);
     dictionary.TryAdd(new Foo(), i + 1);
-    dictionary.TryAdd(new Foo(), i + 2);
-
     var first = dictionary.FirstOrDefault();
     if (first.Value != i)
     {
         Debugger.Break();
-    }
-    else
-    {
-        first = dictionary.FirstOrDefault();
-
-        if (Random.Shared.Next(10) == 1)
-        {
-            if (list.Count > 100)
-            {
-                while (list.Count>50)
-                {
-                    list.RemoveAt(0);
-                }
-                list.RemoveAt(0);
-            }
-
-            list.Add(first.Key);
-        }
     }
 }
 
