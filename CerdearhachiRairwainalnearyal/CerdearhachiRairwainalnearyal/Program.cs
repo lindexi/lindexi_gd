ae4836216d59ce3679b9d2b20b44bbf82fe19784
lindexi <lindexi_gd@163.com>
2023-06-12 19:48:26 +0800

    整理代码用来说明不是首个加入的元素

--- CerdearhachiRairwainalnearyal/CerdearhachiRairwainalnearyal/Program.cs
+++ CerdearhachiRairwainalnearyal/CerdearhachiRairwainalnearyal/Program.cs
@@ -1,21 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Collections.Concurrent;
-using System.Diagnostics;
-
-Console.WriteLine("Hello, World!");
-
-var list = new List<Foo>();
 
 for (int i = 0; i < int.MaxValue; i++)
 {
     var dictionary = new ConcurrentDictionary<Foo, int>();
     dictionary.TryAdd(new Foo(), i);
     dictionary.TryAdd(new Foo(), i + 1);
+
     var first = dictionary.FirstOrDefault();
     if (first.Value != i)
     {
-        Debugger.Break();
+        Console.WriteLine(i);
+        i = 0;
     }
 }
 
