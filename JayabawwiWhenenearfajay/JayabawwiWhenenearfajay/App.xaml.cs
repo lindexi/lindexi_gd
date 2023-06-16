abef940468bef7af6bd9ceed8566229aafda5016
lindexi <lindexi_gd@163.com>
2023-06-16 16:11:52 +0800

    后加入的资源字典影响了资源获取

---
+++ JayabawwiWhenenearfajay/JayabawwiWhenenearfajay/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JayabawwiWhenenearfajay;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var resourceDictionary = new ResourceDictionary()
+        {
+            Source = new Uri("/Dictionary1.xaml", UriKind.RelativeOrAbsolute)
+        };
+
+        Resources.MergedDictionaries.Add(resourceDictionary);
+
+        base.OnStartup(e);
+    }
+}
