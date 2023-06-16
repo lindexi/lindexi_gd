cf93266c7077a9b4acea939ce198bd7a8abe6536
lindexi <lindexi_gd@163.com>
2023-06-16 17:03:07 +0800

    尝试读取资源

--- JayabawwiWhenenearfajay/JayabawwiWhenenearfajay/App.xaml.cs
+++ JayabawwiWhenenearfajay/JayabawwiWhenenearfajay/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Resources;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -15,6 +16,9 @@ public partial class App : Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
+        // 注释这句话试试
+        var value = ((System.Windows.Setter) (Resources.MergedDictionaries)[1].Values.OfType<Style>().First().Setters[0]).Value;
+
         var resourceDictionary = new ResourceDictionary()
         {
             Source = new Uri("/Dictionary1.xaml", UriKind.RelativeOrAbsolute)
