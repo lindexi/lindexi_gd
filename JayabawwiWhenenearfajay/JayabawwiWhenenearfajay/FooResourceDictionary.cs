ac01fffe3908bcf5b69b459e1d3a6e50aa207b9c
lindexi <lindexi_gd@163.com>
2023-06-16 16:24:05 +0800

    只有在获取时才展开

---
+++ JayabawwiWhenenearfajay/JayabawwiWhenenearfajay/FooResourceDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System.CodeDom;
+using System.Diagnostics;
+using System.Windows;
+
+namespace JayabawwiWhenenearfajay;
+
+public class FooResourceDictionary : ResourceDictionary
+{
+    public FooResourceDictionary()
+    {
+        Add("SolidColorBrush", this);
+    }
+
+    protected override void OnGettingValue(object key, ref object value, out bool canCache)
+    {
+        Debug.WriteLine(key);
+        base.OnGettingValue(key, ref value, out canCache);
+    }
+}
