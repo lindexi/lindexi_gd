3252bbca79515a5b998111ce2543b1eca154305e
lindexi <lindexi_gd@163.com>
2023-07-14 08:40:05 +0800

    加上测试代码

---
+++ WhineekelorerjarloFawyerwallqee/DawfallbibolaiwaJidunawkefayhaiwere/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DawfallbibolaiwaJidunawkefayhaiwere;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ WhineekelorerjarloFawyerwallqee/WhineekelorerjarloFawyerwallqee/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Maui.Controls;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Platform;
+using Button = Microsoft.Maui.Controls.Button;
+using Page = Microsoft.Maui.Controls.Page;
+using Window = System.Windows.Window;
+
+namespace WhineekelorerjarloFawyerwallqee;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        WindowHandler.Mapper.Add(nameof(IWindow.Title), (handler, window1) =>
+        {
+
+        });
+
+        ContentViewHandler.Mapper.Add(nameof(IContentView.Content), MapContent);
+
+      
+    }
+}
+
