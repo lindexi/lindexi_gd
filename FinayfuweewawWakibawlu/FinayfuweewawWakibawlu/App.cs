01df4d80d1262d8793df464607f83d9fa11f27c6
lindexi <lindexi_gd@163.com>
2023-07-31 09:14:03 +0800

    创建应用拷贝逻辑

---
+++ FinayfuweewawWakibawlu/FinayfuweewawWakibawlu/App.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace FinayfuweewawWakibawlu;
+
+public partial class App : Application
+{
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        base.OnLaunched(args);
+    }
+}
--- FinayfuweewawWakibawlu/FinayfuweewawWakibawlu/Program.cs
+++ FinayfuweewawWakibawlu/FinayfuweewawWakibawlu/Program.cs
@@ -3,29 +3,33 @@
 using Windows.ApplicationModel.Core;
 using Windows.System;
 using Windows.UI.ViewManagement;
+using Microsoft.UI.Xaml;
 
 namespace FinayfuweewawWakibawlu;
 
-internal class Program
-{
-    [STAThread]
-    unsafe static void Main(string[] args)
-    {
-        var mainView = CoreApplication.MainView;
-
-        var forCurrentThread = DispatcherQueue.GetForCurrentThread();
-
-        var coreApplicationView = CoreApplication.CreateNewView();
+//internal class Program
+//{
+//    [STAThread]
+//    unsafe static void Main(string[] args)
+//    {
+//        var application = new Application();
 
 
-        var applicationView = new ABI.Windows.UI.ViewManagement.ApplicationView();
-        var p = &applicationView;
+//        var mainView = CoreApplication.MainView;
 
-        var fromAbi = ApplicationView.FromAbi(new IntPtr(p));
+//        var forCurrentThread = DispatcherQueue.GetForCurrentThread();
 
-        var currentView = ApplicationView.GetForCurrentView();
+//        var coreApplicationView = CoreApplication.CreateNewView();
 
 
-        Console.WriteLine("Hello, World!");
-    }
-}
+//        var applicationView = new ABI.Windows.UI.ViewManagement.ApplicationView();
+//        var p = &applicationView;
+
+//        var fromAbi = ApplicationView.FromAbi(new IntPtr(p));
+
+//        var currentView = ApplicationView.GetForCurrentView();
+
+
+//        Console.WriteLine("Hello, World!");
+//    }
+//}
