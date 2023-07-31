5bcfc2437d830a89aa406e87219793c7d50dd815
lindexi <lindexi_gd@163.com>
2023-07-31 09:04:49 +0800

    测试控制台创建失败

---
+++ FinayfuweewawWakibawlu/FinayfuweewawWakibawlu/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Windows.ApplicationModel.Core;
+using Windows.System;
+using Windows.UI.ViewManagement;
+
+namespace FinayfuweewawWakibawlu;
+
+internal class Program
+{
+    [STAThread]
+    unsafe static void Main(string[] args)
+    {
+        var mainView = CoreApplication.MainView;
+
+        var forCurrentThread = DispatcherQueue.GetForCurrentThread();
+
+        var coreApplicationView = CoreApplication.CreateNewView();
+
+
+        var applicationView = new ABI.Windows.UI.ViewManagement.ApplicationView();
+        var p = &applicationView;
+
+        var fromAbi = ApplicationView.FromAbi(new IntPtr(p));
+
+        var currentView = ApplicationView.GetForCurrentView();
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
