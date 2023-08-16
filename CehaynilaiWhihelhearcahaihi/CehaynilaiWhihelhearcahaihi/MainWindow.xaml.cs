d56d6372a9a69f933e925ff70524c67e8bc49e1f
lindexi <lindexi_gd@163.com>
2023-08-16 15:24:15 +0800

    测试滚动条

--- CehaynilaiWhihelhearcahaihi/CehaynilaiWhihelhearcahaihi/MainWindow.xaml.cs
+++ CehaynilaiWhihelhearcahaihi/CehaynilaiWhihelhearcahaihi/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -23,10 +24,13 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        foreach (var file in Directory.GetFiles(Environment.CurrentDirectory,"*.png"))
+        var i = 0;
+        foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, "*.png"))
         {
             var bitmapImage = new BitmapImage(new Uri(file));
-            ModelCollection.Add(new Model(bitmapImage));
+            ModelCollection.Add(new Model(bitmapImage, i));
+
+            i++;
         }
 
         InitializeComponent();
@@ -46,14 +50,96 @@ public partial class MainWindow : Window
             border.Height = e.NewSize.Width * 9 / 16;
         }
     }
+
+    private void ListBox_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var scrollViewer = GetScrollViewer(SlideThumbListBox);
+        scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+    }
+
+    public static ScrollViewer? GetScrollViewer(DependencyObject? parent)
+    {
+        if (parent == null)
+            return null;
+        var count = VisualTreeHelper.GetChildrenCount(parent);
+
+        for (int i = 0; i < count; i++)
+        {
+            var item = VisualTreeHelper.GetChild(parent, i);
+            if (item is ScrollViewer viewer)
+            {
+                return viewer;
+            }
+            else
+            {
+                return GetScrollViewer(item);
+            }
+        }
+        return null;
+    }
+
+    private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+    {
+        var scrollViewer = (ScrollViewer) sender;
+        const int limitMaxValue = 20;
+
+        if (_lastElement is not null)
+        {
+            var point = ShowInTop(_lastElement);
+            if (point.Y > _lastElement.ActualHeight / 2 || point.Y < (-1 * _lastElement.ActualHeight * 0.7))
+            {
+                // 超过一半宽度高度了，那就忽略了吧
+            }
+            else
+            {
+                // 保持这一个不动，还没有滚动到看不见
+                return;
+            }
+        }
+
+        foreach (var item in SlideThumbListBox.ItemContainerGenerator.Items.Reverse())
+        {
+            var listItem = SlideThumbListBox.ItemContainerGenerator.ContainerFromItem(item);
+
+            if (listItem is FrameworkElement element)
+            {
+                var translatePoint = ShowInTop(element);
+
+                if (translatePoint.Y < limitMaxValue && translatePoint.Y >= 0)
+                {
+                    if (element != _lastElement)
+                    {
+                        _lastElement = element;
+                        Debug.WriteLine($"滚动到: {item}");
+                        //ScrollItemChanged?.Invoke(this, new ScrollItemChangedEventArgs(item, element));
+                    }
+                    return;
+                }
+            }
+        }
+
+        Point ShowInTop(UIElement element)
+        {
+            var translatePoint = element.TranslatePoint(new Point(), scrollViewer);
+            return translatePoint;
+        }
+    }
+
+    private FrameworkElement? _lastElement;
+
 }
 
 public class Model
 {
-    public Model(BitmapSource image)
+    public Model(BitmapSource image, int index)
     {
         Image = image;
+        Index = index;
     }
 
+    public int Index { get; }
+
     public BitmapSource Image { set; get; }
+
+    public override string ToString() => Index.ToString();
 }
