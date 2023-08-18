6c8641d8a6227d8f5624b2025593d25ef61158b1
lindexi <lindexi_gd@163.com>
2023-08-18 17:56:14 +0800

    Revert "修改任务栏"
    This reverts commit 2382fb1db06f4366813f3ff8a7298801ce6fb085.

--- CehaynilaiWhihelhearcahaihi/CehaynilaiWhihelhearcahaihi/MainWindow.xaml.cs
+++ CehaynilaiWhihelhearcahaihi/CehaynilaiWhihelhearcahaihi/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
