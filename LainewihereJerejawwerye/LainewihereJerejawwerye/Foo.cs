633b162c8486fd72e45e3bb08c29436b4eea4cf7
lindexi <lindexi_gd@163.com>
2023-08-21 19:44:22 +0800

    读取方法返回值的代码进行日志

---
+++ LainewihereJerejawwerye/LainewihereJerejawwerye/Foo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LainewihereJerejawwerye;
+
+public class Foo1
+{
+    public (int x, int) F2()
+    {
+        return default;
+    }
+}
