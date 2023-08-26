8e0f97dd197011f46f565ba4f5144316a4d59f87
lindexi <lindexi_gd@163.com>
2023-08-26 09:14:02 +0800

    测试交互内容，失败

---
+++ HairninokeenecanerCeeheafaydejeki/HairninokeenecanerCeeheafaydejeki/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.SemanticFunctions;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace HairninokeenecanerCeeheafaydejeki;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var kernelBuilder = new KernelBuilder();
+        kernelBuilder.WithAzureTextCompletionService("GPT35", "https://lindexi.openai.azure.com/", args[0]);
+        var kernel = kernelBuilder.Build();
+        var result = await kernel.CreateSemanticFunction("你好").InvokeAsync();
+        Console.WriteLine(result.Result);
+    }
+}
