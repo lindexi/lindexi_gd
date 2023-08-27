eca54565cd10135ae5acbf6242113ba4e25c2c7c
lindexi <lindexi_gd@163.com>
2023-08-27 15:54:32 +0800

    Example02 Pipeline

---
+++ SemanticKernelSamples/Example02_Pipeline/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Skills.Core;
+
+IKernel kernel = new KernelBuilder().Build();
+
+// 加载技能
+var text = kernel.ImportSkill(new TextSkill());
+
+SKContext result = await kernel.RunAsync("    i n f i n i t e     s p a c e     ",
+    text["TrimStart"],
+    text["TrimEnd"],
+    text["Uppercase"]);
+
+Console.WriteLine(result);
