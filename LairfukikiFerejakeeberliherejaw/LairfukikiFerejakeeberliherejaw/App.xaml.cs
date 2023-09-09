04d0136d32f567f6dcbdbcb323ce5649d276a9f9
lindexi <lindexi_gd@163.com>
2023-09-09 23:07:30 +0800

    测试源代码创建的方法写入文件在界面关联逻辑

---
+++ LairfukikiFerejakeeberliherejaw/LairfukikiFerejakeeberliherejaw/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LairfukikiFerejakeeberliherejaw;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
