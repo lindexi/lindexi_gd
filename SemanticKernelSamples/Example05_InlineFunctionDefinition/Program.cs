0c2957b3298cb21df6ccd5dad2a8652d5df9183c
lindexi <lindexi_gd@163.com>
2023-09-19 09:22:29 +0800

    Revert "炼丹"
    This reverts commit c21c849ddb92d13324d67dab004e80a565ede5f7.

--- SemanticKernelSamples/Example05_InlineFunctionDefinition/Program.cs
+++ SemanticKernelSamples/Example05_InlineFunctionDefinition/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.Memory.Sqlite;
-using Microsoft.SemanticKernel.Memory;
 
 var loggerFactory = LoggerFactory.Create(builder =>
 {
@@ -15,24 +13,12 @@ var logger = loggerFactory.CreateLogger("SemanticKernel");
 var endpoint = "https://lindexi.openai.azure.com/"; // 请换成你的地址
 var apiKey = args[0]; // 请换成你的密钥
 
-var sqliteMemoryStore = await SqliteMemoryStore.ConnectAsync("xx.db");
-
 IKernel kernel = new KernelBuilder()
     .WithLogger(logger)
     .WithAzureChatCompletionService("GPT35", endpoint, apiKey)
-    .WithAzureTextEmbeddingGenerationService("Embedding", endpoint, apiKey)
     // 当然，这里也可以支持 OpenAI 的服务。或者是其他第三方的服务
     //.WithOpenAIChatCompletionService()
-    .WithMemoryStorage(sqliteMemoryStore)
     .Build();
-
-await kernel.Memory.SaveInformationAsync("Text", "我是谁？小恩", "Test01");
-await kernel.Memory.SaveInformationAsync("Text", "我能够做什么？我可以帮你：\r\n· 根据课件生成教学设计\r\n· 总结当前页面文本\r\n· 润色你当前选中的文本\r\n· 扩写你当前选中的文本", "Test02");
-
-await foreach (var memoryQueryResult in kernel.Memory.SearchAsync("Text", "你会干什么"))
-{
-    
-}
 
 const string FunctionDefinition = @"
 为给定的事件想出一个创造性的理由或借口。
