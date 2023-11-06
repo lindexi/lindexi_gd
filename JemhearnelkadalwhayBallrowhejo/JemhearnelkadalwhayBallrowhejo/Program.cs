c4da1eaf5b0bedbed9e7f2024a70efe1e2bce607
lindexi <lindexi_gd@163.com>
2023-11-06 14:20:31 +0800

    测试读取到结束

---
+++ JemhearnelkadalwhayBallrowhejo/JemhearnelkadalwhayBallrowhejo/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Threading.Channels;
+
+var channel = Channel.CreateUnbounded<Foo>(new UnboundedChannelOptions()
+{
+    SingleReader = true,
+    SingleWriter = true,
+});
+
+Task.Run(async () =>
+{
+    while (true)
+    {
+        var result = await channel.Reader.WaitToReadAsync();
+        if (result)
+        {
+            var foo = await channel.Reader.ReadAsync();
+            Console.WriteLine($"读取到{foo.Count}");
+        }
+        else
+        {
+            Console.WriteLine($"返回 false 值");
+            return;
+        }
+    }
+});
+
+Task.Run(async () =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        var result = await channel.Writer.WaitToWriteAsync();
+
+        if (result is false)
+        {
+            Debugger.Break();
+        }
+
+        var foo = new Foo(i);
+        await channel.Writer.WriteAsync(foo);
+        await Task.Delay(100);
+    }
+
+    channel.Writer.Complete();
+});
+
+Console.Read();
+
+record Foo(int Count);
