2e02c5f1bdfeb3e53578e6ff63aa560f3ce5a51a
lindexi <lindexi_gd@163.com>
2023-11-08 20:36:07 +0800

    加上构建时间

---
+++ QewelechairWhegalnarlaywhe/QewelechairWhegalnarlaywhe/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"BuildAt={BuildInformation.BuildAt}");
+Console.WriteLine($"Platform={BuildInformation.Platform}");
+Console.WriteLine($"Configuration={BuildInformation.Configuration}");
+
