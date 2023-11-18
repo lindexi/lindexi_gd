2ec91207fff919837fff1c3121d57d0172b4f2bb
lindexi <lindexi_gd@163.com>
2023-11-18 10:56:05 +0800

    编写语法测试代码

---
+++ FaydeenereqelnairderlaHuwicagall/FaydeenereqelnairderlaHuwicagall/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IFoo foo = null;
+
+        var f1 = foo;
+
+        if (foo is var f2)
+        {
+            Console.WriteLine($"居然进来了。 F2={f2}");
+        }
+
+        if (foo is IFoo f3)
+        {
+            Console.WriteLine($"不进来");
+        }
+
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+interface IFoo
+{
+}
