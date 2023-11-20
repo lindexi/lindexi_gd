21ed5a4270fec8520b4d00c49ac937c4f3471462
lindexi <lindexi_gd@163.com>
2023-11-20 14:44:22 +0800

    使用硬链接减少磁盘空间占用

---
+++ WicallbachercalLaicheljaihawwhallbem/WicallbachercalLaicheljaihawwhallbem/Migrations/20231120040013_Lindexi.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WicallbachercalLaicheljaihawwhallbem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Lindexi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileStorageModel",
+                columns: table => new
+                {
+                    FileSha1Hash = table.Column<string>(type: "TEXT", nullable: false),
+                    OriginFilePath = table.Column<string>(type: "TEXT", nullable: false),
+                    ReferenceCount = table.Column<long>(type: "INTEGER", nullable: false),
+                    FileLength = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileStorageModel", x => x.FileSha1Hash);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileStorageModel");
+        }
+    }
+}
