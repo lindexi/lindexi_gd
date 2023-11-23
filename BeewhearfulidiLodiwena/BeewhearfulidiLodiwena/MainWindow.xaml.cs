72c057b5f193e7e827e64726e4871b76779fdb28
lindexi <lindexi_gd@163.com>
2023-11-23 21:55:05 +0800

    测试获取尺寸

---
+++ BeewhearfulidiLodiwena/BeewhearfulidiLodiwena/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeewhearfulidiLodiwena;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        Foo(Grid);
+    }
+
+    private void Foo(Visual element)
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+        {
+            var c = (Visual) VisualTreeHelper.GetChild(element, i);
+            var rect = VisualTreeHelper.GetDescendantBounds(c);
+
+            Foo(c);
+        }
+    }
+}
