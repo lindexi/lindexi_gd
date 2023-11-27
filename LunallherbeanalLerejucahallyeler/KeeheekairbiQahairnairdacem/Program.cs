5c8a31243b7f2e1ad87f49b319dbab39d5d18f0e
lindexi <lindexi_gd@163.com>
2023-11-27 21:09:15 +0800

    测试最大属性量

---
+++ LunallherbeanalLerejucahallyeler/KeeheekairbiQahairnairdacem/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+var stringBuilder = new StringBuilder();
+stringBuilder.AppendLine("""
+                         using System.Windows;
+                         
+                         namespace LunallherbeanalLerejucahallyeler;
+                         """);
+
+for (int i = 0; i < 10; i++)
+{
+    stringBuilder.AppendLine($$"""
+                             public partial class Type{{i}}
+                             {
+                             """);
+
+    for (int j = 0; j < 7000; j++)
+    {
+        var code = $$"""
+                     public static readonly DependencyProperty Foo{{j}}Property = DependencyProperty.Register(
+                         nameof(Foo{{j}}), typeof(string), typeof(Type{{i}}), new PropertyMetadata(default(string)));
+
+                     public string Foo{{j}}
+                     {
+                         get { return (string)GetValue(Foo{{j}}Property); }
+                         set { SetValue(Foo{{j}}Property, value); }
+                     }
+                     """;
+        stringBuilder.AppendLine(code);
+    }
+
+    stringBuilder.AppendLine("}");
+
+}
+
+
+
+var file = "MainWindow.cs";
+File.WriteAllText(file,stringBuilder.ToString());
+
