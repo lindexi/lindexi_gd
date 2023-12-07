1a5e6d57e2aa1e18fa410f176bbfcec576f2141b
lindexi <lindexi_gd@163.com>
2023-12-07 16:52:53 +0800

    测试全代码界面

---
+++ KerejecuyaljiKayjerehebuwu/KerejecuyaljiKayjerehebuwu/GenerateMarkupForAssemblyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.UI.Xaml.Generator;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+internal class GenerateMarkupForAssemblyAttribute : Attribute
+{
+    public GenerateMarkupForAssemblyAttribute(Type type)
+    {
+    }
+}
