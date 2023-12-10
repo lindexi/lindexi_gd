29df54af3d2471c002303eb412372b844b0708bc
lindexi <lindexi_gd@163.com>
2023-12-10 16:18:37 +0800

    制作一个手写数字

--- BenukalliwayaChayjanehall/BenukalliwayaChayjanehall/MainWindow.xaml.cs
+++ BenukalliwayaChayjanehall/BenukalliwayaChayjanehall/MainWindow.xaml.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -16,10 +18,10 @@ using System.Windows.Shapes;
 using Windows.AI.MachineLearning;
 using Windows.Graphics.Imaging;
 using Windows.Media;
-
-using BitmapDecoder = Windows.Graphics.Imaging.BitmapDecoder;
+using Windows.Storage.Streams;
 
 namespace BenukalliwayaChayjanehall;
+
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
@@ -29,43 +31,79 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        Loaded += MainWindow_Loaded;
-    }
-
-    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
-    {
-        LearningModel learningModel = LearningModel.LoadFromFilePath(@"F:\temp\classifier.onnx");
+        LearningModel learningModel = LearningModel.LoadFromFilePath("mnist.onnx");
         var deviceToRunOn = new LearningModelDevice(LearningModelDeviceKind.DirectXHighPerformance);
         var learningModelSession = new LearningModelSession(learningModel, deviceToRunOn);
         var learningModelBinding = new LearningModelBinding(learningModelSession);
 
-        for (int i = 0; i < 3; i++)
+        LearningModel = learningModel;
+        LearningModelSession = learningModelSession;
+        LearningModelBinding = learningModelBinding;
+    }
+
+    public LearningModel LearningModel { get; set; }
+
+    public LearningModelSession LearningModelSession { get; set; }
+
+    public LearningModelBinding LearningModelBinding { get; set; }
+
+    private async void RecognizeButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var width = (int) InkCanvas.ActualWidth;
+        var height = (int) InkCanvas.ActualHeight;
+
+        var bitmapSource = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+        bitmapSource.Render(InkCanvas);
+
+        var length = bitmapSource.PixelWidth * bitmapSource.PixelHeight * bitmapSource.Format.BitsPerPixel / 8;
+        var byteArray = new byte[length];
+        var stride = bitmapSource.PixelWidth * bitmapSource.Format.BitsPerPixel / 8;
+        bitmapSource.CopyPixels(byteArray, stride, 0);
+
+        IBuffer buffer = byteArray.AsBuffer();
+
+        var softwareBitmap = SoftwareBitmap.CreateCopyFromBuffer(buffer, BitmapPixelFormat.Bgra8, bitmapSource.PixelWidth, bitmapSource.PixelHeight);
+       
+        VideoFrame inputImage = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
+
+        var imageFeatureValue = ImageFeatureValue.CreateFromVideoFrame(inputImage);
+
+        LearningModelBinding.Bind("Input3", imageFeatureValue);
+
+        var result = await LearningModelSession.EvaluateAsync(LearningModelBinding, "0");
+
+        var resultOutput = result.Outputs["Plus214_Output_0"] as TensorFloat;
+        var vectorView = resultOutput?.GetAsVectorView();
+        if (vectorView != null)
         {
-            await using var fileStream = File.OpenRead($@"F:\temp\Image\{i}.jpg");
-
-            var randomAccessStream = fileStream.AsRandomAccessStream();
-            var decoder = await BitmapDecoder.CreateAsync(randomAccessStream);
-            var softwareBitmap = await decoder.GetSoftwareBitmapAsync();
-            VideoFrame inputImage = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
-
-            var imageFeatureValue = ImageFeatureValue.CreateFromVideoFrame(inputImage);
-
-            learningModelBinding.Bind("data", imageFeatureValue);
-
-            var result = await learningModelSession.EvaluateAsync(learningModelBinding, "0");
-            var classLabel = result.Outputs["classLabel"] as TensorString;
-            var loss = result.Outputs["loss"] as IList<IDictionary<string, float>>;
-
-            Debug.WriteLine(classLabel);
-
-            if (loss == null) return;
-            foreach (var dictionary in loss)
+            var maxValue = 0f;
+            var maxIndex = -1;
+            // 10 个数字，每个数字
+            for (var number = 0; number < vectorView.Count; number++)
             {
-                foreach (var (key, value) in dictionary)
+                Debug.WriteLine($"{number} {vectorView[number]}");
+
+                if (vectorView[number] > maxValue)
                 {
-                    Debug.WriteLine($"{key} {value}");
+                    maxValue = vectorView[number];
+                    maxIndex = number;
                 }
             }
+
+            if (maxIndex == -1)
+            {
+                TextBlock.Text = $"识别失败";
+            }
+            else
+            {
+                TextBlock.Text = $"识别数字：{maxIndex} 识别率：{maxValue}";
+            }
         }
+    }
+
+    private void ClearButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        InkCanvas.Children.Clear();
+        InkCanvas.Strokes.Clear();
     }
 }
