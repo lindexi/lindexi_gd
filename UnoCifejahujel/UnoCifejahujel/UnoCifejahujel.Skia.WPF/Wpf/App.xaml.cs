ebc019744aee17ebf03bbf4287265f31b6ecb714
lindexi <lindexi_gd@163.com>
2024-01-08 20:04:20 +0800

    加上空项目

---
+++ UnoCifejahujel/UnoCifejahujel/UnoCifejahujel.Skia.WPF/Wpf/App.xaml.cs
@@ -0,0 +1,15 @@
+using Uno.UI.Runtime.Skia.Wpf;
+
+using WpfApp = System.Windows.Application;
+
+namespace UnoCifejahujel.WPF
+{
+    public partial class App : WpfApp
+    {
+        public App()
+        {
+            var host = new WpfHost(Dispatcher, () => new AppHead());
+            host.Run();
+        }
+    }
+}
---
+++ UnoCifejahujel/UnoCifejahujel/UnoCifejahujel/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace UnoCifejahujel
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
