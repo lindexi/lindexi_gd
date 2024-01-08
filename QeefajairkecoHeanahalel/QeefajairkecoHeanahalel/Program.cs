c2507b95a5e0b6236b2a8a7efff23af88dbfb09b
lindexi <lindexi_gd@163.com>
2024-01-08 20:03:59 +0800

    测试官网例子

---
+++ QeefajairkecoHeanahalel/QeefajairkecoHeanahalel/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+// 这里的演示代码需要用到 AzureAI 的支持，需要提前申请好，申请地址：https://aka.ms/oai/access
+
+var endpoint = "https://lindexi.openai.azure.com/"; // 请换成你的地址
+var apiKey = Environment.GetEnvironmentVariable("AzureAIKey"); // 请换成你的密钥
+if (string.IsNullOrEmpty(apiKey))
+{
+    throw new ArgumentException($"请设置为你的密钥");
+}
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion("GPT4", endpoint, apiKey);
+builder.Plugins.AddFromType<LightPlugin>();
+
+var kernel = builder.Build();
+// Create chat history
+ChatHistory history = new ChatHistory();
+
+// Get chat completion service
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Start the conversation
+while (true)
+{
+    // Get user input
+    Console.Write("User > ");
+    history.AddUserMessage(Console.ReadLine()!);
+
+    // Enable auto function calling
+    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+    {
+        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+    };
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content);
+}
+
+Console.Read();
+
+public class LightPlugin
+{
+    public bool IsOn { get; set; } = false;
+
+    [KernelFunction]
+    [Description("Gets the state of the light.")]
+    public string GetState() => this.IsOn ? "on" : "off";
+
+    [KernelFunction]
+    [Description("Changes the state of the light.'")]
+    public string ChangeState(bool newState)
+    {
+        this.IsOn = newState;
+        var state = this.GetState();
+
+        // Print the state to the console
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine($"[Light is now {state}]");
+        Console.ResetColor();
+
+        return state;
+    }
+}
