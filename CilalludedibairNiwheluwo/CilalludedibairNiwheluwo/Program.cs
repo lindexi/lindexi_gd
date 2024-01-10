a309d1bdd9711456b02c79ebdb855caa6069d82a
lindexi <lindexi_gd@163.com>
2024-01-10 17:32:54 +0800

    测试XML字符串内存

---
+++ CilalludedibairNiwheluwo/CilalludedibairNiwheluwo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Xml.Linq;
+
+var file = @"C:\lindexi\Slides\Slide_0.xml";
+
+var xDocument = XDocument.Load(file);
+
+Console.WriteLine("Hello, World!");
