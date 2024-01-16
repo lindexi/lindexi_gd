386dd89100a5d287eb645e45d5a79f0094e55c74
lindexi <lindexi_gd@163.com>
2024-01-16 15:42:40 +0800

    尝试多窗口然而失败

---
+++ LacebayjeejiBehebilawla/LacebayjeejiBehebilawla/Lib/SamplesApp/Foo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.UI.Xaml;
+
+namespace SamplesApp;
+
+internal class Foo
+{
+    public void Fx(Window window)
+    {
+    }
+}
