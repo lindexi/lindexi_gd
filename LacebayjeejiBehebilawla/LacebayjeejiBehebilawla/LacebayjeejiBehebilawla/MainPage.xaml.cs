f592e8ebb18f9bd9a389a69027eace81a75f6cd2
lindexi <lindexi_gd@163.com>
2024-01-16 19:32:23 +0800

    测试漫游文本

--- LacebayjeejiBehebilawla/LacebayjeejiBehebilawla/LacebayjeejiBehebilawla/MainPage.xaml.cs
+++ LacebayjeejiBehebilawla/LacebayjeejiBehebilawla/LacebayjeejiBehebilawla/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Xaml.Input;
+
 namespace LacebayjeejiBehebilawla;
 
 public sealed partial class MainPage : Page
@@ -5,5 +7,12 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+    }
+
+    private void Canvas_OnPointerMoved(object sender, PointerRoutedEventArgs e)
+    {
+        var currentPoint = e.GetCurrentPoint(this);
+        HelloTextBlockTransform.X = currentPoint.Position.X;
+        HelloTextBlockTransform.Y = currentPoint.Position.Y;
     }
 }
