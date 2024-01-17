61a7e77b8b86e17ccf2b5d1a9d0460d09cc95036
lindexi <lindexi_gd@163.com>
2024-01-17 16:34:39 +0800

    测试文件路径

---
+++ NurbeakairweWaharbaner/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+var stringBuilder = new StringBuilder();
+
+foreach (var name in Enum.GetNames<Environment.SpecialFolder>())
+{
+    stringBuilder.AppendLine($"{name} = {Environment.GetFolderPath(Enum.Parse<Environment.SpecialFolder>(name))}");
+}
+
+Console.WriteLine(stringBuilder.ToString());
+
+foreach (var name in new[] { "XDG_DATA_HOME", "XDG_CONFIG_HOME", "XDG_CACHE_HOME" })
+{
+    stringBuilder.AppendLine($"{name} = {Environment.GetEnvironmentVariable(name)}");
+}
+
+File.WriteAllText("output.txt", stringBuilder.ToString());
+
+Console.Read();
