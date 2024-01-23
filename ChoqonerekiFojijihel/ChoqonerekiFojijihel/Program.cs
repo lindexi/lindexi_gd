bdc7f6d136079f726aa04b10859149c36c91a940
lindexi <lindexi_gd@163.com>
2024-01-23 19:02:24 +0800

    测试打开文本文件

---
+++ ChoqonerekiFojijihel/ChoqonerekiFojijihel/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+var filePath = "../Test.txt";
+
+Console.WriteLine($"文件存在 {File.Exists(filePath)}");
+
+Process.Start(new ProcessStartInfo(filePath)
+{
+    UseShellExecute = true
+});
