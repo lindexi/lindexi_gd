06388e21d9380f7ada5404d4906f0e610215191d
lindexi <lindexi_gd@163.com>
2024-01-24 11:25:11 +0800

    Revert "测试在 WPF 嵌入 MAUI 控件"
    This reverts commit 13beb1b7f4646510a35d3c1c35abbb47831599c5.

--- WifaberfearnairWherneyeaquqaki/WifaberfearnairWherneyeaquqaki.MauiControls/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WifaberfearnairWherneyeaquqaki.MauiControls;
-
-public partial class App : Application
-{
-    public App()
-    {
-        InitializeComponent();
-    }
-}
--- WifaberfearnairWherneyeaquqaki/WifaberfearnairWherneyeaquqaki/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace WifaberfearnairWherneyeaquqaki;
-
-public sealed partial class MainPage : Page
-{
-    public MainPage()
-    {
-        this.InitializeComponent();
-    }
-}
