c6c1f723f58994d8a15344e9147a30988eedbf3e
lindexi <lindexi_gd@163.com>
2024-01-26 10:04:26 +0800

    测试AppCenter引用

---
+++ JoquryurjoqaJelhawlujel/JoquryurjoqaJelhawlujel/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Crashes;
+
+namespace JoquryurjoqaJelhawlujel;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        AppCenter.Start("{Your App Secret}", typeof(App), typeof(Crashes));
+    }
+}
+
