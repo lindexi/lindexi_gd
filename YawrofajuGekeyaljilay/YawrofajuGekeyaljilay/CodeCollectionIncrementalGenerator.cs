3b7623ad46e80e8cc88a51e8084339ac29937b64
lindexi <lindexi_gd@163.com>
2024-02-02 10:20:58 +0800

    给增量代码项目加上单元测试

---
+++ YawrofajuGekeyaljilay/YawrofajuGekeyaljilay/CodeCollectionIncrementalGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YawrofajuGekeyaljilay
+{
+    [Generator(LanguageNames.CSharp)]
+    public class CodeCollectionIncrementalGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            string source = @"
+using System;
+
+namespace YawrofajuGekeyaljilay
+{
+    public static partial class Program
+    {
+        public static void HelloFrom(string name)
+        {
+            Console.WriteLine($""Says: Hi from '{name}'"");
+        }
+    }
+}
+";
+
+            context.RegisterPostInitializationOutput(initializationContext =>
+            {
+                initializationContext.AddSource("GeneratedSourceTest", source);
+            });
+        }
+    }
+}
