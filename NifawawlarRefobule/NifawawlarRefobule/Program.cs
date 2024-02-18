5f368b4329bd0458b1384394bbaa0fc3ea11878e
lindexi <lindexi_gd@163.com>
2024-02-18 15:14:51 +0800

    The GetHashCode implementation of the Thickness struct is broken for uniform length

---
+++ NifawawlarRefobule/NifawawlarRefobule/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double a, b, c, d;
+a = b = c = d = 5;
+var hashCode = new System.HashCode();
+hashCode.Add(a);
+hashCode.Add(b);
+hashCode.Add(c);
+hashCode.Add(d);
+var code = hashCode.ToHashCode();
+Console.WriteLine(code);
