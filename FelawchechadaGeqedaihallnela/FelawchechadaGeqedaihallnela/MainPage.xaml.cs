547d6bcf1bad9527041f7624b7bd535a5c46cca6
lindexi <lindexi_gd@163.com>
2024-03-02 17:01:36 +0800

    测试多指但是失败

--- FelawchechadaGeqedaihallnela/FelawchechadaGeqedaihallnela/MainPage.xaml.cs
+++ FelawchechadaGeqedaihallnela/FelawchechadaGeqedaihallnela/MainPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace FelawchechadaGeqedaihallnela;
@@ -8,6 +9,19 @@ public sealed partial class MainPage : Page
     {
         this.InitializeComponent();
         Loaded += MainPage_Loaded;
+
+        //RootPanel.PointerPressed += RootPanel_PointerPressed;
+        RootPanel.PointerReleased += RootPanel_PointerReleased;
+    }
+
+    private void RootPanel_PointerReleased(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(e.Pointer.PointerId);
+    }
+
+    private void RootPanel_PointerPressed(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(e.Pointer.PointerId);
     }
 
     private void MainPage_Loaded(object sender, RoutedEventArgs e)
