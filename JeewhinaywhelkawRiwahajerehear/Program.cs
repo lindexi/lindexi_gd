a42c8db21109975a45c31edd0908f80a530c30bc
lindexi <lindexi_gd@163.com>
2024-03-02 09:20:49 +0800

    Revert "加载不存在的文件将会失败"
    This reverts commit 4938eb0a848a578290122b98c6d4c47afef609a9.

--- JeewhinaywhelkawRiwahajerehear/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Runtime.InteropServices;
-
-Console.WriteLine("Hello, World!");
-
-[DllImport(@"C:\Program Files (x86)\Foo\vcruntime140.dll")]
-static extern void Foo();
