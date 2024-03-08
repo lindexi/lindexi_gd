c57039c42491c89c9102eac3f9b90605a6bc21a6
lindexi <lindexi_gd@163.com>
2024-03-08 11:41:54 +0800

    2024/3/8 11:41:54

--- UnoSpySnoop/UI/UnoSpySnoopDebugger/MainPage.xaml.cs
+++ UnoSpySnoop/UI/UnoSpySnoopDebugger/MainPage.xaml.cs
@@ -1,3 +1,7 @@
+using dotnetCampus.Ipc.IpcRouteds.DirectRouteds;
+
+using UnoSpySnoopDebugger.View;
+
 namespace UnoSpySnoopDebugger;
 
 public sealed partial class MainPage : Page
@@ -5,5 +9,22 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+
+        Loaded += MainPage_Loaded;
+
+        var ipcProvider = new JsonIpcDirectRoutedProvider();
+        IpcProvider = ipcProvider;
+    }
+
+    public JsonIpcDirectRoutedProvider IpcProvider { get; set; }
+
+    private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        var debugName = "UnoSpySnoop"; // todo Update to selected name
+        JsonIpcDirectRoutedClientProxy client = await IpcProvider.GetAndConnectClientAsync(debugName);
+        var snoopUserControl = new SnoopUserControl(client);
+        await snoopUserControl.StartAsync();
+
+        RootGrid.Children.Add(snoopUserControl);
     }
 }
