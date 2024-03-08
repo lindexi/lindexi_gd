7df664436676cfe9f34efa2dfd7cc4b44b8e7d42
lindexi <lindexi_gd@163.com>
2024-03-08 11:05:15 +0800

    完成实现最简单的对接

--- UnoSpySnoop/Lib/UnoSpySnoopDebugger.IpcCommunicationContext/ElementBaseInfo.cs
+++ UnoSpySnoop/Lib/UnoSpySnoopDebugger.IpcCommunicationContext/ElementBaseInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace UnoSpySnoopDebugger.IpcCommunicationContext;
 
-public record ElementBaseInfo(string Token, string ElementType, string? ElementName)
+public record ElementBaseInfo(string Token, string ElementTypeName,string ElementTypeFullName, string? ElementName)
 {
 }
