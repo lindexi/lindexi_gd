03ec48693158c4c62e3559b467073fa26d6fb0c1
lindexi <lindexi_gd@163.com>
2024-03-09 11:56:44 +0800

    删除调试代码

--- UnoSpySnoop/UI/UnoSpySnoopDebugger/MainPage.xaml.cs
+++ UnoSpySnoop/UI/UnoSpySnoopDebugger/MainPage.xaml.cs
@@ -23,7 +23,6 @@ public sealed partial class MainPage : Page
     {
         this.InitializeComponent();
 
-        //Loaded += MainPage_Loaded;
         var currentProcess = Process.GetCurrentProcess();
         var name = $"UnoSpySnoopDebugger_{currentProcess.ProcessName}_{currentProcess.Id}";
         var ipcProvider = new IpcProvider(name, new IpcConfiguration()
@@ -35,9 +34,6 @@ public sealed partial class MainPage : Page
         IpcProvider = jsonIpcDirectRoutedProvider;
 
         _ = RefreshProcessInfoList();
-
-        var dependencyPropertyInfoList = new List<DependencyPropertyInfo>();
-        GetStaticDependencyProperty(this, typeof(SnoopUserControl), dependencyPropertyInfoList);
     }
 
     public ObservableCollection<CandidateDebugProcessInfo> ProcessInfoList { get; } =
@@ -94,72 +90,7 @@ public sealed partial class MainPage : Page
         }
     }
 
-    record DependencyPropertyInfo(string Name, string Value, string DeclaringTypeFullName);
-
-    private void GetStaticDependencyProperty(DependencyObject obj, Type type, List<DependencyPropertyInfo> infoList)
-    {
-        foreach (FieldInfo fieldInfo in type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.Instance))
-        {
-            if (fieldInfo.FieldType == typeof(DependencyProperty))
-            {
-                if (fieldInfo.GetValue(null) is DependencyProperty dependencyProperty)
-                {
-                    AddToInfoList(dependencyProperty);
-                }
-            }
-        }
-
-        foreach (PropertyInfo propertyInfo in type.GetProperties(BindingFlags.Static | BindingFlags.Public))
-        {
-            if (propertyInfo.PropertyType == typeof(DependencyProperty))
-            {
-                if (propertyInfo.GetValue(null) is DependencyProperty dependencyProperty)
-                {
-                    AddToInfoList(dependencyProperty);
-                }
-            }
-        }
-
-        if (type.BaseType is { } baseType)
-        {
-            GetStaticDependencyProperty(obj, baseType, infoList);
-        }
-
-        void AddToInfoList(DependencyProperty dependencyProperty)
-        {
-            try
-            {
-                PropertyInfo nameProperty =
-                    typeof(DependencyProperty).GetProperty("Name", BindingFlags.Instance | BindingFlags.NonPublic)!;
-                var name = (string) nameProperty.GetValue(dependencyProperty)!;
-
-                var value = obj.GetValue(dependencyProperty);
-                var valueText = value?.ToString() ?? "_<NULL>";
-
-                var info = new DependencyPropertyInfo(name, valueText, type.FullName!);
-                infoList.Add(info);
-            }
-            catch (Exception e)
-            {
-                if (e is System.InvalidOperationException)
-                {
-                    // 如获取属性不在相同类型
-                }
-            }
-        }
-    }
-
     public JsonIpcDirectRoutedProvider IpcProvider { get; set; }
-
-    private async void MainPage_Loaded(object sender, RoutedEventArgs e)
-    {
-        var debugName = "UnoSpySnoop"; // todo Update to selected name
-        JsonIpcDirectRoutedClientProxy client = await IpcProvider.GetAndConnectClientAsync(debugName);
-        var snoopUserControl = new SnoopUserControl(client);
-        await snoopUserControl.StartAsync();
-
-        RootGrid.Children.Add(snoopUserControl);
-    }
 
     private async void StartDebugButton_OnClick(object sender, RoutedEventArgs e)
     {
