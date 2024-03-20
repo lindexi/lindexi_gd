6ba9d445cd66fc44420defd1baf846192834eb91
lindexi <lindexi_gd@163.com>
2024-03-20 17:04:46 +0800

    测试绘制线段成功

--- WarheelaigeQekeyelyai/Program.cs
+++ WarheelaigeQekeyelyai/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Cairo;
+
 using Gdk;
+
 using Gtk;
+
 using Window = Gtk.Window;
 using WindowType = Gtk.WindowType;
 
@@ -8,6 +11,7 @@ namespace WarheelaigeQekeyelyai;
 
 internal class Program
 {
+        [STAThread]
     static void Main(string[] args)
     {
         Application.Init();
@@ -17,7 +21,23 @@ internal class Program
 
         Win = new MainWindow("Demo Window");
         App.AddWindow(Win);
-        Win.Show();
+
+        var menu = new GLib.Menu();
+        menu.AppendItem(new GLib.MenuItem("Help", "app.help"));
+        menu.AppendItem(new GLib.MenuItem("About", "app.about"));
+        menu.AppendItem(new GLib.MenuItem("Quit", "app.quit"));
+        App.AppMenu = menu;
+
+        var helpAction = new GLib.SimpleAction("help", null);
+        App.AddAction(helpAction);
+
+        var aboutAction = new GLib.SimpleAction("about", null);
+        App.AddAction(aboutAction);
+
+        var quitAction = new GLib.SimpleAction("quit", null);
+        App.AddAction(quitAction);
+
+        Win.ShowAll();
         Application.Run();
     }
 
@@ -27,20 +47,77 @@ internal class Program
 
 class MainWindow : Window
 {
-    public MainWindow(string title) : base(title)
+        private HeaderBar _headerBar;
+        private TreeView _treeView;
+        private Box _boxContent;
+        private TreeStore _store; private Dictionary<string, (Type type, Widget widget)> _items;
+        //private SourceView _textViewCode;
+        private Notebook _notebook; public MainWindow(string title) : base(WindowType.Toplevel)
     {
         WindowPosition = WindowPosition.Center;
         DefaultSize = new Size(600, 600);
 
-        Widget widget = this;
-        this.TouchEvent += MainWindow_TouchEvent;
 
-        Child = new Box(Orientation.Horizontal, 0)
-        {
-            WidthRequest = 500,
-            HeightRequest = 500,
-            Child = new F()
-        };
+        _headerBar = new HeaderBar();
+        _headerBar.ShowCloseButton = true;
+        _headerBar.Title = "GtkSharp Sample Application";
+
+        var btnClickMe = new Button();
+        btnClickMe.AlwaysShowImage = true;
+        btnClickMe.Image = Image.NewFromIconName("document-new-symbolic", IconSize.Button);
+        _headerBar.PackStart(btnClickMe);
+
+        //Titlebar = _headerBar;
+
+        var hpanned = new HPaned();
+        hpanned.Position = 200;
+
+        _treeView = new TreeView();
+        _treeView.HeadersVisible = false;
+        hpanned.Pack1(_treeView, false, true);
+
+        _notebook = new Notebook();
+
+        var scroll1 = new ScrolledWindow();
+        var vpanned = new VPaned();
+        vpanned.Position = 300;
+        _boxContent = new Box(Orientation.Vertical, 0);
+        _boxContent.Margin = 8;
+        vpanned.Pack1(_boxContent, true, true);
+        //vpanned.Pack2(ApplicationOutput.Widget, false, true);
+        scroll1.Child = vpanned;
+        _notebook.AppendPage(scroll1, new Label { Text = "Data", Expand = true });
+
+        var scroll2 = new ScrolledWindow();
+
+        //_textViewCode = new SourceView();
+        //_textViewCode.ShowLineNumbers = true;
+        //_textViewCode.Buffer.Language = new LanguageManager().GetLanguage("c-sharp");
+
+        //_textViewCode.Margin = 3;
+        //scroll2.Child = _textViewCode;
+        _notebook.AppendPage(scroll2, new Label { Text = "Code", Expand = true });
+
+        _notebook.AppendPage(new F(), new Label() { Text = "Code2", Expand = true });
+
+        hpanned.Pack2(_notebook, true, true);
+
+        Child = hpanned;
+
+        //Widget widget = this;
+        //this.TouchEvent += MainWindow_TouchEvent;
+        //var hpanned = new HPaned();
+        //hpanned.Position = 20;
+        //var f = new F();
+        //hpanned.Pack1(f, false, true);
+        //var button = new Button()
+        //{
+
+        //};
+        //button.Image = Image.NewFromIconName("document-new-symbolic", IconSize.Button);
+        //hpanned.Pack2(button,false,true);
+
+        //Child = hpanned;
     }
 
     private void MainWindow_TouchEvent(object o, TouchEventArgs args)
@@ -53,24 +130,23 @@ class MainWindow : Window
         //if (cr.GetSource() is SurfacePattern surfacePattern)
         //{
         //}
-        cr.SetSourceRGB(0.9,0,0);
+        cr.SetSourceRGB(0.9, 0, 0);
         cr.LineWidth = 10;
-        cr.MoveTo(10,10);
-        cr.LineTo(100,10);
+        cr.MoveTo(10, 10);
+        cr.LineTo(100, 10);
         cr.Stroke();
-        
+
         var result = base.OnDrawn(cr);
         return true;
     }
 }
 
-class F : Widget
+class F : DrawingArea
 {
     public F()
     {
         WidthRequest = 300;
         HeightRequest = 300;
-        
     }
 
     protected override bool OnDrawn(Context cr)
