3b60bd59669851542b3474b81570e9567588a978
lindexi <lindexi_gd@163.com>
2024-03-21 14:51:35 +0800

    尝试输出触摸点信息

--- WarheelaigeQekeyelyai/Program.cs
+++ WarheelaigeQekeyelyai/Program.cs
@@ -1,8 +1,12 @@
-﻿using Cairo;
+﻿using Atk;
+
+using Cairo;
 
 using Gdk;
 
 using Gtk;
+
+using System.Runtime.InteropServices;
 
 using Window = Gtk.Window;
 using WindowType = Gtk.WindowType;
@@ -58,13 +62,84 @@ class F : DrawingArea
         TouchEvent += F_TouchEvent;
     }
 
-    private void F_TouchEvent(object o, TouchEventArgs args)
+    private unsafe void F_TouchEvent(object o, TouchEventArgs args)
     {
         //Console.WriteLine($"{args.Event.Type} {args.Event.Handle}");
 
         var eventTouch = EventTouch.New(args.Event.Handle);
         //Console.WriteLine($"EventTouch {eventTouch.X} {eventTouch.Y}");
         _point = (eventTouch.X, eventTouch.Y);
+
+        if (eventTouch.Type == EventType.TouchBegin)
+        {
+            var device = eventTouch.Device;
+            var numAxes = device.NumAxes;
+
+            Console.WriteLine($"NumAxes={numAxes}");
+
+            var span = new Span<double>((void*) eventTouch.Axes, numAxes);
+            var axes = span.ToArray();
+
+            if (device.GetAxis(axes, AxisUse.X, out var value))
+            {
+                Console.WriteLine($"AxisUse.X={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Y, out value))
+            {
+                Console.WriteLine($"AxisUse.Y={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Pressure, out value))
+            {
+                Console.WriteLine($"Pressure={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Xtilt, out value))
+            {
+                Console.WriteLine($"Xtilt={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Ytilt, out value))
+            {
+                Console.WriteLine($"Ytilt={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Wheel, out value))
+            {
+                Console.WriteLine($"Wheel={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Distance, out value))
+            {
+                Console.WriteLine($"Distance={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Rotation, out value))
+            {
+                Console.WriteLine($"Rotation={value}");
+            }
+
+            if (device.GetAxis(axes, AxisUse.Slider, out value))
+            {
+                Console.WriteLine($"Slider={value}");
+            }
+
+            for (int i = 10; i < numAxes; i++)
+            {
+                
+            }
+
+            /*
+               Xtilt,
+               Ytilt,
+               Wheel,
+               Distance,
+               Rotation,
+               Slider,
+             */
+        }
+
         QueueDraw();
     }
 
@@ -97,5 +172,70 @@ class F : DrawingArea
         cr.Rectangle(_point.X - size / 2, _point.Y - size / 2, size, size);
         cr.Stroke();
         return base.OnDrawn(cr);
+    }
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public partial struct EventTouch
+{
+    public Gdk.EventType Type;
+    private IntPtr _window;
+    public Gdk.Window Window
+    {
+        get
+        {
+            return GLib.Object.GetObject(_window) as Gdk.Window;
+        }
+        set
+        {
+            _window = value == null ? IntPtr.Zero : value.Handle;
+        }
+    }
+    public sbyte SendEvent;
+    public uint Time;
+    public double X;
+    public double Y;
+    public IntPtr Axes;
+    public uint State;
+    private IntPtr _sequence;
+    public Gdk.EventSequence Sequence
+    {
+        get
+        {
+            return _sequence == IntPtr.Zero ? null : (Gdk.EventSequence) GLib.Opaque.GetOpaque(_sequence, typeof(Gdk.EventSequence), false);
+        }
+        set
+        {
+            _sequence = value == null ? IntPtr.Zero : value.Handle;
+        }
+    }
+    public bool EmulatingPointer;
+    private IntPtr _device;
+    public Gdk.Device Device
+    {
+        get
+        {
+            return GLib.Object.GetObject(_device) as Gdk.Device;
+        }
+        set
+        {
+            _device = value == null ? IntPtr.Zero : value.Handle;
+        }
+    }
+    public double XRoot;
+    public double YRoot;
+
+    public static EventTouch Zero = new EventTouch();
+
+    public static EventTouch New(IntPtr raw)
+    {
+        if (raw == IntPtr.Zero)
+            return EventTouch.Zero;
+        return (EventTouch) Marshal.PtrToStructure(raw, typeof(EventTouch));
+    }
+
+    private static GLib.GType GType
+    {
+        get { return GLib.GType.Pointer; }
     }
 }
