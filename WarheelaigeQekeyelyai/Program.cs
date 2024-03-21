fbf5bbe30a488907723df34f15a788673d927d6c
lindexi <lindexi_gd@163.com>
2024-03-21 09:23:55 +0800

    最简代码跟随触摸点

--- WarheelaigeQekeyelyai/Program.cs
+++ WarheelaigeQekeyelyai/Program.cs
@@ -22,7 +22,7 @@ internal class Program
         Win = new MainWindow("Demo Window");
         App.AddWindow(Win);
 
-        Win.Show();
+        Win.ShowAll();
         Application.Run();
     }
 
@@ -52,14 +52,49 @@ class F : DrawingArea
     {
         WidthRequest = 300;
         HeightRequest = 300;
+
+        AddEvents((int) RequestedEvents);
+        EnterNotifyEvent += F_EnterNotifyEvent;
+        TouchEvent += F_TouchEvent;
+    }
+
+    private void F_TouchEvent(object o, TouchEventArgs args)
+    {
+        //Console.WriteLine($"{args.Event.Type} {args.Event.Handle}");
+
+        var eventTouch = EventTouch.New(args.Event.Handle);
+        //Console.WriteLine($"EventTouch {eventTouch.X} {eventTouch.Y}");
+        _point = (eventTouch.X, eventTouch.Y);
+        QueueDraw();
+    }
+
+    private (double X, double Y) _point = (100, 100);
+
+    internal const Gdk.EventMask RequestedEvents =
+        Gdk.EventMask.EnterNotifyMask
+        | Gdk.EventMask.LeaveNotifyMask
+        | Gdk.EventMask.ButtonPressMask
+        | Gdk.EventMask.ButtonReleaseMask
+        | Gdk.EventMask.PointerMotionMask // Move
+        | Gdk.EventMask.SmoothScrollMask
+        | Gdk.EventMask.TouchMask // Touch
+        | Gdk.EventMask.ProximityInMask // Pen
+        | Gdk.EventMask.ProximityOutMask // Pen
+        | Gdk.EventMask.KeyPressMask
+        | Gdk.EventMask.KeyReleaseMask;
+
+    private void F_EnterNotifyEvent(object o, EnterNotifyEventArgs args)
+    {
     }
 
     protected override bool OnDrawn(Context cr)
     {
         cr.SetSourceRGB(0.9, 0, 0);
         cr.LineWidth = 10;
-        cr.MoveTo(10, 10);
-        cr.LineTo(100, 10);
+        //cr.MoveTo(10, 10);
+        //cr.LineTo(_point.X, _point.Y);
+        var size = 10d;
+        cr.Rectangle(_point.X - size / 2, _point.Y - size / 2, size, size);
         cr.Stroke();
         return base.OnDrawn(cr);
     }
