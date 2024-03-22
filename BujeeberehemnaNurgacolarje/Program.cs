d89eabdf32fbd668180d58e36eb571dae837cdef
lindexi <lindexi_gd@163.com>
2024-03-22 14:43:44 +0800

    显示界面是黑的

--- BujeeberehemnaNurgacolarje/Program.cs
+++ BujeeberehemnaNurgacolarje/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System.Runtime.Loader;
+
+using Microsoft.Extensions.Logging;
 using Microsoft.UI.Xaml;
+using Sia;
+
+using Uno.Foundation.Logging;
 using Uno.WinUI.Runtime.Skia.X11;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace BujeeberehemnaNurgacolarje;
 
@@ -14,7 +20,59 @@ internal class Program
         var x11ApplicationHost = new X11ApplicationHost(() =>
         {
             var application = new App();
-           
+
+            var factory = LoggerFactory.Create(builder =>
+            {
+#if __WASM__
+            builder.AddProvider(new global::Uno.Extensions.Logging.WebAssembly.WebAssemblyConsoleLoggerProvider());
+#elif __IOS__ || __MACCATALYST__
+            builder.AddProvider(new global::Uno.Extensions.Logging.OSLogLoggerProvider());
+#else
+                builder.AddConsole();
+#endif
+
+                // Exclude logs below this level
+                builder.SetMinimumLevel(LogLevel.Information);
+
+                // Default filters for Uno Platform namespaces
+                builder.AddFilter("Uno", LogLevel.Information);
+                builder.AddFilter("Windows", LogLevel.Information);
+                builder.AddFilter("Microsoft", LogLevel.Information);
+
+                // Generic Xaml events
+                // builder.AddFilter("Microsoft.UI.Xaml", LogLevel.Debug );
+                // builder.AddFilter("Microsoft.UI.Xaml.VisualStateGroup", LogLevel.Debug );
+                // builder.AddFilter("Microsoft.UI.Xaml.StateTriggerBase", LogLevel.Debug );
+                // builder.AddFilter("Microsoft.UI.Xaml.UIElement", LogLevel.Debug );
+                // builder.AddFilter("Microsoft.UI.Xaml.FrameworkElement", LogLevel.Trace );
+
+                // Layouter specific messages
+                // builder.AddFilter("Microsoft.UI.Xaml.Controls", LogLevel.Debug );
+                // builder.AddFilter("Microsoft.UI.Xaml.Controls.Layouter", LogLevel.Debug );
+                // builder.AddFilter("Microsoft.UI.Xaml.Controls.Panel", LogLevel.Debug );
+
+                // builder.AddFilter("Windows.Storage", LogLevel.Debug );
+
+                // Binding related messages
+                // builder.AddFilter("Microsoft.UI.Xaml.Data", LogLevel.Debug );
+                // builder.AddFilter("Microsoft.UI.Xaml.Data", LogLevel.Debug );
+
+                // Binder memory references tracking
+                // builder.AddFilter("Uno.UI.DataBinding.BinderReferenceHolder", LogLevel.Debug );
+
+                // DevServer and HotReload related
+                // builder.AddFilter("Uno.UI.RemoteControl", LogLevel.Information);
+
+                // Debug JS interop
+                // builder.AddFilter("Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug );
+            });
+
+            global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory = factory;
+
+#if HAS_UNO
+            global::Uno.UI.Adapter.Microsoft.Extensions.Logging.LoggingAdapter.Initialize();
+#endif
+
             return application;
         });
 
@@ -35,13 +93,3 @@ internal class Program
     }
 }
 
-class App : Application
-{
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
-    {
-        base.OnLaunched(args);
-
-        var window = Window.Current;
-        window.Activate();
-    }
-}
