6305ee7f6471c1b711efda65e408b3fad0e7f20d
lindexi <lindexi_gd@163.com>
2024-03-25 11:21:02 +0800

    测试读取挂载磁盘

--- NeberwhereneyeWeefemfalaw/Program.cs
+++ NeberwhereneyeWeefemfalaw/Program.cs
@@ -1,12 +1,25 @@
-﻿namespace NeberwhereneyeWeefemfalaw;
+﻿using dotnetCampus.Configurations.Core;
+
+namespace NeberwhereneyeWeefemfalaw;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         var file = "C:\\lindexi\\Code\\Configuration.coin";
-        var text = File.ReadAllText(file);
-        text = text.Replace("\r\n", "\n");
-        File.WriteAllText(file, text);
+
+        var fileConfigurationRepo = ConfigurationFactory.FromFile(file);
+        var appConfigurator = fileConfigurationRepo.CreateAppConfigurator();
+        string configurationString = appConfigurator.Default["NasSetupRepositoryFolderPath"];
+        Console.WriteLine(Directory.Exists(configurationString));
+
+        //var text = File.ReadAllText(file);
+        //var index = text.IndexOf("Y:\\Setup");
+        //if (index > 0)
+        //{
+        //    text = text.Substring(index);
+        //}
+        //text = text.Replace("\r\n", "\n");
+        //File.WriteAllText(file, text);
     }
 }
