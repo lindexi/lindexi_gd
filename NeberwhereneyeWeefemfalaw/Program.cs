b9a12563a69796843d999720a64172c90e9d3e71
lindexi <lindexi_gd@163.com>
2024-03-25 10:54:11 +0800

    尝试修复换行错误

---
+++ NeberwhereneyeWeefemfalaw/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NeberwhereneyeWeefemfalaw;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var file = "C:\\lindexi\\Code\\Configuration.coin";
+        var text = File.ReadAllText(file);
+        text = text.Replace("\r\n", "\n");
+        File.WriteAllText(file, text);
+    }
+}
