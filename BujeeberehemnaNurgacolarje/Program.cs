2903d3f4a0b6d25136f3a62c3a78fddef1c0c8fe
lindexi <lindexi_gd@163.com>
2024-03-27 17:33:46 +0800

    Revert "去掉 ColorMap 无法运行"
    This reverts commit e5a5ff39dc0b681e462a4b18e71c687dc2eac5cf.

--- BujeeberehemnaNurgacolarje/Program.cs
+++ BujeeberehemnaNurgacolarje/Program.cs
@@ -20,8 +20,7 @@ var valueMask = SetWindowValuemask.BackPixmap
                 | SetWindowValuemask.BitGravity
                 | SetWindowValuemask.WinGravity
                 | SetWindowValuemask.BackingStore
-                //| SetWindowValuemask.ColorMap
-                ;
+                | SetWindowValuemask.ColorMap;
 var attr = new XSetWindowAttributes
 {
     backing_store = 1,
@@ -52,7 +51,6 @@ while (XNextEvent(display, out var xEvent) == default)
     {
         XImage img = CreateImage();
         XPutImage(display, window, gc, ref img, 0, 0, Random.Shared.Next(100), Random.Shared.Next(100), (uint) img.width, (uint) img.height);
-        XClearArea(display, window, 0, 0, 500, 500, true);
     }
 }
 
