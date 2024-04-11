21c68b4ab86af507657f3fa3b9773e8e90060c2b
lindexi <lindexi_gd@163.com>
2024-04-11 10:59:06 +0800

    加上基础的配置功能

--- ReewheaberekaiNayweelehe/ReewheaberekaiNayweelehe/SkInkCanvas.cs
+++ ReewheaberekaiNayweelehe/ReewheaberekaiNayweelehe/SkInkCanvas.cs
@@ -11,7 +11,11 @@ record InkingInputInfo(int Id, StylusPoint StylusPoint, ulong Timestamp);
 
 record InkInfo(int Id);
 
-record SkInkCanvasSettings();
+/// <summary>
+/// 画板的配置
+/// </summary>
+/// <param name="EnableClippingEraser">是否允许使用裁剪方式的橡皮擦，而不是走静态笔迹层</param>
+record SkInkCanvasSettings(bool EnableClippingEraser = true);
 
 class SkInkCanvas
 {
