97bd39ddc9a0e5dd42c6e5d49fa56c0bec3a5417
lindexi <lindexi_gd@163.com>
2024-04-12 15:41:46 +0800

    Revert "歪楼测试一份文件"
    This reverts commit a5fe3a169560c308de093a92c7a0b070fc47ef59.

--- ReewheaberekaiNayweelehe/ReewheaberekaiNayweelehe/MainWindow.xaml.cs
+++ ReewheaberekaiNayweelehe/ReewheaberekaiNayweelehe/MainWindow.xaml.cs
@@ -15,7 +15,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
-using System.Xml.Linq;
 using Microsoft.Maui.Graphics;
 
 using SkiaSharp;
@@ -143,8 +142,7 @@ namespace ReewheaberekaiNayweelehe
             Background = Brushes.Black;
 
             var streamReader = File.OpenText(@"e:\download\下载器\2bdee3df56e2484c9364155d852f529a\Slides\Slide_0.xml");
-            var xDocument = XDocument.Load(streamReader);
-            var root = xDocument.Root;
+            var xmlReader = XmlReader.Create(streamReader);
         }
 
         private void MainWindow_TouchDown(object sender, TouchEventArgs e)
