6dba9f3c1c5eefbe6c333f198945a065064ebe41
lindexi <lindexi_gd@163.com>
2024-04-22 10:12:42 +0800

    使用别人的库解析 edid 信息

---
+++ BehairracercairJifelalihay/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EDIDParser;
+
+var file = "edid";
+
+var data = File.ReadAllBytes(file);
+var edid = new EDID(data);
+
+Console.WriteLine("Hello, World!");
