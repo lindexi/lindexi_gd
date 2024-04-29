03cb6891ff1c8d144934d12d2c1b4759209a5200
lindexi <lindexi_gd@163.com>
2024-04-29 16:12:36 +0800

    继续修改文件换行

--- FealafalnawhurHejerebaiki/Program.cs
+++ FealafalnawhurHejerebaiki/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var file = @"f:\temp\NerairkeweNaircayyawhem\dotnet\Dockerfile";
+var file = @"f:\temp\NerairkeweNaircayyawhem\dotnet\Script\run.sh";
 var text = File.ReadAllText(file);
 
 text = text.Replace("\r\n", "\n");
