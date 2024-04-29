55be6ec1b6ceb1134ff6f1d8950b80c67a69c0bf
lindexi <lindexi_gd@163.com>
2024-04-29 16:12:21 +0800

    修改文件换行

---
+++ FealafalnawhurHejerebaiki/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+var file = @"f:\temp\NerairkeweNaircayyawhem\dotnet\Dockerfile";
+var text = File.ReadAllText(file);
+
+text = text.Replace("\r\n", "\n");
+File.WriteAllText(file, text);
+
+Console.WriteLine("Hello, World!");
