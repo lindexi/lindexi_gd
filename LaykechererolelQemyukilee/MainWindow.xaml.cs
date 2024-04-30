1806c3b45f5f10242da97a2fc1e3fa433fca783d
lindexi <lindexi_gd@163.com>
2024-04-30 18:01:35 +0800

    Get the scan code from win32
    https://github.com/dotnet/wpf/issues/8473

--- LaykechererolelQemyukilee/MainWindow.xaml.cs
+++ LaykechererolelQemyukilee/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,9 +21,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        KeyDown += MainWindow_KeyDown;
     }
 
-    private void MainWindow_OnStylusUp(object sender, StylusEventArgs e)
+    private void MainWindow_KeyDown(object sender, KeyEventArgs e)
     {
+        var key = e.Key;
+        var virtualKey = KeyInterop.VirtualKeyFromKey(key);
+
+        // MAPVK_VK_TO_VSC 0
+        var scanCode = MapVirtualKeyW((uint) virtualKey, 0);
+
+        var scanCodeFromWpf = typeof(KeyEventArgs).GetProperty("ScanCode", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(e);
+
+        Debug.Assert(scanCode == (int) scanCodeFromWpf!);
     }
+
+    [DllImport("User32.dll")]
+    private static extern uint MapVirtualKeyW(uint code, uint mapType);
 }
