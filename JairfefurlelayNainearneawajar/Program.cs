3c7d0da35c1afbe4375ea775cf8c030ea0a22c2f
lindexi <lindexi_gd@163.com>
2024-05-01 10:25:10 +0800

    创建初始化项目

---
+++ JairfefurlelayNainearneawajar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JairfefurlelayNainearneawajar;
+internal class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        var application = new Application();
+        application.Startup += (sender, eventArgs) =>
+        {
+            var window = new Window();
+            window.Show();
+        };
+        application.Run();
+    }
+}
