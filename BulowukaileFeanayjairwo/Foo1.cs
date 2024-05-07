5d0273d110ba1e01e222c9931558e887d3d35f0f
lindexi <lindexi_gd@163.com>
2024-05-07 10:24:42 +0800

    加上简单的数组创建性能测试

--- BulowukaileFeanayjairwo/Foo1.cs
+++ BulowukaileFeanayjairwo/Foo1.cs
@@ -10,13 +10,21 @@ using BenchmarkDotNet.Order;
 namespace BulowukaileFeanayjairwo;
 
 [SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
-public class Foo1
+public class ArrayBenchmark
 {
-    [Benchmark]
-    public void Foo()
-    {
+    [Params(10, 100, 1_000, 10_000, 100_000, 1_000_000)]
+    public int ArraySize { get; set; }
 
-    }
+    [Benchmark(Baseline = true)]
+    public int[] NewArray() => new int[ArraySize];
+
+    [Benchmark]
+    public int[] GCZeroInitialized() => GC.AllocateArray<int>(ArraySize);
+
+    [Benchmark]
+    public int[] GCZeroUninitialized() => GC.AllocateUninitializedArray<int>(ArraySize);
+
+
 }
 
 [SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
