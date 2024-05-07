19a748da2cf9d359b20a4df8d028719e52bdeac6
lindexi <lindexi_gd@163.com>
2024-05-07 16:01:16 +0800

    测试版本相加

---
+++ ChewhearrearnearjelneaqoLallwegeeroqerecere/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+uint version = uint.MaxValue - 1;
+
+for (int i = 0; i < 100; i++)
+{
+    version++;
+}
+
+Console.WriteLine("Hello, World!");
