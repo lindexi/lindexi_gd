1e79b3a7527e6ae9a357ff423a48c6a00263ab2f
lindexi <lindexi_gd@163.com>
2024-05-07 10:20:42 +0800

    创建性能测试项目

---
+++ BulowukaileFeanayjairwo/Foo1.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Order;
+
+namespace BulowukaileFeanayjairwo;
+
+[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
+public class Foo1
+{
+    [Benchmark]
+    public void Foo()
+    {
+
+    }
+}
+
+[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
+public class Foo2
+{
+    [Benchmark]
+    public void Foo()
+    {
+
+    }
+}
