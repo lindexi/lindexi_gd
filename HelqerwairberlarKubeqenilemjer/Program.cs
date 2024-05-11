48a13dc82d73e84d3d4370bb95c39f3b4df43a51
lindexi <lindexi_gd@163.com>
2024-05-11 19:23:46 +0800

    测试其他方式输出

--- HelqerwairberlarKubeqenilemjer/Program.cs
+++ HelqerwairberlarKubeqenilemjer/Program.cs
@@ -6,9 +6,12 @@ using System.CommandLine.Rendering.Views;
 
 var gridView = new GridView()
 {
-    new ContentView("123")
 };
+gridView.SetColumns(ColumnDefinition.Fixed(100), ColumnDefinition.Star(1), ColumnDefinition.Star(1));
+gridView.SetChild(new ContentView("123"),0,0);
+gridView.SetChild(new ContentView("22"),1,0);
+gridView.SetChild(new ContentView("123"),2, 0);
 var consoleRenderer = new ConsoleRenderer(new SystemConsole());
-gridView.Render(consoleRenderer, null);
+gridView.Render(consoleRenderer, new Region(10,10,200,100));
 
 Console.Read();
