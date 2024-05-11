dc1ab78d5da55f02b7e267a60ff3038c37c26d9e
lindexi <lindexi_gd@163.com>
2024-05-11 19:20:01 +0800

    测试输出控制台

---
+++ HelqerwairberlarKubeqenilemjer/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.CommandLine.IO;
+using System.CommandLine.Rendering;
+using System.CommandLine.Rendering.Views;
+
+var systemConsoleTerminal = new SystemConsoleTerminal(new SystemConsole()
+{
+
+})
+{
+
+};
+systemConsoleTerminal.Render(new ContentSpan("123"), new Region(10, 10, 100, 100));
+
+
+
+Console.Read();
