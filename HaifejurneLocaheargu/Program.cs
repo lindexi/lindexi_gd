692e78bbe4bd415b47af1c11a7ae95eb9cbe44b1
lindexi <lindexi_gd@163.com>
2024-05-11 19:33:50 +0800

    列举文件夹

---
+++ HaifejurneLocaheargu/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var folder = @"C:\Program Files\dotnet\";
+
+foreach (var directory in Directory.EnumerateDirectories(folder,"*",SearchOption.AllDirectories))
+{
+    if (directory == @"C:\Program Files\dotnet\sdk")
+    {
+
+    }
+
+    Console.WriteLine(directory);
+}
+
+Console.WriteLine("Hello, World!");
