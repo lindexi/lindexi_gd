2976bdd7bf1468b9c3abe84cfd4d366db986eacd
lindexi <lindexi_gd@163.com>
2024-05-11 14:27:41 +0800

    测试使用 LibGit2Sharp 读取 git 信息

---
+++ FuwemqerejemWhejukelner/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LibGit2Sharp;
+
+var path = Path.GetFullPath(@"..\..\..\..\");
+var repository = new Repository(path);
+var commit = repository.Commits.First();
+Console.WriteLine(commit.Sha); // 第一个就是最后一个
+
+var commitCount = repository.Commits.Count();
+
+
+
+Console.WriteLine("Hello, World!");
