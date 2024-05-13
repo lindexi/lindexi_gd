c8354f643998d01eed8f56757a558623e4d94a8a
lindexi <lindexi_gd@163.com>
2024-05-13 16:24:05 +0800

    测试发送给相同进程的不同窗口

--- DikalehebeekaJaqunicobo/Program.cs
+++ DikalehebeekaJaqunicobo/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CPF.Linux;
+using System;
 using static CPF.Linux.XLib;
 
 var display = XOpenDisplay(IntPtr.Zero);
 var screen = XDefaultScreen(display);
 
-var white = XWhitePixel(display, screen);
 var black = XBlackPixel(display, screen);
 
 var rootWindow = XDefaultRootWindow(display);
@@ -36,35 +36,30 @@ var xSetWindowAttributes = new XSetWindowAttributes
     background_pixel = 0,
 };
 
-var xDisplayWidth = XDisplayWidth(display, screen);
-var xDisplayHeight = XDisplayHeight(display, screen);
+var xDisplayWidth = XDisplayWidth(display, screen) / 2;
+var xDisplayHeight = XDisplayHeight(display, screen) / 2;
 var handle = XCreateWindow(display, rootWindow, 0, 0, xDisplayWidth, xDisplayHeight, 5,
+    32,
+    (int)CreateWindowArgs.InputOutput,
+    visual,
+    (nuint)valueMask, ref xSetWindowAttributes);
+
+
+var window1 = new FooWindow(handle, display);
+
+
+var window2 = new FooWindow(XCreateWindow(display, rootWindow, 0, 0, xDisplayWidth, xDisplayHeight, 5,
     32,
     (int) CreateWindowArgs.InputOutput,
     visual,
-    (nuint) valueMask, ref xSetWindowAttributes);
+    (nuint) valueMask, ref xSetWindowAttributes), display);
 
-var window = handle;
-
-XEventMask ignoredMask = XEventMask.SubstructureRedirectMask | XEventMask.ResizeRedirectMask |
-                         XEventMask.PointerMotionHintMask;
-var mask = new IntPtr(0xffffff ^ (int) ignoredMask);
-XSelectInput(display, window, mask);
-
-XMapWindow(display, window);
-XFlush(display);
-
-var gc = XCreateGC(display, window, 0, 0);
-XSetForeground(display, gc, white);
 
 //XSetInputFocus(Display, Window, 0, IntPtr.Zero);
 
 XSync(display, false);
 
-Task.Run(() =>
-{
-
-});
+Task.Run(() => { });
 
 while (true)
 {
@@ -79,13 +74,11 @@ while (true)
         var x = @event.MotionEvent.x;
         var y = @event.MotionEvent.y;
 
-        XDrawLine(display, @event.MotionEvent.window, gc, x, y, x + 100, y);
-    }
-
-    var count = XEventsQueued(display, 0 /*QueuedAlready*/);
-    if (count == 0)
-    {
-        for (int i = 0; i < 100; i++)
+        if (@event.MotionEvent.window == window1.Window)
+        {
+            XDrawLine(display, window1.Window, window1.GC, x, y, x + 100, y);
+        }
+        else
         {
             var xEvent = new XEvent
             {
@@ -93,15 +86,43 @@ while (true)
                 {
                     type = XEventName.MotionNotify,
                     send_event = true,
-                    window = window,
+                    window = window1.Window,
                     display = display,
-                    x = i,
-                    y = i
+                    x = x,
+                    y = y
                 }
             };
-            XSendEvent(display, window, propagate: false, new IntPtr((int) (EventMask.ButtonMotionMask)), ref xEvent);
+            XSendEvent(display, window1.Window, propagate: false, new IntPtr((int)(EventMask.ButtonMotionMask)),
+                ref xEvent);
         }
     }
 }
 
 Console.WriteLine("Hello, World!");
+
+class FooWindow
+{
+    public FooWindow(nint windowHandle, nint display)
+    {
+        Window = windowHandle;
+
+        XEventMask ignoredMask = XEventMask.SubstructureRedirectMask | XEventMask.ResizeRedirectMask |
+                                 XEventMask.PointerMotionHintMask;
+        var mask = new IntPtr(0xffffff ^ (int)ignoredMask);
+        XSelectInput(display, windowHandle, mask);
+
+        XMapWindow(display, windowHandle);
+        XFlush(display);
+
+        var screen = XDefaultScreen(display);
+        var white = XWhitePixel(display, screen);
+
+        var gc = XCreateGC(display, windowHandle, 0, 0);
+        XSetForeground(display, gc, white);
+
+        GC = gc;
+    }
+
+    public nint Window { get; }
+    public IntPtr GC { get; }
+}
