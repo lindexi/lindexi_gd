4ae5a45eb65cab5a3b9f8991852be9602dee6533
lindexi <lindexi_gd@163.com>
2024-05-14 14:58:11 +0800

    获取 gtk 的 x11 窗口

--- LejarkeebemCowakiwhanar/Program.cs
+++ LejarkeebemCowakiwhanar/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Gdk;
 using Gtk;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 using Window = Gtk.Window;
 using WindowType = Gtk.WindowType;
 
@@ -15,7 +17,7 @@ internal class Program
         _app = new Application("org.Samples.Samples", GLib.ApplicationFlags.None);
         _app.Register(GLib.Cancellable.Current);
 
-        _win = new MainWindow("Demo Window");
+        _win = new MainWindow("lindexi shi doubi");
         _app.AddWindow(_win);
 
         _win.ShowAll();
@@ -38,13 +40,56 @@ class MainWindow : Window
 
     protected override void OnShown()
     {
-        Console.WriteLine("OnShown");
-        base.OnShown();
+        base.OnShown(); // 在这句话调用之前 window.Window 是空
+
+        Window window = this;
+        Gdk.Window gdkWindow = window.Window;
+
+        if (gdkWindow is null)
+        {
+            // 确保 base.OnShown 调用
+            Console.WriteLine($"gdkWindow is null");
+            return;
+        }
+
+        var x11 = gdk_x11_window_get_xid(gdkWindow.Handle);
+        Console.WriteLine($"X11 窗口 0x{x11:x2}");
     }
 
-    protected override void OnActivate()
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    delegate IntPtr d_gdk_x11_window_get_xid(IntPtr gdkWindow);
+
+    private static d_gdk_x11_window_get_xid gdk_x11_window_get_xid =
+        LoadFunction<d_gdk_x11_window_get_xid>("libgdk-3.so.0", "gdk_x11_window_get_xid");
+
+    private static T LoadFunction<T>(string libName, string functionName)
     {
-        Console.WriteLine("OnActivate");
-        base.OnActivate();
+        if (!OperatingSystem.IsLinux())
+        {
+            // 防止炸调试
+            return default(T)!;
+        }
+
+        // LoadLibrary
+        var libPtr = Linux.dlopen(libName, RTLD_GLOBAL | RTLD_LAZY);
+
+        Console.WriteLine($"Load {libName} ptr={libPtr}");
+
+        // GetProcAddress
+        var procAddress = Linux.dlsym(libPtr, functionName);
+        Console.WriteLine($"Load {functionName} ptr={procAddress}");
+        return Marshal.GetDelegateForFunctionPointer<T>(procAddress);
+    }
+
+    private const int RTLD_LAZY = 0x0001;
+    private const int RTLD_GLOBAL = 0x0100;
+
+    private class Linux
+    {
+        [DllImport("libdl.so.2")]
+        public static extern IntPtr dlopen(string path, int flags);
+
+        [DllImport("libdl.so.2")]
+        public static extern IntPtr dlsym(IntPtr handle, string symbol);
     }
 }
