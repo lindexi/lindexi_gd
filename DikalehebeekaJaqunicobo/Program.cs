cb1e35e7c3e42b2d46ce831f058b546381102ea5
lindexi <lindexi_gd@163.com>
2024-05-15 11:49:49 +0800

    Reapply "更换创建窗口方法"
    This reverts commit 0e9cf082c9598b317654beec8707dd23d5a94f83.

--- DikalehebeekaJaqunicobo/Program.cs
+++ DikalehebeekaJaqunicobo/Program.cs
@@ -119,11 +119,7 @@ _ = Task.Run(async () =>
         var mainWindowHandle = handle;
 
         // 再创建另一个窗口设置 Owner-Owned 关系
-        var childWindowHandle = XCreateWindow(display, rootWindow, 0, 0, xDisplayWidth, xDisplayHeight, 5,
-            32,
-            (int) CreateWindowArgs.InputOutput,
-            visual,
-            (nuint) valueMask, ref xSetWindowAttributes);
+        var childWindowHandle = XCreateSimpleWindow(display, rootWindow, 0, 0, 300, 300, 5, white, black);
         XMapWindow(display, childWindowHandle);
 
         // 设置拥有关系
