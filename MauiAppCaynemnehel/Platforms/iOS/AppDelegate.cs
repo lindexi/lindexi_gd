742d59fc07f6615d990bcfe08ec460b8ce16b40a
lindexi <lindexi_gd@163.com>
2024-05-16 20:26:02 +0800

    删除测试项目

--- MauiAppCaynemnehel/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace MauiAppCaynemnehel;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
