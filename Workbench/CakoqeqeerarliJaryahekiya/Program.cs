756fd3d2289172c87df7a6751d56b418c2904681
lindexi <lindexi_gd@163.com>
2024-05-24 19:58:42 +0800

    创建项目调用有道

---
+++ Workbench/CakoqeqeerarliJaryahekiya/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var httpClient = new HttpClient();
+
+var word = "example";
+var url =
+    $"https://dict.youdao.com/jsonapi?xmlVersion=5.1&client=mobile&q={word}&dicts=&keyfrom=&model=&mid=&imei=&vendor=&screen=&ssid=&network=5g&abtest=&jsonversion=2";
+
+var text = await httpClient.GetStringAsync(url);
+
+
+Console.WriteLine("Hello, World!");
