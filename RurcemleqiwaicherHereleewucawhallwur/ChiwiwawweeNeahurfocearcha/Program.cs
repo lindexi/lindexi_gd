c5ab869cf186f8a63b57fb1c014f9bb03b3f1efd
lindexi <lindexi_gd@163.com>
2024-05-26 16:28:02 +0800

    证明判断条件的方法

--- RurcemleqiwaicherHereleewucawhallwur/ChiwiwawweeNeahurfocearcha/Program.cs
+++ RurcemleqiwaicherHereleewucawhallwur/ChiwiwawweeNeahurfocearcha/Program.cs
@@ -1,6 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices;
+
 var byteSize = sizeof(byte);
 var boolSize = sizeof(bool);
 
+var foo = new Foo();
+Console.WriteLine(foo.A);
+
+foo.B = 100;
+Console.WriteLine(foo.A);
+
 Console.WriteLine("Hello, World!");
+
+[StructLayout(LayoutKind.Explicit)]
+struct Foo
+{
+    [FieldOffset(0)]
+    public bool A;
+
+    [FieldOffset(0)]
+    public byte B;
+}
