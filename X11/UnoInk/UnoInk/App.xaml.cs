6fa372d67c44b87b18e7f146ce373222b73015f4
lindexi <lindexi_gd@163.com>
2024-05-27 14:14:52 +0800

    尝试设置内容为红色，也是会闪烁一下，从白色闪烁为红色

--- X11/UnoInk/UnoInk/App.xaml.cs
+++ X11/UnoInk/UnoInk/App.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Uno.Resizetizer;
 
@@ -25,28 +26,31 @@ public partial class App : Application
 #if DEBUG
         MainWindow.EnableHotReload();
 #endif
-
-
-        // Do not repeat app initialization when the Window already has content,
-        // just ensure that the window is active
-        if (MainWindow.Content is not Frame rootFrame)
+        MainWindow.Content = new Grid()
         {
-            // Create a Frame to act as the navigation context and navigate to the first page
-            rootFrame = new Frame();
+            Background = new SolidColorBrush(Colors.Red)
+        };
 
-            // Place the frame in the current Window
-            MainWindow.Content = rootFrame;
+        //// Do not repeat app initialization when the Window already has content,
+        //// just ensure that the window is active
+        //if (MainWindow.Content is not Frame rootFrame)
+        //{
+        //    // Create a Frame to act as the navigation context and navigate to the first page
+        //    rootFrame = new Frame();
 
-            rootFrame.NavigationFailed += OnNavigationFailed;
-        }
+        //    // Place the frame in the current Window
+        //    MainWindow.Content = rootFrame;
 
-        if (rootFrame.Content == null)
-        {
-            // When the navigation stack isn't restored navigate to the first page,
-            // configuring the new page by passing required information as a navigation
-            // parameter
-            rootFrame.Navigate(typeof(MainPage), args.Arguments);
-        }
+        //    rootFrame.NavigationFailed += OnNavigationFailed;
+        //}
+
+        //if (rootFrame.Content == null)
+        //{
+        //    // When the navigation stack isn't restored navigate to the first page,
+        //    // configuring the new page by passing required information as a navigation
+        //    // parameter
+        //    rootFrame.Navigate(typeof(MainPage), args.Arguments);
+        //}
 
         MainWindow.SetWindowIcon();
         // Ensure the current window is active
