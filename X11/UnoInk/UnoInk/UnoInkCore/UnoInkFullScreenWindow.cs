91402956139c84c5fb8d603bf6f2ca85223c3bc5
lindexi <lindexi_gd@163.com>
2024-05-28 14:44:49 +0800

    尝试修复构建

--- X11/UnoInk/UnoInk/UnoInkCore/UnoInkFullScreenWindow.cs
+++ X11/UnoInk/UnoInk/UnoInkCore/UnoInkFullScreenWindow.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.UI;
-using Uno.UI.Xaml;
 using UnoHacker;
+
+#if HAS_UNO
+using Uno.UI.Xaml;
+#endif
 
 namespace UnoInk.UnoInkCore;
 
