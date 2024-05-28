4576d25a281732a498f68ce9afdb6fbed0ee651a
lindexi <lindexi_gd@163.com>
2024-05-28 11:24:11 +0800

    修复背景透明

--- X11/UnoInk/UnoInk/App.xaml.cs
+++ X11/UnoInk/UnoInk/App.xaml.cs
@@ -75,6 +75,8 @@ public partial class App : Application
             rootFrame.Navigate(typeof(MainPage), args.Arguments);
         }
 
+        // 背景透明需要 UNO 还没发布的版本
+        // https://github.com/lindexi/uno/tree/7b282851a8ec3ed7eb42a53af8b50ea7fe045d56
         // 这句话似乎才是关键，设置窗口背景透明。通过 MainWindow.SetBackground 配置是无效的
         Hacker.Do();
 
