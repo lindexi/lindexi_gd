8725d73c6dec3f0e22531e88d3c2af89d3e342c7
lindexi <lindexi_gd@163.com>
2024-05-28 11:28:40 +0800

    Revert "测试是否可以有鼠标"
    This reverts commit b7b432fad48f0c85c9ac7157c2424e52882c2f31.

--- X11/UnoInk/UnoInk/MainPage.xaml.cs
+++ X11/UnoInk/UnoInk/MainPage.xaml.cs
@@ -46,7 +46,7 @@ public sealed partial class MainPage : Page
         //DrawStroke(inkInfo);
         //DrawInNative(position);
 
-        LogTextBlock.Text += $"按下： {e.Pointer.PointerId}\r\n";
+        //LogTextBlock.Text += $"按下： {e.Pointer.PointerId}\r\n";
         //LogTextBlock.Text += $"当前按下点数： {_inkInfoCache.Count} [{string.Join(',', _inkInfoCache.Keys)}]";
         Console.WriteLine($"按下： {e.Pointer.PointerId}");
 
@@ -99,7 +99,7 @@ public sealed partial class MainPage : Page
         //    DrawStroke(inkInfo);
         //}
 
-        LogTextBlock.Text += $"抬起： {e.Pointer.PointerId}\r\n";
+        //LogTextBlock.Text += $"抬起： {e.Pointer.PointerId}\r\n";
         //LogTextBlock.Text += $"当前按下点数： {_inkInfoCache.Count} [{string.Join(',', _inkInfoCache.Keys)}]";
         InvokeAsync(canvas =>
         {
