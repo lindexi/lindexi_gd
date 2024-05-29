7ea90da73d18db9b8891d621d854af671986d038
lindexi <lindexi_gd@163.com>
2024-05-29 14:28:45 +0800

    尝试进入全屏
    窗口不渲染

--- X11/UnoInk/UnoInk/X11Ink/X11InkProvider.cs
+++ X11/UnoInk/UnoInk/X11Ink/X11InkProvider.cs
@@ -19,7 +19,7 @@ namespace UnoInk.X11Ink;
 //interface IX11WindowManager
 
 [SupportedOSPlatform("Linux")]
-internal class X11InkProvider: X11Application
+internal class X11InkProvider : X11Application
 {
     public X11InkProvider()
     {
@@ -47,8 +47,10 @@ internal class X11InkProvider: X11Application
         // Uno.WinUI.Runtime.Skia.X11.X11Window
         var x11WindowType = x11Window.GetType();
 
-        var x11WindowIntPtr = (IntPtr) x11WindowType.GetProperty("Window", BindingFlags.Instance | BindingFlags.Public)!.GetMethod!.Invoke(x11Window, null)!;
-        
+        var x11WindowIntPtr =
+            (IntPtr) x11WindowType.GetProperty("Window", BindingFlags.Instance | BindingFlags.Public)!.GetMethod!.Invoke(
+                x11Window, null)!;
+
         Console.WriteLine($"Uno 窗口句柄 {x11WindowIntPtr}");
 
         var x11InkWindow = new X11InkWindow(this, x11WindowIntPtr);
@@ -63,7 +65,7 @@ internal class X11InkProvider: X11Application
             return _x11InkWindow;
         }
     }
-    
+
     private X11InkWindow? _x11InkWindow;
 
     [MemberNotNull(nameof(_x11InkWindow))]
@@ -74,7 +76,7 @@ internal class X11InkProvider: X11Application
             throw new InvalidOperationException();
         }
     }
-    
+
     internal override void DispatchEvent(XEvent @event)
     {
         if (@event.type == XEventName.Expose)
@@ -93,56 +95,55 @@ internal class X11InkProvider: X11Application
 [SupportedOSPlatform("Linux")]
 class X11InkWindow : X11Window
 {
-    public X11InkWindow(X11Application application, IntPtr mainWindowHandle):base(application, new X11WindowCreateInfo()
-    {
-        IsFullScreen = true
-    })
+    public X11InkWindow(X11Application application, IntPtr mainWindowHandle) : base(application,
+        new X11WindowCreateInfo()
+        {
+            IsFullScreen = true
+        })
     {
         application.EnsureStart();
         X11PlatformThreading = application.X11PlatformThreading;
         var x11Info = application.X11Info;
         _x11Info = x11Info;
         _mainWindowHandle = mainWindowHandle;
-        var display = x11Info.Display;
-        var rootWindow = x11Info.RootWindow;
-        var screen = x11Info.Screen;
 
         var xDisplayWidth = x11Info.XDisplayWidth;
         var xDisplayHeight = x11Info.XDisplayHeight;
 
-       
         // 设置不接受输入
         // 这样输入穿透到后面一层里，由后面一层将内容上报上来
         SetClickThrough();
-        
-        
+
         // 设置一定放在输入的窗口上方
         SetOwner(mainWindowHandle);
 
+        // 进入全屏
+        EnterFullScreen(); 
+
         ShowActive();
-        
+
         var skBitmap = new SKBitmap(xDisplayWidth, xDisplayHeight, SKColorType.Bgra8888, SKAlphaType.Premul);
         _skBitmap = skBitmap;
         var skCanvas = new SKCanvas(skBitmap);
         _skCanvas = skCanvas;
-        
+
         XImage image = CreateImage();
         _image = image;
 
         // 读取屏幕物理尺寸，用于实现橡皮擦功能
         //UpdateScreenPhysicalSize();
-        
+
         var skInkCanvas = // new SkInkCanvas(_skCanvas, _skBitmap);
             new SkInkCanvas();
         skInkCanvas.ApplicationDrawingSkBitmap = _skBitmap;
         skInkCanvas.SetCanvas(_skCanvas);
-        
+
         skInkCanvas.Settings = skInkCanvas.Settings with
         {
             AutoSoftPen = false,
             //EnableEraserGesture = false,
         };
-        
+
         skInkCanvas.RenderBoundsChanged += (sender, rect) =>
         {
             //if (PutImageBeforeExposeOnRenderBoundsChanged)
@@ -151,12 +152,12 @@ class X11InkWindow : X11Window
             //    var y = (int) rect.Y;
             //    var width = (int) rect.Width;
             //    var height = (int) rect.Height;
-                
+
             //    // 曝光之前推送图片
             //    XPutImage(Display, Window, GC, ref _image, x, y, x, y, (uint) width,
             //        (uint) height);
             //}
-            
+
             var xEvent = new XEvent
             {
                 ExposeEvent =
@@ -173,28 +174,29 @@ class X11InkWindow : X11Window
                 }
             };
             // [Xlib Programming Manual: Expose Events](https://tronche.com/gui/x/xlib/events/exposure/expose.html )
-            XSendEvent(x11Info.Display, X11InkWindowIntPtr, propagate: false, new IntPtr((int) (EventMask.ExposureMask)), ref xEvent);
+            XSendEvent(x11Info.Display, X11InkWindowIntPtr, propagate: false, new IntPtr((int) (EventMask.ExposureMask)),
+                ref xEvent);
         };
         SkInkCanvas = skInkCanvas;
     }
 
     public X11PlatformThreading X11PlatformThreading { get; }
-    
+
     private readonly X11Info _x11Info;
     private readonly IntPtr _mainWindowHandle;
     private readonly SKBitmap _skBitmap;
     private readonly SKCanvas _skCanvas;
     private XImage _image;
     public SkInkCanvas SkInkCanvas { get; }
-    
+
     private unsafe XImage CreateImage()
     {
         const int bytePerPixelCount = 4; // RGBA 一共4个 byte 长度
         var bitPerByte = 8;
-        
+
         var bitmapWidth = _skBitmap.Width;
         var bitmapHeight = _skBitmap.Height;
-        
+
         var img = new XImage();
         int bitsPerPixel = bytePerPixelCount * bitPerByte;
         img.width = bitmapWidth;
@@ -209,26 +211,21 @@ class X11InkWindow : X11Window
         img.bytes_per_line = bitmapWidth * bytePerPixelCount;
         img.bits_per_pixel = bitsPerPixel;
         XInitImage(ref img);
-        
+
         return img;
     }
-    
+
     public IntPtr X11InkWindowIntPtr => X11WindowIntPtr;
-    
+
     public Task InvokeAsync(Action<SkInkCanvas> action)
     {
-       return X11PlatformThreading.InvokeAsync(() =>
-       {
-           action(SkInkCanvas);
-       }, X11InkWindowIntPtr);
+        return X11PlatformThreading.InvokeAsync(() => { action(SkInkCanvas); }, X11InkWindowIntPtr);
     }
-    
+
     public void Expose(XExposeEvent exposeEvent)
     {
-        XPutImage(_x11Info.Display, X11InkWindowIntPtr, GC, ref _image, exposeEvent.x, exposeEvent.y, exposeEvent.x, exposeEvent.y, (uint) exposeEvent.width,
+        XPutImage(_x11Info.Display, X11InkWindowIntPtr, GC, ref _image, exposeEvent.x, exposeEvent.y, exposeEvent.x,
+            exposeEvent.y, (uint) exposeEvent.width,
             (uint) exposeEvent.height);
     }
-    
 }
-
-
