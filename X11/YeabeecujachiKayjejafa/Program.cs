ab5c104aa0ed5cc3034c3ee79ad6a1f6eb11ad98
lindexi <lindexi_gd@163.com>
2024-05-30 18:58:21 +0800

    Revert "测试基础库在其他平台运行"
    This reverts commit 2aa5146cb95cf305dddc828872b95408c018fc84.

--- X11/YeabeecujachiKayjejafa/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Lindexi.Src.GitCommand;
-
-var git = new Git(new DirectoryInfo("."));
-Console.WriteLine(git.GetCurrentCommit());
-
-Console.WriteLine("Hello, World!");
