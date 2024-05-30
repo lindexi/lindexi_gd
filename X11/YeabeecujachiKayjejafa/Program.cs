2aa5146cb95cf305dddc828872b95408c018fc84
lindexi <lindexi_gd@163.com>
2024-05-30 18:50:25 +0800

    测试基础库在其他平台运行

---
+++ X11/YeabeecujachiKayjejafa/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lindexi.Src.GitCommand;
+
+var git = new Git(new DirectoryInfo("."));
+Console.WriteLine(git.GetCurrentCommit());
+
+Console.WriteLine("Hello, World!");
