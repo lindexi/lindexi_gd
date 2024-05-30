98d5326726fca5689d8a9ce257402c2748659c2c
lindexi <lindexi_gd@163.com>
2024-05-30 09:23:30 +0800

    修改命名空间

--- X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkCanvasUserControl.xaml.cs
+++ X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkCanvasUserControl.xaml.cs
@@ -1,30 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using BujeeberehemnaNurgacolarje;
+using Microsoft.UI.Xaml.Input;
 using SkiaSharp;
 using SkiaSharp.Views.Windows;
 using UnoInk.Inking.InkCore;
 using UnoInk.Inking.X11Ink;
 using UnoInk.Inking.X11Platforms.Threading;
-using Rect = Microsoft.Maui.Graphics.Rect;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace UnoInk.UnoInkCore;
+namespace UnoInk.Inking.UnoInkCore;
 
 public sealed partial class UnoInkCanvasUserControl : UserControl
 {
