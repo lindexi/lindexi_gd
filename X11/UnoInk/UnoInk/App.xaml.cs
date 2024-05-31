646e8beadb2267640c980fc6e39baae7a7170876
lindexi <lindexi_gd@163.com>
2024-05-31 14:48:30 +0800

    去掉无效代码

--- X11/UnoInk/UnoInk/App.xaml.cs
+++ X11/UnoInk/UnoInk/App.xaml.cs
@@ -33,11 +33,12 @@ public partial class App : Application
 
         var unoInkWindow = new UnoInkFullScreenWindow();
         unoInkWindow.Activate();
-        unoInkWindow.AppWindow.Move(new PointInt32()
-        {
-            X = 100,
-            Y = 100
-        });
+        // 这是无效
+        //unoInkWindow.AppWindow.Move(new PointInt32()
+        //{
+        //    X = 100,
+        //    Y = 100
+        //});
         return;
         
         Console.WriteLine($"Before new Window");
