02444de103c344f8ec0fc698ef20a1f715e1cc07
lindexi <lindexi_gd@163.com>
2024-05-31 11:08:08 +0800

    move it to a Loaded handler of the window content
    https://github.com/unoplatform/uno/issues/16881

---
+++ X11/UnoInk/UnoInk/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace UnoInk;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+}
