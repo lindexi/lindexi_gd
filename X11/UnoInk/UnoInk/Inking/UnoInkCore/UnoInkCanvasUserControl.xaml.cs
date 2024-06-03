3eb2a5f856307ffa6b0c82ea88bbb86051e35f49
lindexi <lindexi_gd@163.com>
2024-06-03 16:19:53 +0800

    注释不使用代码

--- X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkCanvasUserControl.xaml.cs
+++ X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkCanvasUserControl.xaml.cs
@@ -61,7 +61,13 @@ public sealed partial class UnoInkCanvasUserControl : UserControl
     private void SkInkCanvas_StrokesCollected(object? sender, StrokesCollectionInfo e)
     {
         // 这是 X11 线程进入的
+        lock (StrokeInfoList)
+        {
+            StrokeInfoList.Add(e);
+        }
     }
+    
+    private List<StrokesCollectionInfo> StrokeInfoList { get; } = new List<StrokesCollectionInfo>();
 
     private void InvokeInk()
     {
@@ -173,14 +179,14 @@ public sealed partial class UnoInkCanvasUserControl : UserControl
             //    return;
             //}
             canvas.ModeInputDispatcher.Up(ToModeInputArgs(e));
-            _skPathList.AddRange(canvas.CurrentInkStrokePathEnumerable);
+            //_skPathList.AddRange(canvas.CurrentInkStrokePathEnumerable);
             //canvas.Up(ToInkingInputInfo(e));
 
             SkXamlCanvas.Invalidate();
         });
     }
 
-    private readonly List<string> _skPathList = [];
+    //private readonly List<string> _skPathList = [];
 
     //private readonly Dictionary<uint /*PointerId*/, InkInfo> _inkInfoCache = new Dictionary<uint, InkInfo>();
 
@@ -269,18 +275,18 @@ public sealed partial class UnoInkCanvasUserControl : UserControl
         }
 
 
-        foreach (var skPath in _skPathList)
-        {
-            Console.WriteLine($"准备到 UNO 绘制");
-            // 需要进行序列化和反序列化是为了解决跨线程访问 SKPath 导致爆的问题
-            // 可以切到 c82dcaf20da0948aede539b699f47926635b94a3 进行测试
-            // 写一笔就能复现
-            var path = SKPath.ParseSvgPathData(skPath);
-            e.Surface.Canvas.DrawPath(path, skPaint);
-        }
+        //foreach (var skPath in _skPathList)
+        //{
+        //    Console.WriteLine($"准备到 UNO 绘制");
+        //    // 需要进行序列化和反序列化是为了解决跨线程访问 SKPath 导致爆的问题
+        //    // 可以切到 c82dcaf20da0948aede539b699f47926635b94a3 进行测试
+        //    // 写一笔就能复现
+        //    var path = SKPath.ParseSvgPathData(skPath);
+        //    e.Surface.Canvas.DrawPath(path, skPaint);
+        //}
         Console.WriteLine($"完成 UNO 绘制");
 
-        _skPathList.Clear();
+        //_skPathList.Clear();
 
         // 清空笔迹，换成在 UNO 层绘制
         InvokeAsync(canvas =>
