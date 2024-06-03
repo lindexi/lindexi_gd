50a28c770330d12b5ce0d7762af65bd57ea1b959
lindexi <lindexi_gd@163.com>
2024-06-03 16:06:45 +0800

    尝试刷出但是无效

--- X11/UnoInk/UnoInk/Inking/X11Ink/X11InkWindow.cs
+++ X11/UnoInk/UnoInk/Inking/X11Ink/X11InkWindow.cs
@@ -8,6 +8,8 @@ using UnoInk.Inking.X11Platforms;
 using UnoInk.Inking.X11Platforms.Input;
 using UnoInk.Inking.X11Platforms.Threading;
 
+using static CPF.Linux.XLib;
+
 namespace UnoInk.Inking.X11Ink;
 
 [SupportedOSPlatform("Linux")]
@@ -39,6 +41,8 @@ class X11InkWindow : X11Window
             RegisterMultiTouch(pointerDevice);
             Console.WriteLine($"注册RegisterMultiTouch");
         }
+        
+        XFlush(x11Info.Display);
 
         // 设置不接受输入
         // 这样输入穿透到后面一层里，由后面一层将内容上报上来
