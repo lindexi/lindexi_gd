7893b62222d04b1548c8aabcf6578e3bf2eb323f
lindexi <lindexi_gd@163.com>
2024-06-04 20:20:49 +0800

    测试编码

---
+++ QawhifabaheLerjijallfal/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var encoding = Encoding.GetEncoding("GBK");
+var text = encoding.GetString(new byte[] { 0xcb, 0xe6,0xd7,0xc5 });
+
+Console.WriteLine("Hello, World!");
