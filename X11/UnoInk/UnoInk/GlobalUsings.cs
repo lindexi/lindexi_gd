c2dacdd417122e29171007ecb178c6e91f0f887c
lindexi <lindexi_gd@163.com>
2024-06-04 11:46:08 +0800

    创建项目

---
+++ X11/UnoInk/UnoInk/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using System.Collections.Immutable;
+
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Localization;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+
+global using UnoInk.Models;
+global using UnoInk.Presentation;
+
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
