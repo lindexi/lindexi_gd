c6cc611d6dd0bec19411551dda1ae3db4399d86f
lindexi <lindexi_gd@163.com>
2024-06-04 20:52:30 +0800

    测试内存炸了

--- X11/UnoInk/UnoInk/Presentation/MainPage.xaml.cs
+++ X11/UnoInk/UnoInk/Presentation/MainPage.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace UnoInk.Presentation;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Uno.Disposables;
+
+namespace UnoInk.Presentation;
 
 public sealed partial class MainPage : Page
 {
     public MainPage()
     {
         this.InitializeComponent();
+    }
+    
+    private async void SnapButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var renderTargetBitmap = new RenderTargetBitmap();
+        renderTargetBitmap.RenderAsync(this);
+        var pixelBuffer = await renderTargetBitmap.GetPixelsAsync();
+        // +		pixelBuffer	{Windows.Storage.Streams.Buffer}	Windows.Storage.Streams.IBuffer {Windows.Storage.Streams.Buffer}
+        if (pixelBuffer is Windows.Storage.Streams.Buffer buffer)
+        {
+        }
+        renderTargetBitmap.TryDispose();
     }
 }
