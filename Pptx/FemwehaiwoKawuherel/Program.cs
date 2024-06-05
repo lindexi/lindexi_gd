ad5eb99718d332795324c0ecf42bd3f321c0ef5b
lindexi <lindexi_gd@163.com>
2024-06-05 20:07:08 +0800

    Test code base with comment
    https://github.com/dotnet/wpf/pull/9187

---
+++ Pptx/FemwehaiwoKawuherel/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var folder = @"C:\lindexi\Code\WPF\";
+
+foreach (var file in Directory.EnumerateFiles(folder,"*.cs",SearchOption.AllDirectories))
+{
+    foreach (var line in File.ReadAllLines(file))
+    {
+        if (line.Contains(" <item>"))
+        {
+            continue;
+        }
+
+        var match = Regex.Match(line, @"///[\W\w]*base\.");
+        if (match.Success)
+        {
+            
+        }
+    }
+}
+
+Console.WriteLine("Hello, World!");
