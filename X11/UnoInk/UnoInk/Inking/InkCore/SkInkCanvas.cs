159c57132caff2e3ba7779cd8a363c19b4b3aa3d
lindexi <lindexi_gd@163.com>
2024-06-05 17:21:02 +0800

    尝试加上调试界面

--- X11/UnoInk/UnoInk/Inking/InkCore/SkInkCanvas.cs
+++ X11/UnoInk/UnoInk/Inking/InkCore/SkInkCanvas.cs
@@ -443,7 +443,7 @@ class SkInkCanvas : IInputProcessor, IModeInputDispatcherSensitive
 
     private bool DrawStroke(DrawStrokeContext context, out Rect drawRect)
     {
-        StaticDebugLogger.WriteLine($"DrawStroke {context.InputInfo.StylusPoint.Point}");
+        //StaticDebugLogger.WriteLine($"DrawStroke {context.InputInfo.StylusPoint.Point}");
 
         StylusPoint currentStylusPoint = context.InputInfo.StylusPoint;
 
--- X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkFullScreenWindow.cs
+++ X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkFullScreenWindow.cs
@@ -13,12 +13,18 @@ public partial class UnoInkFullScreenWindow : Window
         this.EnableHotReload();
 #endif
 
+        if (OperatingSystem.IsWindows())
+        {
+            Content = new DebugInkUserControl();
+            return;
+        }
+
 #if HAS_UNO
         // 这句话似乎也是无效的
         this.SetBackground(new SolidColorBrush(Colors.Transparent));
         this.AppWindow.GetApplicationView().TryEnterFullScreenMode();
 #endif
-        
+
         // 背景透明需要 UNO 还没发布的版本
         // https://github.com/lindexi/uno/tree/7b282851a8ec3ed7eb42a53af8b50ea7fe045d56
         // 这句话似乎才是关键，设置窗口背景透明。通过 MainWindow.SetBackground 配置是无效的
