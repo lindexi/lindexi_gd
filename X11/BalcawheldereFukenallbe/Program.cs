fdc4816aba71f2cc6bfdcb6c7ca0bba2f0ac2b8c
lindexi <lindexi_gd@163.com>
2024-06-06 19:47:09 +0800

    等待附加调试应用启动

---
+++ X11/BalcawheldereFukenallbe/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+while (!Debugger.IsAttached)
+{
+    await Task.Delay(100);
+}
+Debugger.Break();
+
+Console.WriteLine("Hello, World!");
