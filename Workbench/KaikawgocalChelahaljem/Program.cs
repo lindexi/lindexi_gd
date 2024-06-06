4e7a7d9887c774e6325257da0517d943bb2ee0e2
lindexi <lindexi_gd@163.com>
2024-06-06 17:39:21 +0800

    测试获取不重复的数组

---
+++ Workbench/KaikawgocalChelahaljem/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var list = new List<string>()
+{
+    "aa",
+    "aa",
+    "bb",
+};
+
+var result = list.Distinct().ToArray();
+
+Console.WriteLine("Hello, World!");
