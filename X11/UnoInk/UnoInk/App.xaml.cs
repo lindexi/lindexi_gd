c5bc26ab589e88a29229e12ea09d9276aeafb83c
lindexi <lindexi_gd@163.com>
2024-06-11 17:01:36 +0800

    减少输出日志

--- X11/UnoInk/UnoInk/App.xaml.cs
+++ X11/UnoInk/UnoInk/App.xaml.cs
@@ -34,8 +34,7 @@ public partial class App : Application
 
         // 设置图标的时间太长，设置为空即可跳过这部分的耗时，解决启动窗口闪烁
         // 设置图标只影响任务栏图标
-        Console.WriteLine(global::Windows.ApplicationModel.Package.Current.Logo);
-        //global::Windows.ApplicationModel.Package.Current.Logo = null;
+        global::Windows.ApplicationModel.Package.Current.Logo = null;
 #endif
     }
 
@@ -157,7 +156,7 @@ public partial class App : Application
             builder.SetMinimumLevel(LogLevel.Information);
 
             // Default filters for Uno Platform namespaces
-            builder.AddFilter("Uno", LogLevel.Information);
+            builder.AddFilter("Uno", LogLevel.Warning);
             builder.AddFilter("Windows", LogLevel.Warning);
             builder.AddFilter("Microsoft", LogLevel.Warning);
             
