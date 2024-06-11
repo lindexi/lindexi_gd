5fbcf17f966e9ddbe3f8f4837738733d70a1ce48
lindexi <lindexi_gd@163.com>
2024-06-11 17:36:09 +0800

    NullReferenceException when drawing in InkCanvas after editing mode change
    https://github.com/dotnet/wpf/issues/3840

---
+++ Ink/FujalaifalgilereKonerkeene/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FujalaifalgilereKonerkeene;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnStrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+    {
+        _canvas.EditingMode = InkCanvasEditingMode.Select;
+    }
+
+    private void OnInkClick(object sender, RoutedEventArgs e)
+    {
+        _canvas.EditingMode = InkCanvasEditingMode.Ink;
+    }
+}
