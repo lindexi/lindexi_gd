e395b3f986dc2e8c267755e7791a2de17da76ef9
lindexi <lindexi_gd@163.com>
2024-06-11 19:57:02 +0800

    批量转换图片为视频

---
+++ Workbench/KabijajaiqurwawFawbeaferhall/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+var folder = @"C:\lindexi\Gif\";
+
+foreach (var file in Directory.EnumerateFiles(folder,"*.gif"))
+{
+    Process.Start(new ProcessStartInfo(@"C:\lindexi\Gif\ffmpeg\ffmpeg.exe",
+        $"-i {file} -movflags faststart -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" {Path.GetFileNameWithoutExtension(file)}.mp4")
+    {
+        WorkingDirectory = folder
+    });
+}
+
+Console.WriteLine("Hello, World!");
