9f6331d02ec8a6d66e8cd6968e3a5da1feaadaab
lindexi <lindexi_gd@163.com>
2024-06-11 20:02:28 +0800

    凭借合并的视频的代码

--- Workbench/KabijajaiqurwawFawbeaferhall/Program.cs
+++ Workbench/KabijajaiqurwawFawbeaferhall/Program.cs
@@ -4,13 +4,17 @@ using System.Diagnostics;
 
 var folder = @"C:\lindexi\Gif\";
 
-foreach (var file in Directory.EnumerateFiles(folder,"*.gif"))
-{
-    Process.Start(new ProcessStartInfo(@"C:\lindexi\Gif\ffmpeg\ffmpeg.exe",
-        $"-i {file} -movflags faststart -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" {Path.GetFileNameWithoutExtension(file)}.mp4")
-    {
-        WorkingDirectory = folder
-    });
-}
+//foreach (var file in Directory.EnumerateFiles(folder,"*.gif"))
+//{
+//    Process.Start(new ProcessStartInfo(@"C:\lindexi\Gif\ffmpeg\ffmpeg.exe",
+//        $"-i {file} -movflags faststart -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" {Path.GetFileNameWithoutExtension(file)}.mp4")
+//    {
+//        WorkingDirectory = folder
+//    });
+//}
+
+var videoTextFile = Path.Join(folder, "video.txt");
+
+File.AppendAllLines(videoTextFile, Directory.EnumerateFiles(folder, "*.mp4").Select(t => $"file '{Path.GetFileName(t)}'"));
 
 Console.WriteLine("Hello, World!");
