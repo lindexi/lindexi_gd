7fb1a1db6e329d55ff803944c7a606c870f2dd34
lindexi <lindexi_gd@163.com>
2024-06-12 09:44:09 +0800

    dotnet build fails when WindowsAppSDK is referenced in console application

---
+++ BeregemnallchunufeRojergalbu/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace BeregemnallchunufeRojergalbu;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        global::WinRT.ComWrappersSupport.InitializeComWrappers();
+        global::Microsoft.UI.Xaml.Application.Start((p) =>
+        {
+            var app = new App();
+            app.Launched += (sender, e) =>
+            {
+                var window = new Window()
+                {
+                    Title = "Create Window from console"
+                };
+                window.Content = new Grid()
+                {
+                    Children =
+                    {
+                        new TextBlock()
+                        {
+                            Text = "ConsoleApplication",
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    }
+                };
+                window.Activate();
+            };
+        });
+    }
+}
+
+public class App : Application
+{
+    public event EventHandler<LaunchActivatedEventArgs>? Launched;
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        Launched?.Invoke(this, args);
+    }
+}
