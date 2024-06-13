e7234f8b8ef84335d3a70ecfbb96099d4150ad31
lindexi <lindexi_gd@163.com>
2024-06-13 19:35:40 +0800

    测试采用更新版本的机器，速度更快

---
+++ SemanticKernelSamples/LeakeryeeliBellakeda/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.SemanticKernel;
+
+var keyFile = @"C:\lindexi\CA\Key";
+var key = File.ReadAllText(keyFile);
+
+var kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion("gpt-4o", endpoint: "https://lindexi.openai.azure.com/", apiKey: key)
+    .Build();
+
+await foreach (var streamingKernelContent in kernel.InvokePromptStreamingAsync("Hello"))
+{
+    Console.Write(streamingKernelContent.ToString());
+}
+
+Console.Read();
+
+Console.WriteLine("Hello, World!");
