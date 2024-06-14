dec8a33e518c066daa7103216e623e62f7e5a6e2
lindexi <lindexi_gd@163.com>
2024-06-14 17:29:03 +0800

    加上更多日志

--- X11/UnoInk/UnoInk/Inking/UnoInkCore/PointerToModeInputDispatchers/PointerToModeInputDispatcher.cs
+++ X11/UnoInk/UnoInk/Inking/UnoInkCore/PointerToModeInputDispatchers/PointerToModeInputDispatcher.cs
@@ -1,8 +1,10 @@
 using System.Diagnostics;
 using System.Runtime.Versioning;
 using System.Threading.Channels;
+
 using UnoInk.Inking.InkCore.Interactives;
 using UnoInk.Inking.X11Ink;
+using UnoInk.Inking.X11Platforms;
 
 namespace UnoInk.UnoInkCore;
 
@@ -17,13 +19,17 @@ class PointerToModeInputDispatcher
         Reader = reader;
         X11InkWindow = x11InkWindow;
     }
-    
+
     private ChannelReader<PointerInputInfo> Reader { get; }
     public X11InkWindow X11InkWindow { get; }
     private ModeInputDispatcher ModeInputDispatcher => X11InkWindow.ModeInputDispatcher;
 
     public async Task RunAsync()
     {
+        int count = 0;
+        var stopwatch = new Stopwatch();
+        long time = 0;
+
         while (true)
         {
             var waitToRead = await Reader.WaitToReadAsync()
@@ -52,7 +58,17 @@ class PointerToModeInputDispatcher
                     }
                     else if (info.Type == PointerInputType.Move)
                     {
+                        stopwatch.Restart();
                         ModeInputDispatcher.Move(info.InputArgs);
+                        stopwatch.Stop();
+                        time += stopwatch.ElapsedTicks;
+                        count++;
+                        if (count >= 100)
+                        {
+                            StaticDebugLogger.WriteLine($"平均移动耗时 {time * 1.0 / count / Stopwatch.Frequency * 1000}");
+
+                            count = 0;
+                        }
                     }
                     else if (info.Type == PointerInputType.Up)
                     {
@@ -62,12 +78,12 @@ class PointerToModeInputDispatcher
                     {
                         Debug.Fail("没有考虑过的逻辑");
                     }
-                    
+
                     if (!isFirst)
                     {
-                        
+
                     }
-                    
+
                     isFirst = false;
                     lastInputInfo = info;
                 }
