f98282629e2547facbd46812431d21f6e0b44ce6
lindexi <lindexi_gd@163.com>
2024-06-14 10:12:13 +0800

    The PreviewKeyDown can raise in code

---
+++ UnoDemo/PreviewKeyDownEventIssues/PreviewKeyDownEventIssues/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+namespace PreviewKeyDownEventIssues;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+        PreviewKeyDown += MainPage_PreviewKeyDown;
+        KeyDown += MainPage_KeyDown;
+    }
+    
+    private void MainPage_PreviewKeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+        LogTextBlock.Text += "\r\n PreviewKeyDown";
+    }
+    
+    private void MainPage_KeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+        LogTextBlock.Text += "\r\n KeyDown";
+    }
+}
