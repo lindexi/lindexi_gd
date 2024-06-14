4fb434b809604b7e812f2b08f8c46db63e8bfdde
lindexi <lindexi_gd@163.com>
2024-06-14 20:01:42 +0800

    定义基础的点

---
+++ BearhihihaikeRejonodemwhe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearhihihaikeRejonodemwhe;
+
+internal class Program
+{
+}
+
+
+class Point
+{
+    public virtual void SetInput(InputInfo inputInfo)
+    {
+
+    }
+
+    public virtual async Task<OutputInfo> RunAsync()
+    {
+        return default;
+    }
+}
+
+record InputInfo();
+record OutputInfo();
