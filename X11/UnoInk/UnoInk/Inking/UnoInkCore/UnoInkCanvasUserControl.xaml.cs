4bc62a042c36adbe4c6c1371c95da2ef650c7f16
lindexi <lindexi_gd@163.com>
2024-06-14 16:42:40 +0800

    设计多线程调度

--- X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkCanvasUserControl.xaml.cs
+++ X11/UnoInk/UnoInk/Inking/UnoInkCore/UnoInkCanvasUserControl.xaml.cs
@@ -92,6 +92,7 @@ public sealed partial class UnoInkCanvasUserControl : UserControl
                 // 进入调度器
                 while (true)
                 {
+                    ChannelReader<PointerInputInfo> reader = _channel.Reader;
                     var waitToRead = await _channel.Reader.WaitToReadAsync().ConfigureAwait(false);
                     if (!waitToRead)
                     {
@@ -567,6 +568,20 @@ public sealed partial class UnoInkCanvasUserControl : UserControl
     }
 }
 
+/// <summary>
+/// 从 <see cref="PointerInputInfo"/> 调度到模式输入层
+/// </summary>
+class PointerToModeInputDispatcher
+{
+    public PointerToModeInputDispatcher(ChannelReader<PointerInputInfo> reader, ModeInputDispatcher modeInputDispatcher)
+    {
+        Reader = reader;
+        ModeInputDispatcher = modeInputDispatcher;
+    }
+    
+    private ChannelReader<PointerInputInfo> Reader { get; }
+    private ModeInputDispatcher ModeInputDispatcher { get; }
+}
 
 readonly record struct PointerInputInfo(PointerInputType Type, ModeInputArgs InputArgs)
 {
