313cb1613a1e069a472ec3f53e7406b8f1fa02aa
lindexi <lindexi_gd@163.com>
2024-06-17 14:16:11 +0800

    测试结构体是否装箱

---
+++ Workbench/KiheekawyalawGechurwagocal/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using KiheekawyalawGechurwagocal;
+
+using var disposableStruct = new DisposableStruct();
+
+IDisposable disposable = disposableStruct;
+
+Console.WriteLine("Hello, World!");
