10fa20b8bc2c1e35e4776bf7d88fa99701ac66e0
lindexi <lindexi_gd@163.com>
2024-06-18 20:48:04 +0800

    尝试对接逻辑

--- SemanticKernelSamples/BemjawhufawJairkihawyawkerene/Program.cs
+++ SemanticKernelSamples/BemjawhufawJairkihawyawkerene/Program.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.SemanticKernel;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ML.OnnxRuntimeGenAI;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 
 var modelPath = @"C:\lindexi\Phi3\directml-int4-awq-block-128";
 
 // create kernel
 var builder = Kernel.CreateBuilder();
-builder.AddOnnxRuntimeGenAIChatCompletion(modelPath: modelPath);
+//builder.Services.AddSingleton<IChatCompletionService>(new Phi3ChatCompletionService(modelPath));
+builder.AddOnnxRuntimeGenAIChatCompletion(modelPath);
 var kernel = builder.Build();
 
 // create chat
@@ -33,4 +36,34 @@ while (true)
     }
     history.AddAssistantMessage(response);
     Console.WriteLine("");
+}
+
+
+class Phi3ChatCompletionService : IChatCompletionService
+{
+    public Phi3ChatCompletionService(string modelPath)
+    {
+        var model = new Model(modelPath);
+        var tokenizer = new Tokenizer(model);
+
+        Model = model;
+        Tokenizer = tokenizer;
+    }
+
+    public IReadOnlyDictionary<string, object?> Attributes { get; set; } = new Dictionary<string, object?>();
+    public Model Model { get; }
+    public Tokenizer Tokenizer { get; }
+
+    public Task<IReadOnlyList<ChatMessageContent>> GetChatMessageContentsAsync(ChatHistory chatHistory, PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+
+    public IAsyncEnumerable<StreamingChatMessageContent> GetStreamingChatMessageContentsAsync(ChatHistory chatHistory,
+        PromptExecutionSettings? executionSettings = null, Kernel? kernel = null,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
 }
