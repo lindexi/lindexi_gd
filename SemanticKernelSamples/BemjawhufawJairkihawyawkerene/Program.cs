24b566df89a984c088a0ccd9b942b5bbf9403ed9
lindexi <lindexi_gd@163.com>
2024-06-18 20:34:11 +0800

    测试实现逻辑但是失败

---
+++ SemanticKernelSamples/BemjawhufawJairkihawyawkerene/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var modelPath = @"C:\lindexi\Phi3\directml-int4-awq-block-128";
+
+// create kernel
+var builder = Kernel.CreateBuilder();
+builder.AddOnnxRuntimeGenAIChatCompletion(modelPath: modelPath);
+var kernel = builder.Build();
+
+// create chat
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+var history = new ChatHistory();
+
+// run chat
+while (true)
+{
+    Console.Write("Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+    history.AddUserMessage(userQ);
+
+    Console.Write($"Phi3: ");
+    var response = "";
+    var result = chat.GetStreamingChatMessageContentsAsync(history);
+    await foreach (var message in result)
+    {
+        Console.Write(message.Content);
+        response += message.Content;
+    }
+    history.AddAssistantMessage(response);
+    Console.WriteLine("");
+}
