eb89e15d2f28a54df3a29101fd5fc8b3e172d174
lindexi <lindexi_gd@163.com>
2024-06-19 11:58:39 +0800

    创建控制台项目

---
+++ WPFDemo/NocejefiWeyufilareewe/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+
+namespace NocejefiWeyufilareewe;
+
+public class App : Application
+{
+    public event EventHandler<LaunchActivatedEventArgs>? Launched;
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        Launched?.Invoke(this, args);
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        global::WinRT.ComWrappersSupport.InitializeComWrappers();
+        global::Microsoft.UI.Xaml.Application.Start((p) =>
+        {
+            var app = new App();
+            app.Launched += (sender, e) =>
+            {
+                var window = new Window()
+                {
+                    Title = "控制台创建应用"
+                };
+                window.Content = new Grid()
+                {
+                    Children =
+                    {
+                        new TextBlock()
+                        {
+                            Text = "控制台应用",
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    }
+                };
+                window.Activate();
+            };
+        });
+    }
+}
