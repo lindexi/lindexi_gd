b1e7e7e0f6c5373bf50ec9eb276183da7ee1c0f3
lindexi <lindexi_gd@163.com>
2024-06-20 19:52:48 +0800

    设置窗口关系
    0331c5dd6057106df5cb179e45d34966a3eafd1b

--- X11/JawalwhofuYageakaje/Program.cs
+++ X11/JawalwhofuYageakaje/Program.cs
@@ -10,58 +10,85 @@ XInitThreads();
 var display = XOpenDisplay(IntPtr.Zero);
 var screen = XDefaultScreen(display);
 
-var rootWindow = XDefaultRootWindow(display);
+var win1 = new X11Window(display);
+var win2 = new X11Window(display);
 
-XMatchVisualInfo(display, screen, 32, 4, out var info);
-var visual = info.visual;
+XMapWindow(display, win1.Window);
+XMapWindow(display, win2.Window);
 
-var valueMask =
-        //SetWindowValuemask.BackPixmap
-        0
-        | SetWindowValuemask.BackPixel
-        | SetWindowValuemask.BorderPixel
-        | SetWindowValuemask.BitGravity
-        | SetWindowValuemask.WinGravity
-        | SetWindowValuemask.BackingStore
-        | SetWindowValuemask.ColorMap
-    //| SetWindowValuemask.OverrideRedirect
-    ;
-var xSetWindowAttributes = new XSetWindowAttributes
-{
-    backing_store = 1,
-    bit_gravity = Gravity.NorthWestGravity,
-    win_gravity = Gravity.NorthWestGravity,
-    //override_redirect = true, // 设置窗口的override_redirect属性为True，以避免窗口管理器的干预
-    colormap = XCreateColormap(display, rootWindow, visual, 0),
-    border_pixel = 0,
-    background_pixel = 0,
-};
+XSetTransientForHint(display, win1.Window, win2.Window);
 
-var xDisplayWidth = XDisplayWidth(display, screen) / 2;
-var xDisplayHeight = XDisplayHeight(display, screen) / 2;
-var handle = XCreateWindow(display, rootWindow, 0, 0, xDisplayWidth, xDisplayHeight, 5,
-    32,
-    (int) CreateWindowArgs.InputOutput,
-    visual,
-    (nuint) valueMask, ref xSetWindowAttributes);
-
-
-XEventMask ignoredMask = XEventMask.SubstructureRedirectMask | XEventMask.ResizeRedirectMask |
-                         XEventMask.PointerMotionHintMask;
-var mask = new IntPtr(0xffffff ^ (int) ignoredMask);
-XSelectInput(display, handle, mask);
-
-XMapWindow(display, handle);
 XFlush(display);
 
 var white = XWhitePixel(display, screen);
 var black = XBlackPixel(display, screen);
 
-var gc = XCreateGC(display, handle, 0, 0);
-XSetForeground(display, gc, white);
 XSync(display, false);
 
 while (true)
 {
     var xNextEvent = XNextEvent(display, out var @event);
+}
+
+class X11Window
+{
+    public X11Window(IntPtr display)
+    {
+        var screen = XDefaultScreen(display);
+
+        var rootWindow = XDefaultRootWindow(display);
+
+        XMatchVisualInfo(display, screen, 32, 4, out var info);
+        var visual = info.visual;
+
+        var valueMask =
+                //SetWindowValuemask.BackPixmap
+                0
+                | SetWindowValuemask.BackPixel
+                | SetWindowValuemask.BorderPixel
+                | SetWindowValuemask.BitGravity
+                | SetWindowValuemask.WinGravity
+                | SetWindowValuemask.BackingStore
+                | SetWindowValuemask.ColorMap
+            //| SetWindowValuemask.OverrideRedirect
+            ;
+        var xSetWindowAttributes = new XSetWindowAttributes
+        {
+            backing_store = 1,
+            bit_gravity = Gravity.NorthWestGravity,
+            win_gravity = Gravity.NorthWestGravity,
+            //override_redirect = true, // 设置窗口的override_redirect属性为True，以避免窗口管理器的干预
+            colormap = XCreateColormap(display, rootWindow, visual, 0),
+            border_pixel = 0,
+            background_pixel = 0,
+        };
+
+        var xDisplayWidth = XDisplayWidth(display, screen) / 2;
+        var xDisplayHeight = XDisplayHeight(display, screen) / 2;
+        var window = XCreateWindow(display, rootWindow, 0, 0, xDisplayWidth, xDisplayHeight, 5,
+            32,
+            (int) CreateWindowArgs.InputOutput,
+            visual,
+            (nuint) valueMask, ref xSetWindowAttributes);
+
+        XEventMask ignoredMask = XEventMask.SubstructureRedirectMask | XEventMask.ResizeRedirectMask |
+                                 XEventMask.PointerMotionHintMask;
+        var mask = new IntPtr(0xffffff ^ (int) ignoredMask);
+        XSelectInput(display, window, mask);
+
+        var white = XWhitePixel(display, screen);
+        var black = XBlackPixel(display, screen);
+
+        var gc = XCreateGC(display, window, 0, 0);
+        XSetForeground(display, gc, white);
+        XSync(display, false);
+
+        Window = window;
+        GC = gc;
+    }
+
+    public IntPtr Window { get; set; }
+    public IntPtr GC { get; set; }
+
+
 }
