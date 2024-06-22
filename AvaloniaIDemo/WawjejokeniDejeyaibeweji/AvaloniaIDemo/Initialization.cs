6cc41fa8c98001d034a59d20cad83386ae16b5aa
lindexi <lindexi_gd@163.com>
2024-06-22 15:14:26 +0800

    搭建入口项目让两个框架更具独立可以更加方便发布

---
+++ AvaloniaIDemo/WawjejokeniDejeyaibeweji/AvaloniaIDemo/Initialization.cs
@@ -0,0 +1,19 @@
+﻿using LibDemo;
+
+namespace AvaloniaIDemo;
+
+public static class Initializer
+{
+    public static void InitAssembly()
+    {
+        Runner.SetAppRunner(new AppRunner());
+    }
+}
+
+file class AppRunner : IAppRunner
+{
+    public void Run()
+    {
+        Program.Main([]);
+    }
+}
