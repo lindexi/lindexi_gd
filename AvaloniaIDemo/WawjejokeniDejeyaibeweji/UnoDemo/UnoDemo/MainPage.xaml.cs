f7032fc9a952a073d1e4cdb5d81955e38019ac3c
lindexi <lindexi_gd@163.com>
2024-06-22 14:51:04 +0800

    反向依赖将会失败

--- AvaloniaIDemo/WawjejokeniDejeyaibeweji/UnoDemo/UnoDemo/MainPage.xaml.cs
+++ AvaloniaIDemo/WawjejokeniDejeyaibeweji/UnoDemo/UnoDemo/MainPage.xaml.cs
@@ -9,20 +9,20 @@ public sealed partial class MainPage : Page
     
     private void RunAvaloniaButton_OnClick(object sender, RoutedEventArgs e)
     {
-        // Create the new Thread to run the Avalonia 
+        //// Create the new Thread to run the Avalonia 
         
-        var thread = new Thread(() =>
-        {
-            AvaloniaIDemo.Program.Main([]);
-        })
-        {
-            IsBackground = true,
-            Name = "Avalonia main thread"
-        };
-        if (OperatingSystem.IsWindows())
-        {
-            thread.SetApartmentState(ApartmentState.STA);
-        }
-        thread.Start();
+        //var thread = new Thread(() =>
+        //{
+        //    AvaloniaIDemo.Program.Main([]);
+        //})
+        //{
+        //    IsBackground = true,
+        //    Name = "Avalonia main thread"
+        //};
+        //if (OperatingSystem.IsWindows())
+        //{
+        //    thread.SetApartmentState(ApartmentState.STA);
+        //}
+        //thread.Start();
     }
 }
