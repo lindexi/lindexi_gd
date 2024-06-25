a34766170fedf15d53fbbb99091c9cdd70eed849
lindexi <lindexi_gd@163.com>
2024-06-25 17:10:00 +0800

    输出更多信息

--- SkiaSharp/HikucarwalllereWeajuyanowhea/Program.cs
+++ SkiaSharp/HikucarwalllereWeajuyanowhea/Program.cs
@@ -2,5 +2,14 @@
 
 using SkiaSharp;
 
-var fromFamilyName = SKTypeface.FromFamilyName(null, SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright);
-Console.WriteLine($"fromFamilyName={fromFamilyName}");
+var skTypeFace = SKTypeface.FromFamilyName(null, SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Upright);
+Console.WriteLine($"fromFamilyName={skTypeFace} FamilyName={skTypeFace?.FamilyName} GlyphCount={skTypeFace?.GlyphCount}");
+
+var fontSize = 20;
+var skFont = new SKFont(skTypeFace, fontSize);
+skFont.Edging = SKFontEdging.SubpixelAntialias;
+skFont.Subpixel = true;
+
+
+
+Console.WriteLine($"SKFont={skFont.Size}, {skFont.ScaleX}, {skFont.Metrics}");
