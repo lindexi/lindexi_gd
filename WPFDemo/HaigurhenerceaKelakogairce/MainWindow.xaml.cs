cef1f009b24d81c3def88933a83abce3c8c6bfca
lindexi <lindexi_gd@163.com>
2024-06-27 14:52:03 +0800

    修改路径坐标

--- WPFDemo/HaigurhenerceaKelakogairce/MainWindow.xaml.cs
+++ WPFDemo/HaigurhenerceaKelakogairce/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ public partial class MainWindow : Window
 
         var geometry = InkPath.Data;
         geometry = Geometry.Combine(geometry, Geometry.Empty, GeometryCombineMode.Union, Transform.Identity);
-        geometry.Transform = new TranslateTransform(0, -2);
+        geometry.Transform = new TranslateTransform(0, 1);
         geometry = Geometry.Combine(geometry, Geometry.Empty, GeometryCombineMode.Union, Transform.Identity);
 
         //var geometry = Geometry.Parse("M8,15C7.85289858,15.5677816,7.85289858,16.4322348,8,17L23,29C23.7348015,29.3762198,25,28.8227297,25,28L25,4C25,3.1772867,23.7348015,2.62379657,23,3L8,15z");
