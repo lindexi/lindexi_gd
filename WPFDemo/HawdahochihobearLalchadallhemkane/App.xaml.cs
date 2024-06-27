37142b9a2e13dc87425b1d2f8d7fdff3e2a5c568
lindexi <lindexi_gd@163.com>
2024-06-27 19:57:31 +0800

    测试窗口焦点抢占

---
+++ WPFDemo/HawdahochihobearLalchadallhemkane/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HawdahochihobearLalchadallhemkane;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
