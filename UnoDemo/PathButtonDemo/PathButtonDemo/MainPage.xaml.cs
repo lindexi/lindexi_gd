71141228f5691c92709d30db18e12db4a2a42430
lindexi <lindexi_gd@163.com>
2024-07-02 14:19:39 +0800

    尝试绑定附加属性但是失败

--- UnoDemo/PathButtonDemo/PathButtonDemo/MainPage.xaml.cs
+++ UnoDemo/PathButtonDemo/PathButtonDemo/MainPage.xaml.cs
@@ -7,3 +7,19 @@ public sealed partial class MainPage : Page
         this.InitializeComponent();
     }
 }
+
+public class ButtonHelper
+{
+    public static readonly DependencyProperty ButtonPathProperty = DependencyProperty.RegisterAttached(
+        "ButtonPath", typeof(string), typeof(ButtonHelper), new PropertyMetadata(default(string)));
+    
+    public static void SetButtonPath(DependencyObject element, string value)
+    {
+        element.SetValue(ButtonPathProperty, value);
+    }
+    
+    public static string GetButtonPath(DependencyObject element)
+    {
+        return (string)element.GetValue(ButtonPathProperty);
+    }
+}
