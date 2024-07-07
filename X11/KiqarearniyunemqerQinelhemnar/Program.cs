853afe21e012d4bee978ac78449b05629739cb27
lindexi <lindexi_gd@163.com>
2024-07-07 15:53:47 +0800

    简单绘制内容

--- X11/KiqarearniyunemqerQinelhemnar/Program.cs
+++ X11/KiqarearniyunemqerQinelhemnar/Program.cs
@@ -8,15 +8,26 @@ using System.Runtime;
 using static CPF.Linux.XLib;
 
 var display = XOpenDisplay(IntPtr.Zero);
+var screen = XDefaultScreen(display);
 var rootWindow = XDefaultRootWindow(display);
 
 var x11Window =
 XCreateSimpleWindow(display, rootWindow, 100, 100, 500, 500, 1, IntPtr.Zero, IntPtr.Zero);
 
+XSelectInput(display, x11Window, new IntPtr((int) XEventMask.ExposureMask));
+
 XMapWindow(display, x11Window);
 XFlush(display);
+
+var gc = XCreateGC(display, x11Window, 0, 0);
+var whitePixel = XWhitePixel(display, screen);
+XSetForeground(display, gc, whitePixel);
 
 while (true)
 {
     var xNextEvent = XNextEvent(display, out var @event);
+    if (@event.type == XEventName.Expose)
+    {
+        XDrawLine(display, x11Window, gc, 10, 100, 100, 100);
+    }
 }
