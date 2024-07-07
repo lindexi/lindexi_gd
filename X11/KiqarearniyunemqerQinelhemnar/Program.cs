4de433fe42e3981c71cab52bd8b8a60588c7049b
lindexi <lindexi_gd@163.com>
2024-07-07 15:25:23 +0800

    尝试创建最简单的 X11 应用

---
+++ X11/KiqarearniyunemqerQinelhemnar/Program.cs
@@ -0,0 +1,22 @@
+﻿using CPF.Linux;
+
+using System;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Runtime;
+
+using static CPF.Linux.XLib;
+
+var display = XOpenDisplay(IntPtr.Zero);
+var rootWindow = XDefaultRootWindow(display);
+
+var x11Window =
+XCreateSimpleWindow(display, rootWindow, 100, 100, 500, 500, 1, IntPtr.Zero, IntPtr.Zero);
+
+XMapWindow(display, x11Window);
+XFlush(display);
+
+while (true)
+{
+    var xNextEvent = XNextEvent(display, out var @event);
+}
