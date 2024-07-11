69b940808ab63188fab866fead429309b42d3aab
lindexi <lindexi_gd@163.com>
2024-07-11 14:14:24 +0800

    重新实现逻辑

---
+++ X11/KifawiqeKagemwanea/Program.cs
@@ -0,0 +1,63 @@
+﻿using CPF.Linux;
+
+using static CPF.Linux.XLib;
+
+XInitThreads();
+var display = XOpenDisplay(IntPtr.Zero);
+var screen = XDefaultScreen(display);
+var rootWindow = XDefaultRootWindow(display);
+
+XMatchVisualInfo(display, screen, 32, 4, out var info);
+var visual = info.visual;
+
+var valueMask =
+        //SetWindowValuemask.BackPixmap
+        0
+        | SetWindowValuemask.BackPixel
+        | SetWindowValuemask.BorderPixel
+        | SetWindowValuemask.BitGravity
+        | SetWindowValuemask.WinGravity
+        | SetWindowValuemask.BackingStore
+        | SetWindowValuemask.ColorMap
+    //| SetWindowValuemask.OverrideRedirect
+    ;
+var xSetWindowAttributes = new XSetWindowAttributes
+{
+    backing_store = 1,
+    bit_gravity = Gravity.NorthWestGravity,
+    win_gravity = Gravity.NorthWestGravity,
+    //override_redirect = true, // 设置窗口的override_redirect属性为True，以避免窗口管理器的干预
+    colormap = XCreateColormap(display, rootWindow, visual, 0),
+    border_pixel = 0,
+    background_pixel = 0,
+};
+
+var xDisplayWidth = XDisplayWidth(display, screen);
+var xDisplayHeight = XDisplayHeight(display, screen);
+
+var width = xDisplayWidth / 2;
+var height = xDisplayHeight / 2;
+
+var handle = XCreateWindow(display, rootWindow, 0, 0, width, height, 5,
+    32,
+    (int) CreateWindowArgs.InputOutput,
+    visual,
+    (nuint) valueMask, ref xSetWindowAttributes);
+
+XEventMask ignoredMask = XEventMask.SubstructureRedirectMask | XEventMask.ResizeRedirectMask |
+                         XEventMask.PointerMotionHintMask;
+var mask = new IntPtr(0xffffff ^ (int) ignoredMask);
+XSelectInput(display, handle, mask);
+
+XMapWindow(display, handle);
+XFlush(display);
+
+while (true)
+{
+    var xNextEvent = XNextEvent(display, out var @event);
+
+    if (xNextEvent != 0)
+    {
+        break;
+    }
+}
