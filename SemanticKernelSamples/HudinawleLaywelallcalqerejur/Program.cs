299ec87957d1e23c15d46618ea417495b13e30b5
lindexi <lindexi_gd@163.com>
2024-07-13 11:28:49 +0800

    判断传入权限

--- SemanticKernelSamples/HudinawleLaywelallcalqerejur/Program.cs
+++ SemanticKernelSamples/HudinawleLaywelallcalqerejur/Program.cs
@@ -1,11 +1,12 @@
-﻿using Azure.AI.OpenAI;
+﻿using System.Net;
+using Azure.AI.OpenAI;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 // 这里的演示代码需要用到 AzureAI 的支持，需要提前申请好，申请地址：https://aka.ms/oai/access
 var endpoint = "https://lindexi.openai.azure.com/"; // 请换成你的地址
-var apiKey = File.ReadAllText(@"C:\lindexi\CA\Key"); // 请换成你的密钥
+var apiKey = "TheErrorToken"; // 请换成你的密钥
 
 var kernelBuilder = Kernel.CreateBuilder();
 kernelBuilder.AddAzureOpenAIChatCompletion("GPT4o", endpoint, apiKey);
@@ -18,17 +19,30 @@ var chatHistory = new ChatHistory();
 chatHistory.AddUserMessage("Hello, what is your code?");
 
 var completion = chatCompletionService.GetStreamingChatMessageContentsAsync(chatHistory);
-await foreach (var streamingChatMessageContent in completion)
+try
 {
-    Console.Write(streamingChatMessageContent.Content);
-    if(streamingChatMessageContent is OpenAIStreamingChatMessageContent content)
+    await foreach (var streamingChatMessageContent in completion)
     {
-        if (content.FinishReason is not null)
+        Console.Write(streamingChatMessageContent.Content);
+        if (streamingChatMessageContent is OpenAIStreamingChatMessageContent content)
         {
-            if (content.FinishReason == CompletionsFinishReason.Stopped)
+            if (content.FinishReason is not null)
             {
-                
+                if (content.FinishReason == CompletionsFinishReason.Stopped)
+                {
+
+                }
             }
+        }
+    }
+}
+catch (Exception e)
+{
+    if (e is HttpOperationException httpOperationException)
+    {
+        if (httpOperationException.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            
         }
     }
 }
