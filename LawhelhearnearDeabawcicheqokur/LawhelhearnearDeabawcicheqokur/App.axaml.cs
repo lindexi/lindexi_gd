2b562b421b1ed0b8fb639740af336e6ce69496ed
lindexi <lindexi_gd@163.com>
2024-07-15 09:32:35 +0800

    新建空项目

---
+++ LawhelhearnearDeabawcicheqokur/LawhelhearnearDeabawcicheqokur/App.axaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+
+using LawhelhearnearDeabawcicheqokur.ViewModels;
+using LawhelhearnearDeabawcicheqokur.Views;
+
+using Meadow;
+using Meadow.Pinouts;
+
+namespace LawhelhearnearDeabawcicheqokur;
+
+public partial class App : AvaloniaMeadowApplication<Linux<RaspberryPi>>
+{
+
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+        LoadMeadowOS();
+    }
+
+    public override Task InitializeMeadow()
+    {
+        var r = Resolver.Services.Get<IMeadowDevice>();
+
+        if (r == null)
+        {
+            Resolver.Log.Info("IMeadowDevice is null");
+        }
+        else
+        {
+            Resolver.Log.Info($"IMeadowDevice is {r.GetType().Name}");
+        }
+
+        return Task.CompletedTask;
+
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        // Line below is needed to remove Avalonia data validation.
+        // Without this line you will get duplicate validations from both Avalonia and CT
+        BindingPlugins.DataValidators.RemoveAt(0);
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ LawhelhearnearDeabawcicheqokur/LawhelhearnearDeabawcicheqokur/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace LawhelhearnearDeabawcicheqokur.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
