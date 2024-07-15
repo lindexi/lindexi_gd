1111c23e4c76a3aa7e3418db8ff7b6661cd0d1d2
lindexi <lindexi_gd@163.com>
2024-07-15 17:14:35 +0800

    Incorrect Implementation of GetIntermediatePoints Method in UNO Framework

--- UnoDemo/NeejabaykugoWerjekeeja/NeejabaykugoWerjekeeja/MainPage.xaml.cs
+++ UnoDemo/NeejabaykugoWerjekeeja/NeejabaykugoWerjekeeja/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Xaml.Input;
+
 namespace NeejabaykugoWerjekeeja;
 
 public sealed partial class MainPage : Page
@@ -5,5 +7,14 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+    }
+
+    private void UIElement_OnPointerMoved(object sender, PointerRoutedEventArgs e)
+    {
+        var intermediatePoints = e.GetIntermediatePoints(null);
+
+        LogMessageTextBlock.Text = $"[{Environment.TickCount64}] Count of IntermediatePoints={intermediatePoints.Count}";
+
+        Thread.Sleep(100);
     }
 }
