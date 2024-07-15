80034c47067cfa09d3c79e676b455c21b0ff0d07
lindexi <lindexi_gd@163.com>
2024-07-15 20:19:46 +0800

    获取最大的文件

---
+++ Workbench/NemjibawawJerekalldeha/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+var folder = @"F:\vs2022离线下载2024年7月15日\";
+
+var fileList = new DirectoryInfo(folder).GetFiles("*", SearchOption.AllDirectories).ToList();
+var file = fileList.MaxBy(t => t.Length);
+
+Console.WriteLine("Hello, World!");
