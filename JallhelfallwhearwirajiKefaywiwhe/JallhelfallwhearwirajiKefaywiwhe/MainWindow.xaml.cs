65a6ec8bad9d03c140a432890211e6bf6cb00087
lindexi <lindexi_gd@163.com>
2024-07-15 16:21:42 +0800

    测试报点数量

--- JallhelfallwhearwirajiKefaywiwhe/JallhelfallwhearwirajiKefaywiwhe/MainWindow.xaml.cs
+++ JallhelfallwhearwirajiKefaywiwhe/JallhelfallwhearwirajiKefaywiwhe/MainWindow.xaml.cs
@@ -26,11 +26,82 @@ public sealed partial class MainWindow : Window
 {
     public MainWindow()
     {
+        _mainPage = this;
         this.InitializeComponent();
     }
 
-    private void myButton_Click(object sender, RoutedEventArgs e)
+    private static MainWindow _mainPage = null!;
+
+    private void UIElement_OnPointerPressed(object sender, PointerRoutedEventArgs e)
     {
-        myButton.Content = "Clicked";
+        TouchInfoDictionary[e.Pointer.PointerId] = new TouchInfo(e);
+        Output();
+    }
+
+    private void UIElement_OnPointerMoved(object sender, PointerRoutedEventArgs e)
+    {
+        if (TouchInfoDictionary.TryGetValue(e.Pointer.PointerId, out var touchInfo))
+        {
+            touchInfo.Update(e);
+            Output();
+            LogTextBlock.Text += $"报点 : {e.GetIntermediatePoints(null).Count}";
+        }
+    }
+
+    private void UIElement_OnPointerReleased(object sender, PointerRoutedEventArgs e)
+    {
+        if (TouchInfoDictionary.TryGetValue(e.Pointer.PointerId, out var touchInfo))
+        {
+            touchInfo.Update(e);
+            touchInfo.IsUp = true;
+            Output();
+        }
+    }
+
+    private void Output()
+    {
+        LogTextBlock.Text = $"当前落下点数： {TouchInfoDictionary.Count(t => t.Value.IsUp is false)}\r\n";
+
+        foreach (var (_, value) in TouchInfoDictionary)
+        {
+            if (value.IsUp)
+            {
+                continue;
+            }
+
+            LogTextBlock.Text += $"[{value.PointerId}] XY={value.X:0.00},{value.Y:0.00} WH={value.Width:0.00},{value.Height:0.00} \r\n";
+        }
+    }
+
+    private Dictionary<uint, TouchInfo> TouchInfoDictionary { get; } = new Dictionary<uint, TouchInfo>();
+
+    class TouchInfo
+    {
+        public TouchInfo(PointerRoutedEventArgs e)
+        {
+            Update(e);
+        }
+
+        public uint PointerId { set; get; }
+
+        public double X { set; get; }
+        public double Y { set; get; }
+
+
+        public double Width { set; get; }
+        public double Height { set; get; }
+
+        public bool IsUp { set; get; }
+
+        public void Update(PointerRoutedEventArgs e)
+        {
+            PointerId = e.Pointer.PointerId;
+
+            var point = e.GetCurrentPoint(_mainPage.Content);
+            X = point.Position.X;
+            Y = point.Position.Y;
+            Width = point.Properties.ContactRect.Width;
+            Height = point.Properties.ContactRect.Height;
+        }
     }
 }
