fe668953acc3b4e3fb23e1d2664961ccdd33eee5
lindexi <lindexi_gd@163.com>
2024-07-17 20:56:53 +0800

    优化绘制的界面

--- SkiaSharp/KernilenoQehinaidelnear/MainWindow.xaml.cs
+++ SkiaSharp/KernilenoQehinaidelnear/MainWindow.xaml.cs
@@ -38,13 +38,15 @@ public partial class MainWindow : Window
             skPaint.StrokeWidth = 2;
             skPaint.Style = SKPaintStyle.Stroke;
 
-            for (int y = 0; y < context.SKBitmap.Height; y += 15)
+            for (int y = 0; y < context.SKBitmap.Height; y += 25)
             {
+                skPaint.Color = new SKColor((uint)Random.Shared.Next()).WithAlpha(0xFF);
                 context.SKCanvas.DrawLine(0, y, context.SKBitmap.Width, y, skPaint);
             }
 
-            for (int x = 0; x < context.SKBitmap.Width; x += 15)
+            for (int x = 0; x < context.SKBitmap.Width; x += 25)
             {
+                skPaint.Color = new SKColor((uint)Random.Shared.Next()).WithAlpha(0xFF);
                 context.SKCanvas.DrawLine(x, 0, x, context.SKBitmap.Height, skPaint);
             }
         });
