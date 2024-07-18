99852189b8ea2c7486d92ad7fbf57d46266dd8fb
lindexi <lindexi_gd@163.com>
2024-07-18 14:51:32 +0800

    支持重新播放

--- UnoDemo/WernujarlerhelkayFebairleewel/WernujarlerhelkayFebairleewel/MainPage.xaml.cs
+++ UnoDemo/WernujarlerhelkayFebairleewel/WernujarlerhelkayFebairleewel/MainPage.xaml.cs
@@ -13,8 +13,18 @@ public sealed partial class MainPage : Page
 
     private async void MainPage_Loaded(object sender, RoutedEventArgs e)
     {
-        await Task.Delay(2000);
+        await Task.Delay(200);
         var storyboard = (Storyboard) Resources["FooBorderStoryboard"];
+        storyboard.Completed += Storyboard_Completed;
+        storyboard.Begin();
+    }
+
+    private async void Storyboard_Completed(object? sender, object e)
+    {
+        await Task.Delay(100);
+        var storyboard = (Storyboard) Resources["FooBorderStoryboard"];
+        storyboard.Stop();
+        storyboard.Resume();
         storyboard.Begin();
     }
 }
