976dd81932051a872ced5409a2c815e9c59929c1
lindexi <lindexi_gd@163.com>
2024-07-19 10:54:57 +0800

    定义基础像素变更逻辑

--- SkiaSharp/KernilenoQehinaidelnear/PixelPanel.cs
+++ SkiaSharp/KernilenoQehinaidelnear/PixelPanel.cs
@@ -1,8 +1,10 @@
-﻿namespace KernilenoQehinaidelnear;
+﻿using Microsoft.Maui.Graphics;
+
+namespace KernilenoQehinaidelnear;
 
 class PixelPanel(SkiaCanvasContext context)
 {
     private SkiaCanvasContext Context => context;
 
-
+    public event EventHandler<Rect>? RenderBoundsChanged;
 }
