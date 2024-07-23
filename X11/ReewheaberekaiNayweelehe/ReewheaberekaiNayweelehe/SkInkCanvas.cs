26fc699b4b42ce61c693a35760e56996f085d438
lindexi <lindexi_gd@163.com>
2024-07-23 14:55:29 +0800

    进行更多同步实现

--- X11/ReewheaberekaiNayweelehe/ReewheaberekaiNayweelehe/SkInkCanvas.cs
+++ X11/ReewheaberekaiNayweelehe/ReewheaberekaiNayweelehe/SkInkCanvas.cs
@@ -13,7 +13,17 @@ namespace ReewheaberekaiNayweelehe;
 
 record InkingModeInputArgs(int Id, StylusPoint StylusPoint, ulong Timestamp)
 {
+    public Point Position => StylusPoint.Point;
+
+    /// <summary>
+    /// 是否来自鼠标的输入
+    /// </summary>
     public bool IsMouse { init; get; }
+
+    /// <summary>
+    /// 被合并的其他历史的触摸点。可能为空
+    /// </summary>
+    public IReadOnlyList<StylusPoint>? StylusPointList { init; get; }
 };
 
 enum InputMode
