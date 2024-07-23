2b44d5139a310514f7cedf54bc57f6f718e7f980
lindexi <lindexi_gd@163.com>
2024-07-23 10:41:47 +0800

    再次翻倍的跑分

--- SkiaSharp/KurwalljurchawrallKakeawelba/App.xaml.cs
+++ SkiaSharp/KurwalljurchawrallKakeawelba/App.xaml.cs
@@ -17,11 +17,13 @@ public partial class App : Application
         var stringBuilder = new StringBuilder();
 
         var stopwatch = Stopwatch.StartNew();
+        var compositeFormat = CompositeFormat.Parse("{0}");
+
         for (int i = 0; i < 1000_0000; i++)
         {
             stringBuilder.Clear();
             //stringBuilder.Append(i);
-            stringBuilder.Append(i.ToString(CultureInfo.InvariantCulture));
+            stringBuilder.AppendFormat(CultureInfo.InvariantCulture, compositeFormat, i);
         }
         stopwatch.Stop();
     }
