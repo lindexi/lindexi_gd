69d67d6b1721d9ca5e2853d83cf535bfe2a54535
lindexi <lindexi_gd@163.com>
2024-07-26 22:58:54 +0800

    更新实现平台调用逻辑

--- Workbench/DacemcalqeleHalibarbubem/KeyboardHookListener.cs
+++ Workbench/DacemcalqeleHalibarbubem/KeyboardHookListener.cs
@@ -3,6 +3,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows.Input;
 
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
 namespace DacemcalqeleHalibarbubem;
 
 public static class KeyboardHookListener
@@ -15,44 +19,23 @@ public static class KeyboardHookListener
         //_threadHookId =
         //    InterceptKeys.SetHook(_proc, InterceptKeys.WH_KEYBOARD, GetCurrentThreadId());
         //安装全局的钩子
-        _globalHookId = InterceptKeys.SetHook(_proc, InterceptKeys.WH_KEYBOARD_LL, 0);
+        _globalHookId = SetHook(_proc, WINDOWS_HOOK_ID.WH_KEYBOARD_LL, 0);
     }
 
     /// <summary>
-    /// 获得当前执行线程id
-    /// </summary>
-    /// <returns></returns>
-    [DllImport("Kernel32.dll", ExactSpelling = true)]
-    private static extern uint GetCurrentThreadId();
-    [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
-    private static extern IntPtr GetModuleHandle(string? lpModuleName);
-
-    [DllImport("User32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    private static extern IntPtr SetWindowsHookEx(int idHook,
-        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-    private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-    [DllImport("User32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
-        IntPtr wParam, IntPtr lParam);
-
-
-    /// <summary>
-    /// 卸载钩子，建议在密码框失焦时卸载钩子
+    /// 卸载钩子
     /// </summary>
     public static void UnhookKeyboard()
     {
-        if (_threadHookId != IntPtr.Zero)
-        {
-            UnhookWindowsHookEx(_threadHookId);
-        }
+        //if (_threadHookId != IntPtr.Zero)
+        //{
+        //    PInvoke.UnhookWindowsHookEx(new HHOOK(_threadHookId));
+        //}
 
-        if (_globalHookId != IntPtr.Zero)
+        if (!_globalHookId.IsInvalid)
         {
-            UnhookWindowsHookEx(_globalHookId);
+            PInvoke.UnhookWindowsHookEx(new HHOOK(_globalHookId.DangerousGetHandle()));
+            _globalHookId.SetHandleAsInvalid();
         }
 
         _proc = null;
@@ -62,29 +45,32 @@ public static class KeyboardHookListener
     public static event RawKeyEventHandler? KeyUp;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    private static LRESULT HookCallback(int code, WPARAM wparam, LPARAM lparam)
     {
-        if (wParam == (IntPtr) InterceptKeys.WM_KEYDOWN)
+        if (wparam == PInvoke.WM_KEYDOWN)
         {
-            int vkCode = Marshal.ReadInt32(lParam);
+            int vkCode = Marshal.ReadInt32(lparam);
 
             if (KeyDown != null)
                 KeyDown(null, new RawKeyEventArgs(vkCode, false));
         }
-        else if (wParam == (IntPtr) InterceptKeys.WM_KEYUP)
+        else if (wparam == PInvoke.WM_KEYUP)
         {
-            int vkCode = Marshal.ReadInt32(lParam);
+            int vkCode = Marshal.ReadInt32(lparam);
 
             if (KeyUp != null)
+            {
                 KeyUp(null, new RawKeyEventArgs(vkCode, false));
+            }
         }
-        return CallNextHookEx(_globalHookId, nCode, wParam, lParam);
+
+        return PInvoke.CallNextHookEx(_globalHookId, code, wparam, lparam);
     }
 
     /// <summary>
     /// 定义一个局部变量保存，防止被垃圾回收
     /// </summary>
-    private static LowLevelKeyboardProc? _proc;
+    private static HOOKPROC? _proc;
     public delegate void RawKeyEventHandler(object? sender, RawKeyEventArgs args);
     public class RawKeyEventArgs : EventArgs
     {
@@ -100,34 +86,16 @@ public static class KeyboardHookListener
         }
     }
 
-    private static IntPtr _threadHookId = IntPtr.Zero;
-    private static IntPtr _globalHookId = IntPtr.Zero;
+    //private static IntPtr _threadHookId = IntPtr.Zero;
+    private static UnhookWindowsHookExSafeHandle _globalHookId = new UnhookWindowsHookExSafeHandle(IntPtr.Zero, false);
 
-    static class InterceptKeys
+    private static UnhookWindowsHookExSafeHandle SetHook(HOOKPROC proc, WINDOWS_HOOK_ID hookType, uint threadId)
     {
-        /// <summary>
-        /// 全局钩子定义
-        /// </summary>
-        public const int WH_KEYBOARD_LL = 13;
+        using var currentProcess = Process.GetCurrentProcess();
+        var currentProcessMainModule = currentProcess.MainModule!;
 
-        /// <summary>
-        /// 私有钩子定义
-        /// </summary>
-        public const int WH_KEYBOARD = 2;
+        var moduleHandle = PInvoke.GetModuleHandle(currentProcessMainModule.ModuleName);
 
-        public static int WM_KEYDOWN = 0x0100;
-        public static int WM_KEYUP = 0x0101;
-
-        public static IntPtr SetHook(LowLevelKeyboardProc proc, int hookType, uint threadId)
-        {
-            using (var curProcess = Process.GetCurrentProcess())
-            {
-                using (var curModule = curProcess.MainModule)
-                {
-                    return SetWindowsHookEx(hookType, proc, GetModuleHandle(curModule?.ModuleName),
-                        threadId);
-                }
-            }
-        }
+        return PInvoke.SetWindowsHookEx(hookType, proc, moduleHandle, threadId);
     }
 }
