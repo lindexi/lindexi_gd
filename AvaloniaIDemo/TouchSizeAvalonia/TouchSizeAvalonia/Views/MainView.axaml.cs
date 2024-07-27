00827440308ca9e8239dff2293d320eb107183df
lindexi <lindexi_gd@163.com>
2024-07-27 17:38:55 +0800

    测试触摸面积

--- AvaloniaIDemo/TouchSizeAvalonia/TouchSizeAvalonia/Views/MainView.axaml.cs
+++ AvaloniaIDemo/TouchSizeAvalonia/TouchSizeAvalonia/Views/MainView.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace TouchSizeAvalonia.Views;
 
@@ -15,8 +16,20 @@ public partial class MainView : UserControl
     {
         var currentPoint = e.GetCurrentPoint(null);
         var position = currentPoint.Position;
+
         dynamic d = currentPoint.Properties;
         Rect contactRect = d.ContactRect;
-        MessageTextBlock.Text = $"X: {position.X}, Y: {position.Y} WH={contactRect.Width},{contactRect.Height}";
+
+        if (contactRect.Width > 0 && contactRect.Height > 0)
+        {
+            var translateTransform = (TranslateTransform) FooBorder.RenderTransform!;
+            translateTransform.X = position.X - contactRect.Width / 2;
+            translateTransform.Y = position.Y - contactRect.Height / 2;
+
+            FooBorder.Width = contactRect.Width;
+            FooBorder.Height = contactRect.Height;
+
+            MessageTextBlock.Text = $"X: {position.X}, Y: {position.Y} WH={contactRect.Width},{contactRect.Height}";
+        }
     }
 }
