4c5a4b06b3c3aba549c2e3cbc4f319185a8d337e
lindexi <lindexi_gd@163.com>
2024-07-27 17:23:21 +0800

    Test touch size
    https://github.com/AvaloniaUI/Avalonia/pull/16498

---
+++ AvaloniaIDemo/TouchSizeAvalonia/TouchSizeAvalonia/Views/MainView.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace TouchSizeAvalonia.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+        this.PointerMoved += MainView_PointerMoved;
+    }
+
+    private void MainView_PointerMoved(object? sender, Avalonia.Input.PointerEventArgs e)
+    {
+        var currentPoint = e.GetCurrentPoint(null);
+        var position = currentPoint.Position;
+        dynamic d = currentPoint.Properties;
+        Rect contactRect = d.ContactRect;
+        MessageTextBlock.Text = $"X: {position.X}, Y: {position.Y} WH={contactRect.Width},{contactRect.Height}";
+    }
+}
