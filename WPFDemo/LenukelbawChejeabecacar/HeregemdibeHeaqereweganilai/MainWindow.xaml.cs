6ef3090637b113bcfcc3d95ab10123f71a2fa387
lindexi <lindexi_gd@163.com>
2024-07-30 17:21:59 +0800

    测试显示提示但是失败

---
+++ WPFDemo/LenukelbawChejeabecacar/HeregemdibeHeaqereweganilai/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace HeregemdibeHeaqereweganilai;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 10240))
+        {
+            var xmlDocument = new XmlDocument();
+            // lang=xml
+            xmlDocument.LoadXml(xml: """
+                                     <toast launch='conversationId=9813'>
+                                         <visual>
+                                             <binding template='ToastGeneric'>
+                                                 <text>Some text</text>
+                                             </binding>
+                                         </visual>
+                                     </toast>
+                                     """);
+            var toastNotification = new ToastNotification(xmlDocument);
+            ToastNotificationManager.CreateToastNotifier().Show(toastNotification);
+        }
+    }
+}
