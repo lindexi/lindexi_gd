90bac0749ff427ba2f9b92dc5f15c4c323dfc2d9
lindexi <lindexi_gd@163.com>
2024-07-31 17:37:37 +0800

    去掉别名尝试修复其他项目构建

--- CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Rect2D.cs
+++ CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Rect2D.cs
@@ -23,4 +23,34 @@ public readonly record struct Rect2D(Point2D Position, Size2D Size)
     {
         return new Rect2D(left, top, right - left, bottom - top);
     }
+
+    public bool Contains(Rect2D rect)
+    {
+        return X <= rect.X && Right >= rect.Right && Y <= rect.Y && Bottom >= rect.Bottom;
+    }
+
+    public bool Contains(Point2D pt)
+    {
+        return Contains(pt.X, pt.Y);
+    }
+
+    public bool Contains(double x, double y)
+    {
+        return (x >= Left) && (x < Right) && (y >= Top) && (y < Bottom);
+    }
+
+    public bool IntersectsWith(Rect2D r)
+    {
+        return !((Left >= r.Right) || (Right <= r.Left) || (Top >= r.Bottom) || (Bottom <= r.Top));
+    }
+
+    public Rect2D Union(Rect2D r)
+    {
+        return Union(this, r);
+    }
+
+    public static Rect2D Union(Rect2D r1, Rect2D r2)
+    {
+        return FromLTRB(Math.Min(r1.Left, r2.Left), Math.Min(r1.Top, r2.Top), Math.Max(r1.Right, r2.Right), Math.Max(r1.Bottom, r2.Bottom));
+    }
 }
