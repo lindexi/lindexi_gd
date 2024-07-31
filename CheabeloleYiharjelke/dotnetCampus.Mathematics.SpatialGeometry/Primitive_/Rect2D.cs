cebcedfb6888def4079d18869e0b52615a81e4f8
lindexi <lindexi_gd@163.com>
2024-07-31 20:49:16 +0800

    定义更多实现方法

--- CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Rect2D.cs
+++ CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Rect2D.cs
@@ -1,13 +1,15 @@
-﻿namespace dotnetCampus.Mathematics.SpatialGeometry;
+﻿using System.Drawing;
 
-public readonly record struct Rect2D(Point2D Position, Size2D Size)
+namespace dotnetCampus.Mathematics.SpatialGeometry;
+
+public readonly record struct Rect2D(Point2D Location, Size2D Size)
 {
     public Rect2D(double x, double y, double width, double height) : this(new Point2D(x, y), new Size2D(width, height))
     {
     }
 
-    public double X => Position.X;
-    public double Y => Position.Y;
+    public double X => Location.X;
+    public double Y => Location.Y;
 
     public double Width => Size.Width;
     public double Height => Size.Height;
@@ -52,5 +54,52 @@ public readonly record struct Rect2D(Point2D Position, Size2D Size)
     public static Rect2D Union(Rect2D r1, Rect2D r2)
     {
         return FromLTRB(Math.Min(r1.Left, r2.Left), Math.Min(r1.Top, r2.Top), Math.Max(r1.Right, r2.Right), Math.Max(r1.Bottom, r2.Bottom));
+    }
+
+    public Rect2D Intersect(Rect2D r)
+    {
+        return Intersect(this, r);
+    }
+
+    public static Rect2D Intersect(Rect2D r1, Rect2D r2)
+    {
+        double x = Math.Max(r1.X, r2.X);
+        double y = Math.Max(r1.Y, r2.Y);
+        double width = Math.Min(r1.Right, r2.Right) - x;
+        double height = Math.Min(r1.Bottom, r2.Bottom) - y;
+
+        if (width < 0 || height < 0)
+        {
+            return Zero;
+        }
+        return new Rect2D(x, y, width, height);
+    }
+
+    public Rect2D Inflate(Size sz)
+    {
+        return Inflate(sz.Width, sz.Height);
+    }
+
+    public Rect2D Inflate(double width, double height)
+    {
+        return new Rect2D(X - width, Y - height, Width + width, Height + height);
+    }
+
+    public Rect2D Offset(double dx, double dy)
+    {
+        return this with
+        {
+            Location = new Point2D(X + dx, Y + dy)
+        };
+    }
+
+    public Rect2D Offset(Point dr)
+    {
+        return Offset(dr.X, dr.Y);
+    }
+
+    public Rect2D Round()
+    {
+        return new Rect2D(Math.Round(X), Math.Round(Y), Math.Round(Width), Math.Round(Height));
     }
 }
