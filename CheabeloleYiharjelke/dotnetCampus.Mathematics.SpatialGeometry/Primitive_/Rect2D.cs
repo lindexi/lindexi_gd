4f8bad21e6b7cb68b9a1818937430f7e7729b3f2
lindexi <lindexi_gd@163.com>
2024-07-31 17:24:49 +0800

    创建基础的类型

---
+++ CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Rect2D.cs
@@ -0,0 +1,15 @@
+﻿namespace dotnetCampus.Mathematics.SpatialGeometry;
+
+public readonly record struct Rect2D(Point2D Position, Size2D Size)
+{
+    public double X => Position.X;
+    public double Y => Position.Y;
+
+    public double Width => Size.Width;
+    public double Height => Size.Height;
+
+    public double Left => X;
+    public double Top => Y;
+    public double Right => X + Width;
+    public double Bottom => Y + Height;
+}
---
+++ CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Size2D.cs
@@ -0,0 +1,3 @@
+﻿namespace dotnetCampus.Mathematics.SpatialGeometry;
+
+public readonly record struct Size2D(double Width, double Height);
