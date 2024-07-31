94aba2487eaed2b4023cea12a398899e5467011a
lindexi <lindexi_gd@163.com>
2024-07-31 17:31:14 +0800

    添加更多辅助方法

--- CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Rect2D.cs
+++ CheabeloleYiharjelke/dotnetCampus.Mathematics.SpatialGeometry/Primitive_/Rect2D.cs
@@ -2,6 +2,10 @@
 
 public readonly record struct Rect2D(Point2D Position, Size2D Size)
 {
+    public Rect2D(double x, double y, double width, double height) : this(new Point2D(x, y), new Size2D(width, height))
+    {
+    }
+
     public double X => Position.X;
     public double Y => Position.Y;
 
@@ -12,4 +16,11 @@ public readonly record struct Rect2D(Point2D Position, Size2D Size)
     public double Top => Y;
     public double Right => X + Width;
     public double Bottom => Y + Height;
+
+    public static Rect2D Zero => default;
+
+    public static Rect2D FromLTRB(double left, double top, double right, double bottom)
+    {
+        return new Rect2D(left, top, right - left, bottom - top);
+    }
 }
