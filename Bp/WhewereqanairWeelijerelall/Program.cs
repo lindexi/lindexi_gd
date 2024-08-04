0fd330d59e5a015c807133b828507ad13bfb53c6
lindexi <lindexi_gd@163.com>
2024-08-04 14:59:42 +0800

    新建空白项目定义数据

---
+++ Bp/WhewereqanairWeelijerelall/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhewereqanairWeelijerelall;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+record ElementInput();
+record ElementOutput();
+
+interface IElement
+{
+}
+
+
+class Element : IElement
+{
+}
+
+class Group : IElement
+{
+}
