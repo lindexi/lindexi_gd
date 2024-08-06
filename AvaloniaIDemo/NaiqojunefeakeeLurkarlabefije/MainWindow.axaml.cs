2252b3e97056656aef3446f4dcb26758ac4b30c3
lindexi <lindexi_gd@163.com>
2024-08-06 10:57:01 +0800

    创建空的项目

---
+++ AvaloniaIDemo/NaiqojunefeakeeLurkarlabefije/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NaiqojunefeakeeLurkarlabefije;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
