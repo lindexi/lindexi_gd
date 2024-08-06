07b8351fd930ecb3477e58b31edbdb8a8f7978f4
lindexi <lindexi_gd@163.com>
2024-08-06 08:56:19 +0800

    测试自动登录失败

---
+++ Workbench/BallcaljawoNearnaikarwe/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+var startInfo = new ProcessStartInfo("cmd")
+{
+    WorkingDirectory = Environment.CurrentDirectory,
+    RedirectStandardOutput = true,
+    RedirectStandardError = true,
+    RedirectStandardInput = true,
+
+    //Arguments = "ssh -o ServerAliveInterval=600 uos@172.20.113.26",
+};
+
+// 虚拟机： 172.20.113.26
+// 账号: uos
+// 密码: 123
+
+var process = new Process()
+{
+    StartInfo = startInfo
+};
+process.Start();
+
+process.OutputDataReceived += Process_OutputDataReceived;
+var standardOutput = process.StandardOutput;
+var standardError = process.StandardError;
+var standardInput = process.StandardInput;
+
+
+standardInput.WriteLine("ssh -o ServerAliveInterval=600 uos@172.20.113.26");
+standardInput.WriteLine("123");
+
+Console.SetIn(standardOutput);
+Console.SetOut(standardInput);
+
+Console.WriteLine("Hello, World!");
+
+void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+{
+    Console.WriteLine($"[OutputDataReceived] {e.Data}");
+}
