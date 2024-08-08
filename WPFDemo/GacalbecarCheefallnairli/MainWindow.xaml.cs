ed637357febb31802d068b359aa3671ea68c0d3b
lindexi <lindexi_gd@163.com>
2024-08-08 14:36:40 +0800

    测试不成对触发 Loaded 失败

---
+++ WPFDemo/GacalbecarCheefallnairli/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GacalbecarCheefallnairli;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        await Task.Delay(1000);
+        Foo.Visibility = Visibility.Visible;
+    }
+}
+
+public class Foo : UserControl
+{
+    public Foo()
+    {
+        Loaded += Foo_Loaded;
+    }
+
+    private void Foo_Loaded(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("Loaded");
+    }
+}
