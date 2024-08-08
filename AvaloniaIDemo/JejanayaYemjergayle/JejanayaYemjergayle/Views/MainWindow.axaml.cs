c5f192017088ebbfe4bab42f51c15e6da77105e1
lindexi <lindexi_gd@163.com>
2024-08-08 11:25:05 +0800

    Revert "尝试换成其他的值"
    This reverts commit a5c32bf75af3f14634fff4eb78c133191961994b.

--- AvaloniaIDemo/JejanayaYemjergayle/JejanayaYemjergayle/Views/MainWindow.axaml.cs
+++ AvaloniaIDemo/JejanayaYemjergayle/JejanayaYemjergayle/Views/MainWindow.axaml.cs
@@ -16,18 +16,18 @@ public partial class MainWindow : Window
 
     private void MainWindow_Activated(object? sender, EventArgs e)
     {
-        var pointToScreen = this.PointToScreen(new Point(10,10));
+        var pointToScreen = this.PointToScreen(new Point(0, 0));
         Console.WriteLine($"MainWindow_Activated PointToScreen={pointToScreen}");
     }
 
     private async void MainWindow_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        var pointToScreen = this.PointToScreen(new Point(10, 10));
+        var pointToScreen = this.PointToScreen(new Point(0, 0));
         Console.WriteLine($"MainWindow_Loaded PointToScreen={pointToScreen}");
 
         await Task.Delay(1000);
 
-        pointToScreen = this.PointToScreen(new Point(10, 10));
+        pointToScreen = this.PointToScreen(new Point(0, 0));
         Console.WriteLine(pointToScreen);
     }
 }
