41e6f2ae447aedd10ec5ba5f1b3ed03209b1be89
lindexi <lindexi_gd@163.com>
2024-08-08 11:16:23 +0800

    Can not get from Activated

--- AvaloniaIDemo/JejanayaYemjergayle/JejanayaYemjergayle/Views/MainWindow.axaml.cs
+++ AvaloniaIDemo/JejanayaYemjergayle/JejanayaYemjergayle/Views/MainWindow.axaml.cs
@@ -11,12 +11,19 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         Loaded += MainWindow_Loaded;
+        this.Activated += MainWindow_Activated;
+    }
+
+    private void MainWindow_Activated(object? sender, EventArgs e)
+    {
+        var pointToScreen = this.PointToScreen(new Point(0, 0));
+        Console.WriteLine($"MainWindow_Activated PointToScreen={pointToScreen}");
     }
 
     private async void MainWindow_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
         var pointToScreen = this.PointToScreen(new Point(0, 0));
-        Console.WriteLine(pointToScreen);
+        Console.WriteLine($"MainWindow_Loaded PointToScreen={pointToScreen}");
 
         await Task.Delay(1000);
 
