38279bbfa71913130ad833a0f7a748c9f74500da
lindexi <lindexi_gd@163.com>
2024-08-10 11:16:59 +0800

    加上两个测试但是似乎设计不合理

---
+++ BulowukaileFeanayjairwo/缓存命中Benchmark.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace benchmark;
+
+// Copy from https://github.com/SeWZC
+
+[BenchmarkCategory]
+public class 缓存命中Benchmark
+{
+    // 4 GB
+    public const int PoolCount = 1 << 30;
+    private const int Count = 100_0000;
+
+    public static int[] 待查找数组;
+    public int[] 乱序数组;
+    public int[] 顺序数组;
+
+    static 缓存命中Benchmark()
+    {
+        待查找数组 = new int[PoolCount];
+        for (var i = 0; i < PoolCount; i++)
+            待查找数组[i] = i;
+    }
+
+    public 缓存命中Benchmark()
+    {
+        乱序数组 = new int[Count];
+        for (var i = 0; i < Count; i++)
+            乱序数组[i] = Random.Shared.Next(0, PoolCount);
+        顺序数组 = new int[Count];
+        for (var i = 0; i < Count; i++)
+            顺序数组[i] = i;
+    }
+
+    [Benchmark]
+    public int 乱序数组读取()
+    {
+        var sum = 0;
+        for (var i = 0; i < 乱序数组.Length; i++)
+            sum += 待查找数组[乱序数组[i]];
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int 顺序数组读取()
+    {
+        var sum = 0;
+        for (var i = 0; i < 顺序数组.Length; i++)
+            sum += 待查找数组[顺序数组[i]];
+
+        return sum;
+    }
+}
