b45e5655a102898958bd507119ce623a8a000ef4
lindexi <lindexi_gd@163.com>
2024-08-10 11:33:37 +0800

    优化一点代码逻辑

--- BulowukaileFeanayjairwo/缓存命中Benchmark.cs
+++ BulowukaileFeanayjairwo/缓存命中Benchmark.cs
@@ -11,33 +11,44 @@ public class 缓存命中Benchmark
     public const int PoolCount = 1 << 30;
     private const int Count = 100_0000;
 
-    public static int[] 待查找数组;
-    public int[] 乱序数组;
-    public int[] 顺序数组;
+    private static int[] 待查找数组;
+    private readonly int[] _乱序数组;
+    private readonly int[] _顺序数组;
 
     static 缓存命中Benchmark()
     {
         待查找数组 = new int[PoolCount];
         for (var i = 0; i < PoolCount; i++)
+        {
             待查找数组[i] = i;
+        }
     }
 
     public 缓存命中Benchmark()
     {
-        乱序数组 = new int[Count];
+        _乱序数组 = new int[Count];
         for (var i = 0; i < Count; i++)
-            乱序数组[i] = Random.Shared.Next(0, PoolCount);
-        顺序数组 = new int[Count];
+        {
+            // 这里的随机数是为了让数组乱序，但是性能测试上的随机数顺序将会极大影响测试结果
+            _乱序数组[i] = Random.Shared.Next(0, PoolCount);
+        }
+
+        // 顺序数组只是为了和乱序数组对应，也做一次数组里面的取值
+        _顺序数组 = new int[Count];
         for (var i = 0; i < Count; i++)
-            顺序数组[i] = i;
+        {
+            _顺序数组[i] = i;
+        }
     }
 
     [Benchmark]
     public int 乱序数组读取()
     {
         var sum = 0;
-        for (var i = 0; i < 乱序数组.Length; i++)
-            sum += 待查找数组[乱序数组[i]];
+        for (var i = 0; i < _乱序数组.Length; i++)
+        {
+            sum += 待查找数组[_乱序数组[i]];
+        }
 
         return sum;
     }
@@ -46,8 +57,10 @@ public class 缓存命中Benchmark
     public int 顺序数组读取()
     {
         var sum = 0;
-        for (var i = 0; i < 顺序数组.Length; i++)
-            sum += 待查找数组[顺序数组[i]];
+        for (var i = 0; i < _顺序数组.Length; i++)
+        {
+            sum += 待查找数组[_顺序数组[i]];
+        }
 
         return sum;
     }
