ed775d7e57570835bb18088aa36e91bdb45230fc
lindexi <lindexi_gd@163.com>
2024-08-15 09:31:02 +0800

    Revert "Test the X11FramebufferSurface Blit sync"
    This reverts commit d553a7c1ac7ecce965300526a57261631b1c9ae4.

--- X11/JaibubarebafoNilairdihaw/Program.cs
+++ X11/JaibubarebafoNilairdihaw/Program.cs
@@ -58,12 +58,41 @@ XSelectInput(display, handle, mask);
 XMapWindow(display, handle);
 XFlush(display);
 
+var gc = XCreateGC(display, handle, 0, 0);
+
+var mapLength = width * 4 * height;
+Console.WriteLine($"Length = {mapLength}");
+var backendMap = mmap(IntPtr.Zero, new IntPtr(mapLength), 3, 0x22, -1, IntPtr.Zero);
+
+var skImageInfo = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+var rowBytes = width * 4;
+var skSurface = SKSurface.Create(skImageInfo, backendMap, rowBytes, new SKSurfaceProperties(SKPixelGeometry.BgrHorizontal));
+
 var skBitmap = new SKBitmap(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
 
-var skCanvas = new SKCanvas(skBitmap);
+var skCanvas = skSurface.Canvas; //new SKCanvas(skBitmap);
+
+var xImage = CreateImage(skBitmap);
+
+var skBitmap2 = new SKBitmap(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+
+IntPtr mapCache = IntPtr.Zero;
+
+//{
+//    var stopwatch = Stopwatch.StartNew();
+//    var length = 1000;
+//    for (var i = 0; i < length; i++)
+//    {
+//        ReplacePixels(skBitmap2, skBitmap);
+//    }
+//    stopwatch.Stop();
+//    Console.WriteLine($"拷贝耗时：{stopwatch.ElapsedMilliseconds * 1.0 / length}");
+//}
 
 skCanvas.Clear(new SKColor((uint) Random.Shared.Next()).WithAlpha(0xFF));
 skCanvas.Flush();
+
+IntPtr invokeMessageId = new IntPtr(123123123);
 
 Task.Run(() =>
 {
@@ -72,7 +101,24 @@ Task.Run(() =>
     while (true)
     {
         Console.ReadLine();
-       
+        //var @event = new XEvent
+        //{
+        //    ClientMessageEvent =
+        //    {
+        //        type = XEventName.ClientMessage,
+        //        send_event = true,
+        //        window = handle,
+        //        message_type = 0,
+        //        format = 32,
+        //        ptr1 = invokeMessageId,
+        //        ptr2 = 0,
+        //        ptr3 = 0,
+        //        ptr4 = 0,
+        //    }
+        //};
+
+        //XSendEvent(newDisplay, handle, false, 0, ref @event);
+
         var xEvent = new XEvent
         {
             ExposeEvent =
@@ -94,13 +140,11 @@ Task.Run(() =>
             ref xEvent);
 
         XFlush(newDisplay);
-        Console.WriteLine($"Finish SendEvent");
+        Console.WriteLine($"发送");
     }
 
     XCloseDisplay(newDisplay);
 });
-
-bool useXFlush = false;
 
 while (true)
 {
@@ -117,11 +161,234 @@ while (true)
         skCanvas.Flush();
 
         var stopwatch = Stopwatch.StartNew();
-        Blit(skBitmap);
+        BlitSurface(skSurface);
+
+        //BlitUnsafe(skBitmap);
+        //Blit(skBitmap);
         stopwatch.Stop();
-        Console.WriteLine($"Expose Cost： {stopwatch.ElapsedMilliseconds} ms");
+        Console.WriteLine($"耗时：{stopwatch.ElapsedMilliseconds}");
+    }
+
+    if (@event.type == XEventName.ClientMessage)
+    {
+        if (@event.ClientMessageEvent.ptr1 == invokeMessageId)
+        {
+            skCanvas.Clear(new SKColor((uint) Random.Shared.Next()));
+            skCanvas.Flush();
+
+            var stopwatch = Stopwatch.StartNew();
+            XPutImage(display, handle, gc, ref xImage, 0, 0, 0, 0, (uint) skBitmap.Width,
+                (uint) skBitmap.Height);
+            stopwatch.Stop();
+            Console.WriteLine($"耗时：{stopwatch.ElapsedMilliseconds}");
+        }
     }
 }
+
+unsafe void BlitSurface(SKSurface source)
+{
+    var list = new List<string>();
+
+    var stopwatch = Stopwatch.StartNew();
+    // 从 source 拷贝到 mmap 耗时大概是 5-6
+    // 但从 SKBitmap 拷贝到 SKBitmap 只耗时 1-2
+    var pixel = backendMap;
+
+    int size = mapLength;
+
+    if (mapCache == 0)
+    {
+        mapCache = mmap(IntPtr.Zero, new IntPtr(size), 3, 0x22, -1, IntPtr.Zero);
+    }
+
+    var map = mapCache;
+
+    stopwatch.Stop();
+    list.Add($"创建 Bitmap 耗时 {stopwatch.ElapsedMilliseconds}");
+    stopwatch.Restart();
+
+    //Buffer.MemoryCopy((byte*)pixel,(byte*)map, size, size);
+    Unsafe.CopyBlockUnaligned((byte*) map, (byte*) pixel, (uint) size);
+
+    stopwatch.Stop();
+    list.Add($"拷贝耗时 {stopwatch.ElapsedMilliseconds}");
+    stopwatch.Restart();
+
+    var image = new XImage
+    {
+        width = width,
+        height = height,
+        format = 2, //ZPixmap;
+        data = map,
+        byte_order = 0, // LSBFirst;
+        bitmap_unit = 32,
+        bitmap_bit_order = 0, // LSBFirst;
+        bitmap_pad = 32,
+        depth = 32,
+        bytes_per_line = width * 4,
+        bits_per_pixel = 32,
+    };
+    XInitImage(ref image);
+
+    Task.Run(() =>
+    {
+        var stopwatch = Stopwatch.StartNew();
+        XLockDisplay(display);
+
+        stopwatch.Stop();
+        list.Add($"XLockDisplay 耗时 {stopwatch.ElapsedMilliseconds}");
+        stopwatch.Restart();
+
+        try
+        {
+            stopwatch.Stop();
+            list.Add($"CreateImage 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+
+            var gc = XCreateGC(display, handle, 0, IntPtr.Zero);
+
+            stopwatch.Stop();
+            list.Add($"XCreateGC 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+
+            XPutImage(display, handle, gc, ref image, 0, 0, 0, 0, (uint) width,
+                (uint) height);
+
+            stopwatch.Stop();
+            list.Add($"XPutImage 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+
+            XFreeGC(display, gc);
+            //XSync(display, true); // 这里用同步的速度太慢，需要等下一次收到数据
+            XFlush(display);
+
+            stopwatch.Stop();
+            list.Add($"XFlush 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+        }
+        finally
+        {
+            XUnlockDisplay(display);
+
+            stopwatch.Stop();
+            list.Add($"XUnlockDisplay 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+            //munmap(map, new IntPtr(size));
+        }
+        stopwatch.Stop();
+        //Console.WriteLine($"实际推送耗时 {stopwatch.ElapsedMilliseconds}");
+
+        foreach (var s in list)
+        {
+            Console.WriteLine(s);
+        }
+    });
+}
+
+unsafe void BlitUnsafe(SKBitmap source)
+{
+    var list = new List<string>();
+
+    var stopwatch = Stopwatch.StartNew();
+    // 从 source 拷贝到 mmap 耗时大概是 5-6
+    // 但从 SKBitmap 拷贝到 SKBitmap 只耗时 1-2
+    var pixel = source.GetPixels(out var length);
+
+    int size = length.ToInt32();
+
+    if (mapCache == 0)
+    {
+        mapCache = mmap(IntPtr.Zero, new IntPtr(size), 3, 0x22, -1, IntPtr.Zero);
+    }
+
+    var map = mapCache;
+
+    stopwatch.Stop();
+    list.Add($"创建 Bitmap 耗时 {stopwatch.ElapsedMilliseconds}");
+    stopwatch.Restart();
+
+    //Buffer.MemoryCopy((byte*)pixel,(byte*)map, size, size);
+    Unsafe.CopyBlockUnaligned((byte*) map, (byte*) pixel, (uint) size);
+
+    stopwatch.Stop();
+    list.Add($"拷贝耗时 {stopwatch.ElapsedMilliseconds}");
+    stopwatch.Restart();
+
+    var image = new XImage
+    {
+        width = source.Width,
+        height = source.Height,
+        format = 2, //ZPixmap;
+        data = map,
+        byte_order = 0, // LSBFirst;
+        bitmap_unit = 32,
+        bitmap_bit_order = 0, // LSBFirst;
+        bitmap_pad = 32,
+        depth = 32,
+        bytes_per_line = source.Width * 4,
+        bits_per_pixel = 32,
+    };
+    XInitImage(ref image);
+
+    Task.Run(() =>
+    {
+        var stopwatch = Stopwatch.StartNew();
+        XLockDisplay(display);
+
+        stopwatch.Stop();
+        list.Add($"XLockDisplay 耗时 {stopwatch.ElapsedMilliseconds}");
+        stopwatch.Restart();
+
+        try
+        {
+            stopwatch.Stop();
+            list.Add($"CreateImage 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+
+            var gc = XCreateGC(display, handle, 0, IntPtr.Zero);
+
+            stopwatch.Stop();
+            list.Add($"XCreateGC 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+
+            XPutImage(display, handle, gc, ref image, 0, 0, 0, 0, (uint) source.Width,
+                (uint) source.Height);
+
+            stopwatch.Stop();
+            list.Add($"XPutImage 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+
+            XFreeGC(display, gc);
+            //XSync(display, true); // 这里用同步的速度太慢，需要等下一次收到数据
+            XFlush(display);
+
+            stopwatch.Stop();
+            list.Add($"XFlush 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+        }
+        finally
+        {
+            XUnlockDisplay(display);
+
+            stopwatch.Stop();
+            list.Add($"XUnlockDisplay 耗时 {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+            //munmap(map, new IntPtr(size));
+        }
+        stopwatch.Stop();
+        //Console.WriteLine($"实际推送耗时 {stopwatch.ElapsedMilliseconds}");
+
+        foreach (var s in list)
+        {
+            Console.WriteLine(s);
+        }
+    });
+}
+
+[DllImport("libc", SetLastError = true)]
+static extern IntPtr mmap(IntPtr addr, IntPtr length, int prot, int flags, int fd, IntPtr offset);
+[DllImport("libc", SetLastError = true)]
+static extern int munmap(IntPtr addr, IntPtr length);
 
 async void Blit(SKBitmap source)
 {
@@ -131,12 +398,12 @@ async void Blit(SKBitmap source)
     using var renderBitmap = new SKBitmap(source.Width, source.Height, SKColorType.Bgra8888, SKAlphaType.Premul);
     stopwatch.Stop();
 
-    list.Add($"Create SKBitmap Cost: {stopwatch.ElapsedMilliseconds} ms");
+    list.Add($"创建 Bitmap 耗时 {stopwatch.ElapsedMilliseconds}");
 
     stopwatch.Restart();
     ReplacePixels(renderBitmap, source);
     stopwatch.Stop();
-    list.Add($"Copy Pixel Cost: {stopwatch.ElapsedMilliseconds}  ms");
+    list.Add($"拷贝耗时 {stopwatch.ElapsedMilliseconds}");
 
     await Task.Run(() =>
     {
@@ -144,7 +411,7 @@ async void Blit(SKBitmap source)
         XLockDisplay(display);
 
         stopwatch.Stop();
-        list.Add($"XLockDisplay Cost: {stopwatch.ElapsedMilliseconds} ms");
+        list.Add($"XLockDisplay 耗时 {stopwatch.ElapsedMilliseconds}");
         stopwatch.Restart();
 
         try
@@ -152,54 +419,40 @@ async void Blit(SKBitmap source)
             var image = CreateImage(renderBitmap);
 
             stopwatch.Stop();
-            list.Add($"CreateImage Cost: {stopwatch.ElapsedMilliseconds} ms");
+            list.Add($"CreateImage 耗时 {stopwatch.ElapsedMilliseconds}");
             stopwatch.Restart();
 
             var gc = XCreateGC(display, handle, 0, IntPtr.Zero);
 
             stopwatch.Stop();
-            list.Add($"XCreateGC Cost: {stopwatch.ElapsedMilliseconds}  ms");
+            list.Add($"XCreateGC 耗时 {stopwatch.ElapsedMilliseconds}");
             stopwatch.Restart();
 
             XPutImage(display, handle, gc, ref image, 0, 0, 0, 0, (uint) renderBitmap.Width,
                 (uint) renderBitmap.Height);
 
             stopwatch.Stop();
-            list.Add($"XPutImage Cost: {stopwatch.ElapsedMilliseconds} ms");
+            list.Add($"XPutImage 耗时 {stopwatch.ElapsedMilliseconds}");
             stopwatch.Restart();
 
             XFreeGC(display, gc);
+            //XSync(display, true); // 这里用同步的速度太慢，需要等下一次收到数据
+            XFlush(display);
 
             stopwatch.Stop();
-            list.Add($"XFreeGC Cost: {stopwatch.ElapsedMilliseconds} ms");
+            list.Add($"XFlush 耗时 {stopwatch.ElapsedMilliseconds}");
             stopwatch.Restart();
-
-            if (useXFlush)
-            {
-                XFlush(display);
-                stopwatch.Stop();
-                list.Add($"XFlush Cost: {stopwatch.ElapsedMilliseconds} ms");
-                stopwatch.Restart();
-            }
-            else
-            {
-                XSync(display, true);
-                stopwatch.Stop();
-                list.Add($"XSync Cost: {stopwatch.ElapsedMilliseconds} ms");
-                stopwatch.Restart();
-            }
-
-            useXFlush = !useXFlush;
         }
         finally
         {
             XUnlockDisplay(display);
 
             stopwatch.Stop();
-            list.Add($"XUnlockDisplay Cost: {stopwatch.ElapsedMilliseconds} ms");
+            list.Add($"XUnlockDisplay 耗时 {stopwatch.ElapsedMilliseconds}");
             stopwatch.Restart();
         }
         stopwatch.Stop();
+        //Console.WriteLine($"实际推送耗时 {stopwatch.ElapsedMilliseconds}");
 
         foreach (var s in list)
         {
