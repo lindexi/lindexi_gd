a06ffaccb04b77e5159cbb6761b9bdea534dddfb
lindexi <lindexi_gd@163.com>
2024-08-16 09:09:55 +0800

    尝试创建序列化失败

---
+++ Workbench/YurkallkowhawfearrallkuWanagercika/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Xml.Serialization;
+
+var f1 = new F1<F2<int>>();
+var xmlSerializer = new XmlSerializer(f1.GetType());
+xmlSerializer.Serialize(Console.Out, f1);
+
+Console.WriteLine("Hello, World!");
+
+class F1<T>
+{
+}
+
+class F2<T>
+{
+}
