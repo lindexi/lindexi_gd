470e8c47e9e5241686b360ce8c75ee5a36fd70e4
lindexi <lindexi_gd@163.com>
2024-08-16 14:26:04 +0800

    尝试创建共享的数据
    X Error of failed request:  BadAccess (attempt to access private resource denied)
      Major opcode of failed request:  130 (MIT-SHM)
      Minor opcode of failed request:  1 (X_ShmAttach)
      Serial number of failed request:  16
      Current serial number in output stream:  16

--- X11/WercawchallwarnefeWhedurcachay/Program.cs
+++ X11/WercawchallwarnefeWhedurcachay/Program.cs
@@ -6,6 +6,8 @@ using CPF.Linux;
 
 using SkiaSharp;
 
+using WercawchallwarnefeWhedurcachay;
+
 using static CPF.Linux.XLib;
 
 XInitThreads();
@@ -13,8 +15,69 @@ var display = XOpenDisplay(IntPtr.Zero);
 var screen = XDefaultScreen(display);
 var rootWindow = XDefaultRootWindow(display);
 
+
+[DllImport("libXext.so.6", SetLastError = true)]
+static extern int XShmQueryExtension(IntPtr display);
+
+var status = XShmQueryExtension(display);
+if (status == 0)
+{
+    Console.WriteLine("XShmQueryExtension failed");
+}
+
+/*
+ Status XShmQueryVersion (display, major, minor, pixmaps)
+   Display *display;
+   int *major, *minor;
+   Bool *pixmaps
+ */
+[DllImport("libXext.so.6", SetLastError = true)]
+static extern int XShmQueryVersion(IntPtr display, out int major, out int minor, out bool pixmaps);
+
+status = XShmQueryVersion(display, out var major, out var minor, out var pixmaps);
+Console.WriteLine($"XShmQueryVersion: {status} major={major} minor={minor} pixmaps={pixmaps}");
+
+
+
+
+
+
+
+
+
 XMatchVisualInfo(display, screen, 32, 4, out var info);
 var visual = info.visual;
+
+var xDisplayWidth = XDisplayWidth(display, screen);
+var xDisplayHeight = XDisplayHeight(display, screen);
+
+var width = xDisplayWidth;
+var height = xDisplayHeight;
+
+
+
+/*
+ XImage *XShmCreateImage (display, visual, depth, format, data,
+                    shminfo, width, height)
+   Display *display;
+   Visual *visual;
+   unsigned int depth, width, height;
+   int format;
+   char *data;
+   XShmSegmentInfo *shminfo;
+ */
+[DllImport("libXext.so.6", SetLastError = true)]
+static extern IntPtr XShmCreateImage(IntPtr display, IntPtr visual, uint depth, int format, IntPtr data, ref XShmSegmentInfo shminfo, uint width, uint height);
+
+// /* Create XImage structure and map image memory on it */
+// ximage = XShmCreateImage(display, DefaultVisual(display, 0), DefaultDepth(display, 0), ZPixmap, 0, &shminfo, 100, 100);
+const int ZPixmap = 2;
+var xShmSegmentInfo = new XShmSegmentInfo();
+var shmImage = XShmCreateImage(display, visual, 32, ZPixmap, IntPtr.Zero, ref xShmSegmentInfo, (uint) width, (uint) height);
+
+Console.WriteLine($"XShmCreateImage = {shmImage:X} xShmSegmentInfo={xShmSegmentInfo}");
+
+
 
 var valueMask =
         //SetWindowValuemask.BackPixmap
@@ -38,11 +101,7 @@ var xSetWindowAttributes = new XSetWindowAttributes
     background_pixel = 0,
 };
 
-var xDisplayWidth = XDisplayWidth(display, screen);
-var xDisplayHeight = XDisplayHeight(display, screen);
 
-var width = xDisplayWidth;
-var height = xDisplayHeight;
 
 var handle = XCreateWindow(display, rootWindow, 0, 0, width, height, 5,
     32,
@@ -63,6 +122,49 @@ var gc = XCreateGC(display, handle, 0, 0);
 var mapLength = width * 4 * height;
 Console.WriteLine($"Length = {mapLength}");
 var backendMap = mmap(IntPtr.Zero, new IntPtr(mapLength), 3, 0x22, -1, IntPtr.Zero);
+
+
+
+/*
+ * /* Setting SHM * /
+      shminfo.shmid = shmget(IPC_PRIVATE, 100 * 100 * 4, IPC_CREAT | 0777);
+ */
+
+/*
+ * int shmget(key_t key, size_t size, int shmflg);
+ */
+[DllImport("libc", SetLastError = true)]
+static extern int shmget(int key, IntPtr size, int shmflg);
+
+//    #define IPC_CREAT	01000		/* create key if key does not exist */
+// #define IPC_PRIVATE	((key_t) 0)	/* private key */
+
+const int IPC_CREAT = 01000;
+const int IPC_PRIVATE = 0;
+var shmgetResult = shmget(IPC_PRIVATE, mapLength, IPC_CREAT | 0777);
+Console.WriteLine($"shmgetResult={shmgetResult:X}");
+
+xShmSegmentInfo.shmid = shmgetResult;
+
+// shminfo.shmaddr = ximage->data = (unsigned char *)shmat(shminfo.shmid, 0, 0);
+// void *shmat(int shmid, const void *_Nullable shmaddr, int shmflg);
+unsafe
+{
+    [DllImport("libc", SetLastError = true)]
+    static extern IntPtr shmat(int shmid, IntPtr shmaddr, int shmflg);
+
+    var shmaddr = shmat(shmgetResult, IntPtr.Zero, 0);
+    xShmSegmentInfo.shmaddr = (char*) shmaddr.ToPointer();
+    ((XImage*) shmImage)->data = shmaddr;
+}
+
+// XShmAttach(display, &shminfo);
+[DllImport("libXext.so.6", SetLastError = true)]
+static extern void XShmAttach(IntPtr display, ref XShmSegmentInfo shminfo);
+
+XShmAttach(display, ref xShmSegmentInfo);
+
+
 
 var skImageInfo = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
 var rowBytes = width * 4;
@@ -157,11 +259,38 @@ while (true)
 
     if (@event.type == XEventName.Expose)
     {
-        skCanvas.Clear(new SKColor((uint) Random.Shared.Next()));
-        skCanvas.Flush();
+        //skCanvas.Clear(new SKColor((uint) Random.Shared.Next()));
+        //skCanvas.Flush();
 
         var stopwatch = Stopwatch.StartNew();
-        BlitSurface(skSurface);
+        //BlitSurface(skSurface);
+
+        /* Put XImage into X Server to display */
+        unsafe
+        {
+            /*
+            Bool XShmPutImage(
+                   Display*		/* dpy * /,
+                   Drawable		/* d * /,
+                   GC			/* gc * /,
+                   XImage*		/* image * /,
+                   int			/* src_x * /,
+                   int			/* src_y * /,
+                   int			/* dst_x * /,
+                   int			/* dst_y * /,
+                   unsigned int	/* src_width * /,
+                   unsigned int	/* src_height * /,
+                   Bool		/* send_event * /
+               );
+
+             */
+            [DllImport("libXext.so.6", SetLastError = true)]
+            static extern int XShmPutImage(IntPtr display, IntPtr drawable, IntPtr gc, XImage* image, int src_x, int src_y, int dst_x, int dst_y, uint src_width, uint src_height, bool send_event);
+
+            XShmPutImage(display, handle, gc, (XImage*) shmImage, 0, 0, 0, 0, (uint) width, (uint) height, false);
+
+            XFlush(display);
+        }
 
         //BlitUnsafe(skBitmap);
         //Blit(skBitmap);
