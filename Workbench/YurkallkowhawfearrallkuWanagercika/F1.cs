bf27b507ef43914c1724c1614421369225d46cad
lindexi <lindexi_gd@163.com>
2024-08-16 09:12:37 +0800

    经过测试无法还原

---
+++ Workbench/YurkallkowhawfearrallkuWanagercika/F1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YurkallkowhawfearrallkuWanagercika;
+
+public class F1<T>
+{
+    public T Value { get; set; }
+}
+
+public class F2<T>
+{
+    public T Value { get; set; }
+}
--- Workbench/YurkallkowhawfearrallkuWanagercika/Program.cs
+++ Workbench/YurkallkowhawfearrallkuWanagercika/Program.cs
@@ -1,17 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Xml.Serialization;
+using YurkallkowhawfearrallkuWanagercika;
 
-var f1 = new F1<F2<int>>();
+var f1 = new F1<F2<int>>()
+{
+    Value = new F2<int>()
+    {
+        Value = 100
+    }
+};
 var xmlSerializer = new XmlSerializer(f1.GetType());
 xmlSerializer.Serialize(Console.Out, f1);
 
-Console.WriteLine("Hello, World!");
 
-class F1<T>
-{
-}
-
-class F2<T>
-{
-}
