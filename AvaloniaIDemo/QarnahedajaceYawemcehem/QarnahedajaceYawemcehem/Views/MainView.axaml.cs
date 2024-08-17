ab63a69d864d7b25146448171541da6bd6c09c75
lindexi <lindexi_gd@163.com>
2024-08-17 16:27:43 +0800

    尝试播放动画但是失败

---
+++ AvaloniaIDemo/QarnahedajaceYawemcehem/QarnahedajaceYawemcehem/Views/MainView.axaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
+
+namespace QarnahedajaceYawemcehem.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+
+
+        Loaded += MainView_Loaded;
+    }
+
+    private async void MainView_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        await Task.Delay(100);
+
+        var content = Content;
+        var textBlock = (TextBlock)content!;
+
+        var animation = new Animation()
+        {
+            Duration = TimeSpan.FromSeconds(10),
+            IterationCount = new IterationCount(5),
+            PlaybackDirection = PlaybackDirection.Alternate,
+            Children =
+            {
+                new KeyFrame()
+                {
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.XProperty, 0),
+                    },
+                    KeyTime = TimeSpan.FromSeconds(0)
+                },
+                new KeyFrame()
+                {
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.XProperty, 500),
+                    },
+                    KeyTime = TimeSpan.FromSeconds(10)
+                }
+            }
+        };
+
+        //textBlock.RenderTransform = new TranslateTransform();
+
+        _ = animation.RunAsync(textBlock);
+    }
+}
---
+++ AvaloniaIDemo/QarnahedajaceYawemcehem/QarnahedajaceYawemcehem/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace QarnahedajaceYawemcehem.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
