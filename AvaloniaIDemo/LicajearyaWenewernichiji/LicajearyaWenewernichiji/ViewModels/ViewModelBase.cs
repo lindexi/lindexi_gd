fa40281ef020c2089689f7dcff8909d8acc27d98
lindexi <lindexi_gd@163.com>
2024-08-21 19:25:46 +0800

    测试播放动画

---
+++ AvaloniaIDemo/LicajearyaWenewernichiji/LicajearyaWenewernichiji/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace LicajearyaWenewernichiji.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ AvaloniaIDemo/LicajearyaWenewernichiji/LicajearyaWenewernichiji/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace LicajearyaWenewernichiji.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
