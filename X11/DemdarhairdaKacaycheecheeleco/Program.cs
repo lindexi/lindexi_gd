843c229a5b590f2c1149050d0f203f53dd48f6b6
lindexi <lindexi_gd@163.com>
2024-08-21 14:23:29 +0800

    记录本质的错误原因用于记录到博客的代码

--- X11/DemdarhairdaKacaycheecheeleco/Program.cs
+++ X11/DemdarhairdaKacaycheecheeleco/Program.cs
@@ -99,10 +99,12 @@ unsafe
 
     var stopwatch = new Stopwatch();
 
+    // 一个像素占用4个字节，于是总共的字节数就是 width * 4 * height 的长度
     var mapLength = width * 4 * height;
     //Console.WriteLine($"Length = {mapLength}");
 
-    var xShmProvider = new XShmProvider(new RenderInfo(display, visual, width, height, mapLength, handle, gc));
+    var renderInfo = new RenderInfo(display, visual, width, height, mapLength, handle, gc);
+    var xShmProvider = new XShmProvider(renderInfo);
     while (true)
     {
         var xNextEvent = XNextEvent(display, out var @event);
@@ -163,7 +165,7 @@ class XShmProvider
         return result;
     }
 
-    static unsafe XShmInfo CreateXShmInfo(IntPtr display, nint visual, int width, int height, int mapLength)
+    private static unsafe XShmInfo CreateXShmInfo(IntPtr display, nint visual, int width, int height, int mapLength)
     {
         var status = XShmQueryExtension(display);
         if (status == 0)
@@ -179,6 +181,16 @@ class XShmProvider
         }
 
         const int ZPixmap = 2;
+        // 核心问题就是 XShmSegmentInfo 是结构体，在这里将在栈上分配。后续将使用栈空间的地址传递给 XShmCreateImage 方法，然而在此方法执行之后，将会弹栈，导致 XShmSegmentInfo 的内存地址被覆盖。从而让 XImage 里面记录的 obdata 字段指向错误的地址，导致后续的 XShmPutImage 方法无法正确的使用共享内存，输出如下错误
+        // X Error of failed request:  BadShmSeg (invalid shared segment parameter)
+        //   Major opcode of failed request:  130 (MIT-SHM)
+        //   Minor opcode of failed request:  3 (X_ShmPutImage)
+        //   Segment id in failed request:  0x0
+        //   Serial number of failed request:  17
+        //   Current serial number in output stream:  17
+        // 上述错误的 `Segment id in failed request:  0x0` 就说明了问题，即 XImage 里面记录的 obdata 字段指向了 0x0 的地址。常见的错误就是类似野指针问题或者指针被覆盖的问题
+        // 在本例中，我们将 XShmSegmentInfo 的在栈上分配的内存地址给到 XImage 里面记录的 obdata 字段，方法结束之后，栈空间被覆盖，导致 obdata 字段指向了错误的地址
+        // 为什么刚好是 0x0 的地址呢？其实原因在于后续的 DoDraw 使用 Span<byte> span = stackalloc byte[1024 * 2]; 强行申请更多的栈空间，从而覆盖到了 XShmSegmentInfo 的内存地址。如果非 DoDraw 强行申请且保持默认为 0 的填充，则这里的错误信息 Segment id in failed request 的值会更加迷惑，甚至指向的是一个随机的地址导致 Segmentation fault (core dumped) 段错误或 The RX block to map as RW was not found 或 The RW block to unmap was not found 或 corrupted double-linked list 等
         var xShmSegmentInfo = new XShmSegmentInfo();
         var shmImage = (XImage*)XShmCreateImage(display, visual, 32, ZPixmap, IntPtr.Zero, &xShmSegmentInfo,
             (uint)width, (uint)height);
