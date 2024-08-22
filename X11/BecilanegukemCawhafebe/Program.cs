d08edeade74fff8d39c09d2d47e953c180cca503
lindexi <lindexi_gd@163.com>
2024-08-22 17:58:22 +0800

    尝试输出触摸过程的渲染耗时

--- X11/BecilanegukemCawhafebe/Program.cs
+++ X11/BecilanegukemCawhafebe/Program.cs
@@ -268,7 +268,7 @@ unsafe
 
             stopwatch.Stop();
 
-            Console.WriteLine($"XShmCompletionEvent: type={xShmCompletionEvent->type} serial={xShmCompletionEvent->serial} {xShmCompletionEvent->send_event} {xShmCompletionEvent->display} {xShmCompletionEvent->drawable} ShmReqCode={xShmCompletionEvent->major_code} X_ShmPutImage={xShmCompletionEvent->minor_code} shmseg={xShmCompletionEvent->shmseg} {xShmCompletionEvent->offset}");
+            //Console.WriteLine($"XShmCompletionEvent: type={xShmCompletionEvent->type} serial={xShmCompletionEvent->serial} {xShmCompletionEvent->send_event} {xShmCompletionEvent->display} {xShmCompletionEvent->drawable} ShmReqCode={xShmCompletionEvent->major_code} X_ShmPutImage={xShmCompletionEvent->minor_code} shmseg={xShmCompletionEvent->shmseg} {xShmCompletionEvent->offset}");
             Console.WriteLine($"消费耗时: {stopwatch.ElapsedMilliseconds}");
 
             if (isRequestRender)
@@ -285,5 +285,7 @@ unsafe
 
         XShmPutImage(display, handle, gc, (XImage*) shmImage, 0, 0, 0, 0, (uint) width, (uint) height, true);
         XFlush(display);
+
+        stopwatch.Restart();
     }
 }
