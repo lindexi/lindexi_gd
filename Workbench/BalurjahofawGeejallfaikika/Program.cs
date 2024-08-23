f4175c4b86c923e0a8963a949fb48da3abcaece3
lindexi <lindexi_gd@163.com>
2024-08-23 20:18:59 +0800

    加上服务端测试，确实没有收到

---
+++ Workbench/BalurjahofawGeejallfaikika/Program.cs
@@ -0,0 +1,31 @@
+using System.Text.Json.Serialization;
+
+using Microsoft.AspNetCore.Mvc;
+
+Task.Run(async () =>
+{
+    var foo = new Foo();
+    var jsonContent = JsonContent.Create(foo);
+
+    var httpClient = new HttpClient();
+    await httpClient.PostAsync("http://localhost:5255", jsonContent);
+
+
+});
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+var app = builder.Build();
+
+app.MapPost("/", async context =>
+{
+    await Task.CompletedTask;
+    var headers = context.Request.Headers;
+});
+
+app.Run();
+
+class Foo
+{
+    public int Value { set; get; }
+}
