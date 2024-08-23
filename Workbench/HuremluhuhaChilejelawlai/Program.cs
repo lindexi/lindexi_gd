4be17afb4c658d64cba481fd953100932cdec93f
lindexi <lindexi_gd@163.com>
2024-08-23 21:14:21 +0800

    依然无法创建

--- Workbench/HuremluhuhaChilejelawlai/Program.cs
+++ Workbench/HuremluhuhaChilejelawlai/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Windows.ApplicationModel.Core;
+using Windows.Graphics.DirectX;
 using Windows.Graphics.Display;
 using Windows.UI.Core;
 using Microsoft.Graphics.Canvas;
@@ -77,7 +78,9 @@ public class App : Application, IFrameworkViewSource, IFrameworkView
 
             var coreWindow = window.CoreWindow;
             var sharedDevice = CanvasDevice.GetSharedDevice();
-            var swapChain = CanvasSwapChain.CreateForCoreWindow(sharedDevice, coreWindow, 96);
+
+            var canvasSwapChain = new CanvasSwapChain(sharedDevice,100,100,96);
+            var swapChain = CanvasSwapChain.CreateForCoreWindow(sharedDevice, coreWindow, 96,100,96,DirectXPixelFormat.R8G8B8A8Int,2);
         };
         
         window.Activate();
