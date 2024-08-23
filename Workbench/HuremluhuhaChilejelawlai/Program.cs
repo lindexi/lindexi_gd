6870854021c6be89888c073434373de9de1e6f19
lindexi <lindexi_gd@163.com>
2024-08-23 20:57:55 +0800

    错误信息是参数错误

--- Workbench/HuremluhuhaChilejelawlai/Program.cs
+++ Workbench/HuremluhuhaChilejelawlai/Program.cs
@@ -1,66 +1,55 @@
-﻿using Windows.Graphics.Display;
+﻿using Windows.ApplicationModel.Core;
+using Windows.Graphics.Display;
+using Windows.UI.Core;
 using Microsoft.Graphics.Canvas;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace FinayfuweewawWakibawlu;
 
-public class App : Application
+public class App : IFrameworkViewSource, IFrameworkView
 {
-    public event EventHandler<LaunchActivatedEventArgs>? Launched;
-
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    public IFrameworkView CreateView()
     {
-        Launched?.Invoke(this, args);
+        return this;
+    }
+
+    private CoreApplicationView? _applicationView;
+    private CoreWindow? _coreWindow;
+
+    public void Initialize(CoreApplicationView applicationView)
+    {
+        _applicationView = applicationView;
+    }
+
+    public void SetWindow(CoreWindow window)
+    {
+        _coreWindow  = window;
+    }
+
+    public void Load(string entryPoint)
+    {
+    }
+
+    public void Run()
+    {
+        var swapChain = CanvasSwapChain.CreateForCoreWindow(
+            resourceCreator: CanvasDevice.GetSharedDevice(),
+            coreWindow: _coreWindow,
+            dpi: DisplayInformation.GetForCurrentView().LogicalDpi);
+    }
+
+    public void Uninitialize()
+    {
+      
     }
 }
 
 internal class Program
 {
-    [STAThread]
     unsafe static void Main(string[] args)
     {
         global::WinRT.ComWrappersSupport.InitializeComWrappers();
-        global::Microsoft.UI.Xaml.Application.Start((p) =>
-        {
-            var context = new global::Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(global::Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
-            // 设置线程同步上下文
-            global::System.Threading.SynchronizationContext.SetSynchronizationContext(context);
-            var app = new App();
-            app.Launched += (sender, e) =>
-            {
-                var window = new Window()
-                {
-                    Title = "控制台创建应用"
-                };
-                window.Content = new Grid()
-                {
-                    Children =
-                    {
-                        new TextBlock()
-                        {
-                            Text = "控制台应用",
-                            HorizontalAlignment = HorizontalAlignment.Center,
-                            VerticalAlignment = VerticalAlignment.Center
-                        }
-                    }
-                };
-
-                var sharedDevice = CanvasDevice.GetSharedDevice();
-                var coreWindow = window.CoreWindow;
-
-                window.VisibilityChanged += (o, eventArgs) =>
-                {
-                    var logicalDpi = DisplayInformation.GetForCurrentView().LogicalDpi;
-
-                };
-                
-                window.Activate();
-
-
-                var swapChain = CanvasSwapChain.CreateForCoreWindow(sharedDevice, coreWindow, 96);
-
-            };
-        });
+        CoreApplication.Run(new App());
     }
 }
