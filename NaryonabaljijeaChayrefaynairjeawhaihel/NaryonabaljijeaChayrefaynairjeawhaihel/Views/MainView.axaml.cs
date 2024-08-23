5be4e228413ccdda82732a60a1de615e7725491c
lindexi <lindexi_gd@163.com>
2024-08-23 17:47:13 +0800

    测试输入让渲染停止

---
+++ NaryonabaljijeaChayrefaynairjeawhaihel/NaryonabaljijeaChayrefaynairjeawhaihel/Views/MainView.axaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace NaryonabaljijeaChayrefaynairjeawhaihel.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+
+        PointerMoved += MainView_PointerMoved;
+    }
+
+    private void MainView_PointerMoved(object? sender, Avalonia.Input.PointerEventArgs e)
+    {
+        Thread.Sleep(100);
+        var point = e.GetCurrentPoint(this);
+        var translateTransform = (TranslateTransform) Rectangle.RenderTransform!;
+        translateTransform.X = point.Position.X;
+        translateTransform.Y = point.Position.Y;
+    }
+}
