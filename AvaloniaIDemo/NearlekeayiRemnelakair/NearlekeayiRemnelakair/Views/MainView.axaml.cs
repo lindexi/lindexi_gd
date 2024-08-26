d1cf0d1ef926cdd5a03e5b4695ac43a8e5c85aab
lindexi <lindexi_gd@163.com>
2024-08-26 10:52:56 +0800

    尝试定制渲染逻辑

--- AvaloniaIDemo/NearlekeayiRemnelakair/NearlekeayiRemnelakair/Views/MainView.axaml.cs
+++ AvaloniaIDemo/NearlekeayiRemnelakair/NearlekeayiRemnelakair/Views/MainView.axaml.cs
@@ -1,4 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
 
 namespace NearlekeayiRemnelakair.Views;
 
@@ -8,4 +13,44 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
     }
+}
+
+class Foo : Control
+{
+    public override void Render(DrawingContext context)
+    {
+        context.Custom(new C());
+        base.Render(context);
+    }
+}
+
+class C : ICustomDrawOperation
+{
+    public void Dispose()
+    {
+        
+    }
+
+    public bool Equals(ICustomDrawOperation? other)
+    {
+        return ReferenceEquals(this, other);
+    }
+
+    public bool HitTest(Point p)
+    {
+        return false;
+    }
+
+    public void Render(ImmediateDrawingContext context)
+    {
+        var leaseFeature = context.TryGetFeature<ISkiaSharpApiLeaseFeature>();
+        if (leaseFeature != null)
+        {
+            var skiaSharpApiLease = leaseFeature.Lease();
+            var skSurface = skiaSharpApiLease.SkSurface;
+            var canvas = skiaSharpApiLease.SkCanvas;
+        }
+    }
+
+    public Rect Bounds { get; set; }
 }
