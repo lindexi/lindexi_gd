55c144a9f4f134482dd89da8d94e618c1d7f72d4
lindexi <lindexi_gd@163.com>
2024-08-26 10:44:12 +0800

    创建空白项目

---
+++ AvaloniaIDemo/NearlekeayiRemnelakair/NearlekeayiRemnelakair/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace NearlekeayiRemnelakair.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
