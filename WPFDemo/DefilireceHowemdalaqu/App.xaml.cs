c0d252379aae95a6d1e767207048f3e2c691d834
lindexi <lindexi_gd@163.com>
2024-08-28 19:35:52 +0800

    实际测试鼠标没有进入 Pointer 消息

---
+++ WPFDemo/DefilireceHowemdalaqu/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DefilireceHowemdalaqu;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        AppContext.SetSwitch("Switch.System.Windows.Input.Stylus.EnablePointerSupport", true);
+    }
+}
+
---
+++ WPFDemo/DefilireceHowemdalaqu/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DefilireceHowemdalaqu;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        SourceInitialized += OnSourceInitialized;
+    }
+
+    private void OnSourceInitialized(object sender, EventArgs e)
+    {
+        var windowInteropHelper = new WindowInteropHelper(this);
+        var hwnd = windowInteropHelper.Handle;
+
+        HwndSource source = HwndSource.FromHwnd(hwnd);
+        source.AddHook(Hook);
+    }
+
+    private IntPtr Hook(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+    {
+        const int WM_POINTERDOWN = 0x0246;
+        const int WM_POINTERUPDATE = 0x0245;
+        const int WM_POINTERUP = 0x0247;
+
+        if (msg is WM_POINTERDOWN or WM_POINTERUPDATE or WM_POINTERUP)
+        {
+            var pointerId = (uint)(ToInt32(wparam) & 0xFFFF);
+            Console.WriteLine(pointerId);
+        }
+
+
+        return IntPtr.Zero;
+    }
+
+    private static int ToInt32(IntPtr ptr)
+    {
+        if (IntPtr.Size == 4)
+            return ptr.ToInt32();
+
+        return (int) (ptr.ToInt64() & 0xffffffff);
+    }
+}
