cb32d2db553033aacaeca30f61eb4867cf77dd31
lindexi <lindexi_gd@163.com>
2024-08-28 17:30:27 +0800

    测试界面性能

--- AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Inking/AvaSkiaInkCanvas.cs
+++ AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Inking/AvaSkiaInkCanvas.cs
@@ -11,6 +11,7 @@ namespace NarjejerechowainoBuwurjofear.Views;
 class AvaSkiaInkCanvas : Control
 {
     private int _count;
+    private List<Rect> _list = [];
 
     public override void Render(DrawingContext context)
     {
@@ -20,15 +21,27 @@ class AvaSkiaInkCanvas : Control
         _count++;
         n = Math.Sin(Math.Pow(Math.E * _count, Math.PI));
         var y = Math.Abs(n) * Bounds.Height;
-        context.Custom(new InkCanvasCustomDrawOperation(new Rect(x, y, 10, 10)));
+
+        _list.Add(new Rect(x, y, 10, 10));
+
+        context.Custom(new InkCanvasCustomDrawOperation(_list));
     }
 
     class InkCanvasCustomDrawOperation : ICustomDrawOperation
     {
-        public InkCanvasCustomDrawOperation(Rect bounds)
+        public InkCanvasCustomDrawOperation(List<Rect> list)
         {
+            Rect bounds = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                bounds = bounds.Union(list[i]);
+            }
             Bounds = bounds;
+
+            _list = list;
         }
+
+        private List<Rect> _list;
 
         public void Dispose()
         {
@@ -60,10 +73,16 @@ class AvaSkiaInkCanvas : Control
             skPaint.Color = SKColors.Red;
             skPaint.Style = SKPaintStyle.Fill;
 
-            var x = (float) Bounds.X;
-            var y = (float) Bounds.Y;
+            for (var i = 0; i < _list.Count; i++)
+            {
+                var bounds = _list[i];
+                var x = (float)bounds.X;
+                var y = (float)bounds.Y;
 
-            canvas.DrawRect(x, y, 10, 10, skPaint);
+                skPaint.Color = new SKColor((uint)(Math.Sin(Math.Pow(Math.E * i, Math.PI)) * int.MaxValue));
+
+                canvas.DrawRect(x, y, 10, 10, skPaint);
+            }
         }
 
         public Rect Bounds { get; }
--- AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Views/MainView.axaml.cs
+++ AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Views/MainView.axaml.cs
@@ -10,12 +10,6 @@ public partial class MainView : UserControl
 {
     public MainView()
     {
-        for (int x = 0; x < 100; x++)
-        {
-            var n = Math.Pow(Math.E * x, Math.PI) * Math.Sin(Math.E * Math.PI * x) - Math.Pow(Math.E,Math.PI*x) * Math.Sin(Math.E * Math.PI * x);
-            n = Math.Sin(Math.Pow(Math.E * x, Math.PI));
-        }
-
         InitializeComponent();
 
         Loaded += MainView_Loaded;
