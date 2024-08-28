6c9f01714ba01ee5d60d8da020d402a1f15c00ae
lindexi <lindexi_gd@163.com>
2024-08-28 09:48:46 +0800

    测试锯齿效果

---
+++ AvaloniaIDemo/QalayjideaChaburfokawha/QalayjideaChaburfokawha.Desktop/Properties/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Avalonia;
+global using Avalonia.Controls;
+global using Avalonia.Media;
+global using Avalonia.Controls.Shapes;
+
+global using AppBuilder = Avalonia.AppBuilder;
---
+++ AvaloniaIDemo/QalayjideaChaburfokawha/QalayjideaChaburfokawha/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace QalayjideaChaburfokawha.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
