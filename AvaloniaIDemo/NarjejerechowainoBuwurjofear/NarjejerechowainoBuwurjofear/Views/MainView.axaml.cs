dd7ebc37391cc1359cb4faedbe4365d2b40b352a
lindexi <lindexi_gd@163.com>
2024-08-28 16:44:51 +0800

    创建空白项目

---
+++ AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace NarjejerechowainoBuwurjofear.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
