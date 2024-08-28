e7c8ecd325422cb5bff4ecf50a341f426dfcc026
lindexi <lindexi_gd@163.com>
2024-08-28 19:58:17 +0800

    似乎就是不平滑

--- WPFDemo/DefilireceHowemdalaqu/MainWindow.xaml.cs
+++ WPFDemo/DefilireceHowemdalaqu/MainWindow.xaml.cs
@@ -5,12 +5,16 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Input.Pointer;
 
 namespace DefilireceHowemdalaqu;
 
@@ -26,12 +30,29 @@ public partial class MainWindow : Window
         SourceInitialized += OnSourceInitialized;
     }
 
+    private Dictionary<uint, StrokeVisual> StrokeVisualList { get; } = new Dictionary<uint, StrokeVisual>();
+
+    private StrokeVisual GetStrokeVisual(uint id)
+    {
+        if (StrokeVisualList.TryGetValue(id, out var visual))
+        {
+            return visual;
+        }
+
+        var strokeVisual = new StrokeVisual();
+        StrokeVisualList[id] = strokeVisual;
+        var visualCanvas = new VisualCanvas(strokeVisual);
+        RootGrid.Children.Add(visualCanvas);
+
+        return strokeVisual;
+    }
+
     private void OnSourceInitialized(object sender, EventArgs e)
     {
         var windowInteropHelper = new WindowInteropHelper(this);
         var hwnd = windowInteropHelper.Handle;
 
-        HwndSource source = HwndSource.FromHwnd(hwnd);
+        HwndSource source = HwndSource.FromHwnd(hwnd)!;
         source.AddHook(Hook);
     }
 
@@ -44,38 +65,106 @@ public partial class MainWindow : Window
         if (msg is WM_POINTERDOWN or WM_POINTERUPDATE or WM_POINTERUP)
         {
             var pointerId = (uint)(ToInt32(wparam) & 0xFFFF);
-        }
+            PInvoke.GetPointerTouchInfo(pointerId, out var info);
+            POINTER_INFO pointerInfo = info.pointerInfo;
 
+            Debug.WriteLine($"PixelLocation={pointerInfo.ptPixelLocation.X},{pointerInfo.ptPixelLocation.Y}; Raw={pointerInfo.ptPixelLocationRaw.X},{pointerInfo.ptPixelLocationRaw.Y} Same={pointerInfo.ptPixelLocation== pointerInfo.ptPixelLocationRaw}");
+
+            var point = pointerInfo.ptPixelLocation;
+            PInvoke.ScreenToClient(new HWND(hwnd), ref point);
+
+            if (msg == WM_POINTERUPDATE)
+            {
+                var strokeVisual = GetStrokeVisual(pointerId);
+                strokeVisual.Add(new StylusPoint(point.X, point.Y));
+                strokeVisual.Redraw();
+            }
+            else if (msg == WM_POINTERUP)
+            {
+                StrokeVisualList.Remove(pointerId);
+            }
+        }
 
         return IntPtr.Zero;
     }
 
-    private static int ToInt32(IntPtr ptr)
-    {
-        if (IntPtr.Size == 4)
-            return ptr.ToInt32();
+    private static int ToInt32(IntPtr ptr) => IntPtr.Size == 4 ? ptr.ToInt32() : (int)(ptr.ToInt64() & 0xffffffff);
 
-        return (int) (ptr.ToInt64() & 0xffffffff);
+    /// <summary>
+    ///     用于显示笔迹的类
+    /// </summary>
+    public class StrokeVisual : DrawingVisual
+    {
+        /// <summary>
+        ///     创建显示笔迹的类
+        /// </summary>
+        public StrokeVisual() : this(new DrawingAttributes()
+        {
+            Color = Colors.Red,
+            FitToCurve = true,
+            Width = 5
+        })
+        {
+        }
+
+        /// <summary>
+        ///     创建显示笔迹的类
+        /// </summary>
+        /// <param name="drawingAttributes"></param>
+        public StrokeVisual(DrawingAttributes drawingAttributes)
+        {
+            _drawingAttributes = drawingAttributes;
+        }
+
+        private readonly DrawingAttributes _drawingAttributes;
+
+        /// <summary>
+        ///     设置或获取显示的笔迹
+        /// </summary>
+        public Stroke Stroke { set; get; }
+
+        /// <summary>
+        ///     在笔迹中添加点
+        /// </summary>
+        /// <param name="point"></param>
+        public void Add(StylusPoint point)
+        {
+            if (Stroke == null)
+            {
+                var collection = new StylusPointCollection { point };
+                Stroke = new Stroke(collection) { DrawingAttributes = _drawingAttributes };
+            }
+            else
+            {
+                Stroke.StylusPoints.Add(point);
+            }
+        }
+
+        /// <summary>
+        ///     重新画出笔迹
+        /// </summary>
+        public void Redraw()
+        {
+            using var dc = RenderOpen();
+            Stroke.Draw(dc);
+        }
     }
 
-    [DllImport("user32.dll", SetLastError = true)]
-    static extern bool GetPointerTouchInfo(uint pointerId, out POINTER_TOUCH_INFO touchInfo);
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct POINTER_TOUCH_INFO
+    public class VisualCanvas : FrameworkElement
     {
-        public POINTER_INFO pointerInfo;
-        public TouchFlags touchFlags;
-        public TouchMask touchMask;
-        public int rcContactLeft;
-        public int rcContactTop;
-        public int rcContactRight;
-        public int rcContactBottom;
-        public int rcContactRawLeft;
-        public int rcContactRawTop;
-        public int rcContactRawRight;
-        public int rcContactRawBottom;
-        public uint orientation;
-        public uint pressure;
+        protected override Visual GetVisualChild(int index)
+        {
+            return Visual;
+        }
+
+        protected override int VisualChildrenCount => 1;
+
+        public VisualCanvas(DrawingVisual visual)
+        {
+            Visual = visual;
+            AddVisualChild(visual);
+        }
+
+        public DrawingVisual Visual { get; }
     }
 }
