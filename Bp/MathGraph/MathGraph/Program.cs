6cf6a798449c4446aea995c7e5df1df1147707d9
lindexi <lindexi_gd@163.com>
2024-08-30 20:18:56 +0800

    测试序列化是否相同

--- Bp/MathGraph/MathGraph/Program.cs
+++ Bp/MathGraph/MathGraph/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,13 +34,44 @@ internal class Program
 
         var mathGraphSerializer = mathGraph.GetSerializer();
         var json = mathGraphSerializer.Serialize();
-        Deserialize(json);
+        Equals(mathGraph, Deserialize(json));
     }
 
-    private static void Deserialize(string json)
+    private static MathGraph<int> Deserialize(string json)
     {
         var mathGraph = new MathGraph<int>();
         var mathGraphSerializer = mathGraph.GetSerializer();
         mathGraphSerializer.Deserialize(json);
+        return mathGraph;
+    }
+
+    private static void Equals<T>(MathGraph<T> a, MathGraph<T> b)
+    {
+        Debug.Assert(a.ElementList == b.ElementList);
+        for (var i = 0; i < a.ElementList.Count; i++)
+        {
+            var elementA = a.ElementList[i];
+            var elementB = b.ElementList[i];
+
+            ElementEquals(elementA, elementB);
+
+            ElementListEquals(elementA.InElementList, elementB.InElementList);
+            ElementListEquals(elementA.OutElementList, elementB.OutElementList);
+        }
+
+        static void ElementListEquals(IReadOnlyList<MathGraphElement<T>> a, IReadOnlyList<MathGraphElement<T>> b)
+        {
+            Debug.Assert(a.Count == b.Count);
+            for (var i = 0; i < a.Count; i++)
+            {
+                ElementEquals(a[i], b[i]);
+            }
+        }
+
+        static void ElementEquals(MathGraphElement<T> a, MathGraphElement<T> b)
+        {
+            Debug.Assert(EqualityComparer<T>.Default.Equals(a.Value, b.Value));
+            Debug.Assert(a.Id == b.Id);
+        }
     }
 }
