cfb981280008c0b0772dcaa23889962f8eac4dd1
lindexi <lindexi_gd@163.com>
2024-08-30 19:56:03 +0800

    加上创建的代码

--- Bp/MathGraph/MathGraph/Program.cs
+++ Bp/MathGraph/MathGraph/Program.cs
@@ -9,6 +9,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        var mathGraph = new MathGraph<int>();
+
+        List<MathGraphElement<int>> elementList = [];
+
+        for (int i = 0; i < 10; i++)
+        {
+            var mathGraphElement = mathGraph.CreateAndAddElement(i);
+            elementList.Add(mathGraphElement);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            var mathGraphElement = elementList[i];
+            for (int j = 0; j < 10; j++)
+            {
+                mathGraphElement.AddInElement(elementList[i]);
+            }
+        }
+
 
     }
 }
