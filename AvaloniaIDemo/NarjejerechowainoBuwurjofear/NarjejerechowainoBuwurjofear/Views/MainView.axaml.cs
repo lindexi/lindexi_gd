b67fe072a6fb1592c68ad11d6fa2ef7831f33fa9
lindexi <lindexi_gd@163.com>
2024-08-30 10:28:45 +0800

    测试原生画线性能

--- AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Views/MainView.axaml.cs
+++ AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Views/MainView.axaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Input;
+using Avalonia.Media;
 
 using NarjejerechowainoBuwurjofear.Inking;
 
@@ -24,19 +26,40 @@ public partial class MainView : UserControl
         RootGrid.PointerReleased += RootGrid_PointerReleased;
     }
 
+    private Polyline? _polyline;
+
     private void RootGrid_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
     {
         AvaSkiaInkCanvas.Down(ToInkingInputArgs(e));
+
+        _polyline = new Polyline
+        {
+            Stroke = Brushes.Black,
+            StrokeThickness = 2
+        };
+        _polyline.IsHitTestVisible = false;
+        RootGrid.Children.Add(_polyline);
     }
 
     private void RootGrid_PointerMoved(object? sender, PointerEventArgs e)
     {
         AvaSkiaInkCanvas.Move(ToInkingInputArgs(e));
+
+        if (_polyline != null)
+        {
+            var currentPoint = e.GetCurrentPoint(RootGrid);
+            _polyline.Points.Add(currentPoint.Position);
+        }
     }
 
     private void RootGrid_PointerReleased(object? sender, PointerReleasedEventArgs e)
     {
         AvaSkiaInkCanvas.Up(ToInkingInputArgs(e));
+
+        if (_polyline != null)
+        {
+            RootGrid.Children.Remove(_polyline);
+        }
     }
 
     private InkingInputArgs ToInkingInputArgs(PointerEventArgs args)
