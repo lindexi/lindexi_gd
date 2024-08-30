563bbc595734cca8c604e566ce0bff363340ed22
lindexi <lindexi_gd@163.com>
2024-08-30 19:52:42 +0800

    实现简单的序列化

--- Bp/MathGraph/MathGraph/MathGraph.cs
+++ Bp/MathGraph/MathGraph/MathGraph.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -32,27 +33,6 @@ public class MathGraph<T>
     //{
     //    ElementList.Remove(element);
     //}
-
-
-
-    public string Serialize()
-    {
-       
-
-        return JsonSerializer.Serialize(ElementList);
-    }
-
-    public void Deserialize(string json)
-    {
-        ElementList.Clear();
-        var list = JsonSerializer.Deserialize<List<MathGraphElement<T>>>(json);
-        if (list != null)
-        {
-            ElementList.AddRange(list);
-        }
-    }
-
-
 }
 
 public class MathGraphSerializer<T>
@@ -64,7 +44,7 @@ public class MathGraphSerializer<T>
 
     private readonly MathGraph<T> _mathGraph;
 
-    public readonly record struct SerializationContext(T Value, string Id, int Index, List<int> InList, List<int> OutList);
+    public readonly record struct SerializationContext(string Value, string? ElementType, string Id, int Index, List<int> InList, List<int> OutList);
 
     public string Serialize()
     {
@@ -80,8 +60,8 @@ public class MathGraphSerializer<T>
 
         foreach (var element in elementList)
         {
-            List<int> inList = new List<int>(element.InElementList.Count);
-            List<int> outList = new List<int>(element.OutElementList.Count);
+            var inList = new List<int>(element.InElementList.Count);
+            var outList = new List<int>(element.OutElementList.Count);
 
             foreach (var inElement in element.InElementList)
             {
@@ -93,7 +73,17 @@ public class MathGraphSerializer<T>
                 outList.Add(dictionary[outElement]);
             }
 
-            contextList.Add(new SerializationContext(element.Value, element.Id, dictionary[element], inList, outList));
+            string value;
+            if (element.Value is ISerializableElement serializableElement)
+            {
+                value = serializableElement.Serialize();
+            }
+            else
+            {
+                value = JsonSerializer.Serialize(element.Value);
+            }
+
+            contextList.Add(new SerializationContext(value, element.GetType().FullName, element.Id, dictionary[element], inList, outList));
         }
 
         return JsonSerializer.Serialize(contextList);
@@ -101,7 +91,38 @@ public class MathGraphSerializer<T>
 
     public void Deserialize(string json)
     {
+        var list = JsonSerializer.Deserialize<List<SerializationContext>>(json);
 
+        if (list is null)
+        {
+            return;
+        }
+
+        var dictionary = new Dictionary< int, MathGraphElement<T>>();
+        foreach (var serializationContext in list)
+        {
+            var elementType = serializationContext.ElementType;
+            var value = JsonSerializer.Deserialize<T>(serializationContext.Value);
+
+            Debug.Assert(value is not null);
+
+            MathGraphElement<T> mathGraphElement = _mathGraph.CreateAndAddElement(value, serializationContext.Id);
+            dictionary[serializationContext.Index] = mathGraphElement;
+        }
+
+        foreach (var serializationContext in list)
+        {
+            var mathGraphElement = dictionary[serializationContext.Index];
+            foreach (var inIndex in serializationContext.InList)
+            {
+                mathGraphElement.AddInElement(dictionary[inIndex]);
+            }
+
+            foreach (var outIndex in serializationContext.OutList)
+            {
+                mathGraphElement.AddOutElement(dictionary[outIndex]);
+            }
+        }
     }
 }
 
@@ -129,11 +150,36 @@ public class MathGraphElement<T>
 
     public T Value { get; }
 
-    public List<MathGraphElement<T>> OutElementList { get; } = [];
+    public IReadOnlyList<MathGraphElement<T>> OutElementList => _outElementList;
 
-    public List<MathGraphElement<T>> InElementList { get; } = [];
+    public IReadOnlyList<MathGraphElement<T>> InElementList => _inElementList;
 
     private static ulong _idCounter = 0;
+
+    private readonly List<MathGraphElement<T>> _outElementList = [];
+    private readonly List<MathGraphElement<T>> _inElementList = [];
+
+    public void AddOutElement(MathGraphElement<T> element)
+    {
+        if (_outElementList.Contains(element))
+        {
+            return;
+        }
+
+        _outElementList.Add(element);
+        element._inElementList.Add(this);
+    }
+
+    public void AddInElement(MathGraphElement<T> element)
+    {
+        if (_inElementList.Contains(element))
+        {
+            return;
+        }
+
+        _inElementList.Add(element);
+        element._outElementList.Add(this);
+    }
 }
 
 public class MathGraphEdge<T>
@@ -143,8 +189,8 @@ public class MathGraphEdge<T>
         From = from;
         To = to;
 
-        from.OutElementList.Add(to);
-        to.InElementList.Add(from);
+        //from.AddOutElement(to);
+        //to.AddInElement(from);
     }
 
     public MathGraphElement<T> From { get; }
