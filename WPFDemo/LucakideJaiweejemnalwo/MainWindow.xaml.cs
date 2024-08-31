99966ea46330b5de6c4ce9131a3a1929aff1f917
lindexi <lindexi_gd@163.com>
2024-08-31 09:34:27 +0800

    测试获取菜单附加的方法

---
+++ WPFDemo/LucakideJaiweejemnalwo/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LucakideJaiweejemnalwo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+    {
+        var menuItem = (MenuItem) sender;
+
+        DependencyObject t = menuItem;
+        while (t != null || t is ContextMenu)
+        {
+            t = LogicalTreeHelper.GetParent(t);
+        }
+
+        var c = t as ContextMenu;
+        var placementTarget = c?.PlacementTarget as Grid;
+    }
+}
