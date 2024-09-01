93edd4d7c18a9b2f4ea5e95f8ec18148923c928a
lindexi <lindexi_gd@163.com>
2024-09-01 16:50:04 +0800

    经过测试似乎无法选取移动端的文件夹

---
+++ WPFDemo/NairnukemlodarjurNidinawjalichaw/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace NairnukemlodarjurNidinawjalichaw;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var openFolderDialog = new OpenFolderDialog();
+        openFolderDialog.ShowDialog(this);
+        var folderName = openFolderDialog.FolderName;
+    }
+}
