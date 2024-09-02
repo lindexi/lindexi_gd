3f14f330ee99e5cd88f7b18f22c14cbdd61a0e05
lindexi <lindexi_gd@163.com>
2024-09-02 21:07:42 +0800

    修改命名空间

--- Bp/MathGraph/MathGraph/Serialization/DefaultDeserializationContext.cs
+++ Bp/MathGraph/MathGraph/Serialization/DefaultDeserializationContext.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-namespace MathGraph;
+namespace MathGraph.Serialization;
 
 class DefaultDeserializationContext : IDeserializationContext
 {
--- Bp/MathGraph/MathGraph/Serialization/ISerializableEdge.cs
+++ Bp/MathGraph/MathGraph/Serialization/ISerializableEdge.cs
@@ -1,4 +1,4 @@
-﻿namespace MathGraph;
+﻿namespace MathGraph.Serialization;
 
 public interface ISerializableEdge
 {
