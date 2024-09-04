3bb7ca435d14ac4b10a6673f68784a10a9b00475
lindexi <lindexi_gd@163.com>
2024-09-04 11:03:29 +0800

    拆分橡皮擦模式

--- AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Inking/AvaSkiaInkCanvas.cs
+++ AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Inking/AvaSkiaInkCanvas.cs
@@ -175,51 +175,6 @@ class DynamicStrokeContext
     public SkiaStroke Stroke { get; }
 }
 
-class AvaSkiaInkCanvasEraserMode
-{
-    public AvaSkiaInkCanvasEraserMode(AvaSkiaInkCanvas inkCanvas)
-    {
-        InkCanvas = inkCanvas;
-    }
-
-    public AvaSkiaInkCanvas InkCanvas { get; }
-    public bool IsErasing { get; private set; }
-    private int MainEraserInputId { set; get; }
-
-    public void EraserDown(InkingInputArgs args)
-    {
-        InkCanvas.EnsureInputConflicts();
-        if (!IsErasing)
-        {
-            MainEraserInputId = args.Id;
-
-            IsErasing = true;
-        }
-        else
-        {
-            // 忽略其他的输入点
-        }
-    }
-
-    public void EraserMove(InkingInputArgs args)
-    {
-        InkCanvas.EnsureInputConflicts();
-        if (IsErasing && args.Id == MainEraserInputId)
-        {
-            // 擦除
-        }
-    }
-
-    public void EraserUp(InkingInputArgs args)
-    {
-        InkCanvas.EnsureInputConflicts();
-        if (IsErasing && args.Id == MainEraserInputId)
-        {
-            IsErasing = false;
-        }
-    }
-}
-
 class AvaSkiaInkCanvas : Control
 {
     public AvaSkiaInkCanvas()
---
+++ AvaloniaIDemo/NarjejerechowainoBuwurjofear/NarjejerechowainoBuwurjofear/Inking/AvaSkiaInkCanvasEraserMode.cs
@@ -0,0 +1,46 @@
+﻿namespace NarjejerechowainoBuwurjofear.Inking;
+
+class AvaSkiaInkCanvasEraserMode
+{
+    public AvaSkiaInkCanvasEraserMode(AvaSkiaInkCanvas inkCanvas)
+    {
+        InkCanvas = inkCanvas;
+    }
+
+    public AvaSkiaInkCanvas InkCanvas { get; }
+    public bool IsErasing { get; private set; }
+    private int MainEraserInputId { set; get; }
+
+    public void EraserDown(InkingInputArgs args)
+    {
+        InkCanvas.EnsureInputConflicts();
+        if (!IsErasing)
+        {
+            MainEraserInputId = args.Id;
+
+            IsErasing = true;
+        }
+        else
+        {
+            // 忽略其他的输入点
+        }
+    }
+
+    public void EraserMove(InkingInputArgs args)
+    {
+        InkCanvas.EnsureInputConflicts();
+        if (IsErasing && args.Id == MainEraserInputId)
+        {
+            // 擦除
+        }
+    }
+
+    public void EraserUp(InkingInputArgs args)
+    {
+        InkCanvas.EnsureInputConflicts();
+        if (IsErasing && args.Id == MainEraserInputId)
+        {
+            IsErasing = false;
+        }
+    }
+}
