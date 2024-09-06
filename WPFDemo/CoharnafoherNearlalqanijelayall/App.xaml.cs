a4a7555d54fede7b7e39bb61f1b51f3d3ada37ec
lindexi <lindexi_gd@163.com>
2024-09-06 11:04:51 +0800

    测试捕获全局

--- WPFDemo/CoharnafoherNearlalqanijelayall/App.xaml.cs
+++ WPFDemo/CoharnafoherNearlalqanijelayall/App.xaml.cs
@@ -12,5 +12,10 @@ public partial class App : Application
     public App()
     {
         AppContext.SetSwitch("Switch.System.Windows.Input.Stylus.EnablePointerSupport", true);
+        DispatcherUnhandledException += App_DispatcherUnhandledException;
+    }
+
+    private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
     }
 }
