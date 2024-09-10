b5c74de6320bfaa47d1bd33653a9cfa61474d18b
lindexi <lindexi_gd@163.com>
2024-09-10 11:13:26 +0800

    结构体转换将会失败

---
+++ Workbench/DohefurweBigurqice/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var list = new List<Foo>();
+
+for (int i = 0; i < 10; i++)
+{
+    list.Add(new Foo());
+}
+
+Foo? foo = list.FirstOrDefault(t=>t.N==-1,null);
+
+Console.WriteLine("Hello, World!");
+
+
+struct Foo
+{
+    public Foo()
+    {
+        _count++;
+        N = _count;
+    }
+    public int N { get; }
+
+    private static int _count = 0;
+}
