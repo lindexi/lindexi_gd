ab1340cfb605c8025160a7119e3978e3137ce281
lindexi <lindexi_gd@163.com>
2024-09-13 21:53:23 +0800

    尝试获取字体

--- DirectX/DWrite/HekeherwhiLewherjaroche/MainWindow.xaml.cs
+++ DirectX/DWrite/HekeherwhiLewherjaroche/MainWindow.xaml.cs
@@ -25,12 +25,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
     }
 
-    private void Button_OnClick(object sender, RoutedEventArgs e)
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
     {
-        IDWriteFactory m_pDWriteFactory = null;
-        IDWriteFactory7 m_pDWriteFactory7 = null;
+        IDWriteFactory dwriteFactory = null;
+        IDWriteFactory7 dwriteFactory7 = null;
 
         Guid CLSID_DWriteFactory = new Guid("B859EE5A-D838-4B5B-A2E8-1ADC7D93DB48");
         Guid CLSID_DWriteFactory7 = new Guid("35D0E0B3-9076-4D2E-A016-A91B568A06B4");
@@ -40,8 +42,53 @@ public partial class MainWindow : Window
         HRESULT hr = DWriteCoreCreateFactory(DWRITE_FACTORY_TYPE.DWRITE_FACTORY_TYPE_SHARED, ref CLSID_DWriteFactory7, out pDWriteFactoryPtr);
         if (hr == HRESULT.S_OK)
         {
+            // m_pDWriteFactory = Marshal.GetObjectForIUnknown(pDWriteFactoryPtr) as IDWriteFactory;  
+            dwriteFactory7 = Marshal.GetObjectForIUnknown(pDWriteFactoryPtr) as IDWriteFactory7;
+            //IDWriteFontCollection pFontCollection;  
+            //hr = m_pDWriteFactory.GetSystemFontCollection(out pFontCollection);  
+            IDWriteFontCollection3 pFontCollection;
+            hr = dwriteFactory7.GetSystemFontCollection7(false, DWRITE_FONT_FAMILY_MODEL.DWRITE_FONT_FAMILY_MODEL_TYPOGRAPHIC, out pFontCollection);
+            if (hr == HRESULT.S_OK)
+            {
+                uint nFamilyCount = pFontCollection.GetFontFamilyCount();
+                for (uint i = 0; i < nFamilyCount; i++)
+                {
+                    IDWriteFontFamily pFontFamily;
+                    hr = pFontCollection.GetFontFamily(i, out pFontFamily);
+                    IDWriteLocalizedStrings pFamilyNames;
+                    pFontFamily.GetFamilyNames(out pFamilyNames);
 
+                    uint nIndex = 0;
+                    bool bExists = false;
+                    StringBuilder sbLocaleName = new StringBuilder(LOCALE_NAME_MAX_LENGTH);
+                    int nDefaultLocaleSuccess = GetUserDefaultLocaleName(sbLocaleName, LOCALE_NAME_MAX_LENGTH);
+                    if (nDefaultLocaleSuccess > 0)
+                    {
+                        hr = pFamilyNames.FindLocaleName(sbLocaleName.ToString(), out nIndex, out bExists);
+                    }
+                    if (hr == HRESULT.S_OK && !bExists)
+                    {
+                        hr = pFamilyNames.FindLocaleName("en-us", out nIndex, out bExists);
+                    }
+                    if (!bExists)
+                        nIndex = 0;
+                    hr = pFamilyNames.GetString(nIndex, sbLocaleName, LOCALE_NAME_MAX_LENGTH);
+                    string sName = sbLocaleName.ToString();
+
+                    System.Diagnostics.Debug.WriteLine("Font : " + sName);
+
+                    Marshal.ReleaseComObject(pFamilyNames);
+                    Marshal.ReleaseComObject(pFontFamily);
+                }
+                Marshal.ReleaseComObject(pFontCollection);
+            }
+            Marshal.ReleaseComObject(dwriteFactory7);
         }
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        
     }
 
     // As I could not get DWriteCore.dll from Microsoft.WindowAppSDK.DWrite package when I tested,  
