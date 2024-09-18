4168382f97d2365a2d6096701e333d3f5cb34571
lindexi <lindexi_gd@163.com>
2024-09-18 14:18:21 +0800

    执行测试逻辑但是测试逻辑本身没有符合预期

--- Workbench/JurqallaljeWheneawhederkalfo/Program.cs
+++ Workbench/JurqallaljeWheneawhederkalfo/Program.cs
@@ -1,15 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Buffers;
 using System.Runtime.CompilerServices;
 
 var n = 10;
-
-Foo($"asd{1+2} n={n}");
+string t = string.Join(',', Enumerable.Range(0, 10000));
+for (int i = 0; i < int.MaxValue; i++)
+{
+    Foo($"asd{1 + 2} n={n} {t}");
+    //ArrayPool<int>.Shared.Rent(1024);
+}
 
 Console.WriteLine("Hello, World!");
 
 
-void Foo(DefaultInterpolatedStringHandler handler)
+void Foo(FooInterpolatedStringHandler handler)
 {
 
+}
+
+[InterpolatedStringHandler]
+ref struct FooInterpolatedStringHandler
+{
+    public FooInterpolatedStringHandler(int literalLength, int formattedCount)
+    {
+    }
+
+    public void AppendLiteral(string s)
+    {
+
+    }
+
+    public void AppendFormatted<T>(T t)
+    {
+
+    }
+
+    public void AppendFormatted<T>(T t, string format) where T : IFormattable
+    {
+
+    }
 }
