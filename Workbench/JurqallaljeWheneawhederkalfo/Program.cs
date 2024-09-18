ab7e0bddecc5f58426bd26e7150582edeb67304f
lindexi <lindexi_gd@163.com>
2024-09-18 11:40:56 +0800

    尝试组装字符串但是似乎被序列化

---
+++ Workbench/JurqallaljeWheneawhederkalfo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+var n = 10;
+
+Foo($"asd{1+2} n={n}");
+
+Console.WriteLine("Hello, World!");
+
+
+void Foo(DefaultInterpolatedStringHandler handler)
+{
+
+}
