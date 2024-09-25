5f584bfd61d12bb0fc41c59b9aba842ffa658ece
lindexi <lindexi_gd@163.com>
2024-09-25 10:55:50 +0800

    测试转换逻辑符合预期

---
+++ Workbench/JayfucicayaWawlaykergeawhojurwhea/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var result = System.Uri.EscapeDataString("max-u/0cf80fafd4204476b95633a3ae356e0d.jpg");
+
+Console.WriteLine("Hello, World!");
