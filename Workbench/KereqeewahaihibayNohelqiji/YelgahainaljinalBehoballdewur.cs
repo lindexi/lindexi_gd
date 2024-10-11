bd14c24a15696cedab4b7d3313072c9a7a729264
lindexi <lindexi_gd@163.com>
2024-10-11 16:04:40 +0800

    创建空白项目

---
+++ Workbench/KereqeewahaihibayNohelqiji/YelgahainaljinalBehoballdewur.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KereqeewahaihibayNohelqiji
+{
+    [Generator(LanguageNames.CSharp)]
+    internal class YelgahainaljinalBehoballdewur : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            // 在这里编写代码
+        }
+    }
+}
