40803373fc1a8918c7187a5d93589da6237ecb7d
lindexi <lindexi_gd@163.com>
2024-10-12 20:31:10 +0800

    对接简单的实现

--- LightTextEditorPlus/Demos/AvaloniaDemo/Views/MainView.axaml.cs
+++ LightTextEditorPlus/Demos/AvaloniaDemo/Views/MainView.axaml.cs
@@ -15,39 +15,5 @@ public partial class MainView : UserControl
 
     private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
-        Foo.InvalidateVisual();
-    }
-}
-
-public class Foo : Control
-{
-    public override void Render(DrawingContext context)
-    {
-        base.Render(context);
-    }
-
-    class C : ICustomDrawOperation
-    {
-        public void Dispose()
-        {
-            
-        }
-
-        public bool Equals(ICustomDrawOperation? other)
-        {
-            return false;
-        }
-
-        public bool HitTest(Point p)
-        {
-            return false;
-        }
-
-        public void Render(ImmediateDrawingContext context)
-        {
-            
-        }
-
-        public Rect Bounds { get; }
     }
 }
---
+++ LightTextEditorPlus/LightTextEditorPlus.Avalonia/TextEditor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Avalonia.Controls;
+using LightTextEditorPlus.Core;
+
+namespace LightTextEditorPlus.Avalonia;
+
+public partial class TextEditor : Control
+{
+    public TextEditor()
+    {
+        SkiaTextEditor = new SkiaTextEditor();
+    }
+
+    public SkiaTextEditor SkiaTextEditor { get; }
+    public TextEditorCore TextEditorCore => SkiaTextEditor.TextEditorCore;
+}
