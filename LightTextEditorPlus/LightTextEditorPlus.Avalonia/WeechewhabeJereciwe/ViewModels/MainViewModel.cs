cdf6266eb6d96df960941e16a5083173d2e2dda9
lindexi <lindexi_gd@163.com>
2024-10-12 20:13:54 +0800

    添加 Avalonia 平台的支持

---
+++ LightTextEditorPlus/LightTextEditorPlus.Avalonia/WeechewhabeJereciwe/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WeechewhabeJereciwe.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ LightTextEditorPlus/LightTextEditorPlus.Avalonia/WeechewhabeJereciwe/Views/MainView.axaml.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Rendering.SceneGraph;
+
+namespace WeechewhabeJereciwe.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Foo.InvalidateVisual();
+    }
+}
+
+public class Foo : Control
+{
+    public override void Render(DrawingContext context)
+    {
+        base.Render(context);
+    }
+
+    class C : ICustomDrawOperation
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public bool Equals(ICustomDrawOperation? other)
+        {
+            return false;
+        }
+
+        public bool HitTest(Point p)
+        {
+            return false;
+        }
+
+        public void Render(ImmediateDrawingContext context)
+        {
+            
+        }
+
+        public Rect Bounds { get; }
+    }
+}
