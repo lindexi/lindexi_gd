7b1b8a5e18f30cdec3e85285f2e5e694a5c64117
lindexi <lindexi_gd@163.com>
2024-10-13 09:43:35 +0800

    添加多平台适配代码

---
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/API_/TextEditor.API.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightTextEditorPlus;
+
+// 此文件将被多个 UI 框架共享，用于约束多个 UI 框架之间使用类似或相同的 API 定义
+
+partial class TextEditor
+{
+}
