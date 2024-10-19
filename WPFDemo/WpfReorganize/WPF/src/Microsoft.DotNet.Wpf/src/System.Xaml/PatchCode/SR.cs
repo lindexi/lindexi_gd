84b3e1e9445fa27c7b5f98d6c18c10d740bd4426
lindexi <lindexi_gd@163.com>
2024-10-19 22:01:55 +0800

    尝试修复构建

--- WPFDemo/WpfReorganize/WPF/src/Microsoft.DotNet.Wpf/src/System.Xaml/PatchCode/SR.cs
+++ WPFDemo/WpfReorganize/WPF/src/Microsoft.DotNet.Wpf/src/System.Xaml/PatchCode/SR.cs
@@ -4,8 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace System;
-
-internal partial class SR: System.Xaml.Resources.Strings
+#if WINDOWS_BASE
+namespace MS.Internal.WindowsBase
+#elif PRESENTATION_CORE
+namespace MS.Internal.PresentationCore
+#elif PBTCOMPILER
+namespace MS.Utility
+#elif AUTOMATION
+namespace MS.Internal.Automation
+#elif REACHFRAMEWORK
+namespace System.Windows.Xps
+#elif PRESENTATIONFRAMEWORK
+namespace System.Windows
+#elif PRESENTATIONUI
+namespace System.Windows.TrustUI
+#elif WINDOWSFORMSINTEGRATION
+namespace System.Windows
+#elif RIBBON_IN_FRAMEWORK
+namespace Microsoft.Windows.Controls
+#else
+namespace System
+#endif
 {
+    internal partial class SR :
+#if WINDOWS_BASE
+         global::WindowsBase.Resources.Strings
+#elif SYSTEM_XAML
+         System.Xaml.Resources.Strings
+#endif
+    {
+    }
 }
