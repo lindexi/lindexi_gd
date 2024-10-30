390f24b89c38a81c2e275333dddaeffb86200ecc
lindexi <lindexi_gd@163.com>
2024-10-30 17:27:49 +0800

    经过测试了解旋转非中心点

--- SkiaSharp/WafairlagawheKeyalljawhake/Program.cs
+++ SkiaSharp/WafairlagawheKeyalljawhake/Program.cs
@@ -19,15 +19,16 @@ var originBitmap = new SKBitmap(new SKImageInfo(width, height, SKColorType.Bgra8
 }
 
 SKRect rect = new SKRect(0, 0, width, height);
-var skMatrix = SKMatrix.CreateRotation(MathF.PI / 2)
-.PreConcat(SKMatrix.CreateTranslation(-width / 2f, -height / 2f))
-.PostConcat(SKMatrix.CreateTranslation(height / 2f, width / 2f));
+var skMatrix = SKMatrix.CreateRotation(MathF.PI / 2, width / 2f, height / 2f);
+//.PreConcat(SKMatrix.CreateTranslation(-width / 2f, -height / 2f))
+//.PostConcat(SKMatrix.CreateTranslation(height / 2f, width / 2f));
 
 rect = skMatrix.MapRect(rect);
 
 using var skBitmap = new SKBitmap(new SKImageInfo((int) rect.Width, (int) rect.Height, SKColorType.Bgra8888));
 using (var skCanvas = new SKCanvas(skBitmap))
 {
+    skCanvas.Clear(SKColors.Blue);
     skCanvas.SetMatrix(skMatrix);
     skCanvas.DrawBitmap(originBitmap, 0, 0);
 }
