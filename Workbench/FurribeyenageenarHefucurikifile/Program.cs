49468556f3a2cf3d67b53e2b0c1d100445c4bb65
lindexi <lindexi_gd@163.com>
2024-11-12 09:04:49 +0800

    添加测试程序获取当前的代码信息

---
+++ Workbench/FurribeyenageenarHefucurikifile/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lindexi.Src.GitCommand;
+
+var git = new Git(new DirectoryInfo("."));
+
+var currentCommit = git.GetCurrentCommit();
+Console.WriteLine($"CurrentCommit={currentCommit}");
+
+Console.WriteLine("Hello, World!");
