26349a24ea4e876b45a0c60776b3b0e3a891ed15
lindexi <lindexi_gd@163.com>
2024-11-15 15:16:05 +0800

    提示找不到方法

--- Workbench/CikufarfeLemkerefegelal/Program.cs
+++ Workbench/CikufarfeLemkerefegelal/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+Console.WriteLine(RuntimeInformation.RuntimeIdentifier);
+
 Console.WriteLine("Hello, World!");
