ece08f3e41bc1909f3119d05cec1e11f06d4db48
lindexi <lindexi_gd@163.com>
2024-11-19 21:13:35 +0800

    找不到解码器

--- Workbench/HegerkalailayiwuCulalajer/Program.cs
+++ Workbench/HegerkalailayiwuCulalajer/Program.cs
@@ -1,8 +1,16 @@
 ﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Processing;
 
 var file = @"C:\lindexi\Image\Image.png";
-using Image image = Image.Load(file);
+using Image image = Image.Load(new DecoderOptions()
+{
+    Configuration = new Configuration()
+    {
+        
+    },
+    SkipMetadata = false,
+}, file);
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
