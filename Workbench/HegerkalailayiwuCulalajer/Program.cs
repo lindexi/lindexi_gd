a0cac6500347822fb04e357f81f180c29af9c958
lindexi <lindexi_gd@163.com>
2024-11-19 21:12:10 +0800

    尝试加载图片失败

---
+++ Workbench/HegerkalailayiwuCulalajer/Program.cs
@@ -0,0 +1,8 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+var file = @"C:\lindexi\Image\Image.png";
+using Image image = Image.Load(file);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
