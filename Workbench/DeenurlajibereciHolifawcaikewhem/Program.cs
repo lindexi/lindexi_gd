388f8bfcc1637f358d87d56f4145608182404a08
lindexi <lindexi_gd@163.com>
2024-11-20 09:04:34 +0800

    尝试新的写法

---
+++ Workbench/DeenurlajibereciHolifawcaikewhem/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Sum([1, 2, 3]));
+
+
+int Sum(Span<int> list)
+{
+    return list switch
+    {
+        [] => 0,
+        [var first, .. var other] => first + Sum(other),
+    };
+}
