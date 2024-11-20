63c9ca8f563ad8e5d4b26bc1a740b10af243abce
lindexi <lindexi_gd@163.com>
2024-11-20 08:55:29 +0800

    尝试修复打开图片

--- Workbench/HegerkalailayiwuCulalajer/Program.cs
+++ Workbench/HegerkalailayiwuCulalajer/Program.cs
@@ -1,13 +1,19 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 
 var file = @"C:\lindexi\Image\Image.png";
-using Image image = Image.Load(new DecoderOptions()
+
+using var fileStream = File.OpenRead(file);
+
+var decode = PngDecoder.Instance.Decode(new PngDecoderOptions()
 {
-    Configuration = Configuration.Default,
-    SkipMetadata = false,
-}, file);
+    PngCrcChunkHandling = PngCrcChunkHandling.IgnoreAll,
+}, fileStream);
+
+var decodeSize = decode.Size;
+var pixelType = decode.PixelType;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
