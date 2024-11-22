61870ce4d8e0f05189834ccc4cf2dc5dac7cceaa
lindexi <lindexi_gd@163.com>
2024-11-22 11:41:18 +0800

    测试读取但读取不到正确的值

---
+++ Workbench/WerjiwaijogoNemcuryecho/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+using System.Text;
+
+var file = @"C:\Windows\System32\ntdll.dll";
+
+using var fileStream = File.OpenRead(file);
+var peHeaders = new PEHeaders(fileStream);
+var peHeader = peHeaders.PEHeader!;
+var debugTableDirectory = peHeader.DebugTableDirectory;
+
+var seek = fileStream.Seek(debugTableDirectory.RelativeVirtualAddress,SeekOrigin.Begin);
+var buffer = new byte[debugTableDirectory.Size];
+var readCount = fileStream.Read(buffer, 0, buffer.Length);
+
+var offset =  4 + Marshal.SizeOf<Guid>() + sizeof(uint);
+var allSize = offset  + 255;
+
+var slice = buffer.AsSpan().Slice(offset);
+
+var unicodeText = Encoding.Unicode.GetString(buffer.AsSpan());
+
+var text = Encoding.UTF32.GetString(buffer.AsSpan());
+
+Console.WriteLine("Hello, World!");
