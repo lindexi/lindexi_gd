656b7ac24443ea4d136b476a22eb3332a113c8e7
lindexi <lindexi_gd@163.com>
2024-11-26 17:13:02 +0800

    重新整理逻辑

--- LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaPlatformFontManager.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaPlatformFontManager.cs
@@ -7,7 +7,9 @@ internal class SkiaPlatformFontManager
     public RenderingFontInfo GetRenderingFontInfo(SkiaTextRunProperty runProperty, char unicodeChar)
     {
         // todo 处理缓存和未找到字体的情况
+        // todo 处理加粗
         var typeface = SKTypeface.FromFamilyName(runProperty.FontName.UserFontName);
+        
         return new RenderingFontInfo(typeface);
     }
 }
