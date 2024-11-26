329c257b31805cb397ecca654354d16d22bb4617
lindexi <lindexi_gd@163.com>
2024-11-26 11:11:48 +0800

    开放更多方法

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/Chars_/CharDataReadOnlyListSpanExtension.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/Chars_/CharDataReadOnlyListSpanExtension.cs
@@ -3,8 +3,16 @@ using System.Text;
 
 namespace LightTextEditorPlus.Core.Document;
 
-static class CharDataReadOnlyListSpanExtension
+/// <summary>
+/// 字符数据只读列表的扩展方法
+/// </summary>
+public static class CharDataReadOnlyListSpanExtension
 {
+    /// <summary>
+    /// 将字符数据列表转换为文本
+    /// </summary>
+    /// <param name="list"></param>
+    /// <returns></returns>
     public static string ToText(this ReadOnlyListSpan<CharData> list)
     {
         var stringBuilder = new StringBuilder();
