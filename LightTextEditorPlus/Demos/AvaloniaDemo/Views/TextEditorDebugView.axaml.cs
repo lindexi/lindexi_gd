d59a4dfbe2a7d3dcbb4143052066d0dde1ea84d5
lindexi <lindexi_gd@163.com>
2024-11-28 15:04:05 +0800

    添加图标

---
+++ LightTextEditorPlus/Demos/AvaloniaDemo/Views/TextEditorDebugView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LightTextEditorPlus.AvaloniaDemo.Views;
+
+public partial class TextEditorDebugView : UserControl
+{
+    public TextEditorDebugView()
+    {
+        InitializeComponent();
+    }
+}
