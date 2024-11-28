504c1bf26ce87ea99e34045cd50dce317169ec3f
lindexi <lindexi_gd@163.com>
2024-11-28 15:11:29 +0800

    添加调试界面

--- LightTextEditorPlus/Demos/AvaloniaDemo/Views/TextEditorDebugView.axaml.cs
+++ LightTextEditorPlus/Demos/AvaloniaDemo/Views/TextEditorDebugView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace LightTextEditorPlus.AvaloniaDemo.Views;
@@ -9,5 +10,15 @@ public partial class TextEditorDebugView : UserControl
     public TextEditorDebugView()
     {
         InitializeComponent();
+
+        TextEditorSettingsControl.TextEditor = TextEditor;
+    }
+
+    private void DebugButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+    }
+
+    private void ShowDocumentBoundsButton_OnClick(object? sender, RoutedEventArgs e)
+    {
     }
 }
