84ec7de49f1b98c53a876cdc60c863d89f9bafc3
lindexi <lindexi_gd@163.com>
2024-11-28 20:24:31 +0800

    完成获取加粗字体

--- LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaPlatformFontManager.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaPlatformFontManager.cs
@@ -7,8 +7,7 @@ internal class SkiaPlatformFontManager
     public RenderingFontInfo GetRenderingFontInfo(SkiaTextRunProperty runProperty, char unicodeChar)
     {
         // todo 处理缓存和未找到字体的情况
-        // todo 处理加粗
-        var typeface = SKTypeface.FromFamilyName(runProperty.FontName.UserFontName);
+        var typeface = SKTypeface.FromFamilyName(runProperty.FontName.UserFontName, runProperty.FontWeight, runProperty.Stretch, runProperty.FontStyle);
         
         return new RenderingFontInfo(typeface);
     }
