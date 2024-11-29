3a78ebfa8814136307ee9d0f68ec06c4efcb76a0
lindexi <lindexi_gd@163.com>
2024-11-29 11:26:42 +0800

    删除不使用的内容和尝试支持透明度失败

--- LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaTextRunProperty.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaTextRunProperty.cs
@@ -35,31 +35,6 @@ public record SkiaTextRunProperty : LayoutOnlyRunProperty
         return ResourceManager.GetRenderingRunPropertyInfo(this, unicodeChar);
     }
 
-    public SKTypeface GetRenderSKTypeface(char unicodeChar = '1')
-    {
-        return GetRenderingRunPropertyInfo(unicodeChar).Typeface;
-    }
-
-    public SKFont GetRenderSKFont(char unicodeChar = '1')
-    {
-        
-        SKFont renderSkFont = new SKFont(GetRenderSKTypeface(unicodeChar), (float)FontSize);
-        // From Avalonia
-        // Ideally the requested edging should be passed to the glyph run.
-        // Currently the edging is computed dynamically inside the drawing context, so we can't know it in advance.
-        // But the bounds depends on the edging: for now, always use SubpixelAntialias so we have consistent values.
-        // The resulting bounds may be shifted by 1px on some fonts:
-        // "F" text with Inter size 14 has a 0px left bound with SubpixelAntialias but 1px with Antialias.
-
-        var edging = SKFontEdging.SubpixelAntialias;
-
-        renderSkFont.Hinting = SKFontHinting.Full;
-        renderSkFont.Edging = edging;
-        renderSkFont.Subpixel = edging != SKFontEdging.Alias;
-
-        return renderSkFont;
-    }
-
     public double Opacity { get; init; } = 1;
     public SKColor Foreground { get; init; } = SKColors.Black;
     public SKColor Background { get; init; } = SKColors.Transparent;
