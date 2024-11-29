6afe2cdf1293ec7d9cf0d286ad33fb4539789f83
lindexi <lindexi_gd@163.com>
2024-11-29 16:39:49 +0800

    优化调试界面

--- LightTextEditorPlus/Demos/AvaloniaDemo/Views/TextEditorDebugView.axaml.cs
+++ LightTextEditorPlus/Demos/AvaloniaDemo/Views/TextEditorDebugView.axaml.cs
@@ -3,6 +3,9 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
+using LightTextEditorPlus.AvaloniaDemo.Business;
+using LightTextEditorPlus.AvaloniaDemo.Business.RichTextCases;
+
 namespace LightTextEditorPlus.AvaloniaDemo.Views;
 
 public partial class TextEditorDebugView : UserControl
@@ -14,8 +17,11 @@ public partial class TextEditorDebugView : UserControl
         TextEditorSettingsControl.TextEditor = TextEditor;
     }
 
+    private readonly RichTextCaseProvider _richTextCaseProvider = new RichTextCaseProvider();
+
     private void DebugButton_OnClick(object? sender, RoutedEventArgs e)
     {
+        _richTextCaseProvider.Debug(TextEditor);
     }
 
     private void ShowDocumentBoundsButton_OnClick(object? sender, RoutedEventArgs e)
