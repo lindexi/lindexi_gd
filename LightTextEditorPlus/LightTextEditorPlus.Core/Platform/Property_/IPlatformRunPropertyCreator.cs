f0ade03c9d6153372838fd21e8ae4913174c1b1e
lindexi <lindexi_gd@163.com>
2024-11-29 14:48:52 +0800

    尝试处理平台不能识别的字符属性

--- LightTextEditorPlus/LightTextEditorPlus.Core/Platform/Property_/IPlatformRunPropertyCreator.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Platform/Property_/IPlatformRunPropertyCreator.cs
@@ -1,4 +1,5 @@
 ﻿using LightTextEditorPlus.Core.Document;
+
 using System;
 
 namespace LightTextEditorPlus.Core.Platform;
@@ -17,10 +18,14 @@ public interface IPlatformRunPropertyCreator
 
     /// <summary>
     /// 获取字符属性。需要处理字符的字体降级
+    /// 这个方法包含两个功能：
+    /// 1. 如果传入的平台属性不属于当前的平台属性，则自动进行处理或记录或报告错误
+    /// 2. 如果给定字符不能满足当前的平台属性，则自动处理字符的字体降级、或记录或报告错误
     /// </summary>
     /// <param name="charObject"></param>
     /// <param name="baseRunProperty"></param>
     /// <returns></returns>
+    /// todo 改名
     IReadOnlyRunProperty GetRunProperty(ICharObject charObject, IReadOnlyRunProperty baseRunProperty);
 }
 
@@ -50,6 +55,11 @@ public abstract class PlatformRunPropertyCreatorBase<T> : IPlatformRunPropertyCr
     /// <inheritdoc />
     public virtual IReadOnlyRunProperty GetRunProperty(ICharObject charObject, IReadOnlyRunProperty baseRunProperty)
     {
+        if (baseRunProperty is not T)
+        {
+            throw new NotSupportedException($"传入了非当前平台所能支持的字符属性。当前平台属性类型： {typeof(T).FullName}；传入的字符属性类型：{baseRunProperty?.GetType().FullName ?? "<null>"}");
+        }
+
         return baseRunProperty;
     }
 
--- LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaPlatformRunPropertyCreator.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Document/Property_/RunProperty_/SkiaPlatformRunPropertyCreator.cs
@@ -13,6 +13,19 @@ internal class SkiaPlatformRunPropertyCreator : PlatformRunPropertyCreatorBase<S
 
     private readonly SkiaPlatformResourceManager _skiaPlatformResourceManager;
 
+    public override IReadOnlyRunProperty GetRunProperty(ICharObject charObject, IReadOnlyRunProperty baseRunProperty)
+    {
+        if (baseRunProperty is SkiaTextRunProperty skiaTextRunProperty)
+        {
+            return skiaTextRunProperty;
+        }
+        else
+        {
+            // 非当前平台支持的属性
+            throw new NotSupportedException(); // todo 填充内容
+        }
+    }
+
     protected override SkiaTextRunProperty OnGetDefaultRunProperty()
     {
         return new SkiaTextRunProperty(_skiaPlatformResourceManager);
