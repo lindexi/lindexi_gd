79f129daa5523ec873ad860c90bda93be4dbf904
lindexi <lindexi_gd@163.com>
2024-11-29 09:11:49 +0800

    尝试获取命名然而失败

---
+++ X11/HaluchairwayberNawcakemyijainu/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+unsafe
+{
+    var name = stackalloc char[256];
+    var length = gethostname(name, 256);
+
+    var t = new string(name, 0, length);
+
+    Console.WriteLine(t);
+
+    [DllImport("libc")]
+    static extern int gethostname(char* name, int len);
+}
