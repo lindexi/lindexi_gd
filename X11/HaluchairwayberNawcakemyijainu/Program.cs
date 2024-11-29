b6369998686c90cf0c43e495568caf91df3cdeae
lindexi <lindexi_gd@163.com>
2024-11-29 09:13:47 +0800

    似乎拿不到内容

--- X11/HaluchairwayberNawcakemyijainu/Program.cs
+++ X11/HaluchairwayberNawcakemyijainu/Program.cs
@@ -2,12 +2,12 @@
 using System.Runtime.InteropServices;
 unsafe
 {
-    var name = stackalloc char[256];
-    var length = gethostname(name, 256);
+    var name = stackalloc char[1024];
+    var length = gethostname(name, 1024);
 
     var t = new string(name, 0, length);
 
-    Console.WriteLine(t);
+    Console.WriteLine($"name={t};Length={length}");
 
     [DllImport("libc")]
     static extern int gethostname(char* name, int len);
