0fe359c2847d7098bfcd6cec5dc9ef18bdbb5a01
lindexi <lindexi_gd@163.com>
2024-12-03 11:02:22 +0800

    似乎没有好的写法

--- Workbench/NawralelurnafaLearfarcharela/Program.cs
+++ Workbench/NawralelurnafaLearfarcharela/Program.cs
@@ -1,15 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+
+
 abstract class F1
 {
     public abstract T X<T>();
 }
 
-class F2 : F1
+class F1Request
 {
-    public override int X<int>()
+
+}
+
+class Response
+{
+    internal Response(HttpResponseMessage httpResponseMessage)
     {
-        return default;
+     
+    }
+
+    public TResponse As<TResponse>()
+    {
+        throw null;
+    }
+}
+
+class F1Response
+{
+}
+
+class F2
+{
+    public Response GetResponse(F1Request request)
+    {
+        throw null;
     }
 }
