16c9b59a247a78beabdc63a238d1bc1a26d208e8
lindexi <lindexi_gd@163.com>
2024-12-03 20:52:41 +0800

    尝试修复第二次无法输入

--- Bp/KobaydarwemqiJerkojecere/Program.cs
+++ Bp/KobaydarwemqiJerkojecere/Program.cs
@@ -6,22 +6,38 @@ var url = "http://172.20.114.91:5017";
 
 var httpClient = new HttpClient();
 
-var chatRequest = new ChatRequest("Hello");
 
-var httpRequestMessage = new HttpRequestMessage();
-httpRequestMessage.Content = JsonContent.Create(chatRequest);
-httpRequestMessage.Method = HttpMethod.Post;
-httpRequestMessage.RequestUri = new Uri($"{url}/Chat");
 
-using var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage,HttpCompletionOption.ResponseHeadersRead);
-
-using var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
-using var streamReader = new StreamReader(stream);
-var buffer = new char[1];
-
-while (streamReader.ReadBlock(buffer.AsSpan()) > 0)
+while (true)
 {
-    Console.Write(buffer[0]);
+    Console.WriteLine($"请输入聊天内容");
+    var prompt = Console.ReadLine();
+    if (string.IsNullOrEmpty(prompt))
+    {
+        continue;
+    }
+
+    var chatRequest = new ChatRequest(prompt);
+
+    var httpRequestMessage = new HttpRequestMessage();
+    httpRequestMessage.Content = JsonContent.Create(chatRequest);
+    httpRequestMessage.Method = HttpMethod.Post;
+    httpRequestMessage.RequestUri = new Uri($"{url}/Chat");
+
+    using var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead);
+
+    using var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
+    using var streamReader = new StreamReader(stream);
+    var buffer = new char[1];
+
+    while (streamReader.ReadBlock(buffer.AsSpan()) > 0)
+    {
+        Console.Write(buffer[0]);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("聊天完成");
+    Console.WriteLine();
 }
 
 record ChatRequest(string Prompt)
