ad38c32eea3bff1d13c6b92dba8da8859d62124f
lindexi <lindexi_gd@163.com>
2024-12-03 21:08:24 +0800

    测试简单的调用可以成功

--- Bp/KobaydarwemqiJerkojecere/Program.cs
+++ Bp/KobaydarwemqiJerkojecere/Program.cs
@@ -12,10 +12,73 @@ while (true)
     var prompt = Console.ReadLine();
     if (string.IsNullOrEmpty(prompt))
     {
-        continue;
+        prompt = """
+                 <|system|>
+                 "You are an AI assistant that can help the user with a variety of tasks. You have access to the following functions:
+                 
+                 [
+                     {
+                         "name": "function_name",
+                         "description": "function_description",
+                         "parameters": [
+                             {
+                                 "name": "parameter_name",
+                                 "type": "parameter_type",
+                                 "description": "parameter_description"
+                             },
+                             ...
+                         ],
+                         "required": [ "required_parameter_name_1", ... ],
+                         "returns": [
+                             {
+                                 "name": "output_name",
+                                 "type": "output_type",
+                                 "description": "output_description"
+                             },
+                             ...
+                         ]
+                     },
+                     ...
+                 ] 
+                 
+                 When the user asks you a question, if you need to use functions, provide function calls in the format:
+                 [
+                     { "name": "function_name", "params": { dictionary containing parameters}, "output": "The output variable name, to be possibly used as input for another function"}.
+                     ...
+                 ]
+                 
+                 Here is a list of functions you can use:
+                 [
+                     {
+                         "name": "get_weather_data",
+                         "parameters": [
+                             {
+                                 "name": "city",
+                                 "type": "string",
+                                 "description": "The name of the city for which weather data is required."
+                             }
+                         ],
+                         "returns": [
+                             {
+                                 "name": "weather_data",
+                                 "type": "dictionary",
+                                 "description": "A dictionary containing weather data for the specified city."
+                             }
+                         ]
+                     }
+                 ]
+                 
+                 <|end|>
+                 <|user|>
+                 天津的天气如何<|end|>
+                 <|assistant|>
+                 """;
     }
-
-    var chatRequest = new ChatRequest($@"<|user|>{prompt}<|end|><|assistant|>");
+    else
+    {
+        prompt = $@"<|user|>{prompt}<|end|><|assistant|>";
+    }
+    var chatRequest = new ChatRequest(prompt);
 
     var httpRequestMessage = new HttpRequestMessage();
     httpRequestMessage.Content = JsonContent.Create(chatRequest);
