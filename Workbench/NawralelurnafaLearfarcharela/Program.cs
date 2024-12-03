21f858bbaa6721835fc3b0c9da72fde1ecdd9c16
lindexi <lindexi_gd@163.com>
2024-12-03 10:54:58 +0800

    测试重写时返回指定的响应类型

---
+++ Workbench/NawralelurnafaLearfarcharela/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+abstract class F1
+{
+    public abstract T X<T>();
+}
+
+class F2 : F1
+{
+    public override int X<int>()
+    {
+        return default;
+    }
+}
