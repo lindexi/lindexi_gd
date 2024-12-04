01ca43f8195c822a6be9a06befda81157c9c4da2
lindexi <lindexi_gd@163.com>
2024-12-04 17:01:51 +0800

    能够调用正确方法

---
+++ Workbench/LalerfuqearJojurcawfokel/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var f2 = new F2();
+f2.Do(new F1());
+
+Console.WriteLine("Hello, World!");
+
+
+class Foo<T> where T : F1
+{
+    public void Do(F1 foo)
+    {
+        Do((T)foo);
+    }
+
+    protected virtual void Do(T foo)
+    {
+    }
+}
+
+class F1 { }
+
+class F2 : Foo<F1>
+{
+}
