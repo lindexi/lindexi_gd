69b619c4f8bf052c349da03228326945deee67f4
lindexi <lindexi_gd@163.com>
2024-12-04 19:59:21 +0800

    尝试执行加载

---
+++ Bp/HifehuguBineekaife/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.ML.OnnxRuntime;
+
+var modelFilePath = "model.onnx";
+
+if (args.Length > 0)
+{
+    modelFilePath = args[0];
+}
+
+using SessionOptions options = new SessionOptions();
+options.LogSeverityLevel = OrtLoggingLevel.ORT_LOGGING_LEVEL_INFO;
+
+// Load the model
+using var session = new InferenceSession(modelFilePath, options);
+
+Console.WriteLine("Hello, World!");
