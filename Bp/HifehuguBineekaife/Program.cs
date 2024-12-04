87bd386cb9830ce1bc316e4a553c4c998e9c23c3
lindexi <lindexi_gd@163.com>
2024-12-04 20:38:55 +0800

    似乎可以加载

--- Bp/HifehuguBineekaife/Program.cs
+++ Bp/HifehuguBineekaife/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.ObjectModel;
 using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntimeGenAI;
 
 var modelFilePath = "model.onnx";
+
+var folder =
+    @"C:\lindexi\Phi3\Phi-3-medium-128k-instruct-onnx-cuda\cuda-int4-rtn-block-32";
+
+using var model = new Model(folder);
+using var tokenizer = new Tokenizer(model);
 
 if (args.Length > 0)
 {
@@ -14,5 +22,9 @@ options.LogSeverityLevel = OrtLoggingLevel.ORT_LOGGING_LEVEL_INFO;
 
 // Load the model
 using var session = new InferenceSession(modelFilePath, options);
+
+session.Run(new ReadOnlyCollection<NamedOnnxValue>(new List<NamedOnnxValue>()
+{
+}));
 
 Console.WriteLine("Hello, World!");
