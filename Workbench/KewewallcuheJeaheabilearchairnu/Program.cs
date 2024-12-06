cd17294fc1f30d6420cc5ef0e60a2db92f3f798b
lindexi <lindexi_gd@163.com>
2024-12-06 14:47:03 +0800

    Revert "匹配一半也是正确"
    This reverts commit 0906a6747442f7c091de7a4a100d9841432c8803.

--- Workbench/KewewallcuheJeaheabilearchairnu/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text.RegularExpressions;
-
-var targetFramework = "net9.0";
-Console.WriteLine(Regex.IsMatch(targetFramework, @"net\d"));
