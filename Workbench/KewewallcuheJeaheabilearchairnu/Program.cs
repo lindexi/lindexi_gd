0906a6747442f7c091de7a4a100d9841432c8803
lindexi <lindexi_gd@163.com>
2024-12-06 14:46:45 +0800

    匹配一半也是正确

---
+++ Workbench/KewewallcuheJeaheabilearchairnu/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var targetFramework = "net9.0";
+Console.WriteLine(Regex.IsMatch(targetFramework, @"net\d"));
