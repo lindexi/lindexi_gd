624ff38044e10a2c4da0a917518ca7b6910f60f7
lindexi <lindexi_gd@163.com>
2024-12-10 17:55:21 +0800

    测试重复的数量

--- Workbench/QanairilihaikeHerejubadair/Program.cs
+++ Workbench/QanairilihaikeHerejubadair/Program.cs
@@ -8,7 +8,7 @@ var n = 2d;
 for (int i = 2; i < m; i++)
 {
     n = i;
-    var result = 1 - Factorial(m) / (Factorial(k) * Factorial(m - k)) * Factorial(n) / (Factorial(k) * Factorial(n - k)) / Factorial(m) * Factorial(m - n);
+    var result = 1 - (Factorial(m - n * k - 1)) / (Math.Pow(m, n - 1) * Factorial(m - n * (k + 1)));
     Console.WriteLine($"在 m={m} 人范围中，抽选 n={n} 次时，抽选相同 k=1 的情况，概率是 {result}");
 }
 
@@ -17,6 +17,10 @@ Console.WriteLine("Hello, World!");
 
 double Factorial(double number)
 {
+    if (number < 0)
+    {
+
+    }
     if (number == 0)
     {
         return 1;
