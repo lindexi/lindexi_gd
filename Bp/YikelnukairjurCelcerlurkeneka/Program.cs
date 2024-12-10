b70079fa08b1161c4c33110132daf9faa1d93020
lindexi <lindexi_gd@163.com>
2024-12-10 21:06:17 +0800

    优化随机抽选提示词

--- Bp/YikelnukairjurCelcerlurkeneka/Program.cs
+++ Bp/YikelnukairjurCelcerlurkeneka/Program.cs
@@ -7,9 +7,10 @@ var phiIntentionRecognition = new PhiIntentionRecognition();
 var inputList =
     new string[]
     {
-        "小象过河的图片",
-        "仿写夜宿山寺",
-        "一年级下半个学期，学生的语文学习学情分析",
+        "五年二班随机抽取一名学生",
+        //"小象过河的图片",
+        //"仿写夜宿山寺",
+        //"一年级下半个学期，学生的语文学习学情分析",
     };
 
 foreach (var input in inputList)
