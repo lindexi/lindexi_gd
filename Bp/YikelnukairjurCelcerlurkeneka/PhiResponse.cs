164f8f857312184ed5b2513785968b1c13ba8eb5
lindexi <lindexi_gd@163.com>
2024-12-10 20:58:44 +0800

    尝试炼丹意图识别
    Input=一年级下半个学期，学生的语文学习学情分析 Success=True Intention=生成教学设计

---
+++ Bp/YikelnukairjurCelcerlurkeneka/PhiResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace YikelnukairjurCelcerlurkeneka;
+
+public class PhiResponse : IDisposable, IAsyncDisposable
+{
+    public bool Success { get; init; }
+
+    public string? ErrorMessage { get; init; }
+
+    public Stream ResponseStream { get; init; } = null!;
+
+    public async Task<string> ReadToEndAsync()
+    {
+        using var streamReader = new StreamReader(ResponseStream, leaveOpen: true);
+        return await streamReader.ReadToEndAsync();
+    }
+
+    public async IAsyncEnumerable<char> ReadAsync()
+    {
+        var buffer = new char[1];
+        using var streamReader = new StreamReader(ResponseStream, leaveOpen: true);
+
+        while ((await streamReader.ReadBlockAsync(buffer, 0, 1)) > 0)
+        {
+            yield return buffer[0];
+        }
+    }
+
+    public void Dispose()
+    {
+        ResponseStream.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await ResponseStream.DisposeAsync();
+    }
+}
---
+++ Bp/YikelnukairjurCelcerlurkeneka/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using YikelnukairjurCelcerlurkeneka;
+
+var phiIntentionRecognition = new PhiIntentionRecognition();
+var input = "小象过河的图片";
+input = "仿写夜宿山寺";
+input = "一年级下半个学期，学生的语文学习学情分析";
+var result = await phiIntentionRecognition.RecognizeAsync(input);
+
+Console.WriteLine($"Input={input} Success={result.Success} Intention={result.Intention}");
+
+Console.WriteLine("Hello, World!");
