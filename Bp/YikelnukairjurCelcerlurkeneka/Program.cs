e384f5ddd8605016eb25efb15eaa029a3284fa01
lindexi <lindexi_gd@163.com>
2024-12-11 08:48:54 +0800

    加上更多测试内容

--- Bp/YikelnukairjurCelcerlurkeneka/Program.cs
+++ Bp/YikelnukairjurCelcerlurkeneka/Program.cs
@@ -7,7 +7,8 @@ var phiIntentionRecognition = new PhiIntentionRecognition();
 var inputList =
     new string[]
     {
-        "五年二班随机抽取一名学生",
+        "从1至10数字中，随机抽取4个数字",
+        //"五年二班随机抽取一名学生",
         //"小象过河的图片",
         //"仿写夜宿山寺",
         //"一年级下半个学期，学生的语文学习学情分析",
