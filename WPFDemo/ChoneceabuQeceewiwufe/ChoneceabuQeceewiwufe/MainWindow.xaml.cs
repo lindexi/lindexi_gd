b0b3fcd654f55c7bbebc6c42b8536f6bdd1d7101
lindexi <lindexi_gd@163.com>
2024-12-12 20:16:10 +0800

    似乎依然无法获取

--- WPFDemo/ChoneceabuQeceewiwufe/ChoneceabuQeceewiwufe/MainWindow.xaml.cs
+++ WPFDemo/ChoneceabuQeceewiwufe/ChoneceabuQeceewiwufe/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,11 +25,19 @@ using Windows.UI.Input.Inking.Core;
 using Windows.Win32;
 using Windows.Win32.System.Com;
 using WinRT;
-
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using InkPresenter = Windows.UI.Input.Inking.InkPresenter;
 using Point = Windows.Foundation.Point;
 
 namespace ChoneceabuQeceewiwufe;
+
+//[Guid("062584A6-F830-4BDC-A4D2-0A10AB062B1D")]
+//public unsafe partial struct InkDesktopHost : INativeGuid
+//{
+//    static Guid* INativeGuid.NativeGuid => (Guid*) Unsafe.AsPointer(ref Unsafe.AsRef(in CLSID_InkDesktopHost));
+//}
+
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
@@ -53,6 +62,8 @@ public partial class MainWindow : Window
             // 将抛出 没有注册类 (0x80040154 (REGDB_E_CLASSNOTREG))
             global::Microsoft.UI.Xaml.Application.Start(p =>
             {
+                // class DECLSPEC_UUID("062584a6-f830-4bdc-a4d2-0a10ab062b1d") InkDesktopHost;
+
                 //  __RP_Marker_ClassById(RuntimeProfiler::ProfId_InkCanvas);
                 // https://learn.microsoft.com/en-us/windows/win32/input_ink/inkdesktophost
                 //// 0x8001010e
@@ -61,7 +72,8 @@ public partial class MainWindow : Window
                 // https://github.com/microsoft/microsoft-ui-xaml/blob/ff21f9b212cea2191b959649e45e52486c8465aa/src/controls/dev/InkCanvas/InkCanvas.cpp#L92C9-L92C150
                 // InkDesktopHost 4ce7d875-a981-4140-a1ff-ad93258e8d59
                 var inkDesktopHostGuid = new Guid("4ce7d875-a981-4140-a1ff-ad93258e8d59");
-                var hResult = PInvoke.CoCreateInstance(inkDesktopHostGuid, null, CLSCTX.CLSCTX_INPROC_SERVER, inkDesktopHostGuid, out object inkDesktopHost);
+                var riid = new Guid("062584A6-F830-4BDC-A4D2-0A10AB062B1D");
+                var hResult = PInvoke.CoCreateInstance(inkDesktopHostGuid, null, CLSCTX.CLSCTX_INPROC_SERVER, riid, out object inkDesktopHost);
                 if (hResult.Failed)
                 {
                     // 0x80040154
