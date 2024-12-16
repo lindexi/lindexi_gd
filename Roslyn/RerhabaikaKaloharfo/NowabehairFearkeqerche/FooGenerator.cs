2e8c72f504603927ff9d430813a8bd2f48436dc4
lindexi <lindexi_gd@163.com>
2024-12-16 09:31:41 +0800

    完成搭建简单的调试项目

---
+++ Roslyn/RerhabaikaKaloharfo/NowabehairFearkeqerche/FooGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace NowabehairFearkeqerche
+{
+    [Generator(LanguageNames.CSharp)]
+    public class FooGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
