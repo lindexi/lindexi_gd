f7c98039ac7e4eb6440cf26bff269b26024b463e
lindexi <lindexi_gd@163.com>
2024-12-16 20:25:36 +0800

    测试调用但是失败

---
+++ WPFDemo/NerballbufairCemkurjaiwarbofel/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TerraFX.Interop.Windows;
+
+namespace NerballbufairCemkurjaiwarbofel;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        var inkDesktopHost = new InkDesktopHost();
+        var desktopHost = new IInkDesktopHost();
+        desktopHost.CreateInkPresenter();
+        var inkPresenterDesktop = new IInkPresenterDesktop();
+    }
+}
