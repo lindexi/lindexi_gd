7614525103cafbad61fb1fdbb20861ee297d27a0
lindexi <lindexi_gd@163.com>
2024-12-21 15:35:57 +0800

    简单测试使用

---
+++ Workbench/FichallbibaRenafawwhi/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Icu.Wrapper.Init();
+// Will output "NFC form of XA\u0308bc is XÄbc"
+Console.WriteLine($"NFC form of XA\\u0308bc is {Icu.Normalizer.Normalize("XA\u0308bc",
+    Icu.Normalizer.UNormalizationMode.UNORM_NFC)}");
+Icu.Wrapper.Cleanup();
