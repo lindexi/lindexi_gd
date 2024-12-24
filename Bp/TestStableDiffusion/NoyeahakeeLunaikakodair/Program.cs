8bc17c9752117e38ee73221a474ccbe9d62504d9
lindexi <lindexi_gd@163.com>
2024-12-24 14:15:36 +0800

    尝试应用简单的仓库

---
+++ Bp/TestStableDiffusion/NoyeahakeeLunaikakodair/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HPPH;
+using HPPH.SkiaSharp;
+using StableDiffusion.NET;
+
+using DiffusionModel model = ModelBuilder.StableDiffusion(@"<path to model")
+    .WithVae(@"<optional path to vae>")
+    .WithMultithreading()
+    .Build();
+
+IImage<ColorRGB> image = model.TextToImage("<prompt>", model.GetDefaultParameter().WithSeed(1234).WithSize(1344, 768));
+
+File.WriteAllBytes("output.png", image.ToPng());
