37781366517c96c2e11d173577e981a50e8a7600
lindexi <lindexi_gd@163.com>
2024-12-26 17:43:06 +0800

    添加简单的服务端

---
+++ Workbench/WatchDog/WatchDog.Service/Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WatchDog.Service.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+   
+}
