fca4e3c909fc3884917032b5740473ee25c3b92f
lindexi <lindexi_gd@163.com>
2024-12-26 16:03:42 +0800

    准备空白项目

---
+++ Workbench/WatchDog/WatchDog.Core/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace WatchDog.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class WatchDogProvider
+    {
+        public void Feed()
+        {
+
+        }
+
+        public void GetWang()
+        {
+
+        }
+
+        public void Mute()
+        {
+
+        }
+    }
+
+
+}
