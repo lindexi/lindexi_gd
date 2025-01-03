440f59003daf7fa69ee5d3a207678e900670c29b
lindexi <lindexi_gd@163.com>
2025-01-03 14:47:10 +0800

    测试字体不存在的匹配性能

--- Workbench/KalnurrewelheKewhemnibuke/Program.cs
+++ Workbench/KalnurrewelheKewhemnibuke/Program.cs
@@ -1,7 +1,28 @@
 // See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using SkiaSharp;
 
-var typeface = SKFontManager.Default.MatchFamily("FontNameNotFound");
+var fontFamilies = SKFontManager.Default.GetFontFamilies();
+
+var notFoundNameList = new List<string>();
+
+for (int i = 0; i < 100; i++)
+{
+    notFoundNameList.Add($"FontNameNotFound{i}");
+}
+
+var stopwatch = Stopwatch.StartNew();
+for (var i = 0; i < 100; i++)
+{
+    foreach (var fontName in notFoundNameList)
+    {
+        var typeface = SKFontManager.Default.MatchFamily(fontName);
+        _ = typeface;
+    }
+}
+
+stopwatch.Stop();
+Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}ms Ave:{stopwatch.Elapsed.TotalMilliseconds / notFoundNameList.Count / 100}ms FontFamilies={notFoundNameList.Count}");
 
 Console.Read();
