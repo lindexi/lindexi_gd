bd90597059de07187ab9620ef574766231978cec
lindexi <lindexi_gd@163.com>
2025-01-03 16:52:50 +0800

    测试多次释放

---
+++ SkiaSharp/WerlokeenulaqaiYaijaykarfer/Program.cs
@@ -0,0 +1,12 @@
+// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using SkiaSharp;
+
+var typeface = SKTypeface.FromFamilyName("微软雅黑");
+
+typeface.Dispose();
+typeface.Dispose();
+typeface.Dispose();
+
+Console.Read();
