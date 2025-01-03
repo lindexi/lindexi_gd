2e79a7703b4a858d71a535b8ce36ec0fc5937142
lindexi <lindexi_gd@163.com>
2025-01-03 14:45:02 +0800

    正确获取不存在的字体

--- Workbench/KalnurrewelheKewhemnibuke/Program.cs
+++ Workbench/KalnurrewelheKewhemnibuke/Program.cs
@@ -1,20 +1,7 @@
 // See https://aka.ms/new-console-template for more information
 
-using System.Diagnostics;
 using SkiaSharp;
 
-var fontFamilies = SKFontManager.Default.GetFontFamilies();
-
-var notFoundNameList = new List<string>();
-
-for (int i = 0; i < 100; i++)
-{
-    notFoundNameList.Add($"FontNameNotFound{i}");
-}
-
-foreach (var fontName in notFoundNameList)
-{
-    var typeface = SKTypeface.FromFamilyName(fontName);
-}
+var typeface = SKFontManager.Default.MatchFamily("FontNameNotFound");
 
 Console.Read();
