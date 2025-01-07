9e1cdbfe578f0550c035b352ad2a2b8c2d84acdc
lindexi <lindexi_gd@163.com>
2025-01-07 14:18:23 +0800

    测试行距

---
+++ Pptx/JanarjaldelchawjiNijairkikichi/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+var file = "KaylerrurdedobifelKerereleakur.docx";
+using var wordprocessingDocument = WordprocessingDocument.Open(file,isEditable:true);
+MainDocumentPart? mainDocumentPart = wordprocessingDocument.MainDocumentPart;
+foreach (OpenXmlElement openXmlElement in mainDocumentPart?.Document?.Body ?? [])
+{
+    if (openXmlElement is DocumentFormat.OpenXml.Wordprocessing.Paragraph paragraph)
+    {
+        if (paragraph.ParagraphProperties is {} paragraphProperties)
+        {
+            SpacingBetweenLines? spacingBetweenLines = paragraphProperties.SpacingBetweenLines;
+            if (spacingBetweenLines?.LineRule?.Value == LineSpacingRuleValues.Exact)
+            {
+                var lineValue = spacingBetweenLines.Line?.Value;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Hello, World!");
