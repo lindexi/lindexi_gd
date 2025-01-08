7dc67968a03dd242d6ec6623165fce1180675be4
lindexi <lindexi_gd@163.com>
2025-01-08 19:47:58 +0800

    尝试使用主设备的方式

--- Workbench/WatchDog/WatchDog.Service/Controllers/DogController.cs
+++ Workbench/WatchDog/WatchDog.Service/Controllers/DogController.cs
@@ -20,6 +20,96 @@ public class DogController : ControllerBase
     }
 }
 
+public interface IMasterHostProvider
+{
+    Task<MasterHostResult> GetMasterHostAsync();
+}
+
+public class RedisMasterHostProvider : IMasterHostProvider
+{
+    public RedisMasterHostProvider(IRedisClient redisClient, ISelfHostProvider selfHostProvider)
+    {
+        _redisClient = redisClient;
+        _selfHostProvider = selfHostProvider;
+    }
+
+    private readonly IRedisClient _redisClient;
+
+    private readonly ISelfHostProvider _selfHostProvider;
+
+    public async Task<MasterHostResult> GetMasterHostAsync()
+    {
+        var selfHost = await _selfHostProvider.GetSelfHostAsync();
+        var lockKey = "WatchDog-MasterLock-4ACF7B3F-222D-469C-B99D-5E3966FFB422";
+        // 先获取一遍，发现存在了，则尝试访问一下。如果能够通过，证明主设备存在
+        // 如果发现存在的就是自己，那自己就是主设备
+
+        var redisKey = new RedisKey(lockKey);
+        var redisValue = new RedisValue(selfHost);
+
+        for (int i = 0; i < 1000; i++)
+        {
+            var masterHost = await _redisClient.Db0.GetAsync<string>(lockKey);
+            if (masterHost == selfHost)
+            {
+                // 可能就是自己就是主设备了
+                // 先等等再获取，看对不对
+                await Task.Delay(300);
+
+                masterHost = await _redisClient.Db0.GetAsync<string>(lockKey);
+                if (masterHost == selfHost)
+                {
+                    // 确定自己就是主设备
+                    return new MasterHostResult(true, selfHost);
+                }
+                else
+                {
+                    // 别人是主设备
+                }
+            }
+            //var success = await _redisClient.Db0.Database.SetAddAsync(redisKey, redisValue);
+        }
+
+        throw new InvalidOperationException();
+    }
+}
+
+public interface ISelfHostProvider
+{
+    Task<string> GetSelfHostAsync();
+}
+
+/// <summary>
+/// 获取主机信息结果
+/// </summary>
+/// <param name="SelfIsMaster">当前是否就是主设备</param>
+/// <param name="MasterHost">主设备地址</param>
+public record MasterHostResult(bool SelfIsMaster, string MasterHost);
+
+public class FakeSelfHostProvider : ISelfHostProvider
+{
+    public FakeSelfHostProvider()
+    {
+        _hostName = Random.Shared.Next().ToString();
+    }
+
+    private readonly string _hostName;
+
+    public Task<string> GetSelfHostAsync()
+    {
+        return Task.FromResult(_hostName);
+    }
+}
+
+public static class WatchDogStartup
+{
+    public static void AddWatchDog(this IServiceCollection services)
+    {
+        services.AddSingleton<ISelfHostProvider, FakeSelfHostProvider>();
+        services.AddSingleton<IMasterHostProvider, RedisMasterHostProvider>();
+    }
+}
+
 class RedisLocker
 {
     public RedisLocker(IRedisClient redisClient, string lockKey, string lockValue)
