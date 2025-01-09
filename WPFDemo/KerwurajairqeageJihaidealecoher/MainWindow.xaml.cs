a80deafdf34ffc84dcb7308ccc1861aabc740777
lindexi <lindexi_gd@163.com>
2025-01-09 20:46:13 +0800

    似乎能进入计算

---
+++ WPFDemo/KerwurajairqeageJihaidealecoher/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using SkiaSharp;
+
+namespace KerwurajairqeageJihaidealecoher;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var fontFamily = new FontFamily("微软雅黑");
+        var lineSpacing = fontFamily.LineSpacing;
+
+        SKTypeface? skTypeface = SKFontManager.Default.MatchFamily("微软雅黑");
+        var skFont = new SKFont(skTypeface, 100);
+        var leading = skFont.Metrics.Leading / 100;
+        var h = (-skFont.Metrics.Ascent + skFont.Metrics.Descent) / 100;
+    }
+}
