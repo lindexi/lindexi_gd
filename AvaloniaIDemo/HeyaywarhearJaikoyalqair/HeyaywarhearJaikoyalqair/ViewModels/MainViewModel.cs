a66a404a8e49c97cd662c46a0d2d55735b3676f2
lindexi <lindexi_gd@163.com>
2025-01-13 19:31:12 +0800

    在任意的鼠标点击获取到文本内容

---
+++ AvaloniaIDemo/HeyaywarhearJaikoyalqair/HeyaywarhearJaikoyalqair/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace HeyaywarhearJaikoyalqair.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ AvaloniaIDemo/HeyaywarhearJaikoyalqair/HeyaywarhearJaikoyalqair/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HeyaywarhearJaikoyalqair.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
