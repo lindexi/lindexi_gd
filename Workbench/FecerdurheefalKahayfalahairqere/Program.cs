785445b1bdad8b55f051d4c3bcc4d882dd5c7ff2
lindexi <lindexi_gd@163.com>
2025-01-13 17:47:42 +0800

    提供确实更高的性能

--- Workbench/FecerdurheefalKahayfalahairqere/Program.cs
+++ Workbench/FecerdurheefalKahayfalahairqere/Program.cs
@@ -2,23 +2,31 @@
 
 using System.Runtime.InteropServices;
 
-var span = MemoryMarshal.AsBytes("sdfsadf".AsSpan());
-
 var foo = new Foo()
 {
     F1 = 2
 };
 
-Span<Foo> span1 = MemoryMarshal.CreateSpan(ref foo,1);
-span = MemoryMarshal.AsBytes(span1);
-
+Span<Foo> span1 = MemoryMarshal.CreateSpan(ref foo, 1);
 
 Span<byte> sp = MemoryMarshal.Cast<Foo, byte>(span1);
 sp[0] = 10;
 
+sp[4] = 20; // F2
+
+sp[5] = 30; // F3
+
+sp[6] = 5;  // Fx
+
+
 Console.WriteLine("Hello, World!");
 
+[StructLayout(LayoutKind.Sequential)]
 struct Foo
 {
     public int F1 { get; set; }
+
+    public byte F2 { get; set; }
+    public byte F3 { get; set; }
+    public byte Fx { get; set; }
 }
