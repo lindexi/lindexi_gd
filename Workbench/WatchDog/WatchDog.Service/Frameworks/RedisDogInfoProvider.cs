808e8444956be407e53b55e426c12af8154890a7
lindexi <lindexi_gd@163.com>
2025-01-13 20:49:35 +0800

    去掉不使用代码

--- Workbench/WatchDog/WatchDog.Service/Frameworks/RedisDogInfoProvider.cs
+++ Workbench/WatchDog/WatchDog.Service/Frameworks/RedisDogInfoProvider.cs
@@ -1,29 +1,29 @@
-using StackExchange.Redis.Extensions.Core.Abstractions;
-using WatchDog.Core.Context;
-using WatchDog.Core.Services;
+//using StackExchange.Redis.Extensions.Core.Abstractions;
+//using WatchDog.Core.Context;
+//using WatchDog.Core.Services;
 
-namespace WatchDog.Service.Frameworks;
+//namespace WatchDog.Service.Frameworks;
 
-class RedisDogInfoProvider : IDogInfoProvider
-{
-    public RedisDogInfoProvider(IRedisClient redisClient)
-    {
-        _redisClient = redisClient;
-    }
+//class RedisDogInfoProvider : IDogInfoProvider
+//{
+//    public RedisDogInfoProvider(IRedisClient redisClient)
+//    {
+//        _redisClient = redisClient;
+//    }
 
-    private readonly IRedisClient _redisClient;
+//    private readonly IRedisClient _redisClient;
 
-    public void SetMute(MuteInfo muteInfo)
-    {
+//    public void SetMute(MuteInfo muteInfo)
+//    {
 
-    }
+//    }
 
-    public void RemoveMuteByActive(string id)
-    {
-    }
+//    public void RemoveMuteByActive(string id)
+//    {
+//    }
 
-    public bool ShouldMute(LastFeedDogInfo info, string dogId)
-    {
-        return false;
-    }
-}
+//    public bool ShouldMute(LastFeedDogInfo info, string dogId)
+//    {
+//        return false;
+//    }
+//}
--- Workbench/WatchDog/WatchDog.Service/Frameworks/RedisLocker.cs
+++ Workbench/WatchDog/WatchDog.Service/Frameworks/RedisLocker.cs
@@ -1,44 +1,44 @@
-using StackExchange.Redis;
-using StackExchange.Redis.Extensions.Core.Abstractions;
+//using StackExchange.Redis;
+//using StackExchange.Redis.Extensions.Core.Abstractions;
 
-namespace WatchDog.Service.Frameworks;
+//namespace WatchDog.Service.Frameworks;
 
-class RedisLocker
-{
-    public RedisLocker(IRedisClient redisClient, string lockKey, string lockValue)
-    {
-        _redisClient = redisClient;
-        _lockKey = lockKey;
-        _lockValue = lockValue;
-    }
+//class RedisLocker
+//{
+//    public RedisLocker(IRedisClient redisClient, string lockKey, string lockValue)
+//    {
+//        _redisClient = redisClient;
+//        _lockKey = lockKey;
+//        _lockValue = lockValue;
+//    }
 
-    private readonly IRedisClient _redisClient;
-    private readonly string _lockKey;
-    private readonly string _lockValue;
+//    private readonly IRedisClient _redisClient;
+//    private readonly string _lockKey;
+//    private readonly string _lockValue;
 
-    public async Task DoInLockAsync(Func<Task> task)
-    {
-        var redisKey = new RedisKey(_lockKey);
-        var redisValue = new RedisValue(_lockValue);
+//    public async Task DoInLockAsync(Func<Task> task)
+//    {
+//        var redisKey = new RedisKey(_lockKey);
+//        var redisValue = new RedisValue(_lockValue);
 
-        while (true)
-        {
-            var success = await _redisClient.Db0.Database.SetAddAsync(redisKey, redisValue);
-            if (success)
-            {
-                break;
-            }
+//        while (true)
+//        {
+//            var success = await _redisClient.Db0.Database.SetAddAsync(redisKey, redisValue);
+//            if (success)
+//            {
+//                break;
+//            }
 
-            await Task.Delay(500);
-        }
+//            await Task.Delay(500);
+//        }
 
-        try
-        {
-            await task();
-        }
-        finally
-        {
-            await _redisClient.Db0.Database.SetRemoveAsync(redisKey, redisValue);
-        }
-    }
-}
+//        try
+//        {
+//            await task();
+//        }
+//        finally
+//        {
+//            await _redisClient.Db0.Database.SetRemoveAsync(redisKey, redisValue);
+//        }
+//    }
+//}
