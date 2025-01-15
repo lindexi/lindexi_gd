f0473473717ffebd0d9563a9305bc35042a70d45
lindexi <lindexi_gd@163.com>
2025-01-15 14:08:08 +0800

    经过测试找不到方法

--- WPFDemo/BerwhaywawemFawwherwhike/MainWindow.xaml.cs
+++ WPFDemo/BerwhaywawemFawwherwhike/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Win32;
 
 namespace BerwhaywawemFawwherwhike;
 
@@ -19,5 +20,39 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        // https://github.com/mootoh/mozc/blob/b85bb8e851c0724ca9dcf21ae817eda06ad2cd03/src/dictionary/user_dictionary_importer.cc
+        Span<string> libList =
+        [
+            "imjp14k.dll", // Office 14 / 2010
+            "imjp12k.dll", // Office 12 / 2007
+            "imjp10k.dll", // Windows NT 6.0, 6.1
+            "imjp9k.dll", // Office 11 / 2003
+            // The bottom-of-the-line of our targets is Windows XP
+            // so we should stop looking up IMEs at "imjp81k.dll"
+            // http://b/2440318
+            "imjp81k.dll", // Windows NT 5.1, 5.2
+            "imjp8k.dll",   // Office 10 / XP / 2002]
+        ];
+
+        FreeLibrarySafeHandle libHandle = new FreeLibrarySafeHandle(IntPtr.Zero);
+        foreach (var libName in libList)
+        {
+            libHandle = PInvoke.LoadLibrary(libName);
+            if (!libHandle.IsInvalid)
+            {
+                break;
+            }
+        }
+
+        if (!libHandle.IsInvalid)
+        {
+            return;
+        }
     }
 }
