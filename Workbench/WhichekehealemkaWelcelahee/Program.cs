caaa6244e7cd409fb9d434febb53770e0a7094b6
lindexi <lindexi_gd@163.com>
2025-01-17 17:06:03 +0800

    测试方法参数调用

--- Workbench/WhichekehealemkaWelcelahee/Program.cs
+++ Workbench/WhichekehealemkaWelcelahee/Program.cs
@@ -3,28 +3,37 @@
 using System.Runtime.CompilerServices;
 
 var fx = new Fx();
-fx.Foo($"asd");
+fx.Foo($"asd{GetTestInfo()}");
 
 Console.WriteLine("Hello, World!");
 
+int GetTestInfo()
+{
+    Console.WriteLine($"GetTestInfo");
+    return 2;
+}
 
 class Fx
 {
-    public void Foo([InterpolatedStringHandlerArgument("")]M m)
+    public void Foo([InterpolatedStringHandlerArgument("")] FooInterpolatedStringHandler fooInterpolatedStringHandler)
     {
-
     }
 
     public bool Enable { set; get; }
 
     [InterpolatedStringHandler]
-    public ref struct M
+    public ref struct FooInterpolatedStringHandler
     {
-        public M(int literalLength, int formattedCount, Fx fx)
+        public FooInterpolatedStringHandler(int literalLength, int formattedCount, Fx fx, out bool isEnable)
         {
+            isEnable = fx.Enable;
         }
 
         public void AppendLiteral(string s)
+        {
+        }
+
+        public void AppendFormatted<T>(T value)
         {
         }
     }
