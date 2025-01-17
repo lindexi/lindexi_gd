42289001e125a2e8a34a52ece3d1ef812a94bda1
lindexi <lindexi_gd@163.com>
2025-01-17 16:47:51 +0800

    测试可以获取类型

---
+++ Workbench/WhichekehealemkaWelcelahee/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+var fx = new Fx();
+fx.Foo($"asd");
+
+Console.WriteLine("Hello, World!");
+
+
+class Fx
+{
+    public void Foo([InterpolatedStringHandlerArgument("")]M m)
+    {
+
+    }
+
+    public bool Enable { set; get; }
+
+    [InterpolatedStringHandler]
+    public ref struct M
+    {
+        public M(int literalLength, int formattedCount, Fx fx)
+        {
+        }
+
+        public void AppendLiteral(string s)
+        {
+        }
+    }
+}
