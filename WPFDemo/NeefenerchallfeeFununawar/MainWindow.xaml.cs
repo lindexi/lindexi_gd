930bca8018d829a6983f5a762c28a8d250e808f3
lindexi <lindexi_gd@163.com>
2025-01-20 14:40:10 +0800

    获取系统默认字体

---
+++ WPFDemo/NeefenerchallfeeFununawar/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SystemFonts = System.Windows.SystemFonts;
+
+namespace NeefenerchallfeeFununawar;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var messageFontFamily = SystemFonts.MessageFontFamily;
+        var menuFontName = System.Drawing.SystemFonts.MenuFont?.Name;
+        var defaultFontName = System.Windows.Forms.Control.DefaultFont.Name;
+    }
+}
