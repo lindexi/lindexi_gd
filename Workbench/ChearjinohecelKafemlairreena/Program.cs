115b869fdeaf39967013b4dd7fc30652e1c49d15
lindexi <lindexi_gd@163.com>
2025-02-03 17:00:43 +0800

    测试可以获取变更的文件和变更的类型但是无法获取到内容

--- Workbench/ChearjinohecelKafemlairreena/Program.cs
+++ Workbench/ChearjinohecelKafemlairreena/Program.cs
@@ -14,20 +14,18 @@ bool isPathIgnored = repository.Ignore.IsPathIgnored("bin/obj/Foo.exe");
 var queryableCommitLog = repository.Commits;
 Commit commit = queryableCommitLog.First();
 
-var remoteCollection = repository.Network.Remotes;
+ObjectId commitId = commit.Id;
+GitObject gitObject = repository.Lookup(commitId);
+
+TreeChanges treeChanges = repository.Diff.Compare<TreeChanges>(commit.Parents.First().Tree,commit.Tree);
+foreach (TreeEntryChanges treeEntryChanges in treeChanges)
+{
+    var path = treeEntryChanges.Path;
+    var changeKind = treeEntryChanges.Status;
+}
 
 HistoryDivergence historyDivergence = repository.ObjectDatabase.CalculateHistoryDivergence(queryableCommitLog.Skip(100).First(), commit);
 var historyDivergenceCommonAncestor = historyDivergence.CommonAncestor;
-
-//queryableCommitLog.QueryBy(new CommitFilter()
-//{
-//    SortBy = CommitSortStrategies.None,
-//})
-
-foreach (var note in historyDivergenceCommonAncestor.Notes)
-{
-    
-}
 
 GC.KeepAlive(repository);
 Console.WriteLine("Hello, World!");
