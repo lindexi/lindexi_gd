73487785d01e8138e86733f39e7be1e63522a714
lindexi <lindexi_gd@163.com>
2025-02-03 17:18:27 +0800

    测试获取变更的内容

--- Workbench/ChearjinohecelKafemlairreena/Program.cs
+++ Workbench/ChearjinohecelKafemlairreena/Program.cs
@@ -14,8 +14,48 @@ bool isPathIgnored = repository.Ignore.IsPathIgnored("bin/obj/Foo.exe");
 var queryableCommitLog = repository.Commits;
 Commit commit = queryableCommitLog.First();
 
+
 ObjectId commitId = commit.Id;
 GitObject gitObject = repository.Lookup(commitId);
+var patch = repository.Diff.Compare<Patch>(commit.Parents.First().Tree, commit.Tree);
+
+foreach (PatchEntryChanges patchEntryChanges in patch)
+{
+    var path = patchEntryChanges.Path;
+    foreach (var addedLine in patchEntryChanges.AddedLines)
+    {
+    }
+
+    string patchText = patchEntryChanges.Patch;
+    /*
+    diff --git a/Workbench/ChearjinohecelKafemlairreena/Program.cs b/Workbench/ChearjinohecelKafemlairreena/Program.cs
+    index 63d0e46..6a80004 100644
+    --- a/Workbench/ChearjinohecelKafemlairreena/Program.cs
+    +++ b/Workbench/ChearjinohecelKafemlairreena/Program.cs
+    @@ -14,11 +14,18 @@ bool isPathIgnored = repository.Ignore.IsPathIgnored("bin/obj/Foo.exe");
+     var queryableCommitLog = repository.Commits;
+     Commit commit = queryableCommitLog.First();
+
+    -var remoteCollection = repository.Network.Remotes;
+    +ObjectId commitId = commit.Id;
+    +GitObject gitObject = repository.Lookup(commitId);
+    +
+    +TreeChanges treeChanges = repository.Diff.Compare<TreeChanges>(commit.Parents.First().Tree,commit.Tree);
+    +foreach (TreeEntryChanges treeEntryChanges in treeChanges)
+    +{
+    +    var path = treeEntryChanges.Path;
+    +    var changeKind = treeEntryChanges.Status;
+    +}
+
+     HistoryDivergence historyDivergence = repository.ObjectDatabase.CalculateHistoryDivergence(queryableCommitLog.Skip(100).First(), commit);
+     var historyDivergenceCommonAncestor = historyDivergence.CommonAncestor;
+
+    -
+     GC.KeepAlive(repository);
+     Console.WriteLine("Hello, World!");
+
+     */
+}
 
 TreeChanges treeChanges = repository.Diff.Compare<TreeChanges>(commit.Parents.First().Tree,commit.Tree);
 foreach (TreeEntryChanges treeEntryChanges in treeChanges)
@@ -23,6 +63,7 @@ foreach (TreeEntryChanges treeEntryChanges in treeChanges)
     var path = treeEntryChanges.Path;
     var changeKind = treeEntryChanges.Status;
 }
+
 
 HistoryDivergence historyDivergence = repository.ObjectDatabase.CalculateHistoryDivergence(queryableCommitLog.Skip(100).First(), commit);
 var historyDivergenceCommonAncestor = historyDivergence.CommonAncestor;
