db1c216043bf9f4a4905c6558a25cec4ad311617
lindexi <lindexi_gd@163.com>
2025-02-04 11:19:42 +0800

    添加更多测试

--- Workbench/JerraicunechaijearwheaKereqeanalawall/Program.cs
+++ Workbench/JerraicunechaijearwheaKereqeanalawall/Program.cs
@@ -3,6 +3,25 @@
 var t = int.MinValue;
 var n = t * -1;
 
+var f1 = 1 << 31;
+
+string m = "";
+
+n = -1;
+n = t;
+n = 0;
+for (int i = 0; i < 32; i++)
+{
+    if ((n & (1 << (31 - i))) == 1)
+    {
+        m += "1";
+    }
+    else
+    {
+        m += "0";
+    }
+}
+
 Math.Abs(int.MinValue);
 
 Console.WriteLine("Hello, World!");
