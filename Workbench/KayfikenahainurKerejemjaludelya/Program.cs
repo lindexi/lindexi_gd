fd3e128bed66d150dc3082aed70255f1a8a00fd9
lindexi <lindexi_gd@163.com>
2025-02-11 09:28:58 +0800

    测试不同的转换逻辑

---
+++ Workbench/KayfikenahainurKerejemjaludelya/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Dictionary<string, int> result = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+for (int i = 0; i < 10; i++)
+{
+    result[((char) (('a' + i))).ToString()] = i;
+}
+
+Dictionary<string, int>.AlternateLookup<ReadOnlySpan<char>> alternateLookup = result.GetAlternateLookup<ReadOnlySpan<char>>();
+
+for (int i = 0; i < 10; i++)
+{
+    if (alternateLookup.TryGetValue(((char) (('a' + i))).ToString().AsSpan(),out var value))
+    {
+        
+    }
+}
+
+var lookup = result.GetAlternateLookup<char[]>();
+
+Console.WriteLine("Hello, World!");
