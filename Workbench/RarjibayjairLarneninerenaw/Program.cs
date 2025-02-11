53686f2803180f059783bbcc50902cd5ed6687df
lindexi <lindexi_gd@163.com>
2025-02-11 11:09:52 +0800

    返回有重复的列表

---
+++ Workbench/RarjibayjairLarneninerenaw/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var choices = "0123456789abcdef";
+Span<char> dest = stackalloc char[choices.Length];
+Random.Shared.GetItems(choices, dest);
+
+foreach (var t in dest)
+{
+    Console.Write(t);
+}
+
+Console.WriteLine("Hello, World!");
