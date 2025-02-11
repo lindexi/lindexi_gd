539ae4d981ef2b3619afac8fcca5c9f630428ca0
lindexi <lindexi_gd@163.com>
2025-02-11 15:38:35 +0800

    似乎是没有必要的定义

---
+++ Workbench/YigeawhalkuNemchekileane/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+TextWriter writer = new C();
+Console.WriteLine("Hello, World!");
+
+class C:TextWriter
+{
+    public override Encoding Encoding { get; }
+
+    [AllowNull] public override string NewLine { get; set; }
+}
