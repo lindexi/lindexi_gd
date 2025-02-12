a8acf2817cbbded6106a905b6640637b7ab7f150
lindexi <lindexi_gd@163.com>
2025-02-12 10:57:45 +0800

    继续添加全选方法

---
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/API/Extensions_/TextEditorSelectionExtension.cs
@@ -0,0 +1,25 @@
+using LightTextEditorPlus.Core;
+
+namespace LightTextEditorPlus;
+
+/// <summary>
+/// 文本选择扩展方法
+/// </summary>
+public static class TextEditorSelectionExtension
+{
+    /// <summary>
+    /// 全选文本
+    /// </summary>
+    public static void SelectAll(this TextEditor textEditor)
+    {
+        textEditor.TextEditorCore.SelectAll();
+    }
+
+    /// <summary>
+    /// 清空选择
+    /// </summary>
+    public static void ClearSelection(this TextEditor textEditor)
+    {
+        textEditor.TextEditorCore.SelectAll();
+    }
+}
