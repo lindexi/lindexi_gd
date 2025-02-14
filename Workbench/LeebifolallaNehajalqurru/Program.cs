c8e2be1911f1f48f76f72f1b9309128546e7690d
lindexi <lindexi_gd@163.com>
2025-02-14 11:30:35 +0800

    测试连接方式

---
+++ Workbench/LeebifolallaNehajalqurru/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//UriParser.Register(new Foo(),"file",0);
+
+using System.IO.Pipes;
+
+var uri = new Uri(@"file://./pipe/");
+Console.WriteLine("Hello, World!");
+
+var namedPipeClientStream = new NamedPipeClientStream(@"\\.\pipe\xx");
+
+
+class Foo : UriParser
+{
+    protected override string GetComponents(Uri uri, UriComponents components, UriFormat format)
+    {
+        return base.GetComponents(uri, components, format);
+    }
+}
