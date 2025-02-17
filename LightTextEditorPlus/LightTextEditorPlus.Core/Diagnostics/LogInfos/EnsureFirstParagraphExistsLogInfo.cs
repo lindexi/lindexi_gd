bc2707d94f2a617bad78708415095e8dc56b1f6c
lindexi <lindexi_gd@163.com>
2025-02-17 15:59:21 +0800

    添加必要的日志记录信息

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Diagnostics/LogInfos/EnsureFirstParagraphExistsLogInfo.cs
@@ -0,0 +1,9 @@
+namespace LightTextEditorPlus.Diagnostics.LogInfos;
+
+/// <summary>
+/// 确保第一个段落存在的日志信息
+/// </summary>
+public readonly record struct EnsureFirstParagraphExistsLogInfo
+{
+    // 没有内容，这只是一个标记
+}
