09ab6bb69ed76583c09f35d5ad396ee21fc74123
lindexi <lindexi_gd@163.com>
2025-02-18 11:56:52 +0800

    测试能够符合预期的存放控件逻辑

---
+++ AvaloniaIDemo/WiwokurharWhajebeki/WiwokurharWhajebeki/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+namespace WiwokurharWhajebeki.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ AvaloniaIDemo/WiwokurharWhajebeki/WiwokurharWhajebeki/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WiwokurharWhajebeki.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
