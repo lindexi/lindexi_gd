507a0521cc046a9ee75b30086eec2984f88406bc
lindexi <lindexi_gd@163.com>
2025-02-20 19:31:19 +0800

    测试解析逻辑

--- Workbench/RalbofokereleefeHarbeljarlal/Program.cs
+++ Workbench/RalbofokereleefeHarbeljarlal/Program.cs
@@ -2,15 +2,16 @@
 
 using System.Runtime.CompilerServices;
 
-var t = new F1(1, 2, 3);
+var t = 10;
 Foo(t);
 
 Console.WriteLine(t);
 
-void Foo(in F1 x)
+void Foo<T>(in T x) where T : ISpanParsable<T>
 {
     ref var refX = ref Unsafe.AsRef(in x);
-    refX = new F1(10, 20, 30);
+    //refX = new F1(10, 20, 30);
+    refX = T.Parse("2", null);
 }
 
 readonly record struct F1(int N1, int N2, int N3);
