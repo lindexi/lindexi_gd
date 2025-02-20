e04a11b099c78db2587a6aa96b522d5d5b88c140
lindexi <lindexi_gd@163.com>
2025-02-20 19:28:24 +0800

    明确的传递转换引用逻辑

---
+++ Workbench/RalbofokereleefeHarbeljarlal/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+var t = 1;
+Foo(t);
+
+Console.WriteLine(t);
+
+void Foo(in int x)
+{
+    ref var refX = ref Unsafe.AsRef(in x);
+    refX = 10;
+}
