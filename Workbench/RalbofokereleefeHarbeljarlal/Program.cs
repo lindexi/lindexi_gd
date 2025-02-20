fdcb25934c1c4d5bd6031508a94326144be8da76
lindexi <lindexi_gd@163.com>
2025-02-20 19:29:49 +0800

    测试结构体转换逻辑

--- Workbench/RalbofokereleefeHarbeljarlal/Program.cs
+++ Workbench/RalbofokereleefeHarbeljarlal/Program.cs
@@ -2,13 +2,15 @@
 
 using System.Runtime.CompilerServices;
 
-var t = 1;
+var t = new F1(1, 2, 3);
 Foo(t);
 
 Console.WriteLine(t);
 
-void Foo(in int x)
+void Foo(in F1 x)
 {
     ref var refX = ref Unsafe.AsRef(in x);
-    refX = 10;
+    refX = new F1(10, 20, 30);
 }
+
+readonly record struct F1(int N1, int N2, int N3);
