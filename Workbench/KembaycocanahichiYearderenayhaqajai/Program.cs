ab8c9cfea54f38282b657342ba6157531a2d36b6
lindexi <lindexi_gd@163.com>
2025-02-21 15:37:13 +0800

    测试转换为能够输出的字符

--- Workbench/KembaycocanahichiYearderenayhaqajai/Program.cs
+++ Workbench/KembaycocanahichiYearderenayhaqajai/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
 using System.Xml;
 
-var text = "Hello, World!你好世界\u0001";
+var text = "Hello, World!你好世界\u0001abc";
 
 //for (var i = 0; i < text.Length; i++)
 //{
@@ -17,4 +18,56 @@ var text = "Hello, World!你好世界\u0001";
 //    }
 //}
 
-Console.WriteLine(XmlConvert.EncodeName(text));
+Console.WriteLine(XmlSafeTextContentHelper.ToSafeXmlText(text));
+
+
+internal static class XmlSafeTextContentHelper
+{
+    public static string ToSafeXmlText(string text, char replaceChar = '_')
+    {
+        // 第一遍是跑一下，因为正常是不会有奇怪的字符的，那就进入快速分支
+        // 如果能够找到首个奇怪的字符，则进入慢分支，重新拼装替代字符
+
+        var index = -1;
+        for (var i = 0; i < text.Length; i++)
+        {
+            var c = text[i];
+            if (!XmlConvert.IsXmlChar(c))
+            {
+                index = i;
+                break;
+            }
+        }
+
+        // 不能使用 EncodeName 去跑
+        // 输入： `Hello, World!你好世界\u0001`
+        // 输出： Hello_x002C__x0020_World_x0021_你好世界_x0001_
+        //XmlConvert.EncodeName()
+
+        var canFindInvalidChar = index >= 0;
+        if (canFindInvalidChar)
+        {
+            // 慢分支，开始重新拼装字符串
+            var stringBuilder = new StringBuilder(text.Length);
+            stringBuilder.Append(text, 0, index);
+            for (var i = index; i < text.Length; i++)
+            {
+                var c = text[i];
+                if (XmlConvert.IsXmlChar(c))
+                {
+                    stringBuilder.Append(c);
+                }
+                else
+                {
+                    stringBuilder.Append(replaceChar);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+        else
+        {
+            return text;
+        }
+    }
+}
