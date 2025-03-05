ce1b8324fd9073afe089a12d7fe127613857336a
lindexi <lindexi_gd@163.com>
2025-03-05 16:59:28 +0800

    加上注释内容

--- LightTextEditorPlus/LightTextEditorPlus.Core/Layout/Context_/Platform_/EmptyParagraphLineHeightMeasureArgument.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Layout/Context_/Platform_/EmptyParagraphLineHeightMeasureArgument.cs
@@ -9,6 +9,7 @@ namespace LightTextEditorPlus.Core.Layout;
 /// <param name="ParagraphProperty">段落属性</param>
 /// <param name="ParagraphIndex">段落序号</param>
 /// <param name="ParagraphStartRunProperty"></param>
+/// <param name="UpdateLayoutContext">布局上下文信息</param>
 public readonly record struct EmptyParagraphLineHeightMeasureArgument(
     ParagraphProperty ParagraphProperty,
     ParagraphIndex ParagraphIndex,
