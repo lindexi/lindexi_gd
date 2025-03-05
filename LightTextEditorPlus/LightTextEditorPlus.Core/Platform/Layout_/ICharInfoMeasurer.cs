c1886d80915ac22f83d45f5cb7f7f770c1d0ff30
lindexi <lindexi_gd@163.com>
2025-03-05 10:57:07 +0800

    删除不需要的注释

--- LightTextEditorPlus/LightTextEditorPlus.Core/Platform/Layout_/ICharInfoMeasurer.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Platform/Layout_/ICharInfoMeasurer.cs
@@ -16,13 +16,6 @@ public interface ICharInfoMeasurer
     /// todo 考虑废弃此方法
     CharInfoMeasureResult MeasureCharInfo(in CharInfo charInfo);
 
-    ///// <summary>
-    ///// 测量字符信息。无论当前字符的尺寸是否测量过了，都会调入此方法。在此方法里面，可以超量测量，可以测量超过当前字符的其他字符的信息，可以一次性测量多个字符的信息
-    ///// </summary>
-    ///// <param name="argument"></param>
-    ///// <returns></returns>
-    //CharInfoMeasureResult MeasureCharInfo(in CharMeasureArgument argument);
-
     /// <summary>
     /// 测量和填充 Run 的尺寸信息。要求在此方法内必定完成对当前字符，即 argument.CurrentCharData 进行尺寸填充。可选进行超量测量，可以测量超过当前字符的其他字符的信息，可以一次性测量多个字符的信息
     /// </summary>
