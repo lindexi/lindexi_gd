831565ddf14c6b82502d7b03093eea873974a849
lindexi <lindexi_gd@163.com>
2025-03-12 11:04:44 +0800

    经过实践依然会装箱

---
+++ Workbench/JiyifelqoqeljoJejalyemjal/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+var foo = new Foo();
+foreach (var item in foo)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Hello, World!");
+
+
+class Foo : IReadOnlyList<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new S();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public int Count => 1;
+
+    public int this[int index]
+    {
+        get => throw new NotImplementedException();
+    }
+
+    struct S : IEnumerator<int>
+    {
+        private int _current;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            _current++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IEnumerator<int>.Current => _current;
+
+        object? IEnumerator.Current => _current;
+    }
+}
