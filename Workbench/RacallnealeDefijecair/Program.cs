5c1eaa802a757dd6403ea492cb7321fc52fad293
lindexi <lindexi_gd@163.com>
2025-03-25 11:34:48 +0800

    Revert "测试空转换"
    This reverts commit 7ff65acc6adc3b07b5aba82792166aa89c5566f8.

--- Workbench/RacallnealeDefijecair/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Globalization;
-
-string? t = null;
-var success = decimal.TryParse(t,NumberStyles.Any,null,out var v);
-
-Console.WriteLine("Hello, World!");
