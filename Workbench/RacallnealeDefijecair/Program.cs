7ff65acc6adc3b07b5aba82792166aa89c5566f8
lindexi <lindexi_gd@163.com>
2025-03-25 11:13:03 +0800

    测试空转换

---
+++ Workbench/RacallnealeDefijecair/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+string? t = null;
+var success = decimal.TryParse(t,NumberStyles.Any,null,out var v);
+
+Console.WriteLine("Hello, World!");
