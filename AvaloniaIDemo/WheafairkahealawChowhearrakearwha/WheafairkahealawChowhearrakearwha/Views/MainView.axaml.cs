da21241511b1b7b7834bb5bd2844ece9e9e96ee3
lindexi <lindexi_gd@163.com>
2025-03-31 20:12:12 +0800

    模糊背景动效

---
+++ AvaloniaIDemo/WheafairkahealawChowhearrakearwha/WheafairkahealawChowhearrakearwha/Views/MainView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Input.TextInput;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+
+namespace WheafairkahealawChowhearrakearwha.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+}
