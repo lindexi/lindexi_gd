bd60f4b0b9fe54ba06acbc3d3b005bddd81c30b5
lindexi <lindexi_gd@163.com>
2025-04-01 15:29:40 +0800

    完成基础覆盖模式

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/DocumentManager_/Extensions_/TextEditorSelectionExtension.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/DocumentManager_/Extensions_/TextEditorSelectionExtension.cs
@@ -1,5 +1,7 @@
+using System;
 using LightTextEditorPlus.Core.Attributes;
 using LightTextEditorPlus.Core.Carets;
+using LightTextEditorPlus.Core.Document.Segments;
 
 namespace LightTextEditorPlus.Core.Document;
 
@@ -50,4 +52,32 @@ public static class TextEditorSelectionExtension
     [TextEditorPublicAPI]
     public static Selection GetAllDocumentSelection(this TextEditorCore textEditor) =>
         textEditor.DocumentManager.GetAllDocumentSelection();
+
+    /// <summary>
+    /// 获取覆盖模式下的选择范围
+    /// </summary>
+    /// <param name="textEditor"></param>
+    /// <param name="inputTextLength"></param>
+    /// <returns></returns>
+    public static Selection GetCurrentOvertypeModeSelection(this TextEditorCore textEditor, int inputTextLength)
+    {
+        Selection currentSelection = textEditor.CurrentSelection;
+        if (currentSelection.IsEmpty)
+        {
+            // 只有在无选择的情况下才能使用覆盖模式
+            // 先获取所在的段落，取当前的输入文本长度和光标到段落的末尾的最小值
+            var caretOffset = currentSelection.FrontOffset;
+            HitParagraphDataResult hitParagraphDataResult = textEditor.DocumentManager.ParagraphManager.GetHitParagraphData(caretOffset);
+            // 命中到的段落偏移量
+            ParagraphCaretOffset hitOffset = hitParagraphDataResult.HitOffset;
+            int charCount = hitParagraphDataResult.ParagraphData.CharCount;
+            int min = Math.Min(inputTextLength, charCount - hitOffset.Offset);
+            return new Selection(caretOffset, min);
+        }
+        else
+        {
+            // 有选择的情况下，如果选择范围和输入文本长度不一致，那就不是覆盖模式
+            return currentSelection;
+        }
+    }
 }
