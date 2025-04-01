833d63fe117924e4698aa38ff1ca33f1ff1825b6
lindexi <lindexi_gd@163.com>
2025-04-01 10:22:00 +0800

    拆分代码

--- LightTextEditorPlus/LightTextEditorPlus.Core/Editing/Features_/TextFeatures.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Editing/Features_/TextFeatures.cs
@@ -6,24 +6,9 @@ using System.Threading.Tasks;
 
 namespace LightTextEditorPlus.Core.Editing;
 
-public static class TextFeaturesExtension
-{
-    public static TextFeatures EnableFeatures(this TextFeatures currentFeatures, TextFeatures features)
-    {
-        return currentFeatures | features;
-    }
-
-    public static TextFeatures DisableFeatures(this TextFeatures currentFeatures, TextFeatures features)
-    {
-        return currentFeatures & ~features;
-    }
-
-    public static bool IsFeaturesEnable(this TextFeatures currentFeatures, TextFeatures features)
-    {
-        return (currentFeatures & features) == features;
-    }
-}
-
+/// <summary>
+/// 功能特性
+/// </summary>
 [Flags]
 public enum TextFeatures : long
 {
---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Editing/Features_/TextFeaturesExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace LightTextEditorPlus.Core.Editing;
+
+public static class TextFeaturesExtension
+{
+    public static TextFeatures EnableFeatures(this TextFeatures currentFeatures, TextFeatures features)
+    {
+        return currentFeatures | features;
+    }
+
+    public static TextFeatures DisableFeatures(this TextFeatures currentFeatures, TextFeatures features)
+    {
+        return currentFeatures & ~features;
+    }
+
+    public static bool IsFeaturesEnable(this TextFeatures currentFeatures, TextFeatures features)
+    {
+        return (currentFeatures & features) == features;
+    }
+}
