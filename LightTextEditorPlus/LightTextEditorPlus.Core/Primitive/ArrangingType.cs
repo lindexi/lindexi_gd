70a17c7380f4ad76c8c96d1ecafb2f6f732b5b56
lindexi <lindexi_gd@163.com>
2025-04-07 17:37:42 +0800

    定义更加复杂的排版规则，因为竖排比较复杂

--- LightTextEditorPlus/LightTextEditorPlus.Core/Primitive/ArrangingType.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Primitive/ArrangingType.cs
@@ -18,7 +18,45 @@ public enum ArrangingType
     Vertical,
 
     /// <summary>
-    ///     蒙古文布局
+    ///     蒙古文布局，从右到左
     /// </summary>
     Mongolian
 }
+
+public class TextArrangingType
+{
+    public static TextArrangingType Horizontal => new TextArrangingType();
+    public static TextArrangingType Vertical => new TextArrangingType
+    {
+        IsVertical = true,
+        IsLeftToRightVertical = true,
+    };
+
+    public static TextArrangingType Mongolian => new TextArrangingType
+    {
+        IsVertical = true,
+        // 蒙古文布局，从右到左
+        IsLeftToRightVertical = false,
+    };
+
+    private TextArrangingType()
+    {
+    }
+
+    public bool IsHorizontal => !IsVertical;
+
+    public bool IsVertical { get; private init; }
+
+    public bool IsLeftToRightVertical { get; private init; }
+
+    /// <summary>
+    /// 拉丁文字是否需要旋转，旋转角度为是多少，都是90度倍数为主
+    /// </summary>
+    public int LatinRotationDegree { get; private init; }
+
+    /// <summary>
+    /// 数字是否需要旋转
+    /// </summary>
+    public bool NumberRotationDegree { get; private init; }
+}
+
