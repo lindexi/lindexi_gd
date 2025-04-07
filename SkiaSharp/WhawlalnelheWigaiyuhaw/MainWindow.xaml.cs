f0a1b437b16e0c24265a61df16bf6e8de65c96fc
lindexi <lindexi_gd@163.com>
2025-04-07 11:12:13 +0800

    全零没影响

--- SkiaSharp/WhawlalnelheWigaiyuhaw/MainWindow.xaml.cs
+++ SkiaSharp/WhawlalnelheWigaiyuhaw/MainWindow.xaml.cs
@@ -93,7 +93,7 @@ public class SkiaCanvas : FrameworkElement
             var skRotationScaleMatrixList = new SKRotationScaleMatrix[text.Length];
             for (int i = 0; i < text.Length; i++)
             {
-                var skRotationScaleMatrix = SKRotationScaleMatrix.Create(1, float.Pi / 2, 0, textHeight * i, _ax, _ay);
+                var skRotationScaleMatrix = SKRotationScaleMatrix.Create(1, float.Pi / 2, 0, 0, _ax, _ay);
 
                 skRotationScaleMatrixList[i] = skRotationScaleMatrix;
             }
