cd0625042a9140d6dd4601fecfb302f6f8627f61
lindexi <lindexi_gd@163.com>
2025-04-09 11:14:28 +0800

    测试覆盖模式内容

---
+++ WPFDemo/KeneharcerJarkukallwairgai/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeneharcerJarkukallwairgai;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        var args = new List<string>();
+        Foo foo = new Foo();
+        var f1 = foo.F1;
+        if (args[0] == "")
+        {
+            f1 = args[0];
+        }
+
+        foo = new Foo()
+        {
+            F1 = f1
+        };
+
+        InitializeComponent();
+    }
+}
+
+class Foo
+{
+    public object F1 { get; init; } = "asd";
+}
