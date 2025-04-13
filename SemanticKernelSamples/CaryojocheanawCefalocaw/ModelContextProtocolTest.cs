3ce050875f31936222e2bf393198ae8b71ca6298
lindexi <lindexi_gd@163.com>
2025-04-13 10:44:59 +0800

    测试服务器启动但是卡住

---
+++ SemanticKernelSamples/CaryojocheanawCefalocaw/ModelContextProtocolTest.cs
@@ -0,0 +1,16 @@
+﻿using MCPSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaryojocheanawCefalocaw;
+internal class ModelContextProtocolTest
+{
+    public  async Task RunAsync()
+    {
+        await MCPServer.StartAsync("CalculatorServer", "1.0.0");
+    }
+}
---
+++ SemanticKernelSamples/CaryojocheanawCefalocaw/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CaryojocheanawCefalocaw;
+
+await new ModelContextProtocolTest().RunAsync();
+Console.WriteLine("Hello, World!");
