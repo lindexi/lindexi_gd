8ff20e52029e4f5137d718968a96c9fd09a8e9e8
lindexi <lindexi_gd@163.com>
2025-04-13 11:20:04 +0800

    测试调用结果

--- SemanticKernelSamples/GarwawricainaineKalkuqujee/Program.cs
+++ SemanticKernelSamples/GarwawricainaineKalkuqujee/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using MCPSharp;
+using MCPSharp.Model.Results;
+
 using Microsoft.Extensions.AI;
 
 MCPClient client = new MCPClient("AIClient", "1.0",
@@ -9,5 +11,21 @@ IList<AIFunction> functions = await client.GetFunctionsAsync();
 
 foreach (AIFunction function in functions)
 {
+    if (function.Name == "add")
+    {
+        var result = await function.InvokeAsync(
+        [
+            new KeyValuePair<string, object?>("a", 1),
+            new KeyValuePair<string, object?>("b", 2)
+        ]);
 
+        if (result is CallToolResult callToolResult)
+        {
+            var text = callToolResult.Content[0].Text;
+            if (int.TryParse(text, out var n))
+            {
+
+            }
+        }
+    }
 }
