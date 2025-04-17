25d5e60822d0e6b8036856b9e6e2c1329b1e5f54
lindexi <lindexi_gd@163.com>
2025-04-17 11:32:42 +0800

    存放图片到本地文件

---
+++ AvaloniaIDemo/RerneyawyayWilalawha/RerneyawyayWilalawha/Views/MainView.axaml.cs
@@ -0,0 +1,37 @@
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
+
+using System;
+using Avalonia;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Rendering.Composition;
+
+namespace RerneyawyayWilalawha.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+        Loaded += MainView_Loaded;
+    }
+
+    private void MainView_Loaded(object? sender, RoutedEventArgs e)
+    {
+        if (this.TryGetResource("Image.Startup", out var source) && source is DrawingImage drawingImage)
+        {
+            var imageSize = drawingImage.Size;
+            using var renderTargetBitmap = new RenderTargetBitmap(new PixelSize((int) imageSize.Width, (int) imageSize.Height));
+
+            using (var drawingContext = renderTargetBitmap.CreateDrawingContext())
+            {
+                drawingContext.DrawImage(drawingImage, new Rect(new Point(), imageSize));
+            }
+           
+            renderTargetBitmap.Save("1.png");
+        }
+    }
+}
