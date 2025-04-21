3f1467ef6793dce0ac22dfee0977a27f3d71eada
lindexi <lindexi_gd@163.com>
2025-04-21 09:18:13 +0800

    尝试跑出更快的命令解析

---
+++ Workbench/NaiyafalyiyarHarqoqallcairchear/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// -value:test --foo:test /fx1:test -number:42 /flag:true -PascalCase:value --camelCase:value --kebab-case:value --flag1 --flag2:false --v1 v1 -value2 v2 /value3 v3 --valueue=testue -c d1 -e:f2 -g:ho
+using NaiyafalyiyarHarqoqallcairchear;
+
+string[] argument = 
+[
+    "-value:test",
+    "--foo:test",
+    "/fx1:test",
+    "-number:42",
+    "/flag:true",
+    "-PascalCase:value",
+    "--camelCase:value",
+    "--kebab-case:value",
+    "--flag1",
+    "--flag2:false",
+    "--v1",
+    "v1",
+    "-value2",
+    "v2",
+    "/value3",
+    "v3",
+    "--valueue:testue",
+    "-c",
+    "d1",
+    "-e:f2",
+    "-g:ho"
+];
+
+_ = LindexiCommandLine.Parse<FlexibleOption>(argument);
+
+Console.WriteLine("Hello, World!");
