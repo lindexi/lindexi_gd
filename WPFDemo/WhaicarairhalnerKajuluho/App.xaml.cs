8c46e8f81be31493016a3d35fccde479c8bf2656
lindexi <lindexi_gd@163.com>
2025-04-23 09:54:47 +0800

    减少多语言文件夹

---
+++ WPFDemo/WhaicarairhalnerKajuluho/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WhaicarairhalnerKajuluho;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
