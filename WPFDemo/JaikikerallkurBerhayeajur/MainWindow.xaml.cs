b4e83451ab8bcf51c1ca3de4630f19392600305c
lindexi <lindexi_gd@163.com>
2025-04-24 11:29:31 +0800

    换个库进行对接实现对接逻辑

---
+++ WPFDemo/JaikikerallkurBerhayeajur/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32.SafeHandles;
+using Win32.Graphics.Imaging;
+
+namespace JaikikerallkurBerhayeajur;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var width = (int) 100;
+        var height = (int) 100;
+
+        var writeableBitmap =
+            new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
+        Image.Source = writeableBitmap;
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var writeableBitmap = (WriteableBitmap) Image.Source;
+        var type = writeableBitmap.GetType();
+        var propertyInfo = type.GetProperty("WicSourceHandle", BindingFlags.NonPublic | BindingFlags.Instance);
+        var value = propertyInfo?.GetValue(writeableBitmap);
+        nint wicBitmapHandle = 0;
+        if (value is SafeHandleZeroOrMinusOneIsInvalid safeHandle)
+        {
+            var handle = safeHandle.DangerousGetHandle();
+            wicBitmapHandle = handle;
+            var wicBitmap = new IWICBitmap();
+            unsafe
+            {
+                wicBitmap.lpVtbl = (void**)*(void***)wicBitmapHandle;
+
+                uint w = 0, h = 0;
+                wicBitmap.GetSize(&w, &h);
+            }
+
+            //var wicBitmap = (IWICBitmap) Marshal.GetObjectForIUnknown(handle);
+
+            //var size = wicBitmap.GetSize();
+            //var buffer = new byte[size.Width * size.Height * 4];
+            //Random.Shared.NextBytes(buffer);
+
+            //// 这里的绘制是无效的，因为在 WPF 底层会重新被 m_pFrontBuffer 覆盖
+            //wicBitmap.CopyPixels(4 * size.Width, buffer);
+        }
+    }
+}
