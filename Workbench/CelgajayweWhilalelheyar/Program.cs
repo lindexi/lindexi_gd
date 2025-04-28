c9c78d0d4a51ffe1629d517b57aad3d81a19e049
lindexi <lindexi_gd@163.com>
2025-04-28 17:27:12 +0800

    什么东西都没有测试获取到

---
+++ Workbench/CelgajayweWhilalelheyar/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Device.Net;
+using Device.Net.Windows;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+ILoggerFactory loggerFactory = new NullLoggerFactory();
+Guid GUID_DEVCLASS_MONITOR =
+new Guid(0x4d36e96e, 0xe325, 0x11ce, 0xbf, 0xc1, 0x08, 0x00, 0x2b, 0xe1, 0x03, 0x18);
+
+var windowsDeviceEnumerator = new WindowsDeviceEnumerator(new Logger<WindowsDeviceEnumerator>(loggerFactory), GUID_DEVCLASS_MONITOR,
+    (id, guid) =>
+    {
+        return default;
+    }, definition =>
+    {
+        return default;
+    });
+
+foreach (ConnectedDeviceDefinition connectedDeviceDefinition in await windowsDeviceEnumerator.GetConnectedDeviceDefinitionsAsync())
+{
+    
+}
+
+Console.WriteLine("Hello, World!");
