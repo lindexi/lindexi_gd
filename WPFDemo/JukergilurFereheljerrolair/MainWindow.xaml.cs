34592e22f28ccdcc3cc79a2e9089310d09e2526a
lindexi <lindexi_gd@163.com>
2025-04-29 10:53:24 +0800

    测试无限次数调用显示窗口

---
+++ WPFDemo/JukergilurFereheljerrolair/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JukergilurFereheljerrolair;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        while (true)
+        {
+            await Task.Delay(100);
+            this.Show();
+        }
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        this.Hide();
+    }
+}
