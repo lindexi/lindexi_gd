b5211d974383a52ba7ed37440b08619f56adeb77
lindexi <lindexi_gd@163.com>
2025-04-30 10:08:02 +0800

    创建新项目

---
+++ AvaloniaIDemo/GelwhalhahonelGilerewalfee/GelwhalhahonelGilerewalfee/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GelwhalhahonelGilerewalfee.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
