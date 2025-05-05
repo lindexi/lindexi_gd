22f440323282a0b5bd93063b8955db0ab46b33e9
lindexi <lindexi_gd@163.com>
2025-05-05 15:48:51 +0800

    测试序列化的属性名

---
+++ Workbench/HairfekarqaluwhuHabarwhairku/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using dotnetCampus.Ipc.IpcRouteds.DirectRouteds;
+using Newtonsoft.Json;
+
+var response = new JsonIpcDirectRoutedHandleRequestExceptionResponse()
+{
+    ExceptionInfo = new JsonIpcDirectRoutedHandleRequestExceptionInfo()
+    {
+        ExceptionType = "Foo"
+    }
+};
+
+string jsonText = JsonConvert.SerializeObject(response);
+
+Console.WriteLine(jsonText); // {"__$Exception":{"ExceptionType":"Foo"}}
+
+JsonIpcDirectRoutedHandleRequestExceptionResponse? jsonIpcDirectRoutedHandleRequestExceptionResponse = JsonConvert.DeserializeObject<JsonIpcDirectRoutedHandleRequestExceptionResponse>(jsonText);
+
+Console.WriteLine(jsonIpcDirectRoutedHandleRequestExceptionResponse?.ExceptionInfo?.ExceptionType); // Foo
