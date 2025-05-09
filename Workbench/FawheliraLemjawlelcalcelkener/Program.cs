29ca9e97eab11720d0ad402818092271132f5063
lindexi <lindexi_gd@163.com>
2025-05-09 15:39:54 +0800

    准备搭建项目

---
+++ Workbench/FawheliraLemjawlelcalcelkener/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Collections;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection
+    .AddMcpServer();
+
+IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+Console.WriteLine("Hello, World!");
+
+class SimpleServiceCollection : IServiceCollection
+{
+    public IEnumerator<ServiceDescriptor> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(ServiceDescriptor item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(ServiceDescriptor item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(ServiceDescriptor[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(ServiceDescriptor item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count { get; set; }
+    public bool IsReadOnly { get; set; }
+    public int IndexOf(ServiceDescriptor item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(int index, ServiceDescriptor item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ServiceDescriptor this[int index]
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+}
