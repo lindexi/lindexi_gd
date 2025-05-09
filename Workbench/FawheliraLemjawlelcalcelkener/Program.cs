056e45868a9fade07cda1645b72003fe34566837
lindexi <lindexi_gd@163.com>
2025-05-09 16:41:35 +0800

    进程内的调用逻辑

--- Workbench/FawheliraLemjawlelcalcelkener/Program.cs
+++ Workbench/FawheliraLemjawlelcalcelkener/Program.cs
@@ -1,75 +1,151 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text.Json;
+using System.Threading.Channels;
+using ModelContextProtocol;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol.Messages;
+using ModelContextProtocol.Protocol.Transport;
+using ModelContextProtocol.Protocol.Types;
+using ModelContextProtocol.Server;
 
-using System.Collections;
-using Microsoft.Extensions.DependencyInjection;
+var sameProcessTransportFactory = new SameProcessTransportFactory();
 
-var serviceCollection = new ServiceCollection();
-
-serviceCollection
-    .AddMcpServer();
-
-IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
-
-Console.WriteLine("Hello, World!");
-
-class SimpleServiceCollection : IServiceCollection
+McpServerOptions options = new()
 {
-    public IEnumerator<ServiceDescriptor> GetEnumerator()
+    ServerInfo = new Implementation() { Name = "MyServer", Version = "1.0.0" },
+    Capabilities = new ServerCapabilities()
     {
-        throw new NotImplementedException();
+        Tools = new ToolsCapability()
+        {
+            ListToolsHandler = (request, cancellationToken) =>
+                ValueTask.FromResult(new ListToolsResult()
+                {
+                    Tools =
+                    [
+                        new Tool()
+                        {
+                            Name = "echo",
+                            Description = "Echoes the input back to the client.",
+                            InputSchema = JsonSerializer.Deserialize<JsonElement>("""
+                                {
+                                    "type": "object",
+                                    "properties": {
+                                      "message": {
+                                        "type": "string",
+                                        "description": "The input to echo back"
+                                      }
+                                    },
+                                    "required": ["message"]
+                                }
+                                """),
+                        }
+                    ]
+                }),
+
+            CallToolHandler = (request, cancellationToken) =>
+            {
+                if (request.Params?.Name == "echo")
+                {
+                    if (request.Params.Arguments?.TryGetValue("message", out var message) is not true)
+                    {
+                        throw new McpException("Missing required argument 'message'");
+                    }
+
+                    return ValueTask.FromResult(new CallToolResponse()
+                    {
+                        Content = [new Content() { Text = $"Echo: {message}", Type = "text" }]
+                    });
+                }
+
+                throw new McpException($"Unknown tool: '{request.Params?.Name}'");
+            },
+        }
+    },
+};
+
+await using IMcpServer server = McpServerFactory.Create(sameProcessTransportFactory.GetServerTransport(), options);
+_ = server.RunAsync();
+
+// 以下是客户端代码
+var client = await McpClientFactory.CreateAsync(sameProcessTransportFactory.GetClientTransport());
+
+// Print the list of tools available from the server.
+foreach (var tool in await client.ListToolsAsync())
+{
+    Console.WriteLine($"{tool.Name} ({tool.Description})");
+}
+
+var dictionary = new Dictionary<string, object?>()
+{
+    { "message", "Hello, World!" }
+};
+CallToolResponse callToolResponse = await client.CallToolAsync("echo", dictionary);
+foreach (var content in callToolResponse.Content)
+{
+    Console.WriteLine($"CallToolResponse: Type={content.Type} Text='{content.Text}'");
+}
+
+class SameProcessTransportFactory
+{
+    public SameProcessTransportFactory()
+    {
+        _clientTransport = new ClientTransport(this);
+        _serverTransport = new ServerTransport(this);
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
+    private readonly ClientTransport _clientTransport;
+    private readonly ServerTransport _serverTransport;
+
+    public IClientTransport GetClientTransport()
+        => _clientTransport;
+
+    public ITransport GetServerTransport()
+        => _serverTransport;
+
+    class ClientTransport(SameProcessTransportFactory factory) : TransportBase, IClientTransport, ITransport
     {
-        return GetEnumerator();
+        private readonly SameProcessTransportFactory _factory = factory;
+
+        public Task<ITransport> ConnectAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return Task.FromResult<ITransport>(this);
+        }
+
+        public string Name => "ClientTransport";
+
+        public override async Task SendMessageAsync(JsonRpcMessage message,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            await _factory._serverTransport.Channel.Writer.WriteAsync(message, cancellationToken);
+        }
     }
 
-    public void Add(ServiceDescriptor item)
+    class ServerTransport(SameProcessTransportFactory factory) : TransportBase
     {
-        throw new NotImplementedException();
+        private readonly SameProcessTransportFactory _factory = factory;
+
+        public override async Task SendMessageAsync(JsonRpcMessage message,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            await _factory._clientTransport.Channel.Writer.WriteAsync(message, cancellationToken);
+        }
     }
 
-    public void Clear()
+    abstract class TransportBase : ITransport
     {
-        throw new NotImplementedException();
-    }
+        public Channel<JsonRpcMessage> Channel { get; } =
+            System.Threading.Channels.Channel.CreateUnbounded<JsonRpcMessage>();
 
-    public bool Contains(ServiceDescriptor item)
-    {
-        throw new NotImplementedException();
-    }
+        public ValueTask DisposeAsync()
+        {
+            Channel.Writer.Complete();
+            return ValueTask.CompletedTask;
+        }
 
-    public void CopyTo(ServiceDescriptor[] array, int arrayIndex)
-    {
-        throw new NotImplementedException();
-    }
+        public abstract Task SendMessageAsync(JsonRpcMessage message,
+            CancellationToken cancellationToken = new CancellationToken());
 
-    public bool Remove(ServiceDescriptor item)
-    {
-        throw new NotImplementedException();
-    }
-
-    public int Count { get; set; }
-    public bool IsReadOnly { get; set; }
-    public int IndexOf(ServiceDescriptor item)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Insert(int index, ServiceDescriptor item)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void RemoveAt(int index)
-    {
-        throw new NotImplementedException();
-    }
-
-    public ServiceDescriptor this[int index]
-    {
-        get => throw new NotImplementedException();
-        set => throw new NotImplementedException();
+        public ChannelReader<JsonRpcMessage> MessageReader => Channel.Reader;
     }
 }
