ab04fb037b01999c4036d9f0cbdc29017e0dbebf
lindexi <lindexi_gd@163.com>
2025-05-09 14:08:07 +0800

    重新翻新项目

--- Workbench/WatchDog/WatchDog.Uno/WatchDog.Uno/GlobalUsings.cs
+++ Workbench/WatchDog/WatchDog.Uno/WatchDog.Uno/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using System.Collections.Immutable;
+global using System.Collections.Immutable;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
 global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
---
+++ Workbench/WatchDog/WatchDog.Uno/WatchDog.Uno/Platforms/WebAssembly/WasmScripts/AppManifest.js
@@ -0,0 +1,3 @@
+var UnoAppManifest = {
+    displayName: "WatchDog.Uno"
+}
