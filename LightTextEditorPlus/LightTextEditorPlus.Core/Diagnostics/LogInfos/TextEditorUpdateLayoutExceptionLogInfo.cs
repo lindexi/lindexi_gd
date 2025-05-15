b22239da52285785e9536b17df2eaf24c36f5abb
lindexi <lindexi_gd@163.com>
2025-05-15 20:56:25 +0800

    添加注释更新异常内容

--- LightTextEditorPlus/LightTextEditorPlus.Core/Diagnostics/LogInfos/TextEditorUpdateLayoutExceptionLogInfo.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Diagnostics/LogInfos/TextEditorUpdateLayoutExceptionLogInfo.cs
@@ -2,4 +2,8 @@
 
 namespace LightTextEditorPlus.Core.Diagnostics.LogInfos;
 
+/// <summary>
+/// 更新布局过程中出现异常的日志信息
+/// </summary>
+/// <param name="UpdateLayoutException"></param>
 public readonly record struct TextEditorUpdateLayoutExceptionLogInfo(Exception UpdateLayoutException);
