05b81828b66cb8e27091bd0061673b06433bdaea
lindexi <lindexi_gd@163.com>
2025-05-15 20:46:34 +0800

    压入更多日志信息

---
+++ LightTextEditorPlus/Build/Shared/API/Diagnostics/LogInfos/ForceLayoutContinuousExceptionLogInfo.Shared.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LightTextEditorPlus.Core.Diagnostics.LogInfos;
+
+/// <summary>
+/// 文本布局过程中连续出现异常
+/// </summary>
+public readonly record struct ForceLayoutContinuousExceptionLogInfo(Exception CurrentException);
