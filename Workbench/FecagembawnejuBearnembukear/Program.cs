9d1e0f52472534bba136e18b59f7bd2439cef368
lindexi <lindexi_gd@163.com>
2025-05-19 10:12:45 +0800

    修复计算方法

--- Workbench/FecagembawnejuBearnembukear/Program.cs
+++ Workbench/FecagembawnejuBearnembukear/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+GetLowerLatin(1);
+
 for (int i = 0; i < 200; i++)
 {
     var lowerLatin = GetLowerLatin(i);
@@ -8,10 +11,10 @@ for (int i = 0; i < 200; i++)
 string GetLowerLatin(int num)
 {
     const int startAsciiNum = 'a'; //97
-    const int aToZCount = 'z' - 'a' + 2;// 这里的 +2 的意思是比所有的英文字符大一，因为 'z' - 'a' 没有包含一个字符，于是加先 +1 然后这里业务需要比所有英文字符大一于是就再加 1 就是如你看到的方法
+    const int aToZCount = 'z' - 'a' + 1;
     int count = num / aToZCount;
-    int index = Math.Max(num % aToZCount, 1);
+    int index = num % aToZCount;
 
-    var word = (char) (startAsciiNum + index - 1);
+    var word = (char) (startAsciiNum + index);
     return string.Concat(new string(word, count + 1), ".");
 }
