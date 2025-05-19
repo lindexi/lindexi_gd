3ee08404f00038eb2c1c642f4b95d33c06e73381
lindexi <lindexi_gd@163.com>
2025-05-19 14:33:33 +0800

    加上更多注释内容

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Layout/ReadMe.cs
@@ -0,0 +1,16 @@
+﻿using LightTextEditorPlus.Core.Document;
+using LightTextEditorPlus.Core.Layout.LayoutUtils;
+
+namespace LightTextEditorPlus.Core.Layout;
+
+/// <summary>
+/// 此文件的存在只是用来做引用记录，记录一些机制的实现
+/// </summary>
+/// 布局系统是最为复杂的部分
+///
+/// Q: 缩进是在哪处理的？
+/// A: 缩进分为上下左右方向，其中上下方向成为竖直方向，为 段前间距 和 段后间距。对应在 <see cref="ParagraphProperty.ParagraphBefore"/> 和 <see cref="ParagraphProperty.ParagraphAfter"/> 属性。直接就在 <see cref="ParagraphLayoutArgumentLayoutUtilsExtension.GetParagraphBefore"/> 和 <see cref="ParagraphLayoutArgumentLayoutUtilsExtension.GetParagraphAfter"/> 方法获取，在 <see cref="HorizontalArrangingLayoutProvider.UpdateParagraphLayoutData"/> 方法直接设置到 <see cref="ParagraphData.SetParagraphLayoutContentThickness"/> 里面
+/// 左右方向比较复杂，需要考虑项目符号的存在
+file class ReadMe
+{
+}
