8d4f14691d9a18763da028055a016fd6c8d0f42a
lindexi <lindexi_gd@163.com>
2025-05-21 11:38:24 +0800

    测试支持两次重写

---
+++ Workbench/RibeelalbuQelfadearri/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var c = new C();
+B b = c;
+
+c.Foo();
+b.Foo();
+
+Console.WriteLine("Hello, World!");
+
+abstract class A
+{
+    public abstract void Foo();
+}
+
+class B : A
+{
+    public override void Foo()
+    {
+        Console.WriteLine("B");
+    }
+}
+
+class C : B
+{
+    public override void Foo()
+    {
+        Console.WriteLine("C");
+    }
+}
