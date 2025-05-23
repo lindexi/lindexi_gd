09176cf4cd2cc77953bfdabd21cbfd910bcfdcee
lindexi <lindexi_gd@163.com>
2025-05-23 17:07:07 +0800

    对接更多平台的项目符号渲染

--- LightTextEditorPlus/LightTextEditorPlus.Skia/Rendering/Core/HorizontalSkiaTextRender.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Rendering/Core/HorizontalSkiaTextRender.cs
@@ -38,72 +38,18 @@ class HorizontalSkiaTextRender : BaseSkiaTextRender
         {
             foreach (ParagraphLineRenderInfo lineInfo in paragraphRenderInfo.GetLineRenderInfoList())
             {
-                if (lineInfo.IsFirstLine)
+                if (lineInfo.IsIncludeMarker)
                 {
-                    // todo 考虑渲染项目符号
+                    TextReadOnlyListSpan<CharData> markerCharDataList = lineInfo.GetMarkerCharDataList();
+                    RenderCharList(markerCharDataList, lineInfo);
                 }
 
                 // 先不考虑缓存
                 LineDrawingArgument argument = lineInfo.Argument;
+
                 foreach (TextReadOnlyListSpan<CharData> charList in argument.CharList.GetCharSpanContinuous())
                 {
-                    CharData firstCharData = charList[0];
-
-                    SkiaTextRunProperty skiaTextRunProperty = firstCharData.RunProperty.AsSkiaRunProperty();
-
-                    // 不需要在这里处理字体回滚，在输入的过程中已经处理过了
-                    ////  考虑字体回滚问题
-                    RenderingRunPropertyInfo renderingRunPropertyInfo = skiaTextRunProperty.GetRenderingRunPropertyInfo(firstCharData.CharObject.CodePoint);
-
-                    SKFont skFont = renderingRunPropertyInfo.Font;
-
-                    SKPaint textRenderSKPaint = renderingRunPropertyInfo.Paint;
-
-                    var runBounds = firstCharData.GetBounds();
-                    var startPoint = runBounds.LeftTop;
-
-                    float x = (float) startPoint.X;
-                    float y = (float) startPoint.Y;
-                    float width = 0;
-                    float height = (float) runBounds.Height;
-
-                    using CharDataListToCharSpanResult charSpanResult = charList.ToCharSpan();
-                    ReadOnlySpan<char> charSpan = charSpanResult.CharSpan;
-
-                    foreach (CharData charData in charList)
-                    {
-                        DrawDebugBounds(charData.GetBounds().ToSKRect(), Config.DebugDrawCharBoundsInfo);
-
-                        width += (float) charData.Size!.Value.Width;
-                    }
-
-                    SKRect charSpanBounds = SKRect.Create(x, y, width, height);
-                    DrawDebugBounds(charSpanBounds, Config.DebugDrawCharSpanBoundsInfo);
-                    renderBounds = renderBounds.Union(charSpanBounds.ToTextRect());
-
-                    if (!skFont.ContainsGlyphs(charSpan))
-                    {
-                        // 预计不会出现这样的问题，在渲染之前已经处理过了
-                        throw new TextEditorInnerException($"文本框架内应该确保进入渲染层时，不会出现字体不能包含字符的情况");
-                    }
-
-                    // 绘制四线三格的调试信息
-                    if (TextEditor.DebugConfiguration.ShowHandwritingPaperDebugInfo)
-                    {
-                        CharHandwritingPaperInfo charHandwritingPaperInfo =
-                            renderInfoProvider.GetHandwritingPaperInfo(in lineInfo, firstCharData);
-                        DrawDebugHandwritingPaper(canvas, charSpanBounds, charHandwritingPaperInfo);
-                    }
-
-                    //float x = skiaTextRenderInfo.X;
-                    //float y = skiaTextRenderInfo.Y;
-
-                    var baselineY = /*skFont.Metrics.Leading +*/ (-skFont.Metrics.Ascent);
-
-                    // 由于 Skia 的 DrawText 传入的 Point 是文本的基线，因此需要调整 Y 值
-                    y += baselineY;
-                    using SKTextBlob skTextBlob = SKTextBlob.Create(charSpan, skFont);
-                    canvas.DrawText(skTextBlob, x, y, textRenderSKPaint);
+                    RenderCharList(charList, lineInfo);
                 }
 
                 if (argument.CharList.Count == 0)
@@ -134,6 +80,67 @@ class HorizontalSkiaTextRender : BaseSkiaTextRender
         void DrawDebugBounds(SKRect bounds, TextEditorDebugBoundsDrawInfo? drawInfo)
         {
             DrawDebugBoundsInfo(canvas, bounds, drawInfo);
+        }
+
+        void RenderCharList(TextReadOnlyListSpan<CharData> charList, ParagraphLineRenderInfo lineInfo)
+        {
+            CharData firstCharData = charList[0];
+
+            SkiaTextRunProperty skiaTextRunProperty = firstCharData.RunProperty.AsSkiaRunProperty();
+
+            // 不需要在这里处理字体回滚，在输入的过程中已经处理过了
+            ////  考虑字体回滚问题
+            RenderingRunPropertyInfo renderingRunPropertyInfo = skiaTextRunProperty.GetRenderingRunPropertyInfo(firstCharData.CharObject.CodePoint);
+
+            SKFont skFont = renderingRunPropertyInfo.Font;
+
+            SKPaint textRenderSKPaint = renderingRunPropertyInfo.Paint;
+
+            var runBounds = firstCharData.GetBounds();
+            var startPoint = runBounds.LeftTop;
+
+            float x = (float) startPoint.X;
+            float y = (float) startPoint.Y;
+            float width = 0;
+            float height = (float) runBounds.Height;
+
+            using CharDataListToCharSpanResult charSpanResult = charList.ToCharSpan();
+            ReadOnlySpan<char> charSpan = charSpanResult.CharSpan;
+
+            foreach (CharData charData in charList)
+            {
+                DrawDebugBounds(charData.GetBounds().ToSKRect(), Config.DebugDrawCharBoundsInfo);
+
+                width += (float) charData.Size!.Value.Width;
+            }
+
+            SKRect charSpanBounds = SKRect.Create(x, y, width, height);
+            DrawDebugBounds(charSpanBounds, Config.DebugDrawCharSpanBoundsInfo);
+            renderBounds = renderBounds.Union(charSpanBounds.ToTextRect());
+
+            if (!skFont.ContainsGlyphs(charSpan))
+            {
+                // 预计不会出现这样的问题，在渲染之前已经处理过了
+                throw new TextEditorInnerException($"文本框架内应该确保进入渲染层时，不会出现字体不能包含字符的情况");
+            }
+
+            // 绘制四线三格的调试信息
+            if (TextEditor.DebugConfiguration.ShowHandwritingPaperDebugInfo)
+            {
+                CharHandwritingPaperInfo charHandwritingPaperInfo =
+                    renderInfoProvider.GetHandwritingPaperInfo(in lineInfo, firstCharData);
+                DrawDebugHandwritingPaper(canvas, charSpanBounds, charHandwritingPaperInfo);
+            }
+
+            //float x = skiaTextRenderInfo.X;
+            //float y = skiaTextRenderInfo.Y;
+
+            var baselineY = /*skFont.Metrics.Leading +*/ (-skFont.Metrics.Ascent);
+
+            // 由于 Skia 的 DrawText 传入的 Point 是文本的基线，因此需要调整 Y 值
+            y += baselineY;
+            using SKTextBlob skTextBlob = SKTextBlob.Create(charSpan, skFont);
+            canvas.DrawText(skTextBlob, x, y, textRenderSKPaint);
         }
     }
 }
