8d972a6a7adbb9d44a8e69bd7f0e9425a484e661
lindexi <lindexi_gd@163.com>
2025-05-27 17:41:43 +0800

    拆分文件

---
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/Document/Property_/Decorations/BuildDecorationResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Media;
+
+namespace LightTextEditorPlus.Document.Decorations;
+
+/// <summary>
+/// 构建装饰的结果
+/// </summary>
+/// <param name="Drawing">构建结果内容</param>
+/// <param name="TakeCharCount">装饰层用到了多少个字符参与构建。接下来下一次调用渲染就会跳过这些字符。比如下划线可以整一片一起，那就可以快速跳过一片相同属性的字符了</param>
+public readonly record struct BuildDecorationResult(Drawing? Drawing, int TakeCharCount)
+{
+}
--- LightTextEditorPlus/LightTextEditorPlus.Wpf/Document/Property_/Decorations/TextEditorDecoration.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/Document/Property_/Decorations/TextEditorDecoration.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Media;
-
-using LightTextEditorPlus.Core.Document;
-using LightTextEditorPlus.Core.Primitive;
-using LightTextEditorPlus.Core.Primitive.Collections;
-using LightTextEditorPlus.Core.Rendering;
 
 namespace LightTextEditorPlus.Document.Decorations;
 
@@ -75,23 +69,3 @@ public abstract class TextEditorDecoration
         return HashCode.Combine(TextDecorationLocation, GetType());
     }
 }
-
-/// <summary>
-/// 构建装饰的参数
-/// </summary>
-/// <param name="RunProperty"></param>
-/// <param name="CharDataList">当前相同属性的字符列表，从准备绘制的起始字符开始</param>
-/// <param name="RecommendedBounds">推荐的渲染范围</param>
-/// <param name="LineRenderInfo">段落内的行的渲染信息</param>
-///// <param name="CurrentCharIndexInLine">当前准备绘制的起始字符所在当前行的坐标</param>
-public readonly record struct BuildDecorationArgument(RunProperty RunProperty, /*int CurrentCharIndexInLine,*/ TextReadOnlyListSpan<CharData> CharDataList, TextRect RecommendedBounds, ParagraphLineRenderInfo LineRenderInfo, TextEditor TextEditor);
-
-/// <summary>
-/// 构建装饰的结果
-/// </summary>
-/// <param name="Drawing">构建结果内容</param>
-/// <param name="TakeCharCount">装饰层用到了多少个字符参与构建。接下来下一次调用渲染就会跳过这些字符。比如下划线可以整一片一起，那就可以快速跳过一片相同属性的字符了</param>
-public readonly record struct BuildDecorationResult(Drawing? Drawing, int TakeCharCount)
-{
-}
-
