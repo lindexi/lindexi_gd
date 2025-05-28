e89ea248e2c911f7a182196178b2d3a8bf196cc9
lindexi <lindexi_gd@163.com>
2025-05-28 10:12:28 +0800

    只有特性不会应任何影响

---
+++ Workbench/WhemjurjearjeWerjelijonai/Class1.cs
@@ -0,0 +1,14 @@
+﻿using CearcurwaidereFerkearjurnawhea;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhemjurjearjeWerjelijonai;
+
+[ProgramAttribute]
+internal class Class1
+{
+}
---
+++ Workbench/WhemjurjearjeWerjelijonai/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WhemjurjearjeWerjelijonai;
+
+var class1 = new Class1();
+Console.WriteLine($"Hello, World! {class1}");
