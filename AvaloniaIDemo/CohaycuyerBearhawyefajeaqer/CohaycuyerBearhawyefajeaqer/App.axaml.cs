75eb5d14e5eef5b8276c73f1dce937e29e915b6e
lindexi <lindexi_gd@163.com>
2025-05-29 17:39:59 +0800

    测试关闭窗口的空项目内存

---
+++ AvaloniaIDemo/CohaycuyerBearhawyefajeaqer/CohaycuyerBearhawyefajeaqer/App.axaml.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+using CohaycuyerBearhawyefajeaqer.Views;
+
+namespace CohaycuyerBearhawyefajeaqer;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            desktop.MainWindow = new MainWindow
+            {
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ AvaloniaIDemo/CohaycuyerBearhawyefajeaqer/CohaycuyerBearhawyefajeaqer/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CohaycuyerBearhawyefajeaqer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
