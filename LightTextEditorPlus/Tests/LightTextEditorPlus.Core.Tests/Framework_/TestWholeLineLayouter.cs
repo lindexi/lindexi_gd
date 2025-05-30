8911bf3a4fb398280c7de3b38a939b592303c0c4
lindexi <lindexi_gd@163.com>
2025-05-30 16:51:04 +0800

    尝试修复构建

--- LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/Framework_/TestWholeLineLayouter.cs
+++ LightTextEditorPlus/Tests/LightTextEditorPlus.Core.Tests/Framework_/TestWholeLineLayouter.cs
@@ -1,4 +1,4 @@
-using LightTextEditorPlus.Core.Layout;
+﻿using LightTextEditorPlus.Core.Layout;
 using LightTextEditorPlus.Core.Platform;
 using LightTextEditorPlus.Core.Primitive;
 
@@ -8,7 +8,7 @@ class TestWholeLineLayouter : IWholeLineLayouter
 {
     public Func<WholeLineLayoutArgument, WholeLineLayoutResult>? LayoutWholeLineFunc { set; get; }
 
-    public WholeLineLayoutResult LayoutWholeLine(in WholeLineLayoutArgument argument)
+    public WholeLineLayoutResult UpdateLayoutWholeLine(in WholeLineLayoutArgument argument)
     {
         TextSize size = new TextSize(10, 10);
         return LayoutWholeLineFunc?.Invoke(argument) ?? new WholeLineLayoutResult(size, size, 1, default, argument.CharDataList[0]);
