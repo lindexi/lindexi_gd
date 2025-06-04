7e5fdf5ec7eac5feff5ea042c4e630a93712832c
lindexi <lindexi_gd@163.com>
2025-06-04 09:00:22 +0800

    Test Input Bindung Gesture Intellisense
    https://github.com/dotnet/wpf/issues/10902

---
+++ WPFDemo/CemcerjereyehacawbaBonekuyemfudee/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CemcerjereyehacawbaBonekuyemfudee;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    public ICommand Foo { get; } = new Foo();
+}
+
+public class Foo : ICommand
+{
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
