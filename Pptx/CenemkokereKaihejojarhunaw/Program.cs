059b7e5807c33ebc6e9156971e6b8d51235a9c0e
lindexi <lindexi_gd@163.com>
2025-06-04 09:47:05 +0800

    测试文本上下标内容

---
+++ Pptx/CenemkokereKaihejojarhunaw/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+using DocumentFormat.OpenXml.Drawing;
+
+using TextBody = DocumentFormat.OpenXml.Presentation.TextBody;
+
+using var presentationDocument =
+    DocumentFormat.OpenXml.Packaging.PresentationDocument.Open("Test.pptx", false);
+var presentationPart = presentationDocument.PresentationPart;
+var slidePart = presentationPart!.SlideParts.First();
+var slide = slidePart.Slide;
+
+var textBody = slide.CommonSlideData!.ShapeTree!.Descendants<TextBody>().First();
+Paragraph paragraph = textBody.GetFirstChild<Paragraph>()!;
+
+foreach (var run in paragraph.Elements<Run>())
+{
+    RunProperties? runProperties = run.RunProperties;
+    var baseline = runProperties?.Baseline?.Value;
+    var text = run.Text?.Text;
+}
