09fd601dfc1617477ec68469dc16b8b2f189ac40
lindexi <lindexi_gd@163.com>
2025-06-06 10:40:29 +0800

    测试不支持嵌套对象的方式

---
+++ Workbench/KelkeahedefahawNecheenabi/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+var foo = new Foo(2, "zxc");
+
+var gcHandle = GCHandle.Alloc(foo, GCHandleType.Normal);
+var intPtr = GCHandle.ToIntPtr(gcHandle);
+var gcHandleFromIntPtr = GCHandle.FromIntPtr(intPtr);
+if (ReferenceEquals(foo, gcHandleFromIntPtr.Target))
+{
+
+}
+
+Console.WriteLine("Hello, World!");
+
+record Foo(int Count, string Name)
+{
+
+}
