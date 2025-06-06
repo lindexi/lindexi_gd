99c16127144a1d2abaab2f2d4e9fdbf68783101c
lindexi <lindexi_gd@163.com>
2025-06-06 10:54:10 +0800

    测试不安全调用

---
+++ Workbench/BicherkairharNocharbawni/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+Console.WriteLine(Environment.Is64BitProcess);
+GetSystemInfoWrite();
+Console.WriteLine("Hello, World!");
+
+[DllImport("LightTextEditorPlus.Skia.AotLayer")]
+static extern void GetSystemInfoWrite();
