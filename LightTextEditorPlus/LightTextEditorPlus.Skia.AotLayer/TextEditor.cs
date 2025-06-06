4be8ad06baafd8d43eafe2bbbd4f67ef5e92eee0
lindexi <lindexi_gd@163.com>
2025-06-06 10:54:22 +0800

    提供平台调用层

---
+++ LightTextEditorPlus/LightTextEditorPlus.Skia.AotLayer/TextEditor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightTextEditorPlus.Skia.AotLayer;
+
+internal static class TextEditor
+{
+    [UnmanagedCallersOnly(EntryPoint = "GetSystemInfoWrite")]
+    public static void GetSystemInfo()
+    {
+        Console.WriteLine($"ProcessorCount: {Environment.ProcessorCount}");
+        Console.WriteLine($"MachineName: {Environment.MachineName}");
+    }
+}
