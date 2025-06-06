52d328f28a6c662c5b89b1723f9ada38155d9a88
lindexi <lindexi_gd@163.com>
2025-06-06 14:31:03 +0800

    优化调试程序

--- LightTextEditorPlus/Demos/NativeAotLayerTestDemo/Program.cs
+++ LightTextEditorPlus/Demos/NativeAotLayerTestDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 using LightTextEditorPlus;
@@ -11,19 +12,6 @@ if (Environment.Is64BitProcess)
 
 string text = "这是一段文本";
 
-unsafe
-{
-    fixed (char* t = text)
-    {
-        IntPtr p = new IntPtr(t);
-
-        int byteCount = Encoding.Unicode.GetByteCount(text);
-
-        // 实际测试证明传入的是字符数量，而不是 byte 数量
-        string message = Marshal.PtrToStringUni(p, byteCount);
-    }
-}
-
 TextEditorId textEditorId = TextEditorWrapper.CreateTextEditor();
 Console.WriteLine(textEditorId);
 
@@ -31,9 +19,15 @@ var errorCode = TextEditorWrapper.AppendText(textEditorId, text);
 
 errorCode = TextEditorWrapper.SaveAsImageFile(textEditorId, "1.png");
 
+if (OperatingSystem.IsWindows())
+{
+    Process.Start(new ProcessStartInfo("explorer.exe", "/select,1.png"));
+}
+
 TextEditorWrapper.FreeTextEditor(textEditorId);
 
 /*
 System.DllNotFoundException:“Unable to load DLL 'CreateTextEditor' or one of its dependencies: 找不到指定的模块。 (0x8007007E)”
 */
 Console.WriteLine("Hello, World!");
+
