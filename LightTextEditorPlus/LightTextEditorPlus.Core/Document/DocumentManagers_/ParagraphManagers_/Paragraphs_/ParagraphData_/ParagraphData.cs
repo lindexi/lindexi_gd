4756d23894b2c7a82b7122ad9209fa29cb61dd86
lindexi <lindexi_gd@163.com>
2025-06-09 09:34:15 +0800

    优化调试辅助

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/ParagraphData_/ParagraphData.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/ParagraphData_/ParagraphData.cs
@@ -19,7 +19,7 @@ namespace LightTextEditorPlus.Core.Document;
 /// <summary>
 /// 段落数据
 /// </summary>
-class ParagraphData : ITextParagraph
+internal sealed class ParagraphData : ITextParagraph
 {
     public ParagraphData(IReadOnlyRunProperty paragraphStartRunProperty, ParagraphProperty paragraphProperty, ParagraphManager paragraphManager)
     {
