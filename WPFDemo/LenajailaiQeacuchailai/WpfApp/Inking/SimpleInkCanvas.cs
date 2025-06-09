f220dc3ffd02ac80fd7a49481c5f1804998137e0
lindexi <lindexi_gd@163.com>
2025-06-09 16:30:21 +0800

    完成简单的对接实现逻辑

---
+++ WPFDemo/LenajailaiQeacuchailai/WpfApp/Inking/SimpleInkCanvas.cs
@@ -0,0 +1,89 @@
+﻿extern alias WpfInk;
+using System.Windows;
+using System.Windows.Media;
+
+using WpfApp.InkDataModels;
+
+using WpfInk::MS.Internal.Ink;
+using WpfInk::System.Windows.Ink;
+using WpfInk::System.Windows.Input;
+
+namespace WpfApp.Inking;
+
+public class SimpleInkCanvas : FrameworkElement
+{
+    public SimpleInkCanvas()
+    {
+        HorizontalAlignment = HorizontalAlignment.Stretch;
+        VerticalAlignment = VerticalAlignment.Stretch;
+    }
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+        base.OnRender(drawingContext);
+
+        var arrayOfArrayOfInkDataModel = ArrayOfArrayOfInkDataModel.ReadFromFile(@"Assets\Ink.xml");
+        foreach (ArrayOfInkDataModel arrayOfInkDataModel in arrayOfArrayOfInkDataModel)
+        {
+            //if (arrayOfInkDataModel.Count < 10)
+            //{
+            //    continue;
+            //}
+
+            var streamGeometry = new StreamGeometry()
+            {
+                FillRule = FillRule.Nonzero
+            };
+            using var streamGeometryContext = streamGeometry.Open();
+            WpfInk::WpfInk.API.IStreamGeometryContext context = new InkingStreamGeometryContext(streamGeometryContext);
+
+            var drawingAttributes = new WpfInk::System.Windows.Ink.DrawingAttributes()
+            {
+                Width = 10,
+                Height = 10,
+            };
+            var stylusPointCollection = new StylusPointCollection();
+            foreach (var inkDataModel in arrayOfInkDataModel)
+            {
+                stylusPointCollection.Add(new WpfInk::System.Windows.Input.StylusPoint(inkDataModel.X, inkDataModel.Y));
+            }
+
+            WpfInk::System.Windows.Ink.Stroke stroke = new WpfInk::System.Windows.Ink.Stroke(stylusPointCollection, drawingAttributes);
+            var strokeNodeIterator = StrokeNodeIterator.GetIterator(stroke, drawingAttributes);
+            StrokeRenderer.CalcGeometryAndBounds(strokeNodeIterator, drawingAttributes, calculateBounds: false, context, out _);
+
+            drawingContext.DrawGeometry(Brushes.Red, null, streamGeometry);
+        }
+    }
+}
+
+class InkingStreamGeometryContext : WpfInk::WpfInk.API.IStreamGeometryContext
+{
+    public InkingStreamGeometryContext(StreamGeometryContext streamGeometryContext)
+    {
+        StreamGeometryContext = streamGeometryContext;
+    }
+
+    private StreamGeometryContext StreamGeometryContext { get; }
+
+    public void BeginFigure(WpfInk::WpfInk.PresentationCore.System.Windows.Point startPoint, bool isFilled, bool isClosed)
+    {
+        StreamGeometryContext.BeginFigure(startPoint.ToPoint(), isFilled, isClosed);
+    }
+
+    public void PolyBezierTo(IList<WpfInk::WpfInk.PresentationCore.System.Windows.Point> points, bool isStroked, bool isSmoothJoin)
+    {
+        StreamGeometryContext.PolyBezierTo(points.Select(t => t.ToPoint()).ToList(), isStroked, isSmoothJoin);
+    }
+
+    public void PolyLineTo(IList<WpfInk::WpfInk.PresentationCore.System.Windows.Point> points, bool isStroked, bool isSmoothJoin)
+    {
+        StreamGeometryContext.PolyLineTo(points.Select(t => t.ToPoint()).ToList(), isStroked, isSmoothJoin);
+    }
+
+    public void ArcTo(WpfInk::WpfInk.PresentationCore.System.Windows.Point point, WpfInk::WpfInk.PresentationCore.System.Windows.Size size, double rotationAngle, bool isLargeArc, bool sweepDirection, bool isStroked,
+        bool isSmoothJoin)
+    {
+        StreamGeometryContext.ArcTo(point.ToPoint(), size.ToSize(), rotationAngle, isLargeArc, sweepDirection ? SweepDirection.Clockwise : SweepDirection.Counterclockwise, isStroked, isSmoothJoin);
+    }
+}
--- WPFDemo/LenajailaiQeacuchailai/WpfApp/MainWindow.xaml.cs
+++ WPFDemo/LenajailaiQeacuchailai/WpfApp/MainWindow.xaml.cs
@@ -4,12 +4,9 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-using WpfApp.InkDataModels;
 
 namespace WpfApp;
 
@@ -21,22 +18,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        ArrayOfArrayOfInkDataModel.ReadFromFile(@"Assets\Ink.xml");
-    }
-}
-
-public class SimpleInkCanvas : FrameworkElement
-{
-    public SimpleInkCanvas()
-    {
-        HorizontalAlignment = HorizontalAlignment.Stretch;
-        VerticalAlignment = VerticalAlignment.Stretch;
-    }
-
-    protected override void OnRender(DrawingContext drawingContext)
-    {
-        base.OnRender(drawingContext);
-
-        drawingContext.DrawLine(new Pen(Brushes.Red, 20), new Point(), new Point(200, 200));
     }
 }
