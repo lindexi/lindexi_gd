bfc50b23ea505f4c2606f4fc322cc73bd7b5f4a7
lindexi <lindexi_gd@163.com>
2025-06-09 17:23:47 +0800

    测试弧形逻辑

--- WPFDemo/KagaikohulekelLufaiqeyihe/MainWindow.xaml.cs
+++ WPFDemo/KagaikohulekelLufaiqeyihe/MainWindow.xaml.cs
@@ -30,34 +30,11 @@ public partial class MainWindow : Window
             // Set the begin point of the shape.
             ctx.BeginFigure(new Point(10, 100), true /* is filled */, false /* is closed */);
 
-            // Create a collection of Point structures that will be used with the PolyBezierTo 
-            // Method to create the Bezier curve.
-            List<Point> pointList = new List<Point>();
-
-            // First Bezier curve is specified with these three points.
-
-            // First control point for first Bezier curve.
-            pointList.Add(new Point(100, 0));
-
-            // Second control point for first Bezier curve.
-            pointList.Add(new Point(200, 200));
-
-            // Destination point for first Bezier curve.
-            pointList.Add(new Point(300, 100));
-
-            // Second Bezier curve is specified with these three points.
-
-            //// First control point for second Bezier curve.
-            //pointList.Add(new Point(400, 0));
-
-            //// Second control point for second Bezier curve.
-            //pointList.Add(new Point(500, 200));
-
-            //// Destination point for second Bezier curve.
-            //pointList.Add(new Point(600, 100));
-
-            // Create a Bezier curve using the collection of Point Structures.
-            ctx.PolyBezierTo(pointList, true /* is stroked */, false /* is smooth join */);
+            // Create an arc. Draw the arc from the begin point to 200,100 with the specified parameters.
+            ctx.ArcTo(new Point(203, 161), new Size(131, 56), 45 /* rotation angle */, true /* is large arc */,
+                SweepDirection.Counterclockwise, true /* is stroked */, false /* is smooth join */);
+            // M10,100A131,56,45,1,0,203,161
+            // A Size, rotationAngle, isLargeArc(1), SweepDirection.Counterclockwise(0), Point
         }
 
         // Freeze the geometry (make it unmodifiable)
