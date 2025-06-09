41e2f487f51dda1ca64e9ce7f1285ead5a7f9474
lindexi <lindexi_gd@163.com>
2025-06-09 10:04:22 +0800

    删除不使用的方法

--- LightTextEditorPlus/LightTextEditorPlus.Wpf/Document/Property_/RunProperty_/RunPropertyExtension.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Wpf/Document/Property_/RunProperty_/RunPropertyExtension.cs
@@ -1,10 +1,8 @@
-using LightTextEditorPlus.Core.Document;
+﻿using LightTextEditorPlus.Core.Document;
 
 namespace LightTextEditorPlus.Document;
 
 static class RunPropertyExtension
 {
     public static RunProperty AsRunProperty(this IReadOnlyRunProperty runProperty) => (RunProperty) runProperty;
-
-    public static RunProperty AsIRunProperty(this IReadOnlyRunProperty runProperty) => runProperty.AsRunProperty();
 }
