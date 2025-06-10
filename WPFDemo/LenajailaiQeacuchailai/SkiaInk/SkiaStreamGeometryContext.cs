9eb61c07bb3ecc3a9bebddfbb3ede5dc992e19b3
lindexi <lindexi_gd@163.com>
2025-06-10 10:42:08 +0800

    对接笔迹渲染效果

--- WPFDemo/LenajailaiQeacuchailai/SkiaInk/SkiaStreamGeometryContext.cs
+++ WPFDemo/LenajailaiQeacuchailai/SkiaInk/SkiaStreamGeometryContext.cs
@@ -11,32 +11,54 @@ using WpfInk.PresentationCore.System.Windows;
 
 namespace SkiaInk;
 
-internal class SkiaStreamGeometryContext : IStreamGeometryContext
+public class SkiaStreamGeometryContext : IStreamGeometryContext
 {
     public SkiaStreamGeometryContext(SKPath path)
     {
         Path = path;
+        path.FillType = SKPathFillType.Winding;
     }
 
     public SKPath Path { get; }
 
     public void BeginFigure(Point startPoint, bool isFilled, bool isClosed)
     {
-        throw new NotImplementedException();
+        Path.MoveTo(startPoint.ToPoint());
     }
 
     public void PolyBezierTo(IList<Point> points, bool isStroked, bool isSmoothJoin)
     {
+        // 传入的 points 必定是 3 的倍数
+
+        for (var i = 0; i < points.Count; i += 3)
+        {
+            var a = points[i];
+            var b = points[i + 1];
+            var c = points[i + 2];
+
+            Path.CubicTo(a.ToPoint(), b.ToPoint(), c.ToPoint());
+        }
     }
 
     public void PolyLineTo(IList<Point> points, bool isStroked, bool isSmoothJoin)
     {
-        throw new NotImplementedException();
+        foreach (var point in points)
+        {
+            Path.LineTo(point.ToPoint());
+        }
     }
 
     public void ArcTo(Point point, Size size, double rotationAngle, bool isLargeArc, bool sweepDirection, bool isStroked,
         bool isSmoothJoin)
     {
-        
+        Path.ArcTo((float) size.Width, (float) size.Height, (float) rotationAngle, isLargeArc ? SKPathArcSize.Large : SKPathArcSize.Small, sweepDirection ? SKPathDirection.CounterClockwise : SKPathDirection.Clockwise, (float) point.X, (float) point.Y);
+    }
+}
+
+static class Converter
+{
+    public static SKPoint ToPoint(this Point point)
+    {
+        return new SKPoint((float) point.X, (float) point.Y);
     }
 }
--- WPFDemo/LenajailaiQeacuchailai/WpfApp/Inking/SimpleInkCanvas.cs
+++ WPFDemo/LenajailaiQeacuchailai/WpfApp/Inking/SimpleInkCanvas.cs
@@ -2,11 +2,16 @@
 using System.Windows;
 using System.Windows.Media;
 
+using SkiaInk;
+
+using SkiaSharp;
+
 using WpfApp.InkDataModels;
 
 using WpfInk::MS.Internal.Ink;
 using WpfInk::System.Windows.Ink;
 using WpfInk::System.Windows.Input;
+using WpfInk::WpfInk.API;
 
 namespace WpfApp.Inking;
 
@@ -19,6 +24,8 @@ public class SimpleInkCanvas : FrameworkElement
 
         MouseMove += SimpleInkCanvas_MouseMove;
     }
+
+    public SkiaCanvas? SkiaCanvas { get; set; }
 
     private void SimpleInkCanvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
     {
@@ -43,10 +50,12 @@ public class SimpleInkCanvas : FrameworkElement
     {
         base.OnRender(drawingContext);
 
+        var pathList = new List<SKPath>();
+
         var arrayOfArrayOfInkDataModel = ArrayOfArrayOfInkDataModel.ReadFromFile(@"Assets\Ink.xml");
         foreach (ArrayOfInkDataModel arrayOfInkDataModel in arrayOfArrayOfInkDataModel)
         {
-            if (arrayOfInkDataModel.Count < 10)
+            if (arrayOfInkDataModel.Count < 20)
             {
                 continue;
             }
@@ -73,6 +82,22 @@ public class SimpleInkCanvas : FrameworkElement
             RenderStroke(stylusPointCollection);
         }
 
+        SkiaCanvas?.Draw(canvas =>
+        {
+            using var skPaint = new SKPaint();
+            skPaint.Color = SKColors.Blue;
+            skPaint.Style = SKPaintStyle.Fill;
+            skPaint.IsAntialias = true;
+
+            foreach (var skPath in pathList)
+            {
+                canvas.DrawPath(skPath, skPaint);
+                skPath.Dispose();
+            }
+
+            pathList.Clear();
+        });
+
 
         void RenderStroke(WpfInk::System.Windows.Input.StylusPointCollection stylusPointCollection)
         {
@@ -80,23 +105,49 @@ public class SimpleInkCanvas : FrameworkElement
             {
                 FillRule = FillRule.Nonzero
             };
-            using var streamGeometryContext = streamGeometry.Open();
-            WpfInk::WpfInk.API.IStreamGeometryContext context = new InkingStreamGeometryContext(streamGeometryContext);
-
-            var drawingAttributes = new WpfInk::System.Windows.Ink.DrawingAttributes()
+            using (var streamGeometryContext = streamGeometry.Open())
             {
-                Width = 10,
-                Height = 10,
-                //StylusTip = StylusTip.Rectangle,
-                //FitToCurve = true,
-            };
+                WpfInk::WpfInk.API.IStreamGeometryContext context =
+                    new InkingStreamGeometryContext(streamGeometryContext);
 
-            WpfInk::System.Windows.Ink.Stroke stroke = new WpfInk::System.Windows.Ink.Stroke(stylusPointCollection, drawingAttributes);
-            var strokeNodeIterator = StrokeNodeIterator.GetIterator(stroke, drawingAttributes);
-            StrokeRenderer.CalcGeometryAndBounds(strokeNodeIterator, drawingAttributes, calculateBounds: false, context, out _);
+                RenderToGeometry(stylusPointCollection, context);
+            }
 
+            drawingContext.PushOpacity(0.3);
             drawingContext.DrawGeometry(Brushes.Red, null, streamGeometry);
+            drawingContext.Pop();
+
+            var text = streamGeometry.ToString();
+            _ = text;
+
+            var skPath = new SKPath();
+            var skiaStreamGeometryContext = new SkiaStreamGeometryContext(skPath);
+            RenderToGeometry(stylusPointCollection, skiaStreamGeometryContext);
+            pathList.Add(skPath);
+            var svgPathData = skPath.ToSvgPathData();
+            _ = svgPathData;
+            if (text.Contains(svgPathData))
+            {
+
+            }
         }
+    }
+
+    private static void RenderToGeometry(StylusPointCollection stylusPointCollection, IStreamGeometryContext context)
+    {
+        var drawingAttributes = new WpfInk::System.Windows.Ink.DrawingAttributes()
+        {
+            Width = 10,
+            Height = 10,
+            //StylusTip = StylusTip.Rectangle,
+            //FitToCurve = true,
+        };
+
+        WpfInk::System.Windows.Ink.Stroke stroke =
+            new WpfInk::System.Windows.Ink.Stroke(stylusPointCollection, drawingAttributes);
+        var strokeNodeIterator = StrokeNodeIterator.GetIterator(stroke, drawingAttributes);
+        StrokeRenderer.CalcGeometryAndBounds(strokeNodeIterator, drawingAttributes, calculateBounds: false,
+            context, out _);
     }
 }
 
