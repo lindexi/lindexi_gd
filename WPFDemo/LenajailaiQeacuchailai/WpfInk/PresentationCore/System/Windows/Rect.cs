b491e65d27f85b45f20d93933e4ddfc1cc0860a0
lindexi <lindexi_gd@163.com>
2025-06-10 15:52:29 +0800

    准备修改命名

--- WPFDemo/LenajailaiQeacuchailai/WpfInk/PresentationCore/System/Windows/Rect.cs
+++ WPFDemo/LenajailaiQeacuchailai/WpfInk/PresentationCore/System/Windows/Rect.cs
@@ -1,9 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Windows;
 
 namespace WpfInk.PresentationCore.System.Windows
 {
--- WPFDemo/LenajailaiQeacuchailai/WpfInk/PresentationCore/System/Windows/Size.cs
+++ WPFDemo/LenajailaiQeacuchailai/WpfInk/PresentationCore/System/Windows/Size.cs
@@ -1,9 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.ComponentModel;
-using System.Windows;
-
 namespace WpfInk.PresentationCore.System.Windows
 {
     public struct Size : IFormattable
