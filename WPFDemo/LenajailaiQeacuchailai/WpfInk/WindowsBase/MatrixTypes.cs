78c7aab52dee2ee520ed6613ac1775158365e695
lindexi <lindexi_gd@163.com>
2025-06-11 16:30:40 +0800

    修改为文件级命名空间

---
+++ WPFDemo/LenajailaiQeacuchailai/WpfInk/WindowsBase/MatrixTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MS.Internal;
+
+[Flags]
+internal enum MatrixTypes
+{
+    TRANSFORM_IS_IDENTITY = 0,
+    TRANSFORM_IS_TRANSLATION = 1,
+    TRANSFORM_IS_SCALING     = 2,
+    TRANSFORM_IS_UNKNOWN     = 4
+}
