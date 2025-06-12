c60e5c2a7337cfa07d2f5f9abbdc53c03b20b4f5
lindexi <lindexi_gd@163.com>
2025-06-12 19:45:49 +0800

    测试禁用系统关闭

---
+++ WPFDemo/WhalqojerwiKuwelbaiko/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WhalqojerwiKuwelbaiko;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        this.SessionEnding += App_SessionEnding;
+    }
+
+    private void App_SessionEnding(object sender, SessionEndingCancelEventArgs e)
+    {
+        e.Cancel = true;
+    }
+}
+
