64f9c4ce497cbf4c1cd231fbb8053f7817429a82
lindexi <lindexi_gd@163.com>
2025-06-18 21:16:02 +0800

    压缩完成判断文件是否损坏

--- Workbench/LalwaiwiheLuburreja/InstallerSevenZipTool/Program.cs
+++ Workbench/LalwaiwiheLuburreja/InstallerSevenZipTool/Program.cs
@@ -16,8 +16,69 @@ if (args.Length == 0)
 }
 
 var options = CommandLine.Parse(args).As<Options>();
-Console.WriteLine($"开始制作安装包资产文件。输入文件夹： '{Path.GetFullPath(options.InputDirectory)}' 输出文件： '{Path.GetFullPath(options.OutputFile)}'");
+var inputDirectory = Path.GetFullPath(options.InputDirectory);
 
-DirectoryArchive.Compress(new DirectoryInfo(options.InputDirectory), new FileInfo(options.OutputFile));
+var outputFile = Path.GetFullPath(options.OutputFile);
 
-Console.WriteLine($"安装包资产文件创建成功： {Path.GetFullPath(options.OutputFile)}");
+Console.WriteLine($"开始制作安装包资产文件。输入文件夹： '{inputDirectory}' 输出文件： '{outputFile}'");
+
+DirectoryArchive.Compress(new DirectoryInfo(inputDirectory), new FileInfo(outputFile));
+
+if (options.IgnoreChecksum is not true)
+{
+    Console.WriteLine($"完成压缩，开始测试压缩包是否正确");
+    // 测试的方法就是解压缩到临时目录，然后检查文件是否完整
+    var workingFolder = Path.Join(Path.GetTempPath(), $"Installer_{Path.GetRandomFileName()}");
+
+    DirectoryArchive.Decompress(new FileInfo(outputFile), Directory.CreateDirectory(workingFolder));
+    // 解压缩完成之后，执行文件对比
+
+    foreach (var originFile in Directory.GetFiles(inputDirectory, "*", SearchOption.AllDirectories))
+    {
+        var relativePath = Path.GetRelativePath(inputDirectory, originFile);
+        var unzipFile = Path.Join(workingFolder, relativePath);
+
+        if (!File.Exists(unzipFile))
+        {
+            Console.WriteLine($"压缩过程出错，找不到 {relativePath} 文件");
+            return;
+        }
+
+        CompareFile();
+
+        void CompareFile()
+        {
+            var buffer1 = new byte[4096];
+            var buffer2 = new byte[4096];
+
+            using var file1Stream = File.OpenRead(originFile);
+            using var file2Stream = File.OpenRead(unzipFile);
+
+            if (file1Stream.Length != file2Stream.Length)
+            {
+                Console.WriteLine($"文件 {relativePath} 内容不一致，压缩过程出错");
+                return;
+            }
+
+            while (true)
+            {
+                var readCount1 = file1Stream.Read(buffer1, 0, buffer1.Length);
+                var readCount2 = file2Stream.Read(buffer2, 0, buffer2.Length);
+                if (readCount1 != readCount2 || !buffer1.AsSpan(0, readCount1).SequenceEqual(buffer2.AsSpan(0, readCount2)))
+                {
+                    Console.WriteLine($"文件 {relativePath} 内容不一致，压缩过程出错");
+                    return;
+                }
+                if (readCount1 == 0)
+                {
+                    break; // 文件读取完毕
+                }
+            }
+        }
+    }
+
+    // 删除临时目录
+    Directory.Delete(workingFolder, true);
+}
+
+Console.WriteLine($"安装包资产文件创建成功： {outputFile}");
