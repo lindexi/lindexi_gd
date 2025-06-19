f6cb11e35786cafb6b1ea04095fe7eb6d2995f6b
lindexi <lindexi_gd@163.com>
2025-06-19 14:35:46 +0800

    附带足够的资源

--- Workbench/LalwaiwiheLuburreja/DotNetCampus.Installer.Boost/SplashScreen.cs
+++ Workbench/LalwaiwiheLuburreja/DotNetCampus.Installer.Boost/SplashScreen.cs
@@ -14,6 +14,7 @@ using static DotNetCampus.Installer.Boost.Win32.WindowExStyles;
 using static DotNetCampus.Installer.Boost.Win32.WindowStyles;
 using WindowsMessages = DotNetCampus.Installer.Boost.Win32.WM;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Globalization;
 using SIZE = DotNetCampus.Installer.Boost.Win32.Size;
 
@@ -134,6 +135,7 @@ internal class SplashScreen
         _requestClose = true;
     }
 
+    [Conditional("DEBUG")]
     private void WriteLog(string message)
     {
         Console.WriteLine($"[SplashScreen] {message}");
