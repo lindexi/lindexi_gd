d788002bed927cb14ed13d6b65f91ba020ba7990
lindexi <lindexi_gd@163.com>
2025-06-19 14:31:53 +0800

    列举文件夹

---
+++ Workbench/QeharhiheKalerecikurfai/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+foreach (var file in Directory.EnumerateFiles(".", "*.ico", SearchOption.AllDirectories))
+{
+    var fullPath = Path.GetFullPath(file);
+    Console.WriteLine(fullPath);
+}
+
+Console.WriteLine("Hello, World!");
