b2684dab024338f4d24daf48f2bd6aea33069cd8
lindexi <lindexi_gd@163.com>
2025-06-19 21:14:06 +0800

    安装包创建器处理逻辑

---
+++ Workbench/LalwaiwiheLuburreja/InstallerCreateTool/Option.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetCampus.Cli.Compiler;
+
+namespace InstallerCreateTool;
+
+internal class Option
+{
+    [Option()]
+    public required string PackingFolder { get; init; }
+
+    [Option()]
+    public string? InstallerOutputFile { get; init; }
+
+    [Option()]
+    public required string InstallerBoostProjectPath { get; init; }
+}
