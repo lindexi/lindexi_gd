796f0f85781f638248db332edbe118996ffe9de1
lindexi <lindexi_gd@163.com>
2025-06-23 15:43:34 +0800

    测试同时存在大小写的内容

---
+++ Workbench/NayjokaherewhaiGerebochawha/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var json = """
+{
+    "aaa": "value1",
+    "Aaa": "value2"
+}
+""";
+
+var foo = JsonSerializer.Deserialize<Foo>(json, new JsonSerializerOptions()
+{
+});
+
+Console.WriteLine("Hello, World!");
+
+
+class Foo
+{
+    [JsonPropertyName("aaa")]
+    public required string P1 { get; init; }
+
+    [JsonPropertyName("Aaa")]
+    public required string P2 { get; init; }
+}
