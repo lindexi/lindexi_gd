70c00ca4eb3d342629e6954c3d5d71b062580c48
lindexi <lindexi_gd@163.com>
2025-06-24 20:07:25 +0800

    测试添加图片进入

---
+++ Pptx/HibugalheakearJallcallkayja/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MiniExcelLibs;
+using MiniExcelLibs.Picture;
+
+using System.Data;
+using System.IO;
+
+var imageFile = @"C:\lindexi\SplashScreen.png";
+var outputExcelFile = "1.xlsx";
+outputExcelFile = Path.GetFullPath(outputExcelFile);
+
+
+
+var table = new DataTable();
+{
+    table.Columns.Add("Column1", typeof(string));
+    table.Columns.Add("Column2", typeof(decimal));
+    table.Rows.Add("MiniExcel", 1);
+    table.Rows.Add("Github", 2);
+}
+
+MiniExcel.SaveAs(outputExcelFile, table, overwriteFile: true);
+
+var images = new[]
+{
+    new MiniExcelPicture
+    {
+        ImageBytes = File.ReadAllBytes(imageFile),
+        SheetName = null, // default null is first sheet
+        CellAddress = "C3", // required
+    },
+};
+
+MiniExcel.AddPicture(outputExcelFile, images);
