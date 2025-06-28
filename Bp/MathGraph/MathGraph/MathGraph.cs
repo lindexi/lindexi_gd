d43ebd15b5396c4ab3df56a93dba7922a10c5be2
lindexi <lindexi_gd@163.com>
2025-06-28 09:39:41 +0800

    加上一些注释

--- Bp/MathGraph/MathGraph/MathGraph.cs
+++ Bp/MathGraph/MathGraph/MathGraph.cs
@@ -107,8 +107,19 @@ static class MathGraphElementIdGenerator
     }
 }
 
+/// <summary>
+/// 表示图里的一个元素
+/// </summary>
+/// <typeparam name="TElementInfo">元素本身的类型</typeparam>
+/// <typeparam name="TEdgeInfo">边的类型</typeparam>
 public class MathGraphElement<TElementInfo, TEdgeInfo>
 {
+    /// <summary>
+    /// 创建图里的一个元素
+    /// </summary>
+    /// <param name="mathGraph"></param>
+    /// <param name="value"></param>
+    /// <param name="id"></param>
     public MathGraphElement(MathGraph<TElementInfo, TEdgeInfo> mathGraph, TElementInfo value, string? id = null)
     {
         Value = value;
@@ -128,14 +139,26 @@ public class MathGraphElement<TElementInfo, TEdgeInfo>
         }
     }
 
+    /// <summary>
+    /// 元素所在的图
+    /// </summary>
     public MathGraph<TElementInfo, TEdgeInfo> MathGraph { get; }
 
     public string Id { get; }
 
+    /// <summary>
+    /// 元素自己的数据
+    /// </summary>
     public TElementInfo Value { get; }
 
+    /// <summary>
+    /// 出度的元素列表，包含所有从当前元素出发的边指向的元素。对于带双向边的元素来说，出度与入度是相同的
+    /// </summary>
     public IReadOnlyList<MathGraphElement<TElementInfo, TEdgeInfo>> OutElementList => _outElementList;
 
+    /// <summary>
+    /// 入度的元素列表，包含所有指向当前元素的边的起点元素。对于带双向边的元素来说，出度与入度是相同的
+    /// </summary>
     public IReadOnlyList<MathGraphElement<TElementInfo, TEdgeInfo>> InElementList => _inElementList;
 
     public IReadOnlyList<MathGraphEdge<TElementInfo, TEdgeInfo>> EdgeList => _edgeList;
@@ -235,6 +258,11 @@ public class MathGraphElement<TElementInfo, TEdgeInfo>
         return $"Value={Value} ; Id={Id};";
     }
 
+    /// <summary>
+    /// 确保两个元素在相同的一个图里面
+    /// </summary>
+    /// <param name="element"></param>
+    /// <exception cref="InvalidOperationException"></exception>
     private void EnsureSameMathGraph(MathGraphElement<TElementInfo, TEdgeInfo> element)
     {
         if (!ReferenceEquals(MathGraph, element.MathGraph))
@@ -251,13 +279,24 @@ public class MathGraphElement<TElementInfo, TEdgeInfo>
 /// <typeparam name="TEdgeInfo"></typeparam>
 public class MathGraphUnidirectionalEdge<TElementInfo, TEdgeInfo> : MathGraphEdge<TElementInfo, TEdgeInfo>
 {
+    /// <summary>
+    /// 创建单向边
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
     public MathGraphUnidirectionalEdge(MathGraphElement<TElementInfo, TEdgeInfo> from,
         MathGraphElement<TElementInfo, TEdgeInfo> to) : base(from, to)
     {
     }
 
+    /// <summary>
+    /// 单向边的起点元素
+    /// </summary>
     public MathGraphElement<TElementInfo, TEdgeInfo> From => base.A;
 
+    /// <summary>
+    /// 单向边的终点元素
+    /// </summary>
     public MathGraphElement<TElementInfo, TEdgeInfo> To => base.B;
 
     public override string ToString()
@@ -278,8 +317,14 @@ public class MathGraphBidirectionalEdge<TElementInfo, TEdgeInfo> : MathGraphEdge
     {
     }
 
+    /// <summary>
+    /// 双向边中的一个元素
+    /// </summary>
     public MathGraphElement<TElementInfo, TEdgeInfo> AElement => base.A;
 
+    /// <summary>
+    /// 双向边中的另一个元素
+    /// </summary>
     public MathGraphElement<TElementInfo, TEdgeInfo> BElement => base.B;
 
     public override string ToString()
@@ -288,6 +333,11 @@ public class MathGraphBidirectionalEdge<TElementInfo, TEdgeInfo> : MathGraphEdge
     }
 }
 
+/// <summary>
+/// 表示图的边，包含两个元素，A和B。A和B可以是同一个元素，但不能是null。
+/// </summary>
+/// <typeparam name="TElementInfo"></typeparam>
+/// <typeparam name="TEdgeInfo"></typeparam>
 public abstract class MathGraphEdge<TElementInfo, TEdgeInfo>
 {
     protected MathGraphEdge(MathGraphElement<TElementInfo, TEdgeInfo> a, MathGraphElement<TElementInfo, TEdgeInfo> b)
@@ -298,9 +348,21 @@ public abstract class MathGraphEdge<TElementInfo, TEdgeInfo>
 
     public TEdgeInfo? EdgeInfo { get; set; }
 
+    /// <summary>
+    /// 边的两端中的一个元素
+    /// </summary>
     protected MathGraphElement<TElementInfo, TEdgeInfo> A { get; }
+
+    /// <summary>
+    /// 边的两端中的另一个元素
+    /// </summary>
     protected MathGraphElement<TElementInfo, TEdgeInfo> B { get; }
 
+    /// <summary>
+    /// 确保元素在边的两端之一
+    /// </summary>
+    /// <param name="element"></param>
+    /// <exception cref="InvalidOperationException"></exception>
     public void EnsureContain(MathGraphElement<TElementInfo, TEdgeInfo> element)
     {
         if (!ReferenceEquals(element, A) && !ReferenceEquals(element, B))
@@ -309,6 +371,12 @@ public abstract class MathGraphEdge<TElementInfo, TEdgeInfo>
         }
     }
 
+    /// <summary>
+    /// 传入边的这一个元素，返回边的另一个元素
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">如果传入的元素不属于边的两端两个元素中的一个</exception>
     public MathGraphElement<TElementInfo, TEdgeInfo> GetOtherElement(MathGraphElement<TElementInfo, TEdgeInfo> element)
     {
         if (ReferenceEquals(element, A))
