8b8c4157b2f4a163c90558a0f99b3ad27b762599
lindexi <lindexi_gd@163.com>
2025-07-01 09:34:41 +0800

    似乎无法解压缩成功

---
+++ Workbench/RefaikiwairlurWhanaikeeleda/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Formats.Tar;
+
+var file = @"E:\download\dotnet-runtime-8.0.17-linux-x64.tar.gz";
+
+var output = Path.Join(AppContext.BaseDirectory, "CahohuneaFaidinalnalfaji");
+Directory.CreateDirectory(output);
+// System.IO.InvalidDataException:“Unable to parse number.”
+//TarFile.ExtractToDirectory(file, output, overwriteFiles: true);
+var fileStream = File.OpenRead(file);
+var tarReader = new TarReader(fileStream);
+TarEntry? tarEntry = await tarReader.GetNextEntryAsync();
+
+
+Console.WriteLine("Hello, World!");
