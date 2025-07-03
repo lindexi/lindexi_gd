41f2a94f19ee0a18ee607a0fa012fcd2fd8c0be5
lindexi <lindexi_gd@163.com>
2025-07-03 15:54:44 +0800

    渐变色处理

---
+++ Workbench/RalbearqemkemHefugadelday/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+
+using Image<Rgba32> image = new Image<Rgba32>(200, 200);
+
+Parallel.For(0, image.Height, rowIndex =>
+{
+    // ReSharper disable once AccessToDisposedClosure
+    Memory<Rgba32> rowMemory = image.DangerousGetPixelRowMemory(rowIndex);
+
+    var span = rowMemory.Span;
+
+    for (int i = 0; i < rowMemory.Length; i++)
+    {
+        ref var pixel = ref span[i];
+        pixel.A = 0xFF;
+        pixel.R = (byte) rowIndex;
+        pixel.G = (byte) (rowIndex + 1);
+        pixel.B = (byte) (rowIndex + 2);
+    }
+});
+
+image.SaveAsPng("1.png");
+
+Console.WriteLine("Hello, World!");
