e6eed4a7d4a8f0aff4f76f0790ff954a89708470
lindexi <lindexi_gd@163.com>
2025-07-07 09:22:52 +0800

    命令行库无法通过构建

---
+++ Workbench/HalnerefaiwhoLanallwaynai/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNetCampus.Cli;
+using HalnerefaiwhoLanallwaynai;
+
+var options = CommandLine.Parse(args).As<Options>();
+
+Console.WriteLine("Hello, World!");
