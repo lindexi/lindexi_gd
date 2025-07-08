bfcb671a49121ac9f6410a2725fa0f4116a11824
lindexi <lindexi_gd@163.com>
2025-07-08 19:39:43 +0800

    测试加载和删除程序集文件

---
+++ Workbench/GewobigeheCehijirawha/KarnadikemnemkaCallcilowhijinem/Foo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarnadikemnemkaCallcilowhijinem;
+
+public class Foo
+{
+    public void Do()
+    {
+        Console.WriteLine("Foo");
+    }
+}
