c2994e632c37c3131c33ff7297e124f4d6c88e08
lindexi <lindexi_gd@163.com>
2025-07-09 15:57:33 +0800

    定义序列化实现方法

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Serialization/XmlSerialization/StorageXmlSerializer.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Serialization/XmlSerialization/StorageXmlSerializer.cs
@@ -1,9 +1,22 @@
-﻿namespace DotNetCampus.Storage.Lib;
+﻿using System.Text.Json;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace DotNetCampus.Storage.Lib;
 
 public class StorageXmlSerializer
 {
     public StorageNode Parse()
     {
         return null!;
+    }
+
+    // Serialize
+
+    public async Task DeserializeAsync(FileInfo file)
+    {
+        //var xmlDocument = new XmlDocument();
+        using var fileStream = file.OpenRead();
+        XDocument document = await XDocument.LoadAsync(fileStream, LoadOptions.None, CancellationToken.None);
     }
 }
