2fda8d593c5cf800089e7515e00b14a75ccf68ee
lindexi <lindexi_gd@163.com>
2025-07-09 11:29:14 +0800

    测试遍历里层元素

--- Pptx/KurgehearkayNeelemwodall/Program.cs
+++ Pptx/KurgehearkayNeelemwodall/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Linq;
-
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Packaging;
-
+using DocumentFormat.OpenXml.Presentation;
 using Path = System.IO.Path;
+using Picture = DocumentFormat.OpenXml.Presentation.Picture;
+using TextBody = DocumentFormat.OpenXml.Presentation.TextBody;
 
 var pptxFile = Path.Join(AppContext.BaseDirectory, "Test.pptx");
 if (args.Length == 1)
@@ -16,8 +18,24 @@ using var presentationDocument =
     DocumentFormat.OpenXml.Packaging.PresentationDocument.Open(pptxFile, true);
 var presentationPart = presentationDocument.PresentationPart;
 var slideIndex = 0;
-foreach (SlidePart slidePart in presentationPart.SlideParts)
+var slideIdList = presentationPart.Presentation.SlideIdList;
+foreach (var slideId in slideIdList.OfType<SlideId>())
 {
-    Console.WriteLine($"SlideIndex={slideIndex}");
+    Console.WriteLine($"SlideIndex={slideIndex} SlideId={slideId.Id}");
     slideIndex++;
+
+    var slidePart = (SlidePart) presentationPart.GetPartById(slideId.RelationshipId);
+    var slide = slidePart.Slide;
+    foreach (var openXmlElement in slide.CommonSlideData.ShapeTree)
+    {
+        if (openXmlElement is DocumentFormat.OpenXml.AlternateContent alternateContent)
+        {
+            // +		[0]	{DocumentFormat.OpenXml.AlternateContentChoice}	DocumentFormat.OpenXml.OpenXmlElement {DocumentFormat.OpenXml.AlternateContentChoice}
+            // 
+            var alternateContentChoice = alternateContent.GetFirstChild<DocumentFormat.OpenXml.AlternateContentChoice>();
+            var textBody = alternateContentChoice.Descendants<TextBody>().First();
+
+            Console.WriteLine($"段落数量： {textBody.Descendants<DocumentFormat.OpenXml.Drawing.Paragraph>().Count()}");
+        }
+    }
 }
