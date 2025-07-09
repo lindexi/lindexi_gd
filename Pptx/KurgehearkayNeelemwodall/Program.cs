b864f10b3d50d56111507402b5a8ba28995408e7
lindexi <lindexi_gd@163.com>
2025-07-09 11:16:08 +0800

    测试不同的文件的加载

--- Pptx/KurgehearkayNeelemwodall/Program.cs
+++ Pptx/KurgehearkayNeelemwodall/Program.cs
@@ -4,8 +4,16 @@ using System.Linq;
 using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Packaging;
 
+using Path = System.IO.Path;
+
+var pptxFile = Path.Join(AppContext.BaseDirectory, "Test.pptx");
+if (args.Length == 1)
+{
+    pptxFile = args[0];
+}
+
 using var presentationDocument =
-    DocumentFormat.OpenXml.Packaging.PresentationDocument.Open("Test.pptx", true);
+    DocumentFormat.OpenXml.Packaging.PresentationDocument.Open(pptxFile, true);
 var presentationPart = presentationDocument.PresentationPart;
 var slideIndex = 0;
 foreach (SlidePart slidePart in presentationPart.SlideParts)
