68bfcb5eca1aba245946a6ae288918d4ffcc4667
lindexi <lindexi_gd@163.com>
2025-07-09 11:15:17 +0800

    测试3.0加载成功

---
+++ Pptx/KurgehearkayNeelemwodall/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Packaging;
+
+using var presentationDocument =
+    DocumentFormat.OpenXml.Packaging.PresentationDocument.Open("Test.pptx", true);
+var presentationPart = presentationDocument.PresentationPart;
+var slideIndex = 0;
+foreach (SlidePart slidePart in presentationPart.SlideParts)
+{
+    Console.WriteLine($"SlideIndex={slideIndex}");
+    slideIndex++;
+}
