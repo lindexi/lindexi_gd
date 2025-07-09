84b85a69371a3d10026b2fb20b8d659662e2f1d0
lindexi <lindexi_gd@163.com>
2025-07-09 17:46:43 +0800

    完成反序列化逻辑

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Serialization/XmlSerialization/StorageXmlSerializer.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Serialization/XmlSerialization/StorageXmlSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Generic;
+using System.Text.Json;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -13,6 +14,39 @@ public class StorageXmlSerializer
         using var fileStream = file.OpenRead();
         XDocument document = await XDocument.LoadAsync(fileStream, LoadOptions.None, CancellationToken.None);
         var root = document.Root!;
+        var rootNode = RecursiveParseNode(root);
+    }
 
+    public XName TypeName { get; } = XName.Get("Type", "Storage");
+
+    private StorageNode RecursiveParseNode(XElement currentElement)
+    {
+        List<StorageNode>? children = null;
+        if (currentElement.HasElements)
+        {
+            children = [];
+            var elements = currentElement.Elements();
+            foreach (var element in elements)
+            {
+                children.Add(RecursiveParseNode(element));
+            }
+        }
+
+        StorageNodeType storageNodeType = StorageNodeType.Unknown;
+        var type = currentElement.Attributes(TypeName).FirstOrDefault();
+        if (type is not null && Enum.TryParse(type.Value, out storageNodeType))
+        {
+        }
+
+        var storageNode = new StorageNode()
+        {
+            Name = currentElement.Name.LocalName,
+            Value = currentElement.Nodes().All(static x => x is XText) ?
+                currentElement.Value
+                : StorageTextSpan.NullValue,
+            StorageNodeType = storageNodeType,
+            Children = children,
+        };
+        return storageNode;
     }
 }
