b323b8cf2ee3bb7506810e29f8e84576f0639831
lindexi <lindexi_gd@163.com>
2025-07-09 14:23:54 +0800

    最简复现问题

---
+++ Pptx/WiheneakurFonejerarner/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DocumentFormat.OpenXml.Drawing;
+
+SchemeColor color = new SchemeColor()
+{
+    Val = SchemeColorValues.Accent1
+};
+SchemeColorValues value = color.Val.Value;
+Console.WriteLine(value);
