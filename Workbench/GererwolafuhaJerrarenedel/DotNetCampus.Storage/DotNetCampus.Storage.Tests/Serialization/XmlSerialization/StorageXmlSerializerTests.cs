000e2d1391369fc4a691e27e7291a2b29e4626f0
lindexi <lindexi_gd@163.com>
2025-07-09 16:22:58 +0800

    经过测试完全存放到内存里面

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Tests/Serialization/XmlSerialization/StorageXmlSerializerTests.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Tests/Serialization/XmlSerialization/StorageXmlSerializerTests.cs
@@ -3,9 +3,13 @@ using DotNetCampus.Storage.Lib;
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace DotNetCampus.Storage.Lib.Tests;
 
@@ -13,7 +17,13 @@ namespace DotNetCampus.Storage.Lib.Tests;
 public class StorageXmlSerializerTests
 {
     [TestMethod()]
-    public void ParseTest()
+    public async Task ParseTest()
     {
+        var testFile = @"C:\lindexi\slide1.xml";
+
+        using var fileStream = File.OpenRead(testFile);
+        var document = await XDocument.LoadAsync(fileStream, LoadOptions.None,CancellationToken.None);
+
+        //XmlReader xmlReader = XmlReader.Create(fileStream);
     }
 }
