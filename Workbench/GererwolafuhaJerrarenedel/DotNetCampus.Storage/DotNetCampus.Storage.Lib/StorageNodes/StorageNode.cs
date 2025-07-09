7d74ff29f2bf94daa50eb831eebc4075a78f8451
lindexi <lindexi_gd@163.com>
2025-07-09 15:47:01 +0800

    创建单元测试项目

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/StorageNodes/StorageNode.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/StorageNodes/StorageNode.cs
@@ -9,6 +9,8 @@ namespace DotNetCampus.Storage.Lib;
 /// <summary>
 /// 存储的数据最小单位。用于作为数据模型和具体文件存档格式的中间层
 /// </summary>
+/// 向下转换： 从 <see cref="StorageNode"/> 转换到具体的文件存档格式叫序列化，如 Json 序列化和 XML 序列化等，称为序列化
+/// 向上转换： 从 <see cref="StorageNode"/> 转换到业务的数据模型，如 SaveInfo 等，称为 Parse 转换，使用 Parser 转换器进行转换
 public class StorageNode
 {
     public StorageNodeType StorageNodeType { get; set; }
