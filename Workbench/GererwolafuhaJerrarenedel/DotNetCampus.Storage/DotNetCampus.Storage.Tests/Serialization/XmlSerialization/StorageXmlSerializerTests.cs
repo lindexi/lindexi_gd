d8925f20413c1fb50b2cc8fbedcf8063784cd36c
lindexi <lindexi_gd@163.com>
2025-07-09 16:42:02 +0800

    设计文档存储的写法

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Tests/Serialization/XmlSerialization/StorageXmlSerializerTests.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Tests/Serialization/XmlSerialization/StorageXmlSerializerTests.cs
@@ -25,5 +25,63 @@ public class StorageXmlSerializerTests
         var document = await XDocument.LoadAsync(fileStream, LoadOptions.None,CancellationToken.None);
 
         //XmlReader xmlReader = XmlReader.Create(fileStream);
+        // XML 文档内容：
+        /*
+        <Document Type="Element">
+        </Document>
+         */
     }
+
+    [TestMethod()]
+    public void ParseTest2()
+    {
+        // lang=xml
+        var xmlDocument =
+            """
+            <Document a:Type="Element" xmlns:a="http://schemas.lindexi.com/QarkeahekoYuhawharembaw">
+              <ElementList a:Type="Property">
+                <Picture a:Type="Element" X="100" Y="100">
+                  <Fill a:Type="Property">
+                     <Blip a:Type="Element" Source="Fooxcxszczxc"/>
+                  </Fill>
+                  <!-- <PresetMode a:Type="Property"> -->
+                  <!--   <Effect a:Type="Property"> -->
+                  <!--     <EffectMode a:Type="Element" Type="In" /> -->
+                  <!--   </Effect> -->
+                  <!-- </PresetMode> -->
+                </Picture>
+              </ElementList>
+            </Document>
+            """;
+        var document = XDocument.Parse(xmlDocument);
+        XElement root = document.Root!;
+        foreach (XAttribute attribute in root.Attributes())
+        {
+            
+        }
+    }
+
+    class Document
+    {
+        public List<IElement> ElementList { get; }
+    }
+    interface IElement { }
+    class Picture
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Blip? Fill { get; set; }
+        //public Effect? PresetMode { get; set; }
+    }
+
+    class Blip
+    {
+        public string? Source { get; set; }
+    }
+
+    //class Effect
+    //{
+
+    //}
 }
+
