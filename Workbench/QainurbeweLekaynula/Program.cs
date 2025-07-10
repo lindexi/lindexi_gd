bdeac9ee7f1527aabc59e3313b82911436f275b9
lindexi <lindexi_gd@163.com>
2025-07-10 10:39:53 +0800

    无法处理转义内容

--- Workbench/QainurbeweLekaynula/Program.cs
+++ Workbench/QainurbeweLekaynula/Program.cs
@@ -4,9 +4,12 @@ using System.Xml;
 using System.Xml.Linq;
 
 string text = "\x0001";
+
+string encodeName = XmlConvert.EncodeName(text);
+
 var element = new XElement("Node")
 {
-    Value = text
+    Value = encodeName
 };
 
 var stringWriter = new StringWriter();
@@ -16,4 +19,14 @@ using (var xmlWriter = XmlWriter.Create(stringWriter))
     var document = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), element);
     document.WriteTo(xmlWriter);
 }
+
+var xmlText = stringWriter.GetStringBuilder().ToString();
+
+if (!string.IsNullOrEmpty(xmlText))
+{
+    var document = XDocument.Parse(xmlText);
+    var rootElement = document.Root;
+    var value = rootElement?.Value;
+}
+
 Console.WriteLine("Hello, World!");
