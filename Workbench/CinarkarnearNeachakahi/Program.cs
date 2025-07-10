11556778f931ab8b69d938a14192ea6729bfdfbd
lindexi <lindexi_gd@163.com>
2025-07-10 09:50:29 +0800

    测试强行归还无效的数组内容

---
+++ Workbench/CinarkarnearNeachakahi/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Buffers;
+
+var buffer = new byte[1024];
+ArrayPool<byte>.Shared.Return(buffer);
+Console.WriteLine("Hello, World!");
