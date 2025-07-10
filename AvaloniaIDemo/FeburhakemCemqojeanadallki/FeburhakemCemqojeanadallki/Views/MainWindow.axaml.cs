ab3927abc1684381c9793cb980ba5b1261bdba0d
lindexi <lindexi_gd@163.com>
2025-07-10 15:02:46 +0800

    测试离屏渲染

---
+++ AvaloniaIDemo/FeburhakemCemqojeanadallki/FeburhakemCemqojeanadallki/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FeburhakemCemqojeanadallki.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
