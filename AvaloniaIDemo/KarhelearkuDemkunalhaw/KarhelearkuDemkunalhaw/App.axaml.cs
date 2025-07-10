0ddbc9feaf280ab2fcc857c5e34f6d8a1a80b67d
lindexi <lindexi_gd@163.com>
2025-07-10 11:34:19 +0800

    渲染的都是模糊的内容

--- AvaloniaIDemo/KarhelearkuDemkunalhaw/KarhelearkuDemkunalhaw/App.axaml.cs
+++ AvaloniaIDemo/KarhelearkuDemkunalhaw/KarhelearkuDemkunalhaw/App.axaml.cs
@@ -2,6 +2,7 @@
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Controls.Embedding;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
 
 using KarhelearkuDemkunalhaw.Views;
 
@@ -24,15 +25,22 @@ public partial class App : Application
             };
 
             var embeddableControlRoot = new EmbeddableControlRoot();
+            embeddableControlRoot.Width = 1000;
+            embeddableControlRoot.Height = 600;
             var mainView = new MainView
             {
             };
             embeddableControlRoot.Content = mainView;
             mainView.Loaded += (sender, args) =>
             {
+                var renderTargetBitmap = new RenderTargetBitmap(new PixelSize((int) mainView.Bounds.Width, (int) mainView.Bounds.Height));
+                renderTargetBitmap.Render(mainView);
 
+                renderTargetBitmap.Save("1.png");
             };
             embeddableControlRoot.Prepare(); // 调用此方法会触发 Loaded 事件
+            embeddableControlRoot.StartRendering();
+
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
