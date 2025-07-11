21e5e5e31d72909a643ceda88b2e15bfcbd9ff5c
lindexi <lindexi_gd@163.com>
2025-07-11 09:51:15 +0800

    证明顺序是无关的

---
+++ Workbench/BawheajurnenurdemHerlejeefay/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (var i = 0; i < 100000; i++)
+{
+    var origin = Random.Shared.Next();
+    var a = Random.Shared.Next();
+    var b = Random.Shared.Next();
+
+    var t = origin ^ a;
+    t = t ^ b;
+
+    var t2 = origin ^ b;
+    t2 = t2 ^ a;
+
+    var t3 = t ^ a ^ b;
+    var t4 = t2 ^ b ^ a;
+
+    if (t == t2)
+    {
+        // 预期是相等的
+    }
+    else
+    {
+        return;
+    }
+
+    if (t3 == origin)
+    {
+
+    }
+    else
+    {
+        return;
+    }
+
+    if (t3 == t4)
+    {
+
+    }
+    else
+    {
+        return;
+    }
+}
+
+Console.WriteLine("Hello, World!");
