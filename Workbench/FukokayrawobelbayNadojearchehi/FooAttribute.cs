be77ff474952364bb8f4b33142b7ab9af2f10366
lindexi <lindexi_gd@163.com>
2025-07-11 17:11:32 +0800

    添加单元测试输入内容

---
+++ Workbench/FukokayrawobelbayNadojearchehi/FooAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace FukokayrawobelbayNadojearchehi;
+
+public class FooAttribute : Attribute, ITestDataSource
+{
+    public IEnumerable<object?[]> GetData(MethodInfo methodInfo)
+    {
+        return [[123]];
+    }
+
+    public string? GetDisplayName(MethodInfo methodInfo, object?[]? data)
+    {
+        return "Fxxx";
+    }
+}
---
+++ Workbench/FukokayrawobelbayNadojearchehi/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common.Hosting;
+
+namespace FukokayrawobelbayNadojearchehi;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    [FooAttribute]
+    public void TestMethod1(int n)
+    {
+        
+    }
+}
