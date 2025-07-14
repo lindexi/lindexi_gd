91fdf44f224eda0dfdceef3562a2c842305ed55e
lindexi <lindexi_gd@163.com>
2025-07-14 17:05:23 +0800

    添加单元测试

---
+++ Workbench/FawkeqawjageJuqugeaha/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MSTest.Extensions.Contracts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FawkeqawjageJuqugeaha;
+
+[TestClass]
+public class Class1
+{
+    [ContractTestCase]
+    public void TestMethod1()
+    {
+        "单元测试1".Test(() =>
+        {
+            var expect = 1;
+            var actual = 1;
+            Assert.AreEqual(expect, actual);
+        });
+    }
+}
