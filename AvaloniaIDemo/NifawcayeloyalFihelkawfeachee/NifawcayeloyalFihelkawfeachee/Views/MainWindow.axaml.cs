979806f4352258672c47fef91c71f9f6e6ec4457
lindexi <lindexi_gd@163.com>
2025-07-15 15:16:55 +0800

    记录 ReSharper 无法识别属性

---
+++ AvaloniaIDemo/NifawcayeloyalFihelkawfeachee/NifawcayeloyalFihelkawfeachee/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NifawcayeloyalFihelkawfeachee.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
