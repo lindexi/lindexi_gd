31318b24ff8656a05f0f81bef867b501e425d6bf
lindexi <lindexi_gd@163.com>
2025-07-16 20:26:33 +0800

    Excessive FirstChanceExceptions in NamedPipeClientStream on Linux when connecting to a non-existent target

---
+++ Workbench/BeekefanereHawgudairwochemwhi/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.IO.Pipes;
+
+AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
+{
+    Console.WriteLine($"FirstChanceException: {eventArgs.Exception.GetType().FullName} {eventArgs.Exception.Message}");
+};
+
+var namedPipeClientStream = new NamedPipeClientStream("DoNotExistFoo");
+namedPipeClientStream.Connect();
+
+Console.WriteLine("Hello, World!");
