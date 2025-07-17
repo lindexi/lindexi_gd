a10210428107d0c3b2220fb958e21c7d42c9433f
lindexi <lindexi_gd@163.com>
2025-07-17 20:37:28 +0800

    测试计算方法

---
+++ Workbench/LefalceyeNerenerchanairlaijair/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var second = TimeSpan.FromMinutes(15).TotalSeconds;
+var value = Random.Shared.Next(0, (int)second);
+
+Console.WriteLine(TimeSpan.FromSeconds(value).TotalMinutes);
