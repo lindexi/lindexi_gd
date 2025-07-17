cf2df0e131bc5d427c08bca13ed6ddb693dbf799
lindexi <lindexi_gd@163.com>
2025-07-17 20:30:05 +0800

    添加画刷类型

---
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Primitive/SolidColorSkiaTextBrush.cs
@@ -0,0 +1,21 @@
+﻿using SkiaSharp;
+
+namespace LightTextEditorPlus.Primitive;
+
+/// <summary>
+/// 纯色画刷
+/// </summary>
+/// <param name="color"></param>
+public sealed class SolidColorSkiaTextBrush(SKColor color) : SkiaTextBrush
+{
+    /// <summary>
+    /// 画刷的颜色
+    /// </summary>
+    public SKColor Color => color;
+
+    /// <inheritdoc />
+    protected internal override void Apply(SKPaint paint)
+    {
+        paint.Color = Color;
+    }
+}
---
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Primitive/TextSkiaBrush.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace LightTextEditorPlus.Primitive;
+
+/// <summary>
+/// 文本的 Skia 渲染画刷
+/// </summary>
+public abstract class SkiaTextBrush
+{
+    /// <summary>
+    /// 应用画刷到指定的 <see cref="SKPaint"/> 对象上
+    /// </summary>
+    /// <param name="paint"></param>
+    protected internal abstract void Apply(SKPaint paint);
+
+    /// <summary>
+    /// 转换纯色画刷
+    /// </summary>
+    /// <param name="color"></param>
+    public static implicit operator SkiaTextBrush(SKColor color)
+    {
+        return new SolidColorSkiaTextBrush(color);
+    }
+}
