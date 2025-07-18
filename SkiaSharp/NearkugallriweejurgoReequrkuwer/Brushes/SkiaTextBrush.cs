a13b7fd4a2c6be43987c1b921e443006cfc48369
lindexi <lindexi_gd@163.com>
2025-07-18 09:48:31 +0800

    测试渲染渐变色

---
+++ SkiaSharp/NearkugallriweejurgoReequrkuwer/Brushes/SkiaTextBrush.cs
@@ -0,0 +1,71 @@
+﻿using SkiaSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace NearkugallriweejurgoReequrkuwer.Brushes;
+/// <summary>
+/// 文本的 Skia 渲染画刷
+/// </summary>
+public abstract class SkiaTextBrush
+{
+    /// <summary>
+    /// 应用画刷到指定的 <see cref="SKPaint"/> 对象上
+    /// </summary>
+    /// <param name="paint"></param>
+    protected internal abstract void Apply(SKPaint paint);
+
+    ///// <summary>
+    ///// 转换纯色画刷
+    ///// </summary>
+    ///// <param name="color"></param>
+    //public static implicit operator SkiaTextBrush(SKColor color)
+    //{
+    //    return new SolidColorSkiaTextBrush(color);
+    //}
+}
+
+
+public sealed class LinearGradientSkiaTextBrush : SkiaTextBrush
+{
+    public SKPoint StartPoint { get; set; }
+    public SKPoint EndPoint { get; set; }
+    //public double Opacity { get; set; }
+    public SkiaTextGradientStopCollection GradientStops { get; set; } = [];
+
+    /// <inheritdoc />
+    protected internal override void Apply(SKPaint paint)
+    {
+        var (colorList, offsetList) = GradientStops.GetList();
+
+        var linearGradient = SKShader.CreateLinearGradient(StartPoint, EndPoint, colorList, offsetList, SKShaderTileMode.Clamp);
+        paint.Shader = linearGradient;
+    }
+}
+
+public class SkiaTextGradientStopCollection : List<SkiaTextGradientStop>
+{
+    internal (SKColor[] ColorList, float[] OffsetList) GetList()
+    {
+        SKColor[] colorList = new SKColor[this.Count];
+        float[] offsetList = new float[this.Count];
+
+        for (var i = 0; i < this.Count; i++)
+        {
+            var (skColor, offset) = this[i];
+            colorList[i] = skColor;
+            offsetList[i] = offset;
+        }
+
+        return (colorList, offsetList);
+    }
+}
+
+public readonly record struct SkiaTextGradientStop(SKColor Color, float Offset)
+{
+}
---
+++ SkiaSharp/NearkugallriweejurgoReequrkuwer/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace NearkugallriweejurgoReequrkuwer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void RepeatButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        SkiaCanvas.Draw();
+    }
+}
