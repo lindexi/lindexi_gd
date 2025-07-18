a3884632a47c57a364213f80237b66c5e00b1c60
lindexi <lindexi_gd@163.com>
2025-07-18 20:41:59 +0800

    尝试集合表达式但失败

--- LightTextEditorPlus/LightTextEditorPlus.Skia/Primitive/Brushes/SkiaTextGradientStopCollection.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Primitive/Brushes/SkiaTextGradientStopCollection.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections.Generic;
-using SkiaSharp;
+﻿using SkiaSharp;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace LightTextEditorPlus.Primitive;
 
 /// <summary>
 /// 渐变色刻度集合
 /// </summary>
-public class SkiaTextGradientStopCollection : List<SkiaTextGradientStop>
+[CollectionBuilderAttribute(typeof(SkiaTextGradientStopCollectionBuilder), "Create")]
+public class SkiaTextGradientStopCollection : IReadOnlyList<SkiaTextGradientStop>
 {
+    public SkiaTextGradientStopCollection(IReadOnlyList<SkiaTextGradientStop> list)
+    {
+    }
+
     internal (SKColor[] ColorList, float[] OffsetList) GetList(double opacity)
     {
         SKColor[] colorList = new SKColor[this.Count];
@@ -22,4 +30,50 @@ public class SkiaTextGradientStopCollection : List<SkiaTextGradientStop>
 
         return (colorList, offsetList);
     }
+
+    public IEnumerator<SkiaTextGradientStop> GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public int Count { get; set; }
+
+    public SkiaTextGradientStop this[int index]
+    {
+        get => throw new System.NotImplementedException();
+    }
+}
+
+static class SkiaTextGradientStopCollectionBuilder
+{
+    public static SkiaTextGradientStopCollection Create(IEnumerable<SkiaTextGradientStop> stops)
+    {
+        if (stops is null)
+        {
+            throw new ArgumentNullException(nameof(stops));
+        }
+        return new SkiaTextGradientStopCollection(new List<SkiaTextGradientStop>(stops));
+    }
+}
+
+[System.AttributeUsage(
+    System.AttributeTargets.Class | System.AttributeTargets.Interface | System.AttributeTargets.Struct,
+    Inherited = false)]
+file sealed class CollectionBuilderAttribute : Attribute
+{
+    public Type BuilderType { get; }
+    public string MethodName { get; }
+
+    public CollectionBuilderAttribute(Type builderType, string methodName)
+    {
+        BuilderType = builderType;
+        MethodName = methodName;
+    }
+
+
 }
