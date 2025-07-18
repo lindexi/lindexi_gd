e099d351fe040e34959e2011445b2d9e099c9f1f
lindexi <lindexi_gd@163.com>
2025-07-18 10:20:44 +0800

    完成渐变色的基础实现

--- LightTextEditorPlus/LightTextEditorPlus.Skia/Primitive/Brushes/SolidColorSkiaTextBrush.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Primitive/Brushes/SolidColorSkiaTextBrush.cs
@@ -1,10 +1,9 @@
-﻿using LightTextEditorPlus.Core.Primitive;
+﻿using System;
 
 using SkiaSharp;
 
 using System.Collections.Generic;
 
-using static LightTextEditorPlus.Primitive.GradientSkiaTextBrushRelativePoint;
 
 namespace LightTextEditorPlus.Primitive;
 
@@ -20,27 +19,45 @@ public sealed class SolidColorSkiaTextBrush(SKColor color) : SkiaTextBrush
     public SKColor Color => color;
 
     /// <inheritdoc />
-    protected internal override void Apply(SKPaint paint)
+    protected internal override void Apply(in SkiaTextBrushRenderContext context)
     {
-        paint.Color = Color;
+        context.Paint.Color = color;
     }
 }
 
 public sealed class LinearGradientSkiaTextBrush : SkiaTextBrush
 {
-    public GradientSkiaTextBrushRelativePoint StartPoint { get; set; }
-    public GradientSkiaTextBrushRelativePoint EndPoint { get; set; }
-    public double Opacity { get; set; }
-    public SkiaTextGradientStopCollection GradientStops { get; set; } = [];
+    public GradientSkiaTextBrushRelativePoint StartPoint { get; init; }
+    public GradientSkiaTextBrushRelativePoint EndPoint { get; init; }
+    //public double Opacity { get; init; }
+    public SkiaTextGradientStopCollection GradientStops { get; init; } = [];
 
     /// <inheritdoc />
-    protected internal override void Apply(SKPaint paint)
+    protected internal override void Apply(in SkiaTextBrushRenderContext context)
     {
+        SKPaint paint = context.Paint;
+
+        SKRect renderBounds = context.RenderBounds;
+        SKPoint startPoint = StartPoint.ToSKPoint(renderBounds);
+        SKPoint endPoint = EndPoint.ToSKPoint(renderBounds);
+
+        var (colorList, offsetList) = GradientStops.GetList();
+        var linearGradient = SKShader.CreateLinearGradient(startPoint, endPoint, colorList, offsetList, SKShaderTileMode.Clamp);
+        paint.Shader = linearGradient;
     }
 }
 
-public struct GradientSkiaTextBrushRelativePoint(float X, float Y, RelativeUnit Unit)
+public readonly record struct GradientSkiaTextBrushRelativePoint(float X, float Y, GradientSkiaTextBrushRelativePoint.RelativeUnit Unit)
 {
+    public SKPoint ToSKPoint(SKRect bounds)
+    {
+        return Unit switch
+        {
+            RelativeUnit.Relative => new SKPoint(bounds.Left + X * bounds.Width, bounds.Top + Y * bounds.Height),
+            RelativeUnit.Absolute => new SKPoint(X, Y),
+            _ => throw new ArgumentOutOfRangeException(nameof(Unit), Unit, null)
+        };
+    }
 
     public enum RelativeUnit : byte
     {
@@ -51,8 +68,22 @@ public struct GradientSkiaTextBrushRelativePoint(float X, float Y, RelativeUnit 
 
 public class SkiaTextGradientStopCollection : List<SkiaTextGradientStop>
 {
+    internal (SKColor[] ColorList, float[] OffsetList) GetList()
+    {
+        SKColor[] colorList = new SKColor[this.Count];
+        float[] offsetList = new float[this.Count];
+
+        for (var i = 0; i < this.Count; i++)
+        {
+            var (skColor, offset) = this[i];
+            colorList[i] = skColor;
+            offsetList[i] = offset;
+        }
+
+        return (colorList, offsetList);
+    }
 }
 
-public readonly record struct SkiaTextGradientStop(SKColor Color, double Offset)
+public readonly record struct SkiaTextGradientStop(SKColor Color, float Offset)
 {
 }
