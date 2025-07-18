1d5cc940aef5d4889f54e34cde97f4f232123380
lindexi <lindexi_gd@163.com>
2025-07-18 19:50:05 +0800

    测试纯色的写法

---
+++ AvaloniaIDemo/ChanajujoweeWhurwarallherqawje/ChanajujoweeWhurwarallherqawje/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ChanajujoweeWhurwarallherqawje.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
