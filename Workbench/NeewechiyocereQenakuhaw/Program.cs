2007996b00dab6134265a84df03f2d2728418f96
lindexi <lindexi_gd@163.com>
2025-07-22 10:53:16 +0800

    测试图片格式转换

---
+++ Workbench/NeewechiyocereQenakuhaw/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ImageMagick;
+
+var file = @"C:\lindexi\sample.wmf";
+var outputFile = Path.GetFullPath(@"sample.png");
+
+using var image = new MagickImage(file);
+image.Write(new FileInfo(outputFile), MagickFormat.Png32);
+
+Console.WriteLine("Hello, World!");
