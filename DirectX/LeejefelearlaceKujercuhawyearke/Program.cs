c68d8ff69dfaa16e11f272068dba0909ed37a329
lindexi <lindexi_gd@163.com>
2025-07-23 17:30:01 +0800

    删除测试代码

--- DirectX/LeejefelearlaceKujercuhawyearke/Program.cs
+++ DirectX/LeejefelearlaceKujercuhawyearke/Program.cs
@@ -6,40 +6,12 @@ using Silk.NET.DirectWrite;
 DWrite dWrite = DWrite.GetApi();
 ComPtr<IDWriteFactory6> factory = dWrite.DWriteCreateFactory<IDWriteFactory6>(FactoryType.Shared);
 
+// 宋体字体
 var fontFile = @"C:\windows\fonts\simsun.ttc";
 
 unsafe
 {
-    IDWriteInMemoryFontFileLoader* dWriteInMemoryFontFileLoader;
-    factory.Handle->CreateInMemoryFontFileLoader(&dWriteInMemoryFontFileLoader);
-
-    ComPtr<IDWriteFontFileLoader> dWriteFontFileLoader =
-        dWriteInMemoryFontFileLoader->QueryInterface<IDWriteFontFileLoader>();
-    factory.Handle->RegisterFontFileLoader(dWriteFontFileLoader.Handle);
-
     HResult hr = 0;
-
-    IDWriteFontFile* dWriteFontFile;
-    fixed (char* pFontFile = fontFile)
-    {
-        factory.Handle->CreateFontFileReference(pFontFile, null, &dWriteFontFile);
-    }
-
-    // isSupported, &fileType, &fontFaceType, &numberOfFonts
-    int isSupported = 0;
-    FontFileType fileType = FontFileType.Unknown;
-    FontFaceType fontFaceType = FontFaceType.Unknown;
-    uint numberOfFonts = 0;
-    hr = dWriteFontFile->Analyze(&isSupported, &fileType, &fontFaceType, &numberOfFonts);
-    hr.Throw();
-
-    IDWriteFontSetBuilder* fontSetBuilder;
-    factory.Handle->CreateFontSetBuilder(&fontSetBuilder);
-
-    //IDWriteFontFace* fontFace;
-    //hr = factory.Handle->CreateFontFace(fontFaceType, numberOfFonts, &dWriteFontFile, 0, FontSimulations.None,
-    //    &fontFace);
-    //hr.Throw();
 
     IDWriteFontFaceReference* fontFaceReference;
 
@@ -50,8 +22,6 @@ unsafe
         hr.Throw();
     }
 
-    fontSetBuilder->AddFontFaceReference(fontFaceReference);
-
     IDWriteFontFace3* fontFace3;
     fontFaceReference->CreateFontFace(&fontFace3);
 
@@ -59,8 +29,6 @@ unsafe
     fontFace3->GetMetrics(&fontMetrics);
 
     IDWriteLocalizedStrings* dWriteLocalizedStrings;
-    //fontFace3->GetFaceNames(&dWriteLocalizedStrings);
-    //PrintLocalizedStrings(dWriteLocalizedStrings);
 
     fontFace3->GetFamilyNames(&dWriteLocalizedStrings);
     PrintLocalizedStrings(dWriteLocalizedStrings);
