03a8b9e841fecbc4baaa1efc32ea663fef290439
lindexi <lindexi_gd@163.com>
2025-07-24 20:50:42 +0800

    尝试解析带有特殊字符的文件，解析失败

---
+++ Workbench/HunelerkilereneWairhelyalererallji/Program.cs
@@ -0,0 +1,20 @@
+﻿using SkiaSharp;
+
+using Svg;
+using Svg.Skia;
+
+namespace HunelerkilereneWairhelyalererallji;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var file = "1.svg";
+        file = Path.GetFullPath(file);
+
+        var skSvg = new SKSvg();
+        skSvg.Load(file);
+
+        skSvg.Save("1.png", SKColors.Transparent);
+    }
+}
