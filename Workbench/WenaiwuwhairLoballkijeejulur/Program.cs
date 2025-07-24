2fadd70dd248f95b040990e8d72438a150951a11
lindexi <lindexi_gd@163.com>
2025-07-24 17:57:14 +0800

    尝试在 linux 转换但失败

---
+++ Workbench/WenaiwuwhairLoballkijeejulur/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ImageMagick;
+
+if (args.Length == 0)
+{
+    return;
+}
+
+var file = args[0];
+var fileName = Path.GetFileNameWithoutExtension(file);
+
+var outputFile = Path.GetFullPath($"{fileName}.png");
+
+using var image = new MagickImage(file, new MagickReadSettings()
+{
+    BackgroundColor = MagickColors.Transparent,
+    Format = MagickFormat.Wmf,
+});
+image.Write(new FileInfo(outputFile), MagickFormat.Png32);
+
+Console.WriteLine($"Finish convert. OutputFile={outputFile}");
