1ff13200e01a7d80a7f7c1cb3bce98bd4f16b277
lindexi <lindexi_gd@163.com>
2025-07-24 20:53:59 +0800

    修复特殊字符读取失败

--- Workbench/HunelerkilereneWairhelyalererallji/Program.cs
+++ Workbench/HunelerkilereneWairhelyalererallji/Program.cs
@@ -1,4 +1,5 @@
-﻿using SkiaSharp;
+﻿using System.Xml.Linq;
+using SkiaSharp;
 
 using Svg;
 using Svg.Skia;
@@ -12,8 +13,14 @@ internal class Program
         var file = "1.svg";
         file = Path.GetFullPath(file);
 
+        var fileStream = File.OpenRead(file);
+        var streamReader = new StreamReader(fileStream);
+
+        var xDocument = XDocument.Load(streamReader, LoadOptions.SetLineInfo);
+        xDocument.Save("2.svg");
+
         var skSvg = new SKSvg();
-        skSvg.Load(file);
+        skSvg.Load("2.svg");
 
         skSvg.Save("1.png", SKColors.Transparent);
     }
