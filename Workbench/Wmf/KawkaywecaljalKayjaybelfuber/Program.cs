8c959cab00f44a682cd49bb46097328753d7da82
lindexi <lindexi_gd@163.com>
2025-07-27 16:15:07 +0800

    挣扎加入计算，但依然错误

--- Workbench/Wmf/KawkaywecaljalKayjaybelfuber/Program.cs
+++ Workbench/Wmf/KawkaywecaljalKayjaybelfuber/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using Oxage.Wmf;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -37,8 +38,8 @@ var width = Math.Abs(format.Right - format.Left);
 var height = Math.Abs(format.Bottom - format.Top);
 
 var inchUnit = format.Unit;
-var pixelWidth = (double) width / inchUnit * 96;
-var pixelHeight = (double) height / inchUnit * 96;
+var pixelWidth = (double)width / inchUnit * 96;
+var pixelHeight = (double)height / inchUnit * 96;
 
 var skBitmap = new SKBitmap(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
 SKCanvas canvas = new SKCanvas(skBitmap);
@@ -89,58 +90,59 @@ Encoding CharacterSetToEncoding(CharacterSet characterSet)
     return Encoding.GetEncoding(codePageId);
 }
 
-foreach (var wmfDocumentRecord in wmfDocument.Records)
+for (var i = 0; i < wmfDocument.Records.Count; i++)
 {
+    var wmfDocumentRecord = wmfDocument.Records[i];
     switch (wmfDocumentRecord)
     {
         // The META_SETBKMODE Record defines the background raster operation mix mode in the playback device context. The background mix mode is the mode for combining pens, text, hatched brushes, and interiors of filled objects with background colors on the output surface.
         case WmfSetBkModeRecord setBkModeRecord:
-            {
-                // RecordFunction (2 bytes): A 16-bit unsigned integer that defines this WMF record type. The lower byte MUST match the lower byte of the RecordType Enumeration (section 2.1.1.1) table value META_SETBKMODE.
-                // BkMode (2 bytes): A 16-bit unsigned integer that defines background mix mode. This MUST be one of the values in the MixMode Enumeration (section 2.1.1.20).
+        {
+            // RecordFunction (2 bytes): A 16-bit unsigned integer that defines this WMF record type. The lower byte MUST match the lower byte of the RecordType Enumeration (section 2.1.1.1) table value META_SETBKMODE.
+            // BkMode (2 bytes): A 16-bit unsigned integer that defines background mix mode. This MUST be one of the values in the MixMode Enumeration (section 2.1.1.20).
 
-                break;
-            }
+            break;
+        }
         case WmfSetTextAlignRecord setTextAlignRecord:
-            {
-                // RecordFunction (2 bytes): A 16-bit unsigned integer that defines this WMF record type. The lower byte MUST match the lower byte of the RecordType Enumeration (section 2.1.1.1) table value META_SETTEXTALIGN.
-                // TextAlignmentMode (2 bytes): A 16-bit unsigned integer that defines text alignment. This value MUST be a combination of one or more TextAlignmentMode Flags (section 2.1.2.3) for text with a horizontal baseline, and VerticalTextAlignmentMode Flags (section 2.1.2.4) for text with a vertical baseline.
+        {
+            // RecordFunction (2 bytes): A 16-bit unsigned integer that defines this WMF record type. The lower byte MUST match the lower byte of the RecordType Enumeration (section 2.1.1.1) table value META_SETTEXTALIGN.
+            // TextAlignmentMode (2 bytes): A 16-bit unsigned integer that defines text alignment. This value MUST be a combination of one or more TextAlignmentMode Flags (section 2.1.2.3) for text with a horizontal baseline, and VerticalTextAlignmentMode Flags (section 2.1.2.4) for text with a vertical baseline.
 
-                break;
-            }
+            break;
+        }
         case WmfCreatePenIndirectRecord createPenIndirectRecord:
-            {
-                Color color = createPenIndirectRecord.Color;
-                currentPenColor = new SKColor(color.R, color.G, color.B, color.A);
-                currentPenThickness = Math.Max(createPenIndirectRecord.Width.X, createPenIndirectRecord.Width.Y);
-                break;
-            }
+        {
+            Color color = createPenIndirectRecord.Color;
+            currentPenColor = new SKColor(color.R, color.G, color.B, color.A);
+            currentPenThickness = Math.Max(createPenIndirectRecord.Width.X, createPenIndirectRecord.Width.Y);
+            break;
+        }
         // -		[11]	{== WmfMoveToRecord ==
         // RecordSize: 5 words = 10 bytes
         // RecordType: 0x0214 (RecordType.META_MOVETO)
         // X: 1372Y: 865}	Oxage.Wmf.IBinaryRecord {Oxage.Wmf.Records.WmfMoveToRecord}
         // 
         case WmfMoveToRecord moveToRecord:
-            {
-                currentX = moveToRecord.X;
-                currentY = moveToRecord.Y;
-                break;
-            }
+        {
+            currentX = moveToRecord.X;
+            currentY = moveToRecord.Y;
+            break;
+        }
         // -		[12]	{== WmfLineToRecord ==
         // RecordSize: 5 words = 10 bytes
         // RecordType: 0x0213 (RecordType.META_LINETO)
         // X: 1840Y: 865}	Oxage.Wmf.IBinaryRecord {Oxage.Wmf.Records.WmfLineToRecord}
         // 
         case WmfLineToRecord lineToRecord:
-            {
-                paint.IsStroke = true;
-                paint.Color = currentPenColor;
-                paint.StrokeWidth = currentPenThickness;
+        {
+            paint.IsStroke = true;
+            paint.Color = currentPenColor;
+            paint.StrokeWidth = currentPenThickness;
 
-                canvas.DrawLine(currentX, currentY, lineToRecord.X, lineToRecord.Y, paint);
+            canvas.DrawLine(currentX, currentY, lineToRecord.X, lineToRecord.Y, paint);
 
-                break;
-            }
+            break;
+        }
         // -		[13]	{== WmfSetTextColorRecord ==
         // RecordSize: 5 words = 10 bytes
         // RecordType: 0x0209 (RecordType.META_SETTEXTCOLOR)
@@ -148,11 +150,11 @@ foreach (var wmfDocumentRecord in wmfDocument.Records)
         // }	Oxage.Wmf.IBinaryRecord {Oxage.Wmf.Records.WmfSetTextColorRecord}
         // 
         case WmfSetTextColorRecord setTextColorRecord:
-            {
-                currentPenColor = ToSKColor(setTextColorRecord.Color);
+        {
+            currentPenColor = ToSKColor(setTextColorRecord.Color);
 
-                break;
-            }
+            break;
+        }
         // -		[15]	{== WmfCreateFontIndirectRecord ==
         // RecordSize: 28 words = 56 bytes
         // RecordType: 0x02fb (RecordType.META_CREATEFONTINDIRECT)
@@ -173,67 +175,96 @@ foreach (var wmfDocumentRecord in wmfDocument.Records)
         // Facename: Times New Roman	Oxage.Wmf.IBinaryRecord {Oxage.Wmf.Records.WmfCreateFontIndirectRecord}
         // 
         case WmfCreateFontIndirectRecord createFontIndirectRecord:
-            {
-                // 	"Times New Roman\0è±ñwñ±ñw @ów³\u0011fÁ"
-                currentFontName = createFontIndirectRecord.FaceName.ToString();
-                currentCharacterSet = createFontIndirectRecord.CharSet;
-                currentEncoding = CharacterSetToEncoding(currentCharacterSet);
+        {
+            // 	"Times New Roman\0è±ñwñ±ñw @ów³\u0011fÁ"
+            currentFontName = createFontIndirectRecord.FaceName.ToString();
+            currentCharacterSet = createFontIndirectRecord.CharSet;
+            currentEncoding = CharacterSetToEncoding(currentCharacterSet);
 
-                // Width (2 bytes): A 16-bit signed integer that defines the average width, in logical units, of characters in the font. If Width is 0x0000, the aspect ratio of the device SHOULD be matched against the digitization aspect ratio of the available fonts to find the closest match, determined by the absolute value of the difference.
-                var fontWidth = createFontIndirectRecord.Width;
+            // Width (2 bytes): A 16-bit signed integer that defines the average width, in logical units, of characters in the font. If Width is 0x0000, the aspect ratio of the device SHOULD be matched against the digitization aspect ratio of the available fonts to find the closest match, determined by the absolute value of the difference.
+            var fontWidth = createFontIndirectRecord.Width;
 
-                // Height (2 bytes): A 16-bit signed integer that specifies the height, in logical units, of the font's character cell. The character height is computed as the character cell height minus the internal leading. The font mapper SHOULD interpret the height as follows.
-                // value < 0x0000 The font mapper SHOULD transform this value into device units and match its absolute value against the character height of available fonts.
-                // 0x0000 A default height value MUST be used when creating a physical font.
-                // 0x0000 < value The font mapper SHOULD transform this value into device units and match it against the cell height of available fonts.
-                // For all height comparisons, the font mapper SHOULD find the largest physical font that does not exceed the requested size.<40>
-                // <40> Section 2.2.1.2: All Windows versions: mapping the logical font size to the available physical fonts occurs the first time the logical font needs to be used in a drawing operation.
-                // For the MM_TEXT mapping mode, the following formula can be used to compute the height of a font with a specified point size.
-                // 	Height = -MulDiv(PointSize, GetDeviceCaps(hDC, LOGPIXELSY), 72);
-                // 
-                var fontHeight = createFontIndirectRecord.Height;
+            // Height (2 bytes): A 16-bit signed integer that specifies the height, in logical units, of the font's character cell. The character height is computed as the character cell height minus the internal leading. The font mapper SHOULD interpret the height as follows.
+            // value < 0x0000 The font mapper SHOULD transform this value into device units and match its absolute value against the character height of available fonts.
+            // 0x0000 A default height value MUST be used when creating a physical font.
+            // 0x0000 < value The font mapper SHOULD transform this value into device units and match it against the cell height of available fonts.
+            // For all height comparisons, the font mapper SHOULD find the largest physical font that does not exceed the requested size.<40>
+            // <40> Section 2.2.1.2: All Windows versions: mapping the logical font size to the available physical fonts occurs the first time the logical font needs to be used in a drawing operation.
+            // For the MM_TEXT mapping mode, the following formula can be used to compute the height of a font with a specified point size.
+            // 	Height = -MulDiv(PointSize, GetDeviceCaps(hDC, LOGPIXELSY), 72);
+            // 
+            var fontHeight = createFontIndirectRecord.Height;
 
-                fontWidth = Math.Abs(fontWidth);
-                fontHeight = Math.Abs(fontHeight);
+            fontWidth = Math.Abs(fontWidth);
+            fontHeight = Math.Abs(fontHeight);
 
-                currentFontSize = Math.Max(fontWidth, fontHeight);
+            currentFontSize = Math.Max(fontWidth, fontHeight);
 
-                break;
-            }
+            break;
+        }
         case WmfUnknownRecord unknownRecord:
+        {
+            switch (unknownRecord.RecordType)
             {
-                switch (unknownRecord.RecordType)
+                case RecordType.META_EXTTEXTOUT:
                 {
-                    case RecordType.META_EXTTEXTOUT:
+                    // 测试 17 项
+                    var memoryStream = new MemoryStream(unknownRecord.Data);
+                    var binaryReader = new BinaryReader(memoryStream);
+                    var ty = binaryReader.ReadUInt16();
+                    var tx = binaryReader.ReadUInt16();
+                    var stringLength = binaryReader.ReadUInt16();
+                    var fwOpts = (ExtTextOutOptions)binaryReader.ReadUInt16();
+                    // Rectangle (8 bytes): An optional 8-byte Rect Object (section 2.2.2.18).) When either ETO_CLIPPED, ETO_OPAQUE, or both are specified, the rectangle defines the dimensions, in logical coordinates, used for clipping, opaquing, or both. When neither ETO_CLIPPED nor ETO_OPAQUE is specified, the coordinates in Rectangle are ignored.
+                    var st = 8; /*2byte ofr ty tx stringLength fwOpts*/
+                    if (fwOpts is ExtTextOutOptions.ETO_CLIPPED or ExtTextOutOptions.ETO_OPAQUE)
+                    {
+                        // 此时才有 Rectangle 的值
+                        binaryReader.ReadBytes(8);
+                        st += 8;
+                    }
+
+                    st += stringLength;
+
+                    var stringBuffer = binaryReader.ReadBytes(stringLength);
+                    var text = currentEncoding.GetString(stringBuffer);
+
+                    // Dx (variable): An optional array of 16-bit signed integers that indicate the distance between origins of adjacent character cells. For example, Dx[i] logical units separate the origins of character cell i and character cell i + 1. If this field is present, there MUST be the same number of values as there are characters in the string.
+                    //Debug.Assert(st == unknownRecord.RecordSize);
+                    var dxLength = unknownRecord.Data.Length - st;
+
+                    skFont.Size = currentFontSize;
+                    skFont.Typeface = SKTypeface.FromFamilyName(currentFontName);
+                    paint.Style = SKPaintStyle.Fill;
+
+                    if (text.StartsWith("k"))
+                    {
+                    }
+
+                    if (dxLength == 0)
+                    {
+                        canvas.DrawText(text, currentX + tx, currentY + ty, skFont, paint);
+                    }
+                    else
+                    {
+                        var textIndex = 0;
+                        var currentXOffset = currentX + tx;
+                        while (dxLength > 0 && textIndex < text.Length)
                         {
-                            // 测试 17 项
-                            var memoryStream = new MemoryStream(unknownRecord.Data);
-                            var binaryReader = new BinaryReader(memoryStream);
-                            var ty = binaryReader.ReadUInt16();
-                            var tx = binaryReader.ReadUInt16();
-                            var stringLength = binaryReader.ReadUInt16();
-                            var fwOpts = (ExtTextOutOptions) binaryReader.ReadUInt16();
-                            // Rectangle (8 bytes): An optional 8-byte Rect Object (section 2.2.2.18).) When either ETO_CLIPPED, ETO_OPAQUE, or both are specified, the rectangle defines the dimensions, in logical coordinates, used for clipping, opaquing, or both. When neither ETO_CLIPPED nor ETO_OPAQUE is specified, the coordinates in Rectangle are ignored.
-                            if (fwOpts is ExtTextOutOptions.ETO_CLIPPED or ExtTextOutOptions.ETO_OPAQUE)
-                            {
-                                // 此时才有 Rectangle 的值
-                                binaryReader.ReadBytes(8);
-                            }
-
-                            var stringBuffer = binaryReader.ReadBytes(stringLength);
-                            var text = currentEncoding.GetString(stringBuffer);
-
-                            skFont.Size = currentFontSize;
-                            skFont.Typeface = SKTypeface.FromFamilyName(currentFontName);
-                            paint.Style = SKPaintStyle.Fill;
-                            canvas.DrawText(text, currentX + tx, currentY + ty, skFont, paint);
-
-                            break;
+                            dxLength -= sizeof(UInt16);
+                            var distance = binaryReader.ReadUInt16();
+                            currentXOffset += distance;
+                            canvas.DrawText(text[textIndex].ToString(), currentXOffset, currentY + ty, skFont, paint);
+                            textIndex++;
                         }
-                }
+                    }
 
-                break;
+                    break;
+                }
             }
+
+            break;
+        }
     }
 }
 
