79674f792e11b80bc2aefa33074cf3298808283c
lindexi <lindexi_gd@163.com>
2025-07-27 10:58:49 +0800

    似乎无法计算为像素单位

--- Workbench/Wmf/KawkaywecaljalKayjaybelfuber/Program.cs
+++ Workbench/Wmf/KawkaywecaljalKayjaybelfuber/Program.cs
@@ -8,8 +8,26 @@ using var fileStream = File.OpenRead(file);
 var wmfDocument = new WmfDocument();
 wmfDocument.Load(fileStream);
 
-var width = wmfDocument.Width;
-var height = wmfDocument.Height;
+
+var format = wmfDocument.Format;
+Console.WriteLine(format.Dump());
+
+// Left: 61528
+// Top: 62158
+// Right: 4008
+// Bottom: 3378
+// Unit: 1000
+// Checksum: 21749
+
+
+
+var width = Math.Abs(format.Left - format.Right);
+var height = Math.Abs(format.Top - format.Bottom);
+var aw = 607d;
+var ah = 512d;
+
+var t = width / aw;
+var t2 = height / ah;
 
 var inch = wmfDocument.Width / 1440;
 var pixel = inch * 96;
