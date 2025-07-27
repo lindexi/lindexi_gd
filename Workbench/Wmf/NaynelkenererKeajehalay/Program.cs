21a9c97bbf11595c11128db874052e6a5ac6e62f
lindexi <lindexi_gd@163.com>
2025-07-27 19:12:38 +0800

    测试加载图片但不支持

---
+++ Workbench/Wmf/NaynelkenererKeajehalay/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IronSoftware.Drawing;
+
+var file = @"C:\lindexi\wmf公式\image17.wmf";
+var bitmap = AnyBitmap.FromFile(file);
+Console.WriteLine("Hello, World!");
