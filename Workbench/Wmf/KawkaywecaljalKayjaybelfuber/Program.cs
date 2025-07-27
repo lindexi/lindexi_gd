3558804f751f20eb127faa1b73bad25de0a187af
lindexi <lindexi_gd@163.com>
2025-07-27 11:11:10 +0800

    加上对比测试内容

--- Workbench/Wmf/KawkaywecaljalKayjaybelfuber/Program.cs
+++ Workbench/Wmf/KawkaywecaljalKayjaybelfuber/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Drawing;
+using System.Drawing.Imaging;
 using Oxage.Wmf;
 
 var file = @"C:\lindexi\wmf公式\sample.wmf";
+
+var image = Image.FromFile(file);
+var imageWidth = image.Width;
+var imageHeight = image.Height;
 
 using var fileStream = File.OpenRead(file);
 var wmfDocument = new WmfDocument();
@@ -26,10 +32,21 @@ var height = Math.Abs(format.Top - format.Bottom);
 var aw = 607d;
 var ah = 512d;
 
-var t = width / aw;
-var t2 = height / ah;
+var t = width / 1440;
+var t2 = height / 1440;
+
+var formatUnit = format.Unit;
+
+// mnUnitsPerInch 
 
 var inch = wmfDocument.Width / 1440;
 var pixel = inch * 96;
+
+var wmfDocumentHeader = wmfDocument.Header;
+
+foreach (var wmfDocumentRecord in wmfDocument.Records)
+{
+    
+}
 
 Console.WriteLine("Hello, World!");
