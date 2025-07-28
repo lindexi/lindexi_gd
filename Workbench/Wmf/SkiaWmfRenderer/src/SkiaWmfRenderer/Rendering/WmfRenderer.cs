88550f5d1dd4c2d2823bcadc20f1507dcaa48ffa
lindexi <lindexi_gd@163.com>
2025-07-28 16:32:02 +0800

    测试不正确的值

--- Workbench/Wmf/SkiaWmfRenderer/src/SkiaWmfRenderer/Rendering/WmfRenderer.cs
+++ Workbench/Wmf/SkiaWmfRenderer/src/SkiaWmfRenderer/Rendering/WmfRenderer.cs
@@ -83,6 +83,9 @@ class WmfRenderer
         var width = Math.Abs(format.Right - format.Left);
         var height = Math.Abs(format.Bottom - format.Top);
 
+        width = format.Right;
+        height = format.Bottom;
+
         var renderWidth = width;
         var renderHeight = height;
 
