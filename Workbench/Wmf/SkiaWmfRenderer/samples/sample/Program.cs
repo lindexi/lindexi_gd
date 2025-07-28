1215e4b9574001ff3ddc1a2e31d8bed20dd429e8
lindexi <lindexi_gd@163.com>
2025-07-28 10:43:35 +0800

    搭建转换对接逻辑

--- Workbench/Wmf/SkiaWmfRenderer/samples/sample/Program.cs
+++ Workbench/Wmf/SkiaWmfRenderer/samples/sample/Program.cs
@@ -1,14 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Diagnostics;
 using Oxage.Wmf;
+using Oxage.Wmf.Records;
+
+using SkiaSharp;
+
+using SkiaWmfRenderer;
+
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Text;
-using Oxage.Wmf.Records;
-using SkiaSharp;
 
 var file = @"C:\lindexi\wmf公式\image17.wmf";
+
+SkiaWmfRenderHelper.TryConvertToPng(new FileInfo(file), new FileInfo("foo.png"));
+
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 var image = Image.FromFile(file);
--- Workbench/Wmf/SkiaWmfRenderer/src/SkiaWmfRenderer/SkiaWmfRenderHelper.cs
+++ Workbench/Wmf/SkiaWmfRenderer/src/SkiaWmfRenderer/SkiaWmfRenderHelper.cs
@@ -1,10 +1,78 @@
-﻿using System;
+﻿using Oxage.Wmf;
+
+using SkiaSharp;
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace SkiaWmfRenderer;
+
 public static class SkiaWmfRenderHelper
 {
+    public static bool TryConvertToPng(FileInfo wmfFile, FileInfo outputPngFile)
+    {
+        if (!TryRender(wmfFile, 0, 0, out var skBitmap))
+        {
+            return false;
+        }
+
+        using var outputStream = outputPngFile.OpenWrite();
+        skBitmap.Encode(outputStream, SKEncodedImageFormat.Png, 100);
+
+        return true;
+    }
+
+    public static bool TryRender(FileInfo wmfFile, int requestWidth, int requestHeight, [NotNullWhen(true)] out SKBitmap? skBitmap)
+    {
+        skBitmap = null;
+
+        using var fileStream = wmfFile.OpenRead();
+        var wmfDocument = new WmfDocument();
+        try
+        {
+            wmfDocument.Load(fileStream);
+        }
+        catch (WmfException e)
+        {
+            return false;
+        }
+
+        var wmfRenderer = new WmfRenderer()
+        {
+            WmfDocument = wmfDocument,
+            RequestWidth = requestWidth,
+            RequestHeight = requestHeight,
+        };
+
+        return wmfRenderer.TryRender(out skBitmap);
+    }
+}
+
+class WmfRenderer
+{
+    public required WmfDocument WmfDocument { get; init; }
+    public required int RequestWidth { get; init; }
+    public required int RequestHeight { get; init; }
+
+    public bool TryRender([NotNullWhen(true)] out SKBitmap? skBitmap)
+    {
+        skBitmap = null;
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        var format = WmfDocument.Format;
+
+        var x = Math.Min(format.Left, format.Right);
+        var y = Math.Min(format.Top, format.Bottom);
+
+        var width = Math.Abs(format.Right - format.Left);
+        var height = Math.Abs(format.Bottom - format.Top);
+
+        return skBitmap == null;
+    }
 }
