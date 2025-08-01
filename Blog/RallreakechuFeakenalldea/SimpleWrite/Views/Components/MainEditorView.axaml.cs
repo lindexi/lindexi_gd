6475729ea1d32f509bc105768035ca46cfc3f37d
lindexi <lindexi_gd@163.com>
2025-08-01 20:22:57 +0800

    创建项目

---
+++ Blog/RallreakechuFeakenalldea/SimpleWrite/Views/Components/MainEditorView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace SimpleWrite.Views.Components;
+
+public partial class MainEditorView : UserControl
+{
+    public MainEditorView()
+    {
+        InitializeComponent();
+    }
+}
