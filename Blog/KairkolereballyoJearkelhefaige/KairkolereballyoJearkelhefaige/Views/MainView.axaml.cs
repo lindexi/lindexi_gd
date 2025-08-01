a8c8fd8d0998f097ba57a13cf71727bd04df27d0
lindexi <lindexi_gd@163.com>
2025-08-01 19:53:37 +0800

    尝试重新制作项目

---
+++ Blog/KairkolereballyoJearkelhefaige/KairkolereballyoJearkelhefaige/Views/MainView.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
+
+using System;
+using System.IO;
+using Avalonia;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Media.Immutable;
+using Avalonia.Threading;
+
+namespace KairkolereballyoJearkelhefaige.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+
+    }
+
+}
