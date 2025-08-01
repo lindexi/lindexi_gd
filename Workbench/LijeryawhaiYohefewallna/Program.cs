23c614ba2b83b62ca01eafcfe7e02dd79b8388eb
lindexi <lindexi_gd@163.com>
2025-08-01 17:03:53 +0800

    无法处理的编码

--- Workbench/LijeryawhaiYohefewallna/Program.cs
+++ Workbench/LijeryawhaiYohefewallna/Program.cs
@@ -6,7 +6,7 @@ UInt16 早 = '早';
 Span<byte> 早Span = stackalloc byte[sizeof(UInt16)];
 BitConverter.TryWriteBytes(早Span, 早);
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-var encoding = Encoding.GetEncoding("GB2312");
+var encoding = Encoding.GetEncoding(52936);
 var 早Gbk = encoding.GetString(早Span);
 var 欲 = encoding.GetBytes(['欲']);
 Console.WriteLine(早Gbk);
