e6d2d0ce85bcfc3def2cb094dbac04f7fc7f7eb7
lindexi <lindexi_gd@163.com>
2025-08-01 11:57:51 +0800

    更换龙芯的入口文件

---
+++ Workbench/BawejernejeremCakaljerkawbai/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.NET.HostModel.AppHost;
+
+Console.WriteLine("Hello");
+
+var appHost = @"Microsoft.NETCore.App.Host.linux-loongarch64.9.0.5";
+
+HostWriter.CreateAppHost
+(
+    // AppHost 可从 .NET SDK 里面拷贝，也可以从 NuGet 缓存里面找找
+    // 如 .NET x64 的在 C:\Program Files\dotnet\sdk\<SDK VERSION>\AppHostTemplate\apphost.exe
+    // 龙芯的一般要自己下，下载地址： https://ftp.loongnix.cn/dotnet/8.0.14/8.0.14-1/deb/dotnet-apphost-pack-8.0_8.0.14-1_loongarch64.deb
+    appHostSourceFilePath: appHost,
+    // 输出路径，包括指定输出文件名
+    appHostDestinationFilePath: "Xilium.CefGlue.BrowserProcess",
+    // 入口的 DLL 是哪一个，这是相对于 exe 所在的 dll 路径
+    appBinaryFilePath: "Xilium.CefGlue.BrowserProcess.dll",
+    // 是否传入拷贝资源的程序集，如拷贝图标产品信息等等的程序集
+    assemblyToCopyResorcesFrom: null,
+    // 是否是 GUI 程序。为 false 代表控制台，可以显示控制台内容。为 true 隐藏控制台，为传统的桌面应用程序，如 WinForms 或 WPF 应用
+    windowsGraphicalUserInterface: false
+);
