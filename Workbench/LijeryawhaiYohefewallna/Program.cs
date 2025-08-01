5a11f2e6155b82db72eaf7632bbd3f0e8824a63c
lindexi <lindexi_gd@163.com>
2025-08-01 16:59:25 +0800

    测试编码转换

---
+++ Workbench/LijeryawhaiYohefewallna/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+UInt16 早 = '早';
+Span<byte> 早Span = stackalloc byte[sizeof(UInt16)];
+BitConverter.TryWriteBytes(早Span, 早);
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+var encoding = Encoding.GetEncoding("GB2312");
+var 早Gbk = encoding.GetString(早Span);
+var 欲 = encoding.GetBytes(['欲']);
+Console.WriteLine(早Gbk);
