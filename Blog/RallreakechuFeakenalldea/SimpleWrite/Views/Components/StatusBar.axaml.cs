7973c773618039b3e17fe7174aa2928ea7252e65
lindexi <lindexi_gd@163.com>
2025-08-04 11:15:41 +0800

    添加状态列表

---
+++ Blog/RallreakechuFeakenalldea/SimpleWrite/Views/Components/StatusBar.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace SimpleWrite.Views.Components;
+
+public partial class StatusBar : UserControl
+{
+    public StatusBar()
+    {
+        InitializeComponent();
+    }
+}
