435a80a673e3b906bc03b8ac1efedc6b400211f1
lindexi <lindexi_gd@163.com>
2025-08-04 19:14:16 +0800

    搭建应用框架

---
+++ Blog/RallreakechuFeakenalldea/SimpleWrite/ViewModels/EditorViewModel.cs
@@ -0,0 +1,8 @@
+﻿using SimpleWrite.Models;
+
+namespace SimpleWrite.ViewModels;
+
+public class EditorViewModel
+{
+    public EditorModel EditorModel { get; } = new EditorModel();
+}
---
+++ Blog/RallreakechuFeakenalldea/SimpleWrite/ViewModels/SimpleWriteMainViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleWrite.ViewModels;
+
+public class SimpleWriteMainViewModel
+{
+    public StatusViewModel StatusViewModel { get; } = new StatusViewModel();
+    public EditorViewModel EditorViewModel { get; } = new EditorViewModel();
+}
