98fe7e89809095834fbfa2ed873eaf70afd3f8c3
lindexi <lindexi_gd@163.com>
2025-08-05 16:28:26 +0800

    添加更多测试方法

--- LightTextEditorPlus/Demos/AvaloniaDemo/Business/RichTextCases/Framework_/RichTextCaseProvider.cs
+++ LightTextEditorPlus/Demos/AvaloniaDemo/Business/RichTextCases/Framework_/RichTextCaseProvider.cs
@@ -36,6 +36,16 @@ internal partial class RichTextCaseProvider
         Add(editor =>
         {
             TextEditor textEditor = editor;
+            textEditor.Text = """
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                              Maecenas porttitor congue massa.
+                              Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+                              """;
+        }, "直接设置文本多段内容");
+
+        Add(editor =>
+        {
+            TextEditor textEditor = editor;
             textEditor.AppendText("123");
         }, "追加文本");
 
