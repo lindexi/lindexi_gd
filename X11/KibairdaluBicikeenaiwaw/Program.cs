6008d7e8751c653949cff65a3968fd0a3cc42bc3
lindexi <lindexi_gd@163.com>
2025-08-06 11:03:45 +0800

    测试只更换属性的效果

---
+++ X11/KibairdaluBicikeenaiwaw/Program.cs
@@ -0,0 +1,97 @@
+﻿using BujeeberehemnaNurgacolarje;
+
+using KibairdaluBicikeenaiwaw;
+
+using CPF.Linux;
+
+using SkiaSharp;
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using static CPF.Linux.XLib;
+
+XInitThreads();
+var display = XOpenDisplay(IntPtr.Zero);
+var screen = XDefaultScreen(display);
+var rootWindow = XDefaultRootWindow(display);
+
+var dictionary = new Dictionary<IntPtr, TestX11Window>();
+
+var randr15ScreensImpl = new Randr15ScreensImpl(display, rootWindow);
+var monitorInfos = randr15ScreensImpl.GetMonitorInfos();
+for (var i = 0; i < monitorInfos.Length; i++)
+{
+    // 屏幕0 DisplayPort-1(343) IsPrimary=True XY=1920,309 WH=1920,1080
+    // 屏幕1 DisplayPort-0(626) IsPrimary=False XY=0,0 WH=1920,1080
+    MonitorInfo monitorInfo = monitorInfos[i];
+    Console.WriteLine($"屏幕{i} {monitorInfo}");
+
+    var x = monitorInfo.X;
+    var y = monitorInfo.Y;
+
+    var width = monitorInfo.Width;
+    var height = monitorInfo.Height;
+
+    var testX11Window = new TestX11Window(x, y, width, height, display, rootWindow, screen, isFullScreen: true);
+
+    testX11Window.MapWindow();
+    testX11Window.Draw();
+
+    dictionary[testX11Window.X11Window] = testX11Window;
+
+    Console.WriteLine($"X11Window1={testX11Window.X11Window}");
+}
+
+IntPtr invokeMessageId = new IntPtr(123123123);
+
+while (true)
+{
+    var xNextEvent = XNextEvent(display, out var @event);
+
+    if (xNextEvent != 0)
+    {
+        break;
+    }
+
+    if (@event.type == XEventName.Expose)
+    {
+        var window = @event.ExposeEvent.window;
+        if (dictionary.TryGetValue(window, out var x11Window))
+        {
+            x11Window.Draw();
+        }
+    }
+    else if (@event.type == XEventName.ClientMessage)
+    {
+        if (@event.ClientMessageEvent.ptr1 == invokeMessageId)
+        {
+
+        }
+    }
+    else if (@event.type == XEventName.PropertyNotify)
+    {
+        var atom = @event.PropertyEvent.atom;
+        var atomNamePtr = XGetAtomName(display, atom);
+        var atomName = Marshal.PtrToStringAnsi(atomNamePtr);
+        XFree(atomNamePtr);
+        //Console.WriteLine($"PropertyNotify {atomName}({atom}) State={@event.PropertyEvent.state}");
+    }
+    else if (@event.type is XEventName.KeymapNotify)
+    {
+        // 忽略
+    }
+    else if (@event.type is XEventName.ConfigureNotify)
+    {
+        Console.WriteLine($"ConfigureNotify {@event}");
+    }
+    else
+    {
+        //Console.WriteLine($"Event={@event}");
+    }
+}
+
+
+
+
---
+++ X11/KibairdaluBicikeenaiwaw/TestX11Window.cs
@@ -0,0 +1,140 @@
+﻿using CPF.Linux;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CPF.Linux.XLib;
+
+namespace KibairdaluBicikeenaiwaw;
+
+internal class TestX11Window
+{
+    public TestX11Window(int x, int y, int width, int height, nint display, nint rootWindow, int screen,
+        bool isFullScreen)
+    {
+        Display = display;
+
+        XMatchVisualInfo(display, screen, 32, 4, out var info);
+        var visual = info.visual;
+
+        var valueMask =
+                //SetWindowValuemask.BackPixmap
+                0
+                | SetWindowValuemask.BackPixel
+                | SetWindowValuemask.BorderPixel
+                | SetWindowValuemask.BitGravity
+                | SetWindowValuemask.WinGravity
+                | SetWindowValuemask.BackingStore
+                | SetWindowValuemask.ColorMap
+            //| SetWindowValuemask.OverrideRedirect
+            ;
+        var xSetWindowAttributes = new XSetWindowAttributes
+        {
+            backing_store = 1,
+            bit_gravity = Gravity.NorthWestGravity,
+            win_gravity = Gravity.NorthWestGravity,
+            //override_redirect = true, // 设置窗口的override_redirect属性为True，以避免窗口管理器的干预
+            colormap = XCreateColormap(display, rootWindow, visual, 0),
+            border_pixel = 0,
+            background_pixel = 0,
+        };
+
+        var handle = XCreateWindow(display, rootWindow, x, y, width, height, 5,
+            32,
+            (int)CreateWindowArgs.InputOutput,
+            visual,
+            (nuint)valueMask, ref xSetWindowAttributes);
+
+        X11Window = handle;
+
+        XEventMask ignoredMask = XEventMask.SubstructureRedirectMask | XEventMask.ResizeRedirectMask |
+                                 XEventMask.PointerMotionHintMask;
+        var mask = new IntPtr(0xffffff ^ (int)ignoredMask);
+        XSelectInput(display, handle, mask);
+
+        var gc = XCreateGC(display, handle, 0, 0);
+        GC = gc;
+
+        Width = width;
+        Height = height;
+
+        if (isFullScreen)
+        {
+            // [Window Manager Protocols | Extended Window Manager Hints](https://specifications.freedesktop.org/wm-spec/1.5/ar01s06.html )
+            // 6.3 _NET_WM_FULLSCREEN_MONITORS
+
+            var wmState = XInternAtom(Display, "_NET_WM_FULLSCREEN_MONITORS", true);
+
+            /*
+             data.l[0] = the monitor whose top edge defines the top edge of the fullscreen window
+             data.l[1] = the monitor whose bottom edge defines the bottom edge of the fullscreen window
+             data.l[2] = the monitor whose left edge defines the left edge of the fullscreen window
+             data.l[3] = the monitor whose right edge defines the right edge of the fullscreen window
+            */
+            var left = x;
+            var top = y;
+            var right = x + width;
+            var bottom = y + height;
+
+            int[] monitorEdges = [top, bottom, left, right];
+            XChangeProperty(display, handle, wmState, (IntPtr)Atom.XA_ATOM, format: 32, PropertyMode.Replace,
+                monitorEdges, monitorEdges.Length);
+        }
+    }
+
+    public IntPtr X11Window { get; }
+
+    public IntPtr Display { get; }
+    public IntPtr GC { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public void MapWindow()
+    {
+        XMapWindow(Display, X11Window);
+        XFlush(Display);
+    }
+
+    public void Draw()
+    {
+        using var skBitmap = new SKBitmap(Width, Height, SKColorType.Bgra8888, SKAlphaType.Premul);
+
+        using var skCanvas = new SKCanvas(skBitmap);
+
+        var xImage = CreateImage(skBitmap);
+
+        skCanvas.Clear(new SKColor((uint)Random.Shared.Next()).WithAlpha(0xFF));
+        skCanvas.Flush();
+
+        XPutImage(Display, X11Window, GC, ref xImage, 0, 0, 0, 0, (uint)skBitmap.Width,
+            (uint)skBitmap.Height);
+    }
+
+    static XImage CreateImage(SKBitmap skBitmap)
+    {
+        const int bytePerPixelCount = 4; // RGBA 一共4个 byte 长度
+        var bitPerByte = 8;
+
+        var bitmapWidth = skBitmap.Width;
+        var bitmapHeight = skBitmap.Height;
+
+        var img = new XImage();
+        int bitsPerPixel = bytePerPixelCount * bitPerByte;
+        img.width = bitmapWidth;
+        img.height = bitmapHeight;
+        img.format = 2; //ZPixmap;
+        img.data = skBitmap.GetPixels();
+        img.byte_order = 0; // LSBFirst;
+        img.bitmap_unit = bitsPerPixel;
+        img.bitmap_bit_order = 0; // LSBFirst;
+        img.bitmap_pad = bitsPerPixel;
+        img.depth = bitsPerPixel;
+        img.bytes_per_line = bitmapWidth * bytePerPixelCount;
+        img.bits_per_pixel = bitsPerPixel;
+        XInitImage(ref img);
+
+        return img;
+    }
+}
