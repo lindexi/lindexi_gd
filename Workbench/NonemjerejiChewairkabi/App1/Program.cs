79129eeb754ee67aa75b4a7ad4a9742d17356e62
lindexi <lindexi_gd@163.com>
2025-08-15 16:08:21 +0800

    最小测试，发现可以正常输出内容

---
+++ Workbench/NonemjerejiChewairkabi/App1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+Run();
+Console.WriteLine("Hello, World!");
+
+[DllImport("Lib1.dll")]
+static extern int Run();
---
+++ Workbench/NonemjerejiChewairkabi/Lib1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Lib1;
+
+public static class Class1
+{
+    [UnmanagedCallersOnly(EntryPoint = "Run", CallConvs = [typeof(CallConvCdecl)])]
+    public static int Run()
+    {
+        Console.WriteLine($"Run");
+        return 2;
+    }
+}
