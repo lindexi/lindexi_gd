413af3e5eaa9a0131dd2c581b4787e772e764d8f
lindexi <lindexi_gd@163.com>
2025-08-15 14:41:07 +0800

    独立发布还需要 16MB 大小

---
+++ Workbench/CuwayayqarlurYeaquciqe/Controllers/FooController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CuwayayqarlurYeaquciqe.Controllers;
+
+[ApiController]
+public class FooController : ControllerBase
+{
+    [HttpGet(template: "/")]
+    [Route("/")]
+    public string Get()
+    {
+        return "123123";
+    }
+}
