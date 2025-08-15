ede2d237c50b121b9ad36e03f7578f9b9e674bf7
lindexi <lindexi_gd@163.com>
2025-08-15 16:47:30 +0800

    测试本机发布调用无法输出控制台

---
+++ Workbench/FurhocecaiqayYearwedalofawrair/App1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+var result = Start();
+Console.WriteLine("Hello, World!");
+
+[DllImport("Lib1.dll")]
+static extern int Start();
