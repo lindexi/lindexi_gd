60954446bebcbc84cfa6e91cb8681b551ac60b93
lindexi <lindexi_gd@163.com>
2025-08-18 20:15:34 +0800

    通过符号判断是否相等

---
+++ Roslyn/JicanerekeaFawbairbalbayhearwear/JicanerekeaFawbairbalbayhearwear/Foo.cs
@@ -0,0 +1,48 @@
+﻿using Lindexi;
+
+namespace JicanerekeaFawbairbalbayhearwear;
+
+public interface IFoo
+{
+}
+
+public class F1 : IFoo
+{
+    public F1(IContext context)
+    {
+    }
+}
+
+public class F2 : IFoo
+{
+    public F2(IContext context)
+    {
+    }
+}
+
+public class F3 : IFoo
+{
+    public F3()
+    {
+    }
+}
+
+public interface IContext
+{
+}
+
+public static partial class FooCollection
+{
+    [Collection]
+    public static partial IEnumerable<Func<IContext, IFoo>> GetFooCreatorList();
+}
+
+//public static partial class FooCollection
+//{
+//    public static partial IEnumerable<Func<IContext, IFoo>> GetFooCreatorList()
+//    {
+//        yield return context => new F1(context);
+//        yield return context => new F2(context);
+//        yield return context => new F3(context);
+//    }
+//}
---
+++ Roslyn/JicanerekeaFawbairbalbayhearwear/JicanerekeaFawbairbalbayhearwear/Program.cs
@@ -0,0 +1,6 @@
+using JicanerekeaFawbairbalbayhearwear;
+
+foreach (Func<IContext, IFoo> creator in FooCollection.GetFooCreatorList())
+{
+    
+}
