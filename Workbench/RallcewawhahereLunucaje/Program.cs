0f5536336e102a2aa3948d50d20d275840100a2d
lindexi <lindexi_gd@163.com>
2025-08-19 17:12:43 +0800

    测试定时删除日志的逻辑

---
+++ Workbench/RallcewawhahereLunucaje/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RallcewawhahereLunucaje;
+
+var testLogFolder = $"Log_{Path.GetRandomFileName()}";
+
+var dateTime = DateTime.Now;
+
+for (int i = 0; i < 10; i++)
+{
+    dateTime = dateTime.AddDays(-1);
+    var logFolderName = IndependentProcessLogFolderManager.GetLogFolderName(dateTime, i);
+    var directoryInfo = Directory.CreateDirectory(Path.Join(testLogFolder, logFolderName));
+    directoryInfo.CreationTime = dateTime;
+}
+
+var regularCleaningLogTask = new RegularCleaningLogTask(testLogFolder);
+
+var t1 = regularCleaningLogTask.Run();
+_ = regularCleaningLogTask.Run();
+
+await t1;
+Console.ReadLine();
