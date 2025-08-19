230b62084b26634dbc403ae982a3c6d1649c76b6
lindexi <lindexi_gd@163.com>
2025-08-19 10:35:07 +0800

    测试直接字符串放入锁执行匹配逻辑

---
+++ Workbench/BurwallhefearNolelkacaywule/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var path = @"C:\Users\lindexi\AppData\Roaming\";
+// System.IO.DirectoryNotFoundException:“Could not find a part of the path 'C:\Users\lindexi\AppData\Roaming\'.”
+var mutex = new Mutex(true, path, out var createdNew);
+
+Console.WriteLine("Hello, World!");
