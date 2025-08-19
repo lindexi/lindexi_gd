12f53b68f6cdddd7bc5ea523c7445cb78170abf5
lindexi <lindexi_gd@163.com>
2025-08-19 10:43:12 +0800

    测试简单的序列化能够解决

--- Workbench/BurwallhefearNolelkacaywule/Program.cs
+++ Workbench/BurwallhefearNolelkacaywule/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
 var path = @"C:\Users\lindexi\AppData\Roaming\";
+
+var base64String = Convert.ToBase64String(MemoryMarshal.AsBytes(path.AsSpan()));
 // System.IO.DirectoryNotFoundException:“Could not find a part of the path 'C:\Users\lindexi\AppData\Roaming\'.”
-var mutex = new Mutex(true, path, out var createdNew);
+var mutex = new Mutex(true, base64String, out var createdNew);
 
 Console.WriteLine("Hello, World!");
