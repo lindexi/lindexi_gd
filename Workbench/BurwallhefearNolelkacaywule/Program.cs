ecf2f10a8efb2a4361e5c9d9addd1347d92f4eae
lindexi <lindexi_gd@163.com>
2025-08-19 11:02:54 +0800

    测试更多长度的内容

--- Workbench/BurwallhefearNolelkacaywule/Program.cs
+++ Workbench/BurwallhefearNolelkacaywule/Program.cs
@@ -6,6 +6,8 @@ var path = @"C:\Users\lindexi\AppData\Roaming\";
 
 var base64String = Convert.ToBase64String(MemoryMarshal.AsBytes(path.AsSpan()));
 // System.IO.DirectoryNotFoundException:“Could not find a part of the path 'C:\Users\lindexi\AppData\Roaming\'.”
-var mutex = new Mutex(true, base64String, out var createdNew);
-
+var name = new string(Enumerable.Repeat('c', 1000).ToArray());
+var mutex = new Mutex(true, name, out var createdNew);
+mutex.WaitOne();
 Console.WriteLine("Hello, World!");
+Console.ReadLine();
