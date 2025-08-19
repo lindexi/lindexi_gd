df18cea0fdc713eb3b402684749591af1b6f339d
lindexi <lindexi_gd@163.com>
2025-08-19 10:01:40 +0800

    测试正则表达式内容

---
+++ WPFDemo/RalhekajelRiyemlini/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RalhekajelRiyemlini;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var name = GetLogFolderName();
+        var match = GetLogFolderProcessRegex().Match(name);
+    }
+
+    public static string GetLogFolderName()
+    {
+        // 命名规则： 年月日_小时分钟秒,进程ID
+        // 20250724_171139,63912
+        var now = DateTime.Now;
+        var processId = Environment.ProcessId;
+        return $"{now:yyyyMMdd_HHmmss},{processId}";
+    }
+
+
+    [GeneratedRegex(@"\d{8}_\d{6},(\d+)")]
+    private static partial Regex GetLogFolderProcessRegex();
+}
