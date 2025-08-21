4ff99b9b3e23c04cf8dbbefa6134555231c3b910
lindexi <lindexi_gd@163.com>
2025-08-21 20:48:16 +0800

    实现标题栏的功能

--- Blog/RallreakechuFeakenalldea/SimpleWrite/ViewModels/SimpleWriteMainViewModel.cs
+++ Blog/RallreakechuFeakenalldea/SimpleWrite/ViewModels/SimpleWriteMainViewModel.cs
@@ -4,4 +4,6 @@ public class SimpleWriteMainViewModel
 {
     public StatusViewModel StatusViewModel { get; } = new StatusViewModel();
     public EditorViewModel EditorViewModel { get; } = new EditorViewModel();
+
+
 }
