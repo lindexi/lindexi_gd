b1370ea6617bb33b49b32b0d0667c2f16ed04029
lindexi <lindexi_gd@163.com>
2025-08-25 11:42:41 +0800

    Revert "提供依赖注入的实现代码逻辑"
    This reverts commit e73814471f84580e0bd6438968e607b70d5e6b21.

--- Workbench/CeligerjunereLawwhocerlurkechuwi/Program.cs
+++ Workbench/CeligerjunereLawwhocerlurkechuwi/Program.cs
@@ -56,8 +56,21 @@ while (true)
 
     var code =
         $$"""
-        // {{text}}动画
-        new {{type}}Animation(),
+        /// <summary>
+        /// {{text}}
+        /// </summary>
+        class {{type}}Animation : ElementAnimationSaveInfoConverter
+        {
+            public override string Type => "{{type}}";
+            protected override ElementAnimation ToElementAnimationInner(AnimationInfo animationInfo, ElementSaveInfoLoadContext context)
+            {
+                AnimationSaveInfo animationSaveInfo = animationInfo.AnimationSaveInfo;
+        
+                // todo 还没实现{{text}}动画
+                var animation = new 进入动画.淡入(animationInfo.Element);
+                return SetCommonInfo(animation, animationSaveInfo);
+            }
+        }
         
         """;
     result += code;
