b8552af35bc34c6811b37b3abff2e52654ea9df0
lindexi <lindexi_gd@163.com>
2025-09-04 20:02:59 +0800

    经过测试应该是两个窗口都关闭

---
+++ WPFDemo/DeelagojawweFakejachebelrair/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeelagojawweFakejachebelrair;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        var mainWindow = new MainWindow()
+        {
+            Title = "Windows2"
+        };
+        mainWindow.Owner = this;
+        Task.Run(async () =>
+        {
+            await Task.Delay(2000);
+            Dispatcher.Invoke(() => this.Close());
+        });
+        mainWindow.ShowDialog();
+    }
+}
