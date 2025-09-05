ecffc754e5ae60028556594d962d39a42d8b6bc0
lindexi <lindexi_gd@163.com>
2025-09-05 19:17:49 +0800

    修改堆栈内容

---
+++ Workbench/FehallladaichaWhearjechelra/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.ExceptionServices;
+
+var exception = new ArgumentException();
+ExceptionDispatchInfo.SetRemoteStackTrace(exception, $@"栽赃的堆栈.{nameof(Foo)}.{nameof(Foo.F1)}
+Main");
+Console.WriteLine(exception);
+
+class Foo
+{
+    public void F1()
+    {
+    }
+}
