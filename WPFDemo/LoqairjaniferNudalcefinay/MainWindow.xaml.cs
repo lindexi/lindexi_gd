148ded9ad618c00d5d6d0a01532a516d4a468af0
lindexi <lindexi_gd@163.com>
2025-09-08 10:54:43 +0800

    测试多指快速点下

---
+++ WPFDemo/LoqairjaniferNudalcefinay/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Threading.Channels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LoqairjaniferNudalcefinay;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        this.StylusPlugIns.Add(new MainWindowStylusPlugIn(this));
+
+        var channel = Channel.CreateBounded<string>(new BoundedChannelOptions(10)
+        {
+            FullMode = BoundedChannelFullMode.DropOldest,
+            // 一定要求调度线程，因为一边是触摸、一边是UI线程，两个都不能卡
+            AllowSynchronousContinuations = false,
+            // 单写读性能更好
+            SingleWriter = true,
+            SingleReader = true,
+        });
+        _channel = channel;
+
+        _ = ReadAllAsync();
+    }
+
+    private async Task ReadAllAsync()
+    {
+        while (true)
+        {
+            var canRead = await _channel.Reader.WaitToReadAsync();
+            if(!canRead)
+            {
+                break;
+            }
+
+            var lastMessage = string.Empty;
+            while (_channel.Reader.TryRead(out var message))
+            {
+                lastMessage = message;
+            }
+
+            if (!string.IsNullOrEmpty(lastMessage))
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    LogTextBlock.Text = lastMessage;
+                }, DispatcherPriority.Background);
+            }
+        }
+    }
+
+    private readonly Channel<string> _channel;
+
+    public void LogMessage(string message)
+    {
+        _channel.Writer.TryWrite(message);
+    }
+}
+
+record TouchInfo
+{
+
+}
---
+++ WPFDemo/LoqairjaniferNudalcefinay/MainWindowStylusPlugIn.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input.StylusPlugIns;
+
+namespace LoqairjaniferNudalcefinay;
+
+class MainWindowStylusPlugIn : StylusPlugIn
+{
+    public MainWindowStylusPlugIn(MainWindow mainWindow)
+    {
+        _mainWindow = mainWindow;
+    }
+
+    private readonly MainWindow _mainWindow;
+
+    
+
+    protected override void OnStylusDown(RawStylusInput rawStylusInput)
+    {
+        base.OnStylusDown(rawStylusInput);
+    }
+
+    protected override void OnStylusMove(RawStylusInput rawStylusInput)
+    {
+        base.OnStylusMove(rawStylusInput);
+    }
+
+    protected override void OnStylusUp(RawStylusInput rawStylusInput)
+    {
+        base.OnStylusUp(rawStylusInput);
+    }
+}
