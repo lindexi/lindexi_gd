a86a1e96658cd0ae731b66fb09952a3145ee441d
lindexi <lindexi_gd@163.com>
2025-09-15 20:03:15 +0800

    添加调试逻辑，测试渲染缺少字体

--- LightTextEditorPlus/LightTextEditorPlus.Skia/Platform/ResourceManager_/SkiaPlatformResourceManager.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Skia/Platform/ResourceManager_/SkiaPlatformResourceManager.cs
@@ -156,6 +156,13 @@ public class SkiaPlatformResourceManager :
 
     private SKTypeface? TryMatchCharacterTypeface(SkiaTextRunProperty normalRunProperty, Utf32CodePoint codePoint)
     {
+#if DEBUG
+        if (codePoint.Value == '\u2001')
+        {
+            return null;
+        }
+#endif
+
         // 字体不支持此字符。尝试进入字符回滚策略。核心调用的是 MatchCharacter 方法
         using SKFontStyle skFontStyle = normalRunProperty.ToSKFontStyle();
 
