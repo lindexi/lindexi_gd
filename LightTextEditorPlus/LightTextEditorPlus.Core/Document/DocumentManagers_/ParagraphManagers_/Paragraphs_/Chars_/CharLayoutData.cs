5849ada8c8341cc39ab1ae802d8c947467be3722
lindexi <lindexi_gd@163.com>
2025-09-16 10:52:07 +0800

    尝试定义让 LayoutCharData 包 CharData 的结构，这样的结构更加符合设计

--- LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/Chars_/CharLayoutData.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/Chars_/CharLayoutData.cs
@@ -3,6 +3,34 @@ using LightTextEditorPlus.Core.Primitive;
 
 namespace LightTextEditorPlus.Core.Document;
 
+class LayoutCharData : IParagraphCache
+{
+    public LayoutCharData(CharData charData, ParagraphData paragraph)
+    {
+        CharData = charData;
+        Paragraph = paragraph;
+        paragraph.InitVersion(this);
+    }
+
+    public CharData CharData { get; }
+
+    internal ParagraphData Paragraph { get; }
+
+    public uint CurrentParagraphVersion { get; set; }
+
+    /// <summary>
+    /// 字符数据是否失效
+    /// </summary>
+    /// <remarks>如果字符数据版本和段落版本不同步，则字符数据没有被布局更新，证明数据失效</remarks>
+    /// <returns></returns>
+    public bool IsInvalidVersion() => Paragraph.IsInvalidVersion(this);
+
+    /// <summary>
+    /// 从段落更新缓存版本信息
+    /// </summary>
+    public void UpdateVersion() => Paragraph.UpdateVersion(this);
+}
+
 /// <summary>
 /// 字符的布局信息，包括字符所在的段落和所在的行，字符所在的相对于文本框的坐标
 /// </summary>
