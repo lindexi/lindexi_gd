ffe7fa696b7b452a244956e0e3b4ac8a1cd34101
lindexi <lindexi_gd@163.com>
2025-09-16 10:18:20 +0800

    拆分文件

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Platform/Property_/RunProperty_/Font_/FontFallbackInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace LightTextEditorPlus.Core.Platform;
+
+/// <summary>
+/// 字体回退信息
+/// </summary>
+/// <param name="FallbackFontName">字体名</param>
+/// <param name="IsFallback">这个字体是否是回退的。False: 字体本身不需要回退</param>
+/// <param name="IsFallbackFailed">是否回退失败</param>
+public readonly record struct FontFallbackInfo(string FallbackFontName, bool IsFallback, bool IsFallbackFailed);
