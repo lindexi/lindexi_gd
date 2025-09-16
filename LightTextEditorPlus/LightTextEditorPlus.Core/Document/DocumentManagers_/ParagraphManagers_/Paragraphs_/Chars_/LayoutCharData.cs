946092c44b1a849f59d4a3bb7b15786590e46ea9
lindexi <lindexi_gd@163.com>
2025-09-16 10:55:29 +0800

    拆分代码

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Document/DocumentManagers_/ParagraphManagers_/Paragraphs_/Chars_/LayoutCharData.cs
@@ -0,0 +1,29 @@
+﻿namespace LightTextEditorPlus.Core.Document;
+
+class LayoutCharData : IParagraphCache
+{
+    public LayoutCharData(CharData charData, ParagraphData paragraph)
+    {
+        CharData = charData;
+        Paragraph = paragraph;
+        paragraph.InitVersion(this);
+    }
+
+    public CharData CharData { get; }
+
+    internal ParagraphData Paragraph { get; }
+
+    public uint CurrentParagraphVersion { get; set; }
+
+    /// <summary>
+    /// 字符数据是否失效
+    /// </summary>
+    /// <remarks>如果字符数据版本和段落版本不同步，则字符数据没有被布局更新，证明数据失效</remarks>
+    /// <returns></returns>
+    public bool IsInvalidVersion() => Paragraph.IsInvalidVersion(this);
+
+    /// <summary>
+    /// 从段落更新缓存版本信息
+    /// </summary>
+    public void UpdateVersion() => Paragraph.UpdateVersion(this);
+}
