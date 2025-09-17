1d40161d699d6d8dbd0abeb76a7df576ac4d2843
lindexi <lindexi_gd@163.com>
2025-09-17 17:51:11 +0800

    测试多线程读取字典，返回正常

---
+++ Workbench/HowhaydawrefaballHinenalnay/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Dictionary<int, Foo> dictionary = new();
+
+for (int i = 0; i < 100; i++)
+{
+    dictionary[i] = new Foo();
+}
+
+var manualResetEvent = new ManualResetEvent(false);
+
+for (int i = 0; i < 100; i++)
+{
+    var thread = new Thread(() =>
+    {
+        manualResetEvent.WaitOne();
+
+        foreach (var foo in dictionary.Values)
+        {
+            foo.Count++;
+        }
+    })
+    {
+        IsBackground = true
+    };
+    thread.Start();
+}
+
+manualResetEvent.Set();
+
+Console.WriteLine("Hello, World!");
+while (true)
+{
+    Console.Read();
+}
+
+record Foo
+{
+    public int Count { get; set; }
+}
