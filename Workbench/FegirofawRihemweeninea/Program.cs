f9fdc1ec17657a9ddf7dd78d62bf942cc28823b1
lindexi <lindexi_gd@163.com>
2025-09-19 14:16:09 +0800

    测试构建不通过的代码

---
+++ Workbench/FegirofawRihemweeninea/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var ints = new List<int>();
+
+var code =
+    $$"""
+      var ints = new List<int>();
+      
+      {{F(() => {
+
+          if (ints.Count == 0) {
+              yield return "// No integers";
+        }
+
+           })}}
+      
+      Console.WriteLine(string.Join(", ", ints));
+      """;
+
+Console.WriteLine("Hello, World!");
+
+string F(Func<IEnumerable<string>> func)
+{
+    var result = func();
+    return string.Join("", result);
+}
