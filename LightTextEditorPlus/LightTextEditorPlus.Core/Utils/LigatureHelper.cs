3d0911b44f870e8fa424cbd754b37cb5d457158c
lindexi <lindexi_gd@163.com>
2025-09-25 17:59:53 +0800

    修复光标命中到连写字不可见

---
+++ LightTextEditorPlus/LightTextEditorPlus.Core/Utils/LigatureHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LightTextEditorPlus.Core.Document;
+using LightTextEditorPlus.Core.Exceptions;
+using LightTextEditorPlus.Core.Primitive.Collections;
+
+namespace LightTextEditorPlus.Core.Utils;
+
+internal static class LigatureHelper
+{
+    public static CharData FindLigatureStartCharData(in TextReadOnlyListSpan<CharData> lineCharDataList, int offset)
+    {
+        var firstCharData = lineCharDataList[offset];
+        Debug.Assert(firstCharData.CharDataInfo.Status == CharDataInfoStatus.LigatureContinue);
+
+        for (int i = offset - 1; i >= 0; i--)
+        {
+            var currentCharData = lineCharDataList[i];
+            if (currentCharData.CharDataInfo.Status == CharDataInfoStatus.LigatureStart)
+            {
+                return currentCharData;
+            }
+        }
+
+        throw new TextEditorInnerException($"无法从传入的列表找到正确的连写字");
+    }
+}
