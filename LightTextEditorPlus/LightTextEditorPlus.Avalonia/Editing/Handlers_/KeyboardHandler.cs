9291f3cfd7cf6f143104ae3566575eb8dc48b975
lindexi <lindexi_gd@163.com>
2025-09-30 10:57:33 +0800

    去掉不使用的注释

--- LightTextEditorPlus/LightTextEditorPlus.Avalonia/Editing/Handlers_/KeyboardHandler.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Avalonia/Editing/Handlers_/KeyboardHandler.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using Avalonia.Input;
-using LightTextEditorPlus.Core.Carets;
-using LightTextEditorPlus.Core.Editing;
 
 namespace LightTextEditorPlus.Editing;
 
@@ -32,97 +30,9 @@ internal class KeyboardHandler
                 Command = new TextEditorCommand(command)
             });
         }
-
-        //// 默认命令。默认命令都不用绑定快捷键，因为系统（框架）已经绑定好了
-        //// 剪贴板
-        //TextEditor.CommandBindings.Add(new CommandBinding(ApplicationCommands.Copy, OnCopy));
-        //TextEditor.CommandBindings.Add(new CommandBinding(ApplicationCommands.Cut, OnCut));
-        //TextEditor.CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, OnPaste));
-
-        //// 样式变更-无默认快捷键绑定
-        //TextEditor.CommandBindings.Add(new CommandBinding(EditingCommands.ToggleItalic, (s, e) =>
-        //{
-        //    TextEditor.ToggleItalic();
-        //}));
-        //TextEditor.CommandBindings.Add(new CommandBinding(EditingCommands.ToggleBold, (s, e) =>
-        //{
-        //    TextEditor.ToggleBold();
-        //}));
-        //TextEditor.CommandBindings.Add(new CommandBinding(EditingCommands.ToggleUnderline, (s, e) =>
-        //{
-        //    TextEditor.ToggleUnderline();
-        //}));
-        //TextEditor.CommandBindings.Add(new CommandBinding(EditingCommands.ToggleSuperscript, (s, e) =>
-        //{
-        //    TextEditor.ToggleSuperscript();
-        //}));
-        //TextEditor.CommandBindings.Add(new CommandBinding(EditingCommands.ToggleSubscript, (s, e) =>
-        //{
-        //    TextEditor.ToggleSubscript();
-        //}));
-
-        //textEditor.KeyDown += TextEditor_KeyDown;
     }
 
     private TextEditorHandler TextEditorHandler => TextEditor.TextEditorHandler;
 
-    //private void TextEditor_KeyDown(object sender, KeyEventArgs e)
-    //{
-    //    TextEditorHandler.OnKeyDown(e);
-    //}
-
-    //#region 删除
-
-    //private static void OnDelete(object sender, ExecutedRoutedEventArgs e)
-    //{
-    //    var textEditor = (TextEditor) e.Source;
-    //    textEditor.TextEditorHandler.Delete();
-    //}
-
-    //private static void OnBackspace(object sender, ExecutedRoutedEventArgs e)
-    //{
-    //    var textEditor = (TextEditor) e.Source;
-    //    textEditor.TextEditorHandler.Backspace();
-    //}
-
-    //#endregion
-
-    //private void OnToggleInsert(object sender, ExecutedRoutedEventArgs e)
-    //{
-    //    TextEditorHandler.ToggleInsert();
-    //}
-
-    //#region 方向键
-
-    //private ExecutedRoutedEventHandler MoveCaret(CaretMoveType moveType)
-    //{
-    //    return (o, args) =>
-    //    {
-    //        //var textEditor = (TextEditor)args.Source; // 就是从 TextEditor 订阅的
-    //        TextEditorHandler.MoveCaret(moveType);
-    //    };
-    //}
-   
-    //#endregion
-
     private TextEditor TextEditor { get; }
-
-    #region 剪贴板
-
-    //private void OnCopy(object sender, ExecutedRoutedEventArgs e)
-    //{
-    //    TextEditorHandler.OnCopy(sender, e);
-    //}
-
-    //private void OnCut(object sender, ExecutedRoutedEventArgs e)
-    //{
-    //    TextEditorHandler.OnCut(sender, e);
-    //}
-
-    //private void OnPaste(object sender, ExecutedRoutedEventArgs e)
-    //{
-    //    TextEditorHandler.OnPaste(sender, e);
-    //}
-
-    #endregion
 }
