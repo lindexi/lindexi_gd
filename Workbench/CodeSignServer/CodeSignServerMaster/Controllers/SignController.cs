862683f443ebe4aaf53dfad7cd107b446eebd5dc
lindexi <lindexi_gd@163.com>
2025-10-09 16:39:02 +0800

    初始化空项目

---
+++ Workbench/CodeSignServer/CodeSignServerMaster/Controllers/SignController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.WebSockets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSignServerMaster.Controllers;
+
+[ApiController]
+public class SignController : ControllerBase
+{
+    [HttpPost]
+    [Route("/sign")]
+    public async Task Connect()
+    {
+        using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+        var arraySegment = new ArraySegment<byte>(new byte[1024]);
+        await webSocket.ReceiveAsync(arraySegment, CancellationToken.None);
+
+
+    }
+}
---
+++ Workbench/CodeSignServer/CodeSignServerSlave/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.WebSockets;
+
+var httpClient = new HttpClient();
+
+//WebSocket.CreateFromStream()
+
+var clientWebSocket = new ClientWebSocket();
+await clientWebSocket.ConnectAsync(new Uri("ws://127.0.0.1:5073/sign"),CancellationToken.None);
+
+ReadOnlyMemory<byte> t = new byte[] { 0x12, 0x13 };
+await clientWebSocket.SendAsync(t, WebSocketMessageType.Binary, true, CancellationToken.None);
+
+Console.WriteLine("Hello, World!");
