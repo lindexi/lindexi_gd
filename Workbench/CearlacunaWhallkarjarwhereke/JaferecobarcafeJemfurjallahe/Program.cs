cb4243dfefe937fa8177d5c897bd03679e097f53
lindexi <lindexi_gd@163.com>
2025-10-14 15:23:27 +0800

    测试重复调用

--- Workbench/CearlacunaWhallkarjarwhereke/JaferecobarcafeJemfurjallahe/Program.cs
+++ Workbench/CearlacunaWhallkarjarwhereke/JaferecobarcafeJemfurjallahe/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using System.Net;
+using System.Net.Sockets;
 using TouchSocket.Core;
 using TouchSocket.Http;
 using TouchSocket.Sockets;
@@ -13,29 +15,21 @@ _ = Task.Run(() =>
 
 });
 
-var service = new HttpService();
-await service.SetupAsync(new TouchSocketConfig()//加载配置
-    .SetListenOptions(list =>
+await Foo();
+try
+{
+    await Foo();
+}
+catch (Exception e)
+{
+    if (e is SocketException socketException)
     {
-        list.Add(new TcpListenOption()
-        {
-            IpHost = new IPHost(IPAddress.Loopback, 7799)
-        });
-    })
-    //.SetListenIPHosts(7789)
-    .ConfigureContainer(a =>
-    {
-        a.AddConsoleLogger();
-    })
-    .ConfigurePlugins(a =>
-    {
-        a.Add<MyHttpPlug1>();
-        //default插件应该最后添加，其作用是
-        //1、为找不到的路由返回404
-        //2、处理 header 为Option的探视跨域请求。
-        a.UseDefaultHttpServicePlugin();
-    }));
-await service.StartAsync();
+        // 通常每个套接字地址(协议/网络地址/端口)只允许使用一次
+        Debug.Assert(socketException.ErrorCode == 10048);
+    }
+
+    Console.WriteLine(e);
+}
 
 using var httpClient = new HttpClient();
 using var httpResponseMessage = await httpClient.GetAsync("http://127.0.0.1:7799/success");
@@ -43,6 +37,33 @@ var responseText = await httpResponseMessage.Content.ReadAsStringAsync();
 
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
+
+async Task Foo()
+{
+    var service = new HttpService();
+    await service.SetupAsync(new TouchSocketConfig()//加载配置
+        .SetListenOptions(list =>
+        {
+            list.Add(new TcpListenOption()
+            {
+                IpHost = new IPHost(IPAddress.Loopback, 7799)
+            });
+        })
+        //.SetListenIPHosts(7789)
+        .ConfigureContainer(a =>
+        {
+            a.AddConsoleLogger();
+        })
+        .ConfigurePlugins(a =>
+        {
+            a.Add<MyHttpPlug1>();
+            //default插件应该最后添加，其作用是
+            //1、为找不到的路由返回404
+            //2、处理 header 为Option的探视跨域请求。
+            a.UseDefaultHttpServicePlugin();
+        }));
+    await service.StartAsync();
+}
 
 class MyHttpPlug1 : PluginBase, IHttpPlugin
 {
