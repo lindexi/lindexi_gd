fe628b74888e109616ebe01ef01a4c2cc359156b
lindexi <lindexi_gd@163.com>
2025-10-15 14:54:46 +0800

    测试标题最大长度

---
+++ WPFDemo/KeharwekerKilagearbeyur/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeharwekerKilagearbeyur;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Title = new string(Enumerable.Repeat('a', 20000).ToArray());
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var windowInteropHelper = new WindowInteropHelper(this);
+        var hwnd = windowInteropHelper.Handle;
+
+        var windowTextLength = GetWindowTextLength(hwnd);
+    }
+
+    [DllImport("User32")]
+    public static extern int GetWindowTextLength(IntPtr hWnd);
+}
