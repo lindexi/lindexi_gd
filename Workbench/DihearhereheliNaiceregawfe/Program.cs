6bd65756cd4dbb859e3af74c560d79106ea69490
lindexi <lindexi_gd@163.com>
2025-10-16 16:46:23 +0800

    测试进程退出

---
+++ Workbench/DihearhereheliNaiceregawfe/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PowerThreadPool;
+
+var powerPool = new PowerPool();
+powerPool.Start();
+
+powerPool.QueueWorkItem(() =>
+{
+    // Do something
+});
+
+Console.WriteLine("Hello, World!");
