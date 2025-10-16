5bb18d4a8f9231b24e3b8344650f7bce15748088
lindexi <lindexi_gd@163.com>
2025-10-16 10:19:03 +0800

    Try build System.Drawing.Common

---
+++ WPFDemo/GeehalljawfegallrerWakaylakem/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeehalljawfegallrerWakaylakem;
+
+internal class Class1
+{
+    private System.Drawing.Bitmap _bitmap = new(100, 100);
+}
