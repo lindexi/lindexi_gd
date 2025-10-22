c1ef823a3ce602a7a0a4f746449b7deb7bd9c658
lindexi <lindexi_gd@163.com>
2025-10-22 17:17:35 +0800

    继续测试滚动条不出现问题

--- LightTextEditorPlus/Demos/AvaloniaDemo/Views/ConsoleTextEditorTestView.axaml.cs
+++ LightTextEditorPlus/Demos/AvaloniaDemo/Views/ConsoleTextEditorTestView.axaml.cs
@@ -15,6 +15,23 @@ public partial class ConsoleTextEditorTestView : UserControl
     {
         InitializeComponent();
 
+        Loaded += ConsoleTextEditorTestView_Loaded;
+        DataContextChanged += ConsoleTextEditorTestView_DataContextChanged;
+    }
+
+    private void ConsoleTextEditorTestView_Loaded(object? sender, RoutedEventArgs e)
+    {
+        object? dataContext = DataContext;
+        if (dataContext is not null)
+        {
+            DataContext = null;
+        }
+    }
+
+    private void ConsoleTextEditorTestView_DataContextChanged(object? sender, EventArgs e)
+    {
+        DataContextChanged -= ConsoleTextEditorTestView_DataContextChanged;
+
         ConsoleTextEditorScrollViewer.Content = new ConsoleTextEditor()
         {
             SizeToContent = SizeToContent.Height
@@ -32,4 +49,8 @@ public partial class ConsoleTextEditorTestView : UserControl
         GC.WaitForFullGCComplete();
         GC.Collect();
     }
+}
+
+public class ConsoleTextEditorTestViewModel
+{
 }
