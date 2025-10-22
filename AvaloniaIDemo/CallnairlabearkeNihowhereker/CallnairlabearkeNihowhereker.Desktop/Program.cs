34f3d5a18cef4f65d1b4e73f860f3706a7e3e83f
lindexi <lindexi_gd@163.com>
2025-10-22 16:32:09 +0800

    测试滚动条

---
+++ AvaloniaIDemo/CallnairlabearkeNihowhereker/CallnairlabearkeNihowhereker.Desktop/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Avalonia.ReactiveUI;
+
+namespace CallnairlabearkeNihowhereker.Desktop;
+
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        RunAvalonia(args);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static int RunAvalonia(string[] args)
+    {
+        return BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
+}
---
+++ AvaloniaIDemo/CallnairlabearkeNihowhereker/CallnairlabearkeNihowhereker/Views/MainView.axaml.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Styling;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media.Imaging;
+using Avalonia.Media.Immutable;
+using Avalonia.Threading;
+
+namespace CallnairlabearkeNihowhereker.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+}
+
+class Foo : Control, IScrollable
+//,IScrollSnapPointsInfo
+{
+    public Foo()
+    {
+        Height = 10000;
+    }
+
+    public IReadOnlyList<double> GetIrregularSnapPoints(Orientation orientation, SnapPointsAlignment snapPointsAlignment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double GetRegularSnapPoints(Orientation orientation, SnapPointsAlignment snapPointsAlignment, out double offset)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool AreHorizontalSnapPointsRegular { get; set; }
+    public bool AreVerticalSnapPointsRegular { get; set; }
+    public event EventHandler<RoutedEventArgs>? HorizontalSnapPointsChanged;
+    public event EventHandler<RoutedEventArgs>? VerticalSnapPointsChanged;
+    public Size Extent { get; set; }
+    public Vector Offset { get; set; }
+    public Size Viewport { get; set; }
+}
