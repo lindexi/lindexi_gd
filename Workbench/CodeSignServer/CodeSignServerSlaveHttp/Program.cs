fe4c49a67d5bed98c35c1c220ad7d5a39528147c
lindexi <lindexi_gd@163.com>
2025-10-26 20:15:30 +0800

    完成通过表单上传文件

--- Workbench/CodeSignServer/CodeSignServerSlaveHttp/Program.cs
+++ Workbench/CodeSignServer/CodeSignServerSlaveHttp/Program.cs
@@ -8,12 +8,26 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
 
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Primitives;
+
 namespace CodeSignServerMaster
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Task.Run(async () =>
+            {
+                var httpClient = new HttpClient();
+                var multipartFormDataContent = new MultipartFormDataContent();
+                multipartFormDataContent.Add(new StringContent("foo", Encoding.UTF8));
+                var memoryStream = new FakeStream();
+                multipartFormDataContent.Add(new StreamContent(memoryStream), "File", "f1");
+
+                using var httpResponseMessage = await httpClient.PostAsync("http://127.0.0.1:57563/sign", multipartFormDataContent);
+            });
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -34,9 +48,38 @@ namespace CodeSignServerMaster
 
             var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
-            app.Map("/fetch", async (Microsoft.AspNetCore.Http.HttpContext context) =>
+            app.Map("/sign", async (Microsoft.AspNetCore.Http.HttpContext context) =>
             {
-                
+                var request = context.Request;
+                await request.ReadFormAsync(new FormOptions()
+                {
+                    BufferBody = false,
+                    MultipartBodyLengthLimit = long.MaxValue,
+                });
+
+                IFormCollection formCollection = request.Form;
+                foreach (KeyValuePair<string, StringValues> keyValuePair in formCollection)
+                {
+
+                }
+
+                foreach (IFormFile formCollectionFile in formCollection.Files)
+                {
+                    var stream = formCollectionFile.OpenReadStream();
+                    byte[] buffer = new byte[1024 * 1024];
+                    while (true)
+                    {
+                        var readCount = await stream.ReadAsync(buffer);
+                        if (readCount == buffer.Length)
+                        {
+
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
             });
 
             app.Run();
@@ -44,6 +87,42 @@ namespace CodeSignServerMaster
     }
 }
 
+class FakeStream : Stream
+{
+    public override void Flush()
+    {
+
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        Position += count;
+        if (Position < Length)
+        {
+            return count;
+        }
+        return (int) (Length - (Position - count));
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return Position;
+    }
+
+    public override void SetLength(long value)
+    {
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+    }
+
+    public override bool CanRead => true;
+    public override bool CanSeek => false;
+    public override bool CanWrite => false;
+    public override long Length => 1024 * 1024 * 1024;
+    public override long Position { get; set; }
+}
 
 static class HttpHelper
 {
