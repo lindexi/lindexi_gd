d2df240b7c57f7de213904a3a82db3d62501b43d
lindexi <lindexi_gd@163.com>
2025-10-29 17:15:14 +0800

    补充算法部分

--- WPFDemo/DotNetCampus.InkCanvas/src/DotNetCampus.UnoInkCanvas/UnoInk/Inking/InkCore/SkInkCanvas.cs
+++ WPFDemo/DotNetCampus.InkCanvas/src/DotNetCampus.UnoInkCanvas/UnoInk/Inking/InkCore/SkInkCanvas.cs
@@ -507,55 +507,7 @@ class SkInkCanvas : IInputProcessor, IModeInputDispatcherSensitive
     /// </summary>
     public List<StrokeCollectionInfo> StaticInkInfoList { get; } = new List<StrokeCollectionInfo>();
 
-    /// <summary>
-    /// 按照德熙的玄幻算法，决定传入的点是否能丢掉
-    /// </summary>
-    /// <param name="pointList"></param>
-    /// <param name="currentStylusPoint"></param>
-    /// <param name="dropPointCount"></param>
-    /// <returns></returns>
-    private bool CanDropLastPoint(IReadOnlyList<StylusPoint> pointList, StylusPoint currentStylusPoint,
-        int dropPointCount)
-    {
-        if (pointList.Count < 3)
-        {
-            return false;
-        }
-
-        // 已经丢了10个点了，就不继续丢点了
-        if (dropPointCount >= 10)
-        {
-            return false;
-        }
-
-        // 假定要丢掉倒数第一个点，所以上一个点是倒数第二个点
-        var lastPoint = pointList[^2].Point;
-        var currentPoint = currentStylusPoint.Point;
-
-        var lastPointVector = new Vector2((float) lastPoint.X, (float) lastPoint.Y);
-        var currentPointVector = new Vector2((float) currentPoint.X, (float) currentPoint.Y);
-
-        var lineVector = currentPointVector - lastPointVector;
-        var lineLength = lineVector.Length();
-
-        // 如果移动距离比较长，则不丢点
-        if (lineLength > 10)
-        {
-            return false;
-        }
-
-        var last2Point = pointList[^3].Point;
-        var line2Vector = lastPointVector - new Vector2((float) last2Point.X, (float) last2Point.Y);
-        var line2Length = line2Vector.Length();
-        var vector2 = currentPointVector - lastPointVector;
-        var distance2 = MathF.Abs(line2Vector.X * vector2.Y - line2Vector.Y * vector2.X) / line2Length;
-        if (distance2 > 2)
-        {
-            return false;
-        }
-
-        return true;
-    }
+  
 
     private bool DrawStroke(DrawStrokeContext context, out Rect drawRect)
     {
