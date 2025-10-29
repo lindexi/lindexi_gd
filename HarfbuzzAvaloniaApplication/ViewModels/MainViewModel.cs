12f082bf9a9c11390bfa68559535eb1d8111c2eb
lindexi <lindexi_gd@163.com>
2025-10-29 15:16:34 +0800

    Revert "创建测试龙芯"
    This reverts commit eb74c8de9e2bb402e95073d58345ebce2a45d39d.

--- HarfbuzzAvaloniaApplication/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HarfbuzzAvaloniaApplication.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- HarfbuzzAvaloniaApplication/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace HarfbuzzAvaloniaApplication.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
