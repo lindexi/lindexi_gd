eb74c8de9e2bb402e95073d58345ebce2a45d39d
lindexi <lindexi_gd@163.com>
2025-10-29 15:16:15 +0800

    创建测试龙芯

---
+++ HarfbuzzAvaloniaApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace HarfbuzzAvaloniaApplication.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ HarfbuzzAvaloniaApplication/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HarfbuzzAvaloniaApplication.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
