732508dbb99b789a672e9ec1373092a7eaf778bb
lindexi <lindexi_gd@163.com>
2025-10-30 10:27:55 +0800

    搭建精灵渲染

---
+++ WPFDemo/DotNetCampus.InkCanvas/src/DotNetCampus.InkCanvas.X11InkCanvas/Renders/IApplicationBackendRenderContext.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetCampus.InkCanvas.X11InkCanvas.Renders;
+
+/// <summary>
+/// 应用的后端渲染上下文
+/// </summary>
+/// <remarks>默认要求使用 SKColorType.Bgra8888, SKAlphaType.Premul, SKPixelGeometry.BgrHorizontal格式</remarks>
+interface IApplicationBackendRenderContext
+{
+    int PixelWidth { get; }
+    int PixelHeight { get; }
+
+    int RowBytes => PixelWidth * 4;
+
+    int PixelBytesLength => RowBytes * PixelHeight;
+
+    nint GetPixelIntPtr();
+}
---
+++ WPFDemo/DotNetCampus.InkCanvas/src/DotNetCampus.InkCanvas.X11InkCanvas/Renders/SpriteAdorners/RectangleClipSpriteAdorner.cs
@@ -0,0 +1,28 @@
+﻿using SkiaSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.InkCanvas.X11InkCanvas.Renders.SpriteAdorners;
+
+/// <summary>
+/// 提供一个矩形裁剪的装饰器，可以用于裁剪一个矩形区域
+/// </summary>
+class RectangleClipSpriteAdorner : ISpriteAdorner
+{
+    public RectangleClipSpriteAdorner(SKRectI bounds)
+    {
+        Bounds = bounds;
+        OutputBitmap = new SKBitmap(bounds.Width, bounds.Height, SKColorType.Bgra8888, SKAlphaType.Premul);
+    }
+
+    public SKRectI Bounds { get; }
+    public SKBitmap OutputBitmap { get; }
+    public void Draw(IApplicationBackendRenderContext applicationBitmap)
+    {
+        // 这里是裁剪，啥都不需要做
+    }
+}
