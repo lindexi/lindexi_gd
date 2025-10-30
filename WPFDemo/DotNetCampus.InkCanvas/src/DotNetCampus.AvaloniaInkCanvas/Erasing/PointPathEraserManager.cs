eb5f75f349a7b8c3e5dc6517851507cc3db876f3
lindexi <lindexi_gd@163.com>
2025-10-30 15:55:17 +0800

    实现旋转橡皮擦

--- WPFDemo/DotNetCampus.InkCanvas/src/DotNetCampus.AvaloniaInkCanvas/Erasing/PointPathEraserManager.cs
+++ WPFDemo/DotNetCampus.InkCanvas/src/DotNetCampus.AvaloniaInkCanvas/Erasing/PointPathEraserManager.cs
@@ -3,8 +3,12 @@
 using DotNetCampus.Inking.Contexts;
 using DotNetCampus.Inking.Primitive;
 using DotNetCampus.Inking.Utils;
+using DotNetCampus.Numerics;
+using DotNetCampus.Numerics.Geometry;
 
 using SkiaSharp;
+
+using System.Diagnostics;
 
 using UnoInk.Inking.InkCore;
 
@@ -14,10 +18,22 @@ using Rect = DotNetCampus.Numerics.Geometry.Rect2D;
 
 namespace DotNetCampus.Inking.Erasing;
 
+/// <summary>
+/// 点擦路径擦除
+/// </summary>
 class PointPathEraserManager
 {
+    private bool _isErasing;
+
     public void StartEraserPointPath(IReadOnlyList<SkiaStroke> staticStrokeList)
     {
+        Debug.Assert(_isErasing == false, $"开始橡皮擦时，开始橡皮擦的 {nameof(_isErasing)} 字段状态一定为 false 值");
+        Debug.Assert(WorkList.Count == 0, "橡皮擦计算开始的时候，必然此时没有任何笔迹正在被橡皮擦工作中");
+        // 兜底代码，确保 WorkList 为空，防止重复加入导致进入诡异的逻辑
+        WorkList.Clear();
+
+        _isErasing = true;
+
         WorkList.EnsureCapacity(staticStrokeList.Count);
         var workList = WorkList;
         foreach (var skiaStrokeSynchronizer in staticStrokeList)
@@ -30,16 +46,23 @@ class PointPathEraserManager
 
     private readonly List<SubInkInfoForEraserPointPath> _cacheList = [];
 
-    public void Move(Rect rect)
+    public void Move(Rect2D rect) => Move(new RotatedRect(new Point2D(rect.X, rect.Y), new Size2D(rect.Width, rect.Height), AngularMeasure.Zero));
+
+    public void Move(RotatedRect rotatedEraserRect)
     {
+        var eraserBoundingBox = rotatedEraserRect.GetBoundingBox();
+        var transformation = AffineTransformation2D.Identity.RotateAt(-rotatedEraserRect.Rotate, rotatedEraserRect.Location).Translate(-rotatedEraserRect.Location.ToVector());
+        var eraserBoundingBoxRect2D = new Rect2D(eraserBoundingBox.MinX, eraserBoundingBox.MinY, eraserBoundingBox.Width, eraserBoundingBox.Height);
+
         foreach (InkInfoForEraserPointPath inkInfoForEraserPointPath in WorkList)
         {
             _cacheList.Clear();
 
+            // 擦点的核心逻辑
             foreach (SubInkInfoForEraserPointPath pointPath in inkInfoForEraserPointPath.SubInkInfoList)
             {
                 var bounds = pointPath.CacheBounds;
-                if (!bounds.IntersectsWith(rect))
+                if (!bounds.IntersectsWith(eraserBoundingBoxRect2D))
                 {
                     _cacheList.Add(pointPath);
                     continue;
@@ -57,7 +80,15 @@ class PointPathEraserManager
                     //var point = inkInfoForEraserPointPath.StrokeSynchronizer.StylusPoints[index].Point;
                     //_pointCount++;
 
-                    if (rect.Contains(point))
+                    if (
+                        // 1. 短路过滤：快速判断目标点是否在旋转矩形的外接边框内（这可以过滤掉板书中的绝大部分点）。
+                        eraserBoundingBox.Contains(point)
+                        // 2. 精确判断：判断目标点是否在旋转矩形内。
+                        //    2.1. 将目标点转换到旋转矩形的坐标系中；
+                        && transformation.Transform(point) is { X: >= 0, Y: >= 0 } transformedPoint
+                        //    2.2. 判断变换后的点是否在矩形内。
+                        && transformedPoint.X <= rotatedEraserRect.Size.Width
+                        && transformedPoint.Y <= rotatedEraserRect.Size.Height)
                     {
                         if (start != -1)
                         {
@@ -82,43 +113,85 @@ class PointPathEraserManager
                     }
                 }
 
-                if (start != -1)
+                // 这里的 start 是相对于 pointPath.PointPath 的，而不是相对于当前的 pointPath.PointListSpan 的。因此 start 为 0 不代表就是当前的 pointPath 的起点，而应该是 start == pointPath.PointListSpan.Start 才是代表起点和 pointPath 相同
+                if (start == pointPath.PointListSpan.Start && length == pointPath.PointListSpan.Length)
                 {
-                    // 截断
-                    _cacheList.Add(pointPath.Sub(start, length));
+                    // 短路代码，表示这条笔迹一个点都没被擦掉
+                    _cacheList.Add(pointPath);
+                }
+                else
+                {
+                    if (start != -1)
+                    {
+                        // 截断
+                        _cacheList.Add(pointPath.Sub(start, length));
+                    }
+
+                    // 截断最后需要将原来释放掉
+                    pointPath.Dispose();
                 }
             }
 
             inkInfoForEraserPointPath.SubInkInfoList.Clear();
             inkInfoForEraserPointPath.SubInkInfoList.AddRange(_cacheList);
+
+#if DEBUG
+            foreach (var subInkInfoForEraserPointPath in _cacheList)
+            {
+                if (subInkInfoForEraserPointPath.IsDisposed)
+                {
+                    Debugger.Break();
+                }
+            }
+#endif
+
             _cacheList.Clear();
         }
     }
 
     public PointPathEraserResult Finish()
     {
+        _isErasing = false;
         var count = WorkList.Sum(t => t.SubInkInfoList.Count);
         var erasingSkiaStrokeList = new List<ErasedSkiaStroke>(count);
 
         foreach (var inkInfoForEraserPointPath in WorkList)
         {
             var originSkiaStroke = inkInfoForEraserPointPath.OriginSkiaStroke;
-            var newStrokeList = new List<SkiaStroke>(inkInfoForEraserPointPath.SubInkInfoList.Count);
 
-            foreach (var subInkInfoForEraserPointPath in inkInfoForEraserPointPath.SubInkInfoList)
+            IReadOnlyList<SkiaStroke>? newStrokeList;
+            if (inkInfoForEraserPointPath.SubInkInfoList.Count == 0)
             {
-                var subSpan = subInkInfoForEraserPointPath.PointListSpan;
-                var pointList = new StylusPointListSpan(originSkiaStroke.PointList, subSpan.Start, subSpan.Length);
+                // 笔迹完全被擦掉了
+                newStrokeList = Array.Empty<SkiaStroke>();
+            }
+            else if (inkInfoForEraserPointPath.IsErased)
+            {
+                var strokeList = new List<SkiaStroke>(inkInfoForEraserPointPath.SubInkInfoList.Count);
+                newStrokeList = strokeList;
 
-                var skPath = ToPath(subInkInfoForEraserPointPath);
+                foreach (var subInkInfoForEraserPointPath in inkInfoForEraserPointPath.SubInkInfoList)
+                {
+                    var subSpan = subInkInfoForEraserPointPath.PointListSpan;
+                    var pointList = new StylusPointListSpan(originSkiaStroke.PointList, subSpan.Start, subSpan.Length);
 
-                var skiaStroke = SkiaStroke.CreateStaticStroke(InkId.NewId(), skPath, pointList, originSkiaStroke.Color,
-                    originSkiaStroke.InkThickness, ownSkiaPath:true, originSkiaStroke.InkStrokeRenderer);
-                newStrokeList.Add(skiaStroke);
-                //result.Add(new SkiaStrokeDrawContext(subInkInfoForEraserPointPath.PointPath.OriginSkiaStroke.Color, skPath, skPath.Bounds.ToAvaloniaRect(), ShouldDisposePath: true));
+                    var skPath = subInkInfoForEraserPointPath.CachePath ?? ToPath(subInkInfoForEraserPointPath);
+                    // 已经从 CachePath 取出，不能再有原来的引用，生怕被释放
+                    subInkInfoForEraserPointPath.CachePath = null;
+
+                    var skiaStroke = SkiaStroke.CreateStaticStroke(InkId.NewId(), skPath, pointList, originSkiaStroke.Color,
+                        originSkiaStroke.InkThickness, ownSkiaPath: true, originSkiaStroke.InkStrokeRenderer);
+                    strokeList.Add(skiaStroke);
+                }
+            }
+            else
+            {
+                // 没被擦的笔迹依然可以使用原来的笔迹，设计上配置 newStrokeList 为空，减少对象的创建
+                // 满屏幕的笔迹，然后只擦掉一个笔迹，如果没有被擦掉的笔迹也创建 List 那将会是一个很大的开销
+                newStrokeList = null;
             }
 
-            erasingSkiaStrokeList.Add(new ErasedSkiaStroke(originSkiaStroke, newStrokeList, isErased: true));
+            erasingSkiaStrokeList.Add(new ErasedSkiaStroke(originSkiaStroke, newStrokeList, inkInfoForEraserPointPath.IsErased));
         }
 
         WorkList.Clear();
@@ -126,6 +199,11 @@ class PointPathEraserManager
         return result;
     }
 
+    /// <summary>
+    /// 获取渲染内容
+    /// </summary>
+    /// <returns></returns>
+    /// 为什么获取渲染内容需要在准备渲染时才获取，而不是在擦的过程中计算？ 原因是机器设备性能太差，擦的过程的进入次数会比渲染次数更多，且在插的过程中计算出来的结果没有被实际使用到，于是不如就在准备渲染的时候计算，如此可以稍微提升一些性能
     public IReadOnlyList<SkiaStrokeDrawContext> GetDrawContextList()
     {
         var count = WorkList.Sum(t => t.SubInkInfoList.Count);
@@ -139,7 +217,14 @@ class PointPathEraserManager
                 // 被擦掉的笔迹，就需要逐个笔迹计算
                 foreach (var subInkInfoForEraserPointPath in inkInfoForEraserPointPath.SubInkInfoList)
                 {
-                    var skPath = ToPath(subInkInfoForEraserPointPath);
+                    if (subInkInfoForEraserPointPath.IsDisposed)
+                    {
+                        throw new ObjectDisposedException($"当前所使用的 SubInkInfoForEraserPointPath 已经被释放了，橡皮擦状态不正常");
+                    }
+
+                    subInkInfoForEraserPointPath.CachePath ??= ToPath(subInkInfoForEraserPointPath);
+                    // 为什么需要复制一个？原因是接下来的渲染是交给 Avalonia 的渲染线程上，释放时机不固定。原本的在 UI 线程上的 CachePath 的释放时机是这条笔迹被擦到的时候释放，也不能和渲染线程统一，只好进行拷贝一次
+                    var skPath = subInkInfoForEraserPointPath.CachePath.Clone();
 
                     result.Add(new SkiaStrokeDrawContext(originSkiaStroke.Color, skPath, skPath.Bounds.ToAvaloniaRect(), SKMatrix.Identity, ShouldDisposePath: true));
                 }
@@ -162,19 +247,33 @@ class PointPathEraserManager
         SkiaStroke originSkiaStroke = subInkInfoForEraserPointPath.PointPath.OriginSkiaStroke;
 
         var subSpan = subInkInfoForEraserPointPath.PointListSpan;
-        var skPath = new SKPath();
-        if (subSpan.Length > 2)
+        // 对于 WPF 注入的渲染器，只要大于一个点就可以开始渲染了
+        if (subSpan.Length > 0)
         {
             var pointList = new StylusPointListSpan(originSkiaStroke.PointList, subSpan.Start, subSpan.Length);
 
-            var outlinePointList = SimpleInkRender.GetOutlinePointList(pointList.ToReadOnlyList(), originSkiaStroke.InkThickness);
+            if (originSkiaStroke.InkStrokeRenderer is { } inkStrokeRenderer)
+            {
+                return inkStrokeRenderer.RenderInkToPath(pointList.ToReadOnlyList(), originSkiaStroke.InkThickness);
+            }
 
-            skPath.AddPoly(outlinePointList.Select(t => new SKPoint((float) t.X, (float) t.Y)).ToArray());
+            if (subSpan.Length > 2)
+            {
+                var outlinePointList = SimpleInkRender.GetOutlinePointList(pointList.ToReadOnlyList(), originSkiaStroke.InkThickness);
+
+                var skPath = new SKPath();
+                skPath.AddPoly(outlinePointList.Select(t => new SKPoint((float) t.X, (float) t.Y)).ToArray());
+                return skPath;
+            }
         }
 
-        return skPath;
+        return new SKPath();
     }
 
+    /// <summary>
+    /// 橡皮擦点擦过程中用到的笔迹信息
+    /// </summary>
+    /// 用于中间计算使用
     class InkInfoForEraserPointPath
     {
         public InkInfoForEraserPointPath(SkiaStroke originSkiaStroke)
@@ -191,7 +290,7 @@ class PointPathEraserManager
 
             SubInkInfoList.Add(subInk);
 
-            PointList = new Point[OriginSkiaStroke.PointList.Count];
+            PointList = new Point2D[OriginSkiaStroke.PointList.Count];
             for (var i = 0; i < OriginSkiaStroke.PointList.Count; i++)
             {
                 PointList[i] = OriginSkiaStroke.PointList[i].Point;
@@ -204,7 +303,7 @@ class PointPathEraserManager
         /// 所有实际带的点
         /// </summary>
         /// 比 <see cref="InkStylusPoint"/> 结构体小，如此可以提升遍历性能
-        public Point[] PointList { get; }
+        public Point2D[] PointList { get; }
 
         /// <summary>
         /// 拆分出来的笔迹
@@ -236,7 +335,7 @@ class PointPathEraserManager
     /// <summary>
     /// 被橡皮擦拆分的子笔迹信息
     /// </summary>
-    class SubInkInfoForEraserPointPath
+    class SubInkInfoForEraserPointPath : IDisposable
     {
         public SubInkInfoForEraserPointPath(PointListSpan pointListSpan, InkInfoForEraserPointPath pointPath)
         {
@@ -244,20 +343,22 @@ class PointPathEraserManager
             PointPath = pointPath;
         }
 
+        public SKPath? CachePath { get; set; }
+
         public InkInfoForEraserPointPath PointPath { get; }
 
-        public Rect CacheBounds
+        public Rect2D CacheBounds
         {
             get
             {
                 if (_cacheBounds == null)
                 {
                     var span = PointPath.PointList.AsSpan(PointListSpan.Start, PointListSpan.Length);
-                    Rect bounds = Rect.Zero;
+                    Rect2D bounds = new Rect2D();
 
                     if (span.Length > 0)
                     {
-                        bounds = new Rect(span[0].X, span[0].Y, 0, 0);
+                        bounds = new Rect2D(span[0].X, span[0].Y, 0, 0);
                     }
 
                     for (int i = 1; i < span.Length; i++)
@@ -273,7 +374,7 @@ class PointPathEraserManager
             set => _cacheBounds = value;
         }
 
-        private Rect? _cacheBounds;
+        private Rect2D? _cacheBounds;
 
         public PointListSpan PointListSpan { get; }
 
@@ -283,6 +384,15 @@ class PointPathEraserManager
             {
                 _cacheBounds = null
             };
+        }
+
+        public bool IsDisposed { get; set; }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+            CachePath?.Dispose();
+            CachePath = null;
         }
     }
 
---
+++ WPFDemo/DotNetCampus.InkCanvas/src/DotNetCampus.InkCanvas.InkCore/Inking/Primitive/RotatedRect.cs
@@ -0,0 +1,293 @@
+﻿using DotNetCampus.Numerics;
+using DotNetCampus.Numerics.Geometry;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.Inking.Primitive;
+
+/// <summary>
+/// 带有旋转的矩形。
+/// </summary>
+public readonly record struct RotatedRect : ISimilarityTransformable2D<RotatedRect>
+{
+    /// <summary>
+    /// 创建一个带有旋转的矩形。
+    /// </summary>
+    /// <param name="location">矩形所在的位置。UI 上表示为未旋转前矩形的左上角。</param>
+    /// <param name="size">矩形的大小。</param>
+    /// <param name="rotate">矩形的旋转角度。旋转的中心点为 <paramref name="location" />。</param>
+    public RotatedRect(Point2D location, Size2D size, AngularMeasure rotate)
+    {
+        Location = location;
+        Size = size;
+        Rotate = rotate;
+    }
+
+    /// <summary>
+    /// 矩形所在的位置。UI 上表示为未旋转前矩形的左上角。
+    /// </summary>
+    public Point2D Location { get; }
+
+    /// <summary>
+    /// 矩形的大小。
+    /// </summary>
+    public Size2D Size { get; }
+
+    /// <summary>
+    /// 矩形的旋转角度。旋转的中心点为 <see cref="Location" />。
+    /// </summary>
+    public AngularMeasure Rotate { get; }
+
+    /// <inheritdoc />
+    public RotatedRect Transform(SimilarityTransformation2D transformation)
+    {
+        var newLocation = transformation.Transform(Location);
+        var newRotate = Rotate + transformation.Rotation;
+        if (transformation.IsYScaleNegative)
+        {
+            newLocation -= transformation.Scaling * Size.Height * newRotate.UnitVector.NormalVector;
+        }
+
+        var newSize = new Size2D(Size.Width * transformation.Scaling, Size.Height * transformation.Scaling);
+        return new RotatedRect(newLocation, newSize, newRotate.Normalized);
+    }
+
+    /// <summary>
+    /// 判断指定的点是否在矩形内。这里考虑了矩形的旋转。
+    /// </summary>
+    /// <param name="point">要判断的点。</param>
+    /// <returns></returns>
+    public bool Contains(Point2D point)
+    {
+        var transformation = AffineTransformation2D.Identity.RotateAt(-Rotate, Location);
+        var transformedPoint = transformation.Transform(point);
+        return transformedPoint.X >= Location.X && transformedPoint.X <= Location.X + Size.Width &&
+               transformedPoint.Y >= Location.Y && transformedPoint.Y <= Location.Y + Size.Height;
+    }
+
+    /// <summary>
+    /// 过滤掉不在矩形内的点。这里考虑了矩形的旋转。
+    /// </summary>
+    /// <param name="points"></param>
+    /// <returns></returns>
+    public IEnumerable<Point2D> FilterContained(IEnumerable<Point2D> points)
+    {
+        var transformation = AffineTransformation2D.Identity.RotateAt(-Rotate, Location).Translate(-Location.ToVector());
+        foreach (var point in points)
+        {
+            var transformedPoint = transformation.Transform(point);
+            if (transformedPoint.X >= 0 && transformedPoint.X <= Size.Width && transformedPoint.Y >= 0 && transformedPoint.Y <= Size.Height)
+            {
+                yield return point;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 获取矩形的包围盒。这里考虑了矩形的旋转。
+    /// </summary>
+    /// <returns></returns>
+    public BoundingBox2D GetBoundingBox()
+    {
+        var transformation = AffineTransformation2D.Identity.RotateAt(Rotate, Location);
+        var points = new[]
+        {
+            Location,
+            Location + new Vector2D(Size.Width, 0),
+            Location + new Vector2D(Size.Width, Size.Height),
+            Location + new Vector2D(0, Size.Height),
+        };
+
+        for (var index = 0; index < points.Length; index++)
+        {
+            points[index] = transformation.Transform(points[index]);
+        }
+
+        return BoundingBox2D.Create(points);
+    }
+
+    public RotatedRect FlipAtBottom()
+    {
+        var widthUnitVector = Rotate.UnitVector;
+        var heightUnitVector = Rotate.UnitVector.NormalVector;
+        var newLocation = Location + Size.Width * widthUnitVector + 2 * Size.Height * heightUnitVector;
+        return new RotatedRect(newLocation, Size, (Rotate + AngularMeasure.Pi).Normalized);
+    }
+
+    /// <summary>
+    /// 是否与另一个带旋转的矩形相交。
+    /// </summary>
+    /// <param name="other">另一个带旋转的矩形。</param>
+    /// <returns>是否相交。</returns>
+    public bool Intersects(RotatedRect other)
+    {
+        var widthUnitVector1 = Rotate.UnitVector;
+        var heightUnitVector1 = Rotate.UnitVector.NormalVector;
+        var widthUnitVector2 = other.Rotate.UnitVector;
+        var heightUnitVector2 = other.Rotate.UnitVector.NormalVector;
+        var axisUnitVectors = new[]
+        {
+            widthUnitVector1,
+            heightUnitVector1,
+            widthUnitVector2,
+            heightUnitVector2,
+        };
+
+        var vectors1 = new[]
+        {
+            Location.ToVector(),
+            Location.ToVector() + Size.Width * widthUnitVector1,
+            Location.ToVector() + Size.Width * widthUnitVector1 + Size.Height * heightUnitVector1,
+            Location.ToVector() + Size.Height * heightUnitVector1,
+        };
+        var vectors2 = new[]
+        {
+            other.Location.ToVector(),
+            other.Location.ToVector() + other.Size.Width * widthUnitVector2,
+            other.Location.ToVector() + other.Size.Width * widthUnitVector2 + other.Size.Height * heightUnitVector2,
+            other.Location.ToVector() + other.Size.Height * heightUnitVector2,
+        };
+
+        foreach (var axisUnitVector in axisUnitVectors)
+        {
+            var min1 = double.MaxValue;
+            var max1 = double.MinValue;
+            var min2 = double.MaxValue;
+            var max2 = double.MinValue;
+            foreach (var vector in vectors1)
+            {
+                var projection = axisUnitVector.Dot(vector);
+                min1 = Math.Min(min1, projection);
+                max1 = Math.Max(max1, projection);
+            }
+
+            foreach (var vector in vectors2)
+            {
+                var projection = axisUnitVector.Dot(vector);
+                min2 = Math.Min(min2, projection);
+                max2 = Math.Max(max2, projection);
+            }
+
+            if (max1 < min2 || max2 < min1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// 是否与指定的点集相交。
+    /// </summary>
+    /// <param name="points">指定的点集。</param>
+    /// <param name="isPolyline">点集是否视为折线。</param>
+    /// <returns>是否相交。</returns>
+    public bool Intersects(IEnumerable<Point2D> points, bool isPolyline)
+    {
+        var transformation = AffineTransformation2D.Identity.RotateAt(-Rotate, Location).Translate(-Location.ToVector());
+
+        if (isPolyline)
+        {
+            Point2D? lastPoint = null;
+            foreach (var point in points)
+            {
+                var currentPoint = transformation.Transform(point);
+
+                if (lastPoint is not { } lastPointValue)
+                {
+                    if (currentPoint.X >= 0 && currentPoint.X <= Size.Width && currentPoint.Y >= 0 && currentPoint.Y <= Size.Height)
+                    {
+                        return true;
+                    }
+
+                    lastPoint = point;
+                    continue;
+                }
+
+                var vector = currentPoint - lastPointValue;
+                if (vector.LengthSquared.IsAlmostZero())
+                {
+                    continue;
+                }
+
+                if (!vector.X.IsAlmostEqual(0))
+                {
+                    var k = vector.Y / vector.X;
+                    var y = currentPoint.Y - currentPoint.X * k;
+                    if (y >= 0 && y <= Size.Height)
+                    {
+                        return true;
+                    }
+
+                    y = currentPoint.Y - (currentPoint.X - Size.Width) * k;
+                    if (y >= 0 && y <= Size.Height)
+                    {
+                        return true;
+                    }
+                }
+
+                if (!vector.Y.IsAlmostEqual(0))
+                {
+                    // 斜率的倒数
+                    var rk = vector.X / vector.Y;
+                    var x = currentPoint.X - currentPoint.Y * rk;
+                    if (x >= 0 && x <= Size.Width)
+                    {
+                        return true;
+                    }
+
+                    x = currentPoint.X - (currentPoint.Y - Size.Height) * rk;
+                    if (x >= 0 && x <= Size.Width)
+                    {
+                        return true;
+                    }
+                }
+
+                lastPoint = point;
+            }
+        }
+        else
+        {
+            foreach (var point in points)
+            {
+                var transformedPoint = transformation.Transform(point);
+                if (transformedPoint.X >= 0 && transformedPoint.X <= Size.Width && transformedPoint.Y >= 0 && transformedPoint.Y <= Size.Height)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// 在矩形的四个边上分别向外扩张形成新的矩形。
+    /// </summary>
+    /// <param name="widthAmount">宽度上的扩张量。</param>
+    /// <param name="heightAmount">高度上的扩张量。</param>
+    /// <returns>扩张后的矩形。</returns>
+    public RotatedRect Inflate(double widthAmount, double heightAmount)
+    {
+        var widthUnitVector = Rotate.UnitVector;
+        var heightUnitVector = Rotate.UnitVector.NormalVector;
+        var newLocation = Location - widthAmount * widthUnitVector - heightAmount * heightUnitVector;
+        var newSize = new Size2D(Size.Width + 2 * widthAmount, Size.Height + 2 * heightAmount);
+        return new RotatedRect(newLocation, newSize, Rotate);
+    }
+
+    /// <summary>
+    /// 在矩形的四个边上分别向外扩张形成新的矩形。
+    /// </summary>
+    /// <param name="amount">扩张量。</param>
+    /// <returns>扩张后的矩形。</returns>
+    public RotatedRect Inflate(double amount)
+    {
+        return Inflate(amount, amount);
+    }
+}
