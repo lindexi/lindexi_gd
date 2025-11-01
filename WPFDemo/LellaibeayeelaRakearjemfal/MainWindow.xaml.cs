9bc7db74f69823d0c0d762492dea640c8048d67c
lindexi <lindexi_gd@163.com>
2025-11-01 10:52:39 +0800

    提供更好的解决方案

--- WPFDemo/LellaibeayeelaRakearjemfal/MainWindow.xaml.cs
+++ WPFDemo/LellaibeayeelaRakearjemfal/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -39,68 +40,34 @@ public partial class MainWindow : Window
         const int WM_SYSCOLORCHANGE = 0x0015;
         const int WM_THEMECHANGED = 0x031A;
         const int WM_DWMCOLORIZATIONCOLORCHANGED = 0x0320;
+
         if (msg is WM_SYSCOLORCHANGE or WM_THEMECHANGED or WM_DWMCOLORIZATIONCOLORCHANGED)
         {
+
+        }
+
+        const int WM_NCPAINT = 0x0085;
+        const int WM_SETTINGCHANGE = 0x001A;
+        if (msg is WM_NCPAINT or WM_SETTINGCHANGE)
+        {
             Window window = this;
-            var renderHelper = new RenderHelper(window);
-            renderHelper.ForceRedraw();
+            var rect = new Int32Rect(0,0, (int)Math.Ceiling(window.ActualWidth), (int) Math.Ceiling(window.ActualHeight));
+            unsafe
+            {
+                InvalidateRect(hwnd, &rect, true);
+            }
         }
 
         return IntPtr.Zero;
     }
+
+    [DllImport("User32")]
+    private static extern unsafe bool InvalidateRect(IntPtr hwnd, Int32Rect* lpRect, bool bErase);
 
     private void UpdateButton_OnClick(object sender, RoutedEventArgs e)
     {
         TheTextBlock.Text = $"abc{Random.Shared.Next(10)}";
         InvalidateVisual();
         UpdateLayout();
-    }
-}
-
-class RenderHelper
-{
-    public RenderHelper(Window window)
-    {
-        _window = window;
-    }
-
-    private readonly Window _window;
-
-    public void ForceRedraw()
-    {
-        _window.Dispatcher.InvokeAsync(() =>
-        {
-            InvalidateAllVisual(_window);
-
-            CompositionTarget.Rendering += CompositionTargetOnRendering;
-        }, DispatcherPriority.Render);
-    }
-
-    private int _currentCount;
-
-    void CompositionTargetOnRendering(object? sender, EventArgs e)
-    {
-        if (_currentCount >= 2)
-        {
-            CompositionTarget.Rendering -= CompositionTargetOnRendering;
-        }
-
-        _currentCount++;
-        InvalidateAllVisual(_window);
-    }
-
-    private void InvalidateAllVisual(DependencyObject element)
-    {
-        var childrenCount = VisualTreeHelper.GetChildrenCount(element);
-        for (int i = 0; i < childrenCount; i++)
-        {
-            var child = VisualTreeHelper.GetChild(element, i);
-            InvalidateAllVisual(child);
-        }
-
-        if (element is UIElement uiElement)
-        {
-            uiElement.InvalidateVisual();
-        }
     }
 }
