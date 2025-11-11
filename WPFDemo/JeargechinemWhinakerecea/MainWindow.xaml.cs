e925b04608fcf342caac1bb3f8e50f220ae5e109
lindexi <lindexi_gd@163.com>
2025-11-11 10:10:29 +0800

    无法写继承的代码，因为没有被当成内部

---
+++ WPFDemo/JeargechinemWhinakerecea/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JeargechinemWhinakerecea;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+
+public class ViewModel
+{
+
+}
+
+public interface INotifyBase : INotifyPropertyChanged
+{
+    void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
