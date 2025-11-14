fc58aa9e8ab7c6876adae6fd85568d7e9129fb44
lindexi <lindexi_gd@163.com>
2025-11-14 17:51:08 +0800

    测试序列化是否一行

---
+++ Workbench/LalnanarleneajolerBawreelilerjuqe/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+
+Foo foo = new();
+
+var json = JsonSerializer.Serialize(foo);
+
+Console.WriteLine("Hello, World!");
+
+class Foo
+{
+    public int N1 { get; set; } = 1;
+    public int N2 { get; set; } = 3;
+
+    public List<F2> F2List { get; set; } = new List<F2>()
+    {
+        new F2(),
+        new F2(),
+        new F2(),
+        new F2(),
+        new F2(),
+        new F2(),
+    };
+}
+
+class F2
+{
+    public int N3 { get; set; } = Random.Shared.Next();
+}
