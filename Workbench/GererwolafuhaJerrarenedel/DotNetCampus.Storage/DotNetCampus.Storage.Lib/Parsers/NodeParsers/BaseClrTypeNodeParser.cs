f51b30b9b58a3b10e8c9caba6d8c38f5e58c7cb3
lindexi <lindexi_gd@163.com>
2025-11-20 19:42:32 +0800

    定义存储特性

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Parsers/NodeParsers/BaseClrTypeNodeParser.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Parsers/NodeParsers/BaseClrTypeNodeParser.cs
@@ -1,6 +1,17 @@
-﻿namespace DotNetCampus.Storage.Lib.Parsers.NodeParsers;
+﻿using DotNetCampus.Storage.Lib.SaveInfos;
+
+namespace DotNetCampus.Storage.Lib.Parsers.NodeParsers;
 
 internal abstract class BaseClrTypeNodeParser<T> : NodeParser<T>
 {
     public override string? TargetStorageName => null;
+}
+
+public abstract class SaveInfoNodeParser<T> : NodeParser<T>
+{
+}
+
+public class FooSaveInfo : SaveInfo
+{
+
 }
---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/SaveInfos/SaveInfoContractAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetCampus.Storage.Lib.SaveInfos;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class SaveInfoContractAttribute : Attribute
+{
+    public SaveInfoContractAttribute(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public string? Description { get; set; }
+}
