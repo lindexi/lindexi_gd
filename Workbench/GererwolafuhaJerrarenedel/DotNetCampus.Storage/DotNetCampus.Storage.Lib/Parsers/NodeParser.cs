f73de875e906da630cda343a4b4595f43cb4d24e
lindexi <lindexi_gd@163.com>
2025-11-20 16:44:04 +0800

    尝试定义存储内容

---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Parsers/NodeParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.Storage.Lib.Parsers;
+
+internal abstract class NodeParser
+{
+}
+
+internal class SaveInfoParser : NodeParser
+{
+}
---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/SaveInfos/SaveInfo.cs
@@ -0,0 +1,32 @@
+﻿using DotNetCampus.Storage.Lib.StorageNodes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.Storage.Lib.SaveInfos;
+
+/// <summary>
+/// 数据模型的基类
+/// </summary>
+public abstract class SaveInfo
+{
+    /// <summary>
+    /// 未识别的属性
+    /// <para>一般是新版本的 SaveInfo 在旧版本打开，其部分新增属性无法识别。</para>
+    /// </summary>
+    public IReadOnlyList<StorageNode>? UnknownProperties { get; private init; }
+
+    private List<SaveInfo>? _extensions;
+
+    /// <summary>
+    /// 获取或设置 SaveInfo 的扩展数据。
+    /// </summary>
+    public List<SaveInfo> Extensions
+    {
+        get => _extensions ??= new List<SaveInfo>();
+        set => _extensions = value;
+    }
+}
