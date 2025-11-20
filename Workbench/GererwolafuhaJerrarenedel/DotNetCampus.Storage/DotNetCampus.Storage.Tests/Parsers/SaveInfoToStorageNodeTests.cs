bd94e9e27f58721cd885a6cad9318d639f52c5c7
lindexi <lindexi_gd@163.com>
2025-11-20 21:15:46 +0800

    加上更多测试逻辑

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Tests/Parsers/SaveInfoToStorageNodeTests.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Tests/Parsers/SaveInfoToStorageNodeTests.cs
@@ -40,6 +40,40 @@ public class SaveInfoToStorageNodeTests
         Assert.AreEqual(foo1SaveInfo.Foo1Property, parsedFoo1SaveInfo.Foo1Property);
         Assert.AreEqual(foo1SaveInfo.Foo2Property, parsedFoo1SaveInfo.Foo2Property);
     }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        var storableNodeParserManager = new StorableNodeParserManager();
+        storableNodeParserManager.Register(new Foo1SaveInfoNodeParser());
+        storableNodeParserManager.Register(new FooSaveInfoNodeParser());
+
+        var fooSaveInfo = new FooSaveInfo()
+        {
+            FooProperty = Random.Shared.Next(),
+            Foo1 = new Foo1SaveInfo()
+            {
+                Foo1Property = true,
+                Foo2Property = Random.Shared.Next()
+            }
+        };
+
+        var nodeParser = storableNodeParserManager.GetNodeParser(fooSaveInfo.GetType());
+        var storageNode = nodeParser.Deparse(fooSaveInfo, new DeparseNodeContext()
+        {
+            NodeName = null,
+            ParserManager = storableNodeParserManager
+        });
+        var parsedFooSaveInfo = nodeParser.Parse(storageNode, new ParseNodeContext()
+        {
+            ParserManager = storableNodeParserManager
+        }) as FooSaveInfo;
+        Assert.IsNotNull(parsedFooSaveInfo);
+        Assert.AreEqual(fooSaveInfo.FooProperty, parsedFooSaveInfo.FooProperty);
+        Assert.IsNotNull(parsedFooSaveInfo.Foo1);
+        Assert.AreEqual(fooSaveInfo.Foo1.Foo1Property, parsedFooSaveInfo.Foo1.Foo1Property);
+        Assert.AreEqual(fooSaveInfo.Foo1.Foo2Property, parsedFooSaveInfo.Foo1.Foo2Property);
+    }
 }
 
 
@@ -48,6 +82,85 @@ public class FooSaveInfo : SaveInfo
 {
     [SaveInfoMember("FooProperty", Description = "This is a foo property.")]
     public int FooProperty { get; set; } = 2;
+
+    [SaveInfoMember("F1", Description = "This is a foo property.")]
+    public Foo1SaveInfo? Foo1 { get; set; }
+}
+
+public class FooSaveInfoNodeParser : SaveInfoNodeParser<FooSaveInfo>
+{
+    public override SaveInfoContractAttribute ContractAttribute { get; } = new SaveInfoContractAttribute("Foo");
+
+    protected override FooSaveInfo ParseCore(StorageNode node, in ParseNodeContext context)
+    {
+        StorableNodeParserManager parserManager = context.ParserManager;
+        // 决定不支持 init 的情况，这样才能更好地保留默认值
+        var fooSaveInfo = new FooSaveInfo();
+        if (node.Children is { } children)
+        {
+            foreach (var storageNode in children)
+            {
+                var currentName = storageNode.Name.AsSpan();
+                var propertyNameForFooProperty = "FooProperty";
+                if (currentName.Equals(propertyNameForFooProperty, StringComparison.Ordinal))
+                {
+                    var typeOfFooProperty = typeof(int);
+                    var nodeParserForFooProperty = parserManager.GetNodeParser(typeOfFooProperty);
+                    var valueForFooProperty = nodeParserForFooProperty.Parse(storageNode, context);
+                    fooSaveInfo.FooProperty = (int) valueForFooProperty;
+                }
+
+                var propertyNameForFoo1 = "F1";
+                if (currentName.Equals(propertyNameForFoo1, StringComparison.Ordinal))
+                {
+                    var typeOfFoo1 = typeof(Foo1SaveInfo);
+                    var nodeParserForFoo1 = parserManager.GetNodeParser(typeOfFoo1);
+                    var valueForFoo1 = nodeParserForFoo1.Parse(storageNode, context);
+                    fooSaveInfo.Foo1 = (Foo1SaveInfo) valueForFoo1;
+                }
+            }
+        }
+        return fooSaveInfo;
+    }
+
+    protected override StorageNode DeparseCore(FooSaveInfo obj, in DeparseNodeContext context)
+    {
+        StorableNodeParserManager parserManager = context.ParserManager;
+        var storageNode = new StorageNode();
+        const int saveInfoMemberCount = 2;
+        storageNode.Name = context.NodeName ?? TargetStorageName;
+        storageNode.Children = new List<StorageNode>(saveInfoMemberCount);
+        DeparseNodeContext tempContext;
+        var propertyNameForFooProperty = "FooProperty";
+        var typeOfFooProperty = typeof(int);
+        var nodeParserForFooProperty = parserManager.GetNodeParser(typeOfFooProperty);
+        object? valueForFooProperty = obj.FooProperty;
+        if (valueForFooProperty is not null)
+        {
+            tempContext = context with
+            {
+                NodeName = propertyNameForFooProperty
+            };
+            var childNodeForFooProperty = nodeParserForFooProperty.Deparse(valueForFooProperty, tempContext);
+            storageNode.Children.Add(childNodeForFooProperty);
+        }
+
+        var propertyNameForFoo1 = "F1";
+        var typeOfFoo1 = typeof(Foo1SaveInfo);
+        var nodeParserForFoo1 = parserManager.GetNodeParser(typeOfFoo1);
+        object? valueForFoo1 = obj.Foo1;
+        if (valueForFoo1 is not null)
+        {
+            tempContext = context with
+            {
+                NodeName = propertyNameForFoo1
+            };
+            var childNodeForFoo1 = nodeParserForFoo1.Deparse(valueForFoo1, tempContext);
+            storageNode.Children.Add(childNodeForFoo1);
+        }
+
+        return storageNode;
+    }
 }
 
 [SaveInfoContract("Foo1")]
@@ -71,7 +184,7 @@ public class Foo1SaveInfoNodeParser : SaveInfoNodeParser<Foo1SaveInfo>
         // 决定不支持 init 的情况，这样才能更好地保留默认值
         var foo1SaveInfo = new Foo1SaveInfo();
 
-        if (node.Children is {} children)
+        if (node.Children is { } children)
         {
             foreach (var storageNode in children)
             {
@@ -83,7 +196,7 @@ public class Foo1SaveInfoNodeParser : SaveInfoNodeParser<Foo1SaveInfo>
                     var typeOfFoo1Property = typeof(bool);
                     var nodeParserForFoo1Property = parserManager.GetNodeParser(typeOfFoo1Property);
                     var valueForFoo1Property = nodeParserForFoo1Property.Parse(storageNode, context);
-                    foo1SaveInfo.Foo1Property = (bool)valueForFoo1Property;
+                    foo1SaveInfo.Foo1Property = (bool) valueForFoo1Property;
                 }
 
                 var propertyNameForFoo2Property = "Foo2Property";
@@ -92,7 +205,7 @@ public class Foo1SaveInfoNodeParser : SaveInfoNodeParser<Foo1SaveInfo>
                     var typeOfFoo2Property = typeof(int);
                     var nodeParserForFoo2Property = parserManager.GetNodeParser(typeOfFoo2Property);
                     var valueForFoo2Property = nodeParserForFoo2Property.Parse(storageNode, context);
-                    foo1SaveInfo.Foo2Property = (int)valueForFoo2Property;
+                    foo1SaveInfo.Foo2Property = (int) valueForFoo2Property;
                 }
             }
         }
@@ -105,7 +218,7 @@ public class Foo1SaveInfoNodeParser : SaveInfoNodeParser<Foo1SaveInfo>
         StorableNodeParserManager parserManager = context.ParserManager;
 
         var storageNode = new StorageNode();
-        const int saveInfoMemberCount = 1;
+        const int saveInfoMemberCount = 2;
         storageNode.Name = context.NodeName ?? TargetStorageName;
         storageNode.Children = new List<StorageNode>(saveInfoMemberCount);
 
@@ -114,24 +227,30 @@ public class Foo1SaveInfoNodeParser : SaveInfoNodeParser<Foo1SaveInfo>
         var propertyNameForFoo1Property = "Foo1Property";
         var typeOfFoo1Property = typeof(bool);
         var nodeParserForFoo1Property = parserManager.GetNodeParser(typeOfFoo1Property);
-        object valueForFoo1Property = obj.Foo1Property;
-        tempContext = context with
+        object? valueForFoo1Property = obj.Foo1Property;
+        if (valueForFoo1Property is not null)
         {
-            NodeName = propertyNameForFoo1Property
-        };
-        var childNodeForFoo1Property = nodeParserForFoo1Property.Deparse(valueForFoo1Property, tempContext);
-        storageNode.Children.Add(childNodeForFoo1Property);
+            tempContext = context with
+            {
+                NodeName = propertyNameForFoo1Property
+            };
+            var childNodeForFoo1Property = nodeParserForFoo1Property.Deparse(valueForFoo1Property, tempContext);
+            storageNode.Children.Add(childNodeForFoo1Property);
+        }
 
         var propertyNameForFoo2Property = "Foo2Property";
         var typeOfFoo2Property = typeof(int);
         var nodeParserForFoo2Property = parserManager.GetNodeParser(typeOfFoo2Property);
-        object valueForFoo2Property = obj.Foo2Property;
-        tempContext = context with
+        object? valueForFoo2Property = obj.Foo2Property;
+        if (valueForFoo2Property is not null)
         {
-            NodeName = propertyNameForFoo2Property
-        };
-        var childNodeForFoo2Property = nodeParserForFoo2Property.Deparse(valueForFoo2Property, tempContext);
-        storageNode.Children.Add(childNodeForFoo2Property);
+            tempContext = context with
+            {
+                NodeName = propertyNameForFoo2Property
+            };
+            var childNodeForFoo2Property = nodeParserForFoo2Property.Deparse(valueForFoo2Property, tempContext);
+            storageNode.Children.Add(childNodeForFoo2Property);
+        }
 
         return storageNode;
     }
