b774226b8835b537e220205820c7e942800dbbdc
lindexi <lindexi_gd@163.com>
2025-11-20 19:29:57 +0800

    提供一些基础类型的转换功能

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Parsers/NodeParsers/BaseClrTypeNodeParser.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Parsers/NodeParsers/BaseClrTypeNodeParser.cs
@@ -1,28 +1,6 @@
-﻿using DotNetCampus.Storage.Lib.Parsers.Contexts;
-using DotNetCampus.Storage.Lib.StorageNodes;
-
-namespace DotNetCampus.Storage.Lib.Parsers.NodeParsers;
+﻿namespace DotNetCampus.Storage.Lib.Parsers.NodeParsers;
 
 internal abstract class BaseClrTypeNodeParser<T> : NodeParser<T>
 {
     public override string? TargetStorageName => null;
-}
-
-internal class BoolNodeParser : BaseClrTypeNodeParser<bool>
-{
-    protected internal override bool ParseCore(StorageNode node, in ParseNodeContext context)
-    {
-        
-        return bool.Parse(node.Value.AsSpan());
-    }
-
-    protected internal override StorageNode DeparseCore(bool obj, in DeparseNodeContext context)
-    {
-        var name = context.NodeName!;
-        return new StorageNode()
-        {
-            Name = name,
-            Value = obj.ToString(),
-        };
-    }
 }
---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Parsers/NodeParsers/Int64NodeParser.cs
@@ -0,0 +1,21 @@
+﻿using DotNetCampus.Storage.Lib.Parsers.Contexts;
+using DotNetCampus.Storage.Lib.StorageNodes;
+
+namespace DotNetCampus.Storage.Lib.Parsers.NodeParsers;
+
+internal class Int64NodeParser : BaseClrTypeNodeParser<long>
+{
+    protected internal override long ParseCore(StorageNode node, in ParseNodeContext context)
+    {
+        return long.Parse(node.Value.AsSpan());
+    }
+    protected internal override StorageNode DeparseCore(long obj, in DeparseNodeContext context)
+    {
+        var name = context.NodeName!;
+        return new StorageNode()
+        {
+            Name = name,
+            Value = obj.ToString(),
+        };
+    }
+}
