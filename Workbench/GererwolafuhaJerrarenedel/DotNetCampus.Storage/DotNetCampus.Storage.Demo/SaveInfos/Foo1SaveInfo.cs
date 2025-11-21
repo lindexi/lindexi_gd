532ece011da7fbe5209ea4e59bafd8914e64474b
lindexi <lindexi_gd@163.com>
2025-11-21 14:55:26 +0800

    建立测试项目

---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Demo/SaveInfos/Foo1SaveInfo.cs
@@ -0,0 +1,13 @@
+﻿using DotNetCampus.Storage.Lib.SaveInfos;
+
+namespace DotNetCampus.Storage.Demo.SaveInfos;
+
+[SaveInfoContract("Foo1")]
+public class Foo1SaveInfo : SaveInfo
+{
+    [SaveInfoMember("Foo1Property", Description = "This is a foo1 property.")]
+    public bool Foo1Property { get; set; } = false;
+
+    [SaveInfoMember("Foo2Property", Description = "This is a foo2 property.")]
+    public int Foo2Property { get; set; } = 3;
+}
