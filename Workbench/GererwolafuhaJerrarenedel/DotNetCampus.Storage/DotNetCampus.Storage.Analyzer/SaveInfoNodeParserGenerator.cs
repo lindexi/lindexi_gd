7a2cb45ac5281f7452f65bf4fb15bc90e982436e
lindexi <lindexi_gd@163.com>
2025-11-21 14:42:03 +0800

    建立源代码生成器关联关系

---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Analyzer/SaveInfoNodeParserGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCampus.Storage.Analyzer;
+
+[Generator(LanguageNames.CSharp)]
+public class SaveInfoNodeParserGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        
+    }
+}
