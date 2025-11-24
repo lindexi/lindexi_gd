369327531d5087cd9dd4216d7009e079aef97051
lindexi <lindexi_gd@163.com>
2025-11-24 17:54:04 +0800

    拆分文件

---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Analyzer/Contexts/SaveInfoPropertyInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetCampus.Storage.Analyzer;
+
+record SaveInfoPropertyInfo
+{
+    public required string PropertyName { get; init; }
+    public required string PropertyType { get; init; }
+    public required string StorageName { get; init; }
+    //public required bool IsNullable { get; init; }
+    public IReadOnlyList<string>? Aliases { get; init; }
+    public bool IsListType { get; init; }
+}
---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Analyzer/SaveInfoNodeParserGeneratorConfigHelper.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace DotNetCampus.Storage.Analyzer;
+
+static class SaveInfoNodeParserGeneratorConfigHelper
+{
+    public static IncrementalValueProvider<SaveInfoNodeParserGeneratorConfigOption> GetConfigOption(this IncrementalValueProvider<AnalyzerConfigOptionsProvider> analyzerProvider)
+    {
+        return analyzerProvider.Select((t, _) =>
+        {
+            bool shouldGenerateSaveInfoNodeParser = false;
+            if (t.GlobalOptions.TryGetValue("build_property.GenerateSaveInfoNodeParser",
+                    out var generateSaveInfoNodeParser))
+            {
+                bool.TryParse(generateSaveInfoNodeParser, out shouldGenerateSaveInfoNodeParser);
+            }
+
+            if (!t.GlobalOptions.TryGetValue("build_property.RootNamespace", out var rootNamespace))
+            {
+                rootNamespace = null;
+            }
+
+            return new SaveInfoNodeParserGeneratorConfigOption()
+            {
+                ShouldGenerateSaveInfoNodeParser = shouldGenerateSaveInfoNodeParser,
+                RootNamespace = rootNamespace,
+            };
+        });
+    }
+
+    public static IncrementalValuesProvider<SaveInfoClassInfo> FilterAndUpdateNamespace(this IncrementalValuesProvider<SaveInfoClassInfo> classInfoProvider, IncrementalValueProvider<SaveInfoNodeParserGeneratorConfigOption> configurationProvider)
+    {
+        var provider = classInfoProvider
+            .Combine(configurationProvider)
+            .Select((t, _) =>
+            {
+                if (!t.Right.ShouldGenerateSaveInfoNodeParser)
+                {
+                    return null;
+                }
+
+                if (t.Right.RootNamespace != null)
+                {
+                    return t.Left with
+                    {
+                        Namespace = t.Right.RootNamespace
+                    };
+                }
+                return t.Left;
+            })
+            .Where(static m => m is not null)
+            .Select(static (m, _) => m!);
+        return provider;
+    }
+}
