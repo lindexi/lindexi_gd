d04bcccb60e95ba86c66fe2ba0079abb1754b910
lindexi <lindexi_gd@163.com>
2025-11-25 14:45:10 +0800

    定义存储的日志

---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Logging/CrumbsStoragePathNavigator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.Storage.Lib.Logging;
+
+/// <summary>
+/// 面包屑的存储路径导航
+/// </summary>
+class CrumbsStoragePathNavigator
+{
+    private readonly List<string> _crumbs = new List<string>();
+
+    public void Push(string crumb) => _crumbs.Add(crumb);
+    public void Pop() => _crumbs.RemoveAt(_crumbs.Count - 1);
+
+    public void Restart(string root)
+    {
+        _crumbs.Clear();
+        _crumbs.Add(root);
+    }
+
+    public string GetCurrentPath()
+    {
+        return string.Join("->", _crumbs);
+    }
+}
