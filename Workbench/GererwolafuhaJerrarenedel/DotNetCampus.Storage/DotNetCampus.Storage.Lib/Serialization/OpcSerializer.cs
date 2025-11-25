0f04418a0adc013f0a69eac0045777bdd6159d76
lindexi <lindexi_gd@163.com>
2025-11-25 14:42:01 +0800

    定义存储转换格式

---
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Serialization/OpcSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCampus.Storage.Lib.Serialization;
+
+/// <summary>
+/// 使用 Open Packaging Conventions(OPC) 格式的序列化器
+/// </summary>
+/// https://learn.microsoft.com/en-us/previous-versions/windows/desktop/opc/open-packaging-conventions-overview
+/// > The ECMA-376 OpenXML, 1st Edition, Part 2: Open Packaging Conventions (OPC) can be more easily understood through an analogy with real world filing systems. 
+public class OpcSerializer
+{
+    public OpcSerializer(DirectoryInfo? workingDirectoryInfo = null)
+    {
+        WorkingDirectoryInfo = workingDirectoryInfo ??
+                               new DirectoryInfo(Path.Join(Path.GetTempPath(), Path.GetRandomFileName())); ;
+    }
+
+    public DirectoryInfo WorkingDirectoryInfo { get; }
+
+
+}
