936a681b705f35bca5305832363040847c5be102
lindexi <lindexi_gd@163.com>
2025-11-27 17:58:43 +0800

    提升寻找文件的性能

--- Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Documents/StorageDocuments/ReferencedFileManagers/ReferencedManager.cs
+++ Workbench/GererwolafuhaJerrarenedel/DotNetCampus.Storage/DotNetCampus.Storage.Lib/Documents/StorageDocuments/ReferencedFileManagers/ReferencedManager.cs
@@ -37,8 +37,18 @@ public class ReferencedManager : IReferencedManager
     {
         IReferencedManager referencedManager = this;
 
-        LocalStorageFileInfo? existFile = StorageFileManager.FileList.OfType<LocalStorageFileInfo>()
-            .FirstOrDefault(t => t.FileInfo.FullName == localFile.FullName);
+        LocalStorageFileInfo? existFile;
+
+        if (StorageFileManager is ILocalStorageFileManager localStorageFileManager)
+        {
+            // 对于 LocalStorageFileManager 可以提升一些速度
+            existFile = localStorageFileManager.GetFile(localFile);
+        }
+        else
+        {
+            existFile = StorageFileManager.FileList.OfType<LocalStorageFileInfo>()
+                .FirstOrDefault(t => t.FileInfo.FullName == localFile.FullName);
+        }
 
         // 存在本地文件了，说不定已经存在引用了
         if (existFile is not null)
