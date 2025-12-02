214bb84bf0c5667546a3f7614bb7b7ce88e90c33
lindexi <lindexi_gd@163.com>
2025-12-02 16:28:15 +0800

    主构造函数能够解决基类调用虚拟的优化

---
+++ Workbench/WonewheajeaNelbaylairreda/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var foo = new Foo();
+var f2 = new F2(foo);
+F1 f1 = f2;
+
+Console.WriteLine("Hello, World!");
+
+class Foo
+{
+
+}
+class F1
+{
+    public F1(Foo foo)
+    {
+        Foo = foo;
+        D();
+    }
+
+    public virtual Foo Foo { get; }
+
+    public virtual void D()
+    {
+    }
+}
+
+class F2(Foo foo) : F1(foo)
+{
+    public override Foo Foo => foo;
+
+    public override void D()
+    {
+        var f = Foo;
+    }
+}
