92681a4930b38acf49188662e00edfe2bd48528d
lindexi <lindexi_gd@163.com>
2025-12-16 22:07:19 +0800

    完成创建快捷方式功能

---
+++ Workbench/KaidilineRawrarkairfair/ShortcutHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Win32.System.Com;
+
+namespace KaidilineRawrarkairfair;
+
+internal static class ShortcutHelper
+{
+    /// <summary>
+    /// 创建一个快捷方式
+    /// </summary>
+    /// <param name="lnkFilePath">快捷方式的完全限定路径。</param>
+    /// <param name="workDir"></param>
+    /// <param name="args">快捷方式启动程序时需要使用的参数。</param>
+    /// <param name="targetPath"></param>
+    public static unsafe void CreateShortcut(string lnkFilePath, string targetPath, string workDir, string args = "")
+    {
+        var shellLinkW = ShellLinkProvider.CreateShellLink();
+        shellLinkW->SetPath(targetPath);
+        shellLinkW->SetArguments(args);
+        shellLinkW->SetWorkingDirectory(workDir);
+
+        shellLinkW->QueryInterface(out IPersistFile* persistFile);
+        //persistFile->SaveCompleted(lnkFilePath);
+        persistFile->Save(lnkFilePath, false);
+    }
+}
