eaf06182a95d8fde0243509ecd7c60e678fa8b95
lindexi <lindexi_gd@163.com>
2025-12-19 10:38:58 +0800

    判断传入的是插入硬件

---
+++ Workbench/CawrelibairquJojaijurhewe/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+foreach (var driveInfo in DriveInfo.GetDrives())
+{
+    if (driveInfo.DriveType == DriveType.Removable)
+    {
+        Console.WriteLine($"{driveInfo.RootDirectory} 是 U 盘");
+    }
+}
+Console.WriteLine("Hello, World!");
