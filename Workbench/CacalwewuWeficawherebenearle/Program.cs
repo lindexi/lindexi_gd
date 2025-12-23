f94846f591d5706e15024b1e1fa996f1a7322920
lindexi <lindexi_gd@163.com>
2025-12-23 17:38:34 +0800

    测试设置不存在的图标

--- Workbench/CacalwewuWeficawherebenearle/Program.cs
+++ Workbench/CacalwewuWeficawherebenearle/Program.cs
@@ -2,7 +2,7 @@
 using CacalwewuWeficawherebenearle;
 
 var shortcutFile = Path.Join(AppContext.BaseDirectory, "6.lnk");
-var iconFile = @"C:\lindexi\Work\Icon.ico";
+var iconFile = @"C:\lindexi\不存在\Icon.ico";
 
 ShortcutHelper.CreateShortcut(shortcutFile, "https://blog.lindexi.com/", Directory.GetCurrentDirectory(), iconFile: iconFile);
 
