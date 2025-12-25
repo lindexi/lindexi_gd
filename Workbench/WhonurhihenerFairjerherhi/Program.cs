c18cce641b6c23202d38092ade20b413720a463e
lindexi <lindexi_gd@163.com>
2025-12-25 17:39:10 +0800

    创建头的内容

---
+++ Workbench/WhonurhihenerFairjerherhi/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ReadOnlySpan<byte> head = "Content of DotNetCampus.Installer in PE file Overlay payload 1.0"u8;
+var length = head.Length;
+Console.WriteLine("Hello, World!");
