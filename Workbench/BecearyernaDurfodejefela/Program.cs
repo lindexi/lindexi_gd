8dc0e09a60e1bf0e5a6d88ab63bfeb64da7b3dde
lindexi <lindexi_gd@163.com>
2025-12-26 15:57:14 +0800

    测试创建快捷方式的方法

---
+++ Workbench/BecearyernaDurfodejefela/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BecearyernaDurfodejefela;
+
+var shortcutFile = Path.Join(AppContext.BaseDirectory, "1.lnk");
+
+// 尽管文档上明确说明不能用来创建指向 URL 的快捷方式，但实际上是可以的
+// > This interface cannot be used to create a link to a URL.
+ShortcutHelper.CreateShortcut(shortcutFile, "https://blog.lindexi.com/", Directory.GetCurrentDirectory());
+
+Console.WriteLine("Hello, World!");
+
+Console.Read();
