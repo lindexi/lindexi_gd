9a4f5434f0694d31b8ecb7c7d1fa92e4547e047f
lindexi <lindexi_gd@163.com>
2025-12-27 10:50:34 +0800

    使用基础调用进行测试

--- SemanticKernelSamples/BewukobuheQinalaykodall/Program.cs
+++ SemanticKernelSamples/BewukobuheQinalaykodall/Program.cs
@@ -12,8 +12,16 @@ using OpenAI.Chat;
 var keyFile = @"C:\lindexi\Work\deepseek.txt";
 var key = File.ReadAllText(keyFile);
 
+var openAiClient = new OpenAIClient(new ApiKeyCredential(key),new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://api.deepseek.com/v1")
+});
+var chatClient = openAiClient.GetChatClient("deepseek-chat");
+var result = chatClient.CompleteChat([new UserChatMessage("Tell me a joke about a pirate.")]);
+var chatCompletion = result.Value;
+
 AIAgent agent = new AzureOpenAIClient(
-        new Uri("https://api.deepseek.com/v1"),
+        new Uri("https://api.deepseek.com/"),
         new ApiKeyCredential(key))
     .GetChatClient("deepseek-chat")
     .CreateAIAgent(instructions: "You are good at telling jokes.");
