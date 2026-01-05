b6100da3e1c925b13324cbc79f924e7ebafc17ae
lindexi <lindexi_gd@163.com>
2026-01-05 11:08:14 +0800

    测试命令行解析

---
+++ Workbench/LaryiheahaiwhallcurqiBaywhofikall/App/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using App;
+using DotNetCampus.Cli;
+
+var option = CommandLine.Parse(args).As<Option>();
+
+Console.WriteLine("Hello, World!");
