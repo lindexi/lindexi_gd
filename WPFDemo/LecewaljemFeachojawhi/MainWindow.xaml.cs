598af5ac067563ee296f4fd986ad15ee3d701c49
lindexi <lindexi_gd@163.com>
2026-01-06 09:15:32 +0800

    读取屏幕 Edid 信息

---
+++ WPFDemo/LecewaljemFeachojawhi/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using LecewaljemFeachojawhi.EdidReader;
+
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LecewaljemFeachojawhi;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var stringBuilder = new StringBuilder();
+        var edids = RawEdid.Get();
+        for (var i = 0; i < edids.Length; i++)
+        {
+            var edid = edids[i];
+            var detail = edid.GetDetail();
+            stringBuilder
+                .AppendLine($"Edid Manufacturer: {detail.Manufacturer}")
+                .AppendLine($"{detail.HorizontalDisplaySize}x{detail.VerticalDisplaySize}")
+                .AppendLine($"Raw Data:")
+                .AppendLine($"{string.Join(' ', edid.GetRawData().Select(t => t.ToString("X2")))}")
+                ;
+
+            if (i < edids.Length - 1)
+            {
+                stringBuilder.AppendLine("==========");
+            }
+        }
+
+        LogTextBlock.Text = stringBuilder.ToString();
+    }
+}
+
