8b63c010833b35e6c695fb583b7b08446ed95254
lindexi <lindexi_gd@163.com>
2026-01-07 11:37:05 +0800

    测试不能添加重复的值

---
+++ Workbench/LeawalnebereHearernallka/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+SortedList<int, int> list = [];
+
+list.Add(0, 0);
+list.Add(0, 1);
+list.Add(0, 2);
+
+list.Values.Add(2);
+
+Console.WriteLine("Hello, World!");
