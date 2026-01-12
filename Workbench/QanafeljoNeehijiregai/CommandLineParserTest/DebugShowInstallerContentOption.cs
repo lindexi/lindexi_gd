01a15135c45e9f7745d510b73d21b446184f02d9
lindexi <lindexi_gd@163.com>
2026-01-12 08:55:06 +0800

    最简测试拦截器失败

---
+++ Workbench/QanafeljoNeehijiregai/CommandLineParserTest/DebugShowInstallerContentOption.cs
@@ -0,0 +1,11 @@
+﻿using DotNetCampus.Cli.Compiler;
+
+namespace DotNetCampus.Installer.Lib.Commandlines;
+
+/// <summary>
+/// 显示安装包内容的调试命令
+/// </summary>
+[Command("debug show-content")]
+public class DebugShowInstallerContentOption
+{
+}
