a20890883fd89b1410a92767a43df2b4c5b70c8a
lindexi <lindexi_gd@163.com>
2026-01-15 15:21:56 +0800

    尝试实现 HID 转换逻辑

--- HIDReportParserHowogairlerejaceBemnayferedewall/HIDReportParserHowogairlerejaceBemnayferedewall/Program.cs
+++ HIDReportParserHowogairlerejaceBemnayferedewall/HIDReportParserHowogairlerejaceBemnayferedewall/Program.cs
@@ -6,14 +6,57 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var t = @"06 C9 FF 09 04 A1 5C 09 75 15 00 25 FF 35 00 45
-  00 65 00 55 00 75 08 95 40 81 02 09 76 95 20 91
-  02 09 76 95 04 B1 02 C1 00";
+        var keyboardReport = @"
+          05 01
+          09 06
+          A1 01
+          85 01
+          05 07
+          19 E0
+          29 E7
+          15 00
+          25 01
+          95 08
+          75 01
+          81 02
+          95 01
+          75 08
+          81 03
+          05 07
+          19 00
+          29 68
+          15 00
+          25 68
+          95 06
+          75 08
+          81 00
+          05 08
+          19 01
+          29 05
+          95 05
+          75 01
+          91 02
+          95 01
+          75 03
+          91 01
+          C0
+        ";
 
         var parser = new ReportParser();
 
-        var bytes = parser.ParseHexByteText(t);
+        var bytes = parser.ParseHexByteText(keyboardReport);
         var report = parser.Parse(bytes);
+        Console.WriteLine(report);
+        
+        Console.WriteLine("\n" + new string('-', 60) + "\n");
+        
+        // Test with the custom report
+        var customReport = @"06 C9 FF 09 04 A1 5C 09 75 15 00 25 FF 35 00 45
+  00 65 00 55 00 75 08 95 40 81 02 09 76 95 20 91
+  02 09 76 95 04 B1 02 C1 00";
+
+        bytes = parser.ParseHexByteText(customReport);
+        report = parser.Parse(bytes);
         Console.WriteLine(report);
     }
 }
--- HIDReportParserHowogairlerejaceBemnayferedewall/HIDReportParserHowogairlerejaceBemnayferedewall/ReportParser.cs
+++ HIDReportParserHowogairlerejaceBemnayferedewall/HIDReportParserHowogairlerejaceBemnayferedewall/ReportParser.cs
@@ -11,6 +11,9 @@ namespace HIDReportParserHowogairlerejaceBemnayferedewall;
 
 class ReportParser
 {
+    private int _currentUsagePage = -1;
+    private readonly StringBuilder _output = new();
+
     public IList<byte> ParseHexByteText(string hexByteText)
     {
         List<byte> byteList = new List<byte>();
@@ -30,7 +33,7 @@ class ReportParser
             }
 
             bool IsHexCharIndex(int index) => IsHexChar(hexByteText[index]);
-            bool IsHexChar(char c) => (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F');
+            bool IsHexChar(char c) => (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
         }
 
         return byteList;
@@ -38,18 +41,23 @@ class ReportParser
 
     public string Parse(IList<byte> report)
     {
+        _output.Clear();
+        _currentUsagePage = -1;
+        
+        _output.AppendLine("Report Item Parse:");
+        
         // 用于控制缩进
         int space = 0;
 
-        for (var index = 0; index < report.Count; index++)
+        for (var index = 0; index < report.Count; )
         {
-
             var itemTag = report[index] & TagMask;
             var itemSize = ToItemSize(report[index] & SizeMask);
 
             if (index + itemSize >= report.Count)
             {
-                throw new Exception("Report is too short");
+                _output.AppendLine("out of buffer.");
+                break;
             }
 
             var itemData = GetItemDataLength(report, index + 1, itemSize);
@@ -59,97 +67,325 @@ class ReportParser
                     ToMainItem(itemTag, itemData, ref space);
                     break;
                 case GlobalItem:
-                    //ToGlobalItem(itemTag, itemData, space, ref space);
+                    ToGlobalItem(itemTag, itemData, space);
                     break;
                 case LocalItem:
-                    //ToLocalItem(itemTag, itemData, space, ref space);
+                    ToLocalItem(itemTag, itemData, space);
                     break;
                 default:
+                    _output.AppendLine($"Unknown Type: {itemTag:X2}, index: {index}");
                     break;
             }
             index += (itemSize + 1);
         }
 
-        return null;
+        return _output.ToString();
     }
 
     void ToMainItem(int itemTag, int itemData, ref int space)
     {
-        var stringBuilder = new StringBuilder();
+        var str = new StringBuilder();
 
         for (int i = 0; i < space; i++)
         {
-            stringBuilder.Append("  ");
+            str.Append(' ');
         }
 
-        if (itemTag == Input(0))
+        if ((itemTag & TagMask) == Input(0))
         {
-            stringBuilder.Append($"  Input ({ConvertDataType(Input(0), itemData)})");
+            str.Append($"  Input ({ConvertDataType(Input(0), itemData)})");
         }
-        else if (itemTag == Output(0))
+        else if ((itemTag & TagMask) == Output(0))
         {
-            stringBuilder.Append($"  Output ({ConvertDataType(Output(0), itemData)})");
+            str.Append($"  Output ({ConvertDataType(Output(0), itemData)})");
         }
-        else if (itemTag == Feature(0))
+        else if ((itemTag & TagMask) == Feature(0))
         {
-            stringBuilder.Append($"  Feature ({ConvertDataType(Feature(0), itemData)})");
+            str.Append($"  Feature ({ConvertDataType(Feature(0), itemData)})");
         }
-        else if (itemTag == Collection(0))
+        else if ((itemTag & TagMask) == Collection(0))
         {
-            //space += 2;
-            //stringBuilder.Append($"  Collection ({ConvertColletionType(itemData)})");
-            ////if (itemData & 0xFFFFFF00U)
-            //stringBuilder.Append($"  ???");
-
+            str.Append($"Collection ({ConvertCollectionType((byte)itemData)})");
+            if ((itemData & 0xFFFFFF00) != 0)
+            {
+                str.Append(" ???");
+            }
+            space += 2;
         }
-        else if (itemTag == EndCollection(0))
+        else if ((itemTag & TagMask) == EndCollection(0))
         {
             space -= 2;
-            stringBuilder.Append($"  End Collection ({ConvertDataType(EndCollection(0), itemData)})");
+            // 减少缩进后重新格式化
+            str.Clear();
+            for (int i = 0; i < space; i++)
+            {
+                str.Append(' ');
+            }
+            str.Append("End Collection");
         }
         else
         {
-            stringBuilder.Append($"Unknown Item: {itemTag:X2}X");
+            str.Append($"Unknown Item: {itemTag:X2}");
         }
 
-        Console.WriteLine(stringBuilder.ToString());
+        _output.AppendLine(str.ToString());
     }
 
     string ConvertDataType(int itemTag, int itemData)
     {
         var str = new StringBuilder(128);
 
-        str.Append((itemData & Constant) == 0 ? "Cnst" : "Data");
-        str.Append((itemData & Variable) == 0 ? ", Var" : ", Array");
-        str.Append((itemData & Relative) == 0 ? ", Rel" : ", Abs");
-        if ((itemData & Wrap) == 1)
+        str.Append((itemData & Constant) != 0 ? "Cnst" : "Data");
+        str.Append((itemData & Variable) != 0 ? ", Var" : ", Array");
+        str.Append((itemData & Relative) != 0 ? ", Rel" : ", Abs");
+        
+        if ((itemData & Wrap) != 0)
         {
             str.Append(", Wrap");
         }
 
-        if ((itemData & NonLinear) == 1)
+        if ((itemData & NonLinear) != 0)
         {
             str.Append(", NonLinear");
         }
 
-        if ((itemData & NoPreferred) == 1)
+        if ((itemData & NoPreferred) != 0)
         {
             str.Append(", No Preferred");
         }
 
-        if ((itemData & NullState) == 1)
+        if ((itemData & NullState) != 0)
         {
             str.Append(", Null State");
         }
 
-        if ((itemTag & TagMask) != Input(0) && (itemData & Volatile) == 1)
+        // Input Items Data bit 7 is undefined and is RFU.
+        if ((itemTag & TagMask) != Input(0) && (itemData & Volatile) != 0)
         {
             str.Append(", Volatile");
         }
 
-        if ((itemData & 0xFFFFFF00) == 1)
+        // Data byte 1~3 is RFU.
+        if ((itemData & 0xFFFFFF00) != 0)
         {
             str.Append(", ???");
+        }
+
+        return str.ToString();
+    }
+
+    string ConvertCollectionType(byte itemData)
+    {
+        return itemData switch
+        {
+            ColPhysical => "Physical",
+            ColApplication => "App",
+            ColLogical => "Logical",
+            ColReport => "Report",
+            ColNamedArray => "Named Array",
+            ColUsageSwitch => "Usage Switch",
+            ColUsageModifier => "Usage Modifier",
+            _ => "Unknown"
+        };
+    }
+
+    void ToGlobalItem(int itemTag, int itemData, int space)
+    {
+        var str = new StringBuilder(256);
+
+        for (int i = 0; i < space; i++)
+        {
+            str.Append(' ');
+        }
+
+        if ((itemTag & TagMask) == UsagePage(0))
+        {
+            _currentUsagePage = itemData;
+            str.Append($"Usage Page ({GetUsagePageName(itemData)})");
+        }
+        else if ((itemTag & TagMask) == LogicalMinimum(0))
+        {
+            str.Append($"Logical Min ({itemData})");
+        }
+        else if ((itemTag & TagMask) == LogicalMaximum(0))
+        {
+            str.Append($"Logical Max ({itemData})");
+        }
+        else if ((itemTag & TagMask) == PhysicalMinimum(0))
+        {
+            str.Append($"Physical Min ({itemData})");
+        }
+        else if ((itemTag & TagMask) == PhysicalMaximum(0))
+        {
+            str.Append($"Physical Max ({itemData})");
+        }
+        else if ((itemTag & TagMask) == UnitExponent(0))
+        {
+            str.Append($"Unit Exponent ({GetExponent(itemData)})");
+        }
+        else if ((itemTag & TagMask) == Unit(0))
+        {
+            str.Append($"Unit ({GetUnit((uint)itemData)})");
+        }
+        else if ((itemTag & TagMask) == ReportSize(0))
+        {
+            str.Append($"Report Size ({itemData})");
+        }
+        else if ((itemTag & TagMask) == ReportID(0))
+        {
+            str.Append($"Report ID ({itemData})");
+        }
+        else if ((itemTag & TagMask) == ReportCount(0))
+        {
+            str.Append($"Report Count ({itemData})");
+        }
+        else if ((itemTag & TagMask) == Push(0))
+        {
+            str.Append("Push");
+        }
+        else if ((itemTag & TagMask) == Pop(0))
+        {
+            str.Append("Pop");
+        }
+        else
+        {
+            str.Append($"Unknown Item: {itemTag:X2}");
+        }
+
+        _output.AppendLine(str.ToString());
+    }
+
+    void ToLocalItem(int itemTag, int itemData, int space)
+    {
+        var str = new StringBuilder(256);
+
+        for (int i = 0; i < space; i++)
+        {
+            str.Append(' ');
+        }
+
+        if ((itemTag & TagMask) == Usage(0))
+        {
+            str.Append($"Usage ({ToUsage((uint)_currentUsagePage, (uint)itemData)})");
+        }
+        else if ((itemTag & TagMask) == UsageMinimum(0))
+        {
+            str.Append($"Usage Min ({itemData})");
+        }
+        else if ((itemTag & TagMask) == UsageMaximum(0))
+        {
+            str.Append($"Usage Max ({itemData})");
+        }
+        else if ((itemTag & TagMask) == DesignatorIndex(0))
+        {
+            str.Append($"Designator Index ({itemData})");
+        }
+        else if ((itemTag & TagMask) == DesignatorMinimum(0))
+        {
+            str.Append($"Designator Min ({itemData})");
+        }
+        else if ((itemTag & TagMask) == DesignatorMaximum(0))
+        {
+            str.Append($"Designator Max ({itemData})");
+        }
+        else if ((itemTag & TagMask) == StringIndex(0))
+        {
+            str.Append($"String Index ({itemData})");
+        }
+        else if ((itemTag & TagMask) == StringMinimum(0))
+        {
+            str.Append($"String Min ({itemData})");
+        }
+        else if ((itemTag & TagMask) == StringMaximum(0))
+        {
+            str.Append($"String Max ({itemData})");
+        }
+        else if ((itemTag & TagMask) == Delimiter(0))
+        {
+            var delimiterStr = itemData == 1 ? "Open Set" : (itemData == 0 ? "Close Set" : "Unknown Setting");
+            str.Append($"Delimiter ({delimiterStr})");
+        }
+        else
+        {
+            str.Append($"Unknown Item: {itemTag:X2}");
+        }
+
+        _output.AppendLine(str.ToString());
+    }
+
+    string GetUsagePageName(int usagePage)
+    {
+        return (uint)usagePage switch
+        {
+            UP_Generic_Desktop => "Generic Desktop",
+            UP_Simulation_Controls => "Simulation",
+            UP_VR_Controls => "VR Controls",
+            UP_Sport_Controls => "Sport Controls",
+            UP_Game_Controls => "Game Controls",
+            UP_Generic_Device_Controls => "Generic Device",
+            UP_Keyboard_or_Keypad => "Keyboard",
+            UP_LEDs => "LEDs",
+            UP_Button => "Buttons",
+            UP_Ordinal => "Ordinal",
+            UP_Telephony => "Telephony",
+            UP_Consumer => "Consumer",
+            UP_Digitizer => "Digitizer",
+            UP_PID_Page => "PID Page",
+            UP_Unicode => "Unicode",
+            UP_Alphanumeric_Display => "Alphanumeric Display",
+            UP_Medical_Instruments => "Medical Instruments",
+            _ => "Unknown"
+        };
+    }
+
+    string GetExponent(int itemData)
+    {
+        byte code = (byte)itemData;
+        if (code >= 0x05 && code <= 0x0F)
+        {
+            string[] str = { "5", "6", "7", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1" };
+            return str[code - 5];
+        }
+        return "Unknown";
+    }
+
+    sbyte NibbleToByte(sbyte nibble) => (sbyte)((nibble & 0x08) != 0 ? (nibble | 0xF0) : nibble);
+
+    string GetUnit(uint itemData)
+    {
+        string?[][] strUnits = [
+            null,
+            ["SI Linear", "cm", "Gram", "Seconds", "Kelvin", "Ampere", "Candela"],
+            ["SI Rotation", "rad", "Gram", "Seconds", "Kelvin", "Ampere", "Candela"],
+            ["English Linear", "Inch", "Slug", "Seconds", "Fahrenheit", "Ampere", "Candela"],
+            ["English Rotation", "Degrees", "Slug", "Seconds", "Fahrenheit", "Ampere", "Candela"]
+        ];
+
+        sbyte nibble = (sbyte)(itemData & 0xF);
+        
+        if (nibble == 0)
+            return "None";
+        
+        if (nibble < 1 || nibble > 4)
+            return "Unknown";
+
+        var str = new StringBuilder();
+        var selectedUnit = strUnits[nibble]!; // 保存选中的单位系统
+        str.Append(selectedUnit[0]);
+        str.Append(':');
+
+        itemData >>= 4;
+        int nibbleNo = 1;
+
+        while (itemData != 0 && nibbleNo < 7)
+        {
+            nibble = (sbyte)(itemData & 0xF);
+            if (nibble != 0)
+            {
+                str.Append($" {selectedUnit[nibbleNo]}[{NibbleToByte(nibble)}]");
+            }
+            itemData >>= 4;
+            nibbleNo++;
         }
 
         return str.ToString();
@@ -203,20 +439,56 @@ class ReportParser
     private int Collection(int size) => (MainItem | (0x0A << TagOffset) | (size & SizeMask));
     private int EndCollection(int size) => (MainItem | (0x0C << TagOffset) | (size & SizeMask));
 
-    private const int Data = (0);
-    private const int Constant = (1);
-    private const int Array = (0);
+    // Input, Output and Feature Items Data bit constants
+    private const int Data = 0;
+    private const int Constant = 1;
+    private const int Array = 0;
     private const int Variable = (1 << 1);
-    private const int Absolute = (0);
+    private const int Absolute = 0;
     private const int Relative = (1 << 2);
-    private const int NoWrap = (0);
+    private const int NoWrap = 0;
     private const int Wrap = (1 << 3);
-    private const int Linear = (0);
+    private const int Linear = 0;
     private const int NonLinear = (1 << 4);
-    private const int PreferredState = (0);
+    private const int PreferredState = 0;
     private const int NoPreferred = (1 << 5);
-    private const int NoNullPosition = (0);
+    private const int NoNullPosition = 0;
     private const int NullState = (1 << 6);
-    private const int Nonvolatile = (0);
+    private const int Nonvolatile = 0;
     private const int Volatile = (1 << 7);
+
+    // Collection types
+    private const byte ColPhysical = 0x00;
+    private const byte ColApplication = 0x01;
+    private const byte ColLogical = 0x02;
+    private const byte ColReport = 0x03;
+    private const byte ColNamedArray = 0x04;
+    private const byte ColUsageSwitch = 0x05;
+    private const byte ColUsageModifier = 0x06;
+
+    // Global Item Tag methods
+    private int UsagePage(int size) => (GlobalItem | (0x00 << TagOffset) | (size & SizeMask));
+    private int LogicalMinimum(int size) => (GlobalItem | (0x01 << TagOffset) | (size & SizeMask));
+    private int LogicalMaximum(int size) => (GlobalItem | (0x02 << TagOffset) | (size & SizeMask));
+    private int PhysicalMinimum(int size) => (GlobalItem | (0x03 << TagOffset) | (size & SizeMask));
+    private int PhysicalMaximum(int size) => (GlobalItem | (0x04 << TagOffset) | (size & SizeMask));
+    private int UnitExponent(int size) => (GlobalItem | (0x05 << TagOffset) | (size & SizeMask));
+    private int Unit(int size) => (GlobalItem | (0x06 << TagOffset) | (size & SizeMask));
+    private int ReportSize(int size) => (GlobalItem | (0x07 << TagOffset) | (size & SizeMask));
+    private int ReportID(int size) => (GlobalItem | (0x08 << TagOffset) | (size & SizeMask));
+    private int ReportCount(int size) => (GlobalItem | (0x09 << TagOffset) | (size & SizeMask));
+    private int Push(int size) => (GlobalItem | (0x0A << TagOffset) | (size & SizeMask));
+    private int Pop(int size) => (GlobalItem | (0x0B << TagOffset) | (size & SizeMask));
+
+    // Local Item Tag methods
+    private int Usage(int size) => (LocalItem | (0x00 << TagOffset) | (size & SizeMask));
+    private int UsageMinimum(int size) => (LocalItem | (0x01 << TagOffset) | (size & SizeMask));
+    private int UsageMaximum(int size) => (LocalItem | (0x02 << TagOffset) | (size & SizeMask));
+    private int DesignatorIndex(int size) => (LocalItem | (0x03 << TagOffset) | (size & SizeMask));
+    private int DesignatorMinimum(int size) => (LocalItem | (0x04 << TagOffset) | (size & SizeMask));
+    private int DesignatorMaximum(int size) => (LocalItem | (0x05 << TagOffset) | (size & SizeMask));
+    private int StringIndex(int size) => (LocalItem | (0x07 << TagOffset) | (size & SizeMask));
+    private int StringMinimum(int size) => (LocalItem | (0x08 << TagOffset) | (size & SizeMask));
+    private int StringMaximum(int size) => (LocalItem | (0x09 << TagOffset) | (size & SizeMask));
+    private int Delimiter(int size) => (LocalItem | (0x0A << TagOffset) | (size & SizeMask));
 }
