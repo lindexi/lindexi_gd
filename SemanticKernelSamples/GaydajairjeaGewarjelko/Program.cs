2619464f3d06e5258f8057051ec4b1db3782694c
lindexi <lindexi_gd@163.com>
2026-01-16 11:51:27 +0800

    继续测试处理页面逻辑

--- SemanticKernelSamples/GaydajairjeaGewarjelko/Program.cs
+++ SemanticKernelSamples/GaydajairjeaGewarjelko/Program.cs
@@ -3,7 +3,7 @@ using System;
 using System.ClientModel;
 using System.ComponentModel;
 using System.Text.Json;
-
+using System.Text.Json.Serialization;
 using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
 
@@ -22,16 +22,17 @@ var openAiClient = new OpenAIClient(new ApiKeyCredential(key), new OpenAIClientO
 
 var chatClient = openAiClient.GetChatClient("ep-20260115192014-kgkxq");
 
-JsonElement schema = AIJsonUtilities.CreateJsonSchema(typeof(PersonInfo));
+JsonElement schema = AIJsonUtilities.CreateJsonSchema(typeof(ElementOperationInfo));
 ChatOptions chatOptions = new()
 {
+    // {"type":"object","properties":{"ElementId":{"type":["string","null"]},"OperationName":{"description":"\u5305\u62EC\u79FB\u52A8\u3001\u5220\u9664\u7B49","type":["string","null"]}}}
     ResponseFormat = ChatResponseFormat.ForJsonSchema(
         schema: schema,
-        schemaName: "PersonInfo",
-        schemaDescription: "Information about a person including their name, age, and occupation"),
+        schemaName: "ElementOperationInfo",
+        schemaDescription: "对某个元素的处理的信息"),
 };
 
-chatOptions.Instructions = "You are a helpful assistant.";
+chatOptions.Instructions = "你是一个 PPT 文档处理助手";
 ChatClientAgent aiAgent = chatClient.CreateAIAgent(new ChatClientAgentOptions()
 {
     Name = "HelpfulAssistant",
@@ -39,19 +40,22 @@ ChatClientAgent aiAgent = chatClient.CreateAIAgent(new ChatClientAgentOptions()
     ChatOptions = chatOptions
 });
 
-var response = await aiAgent.RunAsync("Please provide information about John Smith, who is a 35-year-old software engineer.");
+var response = await aiAgent.RunAsync("请帮我删除 Id 为 f1 的元素");
 
-var personInfo = response.Deserialize<PersonInfo>(JsonSerializerOptions.Web);
-Console.WriteLine($"Name: {personInfo.Name}, Age: {personInfo.Age}, Occupation: {personInfo.Occupation}");
+// {{"ElementId": "f1", "OperationName": "删除"}}
+var elementOperationInfo = response.Deserialize<ElementOperationInfo>(JsonSerializerOptions.Web);
 
 Console.WriteLine();
 
 Console.Read();
 
 
-public class PersonInfo
+public class ElementOperationInfo
 {
-    public string? Name { get; set; }
-    public int? Age { get; set; }
-    public string? Occupation { get; set; }
+    [JsonPropertyName("ElementId")]
+    public string? ElementId { get; set; }
+
+    [JsonPropertyName("OperationName")]
+    [Description("包括移动、删除等")]
+    public string? OperationName { get; set; }
 }
