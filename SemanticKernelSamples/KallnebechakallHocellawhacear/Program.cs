1f9d87695e2e1ace8244f304f50e7bca162a0a97
lindexi <lindexi_gd@163.com>
2026-01-19 19:53:11 +0800

    准备封装和测试序列化功能

---
+++ SemanticKernelSamples/KallnebechakallHocellawhacear/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+
+using static System.Net.Mime.MediaTypeNames;
+
+var text = """
+           {
+                # 文本提示词与参数组合
+                "type": "text",
+                "text": "无人机以极快速度穿越复杂障碍或自然奇观，带来沉浸式飞行体验  --duration 5 --camerafixed false --watermark true"
+            }
+           """;
+
+Console.WriteLine(JsonEncodedText.Encode(text).Value);
