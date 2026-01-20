e3973203c744aaa3fa2be80eae5be29d140682e7
lindexi <lindexi_gd@163.com>
2026-01-20 14:09:56 +0800

    尝试回发图片成功，但手机无法打开

--- Workbench/WichijarbejeHearlaridejenerewhu/WichijarbejeHearlaridejenerewhu/MainPage.xaml.cs
+++ Workbench/WichijarbejeHearlaridejenerewhu/WichijarbejeHearlaridejenerewhu/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Windows.Storage.Pickers;
+
 namespace WichijarbejeHearlaridejenerewhu;
 
 public sealed partial class MainPage : Page
@@ -5,5 +7,19 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+    }
+
+    private async void UploadFileButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var fileOpenPicker = new FileOpenPicker();
+        fileOpenPicker.FileTypeFilter.Add(".png");
+        fileOpenPicker.FileTypeFilter.Add(".jpg");
+        fileOpenPicker.FileTypeFilter.Add(".jpeg");
+
+        var file = await fileOpenPicker.PickSingleFileAsync();
+        await using var fileStream = await file.OpenStreamForReadAsync();
+
+        var httpClient = new HttpClient();
+        await httpClient.PostAsync("http://172.20.114.23:12779/", new StreamContent(fileStream));
     }
 }
