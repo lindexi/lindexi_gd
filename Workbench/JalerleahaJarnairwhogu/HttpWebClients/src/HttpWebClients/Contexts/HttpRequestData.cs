1cbec075f2d18ebca0e2bc10233279a36fcf2502
lindexi <lindexi_gd@163.com>
2026-01-22 10:24:34 +0800

    加上实现测试代码

--- Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/Contexts/HttpRequestData.cs
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/Contexts/HttpRequestData.cs
@@ -42,3 +42,92 @@ public interface IGetHttpRequestData : IHttpRequestData
 {
     string? GetQueryString();
 }
+
+
+[HttpRequestData(HttpMethodType.Get, "/api/foo", ResponseType = typeof(FooTestHttpResponseData))]
+public partial class FooTestGetHttpRequestData : HttpRequestData
+{
+    public string? Id { get; init; }
+}
+
+public partial class FooTestGetHttpRequestData : IGetHttpRequestData
+{
+    // 这是源代码生成器生成的部分
+
+    public HttpMethod HttpMethod => HttpMethod.Get;
+    public string Url => "/api/foo";
+    public JsonSerializerContext? JsonSerializerContext { get; set; }
+    public Type? HttpResponseDataType => typeof(FooTestHttpResponseData);
+
+    public string? GetQueryString()
+    {
+        StringBuilder? queryStringBuilder = null;
+
+        AppendQueryString(nameof(Id), Id);
+
+        return queryStringBuilder?.ToString();
+
+        void AppendQueryString(string key, string? value)
+        {
+            if (value is null)
+            {
+                return;
+            }
+            if (queryStringBuilder is null)
+            {
+                queryStringBuilder = new StringBuilder("?");
+            }
+            else
+            {
+                queryStringBuilder.Append('&');
+            }
+            queryStringBuilder.Append(Uri.EscapeDataString(key));
+            queryStringBuilder.Append('=');
+            queryStringBuilder.Append(Uri.EscapeDataString(value));
+        }
+    }
+
+    // 对于 Get 类型的，额外添加方法，获取请求参数
+}
+
+// 附带生成请求扩展方法
+public static partial class FooTestHttpRequestDataExtensions
+{
+    // 根据是 Get 还是 Post 请求，生成不同的扩展方法
+
+    // 如果是没有指定 Response 类型的，则生成 HttpResponseData 返回值的扩展方法
+
+    /// <summary>
+    /// 请求
+    /// </summary>
+    /// <param name="client"></param>
+    /// <param name="requestData"></param>
+    /// <param name="url">如果要重新指定请求地址，则传入此参数。这个参数会根据 FooTestHttpRequestData 是否有标明，而决定是可空默认，还是必填参数</param>
+    /// <returns></returns>
+    public static async Task<HttpJsonResponse<FooTestHttpResponseData>> GetAsync(this HttpWebClient client,
+        FooTestGetHttpRequestData requestData, string? url = null)
+    {
+        url ??= requestData.Url;
+        var requestUrl = url + requestData.GetQueryString();
+
+        var httpClient = client.AsHttpClient();
+        HttpResponseMessage? httpResponseMessage = null;
+        Exception? exception = null;
+        string? contentText = null;
+        try
+        {
+            httpResponseMessage = await httpClient.GetAsync(requestUrl);
+            contentText = await httpResponseMessage.Content.ReadAsStringAsync();
+        }
+        catch (Exception e)
+        {
+            exception = e;
+        }
+
+        return new HttpJsonResponse<FooTestHttpResponseData>(client, requestData, httpResponseMessage, contentText, exception);
+    }
+}
+
+public class FooTestHttpResponseData
+{
+}
