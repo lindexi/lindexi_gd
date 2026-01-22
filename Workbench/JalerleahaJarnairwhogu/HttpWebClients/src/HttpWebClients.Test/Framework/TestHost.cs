0fe092f75ab40e60ef2d51fee58f74961df6f065
lindexi <lindexi_gd@163.com>
2026-01-22 09:47:33 +0800

    定义测试框架

---
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients.Test/Framework/TestHost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace HttpWebClients.Test;
+
+class TestHost : IDisposable
+{
+    public TestHost(string host, WebApplication app)
+    {
+        Host = host;
+        App = app;
+    }
+
+    public string Host { get; }
+    public WebApplication App { get; }
+
+    public void Dispose()
+    {
+        App.DisposeAsync();
+    }
+}
