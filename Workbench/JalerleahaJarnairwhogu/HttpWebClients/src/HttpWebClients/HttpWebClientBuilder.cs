b11529a1e8ae01e5a55e211832039a3facd2227d
lindexi <lindexi_gd@163.com>
2026-01-22 09:58:33 +0800

    添加方便调用的辅助方法

--- Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/HttpWebClientBuilder.cs
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/HttpWebClientBuilder.cs
@@ -1,6 +1,9 @@
 ﻿using HttpWebClients.HostBackup;
+using HttpWebClients.HttpProviders;
 
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text.Json.Serialization;
 
 namespace HttpWebClients;
@@ -18,4 +21,40 @@ public class HttpWebClientBuilder
     private HostBackupManager? _hostBackupManager;
 
     private List<JsonSerializerContext>? _jsonSerializerContextList;
+
+
+    /// <summary>
+    /// 设置提供 <see cref="HttpClient"/> 对象
+    /// </summary>
+    /// <param name="httpClientProvider"></param>
+    /// <returns></returns>
+    public HttpWebClientBuilder UseHttpClientProvider(IHttpClientProvider httpClientProvider)
+    {
+        HttpClientProvider = httpClientProvider;
+        return this;
+    }
+
+    /// <summary>
+    /// 使用指定的 <see cref="HttpClient"/> 对象，传入的 <see cref="HttpClient"/> 对象 必须由传入方负责释放
+    /// </summary>
+    /// <param name="httpClient"></param>
+    /// <returns></returns>
+    public HttpWebClientBuilder UseHttpClient(HttpClient httpClient)
+    {
+        HttpClientProvider = new IncomingHttpClientProvider(httpClient);
+        return this;
+    }
+
+    /// <summary>
+    /// 使用特殊的方式创建 <see cref="HttpClient"/> 对象，创建的 <see cref="HttpClient"/> 对象将会自动释放
+    /// </summary>
+    /// <param name="httpClientCreator"></param>
+    /// <returns></returns>
+    public HttpWebClientBuilder UseHttpClient(Func<HttpClient> httpClientCreator)
+    {
+        HttpClientProvider = new ConfigurableHttpClientProvider(httpClientCreator);
+        return this;
+    }
+
+    private IHttpClientProvider? HttpClientProvider { set; get; }
 }
