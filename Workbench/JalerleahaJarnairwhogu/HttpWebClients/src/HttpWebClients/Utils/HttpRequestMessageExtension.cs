48efda591e85e90e91156628953e7741abb610f9
lindexi <lindexi_gd@163.com>
2026-01-22 10:34:51 +0800

    提供 Cookie 替换的功能

--- Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/Utils/HttpRequestMessageExtension.cs
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/Utils/HttpRequestMessageExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,4 +51,91 @@ internal static class HttpRequestMessageExtension
         }
         return clone;
     }
+
+    /// <summary>
+    /// 给 <paramref name="httpRequestMessage"/> 追加 Cookie 内容
+    /// </summary>
+    /// <param name="httpRequestMessage"></param>
+    /// <param name="cookies"></param>
+    public static void AppendCookies(this HttpRequestMessage httpRequestMessage, params Cookie[] cookies)
+    {
+        var cookieCollection = httpRequestMessage.GetCookies();
+        cookieCollection ??= new CookieCollection();
+        foreach (var cookie in cookies)
+        {
+            cookieCollection.Add(cookie);
+        }
+
+        httpRequestMessage.ReplaceCookie(cookieCollection);
+    }
+
+    /// <summary>
+    /// 获取当前的 <paramref name="httpRequestMessage"/> 的 Cookie 内容
+    /// </summary>
+    /// <param name="httpRequestMessage"></param>
+    /// <returns>如果没有赋值，则返回空</returns>
+    public static CookieCollection? GetCookies(this HttpRequestMessage httpRequestMessage)
+    {
+        if (httpRequestMessage.Options.TryGetValue(CookieHttpRequestOptionsKey, out var value))
+        {
+            return value;
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// 设置请求的 Cookie 内容，设置的内容将会替换掉原来的。清空请传入 null 的值
+    /// </summary>
+    /// <param name="httpRequestMessage"></param>
+    /// <param name="cookieCollection"></param>
+    public static void ReplaceCookie(this HttpRequestMessage httpRequestMessage,
+        CookieCollection? cookieCollection)
+    {
+        httpRequestMessage.Headers.Remove(HttpKnownHeaderNames.Cookie);
+        if (cookieCollection is null)
+        {
+            return;
+        }
+
+        if (cookieCollection.Count > 0)
+        {
+            var cookieContainer = new CookieContainer();
+
+            var uri = httpRequestMessage.RequestUri ?? new Uri("http://foo")/*使用 http://foo 只是为了不炸掉而已*/;
+            cookieContainer.Add(uri, cookieCollection);
+
+            // 必须通过 GetCookieHeader 进行设置，如此设置的 Cookie 之间采用 `;` 分割。通过 Fiddler 抓包能收到正确的 Cookie 内容
+            var cookieHeader = cookieContainer.GetCookieHeader(uri);
+            if (!string.IsNullOrEmpty(cookieHeader))
+            {
+                httpRequestMessage.Headers.Add(HttpKnownHeaderNames.Cookie, cookieHeader);
+            }
+            else
+            {
+                // 可能确实拿到空字符串，空字符串加入到 `httpRequestMessage.Headers.Add` 将会抛出异常
+            }
+        }
+        else
+        {
+            // 没 Cookie 内容就不要加头了，加了也是浪费
+        }
+
+        // 存放入缓存，如此将可以支持追加
+        httpRequestMessage.Options.Set(CookieHttpRequestOptionsKey, cookieCollection);
+
+        // 不能通过如下方式一个个设置，采用以下方式设置的 Cookie 之间采用 `,` 分割，这在 EN 后台是不认的。但是 ASP.NET Core 是认识的
+        //foreach (Cookie? cookie in cookieCollection)
+        //{
+        //    if (cookie is not null)
+        //    {
+        //        httpRequestMessage.Headers.Add(HttpKnownHeaderNames.Cookie, cookie.ToString());
+        //    }
+        //}
+    }
+
+    private static HttpRequestOptionsKey<CookieCollection> CookieHttpRequestOptionsKey =>
+        new HttpRequestOptionsKey<CookieCollection>(CookieKey);
+
+    private const string CookieKey = "Custom CookieCollection Key";
 }
