4418e5df3b033b3c86ffe8132631fe19f911895e
lindexi <lindexi_gd@163.com>
2026-01-22 09:55:30 +0800

    定义网络组件

---
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/HttpProviders/HttpClientProviders/ConfigurableHttpClientProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+
+namespace HttpWebClients.HttpProviders;
+
+/// <summary>
+/// 可配置创建的 HttpClient 提供器
+/// </summary>
+public class ConfigurableHttpClientProvider : IHttpClientProvider
+{
+    /// <summary>
+    /// 创建可配置创建的 HttpClient 提供器
+    /// </summary>
+    /// <param name="creator">创建出来的 HttpClient 对象将会在此类型进行释放</param>
+    public ConfigurableHttpClientProvider(Func<HttpClient> creator)
+    {
+        _httpClient = creator();
+    }
+
+    /// <inheritdoc />
+    public HttpClient GetHttpClient() => _httpClient;
+
+    private readonly HttpClient _httpClient;
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
---
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/HttpProviders/HttpClientProviders/IncomingHttpClientProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+
+namespace HttpWebClients.HttpProviders;
+
+/// <summary>
+/// 外部传入的 HttpClient 对象，由外部决定 HttpClient 的释放
+/// </summary>
+class IncomingHttpClientProvider : IHttpClientProvider
+{
+    public IncomingHttpClientProvider(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public HttpClient GetHttpClient() => _httpClient;
+
+    private readonly HttpClient _httpClient;
+
+    public void Dispose()
+    {
+        // 啥都不做
+    }
+}
