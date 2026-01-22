b629f4ac99d34e19f4c16ffa953d1937971a14f2
lindexi <lindexi_gd@163.com>
2026-01-22 10:22:03 +0800

    加上响应代码

--- Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/Contexts/HttpRequestData.cs
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/Contexts/HttpRequestData.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HttpWebClients.Contexts;
@@ -10,3 +12,12 @@ public class HttpRequestData
 {
 }
 
+public interface IHttpRequestData
+{
+    HttpMethod HttpMethod { get; }
+    string Url { get; }
+
+    JsonSerializerContext? JsonSerializerContext { get; }
+
+    Type? HttpResponseDataType { get; }
+}
---
+++ Workbench/JalerleahaJarnairwhogu/HttpWebClients/src/HttpWebClients/Contexts/HttpResponseData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.ExceptionServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HttpWebClients.Contexts;
+
+public class HttpResponseData : IDisposable
+{
+    internal HttpResponseData(IHttpRequestData requestData, HttpResponseMessage? httpResponseMessage, Exception? exception)
+    {
+        RequestData = requestData;
+        HttpResponseMessage = httpResponseMessage;
+        Exception = exception;
+        Debug.Assert(httpResponseMessage != null || exception != null);
+    }
+
+    public IHttpRequestData RequestData { get; }
+    public HttpResponseMessage? HttpResponseMessage { get; }
+    public Exception? Exception { get; }
+
+    public void EnsureHttpSuccess()
+    {
+        if (HttpResponseMessage is not null)
+        {
+            HttpResponseMessage.EnsureSuccessStatusCode();
+        }
+        else
+        {
+            Debug.Assert(Exception != null);
+            ExceptionDispatchInfo.Throw(Exception);
+        }
+    }
+
+    public string? ContentText => _contextText ??= ReadContentText();
+    private string? _contextText;
+
+    private string? ReadContentText()
+    {
+        if (HttpResponseMessage is null)
+        {
+            return null;
+        }
+
+        if (HttpResponseMessage.Content.Headers.ContentLength == 0)
+        {
+            return null;
+        }
+
+        using var stream = HttpResponseMessage.Content.ReadAsStream();
+        using var streamReader = new StreamReader(stream);
+        return streamReader.ReadToEnd();
+    }
+
+    public T As<T>(JsonSerializerContext? context = null)
+    {
+        context ??= RequestData.JsonSerializerContext;
+
+        var contentText = ContentText;
+
+        if (contentText is null)
+        {
+            contentText = "{}";
+        }
+
+        return JsonSerializer.Deserialize<T>(contentText, context?.Options)!;
+    }
+
+    public void Dispose()
+    {
+        HttpResponseMessage?.Dispose();
+    }
+}
