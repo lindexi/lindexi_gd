4174cf49c4dd46acfa7d06080675d9499b4bdb85
lindexi <lindexi_gd@163.com>
2026-01-30 09:21:40 +0800

    Test winforms Clipboard

---
+++ Workbench/KunalljayfewereNefefoke/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace KunalljayfewereNefefoke;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        Load += Form1_Load;
+    }
+
+    private void Form1_Load(object? sender, EventArgs e)
+    {
+        _ = CopyFromClipboard();
+    }
+
+    private async Task<bool> CopyFromClipboard()
+    {
+        while (Clipboard.ContainsText())
+        {
+            try
+            {
+                string text = Clipboard.GetText();
+                
+            }
+            catch (ExternalException)
+            {
+                await Task.Delay(5).ConfigureAwait(true);
+            }
+        }
+
+        return false;
+    }
+}
