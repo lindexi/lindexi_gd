8dd402bf07229f4b189853f46416e3b5d7f6596d
lindexi <lindexi_gd@163.com>
2026-02-02 20:44:59 +0800

    测试重启删除文件

--- Workbench/DedaykarkiLiwhileeno/Program.cs
+++ Workbench/DedaykarkiLiwhileeno/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Windows.Win32;
+using Windows.Win32.Storage.FileSystem;
+
 if(args.Length == 0)
 {
     Console.WriteLine($"请输入要删除的文件的路径");
@@ -12,5 +15,7 @@ if (!File.Exists(file))
 {
     Console.WriteLine($"找不到 '{file}' 文件");
 }
+
+PInvoke.MoveFileEx(file, null, MOVE_FILE_FLAGS.MOVEFILE_DELAY_UNTIL_REBOOT);
 
 Console.WriteLine($"重启后删除 '{file}' 文件");
