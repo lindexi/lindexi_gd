32357bdba21b7b581a0442ec4f5355cf76d3acb1
lindexi <lindexi_gd@163.com>
2026-02-02 20:11:19 +0800

    测试删除文件

---
+++ Workbench/DedaykarkiLiwhileeno/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if(args.Length == 0)
+{
+    Console.WriteLine($"请输入要删除的文件的路径");
+}
+
+var file = args[0];
+file = Path.GetFullPath(file);
+
+if (!File.Exists(file))
+{
+    Console.WriteLine($"找不到 '{file}' 文件");
+}
+
+Console.WriteLine($"重启后删除 '{file}' 文件");
