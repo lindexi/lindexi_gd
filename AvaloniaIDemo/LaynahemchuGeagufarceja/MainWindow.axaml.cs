b5e6f51e1232d509e8811dea66de4a878ab41482
lindexi <lindexi_gd@163.com>
2026-02-05 11:17:24 +0800

    Test LowLatencyDxgiSwapChain not support TransparencyLevelHint

---
+++ AvaloniaIDemo/LaynahemchuGeagufarceja/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace LaynahemchuGeagufarceja;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CloseButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
