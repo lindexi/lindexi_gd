79d3135ac8f69a2552757def58d543e4e3e689d8
lindexi <lindexi_gd@163.com>
2026-02-05 14:28:18 +0800

    尝试动态设置不可窗口透明但失败

---
+++ AvaloniaIDemo/JowekukurNelaholefewhi/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace JowekukurNelaholefewhi;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ChangeTransparencyLevelHintButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        TransparencyLevelHint = [WindowTransparencyLevel.None];
+    }
+}
