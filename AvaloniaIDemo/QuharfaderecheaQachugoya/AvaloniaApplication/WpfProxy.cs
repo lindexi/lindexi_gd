2a99a58c36d51e6cf2bcfa3d29eb413d7306e475
lindexi <lindexi_gd@163.com>
2026-02-10 09:23:13 +0800

    减少引用的项目

--- AvaloniaIDemo/QuharfaderecheaQachugoya/AvaloniaApplication/WpfProxy.cs
+++ AvaloniaIDemo/QuharfaderecheaQachugoya/AvaloniaApplication/WpfProxy.cs
@@ -1,58 +1,58 @@
-﻿using System.Threading;
-using System.Windows;
-using System.Windows.Threading;
+﻿//using System.Threading;
+//using System.Windows;
+//using System.Windows.Threading;
 
-namespace AvaloniaApplication;
+//namespace AvaloniaApplication;
 
-class WpfProxy
-{
-    public WpfProxy()
-    {
-        var manualResetEventSlim = new ManualResetEventSlim(false);
+//class WpfProxy
+//{
+//    public WpfProxy()
+//    {
+//        var manualResetEventSlim = new ManualResetEventSlim(false);
 
-        Application application = null!;
+//        Application application = null!;
 
-        var thread = new Thread(() =>
-        {
-            application = new Application();
-            // ReSharper disable once AccessToDisposedClosure
-            manualResetEventSlim.Set();
-            application.Run();
-        })
-        {
-            IsBackground = true,
-            Name = "WPF"
-        };
+//        var thread = new Thread(() =>
+//        {
+//            application = new Application();
+//            // ReSharper disable once AccessToDisposedClosure
+//            manualResetEventSlim.Set();
+//            application.Run();
+//        })
+//        {
+//            IsBackground = true,
+//            Name = "WPF"
+//        };
 
-        thread.SetApartmentState(ApartmentState.STA);
+//        thread.SetApartmentState(ApartmentState.STA);
 
-        thread.Start();
+//        thread.Start();
 
-        manualResetEventSlim.Wait();
-        manualResetEventSlim.Dispose();
+//        manualResetEventSlim.Wait();
+//        manualResetEventSlim.Dispose();
 
-        WpfApplication = application;
-    }
+//        WpfApplication = application;
+//    }
 
-    public Application WpfApplication { get; }
+//    public Application WpfApplication { get; }
 
-    public void ShowWpfWindow(nint avaloniaWindowHandle)
-    {
-        WpfApplication.Dispatcher.InvokeAsync(() =>
-        {
-            var mainWindow = new WpfApplication.MainWindow();
-            mainWindow.Show();
-            mainWindow.SetTransparentHitThrough();
-            mainWindow.SetOwner(avaloniaWindowHandle);
-        },DispatcherPriority.Send);
-    }
+//    public void ShowWpfWindow(nint avaloniaWindowHandle)
+//    {
+//        WpfApplication.Dispatcher.InvokeAsync(() =>
+//        {
+//            var mainWindow = new WpfApplication.MainWindow();
+//            mainWindow.Show();
+//            mainWindow.SetTransparentHitThrough();
+//            mainWindow.SetOwner(avaloniaWindowHandle);
+//        },DispatcherPriority.Send);
+//    }
 
-    public void MoveBorder(double x)
-    {
-        WpfApplication.Dispatcher.InvokeAsync(() =>
-        {
-            var mainWindow = (WpfApplication.MainWindow) WpfApplication.MainWindow!;
-            mainWindow.MoveBorder(x);
-        }, DispatcherPriority.Send);
-    }
-}
+//    public void MoveBorder(double x)
+//    {
+//        WpfApplication.Dispatcher.InvokeAsync(() =>
+//        {
+//            var mainWindow = (WpfApplication.MainWindow) WpfApplication.MainWindow!;
+//            mainWindow.MoveBorder(x);
+//        }, DispatcherPriority.Send);
+//    }
+//}
