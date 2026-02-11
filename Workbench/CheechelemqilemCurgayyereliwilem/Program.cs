28e193ab2e63a5fb5bda14c03cd8487d36d90718
lindexi <lindexi_gd@163.com>
2026-02-11 17:51:55 +0800

    测试远程执行的能力

---
+++ Workbench/CheechelemqilemCurgayyereliwilem/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.DotNet.RemoteExecutor;
+
+RemoteExecutor.Invoke(() =>
+{
+    File.WriteAllText(@"C:\lindexi\1.txt", "123");
+});
+
+Console.WriteLine("Hello, World!");
