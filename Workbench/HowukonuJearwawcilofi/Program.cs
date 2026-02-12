a1242fa605b6be803f2aeae194f2212252b0ea76
lindexi <lindexi_gd@163.com>
2026-02-12 20:04:08 +0800

    加上错误收集却只获取到 500 错误

--- Workbench/HowukonuJearwawcilofi/Program.cs
+++ Workbench/HowukonuJearwawcilofi/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.HttpResults;
 
-var builder = WebApplication.CreateSlimBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async httpContext =>
+    {
+        var pds = httpContext.RequestServices.GetService<IProblemDetailsService>();
+        if (pds == null
+            || !await pds.TryWriteAsync(new() { HttpContext = httpContext }))
+        {
+            // Fallback behavior
+            await httpContext.Response.WriteAsync("Fallback: An error occurred.");
+        }
+    });
+});
 
 app.MapPost("/", (FooRequest request) =>
 {
