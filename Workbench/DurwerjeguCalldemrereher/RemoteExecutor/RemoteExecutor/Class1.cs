f015acd89012ef6d6dbeaf63513733d99e80dcda
lindexi <lindexi_gd@163.com>
2026-02-13 16:45:30 +0800

    完成简单的测试

--- Workbench/DurwerjeguCalldemrereher/RemoteExecutor/RemoteExecutor/Class1.cs
+++ Workbench/DurwerjeguCalldemrereher/RemoteExecutor/RemoteExecutor/Class1.cs
@@ -1,6 +1,73 @@
-﻿namespace RemoteExecutor;
+﻿using System.Reflection;
+using DotNetCampus.Cli;
 
-public class Class1
+namespace RemoteExecutors;
+
+public static class RemoteExecutor
 {
+    public static void Invoke(Action action)
+    {
+        var method = action.Method;
 
+        Type? type = method.DeclaringType;
+
+        if (type is null)
+        {
+            throw new ArgumentException();
+        }
+
+        TypeInfo typeInfo = IntrospectionExtensions.GetTypeInfo(type);
+
+        var methodName = method.Name;
+        var className = typeInfo.FullName!;
+        var assemblyFullName = typeInfo.Assembly.FullName!;
+
+        string[] commandLineArgs = 
+            [
+                RemoteExecutorOption.CommandName,
+                "--AssemblyName", assemblyFullName,
+                "--ClassName", className,
+                "--MethodName", methodName,
+                "--Pid", Environment.ProcessId.ToString(),
+            ];
+
+        TryHandle(commandLineArgs);
+    }
+
+    public static bool TryHandle(string[] commandLineArgs)
+    {
+        var index = commandLineArgs.IndexOf(RemoteExecutorOption.CommandName);
+        if (index == -1)
+        {
+            return false;
+        }
+
+        // 解析命令行参数
+        // 只取后面的一部分参数
+        var optionCommandLineArgs = commandLineArgs.Skip(index+1).ToList();
+        var result = CommandLine.Parse(optionCommandLineArgs)
+            .AddHandler<RemoteExecutorOption>(option =>
+            {
+                var assemblyName = option.AssemblyName;
+                var className = option.ClassName;
+                var methodName = option.MethodName;
+
+                var assembly = Assembly.Load(assemblyName);
+                var classType = assembly.GetType(className)!;
+
+                var methodInfo = classType.GetTypeInfo().GetDeclaredMethod(methodName)!;
+                object? instance = null;
+                if (!methodInfo.IsStatic)
+                {
+                    instance = Activator.CreateInstance(classType);
+                }
+                object? result = methodInfo.Invoke(instance, null);
+                _ = result;
+            })
+            .Run();
+
+        _ = result;
+
+        return true;
+    }
 }
