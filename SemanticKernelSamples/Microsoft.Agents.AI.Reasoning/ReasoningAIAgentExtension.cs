6e54ef4f5f9594636333f1921526b9d8a1a144c4
lindexi <lindexi_gd@163.com>
2026-02-24 14:36:32 +0800

    测试存储内容

---
+++ SemanticKernelSamples/Microsoft.Agents.AI.Reasoning/ReasoningAIAgentExtension.cs
@@ -0,0 +1,75 @@
+﻿using System.ClientModel.Primitives;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using OpenAI.Chat;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
+
+namespace Microsoft.Agents.AI.Reasoning;
+
+public static class ReasoningAIAgentExtension
+{
+    public static IAsyncEnumerable<ReasoningAgentResponseUpdate> RunReasoningStreamingAsync(this AIAgent agent, ChatMessage message,
+        AgentSession? session = null,
+        AgentRunOptions? options = null,
+        CancellationToken cancellationToken = default)
+    {
+        return RunReasoningStreamingAsync(agent, [message], session, options, cancellationToken);
+    }
+
+    public static async IAsyncEnumerable<ReasoningAgentResponseUpdate> RunReasoningStreamingAsync(this AIAgent agent, IEnumerable<ChatMessage> messages,
+        AgentSession? session = null,
+        AgentRunOptions? options = null,
+        [EnumeratorCancellation]
+        CancellationToken cancellationToken = default)
+    {
+        bool? isThinking = null;
+        bool isFirstOutputContent = true;
+
+        await foreach (AgentResponseUpdate agentRunResponseUpdate in agent.RunStreamingAsync(messages, session, options, cancellationToken))
+        {
+            var contentIsEmpty = string.IsNullOrEmpty(agentRunResponseUpdate.Text);
+
+            if (contentIsEmpty && agentRunResponseUpdate.RawRepresentation is Microsoft.Extensions.AI.ChatResponseUpdate streamingChatCompletionUpdate)
+            {
+                if (streamingChatCompletionUpdate.RawRepresentation is StreamingChatCompletionUpdate chatCompletionUpdate)
+                {
+                    // System.Text.Encoding.UTF8.GetString(chatCompletionUpdate._patch._rawJson.Value.Span)
+#pragma warning disable SCME0001 // Patch 属性是实验性内容
+                    ref JsonPatch patch = ref chatCompletionUpdate.Patch;
+                    
+                    if (patch.TryGetJson("$.choices[0].delta"u8, out var data))
+                    {
+                        var jsonElement = JsonElement.Parse(data.Span);
+                        if (jsonElement.TryGetProperty("reasoning_content", out var reasoningContent))
+                        {
+                            // 拿到的 reasoningContent 就是思考内容
+                        }
+                    }
+
+#pragma warning restore SCME0001
+                }
+            }
+
+            if (!contentIsEmpty)
+            {
+                var responseUpdate = new ReasoningAgentResponseUpdate(agentRunResponseUpdate);
+
+                if (isFirstOutputContent)
+                {
+                    responseUpdate.IsFirstOutputContent = true;
+                }
+
+                if (isThinking is true && isFirstOutputContent)
+                {
+                    responseUpdate.IsThinkingEnd = true;
+                }
+
+                isFirstOutputContent = false;
+                isThinking = false;
+
+                yield return responseUpdate;
+            }
+        }
+    }
+}
---
+++ SemanticKernelSamples/YawbacawwallWhebaikaicorair/Program.cs
@@ -0,0 +1,69 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.ClientModel;
+using System.Diagnostics;
+using Microsoft.Agents.AI;
+using Microsoft.Agents.AI.Reasoning;
+using Microsoft.Extensions.AI;
+
+using OpenAI;
+using OpenAI.Chat;
+
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
+
+var keyFile = @"C:\lindexi\Work\Doubao.txt";
+var key = File.ReadAllText(keyFile);
+
+var openAiClient = new OpenAIClient(new ApiKeyCredential(key), new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://ark.cn-beijing.volces.com/api/v3")
+});
+
+var chatClient = openAiClient.GetChatClient("ep-20260115192014-kgkxq");
+
+var inMemoryChatHistoryProvider = new InMemoryChatHistoryProvider();
+ChatClientAgent aiAgent = chatClient.AsAIAgent(new ChatClientAgentOptions()
+{
+    ChatHistoryProvider = inMemoryChatHistoryProvider
+});
+
+AgentSession session = await aiAgent.CreateSessionAsync();
+
+var chatHistoryProvider = aiAgent.GetService<ChatHistoryProvider>();
+Debug.Assert(ReferenceEquals(inMemoryChatHistoryProvider, chatHistoryProvider));
+
+var chatMessages = inMemoryChatHistoryProvider.GetMessages(session);
+chatMessages.Add(new ChatMessage(ChatRole.System, "你是一个讲笑话机器人。你将给学龄前的小孩子讲笑话"));
+// 无需调用设置方法，因为直接返回的就是内容本身了
+//inMemoryChatHistoryProvider.SetMessages(session, chatMessages);
+
+ChatMessage message = new ChatMessage(ChatRole.User, "请讲一个笑话");
+
+await foreach (var agentRunResponseUpdate in aiAgent.RunReasoningStreamingAsync(message))
+{
+    if (agentRunResponseUpdate.IsFirstThinking)
+    {
+        Console.WriteLine("思考：");
+    }
+
+    if (agentRunResponseUpdate.Reasoning is not null)
+    {
+        Console.Write(agentRunResponseUpdate.Reasoning);
+    }
+
+    if (agentRunResponseUpdate.IsThinkingEnd)
+    {
+        Console.WriteLine();
+        Console.WriteLine("--------");
+    }
+
+    var text = agentRunResponseUpdate.Text;
+    if (!string.IsNullOrEmpty(text))
+    {
+        Console.Write(text);
+    }
+}
+
+Console.WriteLine();
+
+Console.Read();
