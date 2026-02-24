fc4cf4f485ea3e2268a67c0c6900827d9803d9b3
lindexi <lindexi_gd@163.com>
2026-02-24 11:39:50 +0800

    获取聊天理由

---
+++ SemanticKernelSamples/LadelallkeacheWhikurwearqobakaju/Microsoft.Agents.AI.Reasoning/ReasoningAIAgentExtension.cs
@@ -0,0 +1,94 @@
+﻿using System.ClientModel.Primitives;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using OpenAI.Chat;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
+
+namespace Microsoft.Agents.AI.Reasoning;
+
+public static class ReasoningAIAgentExtension
+{
+    public static IAsyncEnumerable<ReasoningAgentRunResponseUpdate> RunReasoningStreamingAsync(this AIAgent agent, ChatMessage message,
+        AgentSession? session = null,
+        AgentRunOptions? options = null,
+        CancellationToken cancellationToken = default)
+    {
+        return RunReasoningStreamingAsync(agent, [message], session, options, cancellationToken);
+    }
+
+    public static async IAsyncEnumerable<ReasoningAgentRunResponseUpdate> RunReasoningStreamingAsync(this AIAgent agent, IEnumerable<ChatMessage> messages,
+        AgentSession? session = null,
+        AgentRunOptions? options = null,
+        [EnumeratorCancellation]
+        CancellationToken cancellationToken = default)
+    {
+        bool? isThinking = null;
+        bool isFirstOutputContent = true;
+
+        await foreach (AgentResponseUpdate agentRunResponseUpdate in agent.RunStreamingAsync(messages, session, options, cancellationToken))
+        {
+            var contentIsEmpty = string.IsNullOrEmpty(agentRunResponseUpdate.Text);
+
+            if (contentIsEmpty && agentRunResponseUpdate.RawRepresentation is Microsoft.Extensions.AI.ChatResponseUpdate streamingChatCompletionUpdate)
+            {
+                if (streamingChatCompletionUpdate.RawRepresentation is StreamingChatCompletionUpdate chatCompletionUpdate)
+                {
+#pragma warning disable SCME0001
+                    ref JsonPatch patch = ref chatCompletionUpdate.Patch;
+                    if (patch.TryGetJson("$.choices[0].delta"u8, out var data))
+                    {
+                        var jsonElement = JsonElement.Parse(data.Span);
+                        if (jsonElement.TryGetProperty("reasoning_content", out var reasoningContent))
+                        {
+                            bool isFirstThinking = false;
+                            if (isThinking is null)
+                            {
+                                isThinking = true;
+                                isFirstThinking = true;
+                            }
+
+                            if (isThinking is true)
+                            {
+                                yield return new ReasoningAgentRunResponseUpdate(agentRunResponseUpdate)
+                                {
+                                    Reasoning = reasoningContent.ToString(),
+                                    IsFirstThinking = isFirstThinking,
+                                    IsFirstOutputContent = false,
+                                    IsThinkingEnd = false,
+                                };
+                                continue;
+                            }
+                            else
+                            {
+                                Debug.Fail("不能在输出内容之后，再次进入思考");
+                            }
+                        }
+                    }
+
+#pragma warning restore SCME0001
+                }
+            }
+
+            if (!contentIsEmpty)
+            {
+                var responseUpdate = new ReasoningAgentRunResponseUpdate(agentRunResponseUpdate);
+
+                if (isFirstOutputContent)
+                {
+                    responseUpdate.IsFirstOutputContent = true;
+                }
+
+                if (isThinking is true && isFirstOutputContent)
+                {
+                    responseUpdate.IsThinkingEnd = true;
+                }
+
+                isFirstOutputContent = false;
+                isThinking = false;
+
+                yield return responseUpdate;
+            }
+        }
+    }
+}
