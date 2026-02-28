1014f9c25d2c880f5d3f3d7cbec9bd3ac98a3acb
lindexi <lindexi_gd@163.com>
2026-02-28 10:53:22 +0800

    提供设置样式的功能

--- Blog/RallreakechuFeakenalldea/SimpleWrite/Business/TextEditors/SimpleWriteTextEditor.cs
+++ Blog/RallreakechuFeakenalldea/SimpleWrite/Business/TextEditors/SimpleWriteTextEditor.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia.Media;
 
 using LightTextEditorPlus;
+using LightTextEditorPlus.Editing;
+
+using Markdig;
 
 using SimpleWrite.Business.ShortcutManagers;
 
@@ -9,7 +12,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LightTextEditorPlus.Editing;
+using LightTextEditorPlus.Core.Carets;
+using LightTextEditorPlus.Document;
+using LightTextEditorPlus.Primitive;
+using Markdig.Renderers;
+using Markdig.Syntax;
+using SimpleWrite.Utils;
+using SkiaSharp;
 
 namespace SimpleWrite.Business.TextEditors;
 
@@ -21,9 +30,126 @@ internal sealed class SimpleWriteTextEditor : TextEditor
     public SimpleWriteTextEditor()
     {
         CaretConfiguration.SelectionBrush = new Color(0x9F, 0x26, 0x3F, 0xC7);
+       
+        TextEditorCore.DocumentChanged += TextEditorCore_DocumentChanged;
+
+        var normalFontSize = 25;
+
+        SetStyleTextRunProperty(runProperty => runProperty with
+        {
+            FontSize = normalFontSize,
+            Foreground = new SolidColorSkiaTextBrush(SKColors.Azure)
+        });
+
+        var normalTextRunProperty = this.CreateRunProperty(property => property with
+        {
+            FontSize = normalFontSize
+        });
+        NormalTextRunProperty = normalTextRunProperty;
+
+        var titleLevel1RunProperty = this.CreateRunProperty(property => property with
+        {
+            FontSize = normalFontSize + 10,
+            FontWeight = SKFontStyleWeight.Bold,
+        });
+
+        var titleLevel2RunProperty = this.CreateRunProperty(property => property with
+        {
+            FontSize = normalFontSize + 7,
+            FontWeight = SKFontStyleWeight.Bold,
+        });
+
+        var titleLevel3RunProperty = this.CreateRunProperty(property => property with
+        {
+            FontSize = normalFontSize + 5,
+            FontWeight = SKFontStyleWeight.Bold,
+        });
+
+        var titleLevel4RunProperty = this.CreateRunProperty(property => property with
+        {
+            FontSize = normalFontSize + 3,
+            FontWeight = SKFontStyleWeight.Bold,
+        });
+
+        var titleLevel5RunProperty = this.CreateRunProperty(property => property with
+        {
+            FontSize = normalFontSize + 1,
+            FontWeight = SKFontStyleWeight.Bold,
+        });
+
+        TitleLevelRunPropertyList = [titleLevel1RunProperty, titleLevel2RunProperty, titleLevel3RunProperty, titleLevel4RunProperty, titleLevel5RunProperty];
     }
 
+    private void TextEditorCore_DocumentChanged(object? sender, EventArgs e)
+    {
+        var pipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .Build();
+
+        var markdownDocument = Markdown.Parse(this.Text,pipeline);
+        foreach (Block block in markdownDocument)
+        {
+            if (block is ParagraphBlock paragraphBlock)
+            {
+                if (paragraphBlock.Inline is {} inline)
+                {
+                    
+                }
+
+                TrySetRunProperty(NormalTextRunProperty, paragraphBlock.Span);
+            }
+
+            if (block is HeadingBlock headingBlock)
+            {
+                var levelIndex = headingBlock.Level - 1;
+                SkiaTextRunProperty runProperty;
+                if (TitleLevelRunPropertyList.Count-1>levelIndex)
+                {
+                    runProperty = TitleLevelRunPropertyList[levelIndex];
+                }
+                else
+                {
+                    runProperty = TitleLevelRunPropertyList[^1];
+                }
+
+                TrySetRunProperty(runProperty, headingBlock.Span);
+            }
+
+        }
+
+        void TrySetRunProperty(SkiaTextRunProperty runProperty, SourceSpan span)
+        {
+            var selection = SourceSpanToSelection(span);
+
+            // 由于 GetRunPropertyRange 没有去重，导致不能用 OneOrDefault 直接处理
+            //var currentRunProperty = this.GetRunPropertyRange(selection)
+
+            //    // 为什么取 OneOrDefault 呢？ 这是因为如果能够拿到多个字符属性，则必定需要重新设置
+            //    .OneOrDefault();
+
+            //if (currentRunProperty != runProperty)
+            //{
+            //    SetRunProperty(runProperty,selection);
+            //}
+
+            IEnumerable<SkiaTextRunProperty> runPropertyRange = GetRunPropertyRange(selection);
+            var same = runPropertyRange.All(t=>t==runProperty);
+            if (!same)
+            {
+                SetRunProperty(runProperty, selection);
+            }
+        }
+    }
+
+    private Selection SourceSpanToSelection(SourceSpan span) => new Selection(new CaretOffset(span.Start), span.Length);
+
     public required ShortcutExecutor ShortcutExecutor { get; init; }
+
+    /// <summary>
+    /// 正文文本属性
+    /// </summary>
+    public SkiaTextRunProperty NormalTextRunProperty { get;  }
+    public IReadOnlyList<SkiaTextRunProperty> TitleLevelRunPropertyList { get; }
 
     protected override TextEditorHandler CreateTextEditorHandler()
     {
@@ -32,4 +158,15 @@ internal sealed class SimpleWriteTextEditor : TextEditor
             ShortcutExecutor = ShortcutExecutor
         };
     }
+
+    //class F : IMarkdownRenderer
+    //{
+    //    public object Render(MarkdownObject markdownObject)
+    //    {
+    //    }
+
+    //    public ObjectRendererCollection ObjectRenderers { get; set; }
+    //    public event Action<IMarkdownRenderer, MarkdownObject>? ObjectWriteBefore;
+    //    public event Action<IMarkdownRenderer, MarkdownObject>? ObjectWriteAfter;
+    //}
 }
