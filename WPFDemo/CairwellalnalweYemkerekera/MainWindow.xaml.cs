c7d988490f7440e08ddd4c5718585131716a6941
lindexi <lindexi_gd@163.com>
2026-03-03 15:55:50 +0800

    Hyperlink underline too far in DataGridHyperlinkColumn

---
+++ WPFDemo/CairwellalnalweYemkerekera/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CairwellalnalweYemkerekera;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        TextBlock outerBlock = new TextBlock();
+        Hyperlink link = new Hyperlink();
+        InlineUIContainer inlineContainer = new InlineUIContainer();
+        ContentPresenter innerContentPresenter = new ContentPresenter();
+
+        outerBlock.Inlines.Add(link);
+        link.Inlines.Add(inlineContainer);
+        inlineContainer.Child = innerContentPresenter;
+
+        innerContentPresenter.Content = new TextBlock()
+        {
+            Text = "HyperLink outside"
+        };
+        MainStackPanel.Children.Add(outerBlock);
+    }
+}
