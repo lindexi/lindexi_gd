32babc8e728e045912a8fae327689ffeb11f7050
lindexi <lindexi_gd@163.com>
2026-03-04 16:30:44 +0800

    Add the button to DataGridHyperlinkColumn with the content bind model

---
+++ WPFDemo/BuwagarcerefiFerejowalldalelha/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BuwagarcerefiFerejowalldalelha;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+
+public class Model
+{
+    public object Button { get; set; } = new Button()
+    {
+        Width = 50,
+        Height = 20,
+    };
+}
