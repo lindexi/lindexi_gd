0523c19f585713967e6c8eb3481b5184efa47974
lindexi <lindexi_gd@163.com>
2026-03-06 11:32:18 +0800

    虽然在相同的执行上下文但是异常没有对外抛出

---
+++ Workbench/FawfijeefawkerehearwhuRalldawnola/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FawfijeefawkerehearwhuRalldawnola;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        _ = DoXxx();
+        Console.WriteLine("Hello");
+    }
+
+    private static async Task DoXxx()
+    {
+        Throw();
+
+        await Task.Delay(100);
+    }
+
+    private static void Throw()
+    {
+        throw new Exception();
+    }
+}
