10ba1c027072f3667ecb02416ef8805cdc69c41f
lindexi <lindexi_gd@163.com>
2026-03-13 14:55:05 +0800

    创建辅助机器的界面

---
+++ Blog/RallreakechuFeakenalldea/SimpleWrite/Views/Components/CopilotSlideBar.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SimpleWrite.Views.Components;
+
+public partial class CopilotSlideBar : UserControl
+{
+    public CopilotSlideBar()
+    {
+        InitializeComponent();
+    }
+}
