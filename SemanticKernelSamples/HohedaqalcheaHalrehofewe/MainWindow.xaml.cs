61f3c626d2a12d938f3f312c463149290ae05aa2
lindexi <lindexi_gd@163.com>
2026-03-19 14:11:54 +0800

    创建项目

---
+++ SemanticKernelSamples/HohedaqalcheaHalrehofewe/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using OpenAI;
+
+using System.ClientModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HohedaqalcheaHalrehofewe;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded+=OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var keyFile = @"C:\lindexi\Work\Doubao.txt";
+        var key = File.ReadAllText(keyFile);
+
+        var openAiClient = new OpenAIClient(new ApiKeyCredential(key), new OpenAIClientOptions()
+        {
+            Endpoint = new Uri("https://ark.cn-beijing.volces.com/api/v3")
+        });
+    }
+}
