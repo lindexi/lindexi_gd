6f64cd442d30fd469d2404fd37fb9ae4d2d59e97
lindexi <lindexi_gd@163.com>
2026-03-26 14:07:34 +0800

    尝试重新组织项目

---
+++ Workbench/KuhallyediNujewinem/AvaloniaApp/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Workbench/KuhallyediNujewinem/AvaloniaApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using System;
+using System.Threading.Tasks;
+using Avalonia.Threading;
+
+namespace AvaloniaApp;
+
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                Console.ReadLine();
+
+                Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    var mainWindow = new MainWindow();
+                    mainWindow.Show();
+                });
+            }
+        });
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
