5ec1962aae573a6e334ac6cbd15cec5690abb196
lindexi <lindexi_gd@163.com>
2026-03-26 19:50:01 +0800

    Revert "测试另一个方案"
    This reverts commit c019eabcab7415fc60571e8248355fe06a1de50e.

--- Workbench/KuhallyediNujewinem/AvaloniaApp/Program.cs
+++ Workbench/KuhallyediNujewinem/AvaloniaApp/Program.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using Avalonia.Headless;
 using Avalonia.Threading;
 
 namespace AvaloniaApp;
@@ -47,7 +46,7 @@ class Program
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
-            .UseHeadless(new AvaloniaHeadlessPlatformOptions())
+            .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
 }
