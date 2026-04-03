266f08d8df93a15051573c88676671d48136aeb0
lindexi <lindexi_gd@163.com>
2026-04-03 10:35:38 +0800

    记录已知问题

---
+++ AvaloniaIDemo/RemhemlaidejeheWhahaheenalira/Program.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+
+using System;
+
+namespace RemhemlaidejeheWhahaheenalira;
+
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        // System.PlatformNotSupportedException:“Operation is not supported on this platform.”
+        var app = new App();
+
+        return AppBuilder.Configure<App>(() => app)
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+    }
+}
