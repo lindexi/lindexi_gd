c1f005638f8584e6aee27a68e73dae2e5b402a83
lindexi <lindexi_gd@163.com>
2026-04-05 18:17:38 +0800

    测试文档里面的报告内容，但依然失败

--- SemanticKernelSamples/ChawhallerwhenaWawjokaqalla/Program.cs
+++ SemanticKernelSamples/ChawhallerwhenaWawjokaqalla/Program.cs
@@ -5,16 +5,66 @@ using Microsoft.Extensions.AI;
 using OpenAI;
 
 using System.ClientModel;
+using System.Net.Http.Headers;
 
 var keyFile = @"C:\lindexi\Work\Qwen.txt";
 var key = File.ReadAllText(keyFile);
+
+{
+    // 直接测试 POST 内容
+    using var httpClient = new HttpClient();
+    httpClient.BaseAddress = new Uri("https://dashscope.aliyuncs.com/compatible-mode/v1");
+    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
+
+    var postContent =
+        """
+        {
+            "model": "qwen3-omni-flash",
+            "messages": [
+            {
+              "role": "user",
+              "content": [
+                {
+                  "type": "input_audio",
+                  "input_audio": {
+                    "data": "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250211/tixcef/cherry.wav",
+                    "format": "wav"
+                  }
+                },
+                {
+                  "type": "text",
+                  "text": "这段音频在说什么"
+                }
+              ]
+            }
+          ],
+            "stream":true,
+            "stream_options":{
+                "include_usage":true
+            },
+            "modalities":["text","audio"],
+            "audio":{"voice":"Tina","format":"wav"}
+        }
+        """;
+
+    var httpResponseMessage = await httpClient.PostAsync("https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+        new StringContent(postContent, new MediaTypeHeaderValue("application/json"))
+        {
+        });
+    // {"error":{"message":"<400> InternalError.Algo.InvalidParameter: Voice 'Tina' is not supported.","type":"invalid_request_error","param":null,"code":"invalid_parameter_error"},"id":"chatcmpl-73a3b2bf-f305-9d9f-88b3-927851126cd0","request_id":"73a3b2bf-f305-9d9f-88b3-927851126cd0"}
+    var responseText = await httpResponseMessage.Content.ReadAsStringAsync();
+    Console.WriteLine(responseText);
+}
 
 var openAiClient = new OpenAIClient(new ApiKeyCredential(key), new OpenAIClientOptions()
 {
     Endpoint = new Uri("https://dashscope.aliyuncs.com/compatible-mode/v1"),
 });
 
+//openAiClient.GetAudioClient("qwen3-omni-flash").
+
 var chatClient = openAiClient.GetChatClient("qwen3-omni-flash");
+
 
 // qwen3.5-omni-plus: System.ClientModel.ClientResultException:“HTTP 403 (access_denied: access_denied)
 
@@ -41,9 +91,10 @@ ChatMessage[] chatMessageList =
         //new UriContent("https://pro-en-ali-pub.en5static.com/easinote5_public/uwizqwnxhhqjjhnohwvvxlixjjphihhh.mp4", "video/mpeg"),
         //new UriContent("https://pro-en-ali-pub.en5static.com/easinote5_public/uwixjonmhhqjjhnohwvvwyyhvzphihhh.mp3", "audio/mpeg"),
         new UriContent("https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250211/tixcef/cherry.wav","audio/*"),
-        
     ]),
 ];
+
+var uriContent = new UriContent("https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20250211/tixcef/cherry.wav", "audio/*");
 
 //var testInput = new ChatMessage(ChatRole.User, "请问 mp3 的 MIME 类型字符串是什么？");
 //_ = testInput;
@@ -73,4 +124,4 @@ Console.ReadLine();
 Console.WriteLine("Hello, World!");
 
 
-Console.WriteLine("Hello, World!");
+
