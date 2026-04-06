d976f3fded0a6fda2c747f059dc067647109d58c
lindexi <lindexi_gd@163.com>
2026-04-06 11:38:16 +0800

    再次添加火山代码

---
+++ SemanticKernelSamples/VolcEngineSdk/ArkContentGenerations/ArkGetTaskResult.cs
@@ -0,0 +1,12 @@
+﻿namespace VolcEngineSdk;
+
+public record ArkGetTaskResult()
+{
+    public required string TaskId { get; init; }
+
+    public required string Model { get; init; }
+
+    public required ArkTaskStatus Status { get; init; }
+
+    public required ArkGeneratedVideoContent? Content { get; init; }
+}
---
+++ SemanticKernelSamples/VolcEngineSdk/ArkContents/ArkTextContent.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace VolcEngineSdk;
+
+public class ArkTextContent(string text) : ArkContent
+{
+    public string Text => text;
+
+
+    public override string ToJson()
+    {
+        return
+            $$"""
+              {
+                   "type": "text",
+                   "text": "{{JsonEncodedText.Encode(Text).Value}}"
+              }
+              """;
+    }
+}
