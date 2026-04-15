624e0543fee2cb05dd450777b5d39f997bf83bea
lindexi <lindexi_gd@163.com>
2026-04-15 11:32:56 +0800

    测试定义和调用顺序

---
+++ Workbench/JurbairnikeneWananocalwu/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+SetText("123");
+
+string t;
+var l = t.Length;
+
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+void SetText(string input)
+{
+    t = input;
+}
