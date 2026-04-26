1f22da404aa2d6311154be3f63528d1ff660857c
lindexi <lindexi_gd@163.com>
2026-04-26 09:58:10 +0800

    测试 MCP 服务的连接

---
+++ SemanticKernelSamples/WikernajeceeKawyeljahewhanawu/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ModelContextProtocol.Client;
+
+var httpClientTransport = new HttpClientTransport(new HttpClientTransportOptions()
+{
+    Endpoint = new Uri("http://127.0.0.1:53903/mcp")
+});
+
+var mcpClient = await McpClient.CreateAsync(httpClientTransport);
+var toolList = await mcpClient.ListToolsAsync();
+foreach (var mcpClientTool in toolList)
+{
+    Console.WriteLine(mcpClientTool.Name);
+}
+
+Console.WriteLine("Hello, World!");
