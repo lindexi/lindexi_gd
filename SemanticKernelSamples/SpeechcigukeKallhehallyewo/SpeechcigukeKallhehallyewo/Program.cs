55dff2a78021c1b5d25a50103b4f3f5a2d4942c4
lindexi <lindexi_gd@outlook.com>
2026-04-30 09:35:04 +0800

    初始化项目

---
+++ SemanticKernelSamples/SpeechcigukeKallhehallyewo/SpeechcigukeKallhehallyewo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ClientModel;
+using OpenAI;
+
+var keyFile = @"C:\lindexi\Work\Doubao.txt";
+var key = File.ReadAllText(keyFile);
+
+var openAiClient = new OpenAIClient(new ApiKeyCredential(key), new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://ark.cn-beijing.volces.com/api/v3"),
+    NetworkTimeout = TimeSpan.FromHours(1),
+});
+
+var chatClient = openAiClient.GetChatClient("ep-20260306101224-c8mtg");
+
+// 什么的语气讲
+// 每句话的中间加 停顿几秒
+
+Console.WriteLine("Hello, World!");
+
