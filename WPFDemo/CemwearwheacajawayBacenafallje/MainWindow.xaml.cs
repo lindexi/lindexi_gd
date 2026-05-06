b4d4477a583fb4bfc3874f61aef0efef4470e93c
lindexi <lindexi_gd@outlook.com>
2026-05-06 20:27:03 +0800

    尝试输出应用程序运行信息

---
+++ WPFDemo/CemwearwheacajawayBacenafallje/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CemwearwheacajawayBacenafallje;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Loaded += MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        Exception? lastException = null;
+        var app = (App) Application.Current;
+        while (true)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            if (lastException != null && ReferenceEquals(lastException, app.FirstException))
+            {
+                continue;
+            }
+
+            lastException = app.FirstException;
+
+            if (lastException is null)
+            {
+                continue;
+            }
+
+            LogTextBox.Text = lastException.ToString();
+        }
+    }
+}
