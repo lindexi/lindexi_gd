4833ee4a1740f4e4d8b0e2c4b47652b477b83e67
lindexi <lindexi_gd@outlook.com>
2026-05-22 14:27:09 +0800

    支持加载配置逻辑

---
+++ SemanticKernelSamples/AgentLib/AgentLib/Core/AgentApiManagers/LanguageModelProviders/JsonConfigurations/AgentApiManagerConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace AgentLib.Core.AgentApiManagers.LanguageModelProviders;
+
+public record AgentApiManagerConfiguration
+{
+    public string? PrimaryModel { get; init; }
+
+    public IReadOnlyList<OpenAIProtocolLanguageModelConfiguration>? OpenAIConfigurationList { get; init; }
+}
---
+++ SemanticKernelSamples/AgentLib/AgentLib/Core/AgentApiManagers/LanguageModelProviders/JsonConfigurations/JsonConfigurationOpenAIProtocolLanguageModelJsonSerializerContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AgentLib.Core.AgentApiManagers.LanguageModelProviders;
+
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true, PropertyNamingPolicy = JsonKnownNamingPolicy.Unspecified, WriteIndented = true)]
+[JsonSerializable(typeof(OpenAIProtocolLanguageModelConfiguration))]
+[JsonSerializable(typeof(AgentApiManagerConfiguration))]
+public partial class JsonConfigurationOpenAIProtocolLanguageModelJsonSerializerContext : JsonSerializerContext
+{
+}
