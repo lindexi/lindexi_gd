0ce71e4214eacb2921613cd323b5801885211b91
lindexi <lindexi_gd@outlook.com>
2026-05-22 13:00:59 +0800

    提供从配置文件读取

---
+++ SemanticKernelSamples/AgentLib/AgentLib/Core/AgentApiManagers/LanguageModelProviders/JsonConfigurations/JsonConfigurationOpenAIProtocolLanguageModelProvider.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+using AgentLib.Core.AgentApiManagers.Contexts;
+
+namespace AgentLib.Core.AgentApiManagers.LanguageModelProviders.JsonConfigurations;
+
+public class JsonConfigurationOpenAIProtocolLanguageModelProvider(OpenAIProtocolLanguageModelConfiguration configuration) : OpenAIProtocolLanguageModelProviderBase(configuration.EndPoint, configuration.Key)
+{
+    protected override IReadOnlyList<ModelDefinition> GetModelDefinitions()
+    {
+        return _configuration.ModelDefinitions ?? [];
+    }
+
+    private readonly OpenAIProtocolLanguageModelConfiguration _configuration = configuration;
+
+    public static async Task<JsonConfigurationOpenAIProtocolLanguageModelProvider> FromJsonFileAsync(FileInfo file)
+    {
+        await using var fileStream = file.OpenRead();
+        var configuration = await JsonSerializer.DeserializeAsync(fileStream, JsonTypeInfo);
+        return FromConfiguration(configuration);
+    }
+
+    public static JsonConfigurationOpenAIProtocolLanguageModelProvider FromJsonString(string json)
+    {
+        var configuration = JsonSerializer.Deserialize(json, JsonTypeInfo);
+        return FromConfiguration(configuration);
+    }
+
+    public static JsonConfigurationOpenAIProtocolLanguageModelProvider FromJsonElement(JsonElement jsonElement)
+    {
+        var configuration = jsonElement.Deserialize(JsonTypeInfo);
+        return FromConfiguration(configuration);
+    }
+
+    public static JsonConfigurationOpenAIProtocolLanguageModelProvider FromConfiguration(OpenAIProtocolLanguageModelConfiguration? configuration)
+    {
+        ArgumentNullException.ThrowIfNull(configuration);
+        return new JsonConfigurationOpenAIProtocolLanguageModelProvider(configuration);
+    }
+
+    private static JsonTypeInfo<OpenAIProtocolLanguageModelConfiguration> JsonTypeInfo => JsonConfigurationOpenAIProtocolLanguageModelJsonSerializerContext.Default.OpenAIProtocolLanguageModelConfiguration;
+}
+
+public record OpenAIProtocolLanguageModelConfiguration(string EndPoint, string Key)
+{
+    public IReadOnlyList<ModelDefinition>? ModelDefinitions { get; init; }
+}
+
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true, PropertyNamingPolicy = JsonKnownNamingPolicy.Unspecified, WriteIndented = true)]
+[JsonSerializable(typeof(OpenAIProtocolLanguageModelConfiguration))]
+public partial class JsonConfigurationOpenAIProtocolLanguageModelJsonSerializerContext : JsonSerializerContext
+{
+}
