d714efc89cf2d4c60124cec5224ae66acfcab952
lindexi <lindexi_gd@outlook.com>
2026-06-04 09:29:20 +0800

    搭建基础项目逻辑

---
+++ Pptx/PptxGenerator/Code/PptxGenerator/Core/Ui/Views/CharUserControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PptxGenerator;
+
+public partial class CharUserControl : UserControl
+{
+    public CharUserControl()
+    {
+        InitializeComponent();
+    }
+}
