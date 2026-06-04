38baebf12d9a54f2b9957229b9647c78ced7b499
lindexi <lindexi_gd@outlook.com>
2026-06-04 09:27:05 +0800

    优化界面写法

--- Pptx/PptxGenerator/Code/PptxGenerator/MainWindow.axaml.cs
+++ Pptx/PptxGenerator/Code/PptxGenerator/MainWindow.axaml.cs
@@ -7,9 +7,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        Width = 1580;
-        Height = 980;
-        MinWidth = 1280;
-        MinHeight = 800;
     }
 }
