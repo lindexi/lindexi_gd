eb624ee5e1da9d0da292a56d84c5032bd841a35d
lindexi <lindexi_gd@outlook.com>
2026-06-18 12:50:56 +0800

    重新创建项目

---
+++ SemanticKernelSamples/ChatRoom/Code/ChatRoom.AvaloniaShell/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ChatRoom.AvaloniaShell;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
