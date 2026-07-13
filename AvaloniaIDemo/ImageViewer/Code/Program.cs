6c48cbe0f18cc31776b77a2eff6efd99e9716d19
lindexi <lindexi_gd@163.com>
2026-07-13 10:16:15 +0800

    去掉 SingleInstanceCoordinator 的限制，允许图片查看器多开

--- AvaloniaIDemo/ImageViewer/Code/Program.cs
+++ AvaloniaIDemo/ImageViewer/Code/Program.cs
@@ -5,30 +5,13 @@ namespace ImageViewer;
 
 class Program
 {
-    private static SingleInstanceCoordinator? _singleInstanceCoordinator;
-
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
-        if (SingleInstanceCoordinator.TrySendToExistingInstance(args))
-        {
-            return;
-        }
-
-        _singleInstanceCoordinator = new SingleInstanceCoordinator();
-        _singleInstanceCoordinator.TryStart(path => App.OpenFromExternalInstance(path));
-
-        try
-        {
-            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-        }
-        finally
-        {
-            _singleInstanceCoordinator.Dispose();
-        }
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
