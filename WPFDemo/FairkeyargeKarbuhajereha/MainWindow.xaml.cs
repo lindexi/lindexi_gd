cbef4109c97fc43220663248786dafd518ecf717
lindexi <lindexi_gd@outlook.com>
2026-07-13 09:59:04 +0800

    提供图片查看器的图标

---
+++ WPFDemo/FairkeyargeKarbuhajereha/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FairkeyargeKarbuhajereha;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainWindowViewModel();
+    }
+}
