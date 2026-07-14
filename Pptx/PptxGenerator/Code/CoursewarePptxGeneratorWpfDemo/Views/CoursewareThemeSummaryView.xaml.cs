92850e3f686f193e87d5564be258396d32d6ab5e
lindexi <lindexi_gd@outlook.com>
2026-07-14 08:34:13 +0800

    修复界面没有显示

---
+++ Pptx/PptxGenerator/Code/CoursewarePptxGeneratorWpfDemo/Views/CoursewareThemeSummaryView.xaml.cs
@@ -0,0 +1,17 @@
+using System.Windows.Controls;
+
+namespace CoursewarePptxGeneratorWpfDemo.Views;
+
+/// <summary>
+/// Displays the generated theme summary for the analyzed courseware.
+/// </summary>
+public partial class CoursewareThemeSummaryView : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CoursewareThemeSummaryView" /> class.
+    /// </summary>
+    public CoursewareThemeSummaryView()
+    {
+        InitializeComponent();
+    }
+}
