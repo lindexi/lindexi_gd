3e08e364141949646b6b2c0d352295bd01eecbc7
lindexi <lindexi_gd@outlook.com>
2026-07-20 08:39:46 +0800

    删除图片查看器

--- App/Code/Models/ImageFileInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ImageViewer.Models;
-
-internal sealed record ImageFileInfo(
-    string FilePath,
-    string FileName,
-    long FileSizeBytes,
-    int PixelWidth,
-    int PixelHeight,
-    string Format);
