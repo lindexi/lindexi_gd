56368a54b0c56c520e98cda32b3f422e31f273c4
lindexi <lindexi_gd@outlook.com>
2026-07-22 17:03:18 +0800

    创建专门用来编程的聊天器

---
+++ SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CodingChatRoom.AvaloniaShell;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
