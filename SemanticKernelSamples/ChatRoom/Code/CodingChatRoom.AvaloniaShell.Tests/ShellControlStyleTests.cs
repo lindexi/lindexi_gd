24bfa8c9c7aba26f0826014d5896fe9b1796f06d
lindexi <lindexi_gd@163.com>
2026-09-13 16:39:53 +0800

    换用正确的方式实现窗口背景

--- SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell.Tests/ShellControlStyleTests.cs
+++ SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell.Tests/ShellControlStyleTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
@@ -76,6 +77,16 @@ public sealed class ShellControlStyleTests
             Assert.AreEqual(Colors.White, ((ISolidColorBrush) window.Background).Color);
         }
         finally { window.Close(); }
+    }
+
+    [TestMethod]
+    public void MicaTransparencyShouldUseTransparentWindowBackground()
+    {
+        var converter = new Converters.WindowTransparencyBackgroundConverter();
+
+        var background = (ISolidColorBrush) converter.Convert(WindowTransparencyLevel.Mica, typeof(IBrush), null, CultureInfo.InvariantCulture);
+
+        Assert.AreEqual(Colors.Transparent, background.Color);
     }
 
     [TestMethod]
--- SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell/MainWindow.axaml.cs
+++ SemanticKernelSamples/ChatRoom/Code/CodingChatRoom.AvaloniaShell/MainWindow.axaml.cs
@@ -1,7 +1,5 @@
 using System;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Media;
 using CodingChatRoom.AvaloniaShell.ViewModels;
 
 namespace CodingChatRoom.AvaloniaShell;
@@ -13,24 +11,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        UpdateBackgroundForTransparencyLevel();
         Closing += OnClosing;
-    }
-
-    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
-    {
-        base.OnPropertyChanged(change);
-        if (change.Property == ActualTransparencyLevelProperty)
-        {
-            UpdateBackgroundForTransparencyLevel();
-        }
-    }
-
-    private void UpdateBackgroundForTransparencyLevel()
-    {
-        Background = ActualTransparencyLevel == WindowTransparencyLevel.Mica
-            ? Brushes.Transparent
-            : Brushes.White;
     }
 
     private async void OnClosing(object? sender, WindowClosingEventArgs e)
