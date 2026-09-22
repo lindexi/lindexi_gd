7e5516ee0d04170be41db80b38ccc17224072177
lindexi <lindexi_gd@outlook.com>
2026-09-22 20:54:56 +0800

    鼠标向下滚动居然是负数

---
+++ WPFDemo/KairchayabeDeralkurji/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KairchayabeDeralkurji;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MainWindow_OnMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        TheTextBlock.Text += $"{e.Delta}\r\n";
+    }
+}
